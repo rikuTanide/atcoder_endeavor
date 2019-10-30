dfb0b36ce8bb282dfbae9b2f95fcc4b9235add59
riku <riku@tanide.net>
2019-10-30 21:15:31 +0900

    【TLE】[AtCoder Beginner Contest 144]E - Gluttony

--- abc144/abc144_e.cpp
+++ abc144/abc144_e.cpp
@@ -7,18 +7,20 @@ int main() {
     cin >> n;
     cin >> k;
 
-    vector<uint64_t > costs(n);
-    vector<uint64_t > difficults(n);
-    vector<uint64_t > times(n);
-
+    vector<uint64_t> costs(n);
+    vector<uint64_t> difficults(n);
+    vector<uint64_t> times(n);
 
 
     for (uint64_t j = 0; j < n; j++) {
         cin >> costs.at(j);
     }
 
-    int sum = std::accumulate(costs.begin(), costs.end(), 0);
-    if(sum <= k) {
+    uint64_t sum = 0;
+    for (uint64_t i = 0; i < n; i++) {
+        sum += costs.at(i);
+    }
+    if (sum <= k) {
         cout << 0 << endl;
         return 0;
     }
@@ -28,18 +30,21 @@ int main() {
     }
 
     sort(costs.begin(), costs.end());
-    sort(difficults.begin(), difficults.end(), greater<uint64_t >());
+    sort(difficults.begin(), difficults.end(), greater<uint64_t>());
 
     for (uint64_t j = 0; j < n; j++) {
         times.at(j) = costs.at(j) * difficults.at(j);
     }
 
-    for (uint64_t i = 0; i < k; i++) {
+    for (uint64_t i = 0; i < k;) {
         uint64_t max = 0;
+        uint64_t max2 = 0;
+
         uint64_t cause = 0;
         for (uint64_t j = 0; j < n; j++) {
             uint64_t time = times.at(j);
             if (time > max) {
+                max2 = max;
                 max = time;
                 cause = j;
             }
@@ -51,10 +56,17 @@ int main() {
             cout << 0 << endl;
             return 0;
         }
-        cost--;
+
+        uint64_t diff = max - max2;
+        uint64_t par = diff / difficult;
+        par = par == 0 ? 1 : par;
+        if (par > k) {
+            par = k;
+        }
+        cost -= par;
         times.at(cause) = cost * difficult;
         costs.at(cause) = cost;
-
+        i += par;
     }
 
     uint64_t max = 0;
