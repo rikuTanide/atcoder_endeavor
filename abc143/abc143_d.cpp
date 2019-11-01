68b148ebf37a76e4d79bccbd09a91666264d2ecc
riku <riku@tanide.net>
2019-11-01 19:46:23 +0900

    【TLE】[AtCoder Beginner Contest 143]D - Triangles

---
+++ abc143/abc143_d.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef long long ll;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    vector<int> ls(n);
+
+    rep(i, n) {
+        int l;
+        cin >> l;
+        ls[i] = l;
+    }
+    int count = 0;
+    for (int x = 0; x < n; x++) {
+        for (int y = x + 1; y < n; y++) {
+            for (int z = y + 1; z < n; z++) {
+
+                int a = ls[x], b = ls[y], c = ls[z];
+
+                if (a >= (b + c)) {
+                    continue;
+                }
+                if (b >= (a + c)) {
+                    continue;
+                }
+                if (c >= (b + a)) {
+                    continue;
+                }
+                count++;
+            }
+        }
+    }
+
+
+    cout << count << endl;
+
+}
+
