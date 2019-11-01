ef0b12ff9253aa4b202b9166f9e7e7d6a6d2cbd3
riku <riku@tanide.net>
2019-11-01 19:27:34 +0900

    【AC】[AtCoder Beginner Contest 143]A - Curtain

---
+++ abc143/abc143_a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef long long ll;
+
+int main() {
+
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << max(a - (b * 2), 0) << endl;
+
+}
+
