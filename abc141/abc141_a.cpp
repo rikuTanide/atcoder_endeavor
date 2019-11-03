a76e0df1b80709375268b4dbb434d1d1b8c2c279
riku <riku@tanide.net>
2019-11-03 11:36:59 +0900

    【AC】[AtCoder Beginner Contest 141]A - Weather Prediction

---
+++ abc141/abc141_a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef long long ll;
+
+const int INF = 1001001001;
+
+
+int main() {
+
+    string s;
+    cin >> s;
+
+    if (s == "Sunny") {
+        cout << "Cloudy" << endl;
+    } else if (s == "Cloudy") {
+        cout << "Rainy" << endl;
+    } else {
+        cout << "Sunny" << endl;
+    }
+
+}
