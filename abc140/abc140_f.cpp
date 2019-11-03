3f9706c03f56be3a4615573a5871c1cb2b464926
riku <riku@tanide.net>
2019-11-03 21:14:24 +0900

    【WA】[AtCoder Beginner Contest 140]F - Many Slimes

---
+++ abc140/abc140_f.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef long long ll;
+
+const int INF = 1001001001;
+
+int main() {
+
+    int n;
+
+    cin >> n;
+    int n2 = 1 << n;
+
+    vector<int> a(n2);
+
+    rep(i, n2) {
+        cin >> a[1];
+    }
+
+    sort(a.begin(), a.end());
+
+    vector<int> s;
+    s.push_back(a.back());
+    a.back() = -1;
+
+    rep(i, n) {
+        vector<int> p = s;
+        sort(p.begin(), p.end());
+        int ai = n - 1;
+        while (p.size() > 0 && ai >= 0) {
+            if (a[ai] == -1) {
+                ai--;
+                continue;
+            }
+            if (a[ai] < p.back()) {
+                s.push_back(p.back());
+                p.pop_back();
+            }
+            ai--;
+        }
+        if (p.size() > 0) {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+
+    cout << "Yes" << endl;
+}
