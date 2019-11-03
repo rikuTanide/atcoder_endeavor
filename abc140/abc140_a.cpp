ca39253fc3a561a6425c8284f356012171f5fe20
riku <riku@tanide.net>
2019-11-03 16:44:30 +0900

    【AC】[AtCoder Beginner Contest 140]A - Password

---
+++ abc140/abc140_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef long long ll;
+
+const int INF = 1001001001;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << n * n * n << endl;
+
+
+}
