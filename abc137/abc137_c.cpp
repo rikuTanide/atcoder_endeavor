500e9696aec555861bd2b95c534a3fa376d707df
riku <riku@tanide.net>
2019-11-04 20:49:41 +0900

    【CE】[AtCoder Beginner Contest 137]C - Green Bin

--- abc137/abc137_c.cpp
+++ abc137/abc137_c.cpp
@@ -3,24 +3,30 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 #define sz(x) int(x.size())
+typedef long long ll;
 
 int main() {
 
     int n;
     cin >> n;
-    set<string> strs;
+    map<string, int> strs;
 
-    int ans = 0;
+    ll ans = 0;
     rep(i, n) {
         string str;
         cin >> str;
         sort(str.begin(), str.end());
-        ans += strs.count(str);
-        strs.insert(str);
 
+        if (strs.find(str) == strs.end()) {
+            strs[str] = 1;
+        } else {
+            strs[str] ++;
+        }
     }
 
-
+    for (const auto&[key, value] : strs) {
+        ans += value * (value - 1) / 2;
+    }
 
     cout << ans << endl;
 }
