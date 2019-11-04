bf5ff901edc91f8cc86d8e88e076d54dce52d193
riku <riku@tanide.net>
2019-11-04 13:27:43 +0900

    【AC】[AtCoder Beginner Contest 139]E - League

--- abc139/abc139_e.cpp
+++ abc139/abc139_e.cpp
@@ -5,6 +5,39 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> P;
 const int INF = 1001001001;
+const int MAXN = 1005;
+const int MAXV = MAXN * (MAXN - 1) / 2;
+vector<int> to[MAXV];
+int id[MAXN][MAXN];
+
+int toID(int i, int j) {
+    if (i > j) swap(i, j);
+    return id[i][j];
+}
+
+bool visited[MAXV];
+bool calculated[MAXV];
+int dp[MAXV];
+
+int dfs(int v) {
+    if (visited[v]) {
+        if (!calculated[v]) {
+            return -1;
+        }
+        return dp[v];
+    }
+    visited[v] = true;
+    dp[v] = 1;
+    for (int u : to[v]) {
+        int res = dfs(u);
+        if (res == -1) {
+            return -1;
+        }
+        dp[v] = max(dp[v], res + 1);
+    }
+    calculated[v] = true;
+    return dp[v];
+}
 
 int main() {
 
@@ -20,52 +53,38 @@ int main() {
             c--;
             a[i][j] = c;
         }
-        reverse(a[i].begin(), a[i].end());
     }
 
-    vector<P> q;
-    auto check = [&](int i) {
-        if (a[i].size() == 0) return;
-        int j = a[i].back();
-        if (a[j].size() == 0) return;
-        if (a[j].back() == i) {
-            P p(i, j);
-            if (p.second < p.first) swap(p.first, p.second);
-            q.push_back(p);
+    int V = 0;
+    rep(i, n)rep(j, n) {
+            if (i < j) {
+                id[i][j] = V++;
+            }
         }
-    };
 
     rep(i, n) {
-        check(i);
-    }
-
-    int day = 0;
-    while (q.size() > 0) {
-        day++;
-        sort(q.begin(), q.end());
-        q.erase(unique(q.begin(), q.end()), q.end());
-        vector<P> prevQ;
-        swap(prevQ, q);
-
-        for (P p : prevQ) {
-            int i = p.first, j = p.second;
-            a[i].pop_back();
-            a[j].pop_back();
+        rep(j, n - 1) {
+            a[i][j] = toID(i, a[i][j]);
         }
-
-        for (P p : prevQ) {
-            int i = p.first, j = p.second;
-            check(i);
-            check(j);
+        rep(j, n - 2) {
+            to[a[i][j + 1]].push_back(a[i][j]);
         }
     }
-    rep(i, n) {
-        if(a[i].size() != 0) {
-            cout << -1 << endl;
+
+    int ans = 0;
+
+    rep(i, V) {
+        int res = dfs(i);
+        if (res == -1) {
+            puts("-1");
             return 0;
         }
     }
 
-    cout << day << endl;
+    rep(i, V) {
+        ans = max(ans, dfs(i));
+    }
+
+    cout << ans << endl;
 
 }
