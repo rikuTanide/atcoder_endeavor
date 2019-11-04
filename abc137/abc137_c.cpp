8f5a3bef31693479028693f7b251d3550425484f
riku <riku@tanide.net>
2019-11-04 20:35:11 +0900

    【TLE】[AtCoder Beginner Contest 137]C - Green Bin

--- abc137/abc137_c.cpp
+++ abc137/abc137_c.cpp
@@ -12,31 +12,18 @@ int main() {
 
     rep(i, n) {
         cin >> strs[i];
+        sort(strs[i].begin(), strs[i].end());
     }
 
-    vector<int[26]> counts(n);
-
-    for (int i = 0; i < n; i++) {
-        for (char c : strs[i]) {
-            int index = c - 'a';
-            counts[i][index]++;
-        }
-    }
     int ans = 0;
-
     for (int x = 0; x < n; x++) {
         for (int y = x + 1; y < n; y++) {
-
-            bool b = true;
-            for (int z = 0; z < 26; z++) {
-                if (counts[x][z] != counts[y][z]) {
-                    b = false;
-                    break;
-                }
+            if (strs[x] == strs[y]) {
+                ans++;
             }
-            if (b)ans++;
-
         }
     }
+
+
     cout << ans << endl;
 }
