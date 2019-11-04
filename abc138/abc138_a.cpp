af05824c00454325fdecbb6647d676b68f828db9
riku <riku@tanide.net>
2019-11-04 15:58:42 +0900

    【AC】[AtCoder Beginner Contest 138]A - Red or Not

---
+++ abc138/abc138_a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef long long ll;
+const int INF = 1001001001;
+
+
+int main() {
+
+    int a;
+    string s;
+    cin >> a >> s;
+    if (a < 3200) {
+        cout << "red" << endl;
+    } else {
+        cout << s << endl;
+    }
+
+
+}
