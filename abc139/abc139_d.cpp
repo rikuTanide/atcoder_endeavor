2ef3caeea092f2b55b03e56effa99db7bbcc1338
riku <riku@tanide.net>
2019-11-04 11:56:12 +0900

    【WA】[AtCoder Beginner Contest 139]D - ModSum

---
+++ abc139/abc139_d.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef long long ll;
+
+const int INF = 1001001001;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    cout << ((n - 1) * (n) / 2) << endl;
+
+}
