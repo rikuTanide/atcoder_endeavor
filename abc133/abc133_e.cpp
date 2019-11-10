3b4264f817e920abdbeb6caaa5d03abd056aaa47
riku <riku@tanide.net>
2019-11-10 17:41:24 +0900

    【AC】[AtCoder Beginner Contest 133]E - Virus Tree 2

---
+++ abc133/abc133_e.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+typedef pair<int, int> P;
+const ll INF = 1001001001;
+
+const int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+};
+
+struct combination {
+    vector<mint> fact, ifact;
+
+    combination(int n) : fact(n + 1), ifact(n + 1) {
+        assert(n < mod);
+        fact[0] = 1;
+        for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
+        ifact[n] = fact[n].inv();
+        for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
+    }
+
+    mint operator()(int n, int k) {
+        if (k < 0 || k > n) return 0;
+        return fact[n] * ifact[k] * ifact[n - k];
+    }
+} comb(100005);
+
+vector<int> to[1000005];
+int k;
+mint ans;
+
+mint f(int n, int k) {
+    if(n < 0) return 0;
+    // n  p k
+    mint res = comb(n, k);
+    res *= comb.fact[k];
+    return res;
+}
+
+void dfs(int v, int p = -1) {
+    for (int u : to[v]) {
+        if (u == p) continue;
+        dfs(u, v);
+    }
+    int nk = (p == -1) ? k : k - 2;
+    int c = (p == -1) ? to[v].size() + 1 : to[v].size() - 1;
+    ans *= f(nk, c);
+}
+
+
+int main() {
+    int n;
+    cin >> n >> k;
+    rep(i, n - 1) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        to[a].push_back(b);
+        to[b].push_back(a);
+    }
+    ans = 1;
+    dfs(0);
+    cout << ans.x << endl;
+}
+
