717ed93632115e1f2b702327e77c04334059ae7a
riku <riku@tanide.net>
2019-11-10 15:10:33 +0900

    【AC】[AtCoder Beginner Contest 133]A - T or T

---
+++ abc133/abc133_a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+typedef pair<int, int> P;
+const ll INF = 1001001001;
+
+const int mod = 1000000007;
+
+int main() {
+
+    int n, a, b;
+
+    cin >> n >> a >> b;
+
+    cout << min(n * a, b) << endl;
+
+}
+
