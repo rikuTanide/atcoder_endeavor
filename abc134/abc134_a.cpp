75ac2547289fe98f8f0c2f5a2136eda5dc730e4b
riku <riku@tanide.net>
2019-11-10 11:41:35 +0900

    【AC】[AtCoder Beginner Contest 134]A - Dodecagon

---
+++ abc134/abc134_a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+typedef pair<int, int> P;
+const ll INF = 1001001001;
+static const ll MOD = 1000000007;
+const int n = 13;
+ll dp[100005][n];
+
+int main() {
+    int r;
+    cin >> r;
+
+    cout << 3 * r * r << endl;
+}
+
