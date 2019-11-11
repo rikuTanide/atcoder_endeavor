f037307dec07768ef6ae5caea9590acd7dd201ee
riku <riku@tanide.net>
2019-11-11 22:41:17 +0900

    【AC】[AtCoder Beginner Contest 130]A - Rounding

---
+++ abc130/abc130_a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
+const ll INF = 1001001001;
+const int mod = 1000000007;
+
+int main() {
+    int x, a;
+    cin >> x >> a;
+
+    if (x < a) {
+        cout << 0 << endl;
+    } else {
+        cout << 10 << endl;
+    }
+
+}
