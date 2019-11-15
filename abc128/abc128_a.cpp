83625af9ae4c28404a05afcc5246dfd58c120c50
riku <riku@tanide.net>
2019-11-15 22:20:29 +0900

    【AC】[AtCoder Beginner Contest 128]A - Apple Pie

---
+++ abc128/abc128_a.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 1001001001;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int ans = (3 * a + b) / 2;
+
+    cout << ans << endl;
+}
+
