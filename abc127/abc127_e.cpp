ecef5d12f8981920ecf15d9e499da0cac0770603
riku <riku@tanide.net>
2019-11-17 18:28:48 +0900

    【RE】[AtCoder Beginner Contest 127]E - Cell Distance

---
+++ abc127/abc127_e.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 1001001001;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+
+const int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+};
+
+
+struct combination {
+    vector<mint> fact, ifact;
+
+    combination(int n) : fact(n + 1), ifact(n + 1) {
+        assert(n < mod);
+        fact[0] = 1;
+        for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
+        ifact[n] = fact[n].inv();
+        for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
+    }
+
+    mint operator()(int n, int k) {
+        if (k < 0 || k > n) return 0;
+        return fact[n] * ifact[k] * ifact[n - k];
+    }
+};
+
+int main() {
+
+    ll n, m, k;
+    cin >> n >> m >> k;
+
+    mint ans = 0;
+    for (ll i = 0; i < n; i++) {
+        mint a = i;
+        a = i * (n - 1);
+        a = a * m;
+        a = a * m;
+//        ans += i * (n - i) * m * m;
+        ans += a;
+    }
+
+    for (ll i = 0; i < m; i++) {
+        mint a = i;
+        a = i * (m - 1);
+        a = a * m;
+        a = a * m;
+//        ans += i * (n - i) * m * m;
+        ans += a;
+    }
+    mint cb = combination(10005)(n * m -2, k - 2);
+    ans *= cb;
+
+    cout << ans.x << endl ;
+
+}
