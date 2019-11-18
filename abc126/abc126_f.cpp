040a1008925ce94b71a9bc6efd35b22e79fa8d38
riku <riku@tanide.net>
2019-11-18 20:59:55 +0900

    【WA】[AtCoder Beginner Contest 126]F - XOR Matching

---
+++ abc126/abc126_f.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 1001001001;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+
+const int mod = 1e9 + 7;
+
+ll pow(ll a, ll b) {
+    ll res = a;
+    for (ll i = 0; i < b; i++) {
+        res *= a;
+    }
+    return res;
+}
+
+int main() {
+    ll m, k;
+    cin >> m >> k;
+
+    if (m == 0) {
+        cout << "0 0 1 1" << endl;
+        return 0;
+    }
+
+    if (m == 1) {
+        cout << -1 << endl;
+        return 0;
+    }
+    if (pow(2, m) <= k) {
+        cout << -1 << endl;
+        return 0;
+    }
+
+    vector<ll> ans(2 * k + 2);
+    for (ll i = 0; i < k; i++) {
+        ans[i] = i;
+    }
+    ans[k] = k;
+    for (ll i = 2 * k - 1; i > k; i--) {
+        ans[i] = i;
+    }
+
+    ans[2 * k] = k;
+
+    for (auto i : ans) {
+        cout << ' ' << i;
+    }
+    cout << endl;
+
+}
+
