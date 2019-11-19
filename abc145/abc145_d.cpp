80ac1947f8482e3c83131b99d8aaa1a35a1d3725
riku <riku@tanide.net>
2019-11-19 23:58:12 +0900

    【AC】[AtCoder Beginner Contest 145]D - Knight

--- abc145/abc145_d.cpp
+++ abc145/abc145_d.cpp
@@ -71,8 +71,27 @@ struct mint {
     }
 };
 
+const int MAX = 999999 + 1;
+ll fac[MAX], finv[MAX], inv[MAX];
 
-mint factorial(int i);
+// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++) {
+        fac[i] = fac[i - 1] * i % mod;
+        inv[i] = mod - inv[mod % i] * (mod / i) % mod;
+        finv[i] = finv[i - 1] * inv[i] % mod;
+    }
+}
+
+// 二項係数計算
+ll COM(int n, int k) {
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % mod) % mod;
+}
 
 int main() {
     int x, y;
@@ -89,18 +108,10 @@ int main() {
 
     assert(2 * a + b == x);
     assert(2 * b + a == y);
+    COMinit();
+    ll ans = COM(a + b, a);
 
-    mint mol = factorial(a + b);
-    mint den = factorial(a) * factorial(b);
-
-    cout << (mol / den).x << endl;
+    cout << ans << endl;
 
 }
 
-mint factorial(int i) {
-    mint res = i;
-    for (int x = i - 1; x > 1; x--) {
-        res *= x;
-    }
-    return res;
-}
