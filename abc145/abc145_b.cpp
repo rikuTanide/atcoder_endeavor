c8a81cc35fe65a7481fc9337903a7eafcc001fc0
riku <riku@tanide.net>
2019-11-19 22:02:37 +0900

    【AC】[AtCoder Beginner Contest 145]B - Echo

---
+++ abc145/abc145_b.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 1001001001;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+
+const int mod = 1e9 + 7;
+
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    auto s2 = s.substr(0, n / 2);
+
+    cout << (s2 + s2 == s ? "Yes" : "No") << endl;
+
+}
+
