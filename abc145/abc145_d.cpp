804fcf3ddd9d710d326daac79de6be0988555b34
riku <riku@tanide.net>
2019-11-19 22:50:39 +0900

    【WA】[AtCoder Beginner Contest 145]D - Knight

---
+++ abc145/abc145_d.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 1001001001;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+
+const int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+};
+
+
+mint factorial(int i);
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+
+    if ((x + y) % 3 != 0) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    // __| がA |`が Bとする
+    int b = (2 * y - x) / 3;
+    int a = y - 2 * b;
+
+    assert(2 * a + b == x);
+    assert(2 * b + a == y);
+
+    mint mol = factorial(a + b);
+    mint den = factorial(a) * factorial(b);
+
+    cout << (mol / den).x << endl;
+
+}
+
+mint factorial(int i) {
+    mint res = i;
+    for (int x = i - 1; x > 1; x--) {
+        res *= x;
+    }
+    return res;
+}
