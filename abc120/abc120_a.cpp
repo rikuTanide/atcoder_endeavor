f147208bbc351f158b8f2e319720216cdf055051
riku <riku@tanide.net>
2019-11-21 22:33:34 +0900

    【AC】[AtCoder Beginner Contest 120]A - Favorite Sound

---
+++ abc120/abc120_a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX;
+//const int INF = INT_MAX  ;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+
+const int mod = 1000000007;
+
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << min((b / a), c) << endl;
+
+
+}
+
