f3fcade9bedb253b6481fff5132459b48b0b436c
riku <riku@tanide.net>
2019-11-24 13:59:24 +0900

    【AC】[AtCoder Beginner Contest 119]A - Still TBD

---
+++ abc119/abc119_a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX;
+//const int INF = INT_MAX  ;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+
+const int mod = 1000000007;
+
+vector<P> bridges;
+vector<ll> ans;
+
+
+int main() {
+    string s;
+    cin >> s;
+
+    cout << (s.compare( "2019/04/30") <= 0 ? "Heisei" : "TBD") << endl;
+
+}
+
