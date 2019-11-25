0fdb25c55fe65ebea159662a274f58399c9dec42
riku <riku@tanide.net>
2019-11-25 21:36:22 +0900

    【WA】[AtCoder Beginner Contest 116]B - Collatz Problem

---
+++ abc116/abc116_b.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 10e10;
+const ll MINF = -10e10;
+//const int INF = INT_MAX;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+
+const int mod = 1000000007;
+
+int f(int i) {
+    if (i % 2 == 0) {
+        return i / 2;
+    } else {
+        return 3 * i + 1;
+    }
+
+}
+
+int main() {
+    int s;
+    cin >> s;
+
+    set<int> u;
+
+    int before = s;
+    int count = 1;
+    while (true) {
+        before = f(before);
+        auto it = u.find(before);
+        if (it == u.end()) {
+            u.insert(before);
+            count ++;
+        } else {
+            cout << count + 1 << endl;
+            return 0;
+        }
+    }
+
+}
