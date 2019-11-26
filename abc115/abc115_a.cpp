600944248ffe07b97641a4985dd199317ac77d8c
riku <riku@tanide.net>
2019-11-26 23:09:39 +0900

    【AC】[AtCoder Beginner Contest 115]A - Christmas Eve Eve Eve

---
+++ abc115/abc115_a.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 10e10;
+const ll MINF = -10e10;
+//const int INF = INT_MAX;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+const int mod = 1000000007;
+
+
+int main() {
+
+    int d;
+    cin >> d;
+
+    if (d == 25) {
+        cout << "Christmas" << endl;
+    } else if (d == 24) {
+        cout << "Christmas Eve" << endl;
+    } else if (d == 23) {
+        cout << "Christmas Eve Eve" << endl;
+    } else if (d == 22) {
+        cout << "Christmas Eve Eve Eve" << endl;
+    }
+
+}
