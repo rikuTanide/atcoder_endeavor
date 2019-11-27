a54706389d02df367a0fb5a1cff3b3a9fa6c8fd3
riku <riku@tanide.net>
2019-11-27 12:13:38 +0900

    【AC】[AtCoder Beginner Contest 114]A - 753

---
+++ abc114/abc114_a.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 10e10;
+const ll MINF = -10e10;
+//const int INF = INT_MAX;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+const int mod = 1000000007;
+
+
+int main() {
+
+    int x;
+    cin >> x;
+
+    if (x == 7 || x == 5 || x == 3) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+
+}
+
