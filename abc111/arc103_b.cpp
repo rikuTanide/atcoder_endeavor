f1f8603413cbd0d404ace1a90b64d41c56e6bfa3
riku <riku@tanide.net>
2019-11-28 22:04:58 +0900

    【WA】[AtCoder Beginner Contest 111]D - Robot Arms

--- abc111/arc103_b.cpp
+++ abc111/arc103_b.cpp
@@ -41,7 +41,7 @@ bool checkEnability() {
 }
 
 // ある点がn乗の範囲内に収まるか
-int checkInner(int n, ll x, ll y) {
+int checkOddInner(int n, ll x, ll y) {
     ll boundary = (1ll << n) - 1;
     if (abs(x) + abs(y) <= boundary) {
         return true;
@@ -49,17 +49,225 @@ int checkInner(int n, ll x, ll y) {
     return false;
 }
 
-int maxArmCount() {
+int maxOddArmCount() {
     int ans = 0;
     for (P p : targets) {
         for (int i = 1; i < 40; i++) {
-            if (checkInner(i, p.first, p.second)) {
+            if (checkOddInner(i, p.first, p.second)) {
                 ans = max(ans, i);
                 break;
             }
         }
     }
     return ans;
+}
+
+void culcOdd() {
+
+    // ある点を含む最小の2のn乗
+    // 奇数の時だけ考える
+
+    int max_arm_count = maxOddArmCount();
+
+    for (int i = 1; i <= max_arm_count; i++) {
+        cout << (1ll << (i - 1)) << ' ';
+    }
+    cout << endl;
+
+
+    for (P p : targets) {
+
+        string ans = "";
+        ll prev_x = p.first;
+        ll prev_y = p.second;
+        for (int i = (max_arm_count - 1); i >= 0; i--) {
+            ll arm_length = 1ll << i;
+            // U
+            {
+                ll x = prev_x;
+                ll y = prev_y + arm_length;
+                if (checkOddInner(i, x, y)) {
+                    ans += 'D';
+                    prev_x = x;
+                    prev_y = y;
+                    continue;
+                }
+            }
+            // D
+            {
+                ll x = prev_x;
+                ll y = prev_y - arm_length;
+                if (checkOddInner(i, x, y)) {
+                    ans += 'U';
+                    prev_x = x;
+                    prev_y = y;
+                    continue;
+                }
+            }
+            // R
+            {
+                ll x = prev_x - arm_length;
+                ll y = prev_y;
+                if (checkOddInner(i, x, y)) {
+                    ans += 'R';
+                    prev_x = x;
+                    prev_y = y;
+                    continue;
+                }
+            }
+            // L
+            {
+                ll x = prev_x + arm_length;
+                ll y = prev_y;
+                if (checkOddInner(i, x, y)) {
+                    ans += 'L';
+                    prev_x = x;
+                    prev_y = y;
+                    continue;
+                }
+            }
+
+
+        }
+        reverse(ans.begin(), ans.end());
+        cout << ans << endl;
+    }
+}
+
+// ある点がn乗の範囲内に収まるか
+int checkEvenInner(int n, ll x, ll y) {
+    ll boundary = 1ll << (n);
+    if (abs(x) + abs(y) <= boundary) {
+        return true;
+    }
+    return false;
+}
+
+int maxEvenArmCount() {
+    int ans = 0;
+    for (P p : targets) {
+        for (int i = 1; i < 40; i++) {
+            if (checkEvenInner(i, p.first, p.second)) {
+                ans = max(ans, i);
+                break;
+            }
+        }
+    }
+    return ans;
+}
+
+
+void culcEven() {
+
+    // ある点を含む最小の2のn乗
+    // 奇数の時だけ考える
+
+    int max_arm_count = maxEvenArmCount();
+
+    cout << 1 << ' ';
+    for (int i = 1; i <= max_arm_count; i++) {
+        cout << (1ll << (i - 1)) << ' ';
+    }
+    cout << endl;
+
+
+    for (P p : targets) {
+        string ans = "";
+        ll prev_x = p.first;
+        ll prev_y = p.second;
+        for (int i = (max_arm_count - 1); i >= 0; i--) {
+            ll arm_length = 1ll << i;
+            // U
+            {
+                ll x = prev_x;
+                ll y = prev_y + arm_length;
+                if (checkEvenInner(i, x, y)) {
+                    ans += 'D';
+                    prev_x = x;
+                    prev_y = y;
+                    continue;
+                }
+            }
+            // D
+            {
+                ll x = prev_x;
+                ll y = prev_y - arm_length;
+                if (checkEvenInner(i, x, y)) {
+                    ans += 'U';
+                    prev_x = x;
+                    prev_y = y;
+                    continue;
+                }
+            }
+            // L
+            {
+                ll x = prev_x - arm_length;
+                ll y = prev_y;
+                if (checkEvenInner(i, x, y)) {
+                    ans += 'R';
+                    prev_x = x;
+                    prev_y = y;
+                    continue;
+                }
+            }
+            // R
+            {
+                ll x = prev_x + arm_length;
+                ll y = prev_y;
+                if (checkEvenInner(i, x, y)) {
+                    ans += 'L';
+                    prev_x = x;
+                    prev_y = y;
+                    continue;
+                }
+            }
+
+
+        }
+        [&] {
+            // ここから最後の一本
+            // U
+            {
+                ll x = prev_x;
+                ll y = prev_y + 1;
+                if (x == 0 && y == 0) {
+                    ans += 'D';
+                    return;
+                }
+            }
+            // D
+            {
+                ll x = prev_x;
+                ll y = prev_y - 1;
+                if (x == 0 && y == 0) {
+                    ans += 'U';
+                    return;
+                }
+            }
+            // L
+            {
+                ll x = prev_x - 1;
+                ll y = prev_y;
+                if (x == 0 && y == 0) {
+                    ans += 'R';
+                    return;
+                }
+            }
+            // R
+            {
+                ll x = prev_x + 1;
+                ll y = prev_y;
+                if (x == 0 && y == 0) {
+                    ans += 'L';
+                    return;
+                }
+            }
+        }();
+        // ここまで最後の一本
+
+        reverse(ans.begin(), ans.end());
+        cout << ans << endl;
+    }
 }
 
 int main() {
@@ -80,75 +288,10 @@ int main() {
         return 0;
     }
 
-    // ある点を含む最小の2のn乗
-    // 奇数の時だけ考える
-
-    int max_arm_count = maxArmCount();
-
-    for (int i = 1; i <= max_arm_count; i++) {
-        cout << (1ll << (i - 1)) << ' ';
-    }
-    cout << endl;
-
-    if((targets[0].first + targets[0].second)%2 ==0  ) {
-        throw "mada kaite nai";
+    if ((targets[0].first + targets[0].second) % 2 == 0) {
+        culcEven();
+    } else {
+        culcOdd();
     }
 
-    for (P p : targets) {
-
-        string ans = "";
-        ll prev_x = p.first;
-        ll prev_y = p.second;
-        for (int i = (max_arm_count - 1); i >= 0; i--) {
-            ll arm_length = 1ll << i;
-            // U
-            {
-                ll x = prev_x;
-                ll y = prev_y + arm_length;
-                if (checkInner(i, x, y)) {
-                    ans += 'D';
-                    prev_x = x;
-                    prev_y = y;
-                    continue;
-                }
-            }
-            // D
-            {
-                ll x = prev_x;
-                ll y = prev_y - arm_length;
-                if (checkInner(i, x, y)) {
-                    ans += 'U';
-                    prev_x = x;
-                    prev_y = y;
-                    continue;
-                }
-            }
-            // R
-            {
-                ll x = prev_x - arm_length;
-                ll y = prev_y;
-                if (checkInner(i, x, y)) {
-                    ans += 'R';
-                    prev_x = x;
-                    prev_y = y;
-                    continue;
-                }
-            }
-            // L
-            {
-                ll x = prev_x+arm_length;
-                ll y = prev_y;
-                if (checkInner(i, x, y)) {
-                    ans += 'L';
-                    prev_x = x;
-                    prev_y = y;
-                    continue;
-                }
-            }
-
-
-        }
-        reverse(ans.begin(), ans.end());
-        cout << ans << endl;
-    }
 }
