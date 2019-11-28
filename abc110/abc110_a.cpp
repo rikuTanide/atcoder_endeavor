bcfc4263068dc2e2b4bf6a110cd4aaa26b250ca8
riku <riku@tanide.net>
2019-11-28 22:20:37 +0900

    【AC】[AtCoder Beginner Contest 110]A - Maximize the Formula

---
+++ abc110/abc110_a.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 10e10;
+const ll MINF = -10e10;
+//const int INF = INT_MAX;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+const int mod = 1000000007;
+//ifstream myfile("C:\\Users\\riku\\Downloads\\01.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+
+
+int main() {
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    int ans = 0;
+    {
+        int now = 10 * a + b + c;
+        ans = max(ans, now);
+    }
+    {
+        int now = a + 10 * b + c;
+        ans = max(ans, now);
+    }
+    {
+        int now = a + b + c * 10;
+        ans = max(ans, now);
+    }
+
+    cout << ans << endl;
+}
