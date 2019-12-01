82df09121d4970ecb56e80677b7bada5c3147d27
riku <riku@tanide.net>
2019-12-01 19:18:28 +0900

    【AC】[AtCoder Beginner Contest 106]B - 105

---
+++ abc106/abc106_b.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 10e10;
+const ll MINF = -10e10;
+//const int INF = INT_MAX;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+const int mod = 1000000007;
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+
+int factorize(int n) {
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0)continue;
+        if(factorize(i) == 8) {
+            count ++;
+        }
+    }
+    cout << count << endl;
+}
