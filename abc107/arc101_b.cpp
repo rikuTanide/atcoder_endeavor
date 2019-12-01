657cc848e9c38d45e6c62b639fae0952a08db18e
riku <riku@tanide.net>
2019-12-01 17:43:34 +0900

    【WA】[AtCoder Beginner Contest 107]D - Median of Medians

--- abc107/arc101_b.cpp
+++ abc107/arc101_b.cpp
@@ -16,7 +16,7 @@ const ll MINF = -10e10;
 
 typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 const int mod = 1000000007;
-//ifstream myfile("C:\\Users\\riku\\Downloads\\01.txt");
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 
@@ -29,10 +29,73 @@ int comp(ll i, ll j) {
         return 1;
     }
 }
+// https://atcoder.jp/contests/abc107/submissions/8719924
+// 転倒数の数え方が理解できなかったのでお借りした。
+
+// Binary Indexed Tree モジュール (0-indexed)
+template<typename T>
+class BIT {
+private:
+    vector<T> val;
+public:
+    ll n;
+
+    BIT(ll _n) : val(_n + 1, 0), n(_n) {}
+
+    BIT(vector<T> arr) : val(arr.size() + 1, 0), n(arr.size()) {
+        rep(i, n) add(i, arr[i]);
+    }
+
+    // a[i] に v を足す O(logN)
+    void add(const ll _i, const T _v) {
+        for (ll i = _i + 1; i <= n; i += i & (-i)) {
+            val[i] += _v;
+        }
+    }
+
+    // [0, _i) の和を求める O(logN)
+    T get(ll i) const {
+        T ans = 0;
+        for (; i > 0; i ^= i & (-i)) {
+            ans += val[i];
+        }
+        return ans;
+    }
+
+    // [l, r) の和を求める o(N)
+    T sum(const ll l, const ll r) {
+        return get(r) - get(l);
+    }
+};
+
+size_t inversion(vector<ll> &x0) {
+
+    vector<ll> x = x0;
+    const int n = x0.size();
+
+    // 座圧
+    vector<ll> unq = x;
+    sort(unq.begin(), unq.end());
+    unq.erase(unique(unq.begin(), unq.end()), unq.end());
+    map<ll, int> sahz;
+    for (size_t i = 0; i < unq.size(); i++) sahz[unq[i]] = i;
+    for (size_t i = 0; i < x.size(); i++) x[i] = sahz[x[i]];
+
+    // BITで転倒数を求める
+    size_t ans = 0;
+    BIT<int> bit(n);
+    for (int i = 0; i < n; i++) {
+        ans += bit.sum(x[i] + 1, n);
+        bit.add(x[i], 1);
+    }
+
+    return ans;
+
+}
 
 
 int main() {
-    int n;
+    ll n;
     cin >> n;
 
     vector<ll> as(n);
@@ -44,7 +107,7 @@ int main() {
     rep(i, n) min_a = min(min_a, as[i]);
 
     vector<ll> u_or_d(n);
-    vector<ll> ruisekiwa(n + 1);
+    vector<ll> ruisekiwa(n);
 
     ll cell = max_a + 1;
     ll floor = min_a;
@@ -53,27 +116,12 @@ int main() {
 
         rep(j, n) u_or_d[j] = comp(mid, as[j]);
 
-        ruisekiwa[0] = 0;
-        for (int i = 0; i <= n; i++) {
-            ruisekiwa[i + 1] = ruisekiwa[i] + u_or_d[i];
+        ruisekiwa[0] = u_or_d[0];
+        for (int i = 1; i < n; i++) {
+            ruisekiwa[i] = ruisekiwa[i - 1] + u_or_d[i];
         }
 
-
-        bool check = [&]() {
-            ll count = 0;
-            for (ll x = 0; x < n; x++) {
-                for (ll y = x; y < n; y++) {
-                    ll rw = ruisekiwa[y + 1] - ruisekiwa[x];
-                    if (rw >= 0) {
-                        count++;
-                    } else {
-                        count--;
-                    }
-                }
-            }
-            return count >= 0;
-        }();
-
+        bool check = inversion(ruisekiwa) >= 0;
 
         if (!check) {
             cell = mid;
