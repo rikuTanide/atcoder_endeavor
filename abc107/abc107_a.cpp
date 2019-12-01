57572e1fc1ac577aab4918f47542c24f06fcec98
riku <riku@tanide.net>
2019-12-01 12:46:30 +0900

    【AC】[AtCoder Beginner Contest 107]A - Train

---
+++ abc107/abc107_a.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 10e10;
+const ll MINF = -10e10;
+//const int INF = INT_MAX;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+const int mod = 1000000007;
+//ifstream myfile("C:\\Users\\riku\\Downloads\\01.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+
+int main() {
+
+
+    int n, i;
+
+    cin >> n >> i;
+
+    cout << n - i + 1 << endl;
+
+}
