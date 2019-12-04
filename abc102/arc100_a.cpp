96744677e0ce053f6d2686045a44007772c8fd2a
riku <riku@tanide.net>
2019-12-04 16:42:48 +0900

    【WA】[AtCoder Beginner Contest 102]C - Linear Approximation

---
+++ abc102/arc100_a.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 10e10;
+const ll MINF = -10e10;
+//const int INF = INT_MAX;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+const int mod = 1000000007;
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+typedef tuple<int, int, int> Q;
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<ll> as(n);
+    rep(i, n) {
+        cin >> as[i];
+        as[i] -= i;
+    }
+
+    ll sum = accumulate(as.begin(), as.end(), 0ll);
+    ll ave = sum / n;
+
+    ll ans = 0;
+    rep(i, n) {
+        ans += abs(as[i] - ave);
+    }
+
+    cout << ans << endl;
+
+}
+
+
