9e096bf20636af399670eef5beeb067fbcd32bef
riku <riku@tanide.net>
2019-12-06 20:56:43 +0900

    【AC】[AtCoder Beginner Contest 099]B - Stone Monument

---
+++ abc099/abc099_b.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 10e10;
+const ll MINF = -10e10;
+//const int INF = INT_MAX;
+#define mins(x, y) x = min(x, y)
+#define maxs(x, y) x = max(x, y)
+
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+const int mod = 1000000007;
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+int main() {
+    ll a, b;
+    cin >> a >> b;
+    ll k = b - a;
+    ll m = k * (k - 1) / 2;
+    ll ans = m - a;
+
+    cout << ans << endl;
+
+}
