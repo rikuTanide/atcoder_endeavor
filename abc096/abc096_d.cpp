265f791424be781f711f09de678691df8e9b5bbe
riku <riku@tanide.net>
2019-12-08 16:36:09 +0900

    【WA】[AtCoder Beginner Contest 096]D - Five, Five Everywhere

---
+++ abc096/abc096_d.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 10e10;
+const ll MINF = -10e10;
+//const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
+const int mod = 1000000007;
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+bool isSosu(ll n) {
+    ll b = sqrt(n);
+    for (int i = 2; i < b; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> ans;
+    int now = 1;
+    while (ans.size() < n) {
+        now += 5;
+        if (isSosu(now)) {
+            ans.push_back(now);
+        }
+    }
+
+    rep(i, n) {
+        cout << ans[i] << ' ';
+    }
+
+    cout << endl;
+}
