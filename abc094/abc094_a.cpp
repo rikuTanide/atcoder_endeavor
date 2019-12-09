7521a9ec4caeec37bef28fb025be06bde434b4a0
riku <riku@tanide.net>
2019-12-09 21:23:36 +0900

    【AC】[AtCoder Beginner Contest 094]A - Cats and Dogs

---
+++ abc094/abc094_a.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 10e15;
+const ll MINF = -10e10;
+//const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
+const int mod = 1000000007;
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+
+
+int main() {
+
+    int a, b, x;
+    cin >> a >> b >> x;
+
+    if((a + b) < x ) {
+        cout << "NO" << endl;
+        return 0;
+    }
+
+    if(a > x) {
+        cout << "NO" << endl;
+        return 0;
+    }
+
+    cout << "YES" << endl;
+
+}
