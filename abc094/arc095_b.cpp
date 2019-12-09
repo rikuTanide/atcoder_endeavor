e49f7149ace73ad9301973c58e959508c253e0b2
riku <riku@tanide.net>
2019-12-09 22:57:20 +0900

    【AC】[AtCoder Beginner Contest 094]D - Binomial Coefficients

--- abc094/arc095_b.cpp
+++ abc094/arc095_b.cpp
@@ -15,114 +15,50 @@ const ll MINF = -10e10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
-const int mod = 1000000007;
-
-
-struct mint {
-    ll x; // typedef long long ll;
-    mint(ll x = 0) : x((x % mod + mod) % mod) {}
-
-    mint &operator+=(const mint a) {
-        if ((x += a.x) >= mod) x -= mod;
-        return *this;
-    }
-
-    mint &operator-=(const mint a) {
-        if ((x += mod - a.x) >= mod) x -= mod;
-        return *this;
-    }
-
-    mint &operator*=(const mint a) {
-        (x *= a.x) %= mod;
-        return *this;
-    }
-
-    mint operator+(const mint a) const {
-        mint res(*this);
-        return res += a;
-    }
-
-    mint operator-(const mint a) const {
-        mint res(*this);
-        return res -= a;
-    }
-
-    mint operator*(const mint a) const {
-        mint res(*this);
-        return res *= a;
-    }
-
-    mint pow(ll t) const {
-        if (!t) return 1;
-        mint a = pow(t >> 1);
-        a *= a;
-        if (t & 1) a *= *this;
-        return a;
-    }
-
-    // for prime mod
-    mint inv() const {
-        return pow(mod - 2);
-    }
-
-    mint &operator/=(const mint a) {
-        return (*this) *= a.inv();
-    }
-
-    mint operator/(const mint a) const {
-        mint res(*this);
-        return res /= a;
-    }
-};
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
 
-struct combination {
-    vector<mint> fact, ifact;
-
-    combination(int n) : fact(n + 1), ifact(n + 1) {
-        assert(n < mod);
-        fact[0] = 1;
-        for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
-        ifact[n] = fact[n].inv();
-        for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
-    }
-
-    mint operator()(int n, int k) {
-        if (k < 0 || k > n) return 0;
-        return fact[n] * ifact[k] * ifact[n - k];
-    }
-};
+typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
+const int mod = 1000000007;
 
 int main() {
+//    ifstream myfile("C:\\Users\\riku\\Downloads\\even0.txt");
+//
+//    ofstream outfile("log.txt");
+
     ll n;
     cin >> n;
+//    myfile >> n;
     vector<ll> as(n);
     rep(i, n) {
+//        myfile >> as[i];
         cin >> as[i];
     }
 
     sort(as.begin(), as.end());
     ll mx = as[n - 1];
 
-    combination c(mx);
 
-
-    ll mxc = 0;
-    ll mi = 0;
+    ll diff_min = INF;
+    ll comb_min;
     for (ll i = 0; i < n - 1; i++) {
-        mint now = c(mx, as[i]);
-        if (mxc < now.x) {
-            mxc = now.x;
-            mi = as[i];
+        ll u = as[i];
+        ll v = u;
+        if ((u * 2) >= mx) {
+            v = mx - u;
+        }
+
+        ll d = abs((v * 2) - mx);
+
+
+        if (diff_min > d) {
+            comb_min = u;
+            diff_min = d;
         }
     }
-
-    cout << mx << ' ' << mi << endl;
+    cout << mx << ' ' <<  comb_min << endl;
 
 }
