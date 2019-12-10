25d70444dc119e474e2f0319c9948fbcbd1bcb19
riku <riku@tanide.net>
2019-12-10 20:15:26 +0900

    【AC】[AtCoder Beginner Contest 093]B - Small and Large Integers

---
+++ abc093/abc093_b.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = 10e15;
+const ll MINF = -10e10;
+//const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
+const int mod = 1000000007;
+
+int main() {
+
+    ll a, b, k;
+    cin >> a >> b >> k;
+
+    set<ll> ans;
+    for (ll i = 0; i < k; i++) {
+        if ((a + i) > b) break;
+        ans.insert(a + i);
+    }
+
+    for (ll i = 0; i < k; i++) {
+        if ((b - i) < a) break;
+        ans.insert(b - i);
+    }
+
+    for (ll i : ans) {
+        cout << i << endl;
+    }
+
+
+}
