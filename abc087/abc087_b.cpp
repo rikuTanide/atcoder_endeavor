66e7738353a3bc557c9f82e11207e87bb58497d2
riku <riku@tanide.net>
2019-12-13 18:23:15 +0900

    【AC】[AtCoder Beginner Contest 087]B - Coins

---
+++ abc087/abc087_b.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = 10e15;
+const ll MINF = -10e10;
+const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
+const int mod = 1000000007;
+
+
+int main() {
+    int a, b, c, x;
+    cin >> a >> b >> c >> x;
+
+    int ans = 0;
+
+    for (int i = 0; i <= a; i++) {
+        for (int j = 0; j <= b; j++) {
+            for (int k = 0; k <= c; k++) {
+                int now = i * 500 + j * 100 + k * 50;
+                if (now == x) ans++;
+            }
+        }
+    }
+
+    cout << ans << endl;
+
+}
