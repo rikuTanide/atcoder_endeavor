37079007644ce842358a3788db631c2830d7bc94
riku <riku@tanide.net>
2019-12-16 20:23:28 +0900

    【AC】[AtCoder Beginner Contest 081]B - Shift only

---
+++ abc081/abc081_b.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = 10e15;
+const ll MINF = -10e10;
+const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
+const int mod = 1000000007;
+typedef pair<int, char> P;
+
+int mod2(int n) {
+    int ans = 0;
+    while (n % 2 == 0) {
+        n /= 2;
+        ans++;
+    }
+    return ans;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    int ans = INF;
+    rep(i, n) {
+        int a;
+        cin >> a;
+        cmin(ans, mod2(a));
+    }
+
+    cout << ans << endl;
+
+}
