ab2e52d42aa1d1b35d42edc3dbd297b14786d518
riku <riku@tanide.net>
2019-12-17 20:30:40 +0900

    【WA】[AtCoder Beginner Contest 080]D - Recording

--- abc080/abc080_d.cpp
+++ abc080/abc080_d.cpp
@@ -71,7 +71,7 @@ int main() {
         for (Program p : programs[ci]) {
             // 重複の場合は2台いるからSが先
             all.push_back(P(p.start, 'S'));
-            all.push_back(P(p.end, 'T'));
+            all.push_back(P(p.end, 'E'));
         }
     }
 
@@ -81,7 +81,7 @@ int main() {
     int now = 0;
 
     for (P p : all) {
-        if (p.second == 'T') {
+        if (p.second == 'E') {
             now--;
         } else {
             now++;
