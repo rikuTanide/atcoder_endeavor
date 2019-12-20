bc304e36bd6e60bd81e7ede2bcbe4efa564382a1
riku <riku@tanide.net>
2019-12-20 18:24:32 +0900

    【AC】[AtCoder Beginner Contest 073]B - Theater

---
+++ abc073/abc073_b.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = 10e15;
+const ll MINF = -10e10;
+const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+
+//ifstream myfile("~/Downloads/02.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
+const int mod = 1000000007;
+
+
+int main() {
+
+    int n;
+    cin >> n;
+
+
+    ll ans = 0;
+    rep(i,  n){
+        ll l, r;
+        cin >> l >> r;
+
+        ans += r - l + 1;
+
+    }
+
+    cout << ans << endl;
+
+}
