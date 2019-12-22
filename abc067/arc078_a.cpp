730cdd3d3884d96d59ecb886c6d05d16fa5cc572
riku <riku@tanide.net>
2019-12-22 20:46:41 +0900

    【WA】[AtCoder Beginner Contest 067]C - Splitting Pile

---
+++ abc067/arc078_a.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+#include <assert.h>    // LON
+#include <math.h>    // sqrt()
+
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX;
+const ll MINF = -10e10;
+const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
+const int mod = 1000000007;
+
+void ok() {
+    cout << "Possible" << endl;
+}
+
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return 0;
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void calculate() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    CumulativeSum cs(n);
+    rep(i, n) {
+        int a;
+        cin >> a;
+        cs.set(i, a);
+    }
+
+    cs.calculate();
+
+    cout << endl;
+
+    int ans = INF;
+    for (int i = 0; i < n - 1; i++) {
+        int now = abs(cs.getSum(i) - cs.getSectionSum(i + 1, n - 1));
+        cmin(ans, now);
+    }
+    cout << ans << endl;
+
+}
