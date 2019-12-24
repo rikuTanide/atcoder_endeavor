16eb3ad76e09409d50550fafe347e71bbc2e54c6
riku <riku@tanide.net>
2019-12-24 22:38:17 +0900

    【AC】[AtCoder Beginner Contest 064]D - Insertion

---
+++ abc064/abc064_d.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+#include <assert.h>    // LON
+#include <math.h>    // sqrt()
+
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX;
+const ll MINF = -10e10;
+const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
+const int mod = 1000000007;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    stack<char> st;
+    string s;
+    cin >> s;
+
+    int left = 0;
+
+    for (char c :  s) {
+        if (c == '(') {
+            st.push('(');
+        } else {
+            if (st.empty()) {
+                left++;
+            } else {
+                st.pop();
+            }
+        }
+    }
+
+    int right = st.size();
+
+    rep(i, left) {
+        cout << '(';
+    }
+    cout << s;
+    rep(i, right) {
+        cout << ')';
+    }
+
+    cout << endl;
+
+
+}
