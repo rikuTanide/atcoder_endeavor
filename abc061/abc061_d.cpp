2f343e8b2e31b30b41449db66ef041b3912a126f
riku <riku@tanide.net>
2019-12-25 15:52:49 +0900

    【WA】[AtCoder Beginner Contest 061]D - Score Attack

---
+++ abc061/abc061_d.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+#include <assert.h>    // LON
+#include <math.h>    // sqrt()
+
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX;
+const ll MINF = -10e10;
+//const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+// https://qiita.com/wakimiko/items/69b86627bea0e8fe29d5
+struct Edge {
+    ll from; //出発点
+    ll to;   //到達点
+    ll cost; //移動コスト
+};
+
+ll bellmanFord(ll v, ll s, ll g, vector<Edge> &routes) {
+
+    vector<ll> costs(v, INF);
+
+    costs[s] = 0;
+
+    for (ll i = 0; i < v; i++) {
+        for (ll j = 0; j < (ll) routes.size(); j++) {
+
+            Edge e = routes[j];
+
+            if (costs[e.to] > costs[e.from] + e.cost) {  //移動した後のコストが小さいと、頂点のコストを更新
+                costs[e.to] = costs[e.from] + e.cost;
+                if (i == v - 1) {
+                    return MINF;
+                }
+            }
+        }
+    }
+    return costs[g];
+}
+
+int main() {
+    ll n, m;
+    cin >> n >> m;
+
+    vector<Edge> routes(m);
+
+    rep(i, m) {
+        ll a, b, c;
+        cin >> a >> b >> c;
+        a--;
+        b--;
+        c *= -1;
+        Edge edge = {a, b, c};
+        routes.push_back(edge);
+    }
+
+    ll ans = bellmanFord(n, 0, n - 1, routes);
+
+    if (ans == MINF) {
+        cout << "inf" << endl;
+        return 0;
+    }
+
+    cout << -ans << endl;
+}
+
