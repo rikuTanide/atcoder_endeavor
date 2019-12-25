ea2edee152948839f7c20edec29536805ef32223
riku <riku@tanide.net>
2019-12-25 15:32:52 +0900

    【WA】[AtCoder Beginner Contest 061]C - Big Array

---
+++ abc061/abc061_c.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+#include <assert.h>    // LON
+#include <math.h>    // sqrt()
+
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX;
+const ll MINF = -10e10;
+//const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return 0;
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void calculate() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+    int lb(int k) {
+        auto it = lower_bound(sums.begin(), sums.end(), k);
+        return distance(sums.begin(), it);
+    }
+
+};
+
+
+int main() {
+
+    int n, k;
+    cin >> n >> k;
+
+    map<int, int> numbers;
+    rep(i, n) {
+        int a, b;
+        cin >> a >> b;
+        numbers[a] += b;
+    }
+
+    map<int, int> index_to_ans;
+
+    CumulativeSum cs(numbers.size());
+    {
+        int i = 0;
+        for (auto e : numbers) {
+            cs.set(i, e.second);
+            index_to_ans[i] = e.first;
+            i++;
+        }
+    }
+    cs.calculate();
+    int index = cs.lb(k);
+
+    cout << index_to_ans[index] << endl;
+
+}
+
