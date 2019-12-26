4cc704902a35240c7c77889b106126428cb6dbde
riku <riku@tanide.net>
2019-12-26 20:38:28 +0900

    【AC】[AtCoder Beginner Contest 058]C - Dubious Document

---
+++ abc058/arc071_a.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+#include <assert.h>    // LON
+#include <math.h>    // sqrt()
+
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+
+int main() {
+    int n;
+
+    cin >> n;
+    vector<int> characters(26, 0);
+    {
+        string s;
+        cin >> s;
+        for (char c : s) {
+            int index = c - 'a';
+            characters[index]++;
+        }
+    }
+    rep(i, n - 1) {
+        vector<int> nc(26, 0);
+        string s;
+        cin >> s;
+        for (char c : s) {
+            int index = c - 'a';
+            nc[index]++;
+        }
+        for (int j = 0; j < 26; j++) {
+            characters[j] = min(characters[j], nc[j]);
+        }
+    }
+
+    multiset<char> cs;
+
+    for (int i = 0; i < 26; i++) {
+
+        for (int j = 0; j < characters[i]; j++) {
+            cs.insert('a' + i);
+        }
+
+    }
+
+    for(char c : cs) {
+        cout << c ;
+    }
+    cout << endl;
+
+}
+
