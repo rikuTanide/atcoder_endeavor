58b02b19f4fa7975a19f210bf55914343e7a3546
riku <riku@tanide.net>
2019-12-28 21:08:38 +0900

    【WA】[AtCoder Grand Contest 041]A - Table Tennis Training

---
+++ agc041/agc041_a.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+//const int mod = 1000000007;
+const ll mod = INF / 1000;
+
+int main() {
+    ll n, a, b;
+    cin >> n >> a >> b;
+
+    if (a < b) {
+        swap(a, b);
+    }
+
+    if ((a - b)% 2 == 0) {
+        cout << (a - b) / 2 << endl;
+    } else {
+        ll k = a - 1;
+        ll l = n - b ;
+
+        cout << min(k, l) << endl;
+    }
+}
