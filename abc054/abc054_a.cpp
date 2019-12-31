eaf05720bdbe4e59f0d54b8459dfdbe4db727eff
riku <riku@tanide.net>
2019-12-31 19:41:19 +0900

    【AC】[AtCoder Beginner Contest 054]A - One Card Poker

---
+++ abc054/abc054_a.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+
+int main() {
+
+    int a, b;
+    cin >> a >> b;
+    if (a == 1) {
+        a = 14;
+    }
+    if (b == 1) {
+        b = 14;
+    }
+
+    if (a < b) {
+        cout << "Bob" << endl;
+    } else if (a == b) {
+        cout << "Draw" << endl;
+    } else {
+        cout << "Alice" << endl;
+    }
+
+
+}
+
