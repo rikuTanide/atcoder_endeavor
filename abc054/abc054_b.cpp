731e035cc8a50c7a413de0ca1ba6620a605f2d5a
riku <riku@tanide.net>
2019-12-31 19:55:19 +0900

    【WA】[AtCoder Beginner Contest 054]B - Template Matching

---
+++ abc054/abc054_b.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<char>> outer(n, vector<char>(n));
+    vector<vector<char>> inner(m, vector<char>(m));
+
+    rep(i, n) rep(j, n) {
+            cin >> outer[i][j];
+        }
+
+    rep(i, m) rep(j, m) {
+            cin >> inner[i][j];
+        }
+
+    auto check = [&](int xs, int ys) {
+        for (int x = 0; x < m; x++) {
+            for (int y = 0; y < m; y++) {
+                if (outer[x + xs][y + ys] != inner[x][y]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    };
+
+
+    for (int x = 0; x < n - m; x++) {
+        for (int y = 0; y < n - m; y++) {
+            if (check(x, y)) {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+
+    cout << "No" << endl;
+}
+
