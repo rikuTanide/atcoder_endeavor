0d9b6f9441395f0f7d9c059867b2b63035f7c4f8
riku <riku@tanide.net>
2020-01-01 16:40:16 +0900

    【AC】[AtCoder Beginner Contest 053]A - ABC/ARC

---
+++ abc053/abc053_a.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+int main() {
+    int x;
+    cin >> x;
+
+    if (x < 1200) {
+        cout << "ABC" << endl;
+
+    } else {
+        cout << "ARC" << endl;
+    };
+}
+
