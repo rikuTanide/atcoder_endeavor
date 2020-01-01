e806c43dc3cc1d7dc897c3c3ec3cecd614dfdebe
riku <riku@tanide.net>
2020-01-01 17:18:34 +0900

    【AC】[AtCoder Beginner Contest 053]D - Card Eater

---
+++ abc053/arc068_b.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+int main() {
+    int n;
+    cin >> n;
+
+    map<int, int> count;
+    rep(i, n) {
+        int a;
+        cin >> a;
+        count[a]++;
+    }
+
+    int sum = 0;
+    for (auto e : count) {
+        sum += (e.second - 1);
+    }
+    int ans = count.size();
+    if (sum % 2 == 1) {
+        ans -= 1;
+    }
+    cout << ans << endl;
+}
