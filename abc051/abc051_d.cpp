03d1a7d5515079db030b22e1b79a8c56ca6b7f5b
riku <riku@tanide.net>
2020-01-01 19:22:00 +0900

    【AC】[ AtCoder Beginner Contest 051]D - Candidates of No Shortest Paths

---
+++ abc051/abc051_d.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+
+// https://qiita.com/ta-ka/items/a023a11efe17ab097433
+struct Edge {
+    ll to, cost;
+};
+vector<vector<Edge>> edges;
+
+void insertTowWay(ll from, ll to, ll cost) {
+    edges[from].push_back({to, cost});
+    edges[to].push_back({from, cost});
+}
+
+class Dijkstra {
+    ll vector_count;
+
+    vector<ll> distances;
+
+public:
+    Dijkstra(ll n) {
+        vector_count = n;
+        distances.resize(n, INF);
+    }
+
+
+    void dijkstra(ll k) {
+        distances[k] = 0;  // 始点sへの最短距離は0
+
+        priority_queue<P, vector<P>, greater<P>> que;  // 距離が小さい順に取り出せるようgreater<P>を指定
+        que.push(P(0, k));
+        while (!que.empty()) {
+            P p = que.top();
+            que.pop();
+            int v = p.second;  // 更新した頂点の中で距離が最小の頂点v
+            if (distances[v] < p.first) {
+                continue;
+            }
+            for (auto e : edges[v]) {  // 頂点vから出る辺eを走査
+                if (distances[e.to] > distances[v] + e.cost) {
+                    distances[e.to] = distances[v] + e.cost;
+                    que.push(P(distances[e.to], e.to));
+                }
+            }
+        }
+    }
+
+    ll calcDistance(ll to) {
+        return distances[to];
+    }
+
+};
+
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+
+    vector<Dijkstra> dijkstra_list;
+    rep(i, n) {
+        Dijkstra d(n);
+        dijkstra_list.push_back(d);
+    }
+
+
+    edges.resize(n);
+    rep(i, m) {
+        ll a, b, c;
+        cin >> a >> b >> c;
+        a--;
+        b--;
+        insertTowWay(a, b, c);
+    }
+
+    rep(i, n) {
+        dijkstra_list[i].dijkstra(i);
+    }
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        vector<Edge> es = edges[i];
+        for (Edge e : es) {
+            ll cost = dijkstra_list[i].calcDistance(e.to);
+            if (cost < e.cost) {
+                ans++;
+            }
+        }
+    }
+
+    cout << (ans / 2) << endl;
+
+}
