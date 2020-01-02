7b9ed4bd7843d95e881570af0536776973570839
riku <riku@tanide.net>
2020-01-02 18:25:27 +0900

    【AC】[AtCoder Beginner Contest 048]C - Boxes and Candies

---
+++ abc048/arc064_a.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+//typedef long long ll;
+typedef unsigned long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+
+int main() {
+    ll n, x;
+    cin >> n >> x;
+    vector<ll> boxes(n);
+    rep(i, n) cin >> boxes[i];
+
+    ll ans = 0;
+    if (boxes[0] > x) {
+        ans = boxes[0] - x;
+        boxes[0] -= ans;
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        if (boxes[i] + boxes[i + 1] > x) {
+            ll now = boxes[i] + boxes[i + 1] - x;
+            ans += now;
+            boxes[i + 1] -= now;
+        }
+    }
+    cout << ans << endl;
+
+}
