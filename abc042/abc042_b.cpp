96aca6a17351f5887f67718c372c4346a9fabc21
riku <riku@tanide.net>
2020-01-03 20:41:33 +0900

    【AC】[AtCoder Beginner Contest 042]B - Iroha Loves Strings (ABC Edition)

---
+++ abc042/abc042_b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+
+int main() {
+    int n, l;
+    cin >> n >> l;
+    vector<string> terms(n);
+    rep(i, n) cin >> terms[i];
+    sort(terms.begin(), terms.end());
+    for (string &s : terms) {
+        cout << s;
+    }
+    cout << endl;
+}
