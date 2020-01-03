a0cd09991632b03da59f30c34ef7784e3e2107d6
riku <riku@tanide.net>
2020-01-03 21:50:30 +0900

    【AC】[AtCoder Beginner Contest 042]D - Iroha and a Grid

---
+++ abc042/arc058_b.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+};
+
+vector<mint> factorials;
+
+int main() {
+    ll h, w, a, b;
+    cin >> h >> w >> a >> b;
+    factorials.resize(h + w);
+    factorials[0] = 1;
+    for (int i = 1; i < (h + w); i++) {
+        factorials[i] = factorials[i - 1] * i;
+    }
+
+    ll top_height = h - a - 1;
+
+    vector<mint> under(w);
+    for (ll i = 0; i < w; i++) {
+        under[i] = (factorials[top_height + i]) / factorials[top_height] / factorials[i];
+    }
+
+    vector<mint> top(w);
+    ll under_height = a - 1;
+    for (ll i = 0; i < w; i++) {
+        ll index = w - 1 - i;
+        top[i] = factorials[index + under_height] / factorials[index] / factorials[under_height];
+    }
+
+
+    mint ans = 0;
+    for (ll i = b; i < w; i++) {
+        mint now = top[i] * under[i];
+        ans += now;
+    }
+
+    cout << ans.x << endl;
+
+
+}
