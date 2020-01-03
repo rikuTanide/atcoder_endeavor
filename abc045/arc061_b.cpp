4e97370adf551a1fd1d4350223fa4bc6221062a2
riku <riku@tanide.net>
2020-01-03 12:46:10 +0900

    【WA】[AtCoder Beginner Contest 045]D - Snuke's Coloring

--- abc045/arc061_b.cpp
+++ abc045/arc061_b.cpp
@@ -2,8 +2,8 @@
 #include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
+//typedef long long ll;
+typedef unsigned long long ll;
 
 #define rep(i, n) for (ll i = 0; i < (n); ++i)
 //#define rep(i, n) for (int i = 0; i < (n); ++i)
@@ -34,60 +34,50 @@ int main() {
     ll h, w;
     int n;
     cin >> h >> w >> n;
-    map<ll, map<ll, bool>> m;
-    map<int, ll> ans;
 
-    set<ll> check_ys;
-
+    vector<P> blacks(n);
     rep(i, n) {
         ll a, b;
         cin >> a >> b;
         a--;
         b--;
 
-        m[a][b] = true;
-        check_ys.insert(a - 1);
-        check_ys.insert(a);
-        check_ys.insert(a + 1);
-
+        blacks[i].first = b;
+        blacks[i].second = a;
     }
-    check_ys.erase(-1);
-    check_ys.erase(0);
-    check_ys.erase(h - 1);
-    check_ys.erase(h);
 
-    for (ll check_y : check_ys) {
-        set<ll> check_xs;
+    map<ll, map<ll, bool>> m;
+    for (P p : blacks) {
+        m[p.first][p.second] = true;
+    }
 
-        for (int i = 0; i < 3; i++) {
-            for (auto &&e : m[check_y - 1 + i]) {
-                ll x = e.first;
-                check_xs.insert(x - 1);
-                check_xs.insert(x);
-                check_xs.insert(x + 1);
+    set<P> q;
+    for (P p: blacks) {
+        rep(x, 3)rep(y, 3) {
+                ll px = p.first - 1 + x;
+                ll py = p.second - 1 + y;
+                if (px == -1 || px == w) continue;
+                if (px == 0 || px == w - 1) continue;
+                if (py == -1 || py == h) continue;
+                if (py == 0 || py == h - 1) continue;
+
+                P next(px, py);
+                q.insert(next);
             }
-        }
-        check_xs.erase(-1);
-        check_xs.erase(0);
-        check_xs.erase(w - 1);
-        check_xs.erase(w);
-
-        for (ll check_x : check_xs) {
-
-            int black = 0;
-            rep(x, 3)rep(y, 3) {
-                    ll px = check_x - 1 + x;
-                    ll py = check_y - 1 + y;
-                    if (m.find(py) == m.end()) continue;
-                    if (m[py].find(px) == m[py].end())continue;
-                    if (m[py][px]) black++;
-                }
-            ans[black]++;
-
-        }
-
     }
 
+    map<int, ll> ans;
+    for (P p : q) {
+        int black = 0;
+        rep(x, 3)rep(y, 3) {
+                ll px = p.first - 1 + x;
+                ll py = p.second - 1 + y;
+                if (m.find(py) == m.end()) continue;
+                if (m[py].find(px) == m[py].end())continue;
+                if (m[px][py]) black++;
+            }
+        ans[black]++;
+    }
 
     ans[0] = (h - 2) * (w - 2);
     for (auto e : ans) {
