8c06559a67f723032739e3b138f0afc22e61ea8b
riku <riku@tanide.net>
2020-01-03 12:37:16 +0900

    【TLE】[AtCoder Beginner Contest 045]D - Snuke's Coloring

--- abc045/arc061_b.cpp
+++ abc045/arc061_b.cpp
@@ -2,8 +2,8 @@
 #include <cmath>
 
 using namespace std;
-//typedef long long ll;
-typedef unsigned long long ll;
+typedef long long ll;
+//typedef unsigned long long ll;
 
 #define rep(i, n) for (ll i = 0; i < (n); ++i)
 //#define rep(i, n) for (int i = 0; i < (n); ++i)
@@ -35,7 +35,9 @@ int main() {
     int n;
     cin >> h >> w >> n;
     map<ll, map<ll, bool>> m;
-    map<ll, map<ll, bool>> q;
+    map<int, ll> ans;
+
+    set<ll> check_ys;
 
     rep(i, n) {
         ll a, b;
@@ -43,37 +45,48 @@ int main() {
         a--;
         b--;
 
-        m[b][a] = true;
+        m[a][b] = true;
+        check_ys.insert(a - 1);
+        check_ys.insert(a);
+        check_ys.insert(a + 1);
 
-        rep(x, 3)rep(y, 3) {
-                ll px = b - 1 + x;
-                ll py = a - 1 + y;
-                if (px == -1 || px == w) continue;
-                if (px == 0 || px == w - 1) continue;
-                if (py == -1 || py == h) continue;
-                if (py == 0 || py == h - 1) continue;
-
-                q[px][py] = true;
-            }
     }
+    check_ys.erase(-1);
+    check_ys.erase(0);
+    check_ys.erase(h - 1);
+    check_ys.erase(h);
 
-    map<int, ll> ans;
-    for (auto &e : q) {
-        for (auto f : e.second) {
+    for (ll check_y : check_ys) {
+        set<ll> check_xs;
 
-            ll xi = e.first;
-            ll yi = f.first;
+        for (int i = 0; i < 3; i++) {
+            for (auto e : m[check_y - 1 + i]) {
+                ll x = e.first;
+                check_xs.insert(x - 1);
+                check_xs.insert(x);
+                check_xs.insert(x + 1);
+            }
+        }
+        check_xs.erase(-1);
+        check_xs.erase(0);
+        check_xs.erase(w - 1);
+        check_xs.erase(w);
+
+        for (ll check_x : check_xs) {
 
             int black = 0;
             rep(x, 3)
                 rep(y, 3) {
-                    ll px = xi - 1 + x;
-                    ll py = yi - 1 + y;
-                    if (m[px][py]) black++;
+                    ll px = check_x - 1 + x;
+                    ll py = check_y - 1 + y;
+                    if (m[py][px]) black++;
                 }
             ans[black]++;
+
         }
+
     }
+
 
     ans[0] = (h - 2) * (w - 2);
     for (auto e : ans) {
