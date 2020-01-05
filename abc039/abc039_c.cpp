7a6cb471d1349921e755a24afccaf0987dfef6ce
riku <riku@tanide.net>
2020-01-05 13:17:09 +0900

    【WA】[AtCoder Beginner Contest 039]C - ピアニスト高橋君

---
+++ abc039/abc039_c.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+
+int main() {
+    vector<char> original = {
+            'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W',
+            'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W',
+            'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W',
+            'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W',
+    };
+
+    string s;
+    cin >> s;
+
+    map<int, string> ans = {
+            {0,  "Do"},
+            {2,  "Re"},
+            {4,  "Mi"},
+            {5,  "Fa"},
+            {7,  "So"},
+            {9,  "La"},
+            {11, "Si"},
+    };
+
+    for (int i = 0; i < 12; i++) {
+        if (original[i] == 'B')continue;
+
+        for (int j = 0; j < s.size(); j++) {
+            if (original[i + j] != s[j]) break;
+        }
+
+        cout << ans[i] << endl;
+        return 0;
+    }
+
+
+}
