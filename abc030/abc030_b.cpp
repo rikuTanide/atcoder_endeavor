f0f4f8f619eeb95e71ae1b91c09e39551f6eeae7
riku <riku@tanide.net>
2020-01-09 21:11:28 +0900

    【AC】[AtCoder Beginner Contest 030]B - 時計盤

---
+++ abc030/abc030_b.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    n %= 12;
+
+    int nd = 360 / 12 * n;
+    double ndd = (double) nd + (360.0 / 12 / 60 * m);
+    int md = 360 / 60 * m;
+
+    double a = abs(ndd - md);
+
+    cout << min(a, 360.0 - a) << endl;
+
+
+}
