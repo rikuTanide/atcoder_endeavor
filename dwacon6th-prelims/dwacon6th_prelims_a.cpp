00b34972687ddda2041ae28730812a70be0d4a96
riku <riku@tanide.net>
2020-01-11 20:03:44 +0900

    【AC】[第6回 ドワンゴからの挑戦状 予選]A - Falling Asleep

---
+++ dwacon6th-prelims/dwacon6th_prelims_a.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX;
+const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<string> titles(n);
+    vector<int> seconds(n);
+
+    rep(i, n) {
+        cin >> titles[i];
+        cin >> seconds[i];
+    }
+
+    string x;
+    cin >> x;
+    int index = find(titles.begin(), titles.end(), x) - titles.begin();
+
+    int ans = 0;
+    for (int i = index + 1; i < n; i++) {
+        ans += seconds[i];
+    }
+    cout << ans << endl;
+
+}
