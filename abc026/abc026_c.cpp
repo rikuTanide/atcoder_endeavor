107bf07d2e0c9f3f2c5dad8a7c7c5f793e1b37f7
riku <riku@tanide.net>
2020-01-13 12:03:03 +0900

    【AC】[AtCoder Beginner Contest 026]C - 高橋君の給料

---
+++ abc026/abc026_c.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+struct Triangle {
+    int parent;
+    vector<int> children;
+};
+
+class Tree {
+    vector<vector<int>> edges;
+public:
+    Tree(int n) : edges(n) {}
+
+    void edge(int i, int j) {
+        edges[i].push_back(j);
+        edges[j].push_back(i);
+    }
+
+    void root(int r, vector<Triangle> &leafs) {
+        queue<int> vs;
+        vs.push(r);
+        vector<bool> check(edges.size(), false);
+        check[r] = true;
+        while (!vs.empty()) {
+            int k = vs.front();
+            Triangle triangle;
+            triangle.parent = k;
+
+            for (int i : edges[k]) {
+                if (check[i]) continue;
+                check[i] = true;
+                triangle.children.push_back(i);
+                vs.push(i);
+            }
+            vs.pop();
+            leafs.push_back(triangle);
+        }
+        reverse(leafs.begin(), leafs.end());
+    }
+};
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    Tree tree(n);
+
+    rep(i, n - 1) {
+        int b;
+        cin >> b;
+        b--;
+        tree.edge(i + 1, b);
+    }
+    vector<Triangle> leafs;
+    tree.root(0, leafs);
+
+    vector<int> salaries(n, 1);
+    for (int i = 0; i < n; i++) {
+        if (leafs[i].children.empty()) {
+            continue;
+        }
+
+        int mi = INF;
+        int ma = 0;
+        for (int j : leafs[i].children) {
+            cmin(mi, salaries[j]);
+            cmax(ma, salaries[j]);
+        }
+        salaries[leafs[i].parent] = mi + ma + 1;
+    }
+
+    cout << salaries[0] << endl;
+}
