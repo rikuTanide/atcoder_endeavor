1bbd1f8bd4d288a9ac17d69128946a4d523f3fa5
riku <riku@tanide.net>
2020-01-13 11:33:06 +0900

    【AC】[AtCoder Beginner Contest 027]D - ロボット

--- abc027/abc027_d.cpp
+++ abc027/abc027_d.cpp
@@ -2,14 +2,14 @@
 #include <cmath>
 
 using namespace std;
-//typedef long long ll;
-typedef unsigned long long ll;
+typedef long long ll;
+//typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 #define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
 //const ll INF = LONG_LONG_MAX;
 const ll INF = 1e15;
@@ -29,44 +29,37 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
 
-
 int main() {
     string s;
     cin >> s;
-    int n = s.size();
+    vector<int> weights(s.size(), 0);
+    rep(i, s.size()) {
+        char c = s[i];
+        if (c == 'M')continue;
+        if (c == '+') weights[i]++;
+        if (c == '-') weights[i]--;
+    }
+    for (int i = s.size() - 2; i >= 0; i--) {
+        weights[i] += weights[i + 1];
+    }
 
-    assert(n <= 1000);
-    vector<map<int, int>> dp(n + 1, map<int, int>());
-    dp[0][0] = 0;
-
-    auto mcmax = [&](int i, int j, int value) {
-        if (dp[i].find(j) == dp[i].end()) {
-            dp[i][j] = value;
-        } else {
-            cmax(dp[i][j], value);
-        }
-    };
-
-    for (int i = 0; i < n; i++) {
+    vector<int> move_only;
+    rep(i, s.size()) {
         char c = s[i];
         if (c == 'M') {
-            for (auto p : dp[i]) {
-                mcmax(i + 1, p.first + 1, dp[i][p.first]);
-            }
-            for (auto p : dp[i]) {
-                mcmax(i + 1, p.first - 1, dp[i][p.first]);
-            }
-        } else if (c == '+') {
-            for (auto p : dp[i]) {
-                dp[i + 1][p.first] = dp[i][p.first] + p.first;
-            }
-        } else if (c == '-') {
-            for (auto p : dp[i]) {
-                dp[i + 1][p.first] = dp[i][p.first] - p.first;
-            }
+            move_only.push_back(weights[i]);
         }
     }
 
-    cout << dp[s.size()][0] << endl;
+    sort(move_only.begin(), move_only.end());
+
+    int ans = 0;
+    for (int i = 0; i < move_only.size() / 2; i++) {
+        ans -= move_only[i];
+    }
+    for (int i = move_only.size() / 2; i < move_only.size(); i++) {
+        ans += move_only[i];
+    }
+    cout << ans << endl;
 
 }
