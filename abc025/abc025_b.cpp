c4ed65341cb282f3f2d524371b353ffa19398d3c
riku <riku@tanide.net>
2020-01-14 19:48:48 +0900

    【AC】[AtCoder Beginner Contest 025]B - 双子とスイカ割り

---
+++ abc025/abc025_b.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+
+int main() {
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    int now = 0;
+
+    rep(i, n) {
+        string s;
+        int d;
+        cin >> s >> d;
+
+        cmax(d, a);
+        cmin(d, b);
+
+        if (s == "East") {
+            now += d;
+        } else {
+            now -= d;
+        }
+
+    }
+
+    if (now == 0) {
+        cout << 0 << endl;
+    } else if (now > 0) {
+        cout << "East " << abs(now) << endl;
+    } else {
+        cout << "West " << abs(now) << endl;
+    }
+
+}
