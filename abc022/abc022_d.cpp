46d97f0e4969040564d4eaf840a348cdf121cd0f
riku <riku@tanide.net>
2020-01-17 00:05:25 +0900

    【WA】[AtCoder Beginner Contest 022]D - Big Bang

--- abc022/abc022_d.cpp
+++ abc022/abc022_d.cpp
@@ -29,144 +29,46 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
 
-// https://www.geeksforgeeks.org/convex-hull-set-2-graham-scan/
-struct Point
-{
-    int x, y;
+// https://atcoder.jp/contests/abc022/submissions/8900202
+const double eps = 1e-9;
+
+struct Point {
+    double x, y;
+
+    Point &operator-=(const Point &v) {
+        x -= v.x;
+        y -= v.y;
+        return *this;
+    }
+
+    Point operator-(const Point &v) const { return Point(*this) -= v; }
+
+    double cross(const Point &v) const { return x * v.y - v.x * y; }      /* 外積 */
+
 };
-Point p0;
 
-// A utility function to find next to top in a stack
-Point nextToTop(stack<Point> &S)
-{
-    Point p = S.top();
-    S.pop();
-    Point res = S.top();
-    S.push(p);
-    return res;
+int ccw(Point p0, Point p1, Point p2) {
+    Point v1 = p1 - p0;
+    Point v2 = p2 - p0;
+    if (v1.cross(v2) > eps) return 1;
+    if (v1.cross(v2) < -eps) return -1;
+    return 0;
 }
 
-// A utility function to swap two points
-int swap(Point &p1, Point &p2)
-{
-    Point temp = p1;
-    p1 = p2;
-    p2 = temp;
-}
-
-// A utility function to return square of distance
-// between p1 and p2
-int distSq(Point p1, Point p2)
-{
-    return (p1.x - p2.x)*(p1.x - p2.x) +
-           (p1.y - p2.y)*(p1.y - p2.y);
-}
-
-// To find orientation of ordered triplet (p, q, r).
-// The function returns following values
-// 0 --> p, q and r are colinear
-// 1 --> Clockwise
-// 2 --> Counterclockwise
-int orientation(Point p, Point q, Point r)
-{
-    int val = (q.y - p.y) * (r.x - q.x) -
-              (q.x - p.x) * (r.y - q.y);
-
-    if (val == 0) return 0;  // colinear
-    return (val > 0)? 1: 2; // clock or counterclock wise
-}
-
-// A function used by library function qsort() to sort an array of
-// points with respect to the first point
-int compare(const void *vp1, const void *vp2)
-{
-    Point *p1 = (Point *)vp1;
-    Point *p2 = (Point *)vp2;
-
-    // Find orientation
-    int o = orientation(p0, *p1, *p2);
-    if (o == 0)
-        return (distSq(p0, *p2) >= distSq(p0, *p1))? -1 : 1;
-
-    return (o == 2)? -1: 1;
-}
-
-// Prints convex hull of a set of n points.
-vector<Point> convexHull(vector<Point> &points, int n)
-{
-    // Find the bottommost point
-    int ymin = points[0].y, min = 0;
-    for (int i = 1; i < n; i++)
-    {
-        int y = points[i].y;
-
-        // Pick the bottom-most or chose the left
-        // most point in case of tie
-        if ((y < ymin) || (ymin == y &&
-                           points[i].x < points[min].x))
-            ymin = points[i].y, min = i;
+vector<Point> convex_hull(vector<Point> ps) {
+    int n = ps.size(), k = 0;
+    sort(ps.begin(), ps.end(),
+         [](const Point &a, const Point &b) { return a.y != b.y ? a.y < b.y : a.x < b.x; }
+    );
+    vector<Point> ch(2 * n);
+    for (int i = 0; i < n; ch[k++] = ps[i++]) {  // lower-hull
+        while (k >= 2 && ccw(ch[k - 2], ch[k - 1], ps[i]) <= 0) --k;
     }
-
-    // Place the bottom-most point at first position
-    swap(points[0], points[min]);
-
-    // Sort n-1 points with respect to the first point.
-    // A point p1 comes before p2 in sorted output if p2
-    // has larger polar angle (in counterclockwise
-    // direction) than p1
-    p0 = points[0];
-    qsort(&points[1], n-1, sizeof(Point), compare);
-
-    // If two or more points make same angle with p0,
-    // Remove all but the one that is farthest from p0
-    // Remember that, in above sorting, our criteria was
-    // to keep the farthest point at the end when more than
-    // one points have same angle.
-    int m = 1; // Initialize size of modified array
-    for (int i=1; i<n; i++)
-    {
-        // Keep removing i while angle of i and i+1 is same
-        // with respect to p0
-        while (i < n-1 && orientation(p0, points[i],
-                                      points[i+1]) == 0)
-            i++;
-
-
-        points[m] = points[i];
-        m++;  // Update size of modified array
+    for (int i = n - 2, t = k + 1; i >= 0; ch[k++] = ps[i--]) { // upper-hull
+        while (k >= t && ccw(ch[k - 2], ch[k - 1], ps[i]) <= 0) --k;
     }
-
-    // If modified array of points has less than 3 points,
-    // convex hull is not possible
-
-
-    // Create an empty stack and push first three points
-    // to it.
-    stack<Point> S;
-    S.push(points[0]);
-    S.push(points[1]);
-    S.push(points[2]);
-
-    // Process remaining n-3 points
-    for (int i = 3; i < m; i++)
-    {
-        // Keep removing top while the angle formed by
-        // points next-to-top, top, and points[i] makes
-        // a non-left turn
-        while (orientation(nextToTop(S), S.top(), points[i]) != 2)
-            S.pop();
-        S.push(points[i]);
-    }
-
-    // Now stack has the output points, print contents of stack
-
-    vector<Point> res;
-    res.push_back(p0);
-    while(!S.empty()) {
-        res.push_back(S.top());
-        S.pop();
-    }
-    return res;
+    ch.resize(k - 1);
+    return ch;
 }
 
 double calc_outer(vector<Point> &cosmos) {
@@ -196,14 +98,14 @@ int main() {
         cin >> cosmos[i].x;
         cin >> cosmos[i].y;
     }
-    vector<Point> i_g = convexHull(cosmos, n);
+    vector<Point> i_g = convex_hull(cosmos);
 
     vector<Point> cosmos2(n);
     rep(i, n) {
         cin >> cosmos2[i].x;
         cin >> cosmos2[i].y;
     }
-    vector<Point> s_g = convexHull(cosmos2, n);
+    vector<Point> s_g = convex_hull(cosmos2);
 
 
     double i_o = calc_outer(i_g);
