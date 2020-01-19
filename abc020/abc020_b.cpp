2bd42d4a1b47eeff730208c1bf252776dbb75b4a
riku <riku@tanide.net>
2020-01-19 12:16:28 +0900

    【AC】[AtCoder Beginner Contest 020]B - 足し算

---
+++ abc020/abc020_b.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+ll pow2(ll a, ll b) {
+    ll res = a;
+    for (ll i = 1; i < b; i++) {
+        res *= a;
+    }
+    return res;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int keta = log10(b) + 1;
+    int a2 = a * pow2(10, keta);
+    int join = a2 + b;
+    cout << join * 2 << endl;
+}
+
