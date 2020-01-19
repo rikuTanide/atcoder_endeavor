6f8e67ccad01fb205dc640aefef0878755df2411
riku <riku@tanide.net>
2020-01-19 13:10:53 +0900

    【RE】[AtCoder Beginner Contest 020]D - LCM Rush

---
+++ abc020/abc020_d.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+ll gcd(ll x, ll y) {
+    if (x > y) swap(x, y);
+    ll m = 1;
+    while (m != 0) {
+        m = y % x;
+        y = x;
+        x = m;
+    }
+    return y;
+}
+
+ll lcm(ll a, ll b) {
+    return a / gcd(a, b) * b;
+}
+
+int main() {
+
+    ll n, k;
+    cin >> n >> k;
+    assert(n <= 100);
+    assert(k <= 100);
+
+
+    ll ans = 0;
+    for (ll i = 1; i <= n; i++) {
+        ll l = lcm(i, k);
+        ans += l;
+    }
+    cout << ans << endl;
+}
+
