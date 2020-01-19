4925c40a0e4a4bf6e62891e5a444c116749c9635
riku <riku@tanide.net>
2020-01-19 21:04:11 +0900

    【AC】[AtCoder Beginner Contest 152]C - Low Elements

---
+++ abc152/abc152_c.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> numbers(n);
+    rep(i, n) cin >> numbers[i];
+    vector<int> mins(n);
+    mins[0] = numbers[0];
+    rep(i, n - 1) mins[i + 1] = min(mins[i], numbers[i + 1]);
+
+    int ans = 0;
+    rep(i, n) {
+        if (numbers[i] <= mins[i]) ans++;
+    }
+
+    cout << ans << endl;
+}
+
