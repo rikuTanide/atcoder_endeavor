e49100c0a80fc4e386a256970c83d3cac882cf9c
riku <riku@tanide.net>
2020-01-21 19:21:42 +0900

    【AC】[AtCoder Beginner Contest 015]B - 高橋くんの集計

---
+++ abc015/abc015_2.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+int main() {
+    int sum = 0;
+    int count = 0;
+    int n;
+    cin >> n;
+    rep(i, n) {
+        int a;
+        cin >> a;
+        if (a == 0) continue;
+        sum += a;
+        count++;
+    }
+
+    cout << (sum + count - 1) / count << endl;
+
+}
