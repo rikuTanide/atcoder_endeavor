dc0c6c298478d163dec8f9602d177afe9d0c85fe
riku <riku@tanide.net>
2020-01-24 13:38:27 +0900

    【RE】[AtCoder Beginner Contest 008]D - 金塊ゲーム

--- abc008/abc008_4.cpp
+++ abc008/abc008_4.cpp
@@ -2,67 +2,51 @@
 #include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
-
 #define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) ll(x.size())
+//typedef long long ll;
+typedef long long ll;
 //typedef pair<int, int> P;
 typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const ll INF = 10e15;
+const ll MINF = -10e10;
+const int INF = INT_MAX / 100;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ifstream myfile("~/Downloads/02.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+// https://atcoder.jp/contests/abc008/submissions/9264344
+// なんもわからん。
 
+typedef tuple<int, int, int, int> square;
 struct Machine {
     int x, y;
 };
 
-int check(vector<Machine> &machines, int w, int h) {
-    vector<vector<bool>> golds(w, vector<bool>(h, true));
+int search(
+        int x1, int y1, int x2, int y2,
+        vector<Machine> &machines,
+        map<square, int> &memo) {
+    square s(x1, y1, x2, y2);
+    if (memo.find(s) != memo.end()) return memo[s];
     int ans = 0;
+    int c = (x2 - x1) + (y2 - y1) + 1;
     for (Machine &machine : machines) {
-        golds[machine.x][machine.y] = false;
-        ans++;
-        // 上
-        for (int y = machine.y + 1; y < h; y++) {
-            if (!golds[machine.x][y]) break;
-            golds[machine.x][y] = false;
-            ans++;
-        }
-        // 下
-        for (int y = machine.y - 1; y >= 0; y--) {
-            if (!golds[machine.x][y]) break;
-            golds[machine.x][y] = false;
-            ans++;
-        }
-
-        // 右
-        for (int x = machine.x + 1; x < w; x++) {
-            if (!golds[x][machine.y]) break;
-            golds[x][machine.y] = false;
-            ans++;
-        }
-
-        // 左
-        for (int x = machine.x - 1; x >= 0; x--) {
-            if (!golds[x][machine.y]) break;
-            golds[x][machine.y] = false;
-            ans++;
+        if (x1 <= machine.x && machine.x <= x2 && y1 <= machine.y && machine.y <= y2) {
+            int now = c + search(x1, y1, machine.x - 1, machine.y - 1, machines, memo) +
+                      search(x1, machine.y + 1, machine.x - 1, y2, machines, memo) +
+                      search(machine.x + 1, y1, x2, machine.y - 1, machines, memo) +
+                      search(machine.x + 1, machine.y + 1, x2, y2, machines, memo);
+            cmax(ans, now);
         }
     }
     return ans;
@@ -77,24 +61,10 @@ int main() {
         Machine machine;
         cin >> machine.x;
         cin >> machine.y;
-        machine.x--;
-        machine.y--;
         machines.push_back(machine);
     }
 
+    map<square, int> memo;
+    cout << search(1, 1, w, h, machines, memo) << endl;
 
-    vector<int> indexes(n);
-    rep(i, n) indexes[i] = i;
-
-
-    int ans = 0;
-    do {
-        vector<Machine> ordered_machines(n);
-        for (int i : indexes) {
-            ordered_machines[i] = machines[indexes[i]];
-        }
-        int now = check(ordered_machines, w, h);
-        cmax(ans, now);
-    } while (next_permutation(indexes.begin(), indexes.end()));
-    cout << ans << endl;
 }
