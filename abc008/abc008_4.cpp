1fcb1bcb3df021aedc7db2091561c6747dacf8cb
riku <riku@tanide.net>
2020-01-24 18:46:36 +0900

    【TLE】[AtCoder Beginner Contest 008]D - 金塊ゲーム

--- abc008/abc008_4.cpp
+++ abc008/abc008_4.cpp
@@ -2,47 +2,50 @@
 #include <cmath>
 
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef long long ll;
 typedef long long ll;
-//typedef pair<int, int> P;
+//typedef unsigned long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<ll, int> P;
 typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = 10e15;
-const ll MINF = -10e10;
-const int INF = INT_MAX / 100;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("~/Downloads/02.txt");
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
 
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
 // https://atcoder.jp/contests/abc008/submissions/9264344
 // なんもわからん。
 
-typedef tuple<int, int, int, int> square;
+typedef tuple<ll, ll, ll, ll> square;
 struct Machine {
-    int x, y;
+    ll x, y;
 };
 
-int search(
-        int x1, int y1, int x2, int y2,
+ll search(
+        ll x1, ll y1, ll x2, ll y2,
         vector<Machine> &machines,
-        map<square, int> &memo) {
+        map<square, ll> &memo) {
     square s(x1, y1, x2, y2);
     if (memo.find(s) != memo.end()) return memo[s];
-    int ans = 0;
-    int c = (x2 - x1) + (y2 - y1) + 1;
+    ll ans = 0;
+    ll c = (x2 - x1) + (y2 - y1) + 1;
     for (Machine &machine : machines) {
         if (x1 <= machine.x && machine.x <= x2 && y1 <= machine.y && machine.y <= y2) {
-            int now = c + search(x1, y1, machine.x - 1, machine.y - 1, machines, memo) +
+            ll now = c + search(x1, y1, machine.x - 1, machine.y - 1, machines, memo) +
                       search(x1, machine.y + 1, machine.x - 1, y2, machines, memo) +
                       search(machine.x + 1, y1, x2, machine.y - 1, machines, memo) +
                       search(machine.x + 1, machine.y + 1, x2, y2, machines, memo);
@@ -53,9 +56,8 @@ int search(
 }
 
 int main() {
-    int w, h, n;
+    ll w, h, n;
     cin >> w >> h >> n;
-    assert(n <= 8);
     vector<Machine> machines;
     rep(i, n) {
         Machine machine;
@@ -64,7 +66,7 @@ int main() {
         machines.push_back(machine);
     }
 
-    map<square, int> memo;
+    map<square, ll> memo;
     cout << search(1, 1, w, h, machines, memo) << endl;
 
 }
