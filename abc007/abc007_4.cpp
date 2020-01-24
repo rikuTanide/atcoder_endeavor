25c1e0f0120976dd1e7def960987e255e27f7db0
riku <riku@tanide.net>
2020-01-24 20:28:16 +0900

    【RE】[AtCoder Beginner Contest 007]D - 禁止された数字

---
+++ abc007/abc007_4.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<ll, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+bool check(int i) {
+    while (i > 0) {
+        int m = i % 10;
+        if (m == 4 || m == 9) {
+            return true;
+        }
+        i /= 10;
+    }
+    return false;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    assert(a <= 10000);
+    assert(b <= 10000);
+    int ans = 0;
+    for (int i = a; i <= b; i++) {
+        if (check(i))ans++;
+    }
+    cout << ans << endl;
+
+}
