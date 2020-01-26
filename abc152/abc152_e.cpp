037f84978110d3985a8146a05e36d5d330a7b8aa
riku <riku@tanide.net>
2020-01-26 14:12:00 +0900

    【TLE】[AtCoder Beginner Contest 152]E - Flatten

---
+++ abc152/abc152_e.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<ll, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+};
+
+
+map<ll, int> factorize(ll n) {
+    map<ll, int> res;
+
+    for (ll i = 2; i * i <= n; i++) {
+        if (n % i != 0) {
+            continue;
+        }
+        res[i] = 0;
+        while (n % i == 0) {
+            n /= i;
+            res[i]++;
+        }
+    }
+
+    if (n != 1) res[n] = 1;
+    return res;
+
+}
+
+int main() {
+    ll n;
+    cin >> n;
+
+    vector<ll> numbers(n);
+    rep(i, n) cin >> numbers[i];
+
+    vector<map<ll, int>> facts(n);
+    rep(i, n)facts[i] = factorize(numbers[i]);
+
+
+    map<ll, int> facts_map;
+    for (map<ll, int> &fact : facts) {
+        for (pair<ll, int> f : fact) {
+            cmax(facts_map[f.first], f.second);
+        }
+    }
+
+
+    mint ans = 0;
+    for (map<ll, int> &fact : facts) {
+        mint rate = 1;
+        for (pair<ll, int> f : facts_map) {
+            int c = f.second - fact[f.first];
+            for (int i = 0; i < c; i++) {
+                rate *= f.first;
+            }
+        }
+        ans += rate;
+    }
+
+
+    cout << ans.x << endl;
+
+}
