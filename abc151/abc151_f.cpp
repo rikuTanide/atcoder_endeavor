ab27d337fd78d7facc05e076ec77fd45b121a87c
riku <riku@tanide.net>
2020-01-26 16:25:33 +0900

    【AC】[AtCoder Beginner Contest 151]F - Enclose All

---
+++ abc151/abc151_f.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+typedef long double ld;
+using P = complex<ld>;
+const ld eps = 1e-10;
+
+ld cross(const P &a, const P &b) { return a.real() * b.imag() - a.imag() * b.real(); }
+
+template<class iter>
+std::pair<P, ld> min_ball(iter left, iter right, int seed = 1333) {
+    const int n = right - left;
+
+    assert(n >= 1);
+    if (n == 1) {
+        return {*left, ld(0)};
+    }
+
+    std::mt19937 mt(seed);
+    std::shuffle(left, right, mt);
+    // std::random_shuffle(left, right); // simple but deprecated
+
+    iter ps = left;
+    using circle = std::pair<P, ld>;
+
+    auto make_circle_3 = [](const P &a, const P &b, const P &c) -> circle {
+        ld A = std::norm(b - c), B = std::norm(c - a), C = std::norm(a - b),
+                S = cross(b - a, c - a);
+        P p = (A * (B + C - A) * a + B * (C + A - B) * b + C * (A + B - C) * c) / (4 * S * S);
+        ld r2 = std::norm(p - a);
+        return {p, r2};
+    };
+
+    auto make_circle_2 = [](const P &a, const P &b) -> circle {
+        P c = (a + b) / (ld) 2;
+        ld r2 = std::norm(a - c);
+        return {c, r2};
+    };
+
+    auto in_circle = [](const P &a, const circle &c) -> bool {
+        return std::norm(a - c.first) <= c.second + eps;
+    };
+
+    circle c = make_circle_2(ps[0], ps[1]);
+
+    // MiniDisc
+    for (int i = 2; i < n; ++i) {
+        if (!in_circle(ps[i], c)) {
+            // MiniDiscWithPoint
+            c = make_circle_2(ps[0], ps[i]);
+            for (int j = 1; j < i; ++j) {
+                if (!in_circle(ps[j], c)) {
+                    // MiniDiscWith2Points
+                    c = make_circle_2(ps[i], ps[j]);
+                    for (int k = 0; k < j; ++k) {
+                        if (!in_circle(ps[k], c)) {
+                            c = make_circle_3(ps[i], ps[j], ps[k]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return c;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<P> dots;
+    rep(i, n) {
+        int x, y;
+        cin >> x >> y;
+        P p(x, y);
+        dots.push_back(p);
+    }
+
+    auto ans = min_ball(dots.begin(), dots.end());
+    double ad = sqrt(ans.second);
+    printf("%.20f\n", ad);
+
+
+}
