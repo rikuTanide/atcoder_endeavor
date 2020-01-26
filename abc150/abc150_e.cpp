fef9d2d9417ce8ac40ad9a9d0735ccf40269ffa1
riku <riku@tanide.net>
2020-01-26 20:04:19 +0900

    【AC】[AtCoder Beginner Contest 150]E - Change a Little Bit

---
+++ abc150/abc150_e.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+};
+
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    vector<int> costs(n);
+    rep(i, n) cin >> costs[i];
+    sort(costs.begin(), costs.end());
+
+    vector<mint> nijo(1000000);
+    nijo[0] = 1;
+    for (int i = 1; i < 1000000; i++) {
+        nijo[i] = nijo[i - 1] * 2;
+    }
+
+
+    mint ans = 0;
+    for (int i = 0; i < n; i++) {
+        int l = i;
+        int r = n - i - 1;
+        mint now = nijo[r];
+        if (r != 0) now += nijo[r - 1] * r;
+        now *= nijo[l];
+        now *= costs[i];
+        ans += now;
+    }
+
+    ans *= nijo[n];
+
+    cout << ans.x << endl;
+
+}
