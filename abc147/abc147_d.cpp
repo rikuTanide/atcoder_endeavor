23f3e38cb95c1f3aef9b017a1faa415bd477ce3b
riku <riku@tanide.net>
2020-01-29 11:59:27 +0900

    【AC】[AtCoder Beginner Contest 147]D - Xor Sum 4

---
+++ abc147/abc147_d.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<ll, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+
+bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
+
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+const int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+};
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> numbers(n);
+    rep(i, n)cin >> numbers[i];
+
+    vector<int> pop_counts(61, 0);
+    vector<int> zero_counts(61, 0);
+
+    for (ll m : numbers) {
+        for (ll j = 0; j <= 60; j++) {
+            bool pop = ((m >> j) & 1);
+            if (pop) {
+                pop_counts[j]++;
+            } else {
+                zero_counts[j]++;
+            }
+        }
+    }
+
+    mint ans = 0;
+    for (int i = 0; i < 61; i++) {
+        mint s = mint(2).pow(i);
+        int pc = pop_counts[i];
+        int zc = zero_counts[i];
+        mint now = s * pc * zc;
+        ans += now;
+    }
+
+    cout << ans.x << endl;
+}
