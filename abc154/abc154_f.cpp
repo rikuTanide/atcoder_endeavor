f3aec2984716911845fe9232171c3b32907d9f83
riku <riku@tanide.net>
2020-02-09 21:51:22 +0900

    【TLE】[AtCoder Beginner Contest 154]F - Many Many Paths

---
+++ abc154/abc154_f.cpp
@@ -0,0 +1,138 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<ll, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 10;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+
+bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+
+
+void print_line(vector<ll> &line) {
+    if (line.size() == 0ll) {
+        cout << endl;
+        return;
+    }
+    for (ll i = 0; i < line.size(); i++) {
+        cout << line[i];
+        if (i == line.size() - 1) cout << endl;
+        else cout << ' ';
+    }
+}
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+};
+
+struct combination {
+    vector<mint> fact, ifact;
+
+    combination(int n) : fact(n + 1), ifact(n + 1) {
+        assert(n < mod);
+        fact[0] = 1;
+        for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
+        ifact[n] = fact[n].inv();
+        for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
+    }
+
+    mint operator()(int n, int k) {
+        if (k < 0 || k > n) return 0;
+        return fact[n] * ifact[k] * ifact[n - k];
+    }
+} combination(10000000);
+
+typedef priority_queue<long long, vector<long long>, greater<long long>> PQ_ASK;
+
+
+int main() {
+
+    int r1, c1, r2, c2;
+    cin >> r1 >> c1 >> r2 >> c2;
+
+    mint ans = 0;
+
+    for (int i = r1; i <= r2; i++) {
+        for (int j = c1; j <= c2; j++) {
+            ans += combination(i + j, i);
+        }
+    }
+
+    cout << ans.x << endl;
+}
+
+
