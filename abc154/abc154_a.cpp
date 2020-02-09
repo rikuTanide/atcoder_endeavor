2924f7ef3bc530fe4c8265006d045580e4350e65
riku <riku@tanide.net>
2020-02-09 21:01:12 +0900

    【AC】[AtCoder Beginner Contest 154]A - Remaining Balls

---
+++ abc154/abc154_a.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<ll, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 10;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+
+bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+
+void print_line(vector<ll> &line) {
+    if (line.size() == 0ll) {
+        cout << endl;
+        return;
+    }
+    for (ll i = 0; i < line.size(); i++) {
+        cout << line[i];
+        if (i == line.size() - 1) cout << endl;
+        else cout << ' ';
+    }
+}
+
+
+typedef priority_queue<long long, vector<long long>, greater<long long>> PQ_ASK;
+const int mod = 1000000007;
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+    int a, b;
+    cin >> a >> b;
+
+    string u;
+    cin >> u;
+    if (u == s) a--;
+    if (u == t) b--;
+
+    printf("%d %d\n", a, b);
+
+}
+
+
