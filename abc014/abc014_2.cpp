6cc417d045d3152409f3e5bdc2334b0ad4e7dfb8
riku <riku@tanide.net>
2020-02-15 16:30:52 +0900

    【AC】[AtCoder Beginner Contest 014]B - 価格の合計

--- abc014/abc014_2.cpp
+++ abc014/abc014_2.cpp
@@ -1,46 +1,48 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
 //const ll INF = 1e15;
 const ll MINF = LONG_LONG_MIN;
 //const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
     int n, x;
     cin >> n >> x;
-    vector<int> prices(n);
-    rep(i, n)cin >> prices[i];
+
+    vector<int> items(n);
+    rep(i, n) cin >> items[i];
 
     int ans = 0;
-    for (int i = 0; i < n; i++) {
-        bool b = (x >> i) & 1;
-        if (b) {
-            ans += prices[i];
+    rep(i, n) {
+        if ((x >> i) & 1) {
+            ans += items[i];
         }
     }
     cout << ans << endl;
-
 }
