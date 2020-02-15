c619479565dac90f7127adc27789ae8175dd026f
riku <riku@tanide.net>
2020-02-15 16:36:10 +0900

    【AC】[AtCoder Beginner Contest 017]B - choku語

--- abc017/abc017_2.cpp
+++ abc017/abc017_2.cpp
@@ -1,64 +1,59 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
 //const ll INF = 1e15;
 const ll MINF = LONG_LONG_MIN;
 //const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
-bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
-
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-int main() {
-    string s;
-    cin >> s;
-    if (s[0] == 'h') {
-        cout << "NO" << endl;
-        return 0;
+bool is_chokugo_c(char c) {
+    switch (c) {
+        case 'o':
+        case 'k':
+        case 'u':
+            return true;
+        default:
+            return false;
     }
-    if (s.back() == 'c') {
-        cout << "NO" << endl;
-        return 0;
-    }
-    set<char> st{'o', 'k', 'u'};
-
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == 'h') {
-            if (s[i - 1] != 'c') {
-                cout << "NO" << endl;
-                return 0;
-            }
-        } else if (s[i] == 'c') {
-            if (s[i + 1] != 'h') {
-                cout << "NO" << endl;
-                return 0;
-            }
-        } else if (!contain(st, s[i])) {
-            cout << "NO" << endl;
-            return 0;
-        }
-
-    }
-    cout << "YES" << endl;
-
 }
 
+int main() {
+    string x;
+    cin >> x;
+    for (int i = 0; i < x.size(); i++) {
+        char c = x[i];
+        if (is_chokugo_c(c)) continue;
+        if (c == 'c' && i < x.size() - 1 && x[i + 1] == 'h') {
+            i++;
+            continue;
+        }
+        cout << "NO" << endl;
+        return 0;
+    }
+    cout << "YES" << endl;
+}
