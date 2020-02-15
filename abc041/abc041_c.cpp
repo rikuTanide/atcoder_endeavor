818f250b5117c97ef77a212cf8910f7db0eb3279
riku <riku@tanide.net>
2020-02-15 21:22:51 +0900

    【AC】[AtCoder Beginner Contest 041]C - 背の順

--- abc041/abc041_c.cpp
+++ abc041/abc041_c.cpp
@@ -1,47 +1,54 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
 const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
+struct Student {
+    int height, id;
+};
 
 int main() {
+
     int n;
     cin >> n;
 
-    vector<P> students(n);
-    rep(i, n) {
-        ll a;
-        cin >> a;
-        P p(a, i + 1);
-        students[i] = p;
-    }
-    sort(students.rbegin(), students.rend());
-    for (P s : students) {
-        cout << s.second << endl;
-    }
+    vector<Student> students(n);
+    rep(i, n) students[i].id = i;
+    rep(i, n) cin >> students[i].height;
+
+    sort(students.begin(), students.end(), [](Student s, Student t) {
+        return s.height > t.height;
+    });
+
+    for (Student s : students) cout << s.id + 1 << endl;
 }
+
