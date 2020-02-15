d19d727c9f2ef9ebd79e236cebb4ebe1265ee8c0
riku <riku@tanide.net>
2020-02-15 21:16:48 +0900

    【AC】[AtCoder Beginner Contest 040]B - □□□□□

--- abc040/abc040_b.cpp
+++ abc040/abc040_b.cpp
@@ -1,51 +1,52 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-//typedef long long ll;
-typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
 const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 
 int main() {
+
     int n;
     cin >> n;
 
-    int ans = INF;
-    for (int i = 1; i <= n; i++) {
+    int ans = INT_MAX;
+    for (int w = 1; w <= n; w++) {
+        int h = n / w;
 
-        int w = i;
-        int h = n / i;
-
-        int diff = abs(w - h);
-        int rem = n - (w * h);
-
-        int now = diff + rem;
+        int amari = n - (w * h);
+        int sa = abs(w - h);
+        int now = amari + sa;
         cmin(ans, now);
     }
-
     cout << ans << endl;
 
 }
+
