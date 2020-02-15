79b7057260160aed023e3d250b1b841274c8ec0a
riku <riku@tanide.net>
2020-02-15 15:44:56 +0900

    【AC】[AtCoder Beginner Contest 003]B - AtCoderトランプ

--- abc003/abc003_2.cpp
+++ abc003/abc003_2.cpp
@@ -1,63 +1,64 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //#define sz(x) ll(x.size())
 //typedef pair<ll, int> P;
-typedef pair<ll, ll> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
 //const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
 //const ll INF = 1e15;
 const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
-bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
-
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-int main() {
-    set<char> atcoder = {'a', 't', 'c', 'o', 'd', 'e', 'r'};
+bool is_atcoder(char c) {
+    switch (c) {
+        case 'a':
+        case 't':
+        case 'c':
+        case 'o':
+        case 'd':
+        case 'e':
+        case 'r':
+            return true;
+        default:
+            return false;
+    }
+}
+
+signed main() {
     string s, t;
     cin >> s >> t;
-    rep(i, s.size()) {
-        char sc = s[i];
-        char tc = t[i];
 
-        if (sc == tc) {
-            continue;
-        }
-        if (sc != tc) {
-            if (sc == '@') {
-                if (contain(atcoder, tc)) {
-                    continue;
-                }
-                cout << "You will lose" << endl;
-                return 0;
-            } else if (tc == '@') {
-                if (contain(atcoder, sc)) {
-                    continue;
-                }
-                cout << "You will lose" << endl;
-                return 0;
-            } else {
-                cout << "You will lose" << endl;
-                return 0;
-            }
-        }
+    for (int i = 0; i < s.size(); i++) {
+        char sc = s[i], tc = t[i];
+
+        if (sc == tc) continue;
+        if (sc == '@' && is_atcoder(tc)) continue;
+        if (tc == '@' && is_atcoder(sc)) continue;
+        cout << "You will lose" << endl;
+        return 0;
     }
-    cout << "You can win" << endl;
 
+    cout << "You can win" << endl;
 }
