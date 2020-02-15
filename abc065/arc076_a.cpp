76ea8de4d6630fb7ceb4ee28d2d8ea2e17400b4e
riku <riku@tanide.net>
2020-02-15 22:33:02 +0900

    【AC】[AtCoder Beginner Contest 065]C - Reconciled?

--- abc065/arc076_a.cpp
+++ abc065/arc076_a.cpp
@@ -1,32 +1,37 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
-#include <assert.h>    // LON
-#include <math.h>    // sqrt()
-
-
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-//typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
 
 struct mint {
     ll x; // typedef long long ll;
@@ -83,43 +88,41 @@ struct mint {
         mint res(*this);
         return res /= a;
     }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
 };
 
-struct combination {
-    vector<mint> fact, ifact;
-
-    combination(int n) : fact(n + 1), ifact(n + 1) {
-        assert(n < mod);
-        fact[0] = 1;
-        for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
-        ifact[n] = fact[n].inv();
-        for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
-    }
-
-    mint operator()(int n, int k) {
-        if (k < 0 || k > n) return 0;
-        return fact[n] * ifact[k] * ifact[n - k];
-    }
-} combination(10000000);
-
 int main() {
-
     int n, m;
     cin >> n >> m;
     if (n < m) swap(n, m);
 
+    vector<mint> fact(n + 1, 0);
+    fact[0] = 1;
+    for (int i = 1; i <= n; i++) fact[i] = fact[i - 1] * i;
+
     if (n == m) {
-        mint mon = combination.fact[n];
-        mint dog = combination.fact[n];
-        cout << (mon * dog * 2).x << endl;
-
-    } else if (n - 1 == m) {
-        mint mon = combination.fact[n];
-        mint dog = combination.fact[m];
-        cout << (mon * dog).x << endl;
-
-    } else {
-        cout << 0 << endl;
+        cout << fact[m] * fact[n] * 2 << endl;
+        return 0;
     }
 
+    if (n - 1 == m) {
+        cout << fact[m] * fact[n] << endl;
+        return 0;
+    }
+    cout << 0 << endl;
+
+
 }
+
