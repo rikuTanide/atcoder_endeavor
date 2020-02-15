bb2f2c63ab4113e6a7dc68e0d90d93cb5764412e
riku <riku@tanide.net>
2020-02-15 22:39:50 +0900

    【AC】[AtCoder Beginner Contest 066]C - pushpush

--- abc066/arc077_a.cpp
+++ abc066/arc077_a.cpp
@@ -1,59 +1,63 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
-#include <assert.h>    // LON
-#include <math.h>    // sqrt()
-
-
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-//typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 
 int main() {
+
+    list<ll> l;
+
     int n;
     cin >> n;
-    vector<ll> original(n);
-
-    rep(i, n)cin >> original[i];
-    deque<ll> q;
-
-    for (int i = 0; i < n; i++) {
-        ll o = original[i];
+    rep(i, n) {
+        ll a;
+        cin >> a;
         if (i % 2 == 0) {
-            q.push_front(o);
+            l.push_back(a);
         } else {
-            q.push_back(o);
+            l.push_front(a);
         }
     }
 
-    vector<ll> ans(n);
-    copy(q.begin(), q.end(), ans.begin());
-
-    if (n % 2 == 0) reverse(ans.begin(), ans.end());
-
-    for (ll a : ans) {
-        cout << a << ' ';
+    vector<ll> ans;
+    for (ll a : l) {
+        ans.push_back(a);
     }
-    cout << endl;
 
+    if (n % 2 == 1) reverse(ans.begin(), ans.end());
+
+    for (ll a : ans) cout << a << ' ';
+
+    cout << endl;
 }
+
