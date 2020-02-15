79eced1b51be5d907429c22853a2f1d5b9b93ea1
riku <riku@tanide.net>
2020-02-15 17:08:35 +0900

    【AC】[AtCoder Beginner Contest 029]C - Brute-force Attack

--- abc029/abc029_c.cpp
+++ abc029/abc029_c.cpp
@@ -1,49 +1,58 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll INF = 1e15;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
 const ll MINF = LONG_LONG_MIN;
 //const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
-
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
     int n;
     cin >> n;
-    queue<string> q;
-    q.push("");
-    while (!q.empty()) {
-        string s = q.front();
-        for (char c = 'a'; c <= 'c'; c++) {
-            string next = s + c;
-            if (next.size() == n) cout << next << endl;
-            else {
-                q.push(next);
-            }
+    set<string> ans;
+
+    queue<string> candidate;
+    candidate.push("");
+
+    while (!candidate.empty()) {
+        string top = candidate.front();
+        candidate.pop();
+
+        if (top.size() == n) {
+            ans.insert(top);
+            continue;
         }
-        q.pop();
+
+        for (char c : {'a', 'b', 'c'}) {
+            candidate.push(top + c);
+        }
     }
+
+    for (string s : ans) cout << s << endl;
+
 }
