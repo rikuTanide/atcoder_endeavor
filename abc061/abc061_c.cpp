02b3b52bd48cb76ec6b436ff42313f8c24e6923e
riku <riku@tanide.net>
2020-02-15 22:24:02 +0900

    【WA】[AtCoder Beginner Contest 061]C - Big Array

--- abc061/abc061_c.cpp
+++ abc061/abc061_c.cpp
@@ -1,100 +1,60 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
-#include <assert.h>    // LON
-#include <math.h>    // sqrt()
-
-
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-class CumulativeSum {
-    vector<ll> numbers;
-    vector<ll> sums;
-
-public:
-    CumulativeSum(ll n) {
-        numbers.resize(n);
-        sums.resize(n);
-    }
-
-    void set(ll i, ll value) {
-        numbers[i] = value;
-    }
-
-    ll getSum(ll i) {
-        if (i == -1) return 0;
-        if (i == sums.size()) return 0;
-        return sums[i];
-    }
-
-    ll getSectionSum(ll start, ll end) {
-        return getSum(end) - getSum(start - 1);
-    }
-
-    void calculate() {
-        for (ll i = 0; i < numbers.size(); i++) {
-            sums[i] = getSum(i - 1) + numbers[i];
-        }
-    }
-
-    ll lb(ll k) {
-        auto it = lower_bound(sums.begin(), sums.end(), k);
-        return distance(sums.begin(), it);
-    }
-
+struct Insert {
+    int number, count;
 };
 
-
 int main() {
-
-    ll n, k;
+    int n, k;
     cin >> n >> k;
 
-    map<ll, ll> numbers;
-    rep(i, n) {
-        ll a, b;
-        cin >> a >> b;
-        numbers[a] += b;
-    }
+    vector<Insert> numbers(n);
+    rep(i, n) cin >> numbers[i].number >> numbers[i].count;
 
-    map<ll, ll> index_to_ans;
+    sort(numbers.begin(), numbers.end(), [](Insert i, Insert j) {
+        return i.number < j.number;
+    });
 
-    CumulativeSum cs(numbers.size());
-    {
-        ll i = 0;
-        for (auto e : numbers) {
-            cs.set(i, e.second);
-            index_to_ans[i] = e.first;
-            i++;
+    int sum = 0;
+    for (Insert i : numbers) {
+        sum += i.count;
+        if (sum >= k) {
+            cout << i.number << endl;
+            return 0;
         }
     }
-    cs.calculate();
-    ll index = cs.lb(k);
-
-    cout << index_to_ans[index] << endl;
 
 }
 
