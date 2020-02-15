594fc39cb611afd11a53a853755f8a2e6c599b8a
riku <riku@tanide.net>
2020-02-15 16:51:37 +0900

    【AC】[AtCoder Beginner Contest 019]B - 高橋くんと文字列圧縮

--- abc019/abc019_2.cpp
+++ abc019/abc019_2.cpp
@@ -1,54 +1,61 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 //typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
 //const ll INF = 1e15;
 const ll MINF = LONG_LONG_MIN;
 //const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-typedef pair<char, ll> P;
+struct Press {
+    char c;
+    int i;
+};
 
 int main() {
     string s;
     cin >> s;
-    int l = 1;
-    vector<P> sequence;
-    for (int i = 1; i < s.size(); i++) {
-        if (s[i - 1] == s[i]) {
-            l++;
-        } else {
-            sequence.push_back(P(s[i - 1], l));
-            l = 1;
-        }
-    }
-    sequence.push_back(P(s.back(), l));
 
-    for (P p : sequence) {
-        cout << p.first << p.second;
+    stack<Press> press;
+
+    for (char c : s) {
+        if (press.empty()) press.push({c, 1});
+        else if (press.top().c == c) press.top().i++;
+        else press.push({c, 1});
     }
+
+    vector<Press> tmp;
+    while (!press.empty()) {
+        tmp.push_back(press.top());
+        press.pop();
+    }
+    reverse(tmp.begin(), tmp.end());
+
+    for (Press p : tmp) cout << p.c << p.i;
     cout << endl;
 
 }
-
