277ff6498845b1c24e232afd802231ee09a196a4
riku <riku@tanide.net>
2020-02-15 15:57:26 +0900

    【AC】[AtCoder Beginner Contest 004]C - 入れ替え

--- abc004/abc004_3.cpp
+++ abc004/abc004_3.cpp
@@ -1,47 +1,48 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //#define sz(x) ll(x.size())
 //typedef pair<ll, int> P;
-typedef pair<ll, ll> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
 //const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
 //const ll INF = 1e15;
 const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
 
 int main() {
-    ll n;
+
+    int n;
     cin >> n;
     n %= 30;
 
-
-    vector<int> cards(6);
-    rep(i, 6) cards[i] = i;
-
-    for (ll i = 0; i < n; i++) {
-        ll j = i % 5;
-        swap(cards[j], cards[j + 1]);
+    vector<int> numbers(6);
+    rep(i, 6) numbers[i] = i + 1;
+    for (int i = 0; i < n; i++) {
+        swap(numbers[i % 5], numbers[i % 5 + 1]);
     }
-    rep(i, 6) cout << cards[i] + 1;
+    for (int i:numbers) cout << i;
     cout << endl;
-
 }
