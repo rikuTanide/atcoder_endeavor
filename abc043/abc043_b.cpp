7f9b5dc17dd7b9456915e87556bbae561d7818ee
riku <riku@tanide.net>
2020-02-15 21:27:19 +0900

    【AC】[AtCoder Beginner Contest 043]B - Unhappy Hacking (ABC Edit)

--- abc043/abc043_b.cpp
+++ abc043/abc043_b.cpp
@@ -1,63 +1,58 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
 const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
-bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
-
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
-
-
-ll f(ll b, ll n) {
-    if (n < b) return n;
-    return f(b, n / b) + n % b;
-}
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 
 int main() {
-    stack<char> stack;
-
     string s;
     cin >> s;
+    stack<char> stack;
     for (char c : s) {
-        if (c == 'B') {
-            if (!stack.empty()) {
-                stack.pop();
-            }
-        } else {
-            stack.push(c);
-        }
+        if (c == '0') stack.push('0');
+        if (c == '1') stack.push('1');
+        if (c == 'B' && !stack.empty()) stack.pop();
     }
 
-    string ans = "";
+    vector<char> ans;
     while (!stack.empty()) {
-        ans += stack.top();
+        ans.push_back(stack.top());
         stack.pop();
     }
 
     reverse(ans.begin(), ans.end());
-    cout << ans << endl;
 
+    for (char c: ans) cout << c;
+    cout << endl;
 
 }
+
