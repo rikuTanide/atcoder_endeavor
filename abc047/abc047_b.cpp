f37b9442b8b4b94588865922d3170faca0916039
riku <riku@tanide.net>
2020-02-15 21:52:07 +0900

    【WA】[AtCoder Beginner Contest 047]B - Snuke's Coloring 2 (ABC Edit)

--- abc047/abc047_b.cpp
+++ abc047/abc047_b.cpp
@@ -1,64 +1,70 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-//typedef long long ll;
-typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
 const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
-bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
-
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 
 int main() {
 
-    int min_x = 0;
-    int max_x;
-    int min_y = 0;
-    int max_y;
-
-    cin >> max_x >> max_y;
+    int sx = 0, ex, sy = 0, ey;
+    cin >> ex >> ey;
 
     int n;
     cin >> n;
-    rep(i, n) {
-        int x, y, a;
-        cin >> x >> y >> a;
 
-        if (a == 1) {
-            cmax(min_x, x);
-        } else if (a == 2) {
-            cmin(max_x, x);
-        } else if (a == 3) {
-            cmax(min_y, y);
-        } else if (a == 4) {
-            cmin(max_y, y);
+    rep(i, n) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        if (z == 1) {
+            sx = x;
+        } else if (z == 2) {
+            ex = x;
+        } else if (z == 3) {
+            sy = y;
+        } else if (z == 4) {
+            ey = y;
+        } else {
+            __throw_runtime_error("nai");
         }
     }
 
-    int w = max_x - min_x;
-    cmax(w, 0);
-    int h = max_y - min_y;
-    cmax(h, 0);
+    int w = ex - sx;
+    int h = ey - sy;
+    if (w <= 0 || h <= 0) {
+        cout << 0 << endl;
+        return 0;
+    }
+
     cout << w * h << endl;
 
 }
+
