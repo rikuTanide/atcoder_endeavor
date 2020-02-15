322b4a6a7f9fba2bf98e320c0edb1f4cbcc8ab6d
riku <riku@tanide.net>
2020-02-15 17:38:18 +0900

    【WA】[AtCoder Beginner Contest 035]B - ドローン

--- abc035/abc035_b.cpp
+++ abc035/abc035_b.cpp
@@ -1,66 +1,64 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
 const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
+    int x = 0, y = 0;
     string s;
+    cin >> s;
+
     int t;
-    cin >> s >> t;
+    cin >> t;
 
-    int unknown = 0;
-    int x = 0;
-    int y = 0;
-
+    int q = 0;
     for (char c : s) {
-        if (c == 'L') {
-            x--;
-        } else if (c == 'R') {
-            x++;
-        } else if (c == 'U') {
-            y++;
-        } else if (c == 'D') {
-            y--;
-        } else if (c == '?') {
-            unknown++;
+        if (c == 'L') x--;
+        else if (c == 'R') x++;
+        else if (c == 'U') y++;
+        else if (c == 'D') y--;
+        else if (c == '?') q++;
+        else __throw_runtime_error("konai");
+    }
+
+    int md = abs(x) + abs(y);
+    if (t == 1) {
+        cout << md + q << endl;
+    } else {
+        if (q <= md) {
+            cout << md - q << endl;
         } else {
-            __throw_runtime_error("nai");
+            cout << md % 2 << endl;
         }
     }
 
-    int manhattan = abs(x) + abs(y);
-    if (t == 1) {
-        cout << manhattan + unknown << endl;
-    } else {
-        if (manhattan <= unknown) {
-            cout << (unknown - manhattan) % 2 << endl;
-        } else {
-            cout << manhattan - unknown << endl;
-        }
-    }
 }
+
