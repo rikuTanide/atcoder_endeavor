368feae6204ae970aabd49585a8aa8ccf24751cc
riku <riku@tanide.net>
2020-02-15 16:04:07 +0900

    【AC】[AtCoder Beginner Contest 008]B - 投票

--- abc008/abc008_2.cpp
+++ abc008/abc008_2.cpp
@@ -1,52 +1,49 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
 //const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
 //const ll INF = 1e15;
 const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 
 int main() {
-    map<string, int> vote;
+
     int n;
     cin >> n;
-    rep(i, n) {
-        string s;
-        cin >> s;
-        vote[s]++;
-    }
+    vector<string> names(n);
+    rep(i, n)cin >> names[i];
+    map<string, int> m;
+    for (string s : names) m[s]++;
 
-    int ma = 0;
-    string ma_n = "";
-    for (auto e : vote) {
-        if (e.second > ma) {
-            ma = e.second;
-            ma_n = e.first;
-        }
-    }
-    cout << ma_n << endl;
+    auto a = *max_element(m.begin(), m.end(), [](pair<string, int> p, pair<string, int> q) {
+        return p.second < q.second;
+    });
+    cout << a.first << endl;
 
 }
