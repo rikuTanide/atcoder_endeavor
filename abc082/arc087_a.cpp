89694693a0a7e5167f4a1112fa215f8ff7eba1c0
riku <riku@tanide.net>
2020-02-16 12:16:44 +0900

    【AC】[AtCoder Beginner Contest 082]C - Good Sequence

--- abc082/arc087_a.cpp
+++ abc082/arc087_a.cpp
@@ -1,50 +1,55 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = 10e15;
-const ll MINF = -10e10;
-const int INF = INT_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
+bool contain(set<ll> &s, ll a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
 
 int main() {
+
+    map<ll, int> m;
     int n;
     cin >> n;
-    map<ll, int> sequences;
     rep(i, n) {
         ll a;
         cin >> a;
-        sequences[a]++;
+        m[a]++;
     }
+
     ll ans = 0;
-    for (auto e : sequences) {
-
-        ll a = e.first;
-        ll count = e.second;
-        if (a > count) {
-            ans += count;
-        } else {
-            ans += (count - a);
-        }
+    for (auto e : m) {
+        if (e.first == e.second) continue;
+        if (e.first < e.second) ans += e.second - e.first;
+        if (e.first > e.second) ans += e.second;
     }
-
     cout << ans << endl;
-
 }
+
