861a3168d68cf7efc4d93a574bcfcace88ee38a2
riku <riku@tanide.net>
2020-02-16 11:49:30 +0900

    【AC】[AtCoder Beginner Contest 070]C - Multiple Clocks

--- abc070/abc070_c.cpp
+++ abc070/abc070_c.cpp
@@ -1,65 +1,62 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
-
-#include <assert.h>    // LON
-#include <math.h>    // sqrt()
-
-
+//using namespace boost::multiprecision;
 using namespace std;
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-//typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 
 ll gcd(ll x, ll y) {
-    if (x < y) swap(x, y);
-    if (y == 0) {
-        return x;
+    if (x > y) swap(x, y);
+    ll m = 1;
+    while (m != 0) {
+        m = y % x;
+        y = x;
+        x = m;
     }
-    return gcd(y, x % y);
+    return y;
 }
 
-ll lcm(ll x, ll y) {
-    ll g = gcd(x, y);
 
-    if (x % g == 0) {
-        return x / g * y;
-    } else {
-        return y / g * x;
-    }
+ll lcd(ll x, ll y) {
+    return x / gcd(x, y) * y;
 }
-
 
 int main() {
     int n;
     cin >> n;
-    vector<ll> nums(n);
 
-    ll ans = 1;
-    rep(i, n) {
-        ll a;
-        cin >> a;
-        ans = lcm(ans, a);
-    }
-
+    vector<ll> numbers(n);
+    rep(i, n) cin >> numbers[i];
+    ll ans = numbers.front();
+    for (ll a : numbers) ans = lcd(ans, a);
     cout << ans << endl;
 }
+
