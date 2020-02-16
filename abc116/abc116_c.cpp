80728a13571dc7fc42be86a1f8b9b8daf9ce3da3
riku <riku@tanide.net>
2020-02-16 16:29:51 +0900

    【AC】[AtCoder Beginner Contest 116]C - Grand Garden

--- abc116/abc116_c.cpp
+++ abc116/abc116_c.cpp
@@ -1,92 +1,55 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 //typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = 10e10;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
+bool contain(set<ll> &s, ll a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
 const int mod = 1000000007;
-
-ll f(ll i) {
-    if (i % 2 == 0) {
-        return i / 2;
-    } else {
-        return 3 * i + 1;
-    }
-
-}
-
-vector<P> kadan; // <takasa , futosa>
-
-
-void prt(bool b) {
-//    if (b) {
-//        cout << "matome ";
-//    } else {
-//        cout << "       ";
-//    }
-//    for (int i = 0; i < kadan.size(); i++) {
-//        cout << kadan[i].first << ' ';
-//    }
-//    cout << endl;
-}
-
-void matome() {
-
-    for (int i = 0; i < kadan.size() - 1; i++) {
-        if (kadan[i].first == kadan[i + 1].first) {
-            kadan[i].second++;
-            kadan.erase(kadan.begin() + i + 1);
-            i--;
-        }
-    }
-    prt(true);
-
-}
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 
 int main() {
     int n;
     cin >> n;
+    vector<int> numbers(n);
+    rep(i, n) cin >> numbers[i];
 
 
-    rep(i, n) {
-        int h;
-        cin >> h;
-        P p(h, 1);
-        kadan.push_back(p);
+    int ans = numbers.front();
+    for (int i = 1; i < n; i++) {
+        int l = numbers[i - 1];
+        int r = numbers[i];
+        if (l >= r) continue;
+        ans += r - l;
     }
-    ll ans = 0;
-    matome();
-    while (kadan.size() > 1) {
-
-        set<int, greater<int>> q;
-        for (int i = 0; i < kadan.size(); i++) {
-            q.insert(kadan[i].first);
-        }
-        int max = *q.begin();
-        int max2 = *(++q.begin());
-        int diff = max - max2;
-        for (int i = 0; i < kadan.size(); i++) {
-            if (kadan[i].first == max) {
-                kadan[i].first = max2;
-                ans += diff;
-                prt(false);
-            }
-        }
-        matome();
-    }
-    ans += kadan[0].first;
 
     cout << ans << endl;
 
+
 }
+
