b327e0671400044543ab6b7226923fbe6a39c7ab
riku <riku@tanide.net>
2020-02-16 12:04:39 +0900

    【AC】[AtCoder Beginner Contest 073]C - Write and Erase

--- abc073/abc073_c.cpp
+++ abc073/abc073_c.cpp
@@ -1,50 +1,57 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = 10e15;
-const ll MINF = -10e10;
-const int INF = INT_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
+bool contain(set<ll> &s, ll a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("~/Downloads/02.txt");
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 
 int main() {
-
-    set<ll> have;
-
-    ll n;
+    int n;
     cin >> n;
 
+    set<ll> numbers;
     rep(i, n) {
 
         ll a;
         cin >> a;
-        if (have.find(a) == have.end()) {
-            have.insert(a);
+        if (contain(numbers, a)) {
+            numbers.erase(a);
         } else {
-            have.erase(a);
+            numbers.insert(a);
         }
 
     }
 
-    cout << have.size() << endl;
 
+    cout << numbers.size() << endl;
 
 }
+
