c5ea48a5cf9460c7e301b4dafe7e3c2860d35d18
riku <riku@tanide.net>
2020-02-16 15:40:08 +0900

    【AC】[AtCoder Beginner Contest 094]C - Many Medians

--- abc094/arc095_a.cpp
+++ abc094/arc095_a.cpp
@@ -1,67 +1,55 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = 10e15;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
-const int mod = 1000000007;
+bool contain(set<ll> &s, ll a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-
+const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
-    ll n;
+    int n;
     cin >> n;
+    vector<ll> numbers(n);
+    rep(i, n)cin >> numbers[i];
+    vector<ll> numbers2 = numbers;
+    sort(numbers2.begin(), numbers2.end());
 
-    vector<P> xs(n);
+    ll under = numbers2[n / 2 - 1],
+            over = numbers2[n / 2];
 
-    rep(i, n) {
-        int x;
-        cin >> x;
-        P p(x, i);
-        xs[i] = p;
-    }
-    sort(xs.begin(), xs.end());
-
-    ll cli = (n - 1) / 2;
-    ll cl = xs[cli].first;
-    ll rli = n - 1 - ((n - 1) / 2);
-    ll cr = xs[rli].first;
-
-
-    vector<P> ans(n);
-
-    for (ll i = 0; i < (n / 2); i++) {
-        P p = xs[i];
-        P a(p.second, cr);
-        ans[i] = a;
-    }
-    for (ll i = (n / 2); i < n; i++) {
-        P p = xs[i];
-        P a(p.second, cl);
-        ans[i] = a;
-    }
-
-    sort(ans.begin(), ans.end());
-
-    for (P p : ans) {
-        cout << p.second << endl;
+    for (ll a : numbers) {
+        if (a <= under) {
+            cout << over << endl;
+        } else {
+            cout << under << endl;
+        }
     }
 
 }
+
