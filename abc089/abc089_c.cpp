1365884abd56c86716d9762cb98fa1c4e4179e88
riku <riku@tanide.net>
2020-02-16 13:13:53 +0900

    【AC】[AtCoder Beginner Contest 089]C - March

--- abc089/abc089_c.cpp
+++ abc089/abc089_c.cpp
@@ -1,68 +1,61 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 //typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = 10e15;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
+bool contain(set<ll> &s, ll a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
-
-    ll n;
+    int n;
     cin >> n;
-
-    vector<ll> counter(5, 0);
-
+    map<char, ll> m;
+    const vector<char> march = {'M', 'A', 'R', 'C', 'H'};
     rep(i, n) {
-
-        string s;
-        cin >> s;
-        switch (s[0]) {
-            case 'M':
-                counter[0]++;
-                break;
-            case 'A':
-                counter[1]++;
-                break;
-            case 'R':
-                counter[2]++;
-                break;
-            case 'C':
-                counter[3]++;
-                break;
-            case 'H':
-                counter[4]++;
-                break;
-        }
+        string name;
+        cin >> name;
+        m[name.front()]++;
     }
 
     ll ans = 0;
-    for (int x = 0; x < 5; x++) {
-        for (int y = x + 1; y < 5; y++) {
-            for (int z = y + 1; z < 5; z++) {
-                ans += counter[x] * counter[y] * counter[z];
+    for (int i = 0; i < 5; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            for (int k = j + 1; k < 5; k++) {
+                char c = march[i],
+                        d = march[j],
+                        e = march[k];
+
+                ans += m[c] * m[d] * m[e];
+
             }
         }
     }
-
     cout << ans << endl;
-
 }
+
