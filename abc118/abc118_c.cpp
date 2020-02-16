b5a08cc8c30770e3655a7c6de68e49a655b9a333
riku <riku@tanide.net>
2020-02-16 17:54:35 +0900

    【WA】[AtCoder Beginner Contest 118]C - Monsters Battle Royale

--- abc118/abc118_c.cpp
+++ abc118/abc118_c.cpp
@@ -1,47 +1,54 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 //typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = 10e10;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
+bool contain(set<ll> &s, ll a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
 const int mod = 1000000007;
-
-int gcd(ll x, ll y) {
-    if (x < y) swap(x, y);
-    if (y == 0) {
-        return x;
-    }
-    return gcd(y, x % y);
-}
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
     int n;
     cin >> n;
-    vector<ll> as(n);
-    rep (i, n) {
-        int a;
-        cin >> a;
-        as[i] = a;
+    vector<ll> hps(n);
+    rep(i, n) cin >> hps[i];
+    sort(hps.begin(), hps.end());
+
+    for (int i = 0; i < n; i++) {
+        ll now;
+        if (hps[i + 1] % hps[i] == 0) {
+            now = hps[i];
+        } else {
+            now = hps[i + 1] % hps[i];
+        }
+        hps[i + 1] = now;
     }
 
-
-    for (int i = 0; i < n - 1; i++) {
-        as[i + 1] = gcd(as[i], as[i + 1]);
-    }
-
-
-    cout << as[n - 1] << endl;
-
-
+    cout << hps.back() << endl;
 }
 
