833d1d94bc53eb4f7c1f3a661dbeb66b9da4f3fb
riku <riku@tanide.net>
2020-02-16 11:46:22 +0900

    【AC】[AtCoder Beginner Contest 068]C - Cat Snuke and a Voyage

--- abc068/arc079_a.cpp
+++ abc068/arc079_a.cpp
@@ -1,37 +1,41 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
-#include <assert.h>    // LON
-#include <math.h>    // sqrt()
-
-
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
+//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-//typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-// https://qiita.com/ta-ka/items/a023a11efe17ab097433
 struct Edge {
     ll to, cost;
 };
+
 
 class Dijkstra {
     ll vector_count;
@@ -49,6 +53,10 @@ public:
     void insertTowWay(ll from, ll to, ll cost) {
         edges[from].push_back({to, cost});
         edges[to].push_back({from, cost});
+    }
+
+    void insert(ll from, ll to, ll cost) {
+        edges[from].push_back({to, cost});
     }
 
     void dijkstra(ll k) {
@@ -72,35 +80,32 @@ public:
         }
     }
 
-    ll calcDistance(ll to) {
+    ll distance(ll to) {
         return distances[to];
     }
 
 };
 
-
 int main() {
-
-    ll n, m;
-    cin >> n >> m;
+    int n;
+    cin >> n;
 
     Dijkstra dijkstra(n);
-    rep(i, m) {
-        ll a, b;
-        cin >> a >> b;
 
+    int m;
+    cin >> m;
+    rep(i, m) {
+        int a, b;
+        cin >> a >> b;
         a--;
         b--;
         dijkstra.insertTowWay(a, b, 1);
     }
     dijkstra.dijkstra(0);
-
-    ll cost = dijkstra.calcDistance(n - 1);
-
-    if (cost <= 2) {
+    if (dijkstra.distance(n - 1) == 2) {
         cout << "POSSIBLE" << endl;
     } else {
         cout << "IMPOSSIBLE" << endl;
     }
-
 }
+
