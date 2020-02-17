f4197597fc49a35a7569fff1ac0f3928a6b15b34
riku <riku@tanide.net>
2020-02-17 21:04:21 +0900

    【AC】[AtCoder Regular Contest 054]A - 動く歩道

---
+++ arc054/arc054_a.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+
+bool contain(set<string> &s, string a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+int main() {
+    double l, x, y, s, d;
+    cin >> l >> x >> y >> s >> d;
+    double ans1;
+    {
+        // 時計回り
+        double distance = d - s;
+        if (distance < 0) distance += l;
+
+        double speed = y + x;
+        ans1 = distance / speed;
+    }
+
+    double ans2;
+    {
+        // 反時計回り
+        double distance = s - d;
+        if (distance < 0) distance += l;
+        double speed = y - x;
+        if (speed <= 0) ans2 = DBL_MAX;
+        else ans2 = distance / speed;
+    }
+
+    printf("%.20f\n", min(ans1, ans2));
+}
