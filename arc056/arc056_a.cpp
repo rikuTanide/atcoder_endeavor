2e014e652fd13d9d282d6c045f59af6267d20144
riku <riku@tanide.net>
2020-02-17 21:10:06 +0900

    【AC】[AtCoder Regular Contest 056]A - みんなでワイワイみかん

---
+++ arc056/arc056_a.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+
+bool contain(set<string> &s, string a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+
+int main() {
+    ll a, b, k, l;
+    cin >> a >> b >> k >> l;
+
+    if (a * l <= b) {
+        // 単品の方が安い
+        cout << a * k << endl;
+        return 0;
+    }
+
+    ll ans1 = [&]() {
+        ll set_buy_count = k / l;
+        ll set_buy_sum = set_buy_count * l;
+        ll tanpin = k - set_buy_sum;
+        return set_buy_count * b + tanpin * a;
+    }();
+
+    ll ans2 = [&]() {
+        return (k + l - 1) / l * b;
+    }();
+
+
+    cout << min(ans1, ans2) << endl;
+}
