6dde216f4067978d4705516f70b1233ada724ed5
riku <riku@tanide.net>
2020-02-17 22:08:57 +0900

    【AC】[AtCoder Grand Contest 006]A - Prefix and Suffix

---
+++ agc006/agc006_a.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+
+bool contain(set<string> &s, string a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+
+int main() {
+    int n;
+    string s, t;
+    cin >> n >> s >> t;
+
+    int l = 0;
+    for (int i = 0; i <= s.size(); i++) {
+        string a = s.substr(t.size() - i);
+        string b = t.substr(0, i);
+        if (a == b) {
+            l = i;
+        }
+    }
+
+    int now = s.size() + t.size() - l;
+    max(now, n);
+
+    cout << now << endl;
+}
