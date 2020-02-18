4f9a64cdff0673c755f3b68bb9fd7fa7fa2fb01d
riku <riku@tanide.net>
2020-02-18 22:20:38 +0900

    【AC】[AtCoder Regular Contest 081]C - Make a Rectangle

---
+++ arc081/arc081_a.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+
+bool contain(set<string> &s, string a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    map<ll, int> m;
+    rep(i, n) {
+        ll a;
+        cin >> a;
+        m[a]++;
+    }
+
+    vector<ll> c;
+    for (auto e : m) {
+        if (e.second >= 4) c.push_back(e.first);
+        if (e.second >= 2) c.push_back(e.first);
+    }
+
+    sort(c.rbegin(), c.rend());
+
+    if (c.size() < 2) {
+        cout << 0 << endl;
+    } else {
+        cout << c[0] * c[1] << endl;
+    }
+
+
+}
+
