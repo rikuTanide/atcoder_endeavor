79ac5ca044de15ef6f18297750876667f6fba22f
riku <riku@tanide.net>
2020-02-18 21:34:04 +0900

    【WA】[AtCoder Regular Contest 080]C - 4-adjacent

--- arc080/arc080_a.cpp
+++ arc080/arc080_a.cpp
@@ -1,75 +1,108 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
-#include <assert.h>    // LON
-#include <math.h>    // sqrt()
-
-
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
 
+bool contain(set<string> &s, string a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-//typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
+vector<pair<ll, int >> factorize(ll n) {
+    vector<pair<ll, int >> res;
 
-int main() {
-
-    ll n;
-    cin >> n;
-
-    ll b4 = 0;
-    ll b2 = 0;
-    ll k = 0;
-    rep(i, n) {
-        ll a;
-        cin >> a;
-        if (a % 4 == 0) {
-            b4++;
-        } else if (a % 2 == 0) {
-            b2++;
-        } else {
-            k++;
+    for (ll i = 2; i * i <= n; i++) {
+        if (n % i != 0) {
+            continue;
+        }
+        res.emplace_back(i, 0);
+        while (n % i == 0) {
+            n /= i;
+            res.back().second++;
         }
     }
 
-    ll b4_r = b4 + 1;
-    ll k_r = k - b4;
-    if (b4_r < k) {
-        cout << "No" << endl;
-        return 0;
+    if (n != 1) res.emplace_back(n, 1);
+    return res;
+
+}
+
+ll comb(ll l, ll r) {
+    vector<ll> factors(50);
+    for (ll j = l; j > (l - r); j--) {
+        auto fs = factorize(j);
+        for (auto f : fs) {
+            factors[f.first] += f.second;
+        }
     }
 
-    b4_r -= k;
-
-
-    if (b2 == 1 && b4_r == 0) {
-        cout << "No" << endl;
-        return 0;
+    for (ll j = 1; j <= r; j++) {
+        auto fs = factorize(j);
+        for (auto f : fs) {
+            factors[f.first] -= f.second;
+        }
     }
 
-    if (k_r > 0 && b2 > 0) {
-        cout << "No" << endl;
-        return 0;
+    ll s = 1;
+
+    for (int j = 1; j < 50; j++) {
+        for (int k = 0; k < factors[j]; k++) {
+            s *= j;
+        }
     }
 
-    cout << "Yes" << endl;
+    return s;
+
+}
+
+
+int main() {
+    int n;
+
+    cin >> n;
+    int odd = 0, b2 = 0, b4 = 0;
+    rep(i, n) {
+        int a;
+        cin >> a;
+        if (a % 4 == 0) b4++;
+        else if (a % 2 == 0) b2++;
+        else odd++;
+    }
+    if (b2 == 1) {
+        odd++;
+    }
+
+    if (odd - 1 > b4) {
+        cout << "No" << endl;
+    } else {
+        cout << "Yes" << endl;
+    }
 
 
 }
+
