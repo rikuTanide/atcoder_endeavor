cc417bb2dc73863ff78f3bc19e51ca02185e5557
riku <riku@tanide.net>
2020-02-19 21:11:15 +0900

    【AC】[AtCoder Beginner Contest 128]C - Switches

--- abc128/abc128_c.cpp
+++ abc128/abc128_c.cpp
@@ -1,58 +1,84 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 //typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = 1001001001;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
-typedef tuple<string, int, int> T;
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
+
     int n, m;
     cin >> n >> m;
 
-    vector<vector<int>> vec(m);
+    vector<vector<int>> code(n);
 
     rep(i, m) {
         int k;
         cin >> k;
-        vec[i].resize(k);
+
         rep(j, k) {
-            cin >> vec[i][j];
-            vec[i][j]--;
+            int s;
+            cin >> s;
+            s--;
+            code[s].push_back(i);
         }
     }
 
-    vector<int> p(m);
-    rep(i, m) {
-        cin >> p[i];
-    }
-
+    vector<int> ps(m);
+    rep(i, m) cin >> ps[i];
     int ans = 0;
     for (int i = 0; i < (1 << n); i++) {
-        bool ok = true;
-        rep(j, m) {
-            int c = 0;
-            for (int id : vec[j]) {
-                if ((i >> id) & 1) {
-                    c++;
+
+        vector<int> amp(m, 0);
+
+        for (int j = 0; j < n; j++) {
+            if ((i >> j) & 1) {
+
+                for (int k : code[j]) {
+                    amp[k]++;
+                }
+
+            }
+        }
+
+        [&] {
+            for (int j = 0; j < m; j++) {
+                if (amp[j] % 2 != ps[j]) {
+                    return;
                 }
             }
-            c %= 2;
-            if (c != p[j]) {
-                ok = false;
-            }
-        }
-        if (ok) {
             ans++;
-        }
+        }();
+
     }
     cout << ans << endl;
 }
