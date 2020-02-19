e2a27b12ff43f7f6bdb00c50d420779c5ae44c6f
riku <riku@tanide.net>
2020-02-19 20:43:14 +0900

    【AC】[AtCoder Beginner Contest 123]C - Five Transportations

--- abc123/abc123_c.cpp
+++ abc123/abc123_c.cpp
@@ -1,37 +1,50 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 //typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-//const int INF = INT_MAX  ;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
 const int mod = 1000000007;
-
-typedef pair<int, char> PC;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
-
     ll n;
     cin >> n;
 
-    ll bn = INF;
+    ll neck = INF;
     rep(i, 5) {
-        ll t;
-        cin >> t;
-        bn = min(t, bn);
+        ll a;
+        cin >> a;
+        cmin(neck, a);
     }
 
-    ll ans = 4 + ((n + bn - 1) / bn) ;
-
-    cout << ans << endl;
+    cout << (n + neck - 1) / neck + 4 << endl;
 
 }
-
