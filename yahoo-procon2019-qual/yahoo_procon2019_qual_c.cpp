029b844f4c251fb20834df09930207d5596d29c6
riku <riku@tanide.net>
2020-02-19 19:17:32 +0900

    【AC】[「みんなのプロコン 2019」]C - When I hit my pocket...

---
+++ yahoo-procon2019-qual/yahoo_procon2019_qual_c.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
+
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+ll calc_a2(ll k, ll a, ll b) {
+    ll nk = k - a + 1;
+    if (k < 0) return 0;
+
+    ll ns = nk / 2;
+
+    ll pub = b - a;
+    if (pub <= 0) return 0;
+    ll nb = ns * pub;
+
+    ll tb = a + nb + (nk % 2);
+    return tb;
+}
+
+
+int main() {
+    ll k, a, b;
+    cin >> k >> a >> b;
+
+    ll a1 = k + 1;
+
+    ll a2 = calc_a2(k, a, b);
+
+
+    cout << max(a1, a2) << endl;
+}
