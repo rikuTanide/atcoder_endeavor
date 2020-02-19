690b00e46cb24ea85210d2470b22943d4f5c8d8e
riku <riku@tanide.net>
2020-02-19 14:02:20 +0900

    【AC】[キーエンス プログラミング コンテスト 2019]C - Exam and Wizard

---
+++ keyence2019/keyence2019_c.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
+
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return 0;
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void calculate() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
+
+
+int main() {
+
+    int n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    rep(i, n) cin >> a[i];
+    rep(i, n) cin >> b[i];
+
+    ll asum = accumulate(a.begin(), a.end(), 0ll),
+            bsum = accumulate(b.begin(), b.end(), 0ll);
+    if (bsum > asum) {
+        cout << -1 << endl;
+        ret();
+    }
+
+    vector<ll> d(n);
+    rep(i, n) d[i] = a[i] - b[i];
+    sort(d.rbegin(), d.rend());
+
+    ll minus = 0;
+    ll minus_count = 0;
+    rep(i, n) {
+        if (d[i] < 0) {
+            minus += abs(d[i]);
+            minus_count++;
+        }
+    }
+
+    if (minus_count == 0) {
+        cout << 0 << endl;
+        ret();
+    }
+
+    CumulativeSum cs(n);
+    rep(i, n) cs.set(i, d[i]);
+    cs.calculate();
+    rep(i, n) {
+        if (cs.getSum(i) >= minus) {
+            cout << i + 1 + minus_count << endl;
+            ret();
+        }
+    }
+
+}
