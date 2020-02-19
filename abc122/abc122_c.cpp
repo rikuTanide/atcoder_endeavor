fd4b3bf898ff56f2983365bf1e7f201a424b936b
riku <riku@tanide.net>
2020-02-19 20:38:15 +0900

    【AC】[AtCoder Beginner Contest 122]C - GeT AC

--- abc122/abc122_c.cpp
+++ abc122/abc122_c.cpp
@@ -1,21 +1,71 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 //typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-//const int INF = INT_MAX  ;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-typedef tuple<ll, int, int, int> PC;
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return 0;
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void calculate() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
+
 
 int main() {
     int n, q;
@@ -24,23 +74,39 @@ int main() {
     string s;
     cin >> s;
 
-    vector<ll> dp(n + 1, 0);
-    for (int i = 1; i < n; i++) {
-        if (s[i - 1] == 'A' && s[i] == 'C') {
-            dp[i] = dp[i - 1] + 1;
-        } else {
-            dp[i] = dp[i - 1];
+    CumulativeSum rsum(n), lsum(n);
+
+    for (int i = 0; i < n - 1; i++) {
+        if (s[i] == 'A' && s[i + 1] == 'C') {
+            lsum.set(i, 1);
         }
     }
 
-    for (int i = 0; i < q; i++) {
+    for (int i = 1; i < n; i++) {
+        if (s[i] == 'C' && s[i - 1] == 'A') {
+            rsum.set(i, 1);
+        }
+    }
+
+    rsum.calculate();
+    lsum.calculate();
+
+    rep(i, q) {
         int l, r;
         cin >> l >> r;
         l--;
         r--;
-        cout << dp[r] - dp[l] << endl;
+        //         return getSum(end) - getSum(start - 1);
+
+
+        ll rs = rsum.getSum(r);
+        ll ls = lsum.getSum(l - 1);
+
+        ll now = rs - ls;
+
+        cout << now << endl;
+
     }
 
 
 }
-
