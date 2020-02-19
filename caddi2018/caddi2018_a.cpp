0df433dfc010f5f0c4105b2f4e79834a25912bce
riku <riku@tanide.net>
2020-02-19 13:26:26 +0900

    【WA】[CADDi 2018]C - Product and GCD

---
+++ caddi2018/caddi2018_a.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
+
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+vector<pair<ll, int >> factorize(ll n) {
+    vector<pair<ll, int >> res;
+
+    for (ll i = 2; i * i <= n; i++) {
+        if (n % i != 0) {
+            continue;
+        }
+        res.emplace_back(i, 0);
+        while (n % i == 0) {
+            n /= i;
+            res.back().second++;
+        }
+    }
+
+    if (n != 1) res.emplace_back(n, 1);
+    return res;
+
+}
+
+int main() {
+    ll n, p;
+    cin >> n >> p;
+
+    auto f = factorize(p);
+
+    ll ans = 1;
+    for (auto e : f) {
+        if (e.second < n) continue;
+        ans *=e.first * (e.second / n);
+    }
+
+    cout << ans << endl;
+
+}
