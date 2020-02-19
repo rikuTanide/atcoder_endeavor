c064a35a0ed00472af9ab5b55cc17a81b41b16a4
riku <riku@tanide.net>
2020-02-19 20:57:46 +0900

    【AC】[AtCoder Beginner Contest 126]C - Dice and Coin

--- abc126/abc126_c.cpp
+++ abc126/abc126_c.cpp
@@ -1,41 +1,58 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = 1001001001;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
-const int mod = 1e9 + 7;
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+double check(int i, int k) {
+    double j = 1;
+    while (i < k) {
+        i *= 2;
+        j /= 2;
+    }
+    return j;
+}
 
 int main() {
     int n, k;
     cin >> n >> k;
 
-    double p = 0;
-
+    double ans = 0;
     for (int i = 1; i <= n; i++) {
-        // iを何回２倍したらkを超えるか
-        int c = 0;
-        int s = i;
-        while (true) {
-            if (s >= k) {
-                break;
-            }
-            s *= 2;
-            c++;
-        }
-        double ap = 1 / pow(2, c);
-        p += ap;
+        double j = check(i, k);
+        ans += (j / n);
     }
+    printf("%.20f\n", ans);
 
-    cout << printf("%.10f", p / n) << endl;
 }
 
