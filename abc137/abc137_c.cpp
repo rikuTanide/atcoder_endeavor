b83741411217b5efbd58b863f79c8bb679ad1c4c
riku <riku@tanide.net>
2020-02-20 20:59:45 +0900

    【AC】[AtCoder Beginner Contest 137]C - Green Bin

--- abc137/abc137_c.cpp
+++ abc137/abc137_c.cpp
@@ -1,33 +1,55 @@
 #include <bits/stdc++.h>
+#include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) int(x.size())
 typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
+
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
-
-    ll n;
+    int n;
     cin >> n;
-    map<string, int> strs;
 
-    ll ans = 0;
+    map<string, int> m;
     rep(i, n) {
-        string str;
-        cin >> str;
-        sort(str.begin(), str.end());
-
-        if (strs.find(str) == strs.end()) {
-            strs[str] = 1;
-        } else {
-            strs[str]++;
-        }
+        string s;
+        cin >> s;
+        sort(s.begin(), s.end());
+        m[s]++;
     }
-
-    for (auto itr = strs.begin(); itr != strs.end(); ++itr) {
-        ll value = itr->second;
-        ans += value * (value - 1) / 2;
+    ll ans = 0;
+    for (auto e : m) {
+        ll count = e.second;
+        ans += (count * (count - 1)) / 2;
     }
-
     cout << ans << endl;
 }
+
