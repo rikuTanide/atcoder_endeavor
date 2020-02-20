bd47b4143fb00ef5f2ac66f6851a695167c225ff
riku <riku@tanide.net>
2020-02-20 18:54:52 +0900

    【WA】[AtCoder Beginner Contest 130]C - Rectangle Cutting

--- abc130/abc130_c.cpp
+++ abc130/abc130_c.cpp
@@ -1,26 +1,47 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 //typedef pair<ll, ll> P;
-const ll INF = 1001001001;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
+
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
     ll w, h, x, y;
+
     cin >> w >> h >> x >> y;
 
-    double ans = (double) w * h / 2;
+    bool b = x == (w / 2) && y == (h / 2);
 
-    if (x * 2 == w && y * 2 == h) {
-        cout << ans << ' ' << 1 << endl;
-    } else {
-        cout << ans << ' ' << 0 << endl;
-
-    }
+    printf("%.20f %d\n", double(w) * h / 2, b ? 1 : 0);
 
 }
+
