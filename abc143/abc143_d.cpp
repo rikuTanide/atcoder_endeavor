2e02bec5f6ff1365dfb62e8ff469ed48a878d42e
riku <riku@tanide.net>
2020-02-20 22:58:04 +0900

    【WA】[AtCoder Beginner Contest 143]D - Triangles

--- abc143/abc143_d.cpp
+++ abc143/abc143_d.cpp
@@ -1,49 +1,70 @@
 #include <bits/stdc++.h>
+#include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
 typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
+
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+int min_count(vector<int>::iterator s, vector<int>::iterator e, int k) {
+    auto it = lower_bound(s, e, k);
+    if (*it == k) {
+        if (it == s) {
+            return 0;
+        } else {
+            it--;
+        }
+    }
+    int now = distance(s, it);
+    return now;
+}
 
 int main() {
-
     int n;
     cin >> n;
 
-    vector<int> ls(n);
+    vector<int> edges(n);
+    rep(i, n) cin >> edges[i];
+    sort(edges.begin(), edges.end());
 
-    rep(i, n) {
-        int l;
-        cin >> l;
-        ls[i] = l;
-    }
 
-    sort(ls.begin(), ls.end());
-
-    int count = 0;
-    for (int x = 0; x < n; x++) {
-        int a = ls[x];
-        for (int y = x + 1; y < n; y++) {
-            int b = ls[y];
-            for (int z = y + 1; z < n; z++) {
-
-                int c = ls[z];
-
-                if (a >= (b + c)) {
-                    break;
-                }
-                if (b >= (a + c)) {
-                    break;
-                }
-                if (c >= (b + a)) {
-                    break;
-                }
-                count++;
-            }
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int l = edges[i] + edges[j];
+            int k = min_count(edges.begin() + j + 1, edges.end(), l);
+            ans += k;
         }
     }
-
-
-    cout << count << endl;
-
+    cout << ans << endl;
 }
+
 
