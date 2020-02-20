bd25d4972dcf5c86768ea4fcecd23a40a5bc2524
riku <riku@tanide.net>
2020-02-20 19:39:59 +0900

    【AC】[AtCoder Beginner Contest 133]C - Remainder Minimization 2019

--- abc133/abc133_c.cpp
+++ abc133/abc133_c.cpp
@@ -1,32 +1,90 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
-const ll INF = 1001001001;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return 0;
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void calculate() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
 
 int main() {
-
     ll l, r;
     cin >> l >> r;
 
-    r = min(r, l + (2 * 2019));
+    for (ll i = l; i <= r; i++) {
+        if (i % 2019 == 0) {
+            cout << 0 << endl;
+            return 0;
+        }
+    }
 
-    int ans = 2018;
-
+    ll ans = INF;
     for (ll i = l; i <= r; i++) {
         for (ll j = i + 1; j <= r; j++) {
-            int x = i * j % 2019;
-            ans = min(ans, x);
+            ll now = (i * j) % 2019;
+            cmin(ans, now);
         }
     }
 
     cout << ans << endl;
-
 }
 
