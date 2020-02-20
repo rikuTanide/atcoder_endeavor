1b49ec49c4a2f0f79e23e500fc2bc7e41ee69c06
riku <riku@tanide.net>
2020-02-20 19:24:12 +0900

    【AC】[AtCoder Beginner Contest 131]D - Megalomania

--- abc131/abc131_d.cpp
+++ abc131/abc131_d.cpp
@@ -1,53 +1,94 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 //typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-const ll INF = 1001001001;
+typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-ll gcd(ll x, ll y) {
-    if (y == 0) {
-        return x;
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
     }
-    return gcd(y, x % y);
-}
 
-ll lcm(ll a, ll b) {
-    return a * b / gcd(a, b);
-}
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return 0;
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void calculate() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
 
 int main() {
-    ll n;
+    int n;
     cin >> n;
-    priority_queue<P> ab;
+
+    vector<P> task(n);
+
+    rep(i, n) cin >> task[i].second >> task[i].first;
+    sort(task.begin(), task.end());
+
+    CumulativeSum times(n);
 
     rep(i, n) {
-        ll a, b;
-        cin >> a >> b;
-        ab.push(P(b, a));
+        P p = task[i];
+        times.set(i, p.second);
     }
+    times.calculate();
 
-    ll current_limit = INF;
-    while (!ab.empty()) {
-        P p = ab.top();
-        ll this_limit = p.first;
-        ll this_time = p.second;
-
-        current_limit = min(current_limit, this_limit);
-        current_limit -= this_time;
-
-        if (current_limit < 0) {
+    rep(i, n) {
+        if (times.getSum(i) > task[i].first) {
             cout << "No" << endl;
-            return 0;
+            ret();
         }
-        ab.pop();
     }
-
     cout << "Yes" << endl;
 }
+
