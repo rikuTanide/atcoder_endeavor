8d0d6f2d384c71dcb2fe54d2e3a80fc31b161db7
riku <riku@tanide.net>
2020-02-20 21:25:29 +0900

    【WA】[第一回日本最強プログラマー学生選手権-予選-]B - Kleene Inversion

---
+++ jsc2019-qual/jsc2019_qual_b.cpp
@@ -0,0 +1,167 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
+
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
+
+
+struct BIT {
+private:
+    vector<int> bit;
+    int N;
+
+public:
+    BIT(int size) {
+        N = size;
+        bit.resize(N + 1);
+    }
+
+    // 一点更新です
+    void add(int a, int w) {
+        for (int x = a; x <= N; x += x & -x) bit[x] += w;
+    }
+
+    // 1~Nまでの和を求める。
+    int sum(int a) {
+        int ret = 0;
+        for (int x = a; x > 0; x -= x & -x) ret += bit[x];
+        return ret;
+    }
+};
+
+
+int main() {
+    int n;
+    ll k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) { cin >> v[i]; }
+
+    mint tento = 0;
+    BIT b(n);  // これまでの数字がどんな風になってるのかをメモる為のBIT
+    for (int i = 0; i < n; i++) {
+        tento += i - b.sum(v[i]); // BITの総和 - 自分より左側 ＝ 自分より右側
+        b.add(v[i], 1); // 自分の位置に1を足す(ダジャレではないです)
+    }
+
+    tento *= k;
+
+    vector<int> min_count(n, 0);
+
+    rep(i, n) {
+        rep(j, n) {
+            if (v[i] > v[j]) min_count[i]++;
+        }
+    }
+
+    mint c = 0;
+    for (int i : min_count) {
+        c += i * k * (k - 1) / 2;
+    }
+
+    cout << (tento + c) << endl;
+
+
+}
+
+
