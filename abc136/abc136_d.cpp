3fb4ffea1550ec0e0d4cd907f94061ab4b8e4540
riku <riku@tanide.net>
2020-02-20 20:53:04 +0900

    【AC】[AtCoder Beginner Contest 136]D - Gathering Children

--- abc136/abc136_d.cpp
+++ abc136/abc136_d.cpp
@@ -1,46 +1,75 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-typedef long long ll;
+//typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
-const ll INF = 1001001001;
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
-
     string s;
     cin >> s;
 
-    int n = s.size();
-    vector<int> ans(n);
+    vector<int> r(s.size());
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == 'R') r[i] = i;
+        else {
+            r[i] = r[i - 1];
+        }
+    }
+    vector<int> l(s.size());
+    for (int i = s.size() - 1; i >= 0; i--) {
+        if (s[i] == 'L') l[i] = i;
+        else l[i] = l[i + 1];
+    }
 
-    rep(r1, 2) {
-        {
-            int count = 0;
-            rep(i, n) {
-                if (s[i] == 'R') count++;
-                else {
-                    ans[i] += count / 2;
-                    ans[i - 1] += (count + 1) / 2;
-                    count = 0;
-                }
+    vector<int> ans(s.size(), 0);
+
+    rep(i, s.size()) {
+        if (s[i] == 'L') {
+            int nr = r[i];
+            if ((i - nr) % 2 == 0) {
+                ans[nr]++;
+            } else {
+                ans[nr + 1]++;
+            }
+        } else {
+            int nl = l[i];
+            if ((nl - i) % 2 == 0) {
+                ans[nl]++;
+            } else {
+                ans[nl - 1]++;
             }
         }
-
-        reverse(ans.begin(), ans.end());
-        reverse(s.begin(), s.end());
-        rep(i, n) {
-            if (s[i] == 'L')s[i] = 'R';
-            else s[i] = 'L';
-        }
-
     }
-
-    rep(i, n) {
-        cout << ans[i] << ' ';
-    }
-    cout << endl;
+    for (int i : ans) printf("%d ", i);
 }
+
