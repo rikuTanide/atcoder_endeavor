bd153afd90430fc9fc55a4ea22485cc9b99dffd2
riku <riku@tanide.net>
2020-02-21 22:58:51 +0900

    【AC】[AtCoder Beginner Contest 005]C - おいしいたこ焼きの売り方

--- abc005/abc005_3.cpp
+++ abc005/abc005_3.cpp
@@ -1,70 +1,75 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef long long ll;
 typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 //typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = 10e15;
-const ll MINF = -10e10;
-const int INF = INT_MAX / 100;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("~/Downloads/02.txt");
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
-
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
-    int t;
-    cin >> t;
+    int t, n;
+    cin >> t >> n;
 
-    int n;
-    cin >> n;
-    queue<int> takoyaki_q;
+    queue<int> takoyaki;
     rep(i, n) {
         int a;
         cin >> a;
-        takoyaki_q.push(a);
+
+        takoyaki.push(a);
     }
 
     int m;
     cin >> m;
+    queue<int> consumers;
     rep(i, m) {
         int b;
         cin >> b;
-
-        bool ok = [&] {
-            while (!takoyaki_q.empty()) {
-                int f = takoyaki_q.front();
-                if (f > b) {
-                    return false;
-                }
-                if (b - t > f) {
-                    takoyaki_q.pop();
-                    continue;
-                }
-                takoyaki_q.pop();
-                return true;
-            }
-            return false;
-        }();
-        if (!ok) {
-            cout << "no" << endl;
-            return 0;
-        }
+        consumers.push(b);
     }
-    cout << "yes" << endl;
 
+    while (!consumers.empty()) {
+        int c = consumers.front();
+        consumers.pop();
+
+        while (!takoyaki.empty() && takoyaki.front() + t < c) {
+            takoyaki.pop();
+        }
+        if (takoyaki.empty() || takoyaki.front() > c) {
+            cout << "no" << endl;
+            ret();
+        }
+        takoyaki.pop();
+
+    }
+
+    cout << "yes" << endl;
 }
