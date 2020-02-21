44f528f7cfe3759e526899093c334eb5fb2a3884
riku <riku@tanide.net>
2020-02-21 21:12:43 +0900

    【AC】[AtCoder Beginner Contest 154]D - Dice in Line

--- abc154/abc154_d.cpp
+++ abc154/abc154_d.cpp
@@ -1,49 +1,39 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
+const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
 //typedef pair<ll, int> P;
-typedef pair<ll, ll> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 10;
+//const ll INF = LONG_LONG_MAX / 100;
 //const ll INF = (1ll << 31) - 1;
 //const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
+//const ll MINF = LONG_LONG_MIN;
 //const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 //typedef pair<int, int> P;
 //typedef pair<double, double> P;
+#define ret() return 0;
 
-bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-
-void print_line(vector<ll> &line) {
-    if (line.size() == 0ll) {
-        cout << endl;
-        return;
-    }
-    for (ll i = 0; i < line.size(); i++) {
-        cout << line[i];
-        if (i == line.size() - 1) cout << endl;
-        else cout << ' ';
-    }
-}
-
-
-typedef priority_queue<long long, vector<long long>, greater<long long>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 class CumulativeSum {
     vector<double> numbers;
@@ -81,25 +71,20 @@ public:
 int main() {
     int n, k;
     cin >> n >> k;
-
-    vector<int> dices(n);
-    rep(i, n) cin >> dices[i];
-    vector<double> es(n);
-    rep(i, n) {
-        double d = dices[i];
-        es[i] = d * (d + 1) / 2 / d;
-    }
-
     CumulativeSum cs(n);
-    rep(i, n) cs.set(i, es[i]);
+    rep (i, n) {
+        double p;
+        cin >> p;
+
+        double e = (p + 1) / 2;
+        cs.set(i, e);
+    }
     cs.calculate();
     double ans = 0;
     for (int i = 0; i + k <= n; i++) {
         cmax(ans, cs.getSectionSum(i, i + k - 1));
     }
-
     printf("%.20f\n", ans);
-
 }
 
 
