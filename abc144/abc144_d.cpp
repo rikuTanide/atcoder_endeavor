d7e13f76a160541bb7fc6ef0441bcf75648687c2
riku <riku@tanide.net>
2020-02-21 19:51:22 +0900

    【AC】[AtCoder Beginner Contest 144]D - Water Bottle

--- abc144/abc144_d.cpp
+++ abc144/abc144_d.cpp
@@ -1,25 +1,54 @@
 #include <bits/stdc++.h>
+#include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
+
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 
 int main() {
     double a, b, x;
     cin >> a >> b >> x;
 
-    double rad;
-    if (x / a > (a * b / 2)) {
-        double h = 2 * (a*b - (x/a)) / a;
-        rad = atan2(h , a);
-    }else {
-        double h = 2 * x / (a * b);
-        rad = atan2(b,h);
+    x /= a;
+
+    if (x >= a * b / 2) {
+        double c = 2.0 * x / a - b;
+        printf("%.20f\n", atan2(b - c, a) / (2 * PI) * 360);
+    } else {
+        double c = x * 2 / b;
+        printf("%.20f\n", atan2(b, c) / (2 * PI) * 360);
     }
-
-
-    double deg = rad / (2 * M_PI) * 360;
-
-    printf("%.10f", deg);
-
 }
+
+
