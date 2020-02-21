f848528406a174c7020df7a60a5cee0b1305d590
riku <riku@tanide.net>
2020-02-21 22:45:59 +0900

    【AC】[AtCoder Beginner Contest 003]C - AtCoderプログラミング講座

--- abc003/abc003_3.cpp
+++ abc003/abc003_3.cpp
@@ -1,44 +1,50 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
 //typedef pair<ll, int> P;
-typedef pair<ll, ll> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
 //const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
 //const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
-bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
-
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
     int n, k;
     cin >> n >> k;
-    vector<double> atcoders(n);
-    rep(i, n) cin >> atcoders[i];
-    sort(atcoders.rbegin(), atcoders.rend());
+    vector<int> videos(n, 0);
+    rep(i, n) cin >> videos[i];
+    sort(videos.begin(), videos.end());
+
     double rate = 0;
-    rep(i, k) {
-        double next = atcoders[k - i - 1];
-        if (next < rate) break;
-        rate = (rate + next) / 2;
+    for (int i = n - k; i < n; i++) {
+        rate = (videos[i] + rate) / 2;
     }
     printf("%.20f\n", rate);
 }
