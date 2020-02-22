2693d6b2164b5641ef5848139e352e47a5e7a054
riku <riku@tanide.net>
2020-02-22 21:10:52 +0900

    【AC】[AtCoder Beginner Contest 156]C - Rally

---
+++ abc156/abc156_c.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+//const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
+
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> places(n);
+    rep(i, n) cin >> places[i];
+    sort(places.begin(), places.end());
+    ll ans = LONG_LONG_MAX;
+    for (ll p = places.front(); p <= places.back(); p++) {
+
+        ll now = 0;
+
+        for (ll x : places) {
+            ll d = abs(p - x);
+            now += d * d;
+        }
+
+        cmin(ans, now);
+    }
+    cout << ans << endl;
+
+}
