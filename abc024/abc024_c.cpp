798837cfb7003eb5cd5dd2802c827c7a4020f94c
riku <riku@tanide.net>
2020-02-23 15:09:38 +0900

    【AC】[AtCoder Beginner Contest 024]C - 民族大移動

--- abc024/abc024_c.cpp
+++ abc024/abc024_c.cpp
@@ -1,38 +1,72 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 //typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+class Conv {
+    ll cursor = 0;
+    map<ll, ll> to_short; // <original, small >
+    map<ll, ll> to_long; // <small, original>
 
-struct Open {
-    int open;
-    int close;
+
+public:
+    void set(ll original) {
+        if (to_short.find(original) != to_short.end()) {
+            return;
+        }
+        to_long[cursor] = original;
+        to_short[original] = cursor;
+        cursor++;
+    }
+
+    ll revert(ll after) {
+        assert(to_long.find(after) != to_long.end());
+        return to_long[after];
+    }
+
+    ll convert(ll original) {
+        assert(to_short.find(original) != to_short.end());
+        return to_short[original];
+    }
+
+    ll next() {
+        return cursor;
+    }
+
 };
+
 
 class UnionFind {
 public:
@@ -71,111 +105,80 @@ public:
         return true;
     }
 
+    bool is_union(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        return ra == rb;
+    }
 };
 
-class Conv {
-    ll cursor = 0;
-    map<ll, ll> to_short; // <original, small >
-    map<ll, ll> to_long; // <small, original>
+ll calc(ll start, ll goal, Conv &conv, ll d, vector<P> &moving_area) {
+    if (start == goal) {
+        return 0;
+    }
 
+    UnionFind uf(conv.next());
 
-public:
-    void set(ll original) {
-        if (to_short.find(original) != to_short.end()) {
-            return;
+    for (int i = 0; i < d; i++) {
+        bool b = [&] {
+            for (ll m = moving_area[i].first; m <= moving_area[i].second; m++) {
+                if (uf.is_union(start, m)) return true;
+            }
+            return false;
+        }();
+
+        if (b) {
+            for (ll m = moving_area[i].first; m <= moving_area[i].second; m++) {
+                uf.connect(start, m);
+            }
         }
-        to_long[cursor] = original;
-        to_short[original] = cursor;
-        cursor++;
+        if (uf.is_union(start, goal)) {
+            return i + 1;
+        }
     }
-
-    ll revert(ll after) {
-        assert(to_long.find(after) != to_long.end());
-        return to_long[after];
-    }
-
-    ll convert(ll original) {
-        assert(to_short.find(original) != to_short.end());
-        return to_short[original];
-    }
-
-    ll next() {
-        return cursor;
-    }
-
-};
+    __throw_runtime_error("nai");
+}
 
 int main() {
-
     ll n, d, k;
     cin >> n >> d >> k;
-    vector<P> ranges(d);
-    rep(i, d) {
-        cin >> ranges[i].first;
-        cin >> ranges[i].second;
 
-        ranges[i].first--;
-        ranges[i].second--;
-    }
+    vector<P> moving_area(d);
+    rep(i, d) cin >> moving_area[i].first >> moving_area[i].second;
 
-    vector<P> targets(k);
-    rep(i, k) {
-        cin >> targets[i].first;
-        cin >> targets[i].second;
-        targets[i].first--;
-        targets[i].second--;
-    }
+    vector<P> ethnic_move(k);
+    rep(i, k) cin >> ethnic_move[i].first >> ethnic_move[i].second;
 
     Conv conv;
     {
-        set<ll> points;
-        rep(i, d) {
-            points.insert(ranges[i].first);
-            points.insert(ranges[i].second);
+        set<ll> use_points;
+        for (P p : moving_area) {
+            use_points.insert(p.first);
+            use_points.insert(p.second);
         }
-        rep(i, k) {
-            points.insert(targets[i].first);
-            points.insert(targets[i].second);
+        for (P p : ethnic_move) {
+            use_points.insert(p.first);
+            use_points.insert(p.second);
         }
-        for (ll p : points) {
-            conv.set(p);
+        for (ll l : use_points) {
+            conv.set(l);
         }
     }
 
-    vector<P> conv_ranges(d);
     rep(i, d) {
-        conv_ranges[i].first = conv.convert(ranges[i].first);
-        conv_ranges[i].second = conv.convert(ranges[i].second);
+        moving_area[i].first = conv.convert(moving_area[i].first);
+        moving_area[i].second = conv.convert(moving_area[i].second);
+    }
+    rep(i, k) {
+        ethnic_move[i].first = conv.convert(ethnic_move[i].first);
+        ethnic_move[i].second = conv.convert(ethnic_move[i].second);
     }
 
-    rep(i, k) {
-        UnionFind uf(conv.next());
-        P target = targets[i];
-        int start = conv.convert(target.first);
-        int goal = conv.convert(target.second);
+    for (P p : ethnic_move) {
+        ll start = p.first, goal = p.second;
+        ll i = calc(start, goal, conv, d, moving_area);
 
-        rep(j, d) {
-            P cr = conv_ranges[j];
-
-            bool b = [&] {
-                for (ll l = cr.first; l <= cr.second; l++) {
-                    if (uf.root(start) == uf.root(l)) {
-                        return true;
-                    }
-                }
-                return false;
-            }();
-
-            if (b) {
-                for (ll l = cr.first; l < cr.second; l++) {
-                    uf.connect(l, l + 1);
-                }
-            }
-            if (uf.root(start) == uf.root(goal)) {
-                cout << j + 1 << endl;
-                break;
-            }
-        }
+        cout << i << endl;
     }
 
 }
