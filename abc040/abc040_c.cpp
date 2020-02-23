27cf05090253c87198941953ff90afe2cd8ce263
riku <riku@tanide.net>
2020-02-23 20:33:33 +0900

    【WA】[AtCoder Beginner Contest 040]C - 柱柱柱柱柱

--- abc040/abc040_c.cpp
+++ abc040/abc040_c.cpp
@@ -1,57 +1,59 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 //typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+//const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 
 int main() {
     int n;
     cin >> n;
-    vector<ll> pillars(n);
-    rep(i, n) cin >> pillars[i];
-    vector<ll> dp(n, 0);
-    dp[1] = abs(pillars[0] - pillars[1]);
-    for (int i = 2; i < n; i++) {
-        ll back2 = pillars[i - 2];
-        ll back1 = pillars[i - 1];
-        ll now = pillars[i];
+    vector<int> polls(n);
+    rep(i, n) cin >> polls[i];
 
-        ll from_back2 = abs(now - back2);
-        ll from_back1 = abs(now - back1);
+    vector<int> dp(n, INT_MAX / 100);
+    dp[0] = 0;
 
-        ll from_back2_total = dp[i - 2] + from_back2;
-        ll from_back1_total = dp[i - 1] + from_back1;
+    auto set = [&](int from, int to) {
+        if (to >= n) return;
+        cmin(dp[to], dp[from] + abs(polls[from] - polls[to]));
+    };
 
-        dp[i] = min(from_back2_total, from_back1_total);
-
+    for (int i = 0; i < n; i++) {
+        set(i, i + 1);
+        set(i, i + 2);
     }
 
     cout << dp.back() << endl;
-
-
 }
