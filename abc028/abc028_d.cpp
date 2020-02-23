9e794b56ec201077f160f016c41081ac788c1083
riku <riku@tanide.net>
2020-02-23 18:31:34 +0900

    【AC】[AtCoder Beginner Contest 028]D - 乱数生成

--- abc028/abc028_d.cpp
+++ abc028/abc028_d.cpp
@@ -1,58 +1,75 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 //typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
 const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
+//const ll MINF = LONG_LONG_MIN;
 //const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
-
+//const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
-    ll n, k;
+
+    double n, k;
     cin >> n >> k;
 
-    ll over_k = n - k + 1;
-    ll under_k = k;
+    double u = (k - 1) / n;
+    double o = (n - k) / n;
+    double m = 1.0 / n;
 
-    ll all = 0;
 
-    for (ll i = 1; i <= n; i++) {
-        if (i < k) {
-            ll now = over_k * 2 - 1;
-            all += now;
-        } else if (i == k) {
-            ll now = over_k * under_k * 2 - 1;
-            all += now;
-        } else {
-            ll now = under_k * 2 - 1;
-            all += now;
-        }
+    double all = 0;
+    {
+        // 1 2 3
+        double now = u * m * o * 6;
+        all += now;
     }
 
-    double ans = (double) all / (double) (n * n * n);
+    {
+        // 2 2 3
+        double now = m * m * o * 3;
+        all += now;
+    }
 
-    printf("%0.20f\n", ans);
+    {
+        // 1 2 2
+        double now = u * m * m * 3;
+        all += now;
+    }
+
+    {
+        // 2 2 2
+        double now = m * m * m;
+        all += now;
+    }
+
+    printf("%.20f\n", all);
+
 }
