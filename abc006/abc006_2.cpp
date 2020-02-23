ff7b63cdc703918b871947121773274bc3e0ed8d
riku <riku@tanide.net>
2020-02-23 13:47:14 +0900

    【RE】[AtCoder Beginner Contest 006]B - トリボナッチ数列

--- abc006/abc006_2.cpp
+++ abc006/abc006_2.cpp
@@ -1,32 +1,39 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
 //typedef pair<ll, int> P;
-typedef pair<ll, ll> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
 //const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
 //const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 //const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 const int mod = 10007;
 
 struct mint {
@@ -84,27 +91,32 @@ struct mint {
         mint res(*this);
         return res /= a;
     }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
 };
 
 int main() {
     int n;
     cin >> n;
 
-    if (n < 3) {
-        cout << 0 << endl;
-        return 0;
+    vector<mint> tribonacci(n + 2);
+    tribonacci[1] = 0;
+    tribonacci[2] = 0;
+    tribonacci[3] = 1;
+    for (int i = 4; i <= n; i++) {
+        tribonacci[i] = tribonacci[i - 1] + tribonacci[i - 2] + tribonacci[i - 3];
     }
 
-    mint b2 = 0;
-    mint b1 = 0;
-    mint b = 1;
-    for (int i = 0; i < n - 3; i++) {
-        mint n = b2 + b1 + b;
-        b2 = b1;
-        b1 = b;
-        b = n;
-    }
-
-    cout << b.x << endl;
-
+    cout << tribonacci[n] <<endl;
 }
