7955790d82d33d4f09f2b3e6b29a02cece1ccdd4
riku <riku@tanide.net>
2020-02-23 22:03:31 +0900

    【AC】[AtCoder Beginner Contest 045]C - Many Formulas

--- abc045/arc061_a.cpp
+++ abc045/arc061_a.cpp
@@ -1,58 +1,79 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-//typedef long long ll;
-typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 //typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
-bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
-
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
-
-int main() {
-
-    string s;
-    cin >> s;
-
-    vector<string> terms;
-
-    for (int i = 0; i < (1 << (s.size() - 1)); i++) {
-        string t = "";
-        bitset<10> b(i);
-        for (int j = 0; j < s.size(); j++) {
-            t = t + s[j];
-            if (b[j]) {
-                terms.push_back(t);
-                t = "";
-            }
+ll calc(string s) {
+    stack<ll> st;
+    st.push(0);
+    for (char c : s) {
+        if (c == '+') {
+            st.push(0);
+        } else {
+            int i = c - '0';
+            st.top() *= 10;
+            st.top() += i;
         }
-        terms.push_back(t);
     }
 
     ll ans = 0;
-    for (string str : terms) {
-        ans += atoll(str.c_str());
+    while (!st.empty()) {
+        ans += st.top();
+        st.pop();
     }
+    return ans;
+}
 
+int main() {
+    string s;
+    cin >> s;
+
+    int d = s.size() - 1;
+    ll ans = 0;
+    for (int i = 0; i < (1 << d); i++) {
+        string t = "";
+
+        for (int j = 0; j < d; j++) {
+            t += s[j];
+            if ((i >> j) & 1) {
+                t += '+';
+            }
+        }
+        t += s.back();
+        ll now = calc(t);
+        ans += now;
+    }
     cout << ans << endl;
-
 }
