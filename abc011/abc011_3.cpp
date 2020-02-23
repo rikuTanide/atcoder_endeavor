2b4622297838d2eb093ec1593a356c89f6c57f27
riku <riku@tanide.net>
2020-02-23 14:07:24 +0900

    【WA】[AtCoder Beginner Contest 011]C - 123引き算

--- abc011/abc011_3.cpp
+++ abc011/abc011_3.cpp
@@ -1,72 +1,60 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //#define sz(x) ll(x.size())
 //typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
 //const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
+//const ll MINF = LONG_LONG_MIN;
 //const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> ng(3);
-    rep(i, 3) cin >> ng[i];
-    vector<vector<bool>> dp(n + 10, vector<bool>(100 + 10, false));
+    int n, n1, n2, n3;
+    cin >> n >> n1 >> n2 >> n3;
 
-    if (find(ng.begin(), ng.end(), n) != ng.end()) {
-        cout << "NO" << endl;
-        return 0;
-    }
-
-    auto ok = [&](int i, int j) {
-        if (find(ng.begin(), ng.end(), i) != ng.end()) {
-            return;
-        }
-        dp[i][j] = true;
+    vector<int> dp(n + 1, INT_MAX);
+    dp[0] = 0;
+    auto set = [&](int to, int value) {
+        if (to == n1 || to == n2 || to == n3) return;
+        if (to > n) return;;
+        cmin(dp[to], value);
     };
 
-    dp[0][0] = true;
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= 100; j++) {
-            if (!dp[i][j]) continue;
-
-            ok(i + 1, j + 1);
-            ok(i + 2, j + 1);
-            ok(i + 3, j + 1);
-
-        }
+    for (int i = 0; i < n; i++) {
+        if (i == n1 || i == n2 || i == n3) continue;
+        set(i + 1, dp[i] + 1);
+        set(i + 2, dp[i] + 1);
+        set(i + 3, dp[i] + 1);
     }
 
-    for (int i = 0; i <= 100; i++) {
-        if (dp[n][i]) {
-            cout << "YES" << endl;
-            return 0;
-        }
-    }
-    cout << "NO" << endl;
+    cout << (dp.back() <= 100 ? "YES" : "NO") << endl;
 
 }
-
-// dp[iマス目に][j回で]行けるか
