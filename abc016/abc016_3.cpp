9f02dd4ef0a8ba45d0086fbc81f608cbf57a2423
riku <riku@tanide.net>
2020-02-23 14:33:53 +0900

    【AC】[AtCoder Beginner Contest 016]C - 友達の友達

--- abc016/abc016_3.cpp
+++ abc016/abc016_3.cpp
@@ -1,65 +1,80 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 //typedef pair<int, int> P;
-typedef pair<ll, ll> P;
+//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = 10e15;
-const ll MINF = -10e10;
-const int INF = INT_MAX / 100;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("~/Downloads/02.txt");
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
-const int mod = 1000000007;
-
-void warshall_floyd(int n, vector<vector<int>> &distances) {
+void warshall_floyd(int n, vector<vector<ll>> &distances) {
     for (int k = 0; k < n; k++) {       // 経由する頂点
         for (int i = 0; i < n; i++) {    // 始点
             for (int j = 0; j < n; j++) {  // 終点
-                distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
+                if (distances[i][j] == INF && (distances[i][k] == INF || distances[k][j] == INF)) {
+                    distances[i][j] = INF;
+                } else {
+                    distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
+                }
             }
         }
     }
 }
 
 int main() {
+
     int n, m;
     cin >> n >> m;
 
-    vector<vector<int >> distances(n, vector<int>(n, INF));
+    vector<vector<ll>> distances(n, vector<ll>(n, INF));
+    rep(i, n) distances[i][i] = 0;
 
-    rep(i, n) {
-        distances[i][i] = 0;
-    }
     rep(i, m) {
         int a, b;
         cin >> a >> b;
+
         a--;
         b--;
         distances[a][b] = 1;
         distances[b][a] = 1;
     }
+
     warshall_floyd(n, distances);
 
     rep(i, n) {
         int ans = 0;
-        rep(j, n) {
-            if (distances[i][j] == 2) {
-                ans++;
-            }
+        for (int j : distances[i]) {
+            if (j == 2) ans++;
         }
         cout << ans << endl;
     }
+
 }
