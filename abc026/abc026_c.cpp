b51ee681ed234047d21c14f6defd92ff9df99933
riku <riku@tanide.net>
2020-02-23 15:36:54 +0900

    【AC】[AtCoder Beginner Contest 026]C - 高橋君の給料

--- abc026/abc026_c.cpp
+++ abc026/abc026_c.cpp
@@ -1,33 +1,40 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-typedef pair<int, int> P;
-//typedef pair<ll, ll> P;
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
 
 struct Triangle {
     int parent;
@@ -67,36 +74,35 @@ public:
     }
 };
 
-
 int main() {
+
     int n;
     cin >> n;
 
     Tree tree(n);
 
-    rep(i, n - 1) {
+    for (int i = 1; i < n; i++) {
         int b;
         cin >> b;
         b--;
-        tree.edge(i + 1, b);
+        tree.edge(b, i);
     }
+
     vector<Triangle> leafs;
     tree.root(0, leafs);
-
-    vector<int> salaries(n, 1);
-    for (int i = 0; i < n; i++) {
-        if (leafs[i].children.empty()) {
-            continue;
+    vector<int> ans(n, 0);
+    for (Triangle &triangle : leafs) {
+        if (triangle.children.empty()) ans[triangle.parent] = 1;
+        else {
+            int ma = 0;
+            int mi = INT_MAX;
+            for (char child : triangle.children) {
+                cmax(ma, ans[child]);
+                cmin(mi, ans[child]);
+            }
+            ans[triangle.parent] = ma + mi + 1;
         }
-
-        int mi = INF;
-        int ma = 0;
-        for (int j : leafs[i].children) {
-            cmin(mi, salaries[j]);
-            cmax(ma, salaries[j]);
-        }
-        salaries[leafs[i].parent] = mi + ma + 1;
     }
+    cout << ans.front() << endl;
 
-    cout << salaries[0] << endl;
 }
