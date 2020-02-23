55a269f6d92e7d91b10188b4723aa3e9d50e9b66
riku <riku@tanide.net>
2020-02-23 16:24:15 +0900

    【AC】[AtCoder Regular Contest 043]A - 点数変換

---
+++ arc043/arc043_a.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+int main() {
+    int n;
+    double a, b;
+    cin >> n >> a >> b;
+
+    vector<double> score(n);
+    rep(i, n) cin >> score[i];
+
+    double ma = *max_element(score.begin(), score.end()),
+            mi = *min_element(score.begin(), score.end());
+
+    if (ma == mi && b != 0) {
+        cout << -1 << endl;
+        ret();
+    }
+    if (b == 0 && ma != mi) {
+        cout << -1 << endl;
+        ret();
+    }
+
+    double p = b / (ma - mi);
+
+    double asum = accumulate(score.begin(), score.end(), 0.0);
+    double apsum = asum * p;
+
+    double q = (a * n) - apsum;
+    q /= n;
+
+    printf("%.20f %.20f\n", p, q);
+
+}
