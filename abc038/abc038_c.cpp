e8a836237a3443aa0f1cf05a227fefb5cf435c70
riku <riku@tanide.net>
2020-02-23 20:04:16 +0900

    【WA】[AtCoder Beginner Contest 038]C - 単調増加

--- abc038/abc038_c.cpp
+++ abc038/abc038_c.cpp
@@ -1,58 +1,63 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 //typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+//const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
-    ll n;
+    int n;
     cin >> n;
-    vector<ll> numbers(n);
+    vector<int> numbers(n);
     rep(i, n) cin >> numbers[i];
 
-    vector<ll> counts;
-    {
-        ll count = 1;
-        for (ll i = 0; i < n - 1; i++) {
-            if (numbers[i] < numbers[i + 1]) {
-                count++;
-            } else {
-                counts.push_back(count);
-                count = 1;
-            }
-        }
-        counts.push_back(count);
-    }
+    int r = 0;
 
-    ll ans = 0;
-    for (ll c : counts) {
-        ll now = c * (c + 1) / 2;
-        ans += now;
+    auto isUp = [&](int l) {
+        if (r == l) return true;
+        if (r == n) return false;
+        return numbers[r - 1] < numbers[r];
+    };
+
+    int ans = 0;
+
+    for (int l = 0; l < n; l++) {
+        while (isUp(l)) {
+            r++;
+        }
+
+        ans += (r - l);
+
     }
     cout << ans << endl;
-
 }
