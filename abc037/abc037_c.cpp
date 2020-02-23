6f3a59db976cf9d271bcd5de2ddcbccbdf2807b6
riku <riku@tanide.net>
2020-02-23 19:29:07 +0900

    【AC】[AtCoder Beginner Contest 037]C - 総和

--- abc037/abc037_c.cpp
+++ abc037/abc037_c.cpp
@@ -1,32 +1,39 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 //typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+//const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 class CumulativeSum {
     vector<ll> numbers;
@@ -64,22 +71,17 @@ public:
 int main() {
     int n, k;
     cin >> n >> k;
-
     CumulativeSum cs(n);
-
     rep(i, n) {
         ll a;
         cin >> a;
         cs.set(i, a);
     }
     cs.calculate();
-    ll ans = 0;
-
-    for (int i = 0; i <= n - k; i++) {
-        ll now = cs.getSectionSum(i, i + k - 1);
-        ans += now;
+    ll sum = 0;
+    for (int i = 0; i + k <= n; i++) {
+        sum += cs.getSectionSum(i, i + k - 1);
     }
-
-    cout << ans << endl;
+    cout << sum << endl;
 
 }
