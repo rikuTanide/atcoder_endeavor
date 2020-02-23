64fc21d19d2dab9b253b4ffb91e5901bb15d6abb
riku <riku@tanide.net>
2020-02-23 19:21:53 +0900

    【AC】[AtCoder Regular Contest 051]B - 互除法

---
+++ arc051/arc051_b.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
+//typedef pair<int, int> P;
+//typedef pair<ll, int> P;
+typedef pair<ll, ll> P;
+//const double INF = 1e10;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+int counter = 0;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    counter++;
+    return gcd(b, a % b);
+}
+
+
+int main() {
+    int k;
+    cin >> k;
+
+    if(k == 1) {
+        cout << "1 1" <<endl;
+        ret();
+    }
+
+    int a = 1 , b = 0;
+
+    for (int i = 0; i <= k; i++) {
+        int tmp = b;
+        b = a;
+        a = a + tmp;
+    }
+
+    cout << a << ' ' << b << endl;
+
+}
