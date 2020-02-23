0b378936e748059b42b6019250bb509b6fddf109
riku <riku@tanide.net>
2020-02-23 18:52:41 +0900

    【AC】[AtCoder Beginner Contest 033]C - 数式の書き換え

--- abc033/abc033_c.cpp
+++ abc033/abc033_c.cpp
@@ -1,60 +1,56 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
 //typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 //typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
 //const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
-
-bool findZero(vector<char> &cs) {
-    for (char c : cs) {
-        if (c == '0') {
-            return true;
-        }
-    }
-    return false;
-}
+//const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
+    stack<string> st;
+    st.push("");
     string s;
     cin >> s;
-    vector<vector<char>> terms(1);
-
-    for (char c :  s) {
-        if (c == '+') {
-            terms.push_back(vector<char>(0));
-        } else {
-            terms.back().push_back(c);
-        }
+    for (char c : s) {
+        if (c == '+') st.push("");
+        else if (c == '*') continue;
+        else st.top() += c;
     }
 
-    ll ans = 0;
-    for (vector<char> &cs : terms) {
-        if (!findZero(cs)) {
-            ans++;
-        }
+    int ans = 0;
+    while (!st.empty()) {
+        string l = st.top();
+        if (find(l.begin(), l.end(), '0') == l.end()) ans++;
+        st.pop();
     }
 
     cout << ans << endl;
