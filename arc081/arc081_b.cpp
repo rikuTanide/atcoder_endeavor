a21ab62cdcf754628e621b568a2797f52477b95d
riku <riku@tanide.net>
2020-02-24 18:37:25 +0900

    【AC】[AtCoder Regular Contest 081]D - Coloring Dominoes

---
+++ arc081/arc081_b.cpp
@@ -0,0 +1,144 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
+
+int check_k(string &s1, string &s2, int i, int j) {
+    if (i == 0 && j == 0) return 3;
+    if (i == 0) {
+        if (s1[0] == s2[0]) return 1;
+        else return 2;
+    }
+    if (j == 0) {
+        if (s1[i] == s1[i - 1]) return 1;
+        else if (s1[i - 1] == s2[i - 1]) return 2;
+        else {
+            if (s1[i] == s2[i]) return 1;
+            else return 3;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    string s1, s2;
+    cin >> s1 >> s2;
+
+
+    mint ans = 1;
+    rep(i, n) {
+        rep(j, 2) {
+            int k = check_k(s1, s2, i, j);
+            ans *= k;
+        }
+    }
+    cout << ans << endl;
+
+}
