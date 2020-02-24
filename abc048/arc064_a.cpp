029014139509c31d3540dc0720d46c2ae32380d6
riku <riku@tanide.net>
2020-02-24 12:02:50 +0900

    【AC】[AtCoder Beginner Contest 048]C - Boxes and Candies

--- abc048/arc064_a.cpp
+++ abc048/arc064_a.cpp
@@ -1,53 +1,73 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-//typedef long long ll;
-typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+typedef long long ll;
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 //typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
-bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
 
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+//const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 
 int main() {
     ll n, x;
     cin >> n >> x;
-    vector<ll> boxes(n);
-    rep(i, n) cin >> boxes[i];
 
+    vector<ll> candies(n);
+    rep(i, n)cin >> candies[i];
     ll ans = 0;
-    if (boxes[0] > x) {
-        ans = boxes[0] - x;
-        boxes[0] -= ans;
+    if (candies.front() > x) {
+        ans += (candies.front() - x);
+        candies.front() = x;
     }
-
-    for (int i = 0; i < n - 1; i++) {
-        if (boxes[i] + boxes[i + 1] > x) {
-            ll now = boxes[i] + boxes[i + 1] - x;
-            ans += now;
-            boxes[i + 1] -= now;
+    for (int i = 1; i < n; i++) {
+        ll c = candies[i] + candies[i - 1];
+        if (c > x) {
+            ll d = c - x;
+            ans += d;
+            candies[i] -= d;
         }
     }
     cout << ans << endl;
-
 }
