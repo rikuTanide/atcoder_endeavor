c426e4e309115606927a6d287dc3d1b50bd7783d
riku <riku@tanide.net>
2020-02-24 13:04:35 +0900

    【AC】[AtCoder Regular Contest 067]D - Walk and Teleport

---
+++ arc067/arc067_b.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+int main() {
+    int n;
+    ll a, b;
+    cin >> n >> a >> b;
+
+    vector<ll> place(n);
+    rep(i, n) cin >> place[i];
+
+    ll ans = 0;
+    for (int i = 0; i < n - 1; i++) {
+        ll walk = (place[i + 1] - place[i]) * a;
+        ll teleport = b;
+        ll now = min(walk, teleport);
+        ans += now;
+    }
+    cout << ans << endl;
+}
