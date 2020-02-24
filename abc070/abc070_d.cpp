e11753c74a401a7afa08560f049a9bffe0cc9be2
riku <riku@tanide.net>
2020-02-24 16:48:37 +0900

    【AC】[AtCoder Beginner Contest 070]D - Transit Tree Path

--- abc070/abc070_d.cpp
+++ abc070/abc070_d.cpp
@@ -1,89 +1,121 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
-
-#include <assert.h>    // LON
-#include <math.h>    // sqrt()
-
-
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+const ll INF = 1e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-//typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 struct Edge {
     ll to, cost;
 };
 
-vector<ll> distances;
-ll vector_count;
-vector<vector<Edge>> edges;
 
-void dijkstra(int k) {
-    distances[k] = 0;  // 始点sへの最短距離は0
+class Dijkstra {
+    ll vector_count;
 
-    priority_queue<P, vector<P>, greater<P>> que;  // 距離が小さい順に取り出せるようgreater<P>を指定
-    que.push(P(0, k));
-    while (!que.empty()) {
-        P p = que.top();
-        que.pop();
-        int v = p.second;  // 更新した頂点の中で距離が最小の頂点v
-        if (distances[v] < p.first) {
-            continue;
-        }
-        for (auto e : edges[v]) {  // 頂点vから出る辺eを走査
-            if (distances[e.to] > distances[v] + e.cost) {
-                distances[e.to] = distances[v] + e.cost;
-                que.push(P(distances[e.to], e.to));
+    vector<ll> distances;
+    vector<vector<Edge>> edges;
+
+public:
+    Dijkstra(ll n) {
+        vector_count = n;
+        distances.resize(n, INF);
+        edges.resize(n);
+    }
+
+    void insertTowWay(ll from, ll to, ll cost) {
+        edges[from].push_back({to, cost});
+        edges[to].push_back({from, cost});
+    }
+
+    void insert(ll from, ll to, ll cost) {
+        edges[from].push_back({to, cost});
+    }
+
+    void dijkstra(ll k) {
+        distances[k] = 0;  // 始点sへの最短距離は0
+
+        priority_queue<P, vector<P>, greater<P>> que;  // 距離が小さい順に取り出せるようgreater<P>を指定
+        que.push(P(0, k));
+        while (!que.empty()) {
+            P p = que.top();
+            que.pop();
+            int v = p.second;  // 更新した頂点の中で距離が最小の頂点v
+            if (distances[v] < p.first) {
+                continue;
+            }
+            for (auto e : edges[v]) {  // 頂点vから出る辺eを走査
+                if (distances[e.to] > distances[v] + e.cost) {
+                    distances[e.to] = distances[v] + e.cost;
+                    que.push(P(distances[e.to], e.to));
+                }
             }
         }
     }
-}
+
+    ll distance(ll to) {
+        return distances[to];
+    }
+
+};
 
 int main() {
-    cin >> vector_count;
-    edges.resize(vector_count);
-    distances.resize(vector_count, INF);
+    int n;
+    cin >> n;
 
-    rep(i, vector_count - 1) {
-        ll a, b, c;
+    Dijkstra dijkstra(n);
+
+    rep(i, n - 1) {
+        int a, b, c;
         cin >> a >> b >> c;
         a--;
         b--;
-        edges[b].push_back({a, c});
-        edges[a].push_back({b, c});
+        dijkstra.insertTowWay(a, b, c);
     }
 
-    ll q, k;
+    int q, k;
     cin >> q >> k;
     k--;
-
-    dijkstra(k);
+    dijkstra.dijkstra(k);
 
     rep(i, q) {
-        ll x, y;
+        int x, y;
         cin >> x >> y;
         x--;
         y--;
-        cout << distances[x] + distances[y] << endl;
+        cout << dijkstra.distance(x) + dijkstra.distance(y) << endl;
     }
 
 }
