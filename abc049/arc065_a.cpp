4114e85207d1dccdb3cb079154cb830104c567b4
riku <riku@tanide.net>
2020-02-24 12:12:12 +0900

    【AC】[AtCoder Beginner Contest 049]C - Daydream

--- abc049/arc065_a.cpp
+++ abc049/arc065_a.cpp
@@ -1,50 +1,77 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+//typedef unsigned long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define sz(x) ll(x.size())
 //typedef pair<int, int> P;
+//typedef pair<ll, int> P;
 typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX;
+//const ll INF = LONG_LONG_MAX / 100;
+//const ll INF = (1ll << 31) - 1;
+const ll INF = 1e15;
+//const ll MINF = LONG_LONG_MIN;
+//const int INF = INT_MAX / 10;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+//typedef pair<int, int> P;
+//typedef pair<double, double> P;
+#define ret() return 0;
 
-bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
 
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+//const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
+
+    vector<string> term = {"dream", "dreamer", "erase", "eraser",};
 
     string s;
     cin >> s;
     vector<bool> dp(s.size() + 1, false);
-    vector<string> word = {"dream", "dreamer", "erase", "eraser",};
     dp[0] = true;
 
     for (int i = 0; i < s.size(); i++) {
         if (!dp[i]) continue;
-        for (string w : word) {
-            string sub_str = s.substr(i, w.size());
-            if (sub_str == w) {
-                dp[i + w.size()] = true;
+        for (string t : term) {
+            bool b = [&] {
+                for (int j = 0; j < t.size(); j++) {
+                    if (s[i + j] != t[j]) return false;
+                }
+                return true;
+            }();
+            if (b) {
+                dp[i + t.size()] = true;
             }
         }
     }
 
     cout << (dp.back() ? "YES" : "NO") << endl;
-
 }
