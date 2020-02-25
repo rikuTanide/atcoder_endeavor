0bec976f1381fc0511119a01cef2877e28b96e26
riku <riku@tanide.net>
2020-02-25 21:00:20 +0900

    【AC】[AtCoder Beginner Contest 076]C - Dubious Document 2

--- abc076/abc076_c.cpp
+++ abc076/abc076_c.cpp
@@ -1,81 +1,83 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = 10e15;
-const ll MINF = -10e10;
-const int INF = INT_MAX;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 1e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-//typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-bool check(string s, string t, int i) {
+bool is_match(string &s, string &t, int i) {
     for (int j = 0; j < t.size(); j++) {
-        char a = s[i + j];
-        char b = t[j];
-        if (a == '?') continue;
-        if (a == b) continue;
+        if (s[i + j] == '?') continue;
+        if (s[i + j] == t[j]) continue;
         return false;
     }
     return true;
-
 }
 
-string merge(string s, string t, int i) {
-    return s.substr(0, i) + t + s.substr(i + t.size());
-}
-
-string replace(string s) {
-    for (int j = 0; j < s.size(); j++) {
-        if (s[j] == '?') {
-            s[j] = 'a';
-        }
+string replace(string &s, string &t, int i) {
+    string u;
+    for (int j = 0; j < i; j++) {
+        if (s[j] == '?') u.push_back('a');
+        else u.push_back(s[j]);
     }
-    return s;
+    for (int j = 0; j < t.size(); j++) {
+        u.push_back(t[j]);
+    }
+    for (int j = i + t.size(); j < s.size(); j++) {
+        if (s[j] == '?') u.push_back('a');
+        else u.push_back(s[j]);
+    }
+    return u;
 }
 
 int main() {
-    string s, t;
-    cin >> s >> t;
+    string _s, t;
+    cin >> _s >> t;
 
     vector<string> candidate;
 
-    if (s.size() < t.size()) {
-        cout << "UNRESTORABLE" << endl;
-        return 0;
+    for (int i = 0; i + t.size() <= _s.size(); i++) {
+        bool b = is_match(_s, t, i);
+        if (!b) continue;
+        string s = replace(_s, t, i);
+        candidate.push_back(s);
     }
-
-    for (int i = 0; i <= s.size() - t.size(); i++) {
-        bool b = check(s, t, i);
-        if (b) {
-            string n = merge(s, t, i);
-            string now = replace(n);
-            candidate.push_back(now);
-        }
-    }
-
     if (candidate.empty()) {
         cout << "UNRESTORABLE" << endl;
-        return 0;
+        ret();
     }
-
-    string ans = *min_element(candidate.begin(), candidate.end());
-
-    cout << ans << endl;
-
+    sort(candidate.begin(), candidate.end());
+    cout << candidate.front() << endl;
 }
