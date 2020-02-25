d6be6adcefbf07aca6fc97c2feb9280f8a8cb4d3
riku <riku@tanide.net>
2020-02-25 22:03:02 +0900

    【AC】[AtCoder Beginner Contest 080]C - Shopping Street

--- abc080/abc080_c.cpp
+++ abc080/abc080_c.cpp
@@ -1,79 +1,65 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = 10e15;
-const ll MINF = -10e10;
-const int INF = INT_MAX;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 1e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-//typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
-typedef pair<int, int> P;
-
-int wa(ll n) {
-    ll ans = 0;
-    while (n > 0) {
-        ans += (n % 10);
-        n /= 10;
-    }
-    return ans;
-}
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
 int main() {
-
-    ll n;
+    int n;
     cin >> n;
-
-    vector<vector<bool >> calendar(10, vector<bool>(n));// calendar[時間帯][お店]
-    rep(i, n) {
-        rep(t, 10) {
-            int c;
-            cin >> c;
-            calendar[t][i] = c == 1;
+    vector<vector<bool>> sell(n, vector<bool>(10));
+    rep(i, n) rep(j, 10) {
+            int k;
+            cin >> k;
+            sell[i][j] = k == 1;
         }
-    }
-    vector<vector<ll >> profit(11, vector<ll>(n));// calendar[マッチ数][お店]
-    rep(i, n) {
-        rep(t, 11) {
-            ll p;
-            cin >> p;
-            profit[t][i] = p;
-        }
-    }
 
-    ll ans = MINF;
-
-    for (int i = 1; i < 1 << 10; i++) {
-        ll sum = 0;
-        for (ll s = 0; s < n; s++) {
-            ll match = 0;
-            for (ll t = 0; t < 10; t++) {
-                if (((i >> t) & 1) == 1) {
-                    if (calendar[t][s]) {
-                        match++;
-                    }
-                }
+    vector<vector<ll>> benefit(n, vector<ll>(11));
+    rep(i, n) rep(j, 11) cin >> benefit[i][j];
+    ll ans = -INF;
+    for (ll i = 1; i < (1 << 10); i++) {
+        ll now = 0;
+        for (ll k = 0; k < n; k++) {
+            ll m = 0;
+            for (ll l = 0; l < 10; l++) {
+                if ((i >> l) & 1) if (sell[k][l]) m++;
             }
-            ll now = profit[match][s];
-            sum += now;
+            now += benefit[k][m];
         }
-        cmax(ans, sum);
-
+        cmax(ans, now);
     }
     cout << ans << endl;
 }
