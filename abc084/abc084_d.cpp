fd75b85adaa517ed87d6b7e337c1cc54e5a3decb
riku <riku@tanide.net>
2020-02-25 22:34:09 +0900

    【AC】[AtCoder Beginner Contest 084]D - 2017-like Number

--- abc084/abc084_d.cpp
+++ abc084/abc084_d.cpp
@@ -1,65 +1,101 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = 10e15;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+const ll INF = 1e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-const int INF = 100000 + 1000;
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
 
-map<int, bool> factors;
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
 
-bool isFactor(int n) {
-    if (n == 1) return false;
-    if (n == 2) return true;
-    if (factors.find(n) != factors.end()) return factors[n];
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
 
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            factors[n] = false;
-            return false;
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return 0;
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void calculate() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
         }
     }
-    factors[n] = true;
-    return true;
-}
+
+};
+
 
 int main() {
-    vector<ll> like_2017_sum(INF, 0);
-    for (int i = 3; i <= INF; i++) {
-        if (isFactor(i) && isFactor((i + 1) / 2)) {
-            like_2017_sum[i] = like_2017_sum[i - 1] + 1;
-        } else {
-            like_2017_sum[i] = like_2017_sum[i - 1];
+    vector<bool> fact(100001, true);
+    fact[0] = false;
+    fact[1] = false;
+    for (int i = 2; i <= 100000; i++) {
+        if (!fact[i]) continue;
+        for (int j = 2; i * j <= 100000; j++) {
+            fact[i * j] = false;
         }
     }
+
+    auto is_like2017 = [&](ll i) {
+        return fact[i] && fact[(i + 1) / 2];
+    };
+
+    CumulativeSum cs(100001);
+    rep(i, 100001) if (is_like2017(i)) cs.set(i, 1);
+    cs.calculate();
 
     int q;
     cin >> q;
     rep(i, q) {
         int l, r;
         cin >> l >> r;
-
-        cout << (like_2017_sum[r] - like_2017_sum[l - 1]) << endl;
+        cout << cs.getSectionSum(l, r) << endl;
     }
+
 
 }
