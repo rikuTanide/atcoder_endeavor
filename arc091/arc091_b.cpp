8dfb1555b90b82bdca630ba1a30eb37d3b00d09b
riku <riku@tanide.net>
2020-02-26 19:16:23 +0900

    【WA】[AtCoder Regular Contest 091]D - Remainder Reminder

---
+++ arc091/arc091_b.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    int ans = 0;
+    for (int b = 1; b <= n; b++) {
+        int p = n / b;
+        int r = n % b;
+        int ac = p * max(0, b - k);
+        ac += max(0, r - k + 1);
+        if(0 % b >= k ) ac --;
+        ans += ac;
+
+
+    }
+
+    cout << ans << endl;
+}
