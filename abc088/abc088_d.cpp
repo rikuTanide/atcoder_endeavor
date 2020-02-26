20400c1ca591cbfefe2a57d98dea3754427422ea
riku <riku@tanide.net>
2020-02-26 18:31:40 +0900

    【TLE】[AtCoder Beginner Contest 088]D - Grid Repainting

--- abc088/abc088_d.cpp
+++ abc088/abc088_d.cpp
@@ -1,82 +1,82 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = 10e15;
-const ll MINF = -10e10;
-const int INF = INT_MAX;
+const ll INF = 1e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-
-void moveAll(vector<vector<char>> &map, vector<vector<int>> &dijkstra, int y, int x, int h, int w);
-
-void
-on(vector<vector<char>> &map, vector<vector<int>> &dijkstra, int fromY, int fromX, int toY, int toX, int h, int w) {
-    int fromCost = dijkstra[fromY][fromX];
-    int toCost = dijkstra[toY][toX];
-    if (map[toY][toX] == '#') return;
-    if (fromCost + 1 >= toCost) return;
-
-    dijkstra[toY][toX] = fromCost + 1;
-    moveAll(map, dijkstra, toY, toX, h, w);
-
-}
-
-
-void moveAll(vector<vector<char>> &map, vector<vector<int>> &dijkstra, int y, int x, int h, int w) {
-    if (x + 1 < w) {
-        on(map, dijkstra, y, x, y, x + 1, h, w);
-    }
-    if (y + 1 < h) {
-        on(map, dijkstra, y, x, y + 1, x, h, w);
-    }
-    if (x - 1 >= 0) {
-        on(map, dijkstra, y, x, y, x - 1, h, w);
-    }
-    if (y - 1 >= 0) {
-        on(map, dijkstra, y, x, y - 1, x, h, w);
-    }
-
-}
+struct G {
+    int x;
+    int y;
+    int depth;
+};
 
 int main() {
-
     int h, w;
     cin >> h >> w;
-    vector<vector<char>> map(h, vector<char>(w));
-    rep(y, h)rep(x, w) cin >> map[y][x];
-    vector<vector<int>> dijkstra(h, vector<int>(w, INF));
-    dijkstra[0][0] = 1;
-    moveAll(map, dijkstra, 0, 0, h, w);
 
-    int start_white = 0;
-    rep(y, h)rep(x, w) if (map[y][x] == '.') start_white++;
+    vector<vector<char>> grid(h, vector<char>(w, ' '));
+    rep(y, h) rep(x, w) cin >> grid[y][x];
+    int white_count = 0;
+    rep(y, h) rep(x, w) if (grid[y][x] == '.') white_count++;
 
-    int dijkstra_difference = dijkstra[h - 1][w - 1];
+    queue<G> q;
+    q.push({0, 0, 1});
 
-    if (dijkstra_difference == INF) {
-        cout << -1 << endl;
-        return 0;
+    auto can = [&](int x, int y) {
+        if (x == -1 || x == w) return false;
+        if (y == -1 || y == h) return false;
+        if (grid[y][x] == '#') return false;
+        return true;
+    };
+
+    while (!q.empty()) {
+        G g = q.front();
+        q.pop();
+        if (g.x == w - 1 && g.y == h - 1) {
+            cout << white_count - g.depth << endl;
+            ret();
+        }
+        grid[g.y][g.x] = '#';
+
+        if (can(g.x + 1, g.y)) q.push({g.x + 1, g.y, g.depth + 1});
+        if (can(g.x - 1, g.y)) q.push({g.x - 1, g.y, g.depth + 1});
+        if (can(g.x, g.y + 1)) q.push({g.x, g.y + 1, g.depth + 1});
+        if (can(g.x, g.y - 1)) q.push({g.x, g.y - 1, g.depth + 1});
     }
 
-    int ans = start_white - dijkstra_difference;
+    cout << -1 << endl;
 
-    cout << ans << endl;
 }
