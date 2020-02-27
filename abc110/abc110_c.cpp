22c546e806024eca4551afa773aed44ec7e41067
riku <riku@tanide.net>
2020-02-27 21:08:31 +0900

    【WA】[AtCoder Beginner Contest 110]C - String Transformation

--- abc110/abc110_c.cpp
+++ abc110/abc110_c.cpp
@@ -1,60 +1,64 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e10;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-const int mod = 1000000007;
-//ifstream myfile("C:\\Users\\riku\\Downloads\\01.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
-
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
 int main() {
     string s, t;
-
     cin >> s >> t;
+    vector<char> transformation(26);
+    rep(i, 26) transformation[i] = 'a' + i;
 
-    vector<char> q(26, 27);
-    vector<char> r(26, 27);
+    rep(i, s.size()) {
+        char from = s[i];
+        char to = t[i];
 
-    for (int i = 0; i < s.size(); i++) {
-        char sc = s[i] - 'a';
-        char tc = t[i] - 'a';
-
-
-
-        if (q[sc] == 27) {
-            q[sc] = tc;
-        } else {
-            if (tc != q[sc]) {
-                cout << "No" << endl;
-                return 0;
-            }
-        }
-
-
-        if (r[tc] == 27) {
-            r[tc] = sc;
-        } else {
-            if (sc != r[tc]) {
-                cout << "No" << endl;
-                return 0;
-            }
-        }
+        transformation[from - 'a'] = to;
+//        transformation[to - 'a'] = from;
     }
 
+    rep(i, s.size()) {
+        char from = s[i];
+        char to = transformation[from - 'a'];
+        if (t[i] == to) continue;
+        cout << "No" << endl;
+        ret();
+    }
 
     cout << "Yes" << endl;
+
 }
