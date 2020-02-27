c8b28b86e7b93ac2904b93f7794e2e67624ede0f
riku <riku@tanide.net>
2020-02-27 20:36:24 +0900

    【AC】[AtCoder Beginner Contest 109]D - Make Them Even

--- abc109/abc109_d.cpp
+++ abc109/abc109_d.cpp
@@ -1,77 +1,87 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e10;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-const int mod = 1000000007;
-//ifstream myfile("C:\\Users\\riku\\Downloads\\01.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-int gcd(ll x, ll y) {
-    if (x < y) swap(x, y);
-    if (y == 0) {
-        return x;
-    }
-    return gcd(y, x % y);
-}
+struct Move {
+    int x1, y1, x2, y2;
+};
+
+struct Grid {
+    int x, y, a;
+};
 
 int main() {
     int h, w;
     cin >> h >> w;
 
-    vector<vector<int>> board(w, vector<int>(h, -1));
-
-    rep(y, h) rep(x, w) cin >> board[x][y];
-
-    vector<vector<int>> ans;
-
-    int x = 0, y = 0;
-    for (int i = 0; i < (h * w) - 1; i++) {
-
-        int next_x;
-        int next_y;
-
-        if (i % w == (w - 1)) {
-            // 下
-            next_x = x;
-            next_y = y + 1;
-        } else if (i / w % 2 == 0) {
-            // 右
-            next_x = x + 1;
-            next_y = y;
-        } else {
-            // 左
-            next_x = x - 1;
-            next_y = y;
-        }
-//        printf("(%d, %d) (%d, %d)\n", x, y, next_x, next_y);
-
-        if (board[x][y] % 2 == 1) {
-            board[next_x][next_y]++;
-            vector<int> now = {x + 1, y + 1, next_x + 1, next_y + 1};
-            ans.push_back(now);
-        }
-
-        x = next_x;
-        y = next_y;
-
+    vector<Grid> grid(h * w);
+    for (int i = 0; i < h * w; i++) {
+        int a;
+        cin >> a;
+        grid[i].x = i % w;
+        grid[i].y = i / w;
+        grid[i].a = a;
     }
-    cout << ans.size() << endl;
-    for (auto a : ans) {
-        printf("%d %d %d %d\n", a[1], a[0], a[3], a[2]);
+
+    for (int y = 0; y < h; y++) {
+        if (y % 2 == 0) continue;
+        reverse(grid.begin() + (w * y), grid.begin() + (w * (y + 1)));
+    }
+
+
+    vector<Move> move;
+    for (int i = 0; i < h * w - 1; i++) {
+        if (grid[i].a % 2 == 0) continue;
+        grid[i].a--;
+        grid[i + 1].a++;
+
+        Move m;
+        m.x1 = grid[i].x;
+        m.y1 = grid[i].y;
+        m.x2 = grid[i + 1].x;
+        m.y2 = grid[i + 1].y;
+        move.push_back(m);
+    }
+
+    cout << move.size() << endl;
+
+    for (Move m: move) {
+        printf("%d %d %d %d\n", m.y1 + 1, m.x1 + 1, m.y2 + 1, m.x2 + 1);
     }
 
 }
