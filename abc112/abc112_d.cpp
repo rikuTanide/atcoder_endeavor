840e187066563c5c65f46d095d7fbd788638fb20
riku <riku@tanide.net>
2020-02-27 22:18:37 +0900

    【AC】[AtCoder Beginner Contest 112]D - Partition

--- abc112/abc112_d.cpp
+++ abc112/abc112_d.cpp
@@ -1,46 +1,58 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e10;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
-typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-const int mod = 1000000007;
-//ifstream myfile("C:\\Users\\riku\\Downloads\\01.txt");
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
 
 int main() {
-
     ll n, m;
     cin >> n >> m;
+    ll ans = 1;
+    for (ll i = 1; i * i <= m; i++) {
+        if (m % i != 0) continue;
+        ll j = m / i;
+        if (j >= n) {
+            cmax(ans, i);
 
-    ll b = m / n;
-    ll max = sqrt(m) + 1;
-
-    vector<ll> facs;
-    for (ll i = max; i >= 1; i--) {
-        if (m % i == 0) {
-            facs.push_back(i);
-            facs.push_back(m / i);
+        }
+        if (i >= n) {
+            cmax(ans, j);
         }
     }
-
-    sort(facs.begin(), facs.end());
-
-    ll ans = *(--upper_bound(facs.begin(), facs.end(), b));
-
     cout << ans << endl;
-
 
 }
