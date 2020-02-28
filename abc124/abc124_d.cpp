e38b84bba213d5e9c6686b025c885a2b57c3b66a
riku <riku@tanide.net>
2020-02-28 19:41:55 +0900

    【AC】[AtCoder Beginner Contest 124]D - Handstand

--- abc124/abc124_d.cpp
+++ abc124/abc124_d.cpp
@@ -1,90 +1,117 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-//const int INF = INT_MAX  ;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-typedef pair<int, char> PC;
+struct V {
+    char v;
+    int seq;
+};
+
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return 0;
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void calculate() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
 
 int main() {
+
     int n, k;
     cin >> n >> k;
+
+    vector<V> vs;
     string s;
     cin >> s;
 
-    vector<PC> seq_counts;
-    char prev = s[0];
-    int seq = 1;
-    for (int i = 1; i < s.size(); i++) {
-        char now = s[i];
-        if (now != prev) {
-            seq_counts.emplace_back(seq, prev);
-            prev = now;
-            seq = 1;
-        } else {
-            seq++;
-        }
-    }
-    seq_counts.emplace_back(seq, prev);
-
-    queue<PC> left;
-    queue<PC> center;
-    queue<PC> right;
-
-    for (auto p : seq_counts) {
-        right.push(p);
+    for (char c : s) {
+        if (vs.empty()) vs.push_back({c, 1});
+        else if (vs.back().v == c) vs.back().seq++;
+        else vs.push_back({c, 1});
     }
 
-    int count = 0;
+    CumulativeSum cs(vs.size());
+    rep(i, vs.size()) {
+        cs.set(i, vs[i].seq);
+    }
+    cs.calculate();
+
+
     int ans = 0;
-    int c_count = 0; // 直立カウント
-    while (!right.empty()) {
-        auto next = right.front(); // 次に入れたい
-        if (next.second == '1') {
-            right.pop();
-            center.push(next);
-            count += next.first;
-            ans = max(ans, count);
-            continue;
+    for (int i = 0; i < vs.size(); i++) {
+        int seq = vs[i].seq;
+        char v = vs[i].v;
+
+        if (v == '0') {
+            int ma = min(i + k * 2 - 1, (int) vs.size() - 1);
+            int now = cs.getSectionSum(i, ma);
+//            printf("%d %d\n", i, ma - i);
+            cmax(ans, now);
+        } else {
+            int ma = min(i + k * 2, (int) vs.size() - 1);
+            int now = cs.getSectionSum(i, ma);
+//            printf("%d %d\n", i, ma - i);
+            cmax(ans, now);
         }
-        // 次に入れたいのは直立
-
-        if (c_count == k) {
-            // 一番左だけ取り出せばいい時と、二回連続で取り出さないといけないと気があるが、
-            // whileで上手くいく
-            auto pop = center.front();
-            if (pop.second == '1') {
-                center.pop();
-                count -= pop.first;
-                continue;
-            } else {
-                center.pop();
-                count -= pop.first;
-                c_count--;
-                continue;
-            }
-        }
-
-        right.pop();
-        center.push(next);
-        count += next.first;
-        ans = max(ans, count);
-        c_count++;
-
     }
-
     cout << ans << endl;
-
 }
-
