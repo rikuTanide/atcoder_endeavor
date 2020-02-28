2e54f3d6a105680376fed45e129ef7625b4d3989
riku <riku@tanide.net>
2020-02-28 22:02:41 +0900

    【AC】[diverta 2019 Programming Contest]D - DivRem Number

---
+++ diverta2019/diverta2019_d.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+bool check(ll n, ll b) {
+    if (b == 0) return false;
+    return n / b == n % b;
+}
+
+int main() {
+
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    for (ll x = 1; x * x <= n; x++) {
+        if (n % x == 0) {
+            if (check(n, x - 1)) ans += x - 1;
+            if (check(n, (n / x) - 1)) ans += (n / x) - 1;
+        }
+    }
+    cout << ans << endl;
+
+}
