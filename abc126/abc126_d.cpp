6c0e6326c8287cfcf38335fcbc6782e599ba95d5
riku <riku@tanide.net>
2020-02-28 22:14:27 +0900

    【AC】[AtCoder Beginner Contest 126]D - Even Relation

--- abc126/abc126_d.cpp
+++ abc126/abc126_d.cpp
@@ -1,54 +1,129 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 1001001001;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-const int mod = 1e9 + 7;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
 
-int n;
-vector<vector<P>> tos;
-vector<int> rFroms;
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
-void bfs(int target, int d) {
-    if(rFroms[target] != -1) return;
-    rFroms[target] = d;
-    auto to = tos[target];
-    for (auto t : to) {
-        bfs(t.first, (d + t.second) % 2);
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+void warshall_floyd(int n, vector<vector<ll>> &distances) {
+    for (int k = 0; k < n; k++) {       // 経由する頂点
+        for (int i = 0; i < n; i++) {    // 始点
+            for (int j = 0; j < n; j++) {  // 終点
+                if (distances[i][j] == INF && (distances[i][k] == INF || distances[k][j] == INF)) {
+                    distances[i][j] = INF;
+                } else {
+                    distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
+                }
+            }
+        }
     }
 }
 
+struct Edge {
+    ll to, cost;
+};
+
+
+class Dijkstra {
+    ll vector_count;
+
+    vector<ll> distances;
+    vector<vector<Edge>> edges;
+
+public:
+    Dijkstra(ll n) {
+        vector_count = n;
+        distances.resize(n, INF);
+        edges.resize(n);
+    }
+
+    void insertTowWay(ll from, ll to, ll cost) {
+        edges[from].push_back({to, cost});
+        edges[to].push_back({from, cost});
+    }
+
+    void insert(ll from, ll to, ll cost) {
+        edges[from].push_back({to, cost});
+    }
+
+    void dijkstra(ll k) {
+        distances[k] = 0;  // 始点sへの最短距離は0
+
+        priority_queue<P, vector<P>, greater<P>> que;  // 距離が小さい順に取り出せるようgreater<P>を指定
+        que.push(P(0, k));
+        while (!que.empty()) {
+            P p = que.top();
+            que.pop();
+            int v = p.second;  // 更新した頂点の中で距離が最小の頂点v
+            if (distances[v] < p.first) {
+                continue;
+            }
+            for (auto e : edges[v]) {  // 頂点vから出る辺eを走査
+                if (distances[e.to] > distances[v] + e.cost) {
+                    distances[e.to] = distances[v] + e.cost;
+                    que.push(P(distances[e.to], e.to));
+                }
+            }
+        }
+    }
+
+    ll distance(ll to) {
+        return distances[to];
+    }
+
+};
 
 int main() {
+
+    int n;
     cin >> n;
 
-    tos = vector<vector<P>>(n);
-    rFroms = vector<int>(n, -1);
+    Dijkstra dijkstra(n);
 
     rep(i, n - 1) {
-        int from, to, distance;
-        cin >> from >> to >> distance;
-        from--;
-        to--;
-        tos[from].emplace_back(to, distance % 2);
-        tos[to].emplace_back(from, distance % 2);
+        int a, b;
+        ll d;
+        cin >> a >> b >> d;
+        a--;
+        b--;
+        dijkstra.insertTowWay(a, b, d);
     }
+    dijkstra.dijkstra(0);
 
-
-    bfs(0, 0);
-
-    for (auto r : rFroms) {
-        cout << r << endl;
+    rep(i, n) {
+        cout << dijkstra.distance(i) % 2 << endl;
     }
 
 }
-
