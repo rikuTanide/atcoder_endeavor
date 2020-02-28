ca85250386440a46153ca465e1cc98f3b5bf97d0
riku <riku@tanide.net>
2020-02-28 17:08:17 +0900

    【WA】[AtCoder Beginner Contest 115]D - Christmas

--- abc115/abc115_d.cpp
+++ abc115/abc115_d.cpp
@@ -1,68 +1,108 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e10;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
-typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-const int mod = 1000000007;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-vector<ll> sou_count(53, 1);
-vector<ll> pate_count(53, 1);
-
-
-ll rec(ll n, ll x) {
-    if(n == 0) {
-        return 1;
-    }
-
-    // X = 1 のとき: f(N, X) = 0 (一番下のバンだけ)
-    if (x == 1) {
-        return 0;
-    }
-    // 1 < X ≤ 1 + ai−1 のとき: f(N, X) = f(N − 1, X − 1)
-    if (1 < x && x <= 1 + sou_count[n - 1]) {
-        return rec(n - 1, x - 1);
-    }
-    // . X = 2 + ai−1 のとき: f(N, X) = pi−1 + 1
-    if (x == 2 + sou_count[n - 1]) {
-        return pate_count[n - 1] + 1;
-    }
-    // 2 + ai−1 < X ≤ 2 + 2ai−1 のとき: f(N, X) = pi−1 + 1 + f(N − 1, X − 2 − ai−1)
-    if (2 + sou_count[n - 1] < x && x <= 2 + 2 * sou_count[n - 1]) {
-        return pate_count[n - 1] + 1 + rec(n - 1, x - 2 - sou_count[n - 1]);
-    }
-    // X = 3 + 2ai−1 のとき: f(N, X) = 2pi−1 + 1
-    return 2 * pate_count[n - 1] + 1;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
 }
 
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+struct Level {
+    ll pate, ban, total;
+};
+
+const Level ban = {0, 1, 1},
+        pate = {1, 0, 1};
+
+vector<Level> searchBound(int depth, vector<Level> &levels, ll n, ll x) {
+    Level l = levels[n - depth];
+    if (l.total < x) return {l};
+    else {
+        Level next = levels[n - depth - 1];
+        // ban, next, pate, next, ban
+
+        vector<Level> res;
+        ll sum = 0;
+        if (x == 0) return res;
+        sum += 1;
+        res.push_back(ban);
+        if (x == sum) return res;
+        if (sum + next.total >= x) {
+            vector<Level> k = searchBound(depth + 1, levels, n, x - sum);
+            for (Level m : k) res.push_back(m);
+            return res;
+        } else {
+            sum += next.total;
+            res.push_back(next);
+        }
+        sum++;
+        res.push_back(pate);
+        if (x == sum) return res;
+        if (sum + next.total >= x) {
+            vector<Level> k = searchBound(depth + 1, levels, n, x - sum);
+            for (Level m : k) res.push_back(m);
+            return res;
+        } else {
+            sum += next.total;
+            res.push_back(next);
+        }
+        sum++;
+        res.push_back(ban);
+        if (x == sum) return res;
+
+        __throw_runtime_error("konai");
+    }
+}
 
 int main() {
-
-
-    for (int i = 0; i < 52; i++) {
-        ll pc = (1ll << (i + 2)) - 1;
-        ll sum = (1ll << (i + 3)) - 3;
-
-        sou_count[i + 1] = sum;
-        pate_count[i + 1] = pc;
-    }
-
     ll n, x;
     cin >> n >> x;
 
-    ll ans = rec(n,x);
+    vector<Level> levels(n + 1);
+    levels[0].ban = 0;
+    levels[0].pate = 1;
+    levels[0].total = 1;
+    for (int i = 1; i <= n; i++) {
+        levels[i].pate = 1 + levels[i - 1].pate + levels[i - 1].pate;
+        levels[i].ban = 2 + levels[i - 1].ban + levels[i - 1].ban;
+        levels[i].total = levels[i].pate + levels[i].ban;
+    }
 
+    vector<Level> res = searchBound(0, levels, n, x);
+    ll ans = 0;
+    for (Level r : res) {
+        ans += r.pate;
+    }
     cout << ans << endl;
 
 }
-
