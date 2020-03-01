e0baa0e5de8391fa2136844c5c5c58025df67eb4
riku <riku@tanide.net>
2020-03-01 11:54:54 +0900

    【WA】[AtCoder Beginner Contest 130]D - Enough Array

--- abc130/abc130_d.cpp
+++ abc130/abc130_d.cpp
@@ -1,40 +1,98 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
-//typedef pair<ll, ll> P;
-const ll INF = 1001001001;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return 0;
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void calculate() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
 
 int main() {
-    ll n, k;
+    int n;
+    ll k;
+
     cin >> n >> k;
 
-    vector<int> a(n);
-    rep(i, n) {
-        cin >> a[i];
-    }
+    vector<int> numbers(n);
+    rep(i, n) cin >> numbers[i];
 
-    ll s = 0;
-    int j = 0;
+    CumulativeSum cs(n);
+    rep(i, n) cs.set(i, numbers[i]);
+    cs.calculate();
+
+    int r = 0;
     ll ans = 0;
-
-    rep(i, n) {
-        while (j < n && s + a[j] < k) {
-            s += a[j];
-            ++j;
+    for (int l = 0; l < n; l++) {
+        while (cs.getSectionSum(l, r + 1) < k && r + 1 < n) {
+            r++;
         }
-        ans += j - i;
-        s -= a[i];
+
+        ll all = n - l;
+        ll exclude = (r + 1) - l;
+
+        ans += (all - exclude);
     }
-
-    ans = (ll) n * (n + 1) / 2 - ans;
-
     cout << ans << endl;
-
-    return 0;
 }
