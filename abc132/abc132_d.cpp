bceab24c2753ef573528319b34c21318d617a5fc
riku <riku@tanide.net>
2020-03-01 12:24:46 +0900

    【WA】[AtCoder Beginner Contest 132]D - Blue and Red Balls

--- abc132/abc132_d.cpp
+++ abc132/abc132_d.cpp
@@ -1,19 +1,45 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
-const ll INF = 1001001001;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
 struct mint {
-    ll x;
-
-    mint(ll x = 0) : x(x % mod) {}
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
 
     mint &operator+=(const mint a) {
         if ((x += a.x) >= mod) x -= mod;
@@ -44,52 +70,75 @@ struct mint {
         mint res(*this);
         return res *= a;
     }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
 };
 
-mint c[4005][4005];
 
-void init() {
-    c[0][0] = 1;
-    for (int i = 0; i <= 4000; i++) {
-        for (int j = 0; j <= i; j++) {
-            c[i + 1][j] += c[i][j];
-            c[i + 1][j + 1] += c[i][j];
-        }
+struct combination {
+    vector<mint> fact, ifact;
+
+    combination(int n) : fact(n + 1), ifact(n + 1) {
+        assert(n < mod);
+        fact[0] = 1;
+        for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
+        ifact[n] = fact[n].inv();
+        for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
     }
-}
 
-mint comb(int n, int k) {
-    return c[n][k];
-}
-
-mint f2(int n, int k) {
-    return comb(n + k - 1, k - 1);
-}
-
-mint f(int n, int k) {
-    if (n < k) {
-        return 0;
+    mint operator()(int n, int k) {
+        if (k < 0 || k > n) return 0;
+        return fact[n] * ifact[k] * ifact[n - k];
     }
-    if (n == 0 && k == 0) return 1;
-    if (k < 1) {
-        return 0;
-    }
-    return f2(n - k, k);
-}
+} combination(10000);
 
 int main() {
-    init();
+    int n, b, r;
+    cin >> n >> b;
+    r = n - b;
 
-    int n, k;
-    cin >> n >> k;
+    for (int i = 1; i <= b; i++) {
+        // 玉-1 C 箱-1
+        mint blue_comb = combination(b - 1, i - 1);
 
-    for (int i = 1; i <= k; i++) {
-        mint blue = f(k, i);
-        mint red = f2(n - k - (i - 1), i + 1);
-        mint ans = blue * red;
-        printf("%lld\n", ans.x);
+        mint red_outer = combination(r - 1, i + 1 - 1);
+        mint red_inner = combination(r - 1, i - 1 - 1);
+        mint red_left = combination(r - 1, i - 1);
+        mint red_right = combination(r - 1, i - 1);
 
+        cout << blue_comb * red_outer + blue_comb * red_inner + blue_comb * red_left + blue_comb * red_right << endl;
     }
-
-
 }
