e4fca7317206e1722aef82b7b171ebd162fce3ad
riku <riku@tanide.net>
2020-03-01 21:36:48 +0900

    【AC】[AtCoder Beginner Contest 157]D - Friend Suggestions

---
+++ abc157/abc157_d.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+
+class UnionFind {
+public:
+    // 親の番号を格納する。親だった場合-size
+    vector<int> parents;
+
+    UnionFind(int n) {
+        parents = vector<int>(n, -1);
+    }
+
+    // aがどのグループに属しているか
+    int root(int a) {
+        if (parents[a] < 0) {
+            return a;
+        }
+        return parents[a] = root(parents[a]);
+    }
+
+    int size(int a) {
+        return -parents[root(a)];
+    }
+
+    // aとbをくっつける
+    bool connect(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        if (ra == rb) {
+            return false;
+        }
+        // 大きいほうにA
+        if (size(ra) < size(rb)) {
+            swap(ra, rb);
+        }
+        parents[ra] += parents[rb];
+        parents[rb] = ra;
+        return true;
+    }
+
+    bool is_union(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        return ra == rb;
+    }
+};
+
+
+int main() {
+
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    vector<vector<int>> friends(n);
+    vector<vector<int>> blocks(n);
+
+    UnionFind uf(n);
+
+    rep(i, m) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        friends[a].push_back(b);
+        friends[b].push_back(a);
+        uf.connect(a, b);
+    }
+
+
+    rep(i, k) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        blocks[a].push_back(b);
+        blocks[b].push_back(a);
+    }
+
+    rep(i, n) {
+        int size = uf.size(i);
+        size--;
+        for (int j : friends[i]) {
+            if (uf.is_union(i, j)) size--;
+        }
+        for (int j : blocks[i]) {
+            if (uf.is_union(i, j)) size--;
+        }
+        cout << size << ' ';
+    }
+
+    cout << endl;
+}
