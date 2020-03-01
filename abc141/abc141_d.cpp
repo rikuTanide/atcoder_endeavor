4be54f12e0cc403c8adf9c2e529ed0edcef9a6d9
riku <riku@tanide.net>
2020-03-01 17:11:52 +0900

    【AC】[AtCoder Beginner Contest 141]D - Powerful Discount Tickets

--- abc141/abc141_d.cpp
+++ abc141/abc141_d.cpp
@@ -1,38 +1,64 @@
 #include <bits/stdc++.h>
+#include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
 typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-const int INF = 1001001001;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
 
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
 int main() {
+    int n, m;
+    cin >> n >> m;
 
-    ll product_count, voucher_count;
-    cin >> product_count>> voucher_count;
-
-    priority_queue<ll> prices;
-
-    rep(i, product_count) {
-        ll price;
-        cin >> price;
-        prices.push(price);
+    priority_queue<ll> shopping;
+    rep(i, n) {
+        ll a;
+        cin >> a;
+        shopping.push(a);
     }
 
-    for(ll x = 0 ; x < voucher_count ; x ++) {
-        auto price = prices.top();
-        prices.pop();
-        prices.push(price / 2);
+    rep(i, m) {
+        ll t = shopping.top();
+        shopping.pop();
+        shopping.push(t / 2);
     }
 
     ll sum = 0;
-    while(!prices.empty()) {
-        sum += prices.top();
-        prices.pop();
+    while (!shopping.empty()) {
+        sum += shopping.top();
+        shopping.pop();
     }
-
     cout << sum << endl;
-
 
 }
