88d68266c5485beb9fef25a6d78fc4f003b62b5d
riku <riku@tanide.net>
2020-03-01 17:06:11 +0900

    【AC】[AtCoder Beginner Contest 140]D - Face Produces Unhappiness

--- abc140/abc140_d.cpp
+++ abc140/abc140_d.cpp
@@ -1,31 +1,62 @@
 #include <bits/stdc++.h>
+#include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
 typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-const int INF = 1001001001;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+bool check(string &s, int i) {
+    if (i == 0 && s[i] == 'L') return false;
+    if (i == s.size() - 1 && s[i] == 'R') return false;
+    if (s[i] == 'R') {
+        return s[i + 1] == 'R';
+    } else {
+        return s[i - 1] == 'L';
+    }
+}
 
 int main() {
-
     int n, k;
     cin >> n >> k;
-
     string s;
-
     cin >> s;
+    int happy = 0;
+    rep(i, s.size()) if (check(s, i)) happy++;
 
-    int ini = 0;
-
-    for (int i = 0; i < n - 1; i++) {
-        if (s[i] == s[i + 1]) {
-            ini++;
-        }
-    }
-
-    int ans = min(ini + 2 * k, n - 1);
-
-
-    cout << ans << endl;
+    happy += k * 2;
+    happy = cmin(happy, n - 1);
+    cout << happy << endl;
 
 }
