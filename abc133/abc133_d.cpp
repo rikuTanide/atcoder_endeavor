10e71bf0e57fc8300b477aa329f4d17da6bf7721
riku <riku@tanide.net>
2020-03-01 14:45:39 +0900

    【AC】[AtCoder Beginner Contest 133]D - Rain Flows into Dams

--- abc133/abc133_d.cpp
+++ abc133/abc133_d.cpp
@@ -1,37 +1,62 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
-const ll INF = 1001001001;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-const int mod = 1000000007;
-
-int main() {
-
-    int n;
-    cin >> n;
-    vector<int> a(n);
-
-    rep(i, n)cin >> a[i];
-    ll x2 = 0;
-    rep(i, n) {
-        if (i % 2) x2 -= a[i];
-        else x2 += a[i];
-    }
-    vector<int> ans(n);
-    ans[0] = x2 / 2;
-    rep(i, n - 1) {
-        ans[i + 1] = a[i] - ans[i];
-    }
-    rep(i, n)ans[i] *= 2;
-    rep(i, n) {
-        cout << ans[i] << ' ';
-    }
-    cout << endl;
-
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
 }
 
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> water(n);
+    rep(i, n) cin >> water[i];
+
+    ll s = 0;
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) s += water[i];
+        else s -= water[i];
+    }
+
+    vector<ll> mountains(n);
+    mountains[0] = s;
+    for (int i = 1; i < n; i++) {
+        ll a = mountains[i - 1];
+        ll w = water[i - 1];
+        mountains[i] = 2 * w - a;
+    }
+
+    for (ll a : mountains) cout << a << endl;
+
+}
