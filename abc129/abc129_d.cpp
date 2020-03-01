8f38761c1d3d4a9f4a0f2290ecc728300d7f9b86
riku <riku@tanide.net>
2020-03-01 11:23:38 +0900

    【AC】[AtCoder Beginner Contest 129]D - Lamp

--- abc129/abc129_d.cpp
+++ abc129/abc129_d.cpp
@@ -1,64 +1,95 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 1001001001;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
 int main() {
     int h, w;
     cin >> h >> w;
 
-    vector<string> s(h);
+    vector<vector<char>> board(h, vector<char>(w));
+    rep(y, h) rep(x, w) cin >> board[y][x];
 
-    rep(i, h) cin >> s[i];
-    vector<vector<int>> cnt(h, vector<int>(w));
+    vector<vector<int>> horizontal(h, vector<int>(w, 0));
+    vector<vector<int>> vertical(h, vector<int>(w, 0));
 
-    rep(i, h) {
-        vector<int> done(w);
-        rep(j, w) {
-            if (s[i][j] == '#') continue;
-            if (done[j]) continue;
-            int l = 0;
-            while (j + l < w) {
-                if (s[i][j + l] == '#')break;
-                l++;
+    for (int y = 0; y < h; y++) {
+        horizontal[y][0] = board[y][0] == '.' ? 1 : 0;
+        for (int x = 1; x < w; x++) {
+            if (board[y][x] == '.') {
+                horizontal[y][x] = horizontal[y][x - 1] + 1;
+            } else {
+                horizontal[y][x] = 0;
             }
-            rep(k, l) {
-                cnt[i][j + k] += l;
-                done[j + k] = 1;
-            }
-
         }
     }
 
-    rep(j, w) {
-        vector<int> done(h);
-        rep(i, h) {
-            if (s[i][j] == '#') continue;
-            if (done[i]) continue;
-            int l = 0;
-            while (i + l < h) {
-                if (s[i + l][j] == '#')break;
-                l++;
+    for (int y = 0; y < h; y++) {
+        for (int x = w - 2; x >= 0; x--) {
+            if (horizontal[y][x] == 0) continue;
+            horizontal[y][x] = max(horizontal[y][x + 1], horizontal[y][x]);
+        }
+    }
+
+    for (int x = 0; x < w; x++) {
+        vertical[0][x] = board[0][x] == '.' ? 1 : 0;
+        for (int y = 1; y < h; y++) {
+            if (board[y][x] == '.') {
+                vertical[y][x] = vertical[y - 1][x] + 1;
+            } else {
+                vertical[y][x] = 0;
             }
-            rep(k, l) {
-                cnt[i + k][j] += l;
-                done[i + k] = 1;
-            }
+        }
+    }
+
+
+    for (int x = 0; x < w; x++) {
+        for (int y = h - 2; y >= 0; y--) {
+            if (vertical[y][x] == 0) continue;
+            vertical[y][x] = max(vertical[y + 1][x], vertical[y][x]);
         }
     }
 
     int ans = 0;
-    rep(i, h)rep(j, w) ans = max(ans, cnt[i][j] - 1);
+    rep(y, h) rep(x, w) {
+            if (board[y][x] == '#') continue;
+            int now = horizontal[y][x] + vertical[y][x] - 1;
+            cmax(ans, now);
+        }
+
     cout << ans << endl;
-
 }
-
