af45fafc1f83778b2ae356310cf9b686d69dad42
riku <riku@tanide.net>
2020-03-04 22:53:01 +0900

    【AC】[AtCoder Beginner Contest 145]D - Knight

--- abc145/abc145_d.cpp
+++ abc145/abc145_d.cpp
@@ -1,18 +1,42 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
+//using namespace boost::multiprecision;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 1001001001;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+typedef pair<double, double> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
 struct mint {
     ll x; // typedef long long ll;
@@ -69,49 +93,70 @@ struct mint {
         mint res(*this);
         return res /= a;
     }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
 };
 
-const int MAX = 999999 + 1;
-ll fac[MAX], finv[MAX], inv[MAX];
 
-// テーブルを作る前処理
-void COMinit() {
-    fac[0] = fac[1] = 1;
-    finv[0] = finv[1] = 1;
-    inv[1] = 1;
-    for (int i = 2; i < MAX; i++) {
-        fac[i] = fac[i - 1] * i % mod;
-        inv[i] = mod - inv[mod % i] * (mod / i) % mod;
-        finv[i] = finv[i - 1] * inv[i] % mod;
-    }
+int calc_j(int x, int y, int i) {
+    // あとj上に行かないといけない
+    int j = y - i;
+    if (j % 2 != 0) return -1;
+    // k 回上に行く
+    int k = j / 2;
+    if ((i * 2) + k != x) return -1;
+    return k;
 }
 
-// 二項係数計算
-ll COM(int n, int k) {
-    if (n < k) return 0;
-    if (n < 0 || k < 0) return 0;
-    return fac[n] * (finv[k] * finv[n - k] % mod) % mod;
+struct combination {
+    vector<mint> fact, ifact;
+
+    combination(int n) : fact(n + 1), ifact(n + 1) {
+        assert(n < mod);
+        fact[0] = 1;
+        for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
+        ifact[n] = fact[n].inv();
+        for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
+    }
+
+    mint operator()(int n, int k) {
+        if (k < 0 || k > n) return 0;
+        return fact[n] * ifact[k] * ifact[n - k];
+    }
+} combination(10000000);
+
+
+mint calc_pattern(int i, int j) {
+    return combination(i + j - 1 + 1, i);
 }
 
 int main() {
     int x, y;
     cin >> x >> y;
 
-    if ((x + y) % 3 != 0) {
-        cout << 0 << endl;
-        return 0;
+    mint ans = 0;
+    for (int i = 0; i <= x; i++) {
+        // 横にi回移動するとして
+
+        // 縦にj回移動しないといけない
+        int j = calc_j(x, y, i);
+        if (j == -1) continue;
+
+        mint k = calc_pattern(i, j);
+        ans += k;
     }
-
-    // __| がA |`が Bとする
-    int b = (2 * y - x) / 3;
-    int a = y - 2 * b;
-
-    assert(2 * a + b == x);
-    assert(2 * b + a == y);
-    COMinit();
-    ll ans = COM(a + b, a);
 
     cout << ans << endl;
 
 }
-
