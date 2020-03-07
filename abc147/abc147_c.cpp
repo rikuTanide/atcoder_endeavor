b996dd8fb16dae6209053de43cd283ac860ec9cc
riku <riku@tanide.net>
2020-03-07 16:34:20 +0900

    【AC】[AtCoder Beginner Contest 147]C - HonestOrUnkind2

--- abc147/abc147_c.cpp
+++ abc147/abc147_c.cpp
@@ -2,43 +2,56 @@
 #include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
-
 #define rep(i, n) for (ll i = 0; i < (n); ++i)
 //#define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<ll, int> P;
-typedef pair<ll, ll> P;
+#define sz(x) ll(x.size())
+//typedef long long ll;
+typedef long long ll;
+//typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
 //const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e15;
+const ll MINF = -10e10;
+//const int INF = INT_MAX / 100;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 
-bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ifstream myfile("~/Downloads/02.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 const int mod = 1000000007;
+const double PI = 3.14159265358979323846;
 
-struct Testimony {
-    int from, to;
+typedef pair<ll, ll> P;
+
+struct Witness {
+    int to;
     bool is_honest;
 };
 
+int check(int i, int n, vector<vector<Witness>> &witness) {
+    for (int j = 0; j < n; j++) {
+        if (((i >> j) & 1) == 0) {
+            continue;
+        }
+        for (Witness &w : witness[j]) {
+            bool h = (i >> w.to) & 1;
+            if (h != w.is_honest) return -1;
+        }
+    }
+    return __builtin_popcount(i);
+}
+
 int main() {
+
     int n;
     cin >> n;
 
-    vector<Testimony> testimonies;
+    vector<vector<Witness>> witness(n);
 
     rep(i, n) {
         int a;
@@ -47,38 +60,16 @@ int main() {
             int x, y;
             cin >> x >> y;
             x--;
-            Testimony testimony{(int)i, x, y == 1};
-            testimonies.push_back(testimony);
+            Witness w{x, y == 1};
+            witness[i].push_back(w);
         }
     }
 
     int ans = 0;
     for (int i = 0; i < (1 << n); i++) {
-        bool b = [&]() {
-            for (Testimony &testimony : testimonies) {
-                int t_from = testimony.from;
-                int t_to = testimony.to;
-                bool test = testimony.is_honest;
-
-                bool from_honesty = (i >> t_from) & 1;
-                bool to_honesty = (i >> t_to) & 1;
-                if (from_honesty) {
-                    if (to_honesty == test) {
-                        continue;
-                    } else {
-                        return false;
-                    }
-                } else {
-                    continue;
-                }
-            }
-            return true;
-        }();
-
-        if (b) {
-            int now = __builtin_popcount(i);
-            cmax(ans, now);
-        }
+        int now = check(i, n, witness);
+        cmax(ans, now);
     }
     cout << ans << endl;
+
 }
