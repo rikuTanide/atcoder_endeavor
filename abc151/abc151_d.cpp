7a1bdbdc249642c01db047c04cf4d1e9cd8d2bd7
riku <riku@tanide.net>
2020-03-07 17:21:27 +0900

    【AC】[AtCoder Beginner Contest 151]D - Maze Master

--- abc151/abc151_d.cpp
+++ abc151/abc151_d.cpp
@@ -2,40 +2,38 @@
 #include <cmath>
 
 using namespace std;
-//typedef long long ll;
-typedef unsigned long long ll;
-
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
 #define sz(x) ll(x.size())
-typedef pair<int, int> P;
+//typedef long long ll;
+typedef long long ll;
+//typedef pair<int, int> P;
 //typedef pair<ll, ll> P;
 //const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e15;
+const ll MINF = -10e10;
+//const int INF = INT_MAX / 100;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ifstream myfile("~/Downloads/02.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 const int mod = 1000000007;
+const double PI = 3.14159265358979323846;
+
+typedef pair<ll, ll> P;
 
 
-// https://qiita.com/okaryo/items/8e6cd73f8a676b7a5d75
-void warshall_floyd(int n, vector<vector<int>> &d) {
+void warshall_floyd(int n, vector<vector<int>> &distances) {
     for (int k = 0; k < n; k++) {       // 経由する頂点
         for (int i = 0; i < n; i++) {    // 始点
             for (int j = 0; j < n; j++) {  // 終点
-                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+                distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
             }
         }
     }
@@ -45,61 +43,61 @@ int main() {
     int h, w;
     cin >> h >> w;
 
-    vector<vector<char>> maze(h, vector<char>(w));
+    vector<vector<char>> grid(h, vector<char>(w, 0));
 
-    rep(y, h) rep(x, w) {
-            char c;
-            cin >> c;
-            maze[y][x] = c;
-        }
+    rep(y, h) rep(x, w) cin >> grid[y][x];
 
-    vector<vector<int>> distances(h * w, vector<int>(h * w, 1000));
 
-    auto toID = [&](int y, int x) {
+    auto to_id = [&](int y, int x) {
         return y * w + x;
     };
 
-    vector<P> move = {
-            {0,  1},
-            {1,  0},
-            {0,  -1},
-            {-1, 0},
+
+    vector<vector<int >> distance(h * w, vector<int>(h * w, INT_MAX / 100));
+    rep(i, h * w) distance[i][i] = 0;
+
+
+    auto to_next = [&](int y, int x, P p) {
+        return P(y + p.first, x + p.second);
     };
 
+    vector<P> directions = {
+            P(1, 0),
+            P(-1, 0),
+            P(0, 1),
+            P(0, -1),
+    };
 
-    rep(y, h) rep(x, w) {
-            if (maze[y][x] == '#') continue;
-            distances[toID(y, x)][toID(y, x)] = 0;
+    rep(y, h) {
+        rep(x, w) {
+            for (P p : directions) {
+                P next = to_next(y, x, p);
+                int ny = next.first;
+                int nx = next.second;
+                if (ny == -1 || ny == h) continue;
+                if (nx == -1 || nx == w) continue;
+                if (grid[y][x] == '#') continue;
+                if (grid[ny][nx] == '#') continue;
 
-            for (P p : move) {
-                int cy = p.first;
-                int cx = p.second;
-
-                int ny = y + cy;
-                int nx = x + cx;
-
-                if (ny == -1) continue;
-                if (nx == -1) continue;
-                if (ny == h) continue;
-                if (nx == w) continue;
-
-                if (maze[y][x] == '.' && maze[ny][nx] == '.') {
-                    distances[toID(y, x)][toID(ny, nx)] = 1;
-                    distances[toID(ny, nx)][toID(y, x)] = 1;
-                }
+                int f_id = to_id(y, x);
+                int t_id = to_id(ny, nx);
+                distance[f_id][t_id] = 1;
+                distance[t_id][f_id] = 1;
             }
         }
+    }
 
-
-    warshall_floyd(w * h, distances);
+    warshall_floyd(w * h, distance);
 
     int ans = 0;
-    rep(i, h * w) rep(j, h * w) {
-            int d = distances[i][j];
-            if (d >= 1000) {
-                continue;
-            }
-            cmax(ans, d);
+
+    rep(i, w * h) {
+        rep(j, w * h) {
+            if (distance[i][j] == INT_MAX / 100) continue;
+            cmax(ans, distance[i][j]);
         }
+    }
     cout << ans << endl;
+
 }
+
