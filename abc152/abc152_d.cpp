dbe4a955ae9fb69a511a6946153056303e1c1da4
riku <riku@tanide.net>
2020-03-08 17:07:30 +0900

    【AC】[AtCoder Beginner Contest 152]D - Handstand 2

--- abc152/abc152_d.cpp
+++ abc152/abc152_d.cpp
@@ -1,59 +1,41 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
 typedef long long ll;
-//typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<double, double> P;
+const ll INF = 1e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
-
-class Keta {
-
-    vector<int> digits;
-
-public:
-    Keta(ll i) : digits(log10(i) + 1) {
-        for (int k = 0; k < digits.size(); k++) {
-            digits[k] = i % 10;
-            i /= 10;
-        }
-    }
-
-    int keta() {
-        return digits.size();
-    }
-
-    int get(int index) {
-        assert(index != 0);
-        assert(index <= keta());
-        return digits[index - 1];
-    }
-
-    int top() {
-        return get(keta());
-    }
-};
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
 ll pow2(ll a, ll b) {
     if (b == 0) return 1;
@@ -64,75 +46,31 @@ ll pow2(ll a, ll b) {
     return res;
 }
 
-ll calc_bk(ll top, ll back, ll n, ll k) {
-    ll start = pow2(10, k + 1) * top + back;
-    ll end = start;
-    for (ll ik = 0; ik < k; ik++) {
-        ll a = 90 * pow2(10, ik);
-        end += a;
-    }
-
-    if (start > n) return 0;
-    if (end < n) {
-        return pow2(10, k);
-    }
-
-    Keta bk(start);
-
-    ll top_m = top * pow2(10, k + 1);
-
-    ll nm = n - top_m;
-    nm /= 10;
-    if (n % 10 >= back) nm++;
-    return nm;
-
+int top(int i) {
+    return i / (pow2(10, log10(i)));
 }
 
-ll calc_b(ll top, ll back, ll n) {
-    if (top == 0) return 0;
-    ll ans = 0;
-    if (top == back) ans++;
-    for (ll k = 0; k < 6; k++) {
-        // top k k k back;
-        ll now = calc_bk(top, back, n, k);
-        ans += now;
-    }
-    return ans;
-}
-
-ll count(ll i) {
-    ll ans = 0;
-//    ll cans = 0;
-    for (ll a = 1; a <= i; a++) {
-        Keta ak(a);
-        ll now = calc_b(ak.get(1), ak.top(), i);
-        ans += now;
-
-//        for (ll b = 1; b <= i; b++) {
-//
-//            Keta bk(b);
-
-//            if (ak.get(ak.keta()) == bk.get(1) && ak.get(1) == bk.get(bk.keta())) {
-//                cans++;
-//            }
-
-//        }
-
-    }
-    return ans;
+int back(int i) {
+    return i % 10;
 }
 
 int main() {
+    vector<vector<int>> counter(10, vector<int>(10, 0));
 
     int n;
     cin >> n;
-    if(n <= 9) {
-        cout << n << endl;
-        return 0;
+
+    for (int i = 1; i <= n; i++) {
+        int t = top(i);
+        int b = back(i);
+        counter[t][b]++;
     }
-
-    ll ans = count(n);
+    ll ans = 0;
+    for (int i = 1; i <= n; i++) {
+        int t = top(i);
+        int b = back(i);
+        ll count = counter[b][t];
+        ans += count;
+    }
     cout << ans << endl;
-
 }
-
