c07ee177914c5275187c2faa3b9f6c742566f601
riku <riku@tanide.net>
2020-03-08 17:40:24 +0900

    【AC】[AtCoder Beginner Contest 156]D - Bouquet

--- abc156/abc156_d.cpp
+++ abc156/abc156_d.cpp
@@ -2,30 +2,32 @@
 #include <cmath>
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-//typedef pair<ll, int> P;
 //typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-//const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<double, double> P;
+const ll INF = 1e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
 #define ret() return 0;
 
-bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
@@ -33,8 +35,7 @@ bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
 // std::cout << std::bitset<8>(9);
 const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
-
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
 struct mint {
     ll x; // typedef long long ll;
@@ -106,18 +107,18 @@ struct mint {
 
 };
 
-mint exclude(ll n, ll a) {
-    mint bunbo = 1;
-    for (int i = 1; i <= a; i++) {
-        bunbo *= i;
-    }
+
+mint choose(ll n, ll a) {
     mint bunshi = 1;
-    for (int i = 1; i <= a; i++) {
-        bunshi *= (n - a + i);
+    for (ll i = 0; i < a; i++) {
+        bunshi *= (n - i);
+    }
+    mint bunbo = 1;
+    for (ll i = 0; i < a; i++) {
+        bunbo *= (1 + i);
     }
 
     return bunshi / bunbo;
-
 }
 
 int main() {
@@ -127,10 +128,10 @@ int main() {
 
     mint all = mint(2).pow(n) - 1;
 
-    mint ae = exclude(n, a);
-    mint be = exclude(n, b);
+    mint ac = choose(n, a);
+    mint bc = choose(n, b);
 
-    mint ans = all - ae - be;
+    mint ans = all - ac - bc;
+
     cout << ans << endl;
-
 }
