a9816ae333cc2ea5f50ae63633132a5dea8c81fd
riku <riku@tanide.net>
2020-03-10 21:40:48 +0900

    【AC】[AtCoder Beginner Contest 014]C - AtColor

--- abc014/abc014_3.cpp
+++ abc014/abc014_3.cpp
@@ -1,53 +1,55 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
 typedef long long ll;
-//typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<int, int> P;
+const ll INF = 1e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
 int main() {
     int n;
     cin >> n;
+    vector<P> ranges(n);
+    rep(i, n) cin >> ranges[i].first >> ranges[i].second;
+    vector<int> dp(1000002, 0);
+    for (P p : ranges) dp[p.first]++;
+    for (P p : ranges) dp[p.second + 1]--;
 
-    vector<int> colors(1000001, 0);
-
-    rep(i, n) {
-        int a, b;
-        cin >> a >> b;
-        colors[a]++;
-        if (b != 1000000) {
-            colors[b + 1]--;
-        }
+    for (int i = 1; i < 1000002; i++) {
+        dp[i] += dp[i - 1];
     }
 
-    for (int i = 1; i <= 1000000; i++) {
-        colors[i] = colors[i - 1] + colors[i];
-    }
-
-    int ans = *max_element(colors.begin(), colors.end());
+    int ans = *max_element(dp.begin(), dp.end());
     cout << ans << endl;
-
 }
