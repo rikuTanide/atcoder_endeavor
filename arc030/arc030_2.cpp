4a613ed1f1fd3fd8e05b564660d59570e29520d5
riku <riku@tanide.net>
2020-03-10 22:03:32 +0900

    【AC】[AtCoder Regular Contest 030]B - ツリーグラフ

---
+++ arc030/arc030_2.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<int, int> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+struct Triangle {
+    int parent;
+    vector<int> children;
+};
+
+class Tree {
+    vector<vector<int>> edges;
+public:
+    Tree(int n) : edges(n) {}
+
+    void edge(int i, int j) {
+        edges[i].push_back(j);
+        edges[j].push_back(i);
+    }
+
+    void root(int r, vector<Triangle> &leafs) {
+        queue<int> vs;
+        vs.push(r);
+        vector<bool> check(edges.size(), false);
+        check[r] = true;
+        while (!vs.empty()) {
+            int k = vs.front();
+            Triangle triangle;
+            triangle.parent = k;
+
+            for (int i : edges[k]) {
+                if (check[i]) continue;
+                check[i] = true;
+                triangle.children.push_back(i);
+                vs.push(i);
+            }
+            vs.pop();
+            leafs.push_back(triangle);
+        }
+        reverse(leafs.begin(), leafs.end());
+    }
+};
+
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+    x--;
+
+    Tree tree(n);
+
+    vector<int> jewelry(n);
+    rep(i, n) cin >> jewelry[i];
+
+    int jc = accumulate(jewelry.begin(), jewelry.end(), 0);
+
+    rep(i, n - 1) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+
+        tree.edge(a, b);
+    }
+
+    vector<Triangle> leafs;
+    tree.root(x, leafs);
+
+    for (Triangle &t : leafs) {
+        for (int c : t.children) jewelry[t.parent] += jewelry[c];
+    }
+    assert(jewelry[x] == jc);
+
+    int ans = 0;
+    for (Triangle &t : leafs) {
+        for (int c : t.children) {
+            if (jewelry[c] > 0) ans += 2;
+        }
+    }
+    cout << ans << endl;
+}
