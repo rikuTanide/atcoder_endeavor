1db8b9f39b168f109574ccb24921e05efa3e9701
riku <riku@tanide.net>
2020-03-10 20:20:06 +0900

    【AC】[AtCoder Regular Contest 026]B - 完全数

---
+++ arc026/arc026_2.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<int, int> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+
+int main() {
+    ll n;
+    cin >> n;
+
+    set<ll> factor;
+    for (ll i = 1; i * i <= n; i++) {
+        if (n % i != 0) continue;
+        factor.insert(i);
+        factor.insert(n / i);
+    }
+    factor.erase(n);
+    ll sum = accumulate(factor.begin(), factor.end(), 0ll);
+    if (sum == n) {
+        cout << "Perfect" << endl;
+    } else if (sum < n) {
+        cout << "Deficient" << endl;
+    } else {
+        cout << "Abundant" << endl;
+    }
+
+}
