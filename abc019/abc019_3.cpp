5ba0af75c90a0586e6cbce26ec5ef19b7aa11144
riku <riku@tanide.net>
2020-03-11 21:22:24 +0900

    【AC】[AtCoder Beginner Contest 019]C - 高橋くんと魔法の箱

--- abc019/abc019_3.cpp
+++ abc019/abc019_3.cpp
@@ -1,56 +1,68 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
 typedef long long ll;
-//typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<int, int> P;
+const ll INF = 1e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-typedef pair<char, ll> P;
-
-ll to_odd(ll k) {
-    while (k % 2 == 0) {
-        k /= 2;
+ll check(ll x) {
+    ll sum = 0;
+    while (x != 0) {
+        sum += x % 10;
+        x /= 10;
     }
-    return k;
+    return sum;
 }
 
 int main() {
 
     int n;
     cin >> n;
-    vector<ll> numbers(n);
-    rep(i, n) cin >> numbers[i];
 
-    set<ll> seeds;
-    for (ll num : numbers) {
-        ll od = to_odd(num);
-        seeds.insert(od);
+    set<ll> ans;
+
+    rep(i, n) {
+        ll a;
+        cin >> a;
+
+        while (a % 2 == 0) {
+            a /= 2;
+        }
+        ans.insert(a);
+
     }
 
-    cout << seeds.size() << endl;
-
+    cout << ans.size() << endl;
 }
-
