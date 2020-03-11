374643a8649a7677348b142b0b796a77added48c
riku <riku@tanide.net>
2020-03-11 21:58:59 +0900

    【AC】[AtCoder Regular Contest 036]B - 山のデータ

---
+++ arc036/arc036_b.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<int, int> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> horizon(n);
+    rep(i, n) cin >> horizon[i];
+
+    vector<int> increase(n, 0);
+    for (int i = 1; i < n; i++) {
+        if (horizon[i - 1] < horizon[i]) increase[i] = increase[i - 1] + 1;
+    }
+    vector<int> decrease(n);
+    for (int i = n - 2; i >= 0; i--) {
+        if (horizon[i] > horizon[i + 1]) decrease[i] = decrease[i + 1] + 1;
+    }
+
+    int ans = 0;
+    rep(i, n) {
+        int now = increase[i] + decrease[i] + 1;
+        cmax(ans, now);
+    }
+    cout << ans << endl;
+
+}
