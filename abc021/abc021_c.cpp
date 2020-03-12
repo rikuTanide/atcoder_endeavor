1542c9949592d273db21a7bc803fb59c46741c87
riku <riku@tanide.net>
2020-03-12 19:50:37 +0900

    【AC】[AtCoder Beginner Contest 021]C - 正直者の高橋くん

--- abc021/abc021_c.cpp
+++ abc021/abc021_c.cpp
@@ -1,87 +1,185 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e15;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<int, int> P;
+const ll INF = 1e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
 
-//ifstream myfile("~/Downloads/02.txt");
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-// http://kirika-comp.hatenablog.com/entry/2018/01/29/020648
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
 
-P add(P a, P b) {
-    if (a.first != b.first) return min(a, b);
-    return P(a.first, (a.second + b.second) % mod);
-}
-
-P mul(P a, P b) {
-    return P(a.first + b.first, a.second * b.second % mod);
-}
-
-vector<P> calc(int n, int s, vector<vector<P>> &edges) {
-    vector<P> dp(n, P(INF, 0));
-    vector<bool> vis(n, false);
-    priority_queue<P, vector<P>, greater<P> > que;
-    dp[s] = P(0, 1);
-    que.push(P(0, s));
-    while (not que.empty()) {
-        P vd = que.top();
-        que.pop();
-        int v = vd.second;
-        if (vis[v]) continue;
-        vis[v] = true;
-        rep(i, edges[v].size()) {
-            P wc = edges[v][i];
-            int w = wc.first;
-            ll c = wc.second;
-            dp[w] = add(dp[w], mul(dp[v], P(c, 1)));
-            que.push(P(dp[w].first, w));
-        }
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
     }
-    return dp;
-}
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
 
 int main() {
-    int n;
-    cin >> n;
-    vector<vector<P>> edges(n);
-
-
-    int a, b;
-    cin >> a >> b;
-    a--;
-    b--;
+    int n, a, b;
+    cin >> n >> a >> b;
     int m;
     cin >> m;
+
+    a--;
+    b--;
+
+    vector<vector<int>> routes(n);
+
     rep(i, m) {
         int x, y;
         cin >> x >> y;
+
         x--;
         y--;
 
-        edges[x].push_back(P(y, 1));
-        edges[y].push_back(P(x, 1));
+        routes[x].push_back(y);
+        routes[y].push_back(x);
 
     }
 
-    auto ans = calc(n, a, edges);
+    vector<int> min_distances(n, INT_MAX);
+    min_distances[a] = 0;
+    {
 
-    cout << ans[b].second << endl;
+        struct Move {
+            int from;
+            int depth;
+        };
+
+        queue<Move> q;
+        q.push({a, 0});
+        while (!q.empty()) {
+            Move p = q.front();
+            q.pop();
+            for (int to : routes[p.from]) {
+                int next_depth = p.depth + 1;
+                if (min_distances[to] > next_depth) {
+                    min_distances[to] = next_depth;
+                    q.push({to, next_depth});
+                }
+            }
+        }
+    }
+
+    vector<mint> patterns(n, 0);
+    patterns[a] = 1;
+    {
+
+        struct Move {
+            int from;
+            int depth;
+        };
+        vector<bool> visited(n, false);
+        queue<Move> q;
+        q.push({a, 0});
+        while (!q.empty()) {
+            Move p = q.front();
+            q.pop();
+            for (int to : routes[p.from]) {
+                int next_depth = p.depth + 1;
+                if (min_distances[to] != next_depth) {
+                    continue;
+                }
+                patterns[to] += patterns[p.from];
+                if (!visited[to]) {
+                    q.push({to, next_depth});
+                    visited[to] = true;
+                }
+            }
+        }
+    }
+    cout << patterns[b] << endl;
 }
