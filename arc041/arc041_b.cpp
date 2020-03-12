0cd6bc9d0c665655af8a7890b73c0155df1b81b9
riku <riku@tanide.net>
2020-03-12 23:03:02 +0900

    【AC】[AtCoder Regular Contest 041]B - アメーバ

---
+++ arc041/arc041_b.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<int, int> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+
+int main() {
+    int h, w;
+    cin >> h >> w;
+
+    vector<vector<int>> board(h, vector<int>(w, 0));
+    vector<vector<int>> ans(h, vector<int>(w, 0));
+
+    rep(y, h) rep(x, w) {
+            char c;
+            cin >> c;
+            board[y][x] = c - '0';
+        }
+
+    rep(y, h) {
+        rep(x, w) {
+            if (board[y][x] > 0) {
+                int i = board[y][x];
+                board[y][x] -= i;
+                board[y + 1][x - 1] -= i;
+                board[y + 1][x + 1] -= i;
+                board[y + 2][x] -= i;
+                ans[y + 1][x] += i;
+            }
+        }
+    }
+
+    rep(y, h) {
+        for (int i : ans[y]) cout << i;
+        cout << endl;
+    }
+
+}
