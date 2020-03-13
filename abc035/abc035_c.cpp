968529a5da232d8bda475979e75c089a33bd5360
riku <riku@tanide.net>
2020-03-13 19:33:45 +0900

    【AC】[AtCoder Beginner Contest 035]C - オセロ

--- abc035/abc035_c.cpp
+++ abc035/abc035_c.cpp
@@ -1,55 +1,62 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
 typedef long long ll;
-//typedef unsigned long long ll;
-
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+//typedef pair<ll, ll> P;
+typedef pair<int, int> P;
+const ll INF = 1e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
 int main() {
     int n, q;
     cin >> n >> q;
 
-    vector<int> othello(n + 1, 0);
+    vector<int> numbers(n, 0);
 
     rep(i, q) {
         int l, r;
         cin >> l >> r;
         l--;
         r--;
-        othello[l]++;
-        othello[r + 1]--;
+        numbers[l]++;
+        if (r + 1 < n) numbers[r + 1]--;
+    }
+    for (int i = 1; i < n; i++) {
+        numbers[i] += numbers[i - 1];
     }
 
-    rep(i, n - 1) {
-        othello[i + 1] += othello[i];
-    }
-
-    rep (i, n) {
-        int o = othello[i];
-        cout << (o % 2);
+    rep(i, n) {
+        cout << (numbers[i] % 2);
     }
     cout << endl;
 
