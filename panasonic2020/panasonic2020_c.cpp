fa13c40457d54e015f715c5e7dffb97badcfc648
riku <riku@tanide.net>
2020-03-14 21:11:45 +0900

    【WA】[パナソニックプログラミングコンテスト2020]C - Sqrt Inequality

---
+++ panasonic2020/panasonic2020_c.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, int> P;
+const ll INF = 1e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+
+#define EPS 1e-7
+
+bool equal(double f1, double f2) {
+    return abs(f1 - f2) < EPS;
+}
+
+int main() {
+    double a, b, c;
+    cin >> a >> b >> c;
+
+    if (equal(sqrt(a) + sqrt(b), sqrt(c))) {
+        cout << "No" << endl;
+        ret();
+    }
+
+    if (sqrt(a) + sqrt(b) < sqrt(c)) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+}
