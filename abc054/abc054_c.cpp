3a857e7e9e12302ae463cec8a65b928d9eae187a
riku <riku@tanide.net>
2020-03-15 15:56:07 +0900

    【AC】[AtCoder Beginner Contest 054]C - One-stroke Path

--- abc054/abc054_c.cpp
+++ abc054/abc054_c.cpp
@@ -1,75 +1,75 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = 10e15;
-const ll MINF = -10e10;
-const int INF = INT_MAX;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, int> P;
+const ll INF = 1e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
 
-//ifstream myfile("~/Downloads/02.txt");
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-// https://kamino.hatenablog.com/entry/cpp-permutation-combination
-void foreach_permutation(int n, std::function<void(vector<int>)> f) {
-    vector<int> indexes(n);
-    for (int i = 0; i < n; i++) indexes[i] = i;
-    do {
-        f(indexes);
-    } while (std::next_permutation(indexes.begin(), indexes.end()));
+bool check(const vector<int> &arr, const vector<vector<bool>> &bridge) {
+    for (int i = 1; i < arr.size(); i++) {
+        int from = arr[i - 1];
+        int to = arr[i];
+        if (!bridge[from][to]) return false;
+    }
+    return true;
 }
 
-
 int main() {
+
 
     int n, m;
     cin >> n >> m;
 
-    vector<vector<bool>> routes(n, vector<bool>(n, false));
-
+    vector<vector<bool>> bridge(n, vector<bool>(n, false));
     rep(i, m) {
         int a, b;
         cin >> a >> b;
         a--;
         b--;
-        routes[a][b] = true;
-        routes[b][a] = true;
+        bridge[a][b] = true;
+        bridge[b][a] = true;
     }
 
+    vector<int> arr(n);
+    rep(i, n)arr[i] = i;
 
     int ans = 0;
-    foreach_permutation(n - 1, [&](vector<int> indexes) {
-        vector<int> route(n);
-        route[0] = 0;
-        for (int k = 0; k < n - 1; k++) {
-            route[k + 1] = indexes[k] + 1;
-        }
-
-        for (int k = 0; k < n - 1; k++) {
-            int from = route[k];
-            int to = route[k + 1];
-            if (!routes[from][to]) {
-                return;
-            }
-        }
-        ans++;
-
-    });
-
+    do {
+        if (arr[0] != 0) continue;
+        bool b = check(arr, bridge);
+        if (b) ans++;
+    } while (std::next_permutation(arr.begin(), arr.end()));
     cout << ans << endl;
-
 }
