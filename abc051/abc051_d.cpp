f02ba35a3bf51454e837f9dd977f0cd696b44df1
riku <riku@tanide.net>
2020-03-15 15:40:55 +0900

    【AC】[ AtCoder Beginner Contest 051]D - Candidates of No Shortest Paths

--- abc051/abc051_d.cpp
+++ abc051/abc051_d.cpp
@@ -1,121 +1,87 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
 typedef long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, int> P;
+const ll INF = 1e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-
-// https://qiita.com/ta-ka/items/a023a11efe17ab097433
-struct Edge {
-    ll to, cost;
-};
-vector<vector<Edge>> edges;
-
-void insertTowWay(ll from, ll to, ll cost) {
-    edges[from].push_back({to, cost});
-    edges[to].push_back({from, cost});
-}
-
-class Dijkstra {
-    ll vector_count;
-
-    vector<ll> distances;
-
-public:
-    Dijkstra(ll n) {
-        vector_count = n;
-        distances.resize(n, INF);
-    }
-
-
-    void dijkstra(ll k) {
-        distances[k] = 0;  // 始点sへの最短距離は0
-
-        priority_queue<P, vector<P>, greater<P>> que;  // 距離が小さい順に取り出せるようgreater<P>を指定
-        que.push(P(0, k));
-        while (!que.empty()) {
-            P p = que.top();
-            que.pop();
-            int v = p.second;  // 更新した頂点の中で距離が最小の頂点v
-            if (distances[v] < p.first) {
-                continue;
-            }
-            for (auto e : edges[v]) {  // 頂点vから出る辺eを走査
-                if (distances[e.to] > distances[v] + e.cost) {
-                    distances[e.to] = distances[v] + e.cost;
-                    que.push(P(distances[e.to], e.to));
+void warshall_floyd(int n, vector<vector<ll>> &distances) {
+    for (int k = 0; k < n; k++) {       // 経由する頂点
+        for (int i = 0; i < n; i++) {    // 始点
+            for (int j = 0; j < n; j++) {  // 終点
+                if (distances[i][j] == INF && (distances[i][k] == INF || distances[k][j] == INF)) {
+                    distances[i][j] = INF;
+                } else {
+                    distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
                 }
             }
         }
     }
+}
 
-    ll calcDistance(ll to) {
-        return distances[to];
+struct Edge {
+    int from, to, distance;
+
+    friend std::istream &operator>>(std::istream &in, Edge &o) {
+        int a, b, c;
+        in >> a >> b >> c;
+        a--;
+        b--;
+        o.from = a;
+        o.to = b;
+        o.distance = c;
+        return in;
     }
 
 };
 
-
 int main() {
     int n, m;
     cin >> n >> m;
+    vector<Edge> edges(m);
+    rep(i, m) cin >> edges[i];
 
+    vector<vector<ll>> distance(n, vector<ll>(n, INF));
+    rep(i, n) distance[i][i] = 0;
 
-    vector<Dijkstra> dijkstra_list;
-    rep(i, n) {
-        Dijkstra d(n);
-        dijkstra_list.push_back(d);
-    }
+    for (Edge edge : edges) distance[edge.from][edge.to] = edge.distance;
+    for (Edge edge : edges) distance[edge.to][edge.from] = edge.distance;
 
-
-    edges.resize(n);
-    rep(i, m) {
-        ll a, b, c;
-        cin >> a >> b >> c;
-        a--;
-        b--;
-        insertTowWay(a, b, c);
-    }
-
-    rep(i, n) {
-        dijkstra_list[i].dijkstra(i);
-    }
+    warshall_floyd(n, distance);
 
     int ans = 0;
-
-    for (int i = 0; i < n; i++) {
-        vector<Edge> es = edges[i];
-        for (Edge e : es) {
-            ll cost = dijkstra_list[i].calcDistance(e.to);
-            if (cost < e.cost) {
-                ans++;
-            }
-        }
-    }
-
-    cout << (ans / 2) << endl;
-
+    for (Edge edge : edges) if (distance[edge.from][edge.to] != edge.distance) ans++;
+    cout << ans << endl;
 }
