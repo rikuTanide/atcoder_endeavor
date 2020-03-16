877611ec9119d4ce2da3cee5e09abb4928f0fc84
riku <riku@tanide.net>
2020-03-16 20:28:17 +0900

    【AC】[AtCoder Beginner Contest 059]C - Sequence

--- abc059/arc072_a.cpp
+++ abc059/arc072_a.cpp
@@ -1,75 +1,114 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
-#include <assert.h>    // LON
-#include <math.h>    // sqrt()
-
-
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, int> P;
+const ll INF = 1e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-ll to(ll target, ll now_sum, ll n) {
-
-    ll next_sum = now_sum + n;
-    if (target == -1 && next_sum <= -1) return n;
-    if (target == 1 && next_sum >= 1) return n;
-    ll ab = abs(now_sum - target);
-
-    if (target == -1) return -ab;
-    return ab;
-
-}
 
 int main() {
-    ll n;
+    int n;
     cin >> n;
 
     vector<ll> numbers(n);
-    rep(i, n)cin >> numbers[i];
+    rep(i, n) cin >> numbers[i];
 
-    auto func = [&](int target) {
-        ll now_sum = 0;
-
-        vector<ll> changed = vector<ll>(n);
-        for (ll i = 0; i < n; i++) {
-            ll t = to(target, now_sum, numbers[i]);
-            changed[i] = t;
-            now_sum += t;
-            target = -target;
+    ll ans = INF;
+    {
+        ll sum = 0;
+        // +-+-
+        vector<ll> tos(n, 0);
+        rep(i, n) {
+            if (i % 2 == 0) {
+                if (sum + numbers[i] >= 1) {
+                    tos[i] = numbers[i];
+                    sum += numbers[i];
+                } else {
+                    ll to = 1 - sum;
+                    assert(sum + to == 1);
+                    tos[i] = to;
+                    sum += to;
+                }
+            } else {
+                if (sum + numbers[i] <= -1) {
+                    tos[i] = numbers[i];
+                    sum += numbers[i];
+                } else {
+                    ll to = -1 - sum;
+                    assert(sum + to == -1);
+                    tos[i] = to;
+                    sum += to;
+                }
+            }
         }
 
-        ll now = 0;
-        for (ll i = 0; i < n; i++) {
-            now += abs(numbers[i] - changed[i]);
+        ll diff = 0;
+        rep(i, n) diff += abs(numbers[i] - tos[i]);
+        cmin(ans, diff);
+    }
+    {
+        ll sum = 0;
+        // -+-+
+        vector<ll> tos(n, 0);
+        rep(i, n) {
+            if (i % 2 == 1) {
+                if (sum + numbers[i] >= 1) {
+                    tos[i] = numbers[i];
+                    sum += numbers[i];
+                } else {
+                    ll to = 1 - sum;
+                    assert(sum + to == 1);
+                    tos[i] = to;
+                    sum += to;
+                }
+            } else {
+                if (sum + numbers[i] <= -1) {
+                    tos[i] = numbers[i];
+                    sum += numbers[i];
+                } else {
+                    ll to = -1 - sum;
+                    assert(sum + to == -1);
+                    tos[i] = to;
+                    sum += to;
+                }
+            }
         }
-        return now;
-    };
-    // 最初をマイナス
-    ll ans = min(func(1), func(-1));
 
+        ll diff = 0;
+        rep(i, n) diff += abs(numbers[i] - tos[i]);
+        cmin(ans, diff);
+    }
     cout << ans << endl;
-
 }
-
