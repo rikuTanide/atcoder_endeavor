e651b07d91b23ac03fb28285eb772cf12a925ae7
riku <riku@tanide.net>
2020-03-18 21:16:46 +0900

    【AC】[AtCoder Grand Contest 019]B - Reverse and Compare

---
+++ agc019/agc019_b.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e15;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+const int column = 50;
+
+int main() {
+    string a;
+    cin >> a;
+
+
+    ll size = a.size();
+    vector<ll> counts(26, 0);
+    for (char c : a) counts[c - 'a']++;
+
+    ll pair_all = size * (size - 1) / 2;
+
+    vector<ll> pair_counts(26);
+    rep(i, 26) pair_counts[i] = counts[i] * (counts[i] - 1) / 2;
+
+
+    ll ans = pair_all + 1;
+    for (ll l : pair_counts) ans -= l;
+
+    cout << ans << endl;
+
+}
+
