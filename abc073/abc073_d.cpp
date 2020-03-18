027efe318551d606c0e0d1999cc87f3cdaf9a46e
riku <riku@tanide.net>
2020-03-18 21:33:46 +0900

    【AC】[AtCoder Beginner Contest 073]D - joisino's travel

--- abc073/abc073_d.cpp
+++ abc073/abc073_d.cpp
@@ -1,87 +1,108 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = 10e15;
-const ll MINF = -10e10;
-const int INF = INT_MAX;
+const ll INF = 10e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
 
-//ifstream myfile("~/Downloads/02.txt");
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-vector<vector<ll>> dp;
+const int column = 50;
 
-// https://kamino.hatenablog.com/entry/cpp-permutation-combination
-void foreach_permutation(int n, std::function<void(int *)> f) {
-    int indexes[n];
-    for (int i = 0; i < n; i++) indexes[i] = i;
-    do {
-        f(indexes);
-    } while (std::next_permutation(indexes, indexes + n));
-}
+class WarchallFloyd {
 
-// https://qiita.com/okaryo/items/8e6cd73f8a676b7a5d75
-void warshall_floyd(int n) {
-    for (int k = 0; k < n; k++) {       // 経由する頂点
-        for (int i = 0; i < n; i++) {    // 始点
-            for (int j = 0; j < n; j++) {  // 終点
-                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
+    int n;
+    vector<vector<ll>> distances;
+
+public:
+    WarchallFloyd(int n) : n(n), distances(n, vector<ll>(n, INF)) {
+        rep(i, n) distances[i][i] = 0;
+    };
+
+    void warshall_floyd() {
+        for (int k = 0; k < n; k++) {       // 経由する頂点
+            for (int i = 0; i < n; i++) {    // 始点
+                for (int j = 0; j < n; j++) {  // 終点
+                    if (distances[i][j] == INF && (distances[i][k] == INF || distances[k][j] == INF)) {
+                        distances[i][j] = INF;
+                    } else {
+                        distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
+                    }
+                }
             }
         }
     }
-}
+
+    ll distance(int from, int to) {
+        return distances[from][to];
+    }
+
+
+    friend std::istream &operator>>(std::istream &in, WarchallFloyd &o) {
+        int from, to;
+        ll c;
+        cin >> from >> to >> c;
+        from--;
+        to--;
+
+        o.distances[from][to] = c;
+        o.distances[to][from] = c;
+        return in;
+    }
+
+
+};
 
 int main() {
-
-    ll n, m, r;
+    int n, m, r;
     cin >> n >> m >> r;
 
-    vector<ll> targets(r);
-    rep(i, r) {
-        ll t;
-        cin >> t;
-        t--;
-        targets[i] = t;
-    }
+    vector<int> visit(r);
+    rep(i, r) cin >> visit[i];
+    rep(i, r) visit[i]--;
+    sort(visit.begin(), visit.end());
 
-    dp = vector<vector<ll>>(n, vector<ll>(n, INF));
-    rep(i, m) {
-        ll a, b, c;
-        cin >> a >> b >> c;
-        a--;
-        b--;
-        dp[a][b] = c;
-        dp[b][a] = c;
-    }
-    warshall_floyd(n);
-
-
+    WarchallFloyd wf(n);
+    rep(i, m) cin >> wf;
+    wf.warshall_floyd();
     ll ans = INF;
-    foreach_permutation(r, [&](int *indexes) {
+    do {
         ll now = 0;
-        for (ll k = 0; k < r - 1; k++) {
-            ll from = targets[indexes[k]];
-            ll to = targets[indexes[k + 1]];
-
-            now += dp[from][to];
+        for (int i = 1; i < r; i++) {
+            now += wf.distance(visit[i - 1], visit[i]);
         }
         cmin(ans, now);
-    });
+    } while (std::next_permutation(visit.begin(), visit.end()));
     cout << ans << endl;
-
 }
+
