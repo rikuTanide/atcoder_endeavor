ddd452f358d246f6d620bb810b7371eca0e61a3a
riku <riku@tanide.net>
2020-03-18 22:11:34 +0900

    【WA】[AtCoder Beginner Contest 074]C - Sugar Water

--- abc074/arc083_a.cpp
+++ abc074/arc083_a.cpp
@@ -1,72 +1,105 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
-
-#include <assert.h>    // LON
-#include <math.h>    // sqrt()
-
-
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+const ll INF = 10e15;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-//typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
+const int column = 50;
+
+struct Solution {
+    int water, sugar;
+
+    int rate() {
+        return (100 * sugar) / (water + sugar);
+    }
+};
 
 int main() {
-    int A, B, C, D, E, F;
-    cin >> A >> B >> C >> D >> E >> F;
+    int w1, w2, s1, s2, mr, b;
+    cin >> w1 >> w2 >> s1 >> s2 >> mr >> b;
 
-    auto canSolve = [&](int a, int b, int c, int d) {
-        int sugar = (c * C) + (d * D);
-        int water = (a * A * 100) + (b * B * 100);
-        return water * E >= 100 * sugar;
-    };
+    w1 *= 100;
+    w2 *= 100;
 
-    auto concentration = [&](int a, int b, int c, int d) {
-        int sugar = (c * C) + (d * D);
-        int water = (a * A * 100) + (b * B * 100);
-        return (100.0 * sugar) / (water + sugar);
-    };
-
-    double par = 0;
-    int sugar = 0, sugar_water = 0;
-    for (int a = 0; a * A * 100 <= F; a++) {
-        int aw = a * A * 100;
-        for (int b = 0; aw + (b * B * 100) <= F; b++) {
-            int bw = b * B * 100;
-            for (int c = 0; aw + bw + (c * C) <= F&& canSolve(a, b, c, 0); c++) {
-                int cs = c * C;
-                for (int d = 0; aw + bw + cs + (d * D) <= F&& canSolve(a, b, c, d); d++) {
-
-                    double p = concentration(a, b, c, d);
-                    if (p >= par) {
-                        par = p;
-                        sugar = (c * C) + (d * D);
-                        sugar_water = (a * A * 100) + (b * B * 100) + (c * C) + (d * D);
-                    }
-                }
-            };
+    set<int> sugars;
+    for (int s1i = 0; s1i * s1 <= b; s1i++) {
+        for (int s2i = 0; (s1i * s1) + (s2i * s2) <= b; s2i++) {
+            sugars.insert(s1 * s1i + s2 * s2i);
         }
     }
 
-    cout << sugar_water << ' ' << sugar << endl;
+    auto check_rate = [&](int w) {
+        int b = w * mr / 100;
+        auto it = sugars.upper_bound(b);
+        assert(it != sugars.begin());
+        it--;
+        int l = *it;
+        assert((100 * l) / (w + l) <= mr);
+        return l;
+    };
+
+    auto check_b = [&](int w) {
+        int nokori = b - w;
+        assert(nokori >= 0);
+        auto it = sugars.upper_bound(nokori);
+        if (it == sugars.begin()) return 0;
+        it--;
+        int l = *it;
+        assert(w + l <= b);
+        return l;
+    };
+
+    vector<Solution> solutions;
+
+    for (int w1i = 0; w1i * w1 <= b; w1i++) {
+        for (int w2i = 0; w1i * w1 + w2i * w2 <= b; w2i++) {
+            if (w1i == 0 && w2i == 0) continue;
+            int w = w1i * w1 + w2i * w2;
+            int s_r = check_rate(w);
+            int r_b = check_b(w);
+            int now = min(s_r, r_b);
+            solutions.push_back(Solution{w, now});
+        }
+    }
+
+    Solution s = *max_element(solutions.begin(), solutions.end(), [](Solution &s, Solution &t) {
+        return s.rate() < t.rate();
+    });
+
+    cout << s.water + s.sugar << ' ' << s.sugar << endl;
 
 }
+
