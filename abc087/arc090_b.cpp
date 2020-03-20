45d182a451930a960b73577e93e0d299223b9c9a
riku <riku@tanide.net>
2020-03-20 14:20:46 +0900

    【AC】[AtCoder Beginner Contest 087]D - People on a Line

--- abc087/arc090_b.cpp
+++ abc087/arc090_b.cpp
@@ -1,117 +1,127 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = 10e15;
-const ll MINF = -10e10;
-const int INF = INT_MAX;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
 
-//ifstream myfile("~/Downloads/02.txt");
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-class UnionFind {
-public:
-    // 親の番号を格納する。親だった場合-size
-    vector<int> parents;
-    vector<int> distance;
+#include <iostream>
+#include <vector>
 
-    UnionFind(int n) {
-        parents = vector<int>(n, -1);
-        distance = vector<int>(n, 0);
+using namespace std;
+
+template<class Abel>
+struct UnionFind {
+    vector<int> par;
+    vector<int> rank;
+    vector<Abel> diff_weight;
+
+    UnionFind(int n = 1, Abel SUM_UNITY = 0) {
+        init(n, SUM_UNITY);
     }
 
-    // aがどのグループに属しているか
-    int root(int a) {
-        if (parents[a] < 0) {
-            return a;
-        }
-
-        int a_parent = parents[a];
-        int a_ancestor = root(parents[a]);
-        if (a_parent == a_ancestor) {
-            return a_ancestor;
-        }
-        int a_distance = distance[a];
-        int a_parent_distance = distance[a_parent];
-        distance[a] = a_distance + a_parent_distance;
-
-        parents[a] = a_ancestor;
-        return a_ancestor;
+    void init(int n = 1, Abel SUM_UNITY = 0) {
+        par.resize(n);
+        rank.resize(n);
+        diff_weight.resize(n);
+        for (int i = 0; i < n; ++i) par[i] = i, rank[i] = 0, diff_weight[i] = SUM_UNITY;
     }
 
-    int size(int a) {
-        return -parents[root(a)];
-    }
-
-    // aとbをくっつける
-    bool connect(int a, int b, int d) {
-        int ra = root(a);
-        int rb = root(b);
-
-
-
-        if (ra == rb) {
-            int distance_a = distance[a];
-            int distance_b = distance[b];
-            int all_distance =  distance_a - distance_b;
-            if (all_distance != d) {
-                cout << "No" << endl;
-                exit(0);
-            }
-            return false;
-        }
-
-        // 大きいほうにA
-        if (size(ra) < size(rb)) {
-            swap(ra, rb);
-            swap(a, b);
+    int root(int x) {
+        if (par[x] == x) {
+            return x;
         } else {
-            d *= -1;
+            int r = root(par[x]);
+            diff_weight[x] += diff_weight[par[x]];
+            return par[x] = r;
         }
+    }
 
-        parents[ra] += parents[rb];
-        parents[rb] = ra;
-        distance[rb] = (distance[a] - distance[b]) + d;
+    Abel weight(int x) {
+        root(x);
+        return diff_weight[x];
+    }
+
+    bool issame(int x, int y) {
+        return root(x) == root(y);
+    }
+
+    bool merge(int x, int y, Abel w) {
+        w += weight(x);
+        w -= weight(y);
+        x = root(x);
+        y = root(y);
+        if (x == y) return false;
+        if (rank[x] < rank[y]) swap(x, y), w = -w;
+        if (rank[x] == rank[y]) ++rank[x];
+        par[y] = x;
+        diff_weight[y] = w;
         return true;
     }
 
+    Abel diff(int x, int y) {
+        return weight(y) - weight(x);
+    }
 };
 
+struct Point {
+    int l, r;
+    ll d;
+};
 
 int main() {
-
-//    ifstream myfile("/home/riku/Downloads/02.txt");
-
-
     int n, m;
     cin >> n >> m;
 
-    UnionFind uf(n);
-
-    rep(i, m) {
-        int l, r, d;
-        cin >> l >> r >> d;
-        l--;
-        r--;
-
-        uf.connect(l, r, d);
+    vector<Point> points(m);
+    rep(i, m) cin >> points[i].l >> points[i].r >> points[i].d;
+    rep(i, m) points[i].l--;
+    rep(i, m) points[i].r--;
+    UnionFind<ll> uf(n);
+    for (Point p : points) {
+        uf.merge(p.l, p.r, p.d);
     }
 
+    for (Point p : points) {
+        if (uf.diff(p.l, p.r) != p.d) {
+            cout << "No" << endl;
+            ret();
+        }
+    }
     cout << "Yes" << endl;
 
 }
+
+
