9bf622e0a56933518266e7d0dcead965f1d13b8b
riku <riku@tanide.net>
2020-03-20 17:38:22 +0900

    【AC】[AtCoder Regular Contest 092]C - 2D Plane 2N Points

--- arc092/arc092_a.cpp
+++ arc092/arc092_a.cpp
@@ -1,67 +1,121 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e15;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
+#include <iostream>
+#include <vector>
 
-int main() {
-    int n;
-    cin >> n;
-    vector<P> reds(n);
-    vector<P> blues(n);
+using namespace std;
 
-    rep(i, n) {
-        int a, b;
-        cin >> a >> b;
-        P p(a, b);
-        reds[i] = p;
+struct BipartiteMatching {
+    vector<vector<int> > graph;
+    vector<int> match, alive, used;
+    int timestamp;
+
+    BipartiteMatching(int n) : graph(n), alive(n, 1), used(n, 0), match(n, -1), timestamp(0) {}
+
+    void add_edge(int u, int v) {
+        graph[u].push_back(v);
+        graph[v].push_back(u);
     }
 
-    rep(i, n) {
-        int a, b;
-        cin >> a >> b;
-        P p(a, b);
-        blues[i] = p;
+    bool dfs(int idx) {
+        used[idx] = timestamp;
+        for (auto &to : graph[idx]) {
+            int to_match = match[to];
+            if (alive[to] == 0) continue;
+            if (to_match == -1 || (used[to_match] != timestamp && dfs(to_match))) {
+                match[idx] = to;
+                match[to] = idx;
+                return true;
+            }
+        }
+        return false;
     }
 
-    sort(blues.begin(), blues.end());
+    int bipartite_matching() {
+        int ret = 0;
+        for (int i = 0; i < graph.size(); i++) {
+            if (alive[i] == 0) continue;
+            if (match[i] == -1) {
+                ++timestamp;
+                ret += dfs(i);
+            }
+        }
+        return ret;
+    }
 
-    sort(reds.begin(), reds.end(), [](P i, P j) {
-        return i.second > j.second;
-    });
-    set<int> red_used;
-    int ans = 0;
-    for (P blue : blues) {
-        for (int i = 0; i < n; i++) {
-            P red = reds[i];
-            if (blue.first <= red.first || blue.second <= red.second) continue;
-            if (red_used.find(i) != red_used.end()) continue;
-            red_used.insert(i);
-            ans++;
-            break;
+    void output() {
+        for (int i = 0; i < graph.size(); i++) {
+            if (i < match[i]) {
+                cout << i << "-" << match[i] << endl;
+            }
         }
     }
+};
 
-    cout << ans << endl;
+int main() {
+    struct Ball {
+        int i, x, y;
+    };
+
+    int n;
+    cin >> n;
+    vector<Ball> blues(n), reds(n);
+    rep(i, n) cin >> reds[i].x >> reds[i].y;
+    rep(i, n) reds[i].i = i;
+    rep(i, n) cin >> blues[i].x >> blues[i].y;
+    rep(i, n) blues[i].i = i;
+
+
+    BipartiteMatching bm(2 * n);
+
+    for (Ball blue: blues) {
+        for (Ball red : reds) {
+            if (red.x < blue.x && red.y < blue.y) {
+                bm.add_edge(red.i, n + blue.i);
+            }
+        }
+    }
+    cout << bm.bipartite_matching() << endl;
+
+
 }
+
+
