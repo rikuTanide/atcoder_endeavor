982b367947e34b7cff00509cf358a2ac9369c7a4
riku <riku@tanide.net>
2020-03-20 09:38:48 +0900

    【AC】[AtCoder Beginner Contest 078]D - ABS

--- abc078/arc085_b.cpp
+++ abc078/arc085_b.cpp
@@ -1,77 +1,59 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = 10e15;
-const ll MINF = -10e10;
-const int INF = INT_MAX;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-//typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-vector<int> numbers;
-
-
-int yGame(int x, int y, int start);
-
-int xGame(int x, int y, int start) {
-    int last = abs(numbers[numbers.size() - 1] - y);
-    int last2 = abs(numbers[numbers.size() - 2] - numbers[numbers.size() - 1]);
-
-    if (numbers.size() - start == 3) {
-        return max(last, last2);
-    }
-
-    int y_game = yGame(start + 1, numbers[start], start + 1);
-
-    return max(max(last, last2), y_game);
-}
-
-int yGame(int x, int y, int start) {
-    int last = abs(numbers[numbers.size() - 1] - x);
-    int last2 = abs(numbers[numbers.size() - 2] - numbers[numbers.size() - 1]);
-
-    if (numbers.size() - start == 3) {
-        return min(last, last2);
-    }
-
-    int x_game = xGame(x, numbers[start], start + 1);
-    return min(min(last, last2), x_game);
-
-}
 
 int main() {
-
-    int n, z, w;
+    int n;
+    ll z, w;
     cin >> n >> z >> w;
+    vector<ll> cards(n);
+    rep(i, n) cin >> cards[i];
 
     if (n == 1) {
-        int a;
-        cin >> a;
-        cout << abs(a - w) << endl;
-        return 0;
+        cout << abs(cards.back() - w) << endl;
+        ret();
     }
 
-    numbers.resize(n);
-    rep(i, n) {
-        cin >> numbers[i];
-    }
+    ll a1 = abs(cards.back() - w);
+    ll a2 = abs(cards[n - 1] - cards[n - 2]);
 
-    int ans = xGame(z, w, 0);
-    cout << ans << endl;
+    cout << max(a1, a2) << endl;
+
 }
+
