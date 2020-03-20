e1e9ebe62c3402f865e3ea464f0f0f612cc114be
riku <riku@tanide.net>
2020-03-20 21:01:58 +0900

    【AC】[AtCoder Beginner Contest 095]D - Static Sushi

--- abc095/arc096_b.cpp
+++ abc095/arc096_b.cpp
@@ -1,137 +1,123 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e15;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
-const int mod = 1000000007;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
+#include <iostream>
+#include <vector>
 
+using namespace std;
+const int ROW_MAX = 100;
+
+struct Sushi {
+    ll cost, nutrition;
+};
 
 int main() {
-//    ifstream myfile("C:\\Users\\riku\\Downloads\\b24");
-
-    ll n, c;
+    int n;
+    ll c;
     cin >> n >> c;
-//    myfile >> n >> c;
 
-    vector<P> counter(n);
+    vector<Sushi> sushi_lr(n), sushi_rl;
+    rep(i, n) cin >> sushi_lr[i].cost >> sushi_lr[i].nutrition;
+    sushi_rl = sushi_lr;
+    reverse(sushi_rl.begin(), sushi_rl.end());
+    rep(i, n) sushi_rl[i].cost = c - sushi_rl[i].cost;
 
-    rep(i, n) {
-        ll x, v;
-//        myfile >> x >> v;
-        cin >> x >> v;
-        P p(x, v);
-        counter[i] = p;
+    vector<ll> candidate;
+
+    if (n == 1) {
+        ll e1 = sushi_lr[0].nutrition - sushi_lr[0].cost;
+        ll e2 = sushi_lr[0].nutrition - (c - sushi_lr[0].cost);
+        ll ans = max({0ll, e1, e2});
+        cout << ans << endl;
+        ret();
     }
 
+    auto check = [&] {
+        vector<ll> lr_nutrition_sum(n);
+        vector<ll> rl_nutrition_sum(n);
 
-    auto getCalorie = [&](int i) {
-        return counter[i].second;
+        rep(i, n) {
+            lr_nutrition_sum[i] = sushi_lr[i].nutrition;
+        }
+        rep(i, n) {
+            if (i == 0)continue;
+            lr_nutrition_sum[i] += lr_nutrition_sum[i - 1];
+        }
+
+        rep(i, n) {
+            rl_nutrition_sum[i] = sushi_rl[i].nutrition;
+        }
+        rep(i, n) {
+            if (i == 0)continue;
+            rl_nutrition_sum[i] += rl_nutrition_sum[i - 1];
+        }
+
+
+        vector<ll> rl_max_total(n);
+        rep(i, n)rl_max_total[i] = rl_nutrition_sum[i] - sushi_rl[i].cost;
+
+        vector<ll> lr_max_total(n);
+        rep(i, n)lr_max_total[i] = lr_nutrition_sum[i] - sushi_lr[i].cost * 2;
+
+        candidate.push_back(*max_element(rl_max_total.begin(), rl_max_total.end()));
+
+        rep(i, n) {
+            if (i == 0)continue;
+            lr_max_total[i] = max(lr_max_total[i], lr_max_total[i - 1]);
+        }
+
+        rep(i, n - 1) {
+            int rl = i;
+            int lr = n - rl - 2;
+
+            ll now = rl_max_total[rl] + lr_max_total[lr];
+            candidate.push_back(now);
+        }
+
     };
+    check();
+    swap(sushi_lr, sushi_rl);
+    check();
 
-    auto getLeftDistance = [&](int i) {
-        return counter[i].first;
-    };
-
-    auto getRightDistance = [&](int i) {
-        return c - counter[i].first;
-    };
-
-
-    // 時計回り
-    ll tokei = 0;
-    {
-        vector<ll> tokei_ruiseki(n, 0);
-        vector<ll> han_tokei_ruiseki(n + 1, 0);
-
-        ll left_calorie = 0;
-        ll left_max = 0;
-        for (ll i = 0; i < n; i++) {
-            left_calorie += getCalorie(i);
-            ll now = left_calorie - getLeftDistance(i);
-            cmax(left_max, now);
-            tokei_ruiseki[i] = left_max;
-        }
-
-        ll right_calorie = 0;
-        ll right_max = 0;
-        for (ll i = n - 1; i > 0; i--) {
-            right_calorie += getCalorie(i);
-            ll now = right_calorie - (getRightDistance(i) * 2);
-            cmax(right_max, now);
-            han_tokei_ruiseki[i] = right_max;
-        }
-
-        ll ans = 0;
-        for (ll l = 0; l < n; l++) {
-            ll r = l + 1;
-            ll l_max = tokei_ruiseki[l];
-            ll r_max = han_tokei_ruiseki[r];
-            ll now = l_max + r_max;
-            cmax(ans, now);
-        }
-        tokei = ans;
-    }
-
-    ll han_tokei = 0;
-    {
-
-        // 反時計回り
-        vector<ll> tokei_ruiseki(n + 1, 0);
-        vector<ll> han_tokei_ruiseki(n, 0);
-
-        ll right_calorie = 0;
-        ll right_max = 0;
-        for (ll i = n - 1; i >= 0; i--) {
-            right_calorie += getCalorie(i);
-            ll now = right_calorie - getRightDistance(i);
-            cmax(right_max, now);
-            han_tokei_ruiseki[i] = right_max;
-        }
-
-        ll left_calorie = 0;
-        ll left_max = 0;
-        for (ll i = 0; i < n; i++) {
-            left_calorie += getCalorie(i);
-            ll now = left_calorie - (getLeftDistance(i) * 2);
-            cmax(left_max, now);
-            tokei_ruiseki[i + 1] = left_max;
-        }
-
-        ll ans = 0;
-        for (ll r = n - 1; r >= 0; r--) {
-            ll l = r;
-            ll r_max = han_tokei_ruiseki[r];
-            ll l_max = tokei_ruiseki[l];
-            ll now = l_max + r_max;
-            cmax(ans, now);
-        }
-        han_tokei = ans;
-    }
-
-
-    ll ans = 0;
-    cmax(ans, tokei);
-    cmax(ans, han_tokei);
-
-    cout << ans << endl;
+    cout << *max_element(candidate.begin(), candidate.end()) << endl;
 
 }
+
+
