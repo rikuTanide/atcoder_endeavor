dcb0a415a40801aa8cc6632d36f90696dd0712af
riku <riku@tanide.net>
2020-03-20 16:49:25 +0900

    【AC】[AtCoder Beginner Contest 089]D - Practical Skill Test

--- abc089/abc089_d.cpp
+++ abc089/abc089_d.cpp
@@ -1,84 +1,95 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e15;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Place {
+    int x, y;
+};
 
 int main() {
     ll h, w, d;
-
     cin >> h >> w >> d;
 
-    vector<P> board(h * w);
+    vector<vector<ll>> grid(h, vector<ll>(w));
+    rep(y, h) rep(x, w) cin >> grid[y][x];
+    rep(y, h) rep(x, w) grid[y][x]--;
 
-    rep(y, h) {
-        rep(x, w) {
-            int a;
-            cin >> a;
-            a--;
-            board[a] = P(x, y);
+    vector<Place> places(h * w);
+    rep(y, h) rep(x, w) {
+            ll i = grid[y][x];
+            places[i].y = y;
+            places[i].x = x;
         }
-    }
-    ll max_step = h * w / d;
-    vector<vector<ll>> move_mp(d, vector<ll>(max_step, 0));
 
-    for (ll i = 0; i <= h * w - d; i++) {
-        ll da = i % d;
-        ll p = i / d;
-        assert(p * d + da == i);
-        P before = board[i];
-        if (i + d >= h * w) break;
-        P next = board[i + d];
-        move_mp[da][p] = abs(before.first - next.first) + abs(before.second - next.second);
-    }
+    auto diff = [](Place &p, Place &q) {
+        return abs(p.x - q.x) + abs(p.y - q.y);
+    };
 
-    vector<vector<ll>> move_sum(d, vector<ll>(max_step + 1, 0));
-    for (ll da = 0; da < move_mp.size(); da++) {
-        vector<ll> mmp = move_mp[da];
-        for (ll p = 0; p < mmp.size(); p++) {
-            move_sum[da][p + 1] = move_sum[da][p] + mmp[p];
-        }
+    vector<ll> distances_sum(h * w, 0);
+    for (ll i = 0; i < h * w; i++) {
+        if (i - d < 0) continue;
+        distances_sum[i] = diff(places[i], places[i - d]);
+    }
+    for (ll i = 0; i < h * w; i++) {
+        if (i - d < 0) continue;
+        distances_sum[i] += distances_sum[i - d];
     }
 
-    ll q;
+
+    int q;
     cin >> q;
-
     rep(i, q) {
         ll l, r;
         cin >> l >> r;
-
         l--;
         r--;
+        ll end = distances_sum[r];
+        ll start = distances_sum[l];
 
-        ll l_da = l % d;
-        ll l_p = l / d;
-
-        ll r_da = r % d;
-        ll r_p = r / d;
-
-        ll now = move_sum[r_da][r_p] - move_sum[l_da][l_p];
-        cout << now << endl;
-
+        cout << end - start << endl;
     }
 
 }
+
+
