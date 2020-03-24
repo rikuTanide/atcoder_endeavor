155d2668a5f1045172ce729602639cf1dfd01232
riku <riku@tanide.net>
2020-03-24 19:28:45 +0900

    【AC】[AtCoder Beginner Contest 099]D - Good Grid

--- abc099/abc099_d.cpp
+++ abc099/abc099_d.cpp
@@ -1,97 +1,103 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e10;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-const int mod = 1000000007;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 
 int main() {
-
-// 普通に実装
-// (i + j) % 3 = 0の色が0 ~ c
-
     int n, c;
     cin >> n >> c;
 
+    vector<vector<int>> costs(c, vector<int>(c));
+    rep(i, c) rep(j, c) cin >> costs[i][j];
 
-    vector<vector<int>> colors(n + 1, vector<int>(n + 1, 0));
-    vector<vector<int>> iwakans(c, vector<int>(c, 0));
+    vector<vector<int>> grid(n, vector<int>(n));
+    rep(i, n) rep(j, n) cin >> grid[i][j];
+    rep(i, n) rep(j, n) grid[i][j]--;
 
-    rep(i, c) rep(j, c) {
-            int d;
-            cin >> d;
-            iwakans[i][j] = d;
-        }
+    vector<vector<int>> use(c, vector<int>(3));
 
-    rep(x, n)rep(y, n) {
-            int cc;
-            cin >> cc;
-            cc--;
-            colors[x + 1][y + 1] = cc;
-        }
-
-    vector<vector<int>> ori(3, vector<int>(c, 0)); // %3=iのところにいるcolorの数
-    for (int x = 1; x <= n; x++) {
-        for (int y = 1; y <= n; y++) {
-            int m = (x + y) % 3;
-            int color = colors[x][y];
-            ori[m][color]++;
+    rep(i, n) {
+        rep(j, n) {
+            int p = (i + j) % 3;
+            int ci = grid[i][j];
+            use[ci][p]++;
         }
     }
 
+    auto check = [&](int d, int e, int f) {
+        ll ans = 0;
+        for (int i = 0; i < c; i++) {
+            ll now = use[i][0] * costs[i][d];
+            ans += now;
+        }
+        for (int i = 0; i < c; i++) {
+            ll now = use[i][1] * costs[i][e];
+            ans += now;
+        }
+        for (int i = 0; i < c; i++) {
+            ll now = use[i][2] * costs[i][f];
+            ans += now;
+        }
+        return ans;
+    };
 
     ll ans = INF;
-    for (int i = 0; i < c; i++) {
-        for (int j = 0; j < c; j++) {
-            if (i == j) continue;
 
-            for (int k = 0; k < c; k++) {
-                if (i == k || j == k) continue;
-
-//                printf("%d %d %d\n", i, j, k);
-
-                ll now = 0;
-
-                for (int color = 0; color < c; color++) {
-                    {
-                        int masu_su = ori[0][color];
-                        int cost = iwakans[color][i];
-                        now += masu_su * cost;
-                    }
-                    {
-                        int masu_su = ori[1][color];
-                        int cost = iwakans[color][j];
-                        now += masu_su * cost;
-                    }
-                    {
-                        int masu_su = ori[2][color];
-                        int cost = iwakans[color][k];
-                        now += masu_su * cost;
-                    }
-                }
-
-                mins(ans, now);
+    for (int d = 0; d < c; d++) {
+        for (int e = 0; e < c; e++) {
+            for (int f = 0; f < c; f++) {
+                if (d == e) continue;
+                if (e == f) continue;
+                if (f == d) continue;
+                ll now = check(d, e, f);
+                cmin(ans, now);
             }
         }
     }
 
     cout << ans << endl;
-
 }
+
+
+
