28f6e442d01aab710907ce700ea8818f0a5f8b31
riku <riku@tanide.net>
2020-03-24 21:48:18 +0900

    【AC】[AtCoder Beginner Contest 103]D - Islands War

--- abc103/abc103_d.cpp
+++ abc103/abc103_d.cpp
@@ -1,56 +1,133 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = 10e10;
-const ll MINF = -10e10;
-const int INF = INT_MAX;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-const int mod = 1000000007;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-typedef tuple<int, int, int> Q;
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class UnionFind {
+public:
+    // 親の番号を格納する。親だった場合-size
+    vector<int> parents;
+
+    UnionFind(int n) {
+        parents = vector<int>(n, -1);
+    }
+
+    // aがどのグループに属しているか
+    int root(int a) {
+        if (parents[a] < 0) {
+            return a;
+        }
+        return parents[a] = root(parents[a]);
+    }
+
+    int size(int a) {
+        return -parents[root(a)];
+    }
+
+    // aとbをくっつける
+    bool connect(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        if (ra == rb) {
+            return false;
+        }
+        // 大きいほうにA
+        if (size(ra) < size(rb)) {
+            swap(ra, rb);
+        }
+        parents[ra] += parents[rb];
+        parents[rb] = ra;
+        return true;
+    }
+
+    bool is_union(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        return ra == rb;
+    }
+};
 
 
 int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<P> wars;
+    vector<vector<int>> rival(n);
     rep(i, m) {
         int a, b;
         cin >> a >> b;
-
-        P p(b, a);
-        wars.push_back(p);
+        a--;
+        b--;
+        rival[a].push_back(b);
+        rival[b].push_back(a);
     }
 
-    sort(wars.begin(), wars.end());
+    UnionFind uf(n);
 
+    int bridge = 0;
+    rep(i, n - 1) {
+        int a = i;
+        int b = i + 1;
 
-    int current_broken = wars[0].first - 1;
-    int ans = 1;
-    for(int i = 1 ; i < wars.size(); i ++) {
-        P p = wars[i];
-        if(p.second > current_broken ) {
-            ans ++;
-            current_broken = p.first - 1;
+        bool ok = [&] {
+            for (int c : rival[b]) {
+                if (uf.is_union(a, c)) {
+                    return false;
+                }
+            }
+            return true;
+        }();
+
+        if (ok) {
+            bridge++;
+            uf.connect(a, b);
         }
     }
 
-    cout << ans << endl ;
+    cout << n - 1 - bridge << endl;
 
 }
+
+
 
