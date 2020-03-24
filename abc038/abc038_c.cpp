6a7bedfe63be76e8378bf7412c3a3c9aaf31da33
riku <riku@tanide.net>
2020-03-24 12:22:01 +0900

    【AC】[AtCoder Beginner Contest 038]C - 単調増加

--- abc038/abc038_c.cpp
+++ abc038/abc038_c.cpp
@@ -2,28 +2,31 @@
 #include <cmath>
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
 using namespace std;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-//typedef pair<ll, int> P;
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-const ll INF = 1e15;
-//const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
 #define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
@@ -31,33 +34,155 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-//const int mod = 1000000007;
+const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class TowPointerListener {
+
+public:
+    virtual bool check_begin(int i) = 0;
+
+    virtual void begin(int i) = 0;
+
+    virtual bool check_right(int right) = 0;
+
+    virtual void push_right(int right) = 0;
+
+    virtual void pop_left(int left) = 0;
+
+    virtual void dead_end(int left, int right) = 0;
+};
+
+class TowPointer {
+
+    int n;
+    TowPointerListener &tpl;
+
+
+    vector<P> get_ranges() {
+        vector<bool> enable_items(n);
+        rep(i, n) enable_items[i] = tpl.check_begin(i);
+
+        auto start = [&](int i) {
+            if (i == 0) {
+                return enable_items[i] == true;
+            }
+            if (!enable_items[i - 1] && enable_items[i]) {
+                return true;
+            }
+            return false;
+        };
+
+        auto end = [&](int i) {
+
+            if (i == n - 1) {
+                return enable_items[i] == true;
+            }
+
+            if (enable_items[i] && !enable_items[i + 1]) {
+                return true;
+            }
+
+            return false;
+        };
+
+        vector<P> ans;
+        P p;
+        rep(i, n) {
+            if (start(i)) {
+                p = P(i, -1);
+            }
+            if (end(i)) {
+                p.second = i;
+                ans.push_back(p);
+            }
+        }
+        return ans;
+    }
+
+    void _go(P range) {
+        int right = -1;
+        for (int left = range.first; left <= range.second; left++) {
+            if (right < left) {
+                right = left;
+                tpl.begin(left);
+            }
+            while (right + 1 <= range.second && tpl.check_right(right + 1)) {
+                right++;
+                tpl.push_right(right);
+            }
+            tpl.dead_end(left, right);
+            tpl.pop_left(left);
+        }
+    }
+
+public:
+    TowPointer(int n, TowPointerListener &tpl) : n(n), tpl(tpl) {}
+
+    void go() {
+
+        vector<P> ranges = get_ranges();
+
+        for (P range: ranges) _go(range);
+
+    }
+
+
+};
+
+
+class TowPointerListenerImpl : public TowPointerListener {
+
+    vector<ll> &numbers;
+
+public:
+    TowPointerListenerImpl(vector<ll> &numbers) : numbers(numbers) {}
+
+    bool check_begin(int i) {
+        return true;
+    }
+
+    void begin(int i) {
+    }
+
+    bool check_right(int right) {
+        return numbers[right - 1] < numbers[right];
+    }
+
+    void push_right(int right) {
+
+    }
+
+    void pop_left(int left) {
+
+    }
+
+    ll ans = 0;
+
+    void dead_end(int left, int right) {
+        ll now = right - left + 1;
+        ans += now;
+    }
+};
 
 int main() {
     int n;
     cin >> n;
-    vector<int> numbers(n);
+
+    vector<ll> numbers(n);
     rep(i, n) cin >> numbers[i];
 
-    int r = 0;
 
-    auto isUp = [&](int l) {
-        if (r == l) return true;
-        if (r == n) return false;
-        return numbers[r - 1] < numbers[r];
-    };
+    TowPointerListenerImpl listener(numbers);
+    TowPointer tp(n, listener);
+    tp.go();
+    cout << listener.ans << endl;
 
-    ll ans = 0;
-
-    for (int l = 0; l < n; l++) {
-        while (isUp(l)) {
-            r++;
-        }
-
-        ans += (r - l);
-
-    }
-    cout << ans << endl;
 }
