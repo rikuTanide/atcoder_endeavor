482b3f5b7b1e4fae1b5efe1d889ab0937b2c3bb1
riku <riku@tanide.net>
2020-03-24 21:22:17 +0900

    【AC】[SoundHound Inc. Programming Contest 2018 -Masters Tournament-]C - Ordinary Beauty

---
+++ soundhound2018-summer-qual/soundhound2018_summer_qual_c.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    double n, m, d;
+    cin >> n >> m >> d;
+
+    if (d == 0) {
+        double ans = 1 / n * (m - 1);
+        printf("%.20f\n", ans);
+    } else {
+        double ans = 2.0 * (n - d) / n / n * (m - 1);
+        printf("%.20f\n", ans);
+    }
+
+
+}
+
+
+
