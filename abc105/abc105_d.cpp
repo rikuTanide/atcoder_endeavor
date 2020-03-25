9d741ba354b8bbd1ca9e7abab177803ff0efc4f7
riku <riku@tanide.net>
2020-03-25 12:58:11 +0900

    【AC】[AtCoder Beginner Contest 105]D - Candy Distribution

--- abc105/abc105_d.cpp
+++ abc105/abc105_d.cpp
@@ -1,50 +1,108 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e10;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-const int mod = 1000000007;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return 0;
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void calculate() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
+
 
 int main() {
     ll n, m;
     cin >> n >> m;
 
-    vector<ll> as(n);
-    rep(i, n) {
-        cin >> as[i];
-    }
+    vector<ll> candies(n);
+    rep(i, n) cin >> candies[i];
 
-    vector<ll> sum(n + 1);
-    sum[0] = 0;
-    for (ll i = 0; i < n; i++) {
-        sum[i + 1] = (sum[i] + as[i]) % m;
-    }
+    CumulativeSum cs(n);
+    rep(i, n) cs.set(i, candies[i]);
+    cs.calculate();
 
-    map<ll, ll> used;
+    map<ll, ll> happened;
+    rep(i, n) happened[cs.getSum(i) % m]++;
 
     ll ans = 0;
-    for(ll i = 0 ; i < sum.size() ; i ++) {
-        ans +=  used[sum[i]];
-        used[sum[i]] ++;
+    ans += happened[0];
+    rep(i, n) {
+        happened[cs.getSum(i) % m]--;
+        ll prev = cs.getSum(i) % m;// (m - ) % m;
+        ans += happened[prev];
     }
 
     cout << ans << endl;
 
+    for (auto e : happened) assert(e.second == 0);
 
 }
+
+
+
