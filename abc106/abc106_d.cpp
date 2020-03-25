227f4a0b4363d5b3c6a76d352610b02826af2481
riku <riku@tanide.net>
2020-03-25 19:42:12 +0900

    【AC】[AtCoder Beginner Contest 106]D - AtCoder Express 2

--- abc106/abc106_d.cpp
+++ abc106/abc106_d.cpp
@@ -1,77 +1,112 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e10;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-const int mod = 1000000007;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-vector<vector<ll>> sum;
+#include <iostream>
+#include <vector>
 
-void buildSumTable(ll n, ll m, vector<ll> &ls, vector<ll> &rs) {
-    sum = vector<vector<ll>>(n + 1, vector<ll>(n + 1, 0));
-    for (ll i = 0; i < m; i++) {
-        sum[ls[i]][rs[i]]++;
+using namespace std;
+
+class MatrixSum {
+    vector<vector<ll>> sum;
+    ll w, h;
+public:
+    MatrixSum(ll w, ll h) : w(w), h(h) {
+        sum = vector<vector<ll>>(w, vector<ll>(h));
     }
 
-    for (ll i = 1; i <= n; i++) {
-        for (ll j = 1; j <= n; j++) {
-            sum[i][j] += sum[i - 1][j];
-            sum[i][j] += sum[i][j - 1];
-            sum[i][j] -= sum[i - 1][j - 1];
+    void add(ll x, ll y, ll value) {
+        if (x == w || y == h) return;
+        sum[x][y] += value;
+    }
+
+    ll get(ll x, ll y) {
+        if (x == -1 || y == -1) {
+            return 0;
+        }
+        if (x == sum.size() || y == sum[x].size()) {
+            return 0;
+        }
+        return sum[x][y];
+    }
+
+    void setUp() {
+        for (ll x = 0; x < sum.size(); x++) {
+            for (ll y = 0; y < sum[x].size(); y++) {
+                sum[x][y] += get(x - 1, y) + get(x, y - 1) - get(x - 1, y - 1);
+            }
         }
     }
-}
 
-ll getSumTable(ll l, ll r) {
-    ll ans = sum[r][r] - sum[r][l - 1] - sum[l - 1][r] + sum[l - 1][l - 1];
-    return ans;
-}
+    ll getSum(ll xs, ll ys, ll xe, ll ye) {
+        return get(xe, ye) - get(xs - 1, ye) - get(xe, ys - 1) + get(xs - 1, ys - 1);
+    }
+
+};
 
 int main() {
     ll n, m, q;
-
     cin >> n >> m >> q;
 
-    vector<ll> ls;
-    vector<ll> rs;
+    MatrixSum ms(n, n);
 
     rep(i, m) {
         ll l, r;
         cin >> l >> r;
-        ls.push_back(l);
-        rs.push_back(r);
+        l--;
+        r--;
+
+        ms.add(l, r, 1);
     }
 
-    vector<ll> qls;
-    vector<ll> qrs;
+    ms.setUp();
+
     rep(i, q) {
         ll l, r;
         cin >> l >> r;
-        qls.push_back(l);
-        qrs.push_back(r);
-    }
-
-    buildSumTable(n, m, ls, rs);
-
-    for (ll i = 0; i < q; i++) {
-        ll ans = getSumTable(qls[i], qrs[i]);
-        cout << ans << endl;
+        l--;
+        r--;
+        cout << ms.getSum(l, l, r, r) << endl;
     }
 
 
 }
+
+
+
