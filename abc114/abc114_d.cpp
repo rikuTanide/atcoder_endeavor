414e696ae2d3fb46d0c5d505a2101521b56243b3
riku <riku@tanide.net>
2020-03-26 22:15:39 +0900

    【WA】[AtCoder Beginner Contest 114]D - 756

--- abc114/abc114_d.cpp
+++ abc114/abc114_d.cpp
@@ -1,44 +1,66 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = 10e10;
-const ll MINF = -10e10;
-const int INF = INT_MAX;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
-typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-const int mod = 1000000007;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-int gcd(ll x, ll y) {
-    if (x < y) swap(x, y);
-    if (y == 0) {
-        return x;
-    }
-    return gcd(y, x % y);
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
 }
 
-vector<pair<ll, int >> factorize(ll n) {
-    vector<pair<ll, int >> res;
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int mod = 1000000007;
+
+map<ll, ll> factorize(ll n) {
+    map<ll, ll> res;
 
     for (ll i = 2; i * i <= n; i++) {
         if (n % i != 0) {
             continue;
         }
-        res.emplace_back(i, 0);
+        res[i] = 0;
         while (n % i == 0) {
             n /= i;
-            res.back().second++;
+            res[i]++;
         }
     }
 
-    if (n != 1) res.emplace_back(n, 1);
+
+    if (n != 1) res[n] = 1;
     return res;
 
 }
@@ -47,77 +69,45 @@ vector<pair<ll, int >> factorize(ll n) {
 int main() {
     ll n;
     cin >> n;
-    vector<pair<ll, int>> n_kaijo_soinsu_list;
-    {
-        map<ll, ll> n_kaijo_soinsu_map;
-        for (ll i = 2; i <= n; i++) {
-            vector<pair<ll, int>> i_soinsu_list = factorize(i);
-            for (auto p : i_soinsu_list) {
-                n_kaijo_soinsu_map[p.first] += p.second;
-            }
+    map<ll, ll> factors;
+    for (ll i = 1; i <= n; i++) {
+        map<ll, ll> f = factorize(i);
+        for (auto e : f) {
+            factors[e.first] += e.second;
         }
-        for (auto sosu : n_kaijo_soinsu_map) {
-            pair<ll, int> p(sosu.first, sosu.second);
-            n_kaijo_soinsu_list.push_back(p);
-        }
-        cout << endl;
-
     }
 
-    int ans = 0;
 
-    for (int p = 0; p < n_kaijo_soinsu_list.size(); p++) {
-        for (int q = p + 1; q < n_kaijo_soinsu_list.size(); q++) {
-            for (int r = q + 1; r < n_kaijo_soinsu_list.size(); r++) {
-                int pe = n_kaijo_soinsu_list[p].second;
-                int qe = n_kaijo_soinsu_list[q].second;
-                int re = n_kaijo_soinsu_list[r].second;
+    ll ans = 0;
+    for (auto e : factors) {
+        for (auto f : factors) {
+            for (auto g : factors) {
+                if (e.first == f.first) continue;
+                if (f.first == g.first) continue;
+                if (g.first == e.first) continue;
+                if (f.first >= g.first) continue;
 
-                if (pe >= 4 && qe >= 4 && re >= 2) {
-                    ans++;
-                }
-                if (pe >= 4 && qe >= 2 && re >= 4) {
-                    ans++;
-                }
-                if (pe >= 2 && qe >= 4 && re >= 4) {
-                    ans++;
-                }
+                if (e.second >= 2 && f.second >= 4 && g.second >= 4) ans++;
 
             }
         }
     }
 
-    for (int p = 0; p < n_kaijo_soinsu_list.size(); p++) {
-        for (int q = p + 1; q < n_kaijo_soinsu_list.size(); q++) {
-            int pe = n_kaijo_soinsu_list[p].second;
-            int qe = n_kaijo_soinsu_list[q].second;
-
-            if (pe >= 14 && qe >= 4) {
-                ans++;
-            }
-            if (pe >= 4 && qe >= 14) {
-                ans++;
-            }
-            if (pe >= 24 && qe >= 2) {
-                ans++;
-            }
-            if (pe >= 2 && qe >= 24) {
-                ans++;
-            }
-
+    for (auto e : factors) {
+        for (auto f : factors) {
+            if (e.first == f.first) continue;
+            if (e.second >= 2 && f.second >= 24) ans++;
         }
     }
 
-    for (int p = 0; p < n_kaijo_soinsu_list.size(); p++) {
-        int pe = n_kaijo_soinsu_list[p].second;
-        if (pe >= 74) {
-            ans++;
+    for (auto e : factors) {
+        for (auto f : factors) {
+            if (e.first == f.first) continue;
+            if (e.second >= (5 - 1) && f.second >= (15 - 1)) ans++;
         }
-
     }
 
+    for (auto e : factors) if (e.second >= 75) ans++;
 
     cout << ans << endl;
-
 }
-
