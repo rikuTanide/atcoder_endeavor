196bc6dfe08fee995cc89ec3daa0cc4c85807312
riku <riku@tanide.net>
2020-03-28 21:07:56 +0900

    【AC】[AtCoder Beginner Contest 160]C - Traveling Salesman around Lake

---
+++ abc160/abc160_c.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int mod = 1000000007;
+
+
+int main() {
+    ll k, n;
+    cin >> k >> n;
+
+    vector<ll> lake(n);
+    rep(i, n) cin >> lake[i];
+
+    rep(i, n) lake.push_back(lake[i] + k);
+
+    ll ans = INF;
+    rep(i, n) {
+        ll now = lake[i + n - 1] - lake[i];
+        cmin(ans, now);
+
+    }
+    cout << ans << endl;
+
+}
