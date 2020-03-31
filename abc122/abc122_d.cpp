8b837d60c19e0c4fc82c7e56423ce667f30be2d5
riku <riku@tanide.net>
2020-03-31 23:47:15 +0900

    【RE】[AtCoder Beginner Contest 122]D - We Like AGC

--- abc122/abc122_d.cpp
+++ abc122/abc122_d.cpp
@@ -1,63 +1,181 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-//const int INF = INT_MAX  ;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 
 const int mod = 1000000007;
 
-typedef tuple<ll, int, int, int> PC;
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
 
-int dp[101][4][4][4];
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
+
+
+bool check(string &s) {
+    {
+        string u = s.substr(0, 3);
+        string v = s.substr(1, 3);
+        if (u == "AGC") return false;
+        if (v == "AGC") return false;
+    }
+    for (int i = 0; i < s.size() - 1; i++) {
+        string t = s;
+        swap(t[i], t[i + 1]);
+        string u = t.substr(0, 3);
+        string v = t.substr(1, 3);
+        if (u == "AGC") return false;
+        if (v == "AGC") return false;
+    }
+    return true;
+}
 
 int main() {
     int n;
     cin >> n;
 
-    dp[0][3][3][3] = 1;
+    n -= 4;
+    vector<map<string, mint>> dp(n + 1);
 
-    rep(len, n) {
-        rep(c1, 4) {
-            rep(c2, 4) {
-                rep(c3, 4) {
-                    if (dp[len][c1][c2][c3] == 0) {
-                        continue;
-                    }
+    vector<char> agct = {'A', 'G', 'C', 'T'};
 
-                    for (int a = 0; a < 4; a++) {
-                        if (a == 2 && c1 == 1 && c2 == 0) continue;
-                        if (a == 2 && c1 == 0 && c2 == 1) continue;
-                        if (a == 1 && c1 == 2 && c2 == 0) continue;
-                        if (a == 2 && c1 == 1 && c3 == 0) continue;
-                        if (a == 2 && c2 == 1 && c3 == 0) continue;
-
-                        dp[len + 1][a][c1][c2] += dp[len][c1][c2][c3];
-                        dp[len + 1][a][c1][c2] %= mod;
-                    }
-
+    for (char c : agct) {
+        for (char d : agct) {
+            for (char e : agct) {
+                for (char f : agct) {
+                    string k = "";
+                    k += c;
+                    k += d;
+                    k += e;
+                    k += f;
+                    if (!check(k)) continue;
+                    dp[0][k] = 1;
                 }
             }
         }
     }
 
-    int ans = 0;
-    rep(c1, 4) {
-        rep(c2, 4) {
-            rep(c3, 4) {
-                ans += dp[n][c1][c2][c3];
-                ans %= mod;
+    for (int i = 0; i < n; i++) {
+        for (auto &e : dp[i]) {
+            string s = e.first;
+            mint k = e.second;
+            string t = s.substr(1);
+            for (char c : agct) {
+                string f = t + c;
+                if (!check(f)) continue;
+                dp[i + 1][f] += k;
             }
         }
     }
-    cout << ans << endl;
-}
 
+    mint ans = 0;
+    for (auto &e : dp.back()) {
+        ans += e.second;
+    }
+    cout << ans << endl;
+
+}
