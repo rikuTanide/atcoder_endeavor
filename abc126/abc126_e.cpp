b3d4d8457faa043fc91c04155c0d214caa4c560a
riku <riku@tanide.net>
2020-04-01 12:03:49 +0900

    【AC】[AtCoder Beginner Contest 126]E - 1 or 2

--- abc126/abc126_e.cpp
+++ abc126/abc126_e.cpp
@@ -1,70 +1,107 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 1001001001;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-const int mod = 1e9 + 7;
-
-int n;
-vector<vector<int>> tos;
-set<int> not_visited;
-
-void bfs(int target) {
-    if (not_visited.find(target) == not_visited.end()) return;
-    not_visited.erase(target);
-    auto to = tos[target];
-    for (auto t : to) {
-        bfs(t);
-    }
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
 }
 
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class UnionFind {
+public:
+    // 親の番号を格納する。親だった場合-size
+    vector<int> parents;
+
+    UnionFind(int n) {
+        parents = vector<int>(n, -1);
+    }
+
+    // aがどのグループに属しているか
+    int root(int a) {
+        if (parents[a] < 0) {
+            return a;
+        }
+        return parents[a] = root(parents[a]);
+    }
+
+    int size(int a) {
+        return -parents[root(a)];
+    }
+
+    // aとbをくっつける
+    bool connect(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        if (ra == rb) {
+            return false;
+        }
+        // 大きいほうにA
+        if (size(ra) < size(rb)) {
+            swap(ra, rb);
+        }
+        parents[ra] += parents[rb];
+        parents[rb] = ra;
+        return true;
+    }
+
+    bool is_union(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        return ra == rb;
+    }
+};
 
 int main() {
-    int m;
+    int n, m;
     cin >> n >> m;
 
-    tos = vector<vector<int>>(n);
-
+    UnionFind uf(n);
     rep(i, m) {
         int x, y, z;
         cin >> x >> y >> z;
-
         x--;
         y--;
-        z--;
-
-        tos[x].push_back(y);
-//        tos[x].push_back(z);
-
-        tos[y].push_back(x);
-//        tos[y].push_back(z);
-
-//        tos[z].push_back(x);
-//        tos[z].push_back(y);
-
+        uf.connect(x, y);
     }
 
-    rep(i, n) {
-        not_visited.insert(i);
-    }
+    set<int> roots;
+    rep(i, n) roots.insert(uf.root(i));
+    cout << roots.size() << endl;
 
-    int c = 0;
-    while (!not_visited.empty()) {
-        int i = *not_visited.begin();
-        bfs(i);
-        c++;
-    }
-
-    cout << c << endl;
 
 }
-
