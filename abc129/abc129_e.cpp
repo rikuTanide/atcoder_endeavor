eae99aa1f65009a5351671844e59042b78ab8fb0
riku <riku@tanide.net>
2020-04-01 20:40:13 +0900

    【WA】[AtCoder Beginner Contest 129]E - Sum Equals Xor

--- abc129/abc129_e.cpp
+++ abc129/abc129_e.cpp
@@ -1,43 +1,143 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 1001001001;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
-const int mod = 1000000007;
-int dp[100005][2];
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-int main() {
-    string s;
-    cin >> s;
-    int n = s.size();
-    dp[0][0] = 1;
-    rep(i, n) {
-        {
-            if (s[i] == '0') {
-                dp[i + 1][0] = dp[i][0];
-                dp[i + 1][1] = dp[i][1];
-            } else {
-                dp[i + 1][1] = (dp[i][0] + dp[i][1]) % mod;
-            }
-        }
-        {
-            if (s[i] == '0') {
-                (dp[i + 1][1] += dp[i][1] * 2 % mod) %= mod;
-            } else {
-                (dp[i + 1][0] += dp[i][0] * 2 % mod) %= mod;
-                (dp[i + 1][1] += dp[i][1] * 2 % mod) %= mod;
-            }
-        }
-    }
-    int ans = (dp[n][0] + dp[n][1]) % mod;
-    cout << ans << endl;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
 }
 
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct mint {
+    static const int mod = 1000000007;
+
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
+
+int main() {
+
+    string s;
+    cin >> s;
+
+    int n = s.size();
+    vector<vector<mint>> dp(n + 1, vector<mint>(2, 0));
+    dp[0][0] = 1;
+
+    rep(i, n) {
+        if (s[i] == 0) {
+            dp[i + 1][0] = dp[i][0];
+            dp[i + 1][1] = dp[i][1];
+        } else {
+            dp[i + 1][1] = dp[i][0] + dp[i][1];
+        }
+        if (s[i] == '0') {
+            dp[i + 1][1] += dp[i][1] * 2;
+        } else {
+            dp[i + 1][0] += dp[i][0] * 2;
+            dp[i + 1][1] += dp[i][1] * 2;
+        }
+    }
+
+    cout << dp[n][0] + dp[n][1] << endl;
+
+}
