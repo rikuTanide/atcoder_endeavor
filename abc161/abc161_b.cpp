4ab88902c59ad319fd35354173673e069da086b5
riku <riku@tanide.net>
2020-04-04 21:04:45 +0900

    【AC】[AtCoder Beginner Contest 161]B - Popular Vote

---
+++ abc161/abc161_b.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    ll n, m;;
+    cin >> n >> m;
+    vector<ll> popular(n);
+    rep(i, n)cin >> popular[i];
+    ll sum = accumulate(popular.begin(), popular.end(), 0ll);
+    sort(popular.rbegin(), popular.rend());
+    int ans = 0;
+    for(ll p : popular) {
+        if (sum > p * 4 * m) continue;
+        ans++;
+    }
+    if(ans >= m) {
+        cout << "Yes" <<endl;
+    }else {
+        cout << "No"<<endl;
+    }
+}
