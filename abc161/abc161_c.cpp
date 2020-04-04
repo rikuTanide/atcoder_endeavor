138d6722d9d1842c60182a7374bafd37efb67178
riku <riku@tanide.net>
2020-04-04 21:23:50 +0900

    【AC】[AtCoder Beginner Contest 161]C - Replacing Integer

---
+++ abc161/abc161_c.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    ll n, k;
+    cin >> n >> k;
+
+    if (k == 1) {
+        cout << 0 << endl;
+        ret();
+    }
+    if (n == k) {
+        cout << 0 << endl;
+        ret();
+    }
+
+    if (n < k) {
+        cout << min(n, k - n) << endl;
+        ret();
+    }
+
+    ll n2 = n % k;
+
+    cout << min(n2, k - n2) << endl;
+
+
+}
