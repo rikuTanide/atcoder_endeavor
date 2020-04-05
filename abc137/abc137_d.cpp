603a3aaf42595399780f2244a5119034ad11a258
riku <riku@tanide.net>
2020-04-05 16:54:45 +0900

    【AC】[AtCoder Beginner Contest 137]D - Summer Vacation

--- abc137/abc137_d.cpp
+++ abc137/abc137_d.cpp
@@ -1,35 +1,74 @@
 #include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
 
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) int(x.size())
-typedef long long ll;
 
 int main() {
 
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> jobs(m);
+    map<int, vector<int>> v;
+
     rep(i, n) {
         int a, b;
         cin >> a >> b;
-        if (a > m) continue;
-        jobs[m - a].push_back(b);
-
+        v[a].push_back(b);
     }
-
+    ll sum = 0;
     priority_queue<int> q;
-    ll ans = 0;
-
-    for (int i = m - 1; i >= 0; i--) {
-        for(int b : jobs[i]) {
-            q.push(b);
+    rep(i, m + 1) {
+        if (v.find(i) != v.end()) {
+            for (int b : v[i]) {
+                q.push(b);
+            }
         }
-        if(!q.empty()) {
-            ans += q.top();
+        if (!q.empty()) {
+            int t = q.top();
+            sum += t;
             q.pop();
         }
     }
-
-    cout << ans << endl;
+    cout << sum << endl;
 }
+
+
+
