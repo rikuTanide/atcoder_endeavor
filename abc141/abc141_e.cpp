3c5d810ea0fc44238413e34f6670459bfaa6e1cd
riku <riku@tanide.net>
2020-04-05 20:20:01 +0900

    【AC】[AtCoder Beginner Contest 141]E - Who Says a Pun?

--- abc141/abc141_e.cpp
+++ abc141/abc141_e.cpp
@@ -1,36 +1,76 @@
 #include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
 
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-typedef long long ll;
-
-const int INF = 1001001001;
-
-int dp[5005][5005];
 
 int main() {
-
     int n;
     string s;
     cin >> n >> s;
 
+    vector<vector<int>> dp(n, vector<int>(n, -1));
+
+    auto get = [&](int i, int j) {
+        if (i == n || j == n) return 0;
+        return dp[i][j];
+    };
+
     for (int i = n - 1; i >= 0; i--) {
         for (int j = n - 1; j >= 0; j--) {
-            if (s[i] != s[j]) {
-                dp[i][j] = 0;
-            } else {
-                dp[i][j] = dp[i + 1][j + 1] + 1;
-            }
+            if (s[i] != s[j]) dp[i][j] = 0;
+            else dp[i][j] = get(i + 1, j + 1) + 1;
         }
     }
+
     int ans = 0;
     rep(i, n) {
         rep(j, n) {
-            if (i >= j) continue;
-            int now = min(dp[i][j], j - i);
-            ans = max(ans, now);
+            int now = dp[i][j];
+            now = min(now, abs(i - j));
+            cmax(ans, now);
         }
     }
     cout << ans << endl;
-
 }
+
+
+
