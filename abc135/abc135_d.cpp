e6b24f71824e0e712b264f088854de2c3bcc7878
riku <riku@tanide.net>
2020-04-05 16:25:27 +0900

    【AC】[AtCoder Beginner Contest 135]D - Digits Parade

--- abc135/abc135_d.cpp
+++ abc135/abc135_d.cpp
@@ -1,38 +1,145 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
-const ll INF = 1001001001;
-static const ll MOD = 1000000007;
-const int n = 13;
-ll dp[100005][n];
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
 
 int main() {
     string s;
     cin >> s;
+    int n = s.size();
+    vector<vector<mint>> dp(n + 1, vector<mint>(13));
     dp[0][0] = 1;
-    for (int i = 0; i < s.size(); i++) {
-        char c = s[i];
-        if (c == '?') {
-            for (int k = 0; k < 10; k++) {
-                for (int j = 0; j < n; j++) {
-                    dp[i + 1][(j * 10 + k) % n] += dp[i][j];
-                    dp[i + 1][(j * 10 + k) % n] %= MOD;
+    rep(i, n) {
+        if (s[i] == '?') {
+            rep(j, 13) {
+                rep(k, 10) {
+                    int l = (j * 10 + k) % 13;
+                    dp[i + 1][l] += dp[i][j];
                 }
             }
         } else {
-            int k = c - '0';
-            for (int j = 0; j < n; j++) {
-                dp[i + 1][(j * 10 + k) % n] += dp[i][j];
-                dp[i + 1][(j * 10 + k) % n] %= MOD;
+            int k = s[i] - '0';
+            rep(j, 13) {
+                int l = (j * 10 + k) % 13;
+                dp[i + 1][l] += dp[i][j];
             }
         }
     }
 
-    cout << dp[s.length()][5] << endl;
+    cout << dp[n][5] << endl;
 
 }
+
+
+
