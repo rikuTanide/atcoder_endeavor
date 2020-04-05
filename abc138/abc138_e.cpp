2d9e1e4ba05787eb68f0fc5c2b6496e720cc73f6
riku <riku@tanide.net>
2020-04-05 18:04:20 +0900

    【RE】[AtCoder Beginner Contest 138]E - Strings of Impurity

--- abc138/abc138_e.cpp
+++ abc138/abc138_e.cpp
@@ -1,44 +1,77 @@
 #include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
 
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) int(x.size())
-
-typedef long long ll;
-const int INF = 1001001001;
-
 
 int main() {
     string s, t;
     cin >> s >> t;
 
-    int n = sz(s), m = sz(t);
+    s = s + s;
 
-
-    vector<vector<int>> is(26);
-    rep(i, n) {
-        is[s[i] - 'a'].push_back(i);
+    vector<vector<int >> point(26);
+    rep(i, s.size()) {
+        char c = s[i];
+        int j = c - 'a';
+        point[j].push_back(i);
     }
-    rep(i, n) {
-        is[s[i] - 'a'].push_back(i + n);
-    }
-    ll ans = 0;
-    int p = 0;
 
-    rep(i, m) {
-        int c = t[i] - 'a';
-        if (sz(is[c]) == 0) {
-            puts("-1");
-            return 0;
+    ll position = 0;
+    rep(i, t.size()) {
+        char c = t[i];
+        int j = c - 'a';
+        int ps = position % (s.size() / 2);
+        auto it = lower_bound(point[j].begin(), point[j].end(), ps);
+        if (it == point[j].end()) {
+            cout << -1 << endl;
+            ret();
         }
-        p = *lower_bound(is[c].begin(), is[c].end(), p) + 1;
-        if (p >= n) {
-            p -= n;
-            ans += n;
-        }
+        int k = *it;
+        assert(ps < k);
+        int m = k - ps;
+        position += m;
     }
-
-    ans += p;
-    cout << ans << endl;
-
+    cout << position + 1 << endl;
 }
+
+
+
