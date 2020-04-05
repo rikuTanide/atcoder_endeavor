16e9b46416147dc17374d11fd35d87ca18021e48
riku <riku@tanide.net>
2020-04-05 15:43:36 +0900

    【AC】[AtCoder Beginner Contest 134]E - Sequence Decomposing

--- abc134/abc134_e.cpp
+++ abc134/abc134_e.cpp
@@ -1,34 +1,69 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
-const ll INF = 1001001001;
-static const ll MOD = 1000000007;
+const double EPS = 1e-9;
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 
 int main() {
     int n;
     cin >> n;
 
-    vector<int> a(n);
-    rep(i, n)cin >> a[i];
-
-    deque<int> d;
+    multiset<ll> tops;
     rep(i, n) {
-        int p = lower_bound(d.begin(), d.end(), a[i]) - d.begin();
-        if (p == 0) {
-            d.push_front(a[i]);
+        ll a;
+        cin >> a;
+        a = -a;
+        auto it = tops.upper_bound(a);
+        if (it == tops.end()) {
+            tops.insert(a);
         } else {
-            d[p - 1] = a[i];
+            tops.erase(it);
+            tops.insert(a);
         }
     }
 
-    int ans = d.size();
-    cout << ans << endl;
+    cout << tops.size() << endl;
 
 }
+
+
 
