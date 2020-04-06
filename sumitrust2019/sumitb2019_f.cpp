d54351dbdc9d320beaa8cbc078003110d20ff5b8
riku <riku@tanide.net>
2020-04-06 14:39:15 +0900

    【AC】[三井住友信託銀行プログラミングコンテスト2019]F - Interval Running

---
+++ sumitrust2019/sumitb2019_f.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ll t1, t2, a1, a2, b1, b2;
+    cin >> t1 >> t2 >> a1 >> a2 >> b1 >> b2;
+
+    ll p = (a1 - b1) * t1;
+    ll q = (a2 - b2) * t2;
+
+    if (p > 0) {
+        p *= -1;
+        q *= -1;
+    }
+
+    if (p + q == 0) {
+        cout << "infinity" << endl;
+    } else if (p + q < 0) {
+        cout << 0 << endl;
+    } else {
+        ll s = (-p) / (p + q);
+        ll t = (-p) % (p + q);
+        if (t == 0) {
+            cout << s * 2 << endl;
+        } else {
+            cout << s * 2 + 1 << endl;
+        }
+    }
+
+}
