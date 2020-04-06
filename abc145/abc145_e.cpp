f8c5ae658a24745dabf3e01926c8d0e2077c4606
riku <riku@tanide.net>
2020-04-06 12:21:52 +0900

    【AC】[AtCoder Beginner Contest 145]E - All-you-can-eat

--- abc145/abc145_e.cpp
+++ abc145/abc145_e.cpp
@@ -1,101 +1,112 @@
 #include <bits/stdc++.h>
 #include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
 typedef long long ll;
-//typedef unsigned long long ll;
-
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<ll, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
 
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-vector<vector<ll>> dynamic_programing(ll n, ll t, vector<P> menu) {
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Food {
+    ll time, good;
+};
+
+vector<vector<ll>> knapsack(int n, ll t, vector<Food> &foods) {
     vector<vector<ll>> dp(n + 1, vector<ll>(t + 1, -1));
     dp[0][0] = 0;
 
-    auto get = [&](ll i, ll j) {
-        return dp[i][j];
-    };
-
-    auto set = [&](ll i, ll j, ll v) {
+    auto set = [&](int i, ll j, ll value) {
         if (i > n) return;
         if (j > t) return;
-        ll value = get(i, j);
-        dp[i][j] = max(value, v);
+        if (dp[i][j] == -1) dp[i][j] = value;
+        else
+            cmax(dp[i][j], value);
     };
 
-    for (ll i = 0; i < n; i++) {
-        P p = menu[i];
-        for (ll j = 0; j <= t; j++) {
-            ll prev = get(i, j);
-            if (prev == -1) continue;
-            set(i + 1, j + p.first, prev + p.second);
-            set(i + 1, j, prev);
+    rep(i, n) {
+        rep(j, t + 1) {
+            if (dp[i][j] == -1) continue;
+            set(i + 1, j, dp[i][j]);
+            set(i + 1, j + foods[i].time, dp[i][j] + foods[i].good);
         }
     }
-    for (ll i = 0; i <= n; i++) {
-        assert(dp[i][0] == 0);
-        for (ll j = 1; j <= t; j++) {
-            dp[i][j] = max(dp[i][j], dp[i][j - 1]);
+
+
+    rep(i, n + 1) {
+        rep(j, t + 1) {
+            if (j == 0) continue;
+            cmax(dp[i][j], dp[i][j - 1]);
         }
     }
     return dp;
 }
 
+vector<vector<ll>> knapsack2(int n, ll t, vector<Food> foods) {
+    reverse(foods.begin(), foods.end());
+    vector<vector<ll>> ans = knapsack(n, t, foods);
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
 int main() {
-    ll n, t;
+    int n;
+    ll t;
     cin >> n >> t;
 
-    vector<P> menu(n);
-    rep(i, n) cin >> menu[i].first >> menu[i].second;
+    vector<Food> foods(n);
+    rep(i, n)cin >> foods[i].time >> foods[i].good;
 
-    vector<vector<ll>> dp1 = dynamic_programing(n, t - 1, menu);
-    reverse(menu.begin(), menu.end());
-    vector<vector<ll>> dp2 = dynamic_programing(n, t - 1, menu);
-    reverse(menu.begin(), menu.end());
+    vector<vector<ll>> k1 = knapsack(n, t - 1, foods);
+    vector<vector<ll>> k2 = knapsack2(n, t - 1, foods);
 
     ll ans = 0;
-    for (ll i = 0; i < n; i++) {
-        assert(i < dp1.size());
-        ll i2 = n - i - 1;
-        assert(i2 < dp2.size());
-        assert(i + i2 == n - 1);
-        // i を最後にとっておいた場合の
-        for (ll bj = 0; bj <= t - 1; bj++) {
-            ll aj = t - bj - 1;
-            assert(bj < dp1[i].size());
-            assert(aj < dp2[i].size());
-            assert(aj + bj == t - 1);
-            ll bh = dp1[i][bj];
-            ll ah = dp2[i2][aj];
-            assert(bh >= 0);
-            assert(ah >= 0);
-            ll lh = menu[i].second;
-            ll now = bh + ah + lh;
+    rep(i, n) {
+        rep(t1, t) {
+            ll t2 = t - t1 - 1;
+            assert(t1 + t2 + 1 == t);
+
+            ll left = k1[i][t1];
+            ll right = k2[i + 1][t2];
+            ll g = foods[i].good;
+            ll now = left + right + g;
             cmax(ans, now);
         }
     }
-
     cout << ans << endl;
-
 }
