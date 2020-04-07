01fcb772deedc10489b67eee054dbda5440ce34f
riku <riku@tanide.net>
2020-04-07 13:41:22 +0900

    【AC】[CODE FESTIVAL 2015 予選B]C - Hotel

---
+++ code-festival-2015-qualb/codefestival_2015_qualB_c.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+int main() {
+    ll n, m;
+    cin >> n >> m;
+    vector<ll> as(n), bs(m);
+    rep(i, n) cin >> as[i];
+    rep(i, m) cin >> bs[i];
+    if (n < m) {
+        cout << "NO" << endl;
+        ret();
+    }
+
+    sort(as.rbegin(), as.rend());
+    sort(bs.rbegin(), bs.rend());
+
+    rep(i, m) {
+        if (as[i] < bs[i]) {
+            cout << "NO" << endl;
+            ret();
+        }
+    }
+    cout << "YES" << endl;
+
+}
