082c3d2db820a3e2bd7024be534d8e1c91131991
riku <riku@tanide.net>
2020-04-07 14:52:27 +0900

    【AC】[MUJIN プログラミングチャレンジ Programming Challenge]B - Robot Arm

---
+++ mujin-pc-2016/mujin_pc_2016_b.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+int main() {
+    vector<double> arms(3);
+    rep(i, 3) cin >> arms[i];
+    sort(arms.begin(), arms.end());
+    double r123 = accumulate(arms.begin(), arms.end(), 0.0);
+    double r12 = arms[0] + arms[1];
+    if (r12 < arms[2]) {
+        double outer = r123 * r123 * PI;
+        double inner = (arms[2] - r12) * (arms[2] - r12) * PI;
+        printf("%.20f\n", outer - inner);
+    } else {
+        double outer = r123 * r123 * PI;
+        printf("%.20f\n", outer);
+
+    }
+}
