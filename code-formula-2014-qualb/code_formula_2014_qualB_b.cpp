1fabdd1c9586c9e0f66a87b8686524eb64bf20bc
riku <riku@tanide.net>
2020-04-07 11:44:15 +0900

    【AC】[Code Formula 2014 予選B]B - 11の倍数

---
+++ code-formula-2014-qualb/code_formula_2014_qualB_b.cpp
@@ -0,0 +1,66 @@
+#define _GLIBCXX_DEBUG
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    string s;
+    cin >> s;
+    reverse(s.begin(), s.end());
+
+    ll e = 0, o = 0;
+
+    rep(i, s.size()) {
+        char c = s[i];
+        int j = c - '0';
+        if (i % 2) {
+            e += j;
+        } else {
+            o += j;
+        }
+    }
+    printf("%lld %lld\n", e, o);
+}
