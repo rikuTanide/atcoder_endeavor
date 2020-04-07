a5c6a0b33872f35a97faab8f1c5caaeb2a238fd9
riku <riku@tanide.net>
2020-04-07 14:31:56 +0900

    【AC】[第2回 ドワンゴからの挑戦状 予選]B - 積み鉛筆

---
+++ dwango2016-prelims/dwango2016qual_b.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+int main() {
+    int n;
+    cin >> n;
+    n--;
+    vector<ll> pencils(n);
+    rep(i, n) cin >> pencils[i];
+
+    vector<ll> ans(n + 1);
+    ans.front() = pencils.front();
+    ans.back() = pencils.back();
+
+    rep(i, n) {
+        if (i == 0) continue;
+        ans[i] = min(pencils[i - 1], pencils[i]);
+    }
+
+    for (ll l : ans) cout << l << ' ';
+    cout << endl;
+
+}
