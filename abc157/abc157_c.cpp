adfc4a4b4ac15053bc7d7981601db8e4ca1d889a
riku <riku@tanide.net>
2020-04-07 22:49:20 +0900

    【AC】[AtCoder Beginner Contest 157]C - Guess The Number

--- abc157/abc157_c.cpp
+++ abc157/abc157_c.cpp
@@ -1,14 +1,17 @@
+#define _GLIBCXX_DEBUG
+
 #include <bits/stdc++.h>
-#include <cmath>
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-const ll INF = 1e15;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
@@ -29,55 +32,53 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
-//const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-bool check(vector<int> &bingo, int i, int j, int k) {
-    return bingo[i] == -1 && bingo[j] == -1 && bingo[k] == -1;
-}
+//const ll mod = 1e10;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+#include <iostream>
+#include <vector>
+
 
 int main() {
-
     int n, m;
     cin >> n >> m;
-    vector<int> number(n, -1);
+    vector<int> digits(n, -1);
+    vector<P> conditions(m);
+    rep(i, m) cin >> conditions[i].first >> conditions[i].second;
+    rep(i, m) conditions[i].first--;
+    for (P p : conditions) {
+        digits[p.first] = p.second;
+    }
 
-    if (n == 1 && m == 0) {
-        cout << 0 << endl;
+    if (n == 1) {
+        if (digits[0] == -1) digits[0] = 0;
+    } else {
+        rep(i, n) {
+            if (i == 0 && digits[0] == -1) digits[0] = 1;
+            else if (digits[i] == -1)digits[i] = 0;
+        }
+    }
+
+    for (P p : conditions) {
+        if (digits[p.first] != p.second) {
+            cout << -1 << endl;
+            ret();
+        }
+    }
+
+    if (digits.size() > 1 && *max_element(digits.begin(), digits.end()) == 0) {
+        cout << -1 << endl;
         ret();
     }
 
-    rep(i, m) {
-        int s, c;
-        cin >> s >> c;
-        s--;
-
-        if (s == 0 && c == 0 && n != 1) {
-            cout << -1 << endl;
-            ret();
-        }
-
-        if (number[s] == -1) {
-            number[s] = c;
-        } else if (number[s] == c) {
-            continue;
-        } else {
-            cout << -1 << endl;
-            ret();
-        }
+    int ans = 0;
+    rep(i, n) {
+        ans *= 10;
+        ans += digits[i];
     }
-
-    if (number[0] == -1) number[0] = 1;
-    for (int i = 1; i < n; i++) {
-        if (number[i] == -1) number[i] = 0;
-    }
-
-    for (int num : number) cout << num;
-    cout << endl;
-
+    cout << ans << endl;
 }
