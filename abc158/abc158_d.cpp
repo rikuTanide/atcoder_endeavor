00dfa1734a9928cb1306813d3e9faf505f031413
riku <riku@tanide.net>
2020-04-07 22:58:04 +0900

    【AC】[AtCoder Beginner Contest 158]D - String Formation

--- abc158/abc158_d.cpp
+++ abc158/abc158_d.cpp
@@ -1,15 +1,17 @@
+#define _GLIBCXX_DEBUG
+
 #include <bits/stdc++.h>
-#include <cmath>
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
-typedef pair<double, double> P;
-const ll INF = 1e15;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
@@ -30,55 +32,51 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
+
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
 
 int main() {
-    int direction = 1;
     string s;
     int q;
     cin >> s >> q;
 
-    list<char> l;
-    for (char c : s) l.push_back(c);
+    list<char> ans;
+
+    for(char c : s) ans.push_back(c);
+
+    bool direction = true;
 
     rep(i, q) {
-        int t;
-        cin >> t;
-        if (t == 1) direction *= -1;
-        else {
-            int f;
-            char c;
-            cin >> f >> c;
+        int m;
+        cin >> m;
 
-            if (direction == -1) {
-                if (f == 1) f = 2;
-                else f = 1;
+        if (m == 1) direction = !direction;
+        else {
+            int p;
+            char c;
+            cin >> p >> c;
+
+            if (!direction) {
+                if (p == 1) p = 2;
+                else p = 1;
             }
 
-            if (f == 1) {
-                l.push_front(c);
+            if (p == 1) {
+                ans.push_front(c);
             } else {
-                l.push_back(c);
+                ans.push_back(c);
             }
         }
     }
 
-    string ans = "";
-    for (char c: l) {
-        ans += c;
-    }
+    if(!direction) reverse(ans.begin(), ans.end());
 
-    if (direction == -1) {
-        reverse(ans.begin(), ans.end());
-    }
-
-    cout << ans << endl;
-
+    for (char c : ans) cout << c;
+    cout << endl;
 
 }
