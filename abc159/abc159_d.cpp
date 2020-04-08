32efedb6693eecb70c1a63ddcd8fe67231d75459
riku <riku@tanide.net>
2020-04-08 16:19:13 +0900

    【AC】[AtCoder Beginner Contest 159]D - Banned K

--- abc159/abc159_d.cpp
+++ abc159/abc159_d.cpp
@@ -1,12 +1,14 @@
+#define _GLIBCXX_DEBUG
+
 #include <bits/stdc++.h>
-#include <cmath>
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
@@ -30,52 +32,31 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
+
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 
 int main() {
     int n;
     cin >> n;
-    vector<int> balls(n);
-    rep(i, n) cin >> balls[i];
+    vector<ll> numbers(n);
+    rep(i, n) cin >> numbers[i];
 
     map<int, ll> counts;
-    for (int b : balls) counts[b]++;
+    for (ll l : numbers) counts[l]++;
 
     ll all = 0;
-    for (auto e : counts) {
-        all += (e.second * (e.second - 1) / 2);
-    }
+    for (auto e : counts) all += (e.second * (e.second - 1) / 2);
 
-    for (int i : balls) {
-        if (counts.find(i) == counts.end()) {
-            cout << all << endl;
-            continue;
-        }
-        ll count = counts[i];
-        if (count == 1) {
-            cout << all << endl;
-            continue;
-        }
-        ll origi = count * (count - 1) / 2;
-        ll next = (count - 1) * (count - 2) / 2;
-        ll sub = origi - next;
-        cout << all - sub << endl;
+    for (ll l : numbers) {
+        ll sub = counts[l] * (counts[l] - 1) / 2;
+        ll add = (counts[l] - 1) * (counts[l] - 2) / 2;
+        ll now = all - sub + add;
+        cout << now << endl;
     }
-
 
 }
-
-
-
