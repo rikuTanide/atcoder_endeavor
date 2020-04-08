e1ce3551073d959ae5423f02ca1a012d233086b1
riku <riku@tanide.net>
2020-04-08 20:59:53 +0900

    【AC】[AtCoder Grand Contest 043]A - Range Flip Find Route

--- agc043/agc043_a.cpp
+++ agc043/agc043_a.cpp
@@ -1,12 +1,14 @@
+#define _GLIBCXX_DEBUG
+
 #include <bits/stdc++.h>
-#include <cmath>
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
@@ -30,37 +32,22 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
+
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
     int h, w;
     cin >> h >> w;
-    vector<vector<char>> board(h, vector<char>(w));
-    rep(y, h) rep(x, w) cin >> board[y][x];
 
-    vector<vector<int>> dp(h, vector<int>(w, INT_MAX / 10));
-    if (board.front().front() == '.') dp[0][0] = 0;
-    else dp[0][0] = 1;
+    vector<vector<char>> grid(h, vector<char>(w));
+    rep(i, h) rep(j, w) cin >> grid[i][j];
 
-    struct Direction {
-        int x, y;
-    };
-
-    vector<Direction> directions = {
-            {0, 1},
-            {1, 0},
-    };
+    vector<vector<int>> dp(h, vector<int>(w, INT_MAX / 100));
+    dp[0][0] = grid[0][0] == '#' ? 1 : 0;
 
     auto set = [&](int y, int x, int value) {
         cmin(dp[y][x], value);
@@ -68,21 +55,20 @@ int main() {
 
     rep(y, h) {
         rep(x, w) {
-            for (Direction d : directions) {
-                int ny = y + d.y;
-                int nx = x + d.x;
-                if (ny == h || nx == w) continue;
-
-                if (board[ny][nx] == '.') set(ny, nx, dp[y][x]);
-                else if (board[y][x] == '#') set(ny, nx, dp[y][x]);
-                else if (board[y][x] == '.' && board[ny][nx] == '#') set(ny, nx, dp[y][x] + 1);
-                else __throw_runtime_error("konaide");
+            if (y != 0) {
+                if (grid[y][x] == '.') set(y, x, dp[y - 1][x]);
+                else if (grid[y - 1][x] == '#')set(y, x, dp[y - 1][x]);
+                else if (grid[y - 1][x] == '.' && grid[y][x] == '#') set(y, x, dp[y - 1][x] + 1);
+                else __throw_runtime_error("konai");
+            }
+            if (x != 0) {
+                if (grid[y][x] == '.') set(y, x, dp[y][x - 1]);
+                else if (grid[y][x - 1] == '#')set(y, x, dp[y][x - 1]);
+                else if (grid[y][x - 1] == '.' && grid[y][x] == '#') set(y, x, dp[y][x - 1] + 1);
+                else __throw_runtime_error("konai");
             }
         }
     }
 
     cout << dp.back().back() << endl;
-
 }
-
-
