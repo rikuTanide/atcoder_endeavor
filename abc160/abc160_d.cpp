633c1f98dcd3c23114d9fb0187e16f90ae4d9324
riku <riku@tanide.net>
2020-04-09 23:28:16 +0900

    【WA】[AtCoder Beginner Contest 160]D - Line++

--- abc160/abc160_d.cpp
+++ abc160/abc160_d.cpp
@@ -1,8 +1,10 @@
+#define _GLIBCXX_DEBUG
+
 #include <bits/stdc++.h>
-#include <cmath>
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 //#define rep(i, n) for (int i = 0; i < (n); ++i)
@@ -35,98 +37,55 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 // std::cout << std::bitset<8>(9);
 
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-const int mod = 1000000007;
-
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-
     int n, x, y;
     cin >> n >> x >> y;
-
     x--;
     y--;
 
+    vector<ll> from_xs(n, INF), from_ys(n, INF);
+    {
+        from_xs[x] = 0;
+        from_xs[y] = 1;
+        rep(i, n) {
+            if (i == 0) continue;
+            cmin(from_xs[i], from_xs[i - 1] + 1);
+        }
+        rep(i, n) {
+            if (i == 0) continue;
+            ll j = n - 1 - i;
+            cmin(from_xs[j], from_xs[j + 1] + 1);
+        }
+    }
+    {
+        from_ys[x] = 1;
+        from_ys[y] = 0;
+        rep(i, n) {
+            if (i == 0) continue;
+            cmin(from_ys[i], from_ys[i - 1] + 1);
+        }
+        rep(i, n) {
+            if (i == 0) continue;
+            ll j = n - 1 - i;
+            cmin(from_ys[j], from_ys[j + 1] + 1);
+        }
+    }
 
-    vector<vector<ll>> distances(n, vector<ll>(n, INF));
-    rep(i, n) distances[i][i] = 0;
+
+    vector<ll> ans(n - 1);
 
     rep(i, n) {
         rep(j, n) {
-            if (i >= j) continue;
-            assert(i < j);
-            if (j <= x) {
-                distances[i][j] = j - i;
-                distances[j][i] = j - i;
-            } else if (y <= i) {
-                distances[i][j] = j - i;
-                distances[j][i] = j - i;
-            } else if (i <= x && y <= j) {
-                ll l = x - i;
-                ll r = j - y;
-                ll now = l + r + 1;
-                distances[i][j] = now;
-                distances[j][i] = now;
-            } else if (i <= x && x <= j && j <= y) {
-                ll now = INF;
-                // 直線
-                {
-                    now = j - i;
-                }
-                // 経由
-                {
-                    ll r = y - j;
-                    ll l = x - i;
-                    ll n2 = r + l + 1;
-                    cmin(now, n2);
-                }
-                distances[i][j] = now;
-                distances[j][i] = now;
-            } else if (x <= i && i <= y && y <= j) {
-                ll now = INF;
-                // 直線
-                {
-                    now = j - i;
-                }
-                // 経由
-                {
-                    ll l = i - x;
-                    ll r = j - y;
-                    ll n2 = l + r + 1;
-                    cmin(now, n2);
-                }
-                distances[i][j] = now;
-                distances[j][i] = now;
-            } else if (x <= i && j <= y) {
-                ll now = INF;
-                // 直線
-                {
-                    now = j - i;
-                }
-                {
-                    ll l1 = i - x;
-                    ll l2 = y - j;
-                    ll n2 = l1 + l2 + 1;
-                    cmin(now, n2);
-                }
-                distances[i][j] = now;
-                distances[j][i] = now;
-            } else {
-                __throw_runtime_error("konai");
-            }
+            if (i <= j) continue;
+            ll direct = abs(i - j);
+            ll via_x = from_xs[i] + from_xs[j];
+            ll via_y = from_ys[i] + from_ys[j];
+
+            ll now = min({direct, via_x, via_y});
+            ans[now]++;
         }
     }
-    vector<ll> ans(n);
-    rep(i, n) rep(j, n) ans[distances[i][j]]++;
-
-    rep(i, n) {
-        if (i == 0) continue;
-        cout << ans[i] / 2 << endl;
-    }
+    for (ll a : ans) cout << a << endl;
 }
