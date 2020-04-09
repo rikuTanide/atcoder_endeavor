1ea0b3c58491c61704fb4906535a3cb74fe3d4ca
riku <riku@tanide.net>
2020-04-09 23:36:35 +0900

    【TLE】[AtCoder Beginner Contest 160]E - Red and Green Apples

--- abc160/abc160_e.cpp
+++ abc160/abc160_e.cpp
@@ -1,8 +1,10 @@
+#define _GLIBCXX_DEBUG
+
 #include <bits/stdc++.h>
-#include <cmath>
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 //#define rep(i, n) for (int i = 0; i < (n); ++i)
@@ -37,62 +39,22 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 //const ll mod = 1e10;
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-const int mod = 1000000007;
-
-
 int main() {
     int x, y, a, b, c;
     cin >> x >> y >> a >> b >> c;
+    vector<ll> reds(a), blues(b), whites(c);
+    rep(i, a) cin >> reds[i];
+    rep(i, b) cin >> blues[i];
+    rep(i, c) cin >> whites[i];
 
-    vector<ll> red_temp(a), blue_temp(b);
-    rep(i, a) cin >> red_temp[i];
-    rep(i, b) cin >> blue_temp[i];
-    sort(red_temp.rbegin(), red_temp.rend());
-    sort(blue_temp.rbegin(), blue_temp.rend());
-
-
-    PQ_ASK red, blue;
-    rep(i, x) {
-        red.push(red_temp[i]);
-    }
-    rep(i, y) {
-        blue.push(blue_temp[i]);
-    }
-
-    vector<ll> white(c);
-    rep(i, c) cin >> white[i];
-    sort(white.rbegin(), white.rend());
-
-    for (ll w : white) {
-        if (red.top() < blue.top()) {
-            if (w > red.top()) {
-                red.pop();
-                red.push(w);
-            }
-        } else {
-            if (w > blue.top()) {
-                blue.pop();
-                blue.push(w);
-            }
-        }
-    }
-
-
-    ll ans = 0;
-
-    while (!red.empty()) {
-        ans += red.top();
-        red.pop();
-    }
-    while (!blue.empty()) {
-        ans += blue.top();
-        blue.pop();
-    }
-
+    sort(reds.rbegin(), reds.rend());
+    sort(blues.rbegin(), blues.rend());
+    sort(whites.rbegin(), whites.rend());
+    vector<ll> eats;
+    rep(i, x) eats.push_back(reds[i]);
+    rep(i, y) eats.push_back(blues[i]);
+    for (ll l : whites) eats.push_back(l);
+    sort(eats.rbegin(), eats.rend());
+    ll ans = accumulate(eats.begin(), eats.begin() + x + y, 0ll);
     cout << ans << endl;
 }
