32553a66486530755dde6d127b13ca8b1bd41dd2
riku <riku@tanide.net>
2020-04-12 16:48:57 +0900

    【AC】[AtCoder Regular Contest 014]C - 魂の還る場所

---
+++ arc014/arc014_3.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int r = 0, g = 0, b = 0;
+    int n;
+    string s;
+    cin >> n >> s;
+    for (char c : s) {
+        if (c == 'R') r++;
+        else if (c == 'G') g++;
+        else if (c == 'B') b++;
+    }
+
+    r %= 2;
+    g %= 2;
+    b %= 2;
+    cout << r + g + b << endl;
+
+}
