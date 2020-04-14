d9718be9c9fde0c178058f63a116658190763147
riku <riku@tanide.net>
2020-04-14 22:29:47 +0900

    【AC】[AtCoder Beginner Contest 005]D - おいしいたこ焼きの焼き方

--- abc005/abc005_4.cpp
+++ abc005/abc005_4.cpp
@@ -1,28 +1,41 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef long long ll;
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = 10e15;
-const ll MINF = -10e10;
-const int INF = INT_MAX / 100;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
 
-//ifstream myfile("~/Downloads/02.txt");
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<P, vector<P>, greater<P>> PQ_ASK;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 const int mod = 1000000007;
 
 class MatrixSum {
@@ -32,19 +45,19 @@ public:
         sum = vector<vector<ll>>(x, vector<ll>(y));
     }
 
-    void set(ll x, ll y, ll v) {
-        sum[x][y] = v;
-    }
-
     void add(ll x, ll y) {
         sum[x][y]++;
     }
 
+    void add(ll x, ll y, ll value) {
+        sum[x][y] = value;
+    }
+
     ll get(ll x, ll y) {
-        if (x < -1 || y < -1) {
+        if (x == -1 || y == -1) {
             return 0;
         }
-        if (x >= sum.size() || y >= sum[x].size()) {
+        if (x == sum.size() || y == sum[x].size()) {
             return 0;
         }
         return sum[x][y];
@@ -64,50 +77,43 @@ public:
 
 };
 
-vector<P> pairs(int n, int p) {
-    vector<P> pr;
-    for (int x = 1; x <= n; x++) {
-        int h = 0;
-        for (int y = 1; y <= n; y++) {
-            if (x * y <= p) h = y;
-        }
-        if (h > 0)pr.emplace_back(x, h);
-    }
-    return pr;
-}
-
 int main() {
-    ll n;
+    int n;
     cin >> n;
+    vector<vector<ll>> plate(n, vector<ll>(n));
+    rep(y, n) rep(x, n) cin >> plate[y][x];
 
     MatrixSum ms(n, n);
-    rep(y, n) {
-        rep(x, n) {
-            ll d;
-            cin >> d;
-            ms.set(y, x, d);
-        }
-    }
+    rep(y, n) rep(x, n) ms.add(x, y, plate[y][x]);
     ms.setUp();
 
-    int q;
-    cin >> q;
-    rep(i, q) {
-        int p;
-        cin >> p;
+    vector<ll> dp(n * n + 1, 0);
 
-        ll ans = 0;
-
-        auto rects = pairs(n, p);
-        for (P rect : rects) {
-            for (int y = 0; y < n - rect.second + 1; y++) {
-                for (int x = 0; x < n - rect.first + 1; x++) {
-                    ll now = ms.getSum(x, y, x + rect.first - 1, y + rect.second - 1);
-                    cmax(ans, now);
+    for (int ys = 0; ys < n; ys++) {
+        for (int ye = ys; ye < n; ye++) {
+            for (int xs = 0; xs < n; xs++) {
+                for (int xe = xs; xe < n; xe++) {
+                    int w = xe - xs + 1;
+                    int h = ye - ys + 1;
+                    int area = w * h;
+                    ll sum = ms.getSum(xs, ys, xe, ye);
+                    cmax(dp[area], sum);
                 }
             }
         }
+    }
 
-        cout << ans << endl;
+    rep(i, n * n) {
+        cmax(dp[i + 1], dp[i]);
+    }
+
+    int q;
+    cin >> q;
+
+    rep(i, q) {
+        int a;
+        cin >> a;
+        cout << dp[a] << endl;
     }
 }
+
