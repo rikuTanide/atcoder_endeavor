411510f671d259ba325150fbcb2305c42ce91d8a
riku <riku@tanide.net>
2020-04-17 22:10:46 +0900

    【WA】[AtCoder Beginner Contest 019]D - 高橋くんと木の直径

--- abc019/abc019_4.cpp
+++ abc019/abc019_4.cpp
@@ -1,72 +1,82 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-//typedef pair<char, ll> P;
+struct Point {
+    ll from, to, distance;
+};
 
-ll to_odd(ll k) {
-    while (k % 2 == 0) {
-        k /= 2;
-    }
-    return k;
+ll query(ll from, ll to) {
+    printf("? %lld %lld\n", from, to);
+    flush(cout);
+    ll ans;
+    cin >> ans;
+    return ans;
 }
 
 int main() {
-
     int n;
     cin >> n;
 
-    vector<P> distances1(n, P(0, 0));
-    for (int i = 0; i < n; i++) {
+    vector<Point> distance;
+    rep(i, n) {
         if (i == 0) continue;
-        printf("? %d %d\n", 1, i + 1);
-        fflush(stdout);
-        ll distance;
-        cin >> distance;
-        distances1[i].first = distance;
-        distances1[i].second = i;
+        ll d = query(0, i);
+        Point p = {0, i, d};
+        distance.push_back(p);
     }
-    sort(distances1.rbegin(), distances1.rend());
-    P far = distances1[0];
 
-
-    vector<P> distances2(n, P(0, 0));
-    for (int i = 0; i < n; i++) {
-        if (i == far.second) continue;
-        printf("? %lld %d\n", far.second + 1, i + 1);
-        fflush(stdout);
-        ll distance;
-        cin >> distance;
-        distances2[i].first = distance;
-        distances2[i].second = i;
+    Point p = *max_element(distance.begin(), distance.end(), [](Point p, Point q) {
+        return p.distance < q.distance;
+    });
+    vector<Point> candidate;
+    rep(i, n) {
+        if (i == p.to || i == p.from) continue;
+        ll d = query(p.to, i);
+        Point q = {p.to, i, d};
+        candidate.push_back(q);
     }
-    sort(distances2.rbegin(), distances2.rend());
-    cout << '!' << ' ' << distances2.front().first << endl;
+
+    Point ans = *max_element(candidate.begin(), candidate.end(), [](Point p, Point q) {
+        return p.distance < q.distance;
+    });
+
+    printf("! %lld\n", ans.distance);
+
 }
 
