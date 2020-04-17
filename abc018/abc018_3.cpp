679389aa908976f217237a2b52dfb74dab6c5be4
riku <riku@tanide.net>
2020-04-17 18:26:24 +0900

    【AC】[AtCoder Beginner Contest 018]C - 菱型カウント

--- abc018/abc018_3.cpp
+++ abc018/abc018_3.cpp
@@ -1,101 +1,95 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-class BoardSum {
-    vector<vector<int>> board_sum;
-    int r, c;
-public:
-    BoardSum(vector<vector<char>> &board, int r, int c) : board_sum(r, vector<int>(c)), r(r), c(c) {
-        rep(y, r) rep(x, c) {
-                if (x == 0) {
-                    if (board[y][x] == 'o') {
-                        board_sum[y][x] = 1;
-                    } else {
-                        board_sum[y][x] = 0;
-                    }
-                } else {
-                    if (board[y][x] == 'o') {
-                        board_sum[y][x] = board_sum[y][x - 1] + 1;
-                    } else {
-                        board_sum[y][x] = 0;
-                    }
-                }
-            }
-    };
-
-    int get(int y, int x) {
-        if (y < 0) return -1;
-        if (y >= r) return -1;
-        if (x < 0) return -1;
-        if (x >= c) return -1;
-        return board_sum[y][x];
-    }
-
-};
-
-bool check(BoardSum &boardSum, int y, int x, int k, int r, int c) {
-
-    for (int yz = -k + 1; yz < k; yz++) {
-        int yp = y + yz;
-        if (yp < 0) return false;
-        if (yp >= r) return false;
-
-        int max_c = 2 * k - 1;
-        int dis = abs(yz);
-        int p_c = max_c - dis * 2;
-        int rt = x + p_c / 2;
-        int a = boardSum.get(yp, rt);
-        if (a == -1) return false;
-        if (a < p_c) return false;
-
-    }
-
-    return true;
-}
 
 int main() {
+
     int r, c, k;
     cin >> r >> c >> k;
 
-    vector<vector<char>> board(r, vector<char>(c, ' '));
+    vector<vector<char>> board(r, vector<char>(c));
+    rep(y, r) rep(x, c) cin >> board[y][x];
 
-    rep(y, r) rep(x, c)cin >> board[y][x];
+    vector<vector<int>> dp1(r, vector<int>(c, 0));
+    vector<vector<int>> dp2(r, vector<int>(c, 0));
+    vector<vector<int>> dp3(r, vector<int>(c, 0));
+    vector<vector<int>> dp4(r, vector<int>(c, 0));
 
-    BoardSum boardSum(board, r, c);
+    auto get = [&](vector<vector<int>> &dp, int y, int x) {
+        if (y == -1 || y == r || x == -1 || x == c) return 0;
+        return dp[y][x];
+    };
 
+    // 右上
+    for (int y = 0; y < r; y++) {
+        for (int x = 0; x < c; x++) {
+            if (board[y][x] == 'x') continue;
+            dp1[y][x] = min(get(dp1, y - 1, x), get(dp1, y, x - 1)) + 1;
+        }
+    }
 
+    // 左下
+    for (int y = r - 1; y >= 0; y--) {
+        for (int x = 0; x < c; x++) {
+            if (board[y][x] == 'x') continue;
+            dp2[y][x] = min(get(dp2, y + 1, x), get(dp2, y, x - 1)) + 1;
+        }
+    }
+
+    // 右上
+    for (int y = 0; y < r; y++) {
+        for (int x = c - 1; x >= 0; x--) {
+            if (board[y][x] == 'x') continue;
+            dp3[y][x] = min(get(dp3, y - 1, x), get(dp3, y, x + 1)) + 1;
+        }
+    }
+
+    // 右下
+    for (int y = r - 1; y >= 0; y--) {
+        for (int x = c - 1; x >= 0; x--) {
+            if (board[y][x] == 'x') continue;
+            dp4[y][x] = min(get(dp4, y + 1, x), get(dp4, y, x + 1)) + 1;
+        }
+    }
     int ans = 0;
     rep(y, r) rep(x, c) {
-            if (check(boardSum, y, x, k, r, c)) {
-                ans++;
-            }
+            if (dp1[y][x] >= k && dp2[y][x] >= k && dp3[y][x] >= k && dp4[y][x] >= k) ans++;
         }
     cout << ans << endl;
 }
