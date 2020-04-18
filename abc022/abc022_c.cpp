07c50833f848bc8e590da714cd8557d240f6ef34
riku <riku@tanide.net>
2020-04-18 21:31:51 +0900

    【AC】[AtCoder Beginner Contest 022]C - Blue Bird

--- abc022/abc022_c.cpp
+++ abc022/abc022_c.cpp
@@ -1,149 +1,146 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-// https://qiita.com/ta-ka/items/a023a11efe17ab097433
-struct Edge {
-    ll id, to, cost;
-};
 
-struct Shortness {
-    ll value;
-    set<ll> route;
-};
+class WarchallFloyd {
 
-class RouteDijkstra {
-    ll vector_count;
-
-    vector<Shortness> distances;
-    vector<vector<Edge>> edges;
+    int n;
+    vector<vector<ll>> distances;
 
 public:
-    RouteDijkstra(ll n) {
-        vector_count = n;
-        distances.resize(n, Shortness{INF, set<ll>()});
-        edges.resize(n);
-    }
+    WarchallFloyd(int n) : n(n), distances(n, vector<ll>(n, INF)) {
+        rep(i, n) distances[i][i] = 0;
+    };
 
-    void insertTowWay(ll id, ll from, ll to, ll cost) {
-        edges[from].push_back({id, to, cost});
-        edges[to].push_back({id, from, cost});
-    }
-
-    void dijkstra(ll k) {
-        distances[k].value = 0;  // 始点sへの最短距離は0
-
-        priority_queue<P, vector<P>, greater<P>> que;  // 距離が小さい順に取り出せるようgreater<P>を指定
-        que.push(P(0, k));
-        while (!que.empty()) {
-            P p = que.top();
-            que.pop();
-            int v = p.second;  // 更新した頂点の中で距離が最小の頂点v
-            if (distances[v].value < p.first) {
-                continue;
-            }
-            for (auto e : edges[v]) {  // 頂点vから出る辺eを走査
-                if (distances[e.to].value > distances[v].value + e.cost) {
-                    distances[e.to].value = distances[v].value + e.cost;
-                    distances[e.to].route = distances[v].route;
-                    distances[e.to].route.insert(e.id);
-                    que.push(P(distances[e.to].value, e.to));
+    void warshall_floyd() {
+        for (int k = 0; k < n; k++) {       // 経由する頂点
+            for (int i = 0; i < n; i++) {    // 始点
+                for (int j = 0; j < n; j++) {  // 終点
+                    if (distances[i][j] == INF && (distances[i][k] == INF || distances[k][j] == INF)) {
+                        distances[i][j] = INF;
+                    } else {
+                        distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
+                    }
                 }
             }
         }
     }
 
-    ll calcDistance(ll to) {
-        return distances[to].value;
+    ll distance(int from, int to) {
+        return distances[from][to];
     }
 
-    set<ll> calcRoute(ll to) {
-        return distances[to].route;
+    void add(int from, int to, ll cost) {
+        distances[from][to] = cost;
+        distances[to][from] = cost;
     }
+
+    friend std::istream &operator>>(std::istream &in, WarchallFloyd &o) {
+        int from, to;
+        ll c;
+        cin >> from >> to >> c;
+        from--;
+        to--;
+
+        o.distances[from][to] = c;
+        o.distances[to][from] = c;
+        return in;
+    }
+
 
 };
 
-struct Route {
-    ll id, u, v, l;
+struct Road {
+    int x, y;
+    ll cost;
+
+    friend std::istream &operator>>(std::istream &in, Road &o) {
+        int from, to;
+        ll c;
+        cin >> from >> to >> c;
+        from--;
+        to--;
+
+        o.x = from;
+        o.y = to;
+        o.cost = c;
+
+        return in;
+    }
+
+
 };
 
 int main() {
-    ll n, m;
+    int n, m;
     cin >> n >> m;
 
-    RouteDijkstra routeDijkstra(n);
+    vector<Road> roads(m);
+    rep(i, m) cin >> roads[i];
 
-    vector<Route> routes(m);
-
-    rep(i, m) {
-        ll u, v, l;
-        cin >> u >> v >> l;
-        u--;
-        v--;
-
-        routes[i].id = i;
-        routes[i].u = u;
-        routes[i].v = v;
-        routes[i].l = l;
-
-        routeDijkstra.insertTowWay(i, u, v, l);
+    vector<Road> near;
+    for (Road r : roads) {
+        if (r.x == 0 || r.y == 0) near.push_back(r);
     }
 
-    routeDijkstra.dijkstra(0);
+    WarchallFloyd wf(n);
+    for (Road r : roads) {
+        if (!(r.x == 0 || r.y == 0))wf.add(r.x, r.y, r.cost);
+    }
+    wf.warshall_floyd();
 
     ll ans = INF;
-    rep(k, n) {
-        ll go = routeDijkstra.calcDistance(k);
-        if (go == INF) continue;
-        if (k == 0) continue;
-        set<ll> used = routeDijkstra.calcRoute(k);
 
-        RouteDijkstra dijkstra(n);
-        rep(i, m) {
-            if (used.find(i) != used.end())continue;
-            Route route = routes[i];
-            dijkstra.insertTowWay(route.id, route.u, route.v, route.l);
+    for (int i = 0; i < near.size(); i++) {
+        for (int j = i + 1; j < near.size(); j++) {
+            ll cost = wf.distance(near[i].y, near[j].y);
+            if (cost == INF) continue;
+            ll now = near[i].cost + near[j].cost + cost;
+            cmin(ans, now);
         }
-        dijkstra.dijkstra(0);
-        ll back = dijkstra.calcDistance(k);
-        if (back == INF) continue;
-        ll sum = back + go;
-        cmin(ans, sum);
-
     }
 
     if (ans == INF) {
         cout << -1 << endl;
-        return 0;
+    } else {
+        cout << ans << endl;
     }
 
-
-    cout << ans << endl;
 }
+
