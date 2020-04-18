491b1cb4e24c99c6b42fbd46fca4e4aa60be8fa2
riku <riku@tanide.net>
2020-04-18 20:59:00 +0900

    【WA】[AtCoder Beginner Contest 020]C - 壁抜け

--- abc020/abc020_c.cpp
+++ abc020/abc020_c.cpp
@@ -1,116 +1,172 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-void warshall_floyd(ll n, vector<vector<ll>> &distances) {
-    for (ll k = 0; k < n; k++) {       // 経由する頂点
-        for (ll i = 0; i < n; i++) {    // 始点
-            for (ll j = 0; j < n; j++) {  // 終点
-                distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
-            }
-        }
-    }
+struct Point {
+    int y, x;
+};
+
+Point get_start(vector<vector<char>> &grid, int h, int w) {
+    rep(y, h) rep (x, w) if (grid[y][x] == 'S') return Point{y, x};
+    __throw_runtime_error("konai");
 }
 
 
-ll check(ll k, vector<vector<char>> &field, ll h, ll w) {
-    vector<vector<ll>> distances(h * w, vector<ll>(h * w, INF));
+Point get_goal(vector<vector<char>> &grid, int h, int w) {
+    rep(y, h) rep (x, w) if (grid[y][x] == 'G') return Point{y, x};
+    __throw_runtime_error("konai");
+}
 
-    auto to_id = [&](ll y, ll x) {
-        return y * w + x;
+
+class WarchallFloyd {
+
+    int n;
+    vector<vector<ll>> distances;
+
+public:
+    WarchallFloyd(int n) : n(n), distances(n, vector<ll>(n, INF)) {
+        rep(i, n) distances[i][i] = 0;
     };
 
-    rep(y, h)rep(x, w) {
-            distances[to_id(y, x)][to_id(y, x)] = 0;
-            vector<P> move = {P(1, 0), P(0, 1), P(-1, 0), P(0, -1)};
-            for (P m : move) {
-                ll ny = y + m.first;
-                ll nx = x + m.second;
-
-                if (ny < 0) continue;
-                if (ny == h) continue;
-                if (nx < 0) continue;
-                if (nx == w) continue;
-
-                char n_color = field[ny][nx];
-                if (n_color == 'S') n_color = '.';
-                if (n_color == 'G') n_color = '.';
-
-                if (n_color == '.') {
-                    distances[to_id(y, x)][to_id(ny, nx)] = 1;
-                } else {
-                    distances[to_id(y, x)][to_id(ny, nx)] = k;
+    void warshall_floyd() {
+        for (int k = 0; k < n; k++) {       // 経由する頂点
+            for (int i = 0; i < n; i++) {    // 始点
+                for (int j = 0; j < n; j++) {  // 終点
+                    if (distances[i][j] == INF && (distances[i][k] == INF || distances[k][j] == INF)) {
+                        distances[i][j] = INF;
+                    } else {
+                        distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
+                    }
                 }
             }
         }
+    }
 
-    warshall_floyd(h * w, distances);
+    ll distance(int from, int to) {
+        return distances[from][to];
+    }
 
-    ll start;
-    ll goal;
-    rep(y, h)rep(x, w) {
-            if (field[y][x] == 'S') start = to_id(y, x);
-            if (field[y][x] == 'G') goal = to_id(y, x);
-        }
+    void add(int from, int to, ll cost) {
+        distances[from][to] = cost;
+        distances[to][from] = cost;
+    }
 
-    return distances[start][goal];
+    friend std::istream &operator>>(std::istream &in, WarchallFloyd &o) {
+        int from, to;
+        ll c;
+        cin >> from >> to >> c;
+        from--;
+        to--;
 
-}
+        o.distances[from][to] = c;
+        o.distances[to][from] = c;
+        return in;
+    }
 
-int main() {
-    ll h, w;
-    ll t;
-    cin >> h >> w >> t;
 
-    vector<vector<char>> field(h, vector<char>(w));
-    rep(y, h)rep(x, w)cin >> field[y][x];
+};
 
-    ll ceil = INF / 100;
-    ll floor = 1;
 
-    while (floor + 1 < ceil) {
-        ll mid = (floor + ceil) / 2;
-        ll time = check(mid, field, h, w);
+bool check(vector<vector<char>> &grid, ll mid, int h, int w, Point start, Point goal, ll t) {
+    struct Direction {
+        int x, y;
+    };
 
-        if (time == t) {
-            cout << mid << endl;
-            return 0;
-        }
-        if (time > t) {
-            ceil = mid;
-        } else {
-            floor = mid;
+    vector<Direction> directions = {
+            {0,  1},
+            {1,  0},
+            {0,  -1},
+            {-1, 0},
+    };
+
+
+    auto reachable = [&](int x, int y) {
+        if (x == -1 || x == w || y == -1 || y == h) return false;
+        return true;
+    };
+
+    auto to_id = [&](int y, int x) {
+        return y * w + x;
+    };
+
+    WarchallFloyd wf(h * w);
+
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            for (Direction d : directions) {
+                int nx = x + d.x;
+                int ny = y + d.y;
+                if (!reachable(nx, ny))continue;
+                char next = grid[ny][nx];
+                if (next == '#') {
+                    wf.add(to_id(y, x), to_id(ny, nx), mid);
+                } else {
+                    wf.add(to_id(y, x), to_id(ny, nx), 1);
+                }
+            }
         }
     }
 
+    wf.warshall_floyd();
+
+    return wf.distance(to_id(start.y, start.x), to_id(goal.y, goal.x)) <= t;
+}
+
+int main() {
+    int h, w;
+    ll t;
+    cin >> h >> w >> t;
+    vector<vector<char>> grid(h, vector<char>(w));
+    rep(y, h) rep (x, w) cin >> grid[y][x];
+
+    Point start = get_start(grid, h, w);
+    Point goal = get_goal(grid, h, w);
+
+    ll floor = 0, ceil = INF;
+    while (floor + 1 < ceil) {
+        ll mid = (floor + ceil) / 2;
+        bool b = check(grid, mid, h, w, start, goal, t);
+        if (b) {
+            floor = mid;
+        } else {
+            ceil = mid;
+        }
+    }
     cout << floor << endl;
-
-
 }
 
