274b10ce69c8b37b4a5ab5ada24a543f5eabc3ae
riku <riku@tanide.net>
2020-04-19 21:05:55 +0900

    【AC】[AtCoder Beginner Contest 163]A - Circle Pond

--- abc163/abc163_a.cpp
+++ abc163/abc163_a.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 const double PI = 3.14159265358979323846;
 typedef long long ll;
 const double EPS = 1e-9;
@@ -13,40 +13,39 @@ const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
-
+ 
 double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
-
+ 
 std::istream &operator>>(std::istream &in, set<int> &o) {
     ll a;
     in >> a;
     o.insert(a);
     return in;
 }
-
+ 
 std::istream &operator>>(std::istream &in, queue<int> &o) {
     ll a;
     in >> a;
     o.push(a);
     return in;
 }
-
+ 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
+ 
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
 //const ll mod = 1e10;
-
+ 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 const int mod = 1000000007;
-
-
+ 
+ 
 int main() {
     double r;
     cin >> r;
-
+ 
     printf("%.20f\n", r * 2 * PI);
 }
-
