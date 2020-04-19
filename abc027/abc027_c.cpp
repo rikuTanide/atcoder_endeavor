47cf49cf71907b902b8b28403349d486f8271d38
riku <riku@tanide.net>
2020-04-19 16:45:28 +0900

    【AC】[AtCoder Beginner Contest 027]C - 倍々ゲーム

--- abc027/abc027_c.cpp
+++ abc027/abc027_c.cpp
@@ -1,70 +1,79 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-//typedef long long ll;
-typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
 const int mod = 1000000007;
 
-bool check(ll k, ll n) {
-    ll a = k * 2;
-    ll b = 2 * k + 1;
+bool calc(int x, int n, bool b) {
+    if (2 * x > n) {
+        return !b;
+    }
+    if (calc(2 * x, n, !b) == b || calc(2 * x + 1, n, !b) == b) {
+        return b;
+    }
+    return !b;
+}
 
-    if (a > n && b > n) {
-        return false;
+string calc2(ll n) {
+    if (n == 1) return "Aoki";
+    ll x = 1;
+    ll k = 4;
+
+    while (true) {
+        x += k;
+        if (n <= x) {
+            return "Takahashi";
+        }
+        x += k;
+        if (n <= x) {
+            return "Aoki";
+        }
+        k *= 4;
     }
-    if (!check(a, n) || !check(b, n)) {
-        return true;
-    }
-    return false;
 }
 
 int main() {
-
     ll n;
     cin >> n;
-
-    if (n == 1) {
-        cout << "Aoki" << endl;
-        return 0;
-    }
-    ll par = 1;
-    ll line = 1;
-    while (true) {
-        par *= 4;
-        line += par;
-        if (line >= n) {
-            cout << "Takahashi" << endl;
-            return 0;
-        }
-        line += par;
-        if (line >= n) {
-            cout << "Aoki" << endl;
-            return 0;
-        }
-    }
-
+    cout << calc2(n) << endl;
 }
+
