01145163274bdb413296eecd3a9e1ef7d725728d
riku <riku@tanide.net>
2020-04-20 00:07:02 +0900

    【AC】[天下一プログラマーコンテスト2015予選B]B - 天下一リテラル

---
+++ tenka1-2015-qualb/tenka1_2015_qualB_b.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (s == "{}") {
+        cout << "dict" << endl;
+        ret();
+    }
+    int depth = 0;
+    for (char c : s) {
+        if (c == ':' && depth == 1) {
+            cout << "dict" << endl;
+            ret();
+        } else if (c == '{') {
+            depth++;
+        } else if (c == '}') {
+            depth--;
+        }
+    }
+    cout << "set" << endl;
+}
