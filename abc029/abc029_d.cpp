2d1376fbb2d8fe8553c41ae7136751a9de60c3cc
riku <riku@tanide.net>
2020-04-20 01:11:45 +0900

    【AC】[AtCoder Beginner Contest 029]D - 1

--- abc029/abc029_d.cpp
+++ abc029/abc029_d.cpp
@@ -1,101 +1,185 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-ll pow2(ll a, ll b) {
-    if (b == 0) return 1;
-    if (b == 1) return a;
-    ll res = a;
-    for (ll i = 1; i < b; i++) {
-        res *= a;
-    }
-    return res;
-}
-
-class Keta {
-
-    vector<int> digits;
-
-public:
-    Keta(ll i) : digits(log10(i) + 1) {
-        for (int k = 0; k < digits.size(); k++) {
-            digits[k] = i % 10;
-            i /= 10;
+vector<vector<vector<ll>>> digit_dp_all(ll k) {
+    const int DIGIT_MAX = 30;
+    vector<vector<vector<ll>>> dp(DIGIT_MAX, vector<vector<ll>>(2, vector<ll>(10, 0)));
+    vector<ll> digits(DIGIT_MAX);
+    {
+        int i = 0;
+        while (k > 0) {
+            ll m = k % 10;
+            digits[i] = m;
+            k /= 10;
+            i++;
         }
+        reverse(digits.begin(), digits.end());
     }
 
-    int keta() {
-        return digits.size();
+    int first_i = 0, first_j = 0;
+    rep(i, DIGIT_MAX) {
+        if (digits[i] == 0) continue;
+        first_i = i;
+        first_j = digits[i];
+        break;
     }
-
-    int get(int index) {
-        assert(index != 0);
-        assert(index <= keta());
-        return digits[index - 1];
+    rep(i, 10) {
+        if (i == 0) continue;
+        if (i < first_j) dp[first_i][true][i] = 1;
     }
+    dp[first_i][false][first_j] = 1;
 
-    int sum(int start, int end) {
-//        assert(start <= end);
-        int ans = 0;
-        for (int i = start; i <= end; i++) {
-            ans += get(i) * pow2(10, i - 1);
+    // Kより小さいことが......
+
+    for (int i = first_i; i < DIGIT_MAX - 1; i++) {
+        int d = digits[i];
+        int n = digits[i + 1];
+        assert(d < 10);
+        assert(n < 10);
+        // 未確定から未確定
+        dp[i + 1][false][n] = 1;
+
+        // 未確定から確定
+        for (int jn = 0; jn < 10; jn++) {
+            if (jn >= n) continue;
+            dp[i + 1][true][jn] += dp[i][false][d];
         }
-        return ans;
-    }
 
-};
-
-
-ll solve(ll s) {
-    Keta keta(s);
-    ll ans = 0;
-    for (int d = keta.keta(); d >= 1; d--) {
-        ll rate = pow2(10, d - 2) * (d - 1);
-        for (int i = 1; i <= keta.get(d); i++) {
-            ans += rate;
-            if (i == 2) {
-                ll spe = pow2(10, d - 1);
-                ans += spe;
+        // 確定から確定
+        for (int jp = 0; jp < 10; jp++) {
+            for (int jn = 0; jn < 10; jn++) {
+                dp[i + 1][true][jn] += dp[i][true][jp];
             }
         }
-        if (keta.get(d) == 1) {
-            ll plus = keta.sum(1, d - 1) + 1;
-            ans += plus;
+
+        for (int j = 1; j < 10; j++) {
+            dp[i + 1][true][j] += 1;
         }
     }
-    return ans;
+
+    ll sum = 0;
+    for (int i = 0; i < 10; i++) {
+        sum += dp[DIGIT_MAX - 1][true][i];
+    }
+    for (int i = 0; i < 10; i++) {
+        sum += dp[DIGIT_MAX - 1][false][i];
+    }
+
+    return dp;
+}
+
+
+ll digit_dp(ll k) {
+
+    vector<vector<vector<ll>>> flow = digit_dp_all(k);
+
+    const int DIGIT_MAX = 30;
+    vector<vector<vector<ll>>> dp(DIGIT_MAX, vector<vector<ll>>(2, vector<ll>(10, 0)));
+    vector<ll> digits(DIGIT_MAX);
+    {
+        int i = 0;
+        while (k > 0) {
+            ll m = k % 10;
+            digits[i] = m;
+            k /= 10;
+            i++;
+        }
+        reverse(digits.begin(), digits.end());
+    }
+
+    int first_i = 0, first_j = 0;
+    rep(i, DIGIT_MAX) {
+        if (digits[i] == 0) continue;
+        first_i = i;
+        first_j = digits[i];
+        break;
+    }
+    dp[first_i][first_j > 1][1] = 1;
+
+    // Kより小さいことが......
+
+    for (int i = first_i; i < DIGIT_MAX - 1; i++) {
+        int d = digits[i];
+        int n = digits[i + 1];
+        assert(d < 10);
+        assert(n < 10);
+        // 未確定から未確定
+        dp[i + 1][false][n] += dp[i][false][d];
+        if (n == 1) dp[i + 1][false][n]++;
+
+        // 未確定から確定
+        for (int jn = 0; jn < 10; jn++) {
+            if (jn >= n) continue;
+            dp[i + 1][true][jn] += dp[i][false][d];
+            if (jn == 1) dp[i + 1][true][jn]++;
+        }
+
+        // 確定から確定
+        for (int jp = 0; jp < 10; jp++) {
+            for (int jn = 0; jn < 10; jn++) {
+                dp[i + 1][true][jn] += dp[i][true][jp];
+            }
+            dp[i + 1][true][1] += flow[i][true][jp];
+        }
+
+
+        dp[i + 1][true][1]++;
+    }
+
+    ll sum = 0;
+    for (int i = 0; i < 10; i++) {
+        sum += dp[DIGIT_MAX - 1][true][i];
+    }
+    for (int i = 0; i < 10; i++) {
+        sum += dp[DIGIT_MAX - 1][false][i];
+    }
+
+    return sum;
 }
 
 int main() {
-    ll i;
-    cin >> i;
-    ll ans = solve(i);
-    cout << ans << endl;
+    ll n;
+    cin >> n;
+
+    cout << digit_dp(n) << endl;
 }
