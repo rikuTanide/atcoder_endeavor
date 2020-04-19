2694bcf7c9721430ebd4b6a7a3defc07ec471448
riku <riku@tanide.net>
2020-04-19 17:38:36 +0900

    【WA】[AtCoder Regular Contest 043]B - 難易度

---
+++ arc043/arc043_b.cpp
@@ -0,0 +1,186 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+const int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
+
+class CumulativeSum {
+    vector<mint> numbers;
+    vector<mint> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, mint value) {
+        numbers[i] = value;
+    }
+
+    mint getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return 0;
+        return sums[i];
+    }
+
+    mint getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void calculate() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> numbers(n);
+    rep(i, n) cin >> numbers[i];
+
+    CumulativeSum dp1(n);
+    auto begin = numbers.begin();
+    auto end = numbers.end();
+
+    rep(i, n) {
+        ll x = numbers[i] * 2;
+        ll r = distance(upper_bound(begin, end, x), end);
+        auto er = equal_range(begin, end, x);
+        ll e = distance(er.first, er.second);
+        dp1.set(i, r + e);
+    }
+    dp1.calculate();
+
+    CumulativeSum dp2(n);
+    rep(i, n) {
+        ll x = numbers[i] * 2;
+        auto it = lower_bound(begin, end, x);
+        int index = distance(begin, it);
+        dp2.set(i, dp1.getSectionSum(index, n - 1));
+    }
+    dp2.calculate();
+    CumulativeSum dp3(n);
+    rep(i, n) {
+        ll x = numbers[i] * 2;
+        auto it = lower_bound(begin, end, x);
+        int index = distance(begin, it);
+        dp3.set(i, dp2.getSectionSum(index, n - 1));
+    }
+    dp3.calculate();
+    cout << dp3.getSum(n - 1) << endl;
+
+}
+
