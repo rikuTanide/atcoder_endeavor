81cee71a3b566ece16004358548a71d5a3f7861d
riku <riku@tanide.net>
2020-04-20 23:22:47 +0900

    【AC】[AtCoder Beginner Contest 036]D - 塗り絵

--- abc036/abc036_d.cpp
+++ abc036/abc036_d.cpp
@@ -1,81 +1,116 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 const int mod = 1000000007;
 
 struct mint {
     ll x; // typedef long long ll;
-    mint(ll x=0):x((x%mod+mod)%mod){}
-    mint& operator+=(const mint a) {
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
         if ((x += a.x) >= mod) x -= mod;
         return *this;
     }
-    mint& operator-=(const mint a) {
-        if ((x += mod-a.x) >= mod) x -= mod;
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
         return *this;
     }
-    mint& operator*=(const mint a) {
+
+    mint &operator*=(const mint a) {
         (x *= a.x) %= mod;
         return *this;
     }
+
     mint operator+(const mint a) const {
         mint res(*this);
-        return res+=a;
+        return res += a;
     }
+
     mint operator-(const mint a) const {
         mint res(*this);
-        return res-=a;
+        return res -= a;
     }
+
     mint operator*(const mint a) const {
         mint res(*this);
-        return res*=a;
+        return res *= a;
     }
+
     mint pow(ll t) const {
         if (!t) return 1;
-        mint a = pow(t>>1);
+        mint a = pow(t >> 1);
         a *= a;
-        if (t&1) a *= *this;
+        if (t & 1) a *= *this;
         return a;
     }
 
     // for prime mod
     mint inv() const {
-        return pow(mod-2);
+        return pow(mod - 2);
     }
-    mint& operator/=(const mint a) {
+
+    mint &operator/=(const mint a) {
         return (*this) *= a.inv();
     }
+
     mint operator/(const mint a) const {
         mint res(*this);
-        return res/=a;
+        return res /= a;
     }
-};
 
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
 
 
 struct Triangle {
@@ -84,7 +119,6 @@ struct Triangle {
 };
 
 class Tree {
-
     vector<vector<int>> edges;
 public:
     Tree(int n) : edges(n) {}
@@ -112,24 +146,17 @@ public:
             }
             vs.pop();
             leafs.push_back(triangle);
+
         }
         reverse(leafs.begin(), leafs.end());
+
     }
-
-};
-
-struct Ans {
-    mint black;
-    mint white;
 };
 
 int main() {
-
     int n;
     cin >> n;
-
     Tree tree(n);
-
     rep(i, n - 1) {
         int a, b;
         cin >> a >> b;
@@ -141,19 +168,25 @@ int main() {
     tree.root(0, leafs);
 
 
-    vector<Ans> dp(n);
-    for (Triangle &triangle : leafs) {
-        mint black = 1;
-        mint white = 1;
+    vector<vector<mint>> dp(n, vector<mint>(2, 1));
 
-        for (int c : triangle.children) {
-            black *= dp[c].white;
-            white *= (dp[c].black + dp[c].white);
+    // 0 黒
+    // 1 白
+    for (Triangle leaf : leafs) {
+        int parent = leaf.parent;
+
+        mint white = 1;
+        for (int child : leaf.children) {
+            white *= (dp[child][0] + dp[child][1]);
         }
-        dp[triangle.parent].white = white;
-        dp[triangle.parent].black = black;
+
+        mint black = 1;
+        for (int child : leaf.children) {
+            black *= dp[child][1];
+        }
+        dp[parent][1] = white;
+        dp[parent][0] = black;
     }
 
-    cout << (dp[0].white + dp[0].black).x << endl;
-
+    cout << dp[0][0] + dp[0][1] << endl;
 }
