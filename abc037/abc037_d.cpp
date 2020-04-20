43834808cb9a9b3ce4a443134893dce3ab0542ee
riku <riku@tanide.net>
2020-04-21 01:00:08 +0900

    【AC】[AtCoder Beginner Contest 037]D - 経路

--- abc037/abc037_d.cpp
+++ abc037/abc037_d.cpp
@@ -1,37 +1,47 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
 const int mod = 1000000007;
-
-struct Cell {
-    int x, y;
-    ll value;
-};
 
 struct mint {
     ll x; // typedef long long ll;
@@ -88,64 +98,83 @@ struct mint {
         mint res(*this);
         return res /= a;
     }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
 };
+
+
+mint dfs(vector<vector<int>> &edges, vector<mint> &count, vector<bool> &visited, vector<bool> &calculated, int to) {
+    if (visited[to]) {
+        if (!calculated[to]) __throw_runtime_error("heiro nai");
+        return count[to];
+    }
+    visited[to] = true;
+    mint now = 1;
+    for (int next : edges[to]) {
+        mint r = dfs(edges, count, visited, calculated, next);
+        now += r;
+    }
+    calculated[to] = true;
+    count[to] += now;
+    return count[to];
+}
 
 int main() {
     int h, w;
     cin >> h >> w;
 
-    vector<Cell> board(h * w);
-    rep(y, h) {
-        rep(x, w) {
-            ll a;
-            cin >> a;
-            board[y * w + x].x = x;
-            board[y * w + x].y = y;
-            board[y * w + x].value = a;
-        }
-    }
+    vector<vector<int>> grid(h, vector<int>(w, 0));
+    rep(y, h) rep(x, w) cin >> grid[y][x];
 
-    sort(board.begin(), board.end(), [](Cell &c, Cell &d) {
-        return c.value > d.value;
-    });
 
-    vector<vector<int>> ref(h, vector<int>(w, 0));
-    rep(i, h * w) {
-        ref[board[i].y][board[i].x] = i;
-    }
+    struct Direction {
+        int x, y;
+    };
 
-    vector<mint> scores(h * w, 1);
 
-    rep(i, h * w) {
+    auto reachable = [&](int x, int y, int nx, int ny) {
+        if (nx == -1 || nx == w || ny == -1 || ny == h) return false;
+        return grid[y][x] < grid[ny][nx];
+    };
 
-        auto f = [&](int y, int x, int value) {
-            if (y == -1) return;
-            if (y == h) return;
-            if (x == -1) return;
-            if (x == w) return;
+    vector<vector<int>> edges(h * w);
 
-            int index = ref[y][x];
-            if (board[index].value <= value) return;
-            scores[i] += scores[index];
-        };
+    vector<Direction> directions = {
+            {0,  1},
+            {1,  0},
+            {0,  -1},
+            {-1, 0},
+    };
 
-        Cell &cell = board[i];
-        int y = cell.y;
-        int x = cell.x;
-        int v = cell.value;
+    auto to_id = [&](int y, int x) {
+        return y * w + x;
+    };
 
-        f(y - 1, x, v);
-        f(y + 1, x, v);
-        f(y, x - 1, v);
-        f(y, x + 1, v);
-
-    }
+    rep(y, h) rep(x, w) for (Direction d : directions) {
+                int ny = y + d.y;
+                int nx = x + d.x;
+                if (!reachable(x, y, nx, ny)) continue;
+                edges[to_id(y, x)].push_back(to_id(ny, nx));
+            }
 
     mint ans = 0;
-    for (mint a : scores) {
-        ans += a;
+    vector<mint> count(edges.size(), 0);
+    vector<bool> visited(edges.size(), false);
+    vector<bool> calculated(edges.size(), false);
+    rep(i, edges.size()) {
+        mint t = dfs(edges, count, visited, calculated, i);
+        ans += t;
     }
-
-    cout << ans.x << endl;
-
+    cout << ans << endl;
 }
