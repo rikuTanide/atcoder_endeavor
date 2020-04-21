0b1b7ecb23cdcc6e9163a8e40ae781bbc7074534
riku <riku@tanide.net>
2020-04-21 17:13:08 +0900

    【AC】[AtCoder Beginner Contest 150]D - Semi Common Multiple

--- abc150/abc150_d.cpp
+++ abc150/abc150_d.cpp
@@ -1,33 +1,46 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
 
 ll gcd(ll x, ll y) {
     if (x > y) swap(x, y);
@@ -40,35 +53,63 @@ ll gcd(ll x, ll y) {
     return y;
 }
 
-ll lcd(ll x, ll y) {
+ll lcm(ll x, ll y) {
     return x / gcd(x, y) * y;
 }
 
-int main() {
+bool check2(vector<ll> &numbers, int n) {
+    vector<ll> tows(n);
+    rep(i, n) {
+        int a = 0;
+        ll k = numbers[i];
+        while (k % 2 == 0) {
+            a++;
+            k /= 2;
+        }
+        tows[i] = a;
+    }
+    return *max_element(tows.begin(), tows.end()) == *min_element(tows.begin(), tows.end());
+}
 
-    ll n, m;
+ll get2(vector<ll> &numbers, int n) {
+    ll k = numbers[0];
+    ll ans = 1;
+    while (k % 2 == 0) {
+        ans *= 2;
+        k /= 2;
+    }
+    return ans;
+}
+
+int main() {
+    int n, m;
     cin >> n >> m;
 
     vector<ll> numbers(n);
-    rep(i, n)cin >> numbers[i];
+    rep(i, n) cin >> numbers[i];
 
-
-    ll l = 1;
-    rep(i, n) {
-        l = lcd(l, numbers[i] / 2);
-    }
-
-    rep(i, n) if (l / (numbers[i] / 2) % 2 == 0) {
+    rep(i, n) if (numbers[i] % 2 == 1) {
             cout << 0 << endl;
-            return 0;
+            ret();
         }
 
-    ll nm = m - l;
-    if (nm < 0) {
+    rep(i, n) numbers[i] /= 2;
+
+    if (!check2(numbers, n)) {
         cout << 0 << endl;
-        return 0;
+        ret();
     }
 
-    cout <<  (m / l + 1) / 2 << endl;
+    ll tow = get2(numbers, n);
+    rep(i, n) numbers[i] /= tow;
+
+    ll l = numbers[0];
+    rep(i, n) l = lcm(l, numbers[i]);
+
+    ll md = m / tow / l;
+
+    ll ans = (md + 1) / 2;
+    cout << ans << endl;
+
 
 }
