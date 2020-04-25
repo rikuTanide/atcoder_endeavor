c2ab3d07747c5e936665b0753303d37eab4b1bd3
riku <riku@tanide.net>
2020-04-25 21:09:28 +0900

    【RE】[AtCoder Beginner Contest 006]D - トランプ挿入ソート

--- abc006/abc006_4.cpp
+++ abc006/abc006_4.cpp
@@ -1,60 +1,75 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<ll, int> P;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
-
-int check(vector<int> &numbers, int n) {
-    vector<int> dp(n + 1, INF);
-    dp[0] = -INF;
-
-    for (int j = 0; j < n; j++) {
-        auto it = lower_bound(dp.begin(), dp.end(), numbers[j]);
-        cmin(*it, numbers[j]);
-    }
-
-    int ans = 0;
-    for (int i = 1; i <= n; i++) {
-        if (dp[i] != INF) ans = i;
-    }
-    return ans;
-}
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-
     int n;
     cin >> n;
-    vector<int> numbers(n);
-    rep(i, n) cin >> numbers[i];
 
-    int ans = 0;
-    int now = check(numbers, n);
-    cmax(ans, now);
-    cout << n - ans << endl;
+    vector<int> cards(n);
+    rep(i, n) cin >> cards[i];
+    assert(n <= 16);
 
+    int all = 0;
+
+    rep(i, 1 << n) {
+        vector<int> tmp;
+        rep(j, n) {
+            if (!((i >> j) & 1)) continue;
+            tmp.push_back(cards[j]);
+        }
+        bool b = [&] {
+            rep(j, (int)tmp.size() - 1) {
+                if (tmp[j] < tmp[j + 1]) continue;
+                return false;
+            }
+            return true;
+        }();
+        if(b) {
+           cmax(all, (int)tmp.size());
+        }
+    }
+
+    cout << n - all << endl;
 
 }
+
