5cacca85362db33165dae4c402757292016ff777
riku <riku@tanide.net>
2020-04-27 20:46:48 +0900

    【RE】[AtCoder Beginner Contest 013]D - 阿弥陀

--- abc013/abc013_4.cpp
+++ abc013/abc013_4.cpp
@@ -1,71 +1,45 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
-
-class UnionFind {
-public:
-    // 親の番号を格納する。親だった場合-size
-    vector<int> parents;
-
-    UnionFind(int n) {
-        parents = vector<int>(n, -1);
-    }
-
-    // aがどのグループに属しているか
-    int root(int a) {
-        if (parents[a] < 0) {
-            return a;
-        }
-        return parents[a] = root(parents[a]);
-    }
-
-    int size(int a) {
-        return -parents[root(a)];
-    }
-
-    // aとbをくっつける
-    bool connect(int a, int b) {
-        int ra = root(a);
-        int rb = root(b);
-        if (ra == rb) {
-            return false;
-        }
-        // 大きいほうにA
-        if (size(ra) < size(rb)) {
-            swap(ra, rb);
-        }
-        parents[ra] += parents[rb];
-        parents[rb] = ra;
-        return true;
-    }
-
-};
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 vector<int> amida_simulator(vector<int> &horizontal, int width) {
     vector<int> currents(width);
@@ -85,61 +59,20 @@ vector<int> amida_simulator(vector<int> &horizontal, int width) {
     return current_indexes;
 }
 
-struct Doubling {
-    const int LOG;
-    vector<vector<int> > table;
-
-    Doubling(int sz, int64_t lim_t) : LOG(64 - __builtin_clzll(lim_t)) {
-        table.assign(LOG, vector<int>(sz, -1));
-    }
-
-    void set_next(int k, int x) {
-        table[0][k] = x;
-    }
-
-    void build() {
-        for (int k = 0; k + 1 < LOG; k++) {
-            for (int i = 0; i < table[k].size(); i++) {
-                if (table[k][i] == -1) table[k + 1][i] = -1;
-                else table[k + 1][i] = table[k][table[k][i]];
-            }
-        }
-    }
-
-    int query(int k, int64_t t) {
-        for (int i = LOG - 1; i >= 0; i--) {
-            if ((t >> i) & 1) k = table[i][k];
-        }
-        return k;
-    }
-};
-
 int main() {
-
-    int n, m, d;
+    int n, m;
+    ll d;
     cin >> n >> m >> d;
 
-//    assert(n <= 8);
+    assert(d == 1);
 
     vector<int> horizontal(m);
-    rep(i, m) {
-        int a;
-        cin >> a;
-        a--;
-        horizontal[i] = a;
-    }
-    vector<int> move_table = amida_simulator(horizontal, n);
+    rep(i, m) cin >> horizontal[i];
+    rep(i, m) horizontal[i]--;
 
-    UnionFind uf(n);
-    rep(i, n) uf.connect(i, move_table[i]);
+    auto res = amida_simulator(horizontal, n);
 
-    Doubling doubling(n, d);
-    rep(i, n) doubling.set_next(i, move_table[i]);
-    doubling.build();
-
-    rep(i, n) {
-        cout << doubling.query(i, d) + 1 << endl;
-    }
+    for (int i : res) cout << i + 1 << endl;
 
 
 }
