a0de0319cde95dd4414a69f961f5ca59ca2be077
riku <riku@tanide.net>
2020-04-27 12:17:13 +0900

    【AC】[AtCoder Regular Contest 026]C - 蛍光灯

---
+++ arc026/arc026_3.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+template<class T, class F>
+struct SegmentTree {
+    F f;
+    T ti;
+    vector<T> dat;
+    int sz;
+
+    SegmentTree(const F &f, const T &ti) : f(f), ti(ti) {}
+
+    void build(const vector<T> &v) {
+        assert(v.size());
+        sz = 1;
+        while (sz < v.size())sz <<= 1;
+        dat.resize(sz << 1, ti);
+        for (int i = 0; i < v.size(); i++)dat[sz - 1 + i] = v[i];
+        for (int i = sz - 2; i >= 0; i--)dat[i] = f(dat[i * 2 + 1], dat[i * 2 + 2]);
+    }
+
+    inline void update(int k, T x) {
+        k += sz - 1;
+        dat[k] = x;
+        while (k) {
+            k = (k - 1) / 2;
+            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
+        }
+    }
+
+    inline void add(int k, int x) {
+        k += sz - 1;
+        dat[k] = f(dat[k], x);
+        while (k) {
+            k = (k - 1) / 2;
+            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
+        }
+    }
+
+    inline T query(int a, int b) {
+        return query(a, b, 0, 0, sz);
+    }
+
+    T query(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l)return ti;
+        if (a <= l && r <= b)return dat[k];
+        return f(query(a, b, k * 2 + 1, l, (l + r) / 2), query(a, b, k * 2 + 2, (l + r) / 2, r));
+    }
+};
+
+
+struct Light {
+    int right, left;
+    ll cost;
+
+
+};
+
+std::istream &operator>>(std::istream &in, Light &o) {
+    cin >> o.left >> o.right >> o.cost;
+    return in;
+}
+
+int main() {
+    int n, l;
+    cin >> n >> l;
+
+
+    auto f = [](ll i, ll j) { return min(i, j); };
+    SegmentTree<ll, decltype(f)> segmentTree(f, INF);
+    vector<ll> dp(l + 1, INF);
+    dp[0] = 0;
+    segmentTree.build(dp);
+
+    vector<Light> lights(n);
+    rep(i, n) cin >> lights[i];
+    sort(lights.begin(), lights.end(), [&](Light &l1, Light &l2) {
+        return l1.left < l2.left;
+    });
+
+
+    for (Light light : lights) {
+        ll mi = segmentTree.query(light.left, light.right + 1);
+        ll next = min(segmentTree.query(light.right, light.right + 1), mi + light.cost);
+        segmentTree.update(light.right, next);
+    }
+    cout << segmentTree.query(l, l + 1) << endl;
+
+}
