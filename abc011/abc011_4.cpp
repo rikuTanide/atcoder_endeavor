b51ff6e36fdaeb4620bf8b17273850b1f3a65d87
riku <riku@tanide.net>
2020-04-27 11:30:20 +0900

    【AC】[AtCoder Beginner Contest 011]D - 大ジャンプ

--- abc011/abc011_4.cpp
+++ abc011/abc011_4.cpp
@@ -1,75 +1,117 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
 typedef long long ll;
-//typedef unsigned long long ll;
-
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
 const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+P get_count(ll lr, ll x) {
+    assert(x >= 0);
+    assert(lr >= x);
+
+    ll nokori = lr - x;
+    assert(nokori % 2 == 0);
+    ll add = nokori / 2;
+
+    ll r = x + add;
+    ll l = add;
+
+    return P(r, l);
+
+}
+
 
 int main() {
-    ll n, d;
-    cin >> n >> d;
-    ll x, y;
-    cin >> x >> y;
+    int n;
+    ll d, x, y;
+    cin >> n >> d >> x >> y;
 
-    if (x % d != 0) {
-        cout << 0 << endl;
-        return 0;
-    }
+    x = abs(x);
+    y = abs(y);
 
-    if (y % d != 0) {
+    if (x % d != 0 || y % d != 0) {
         cout << 0 << endl;
-        return 0;
+        ret();
     }
 
     x /= d;
     y /= d;
 
 
-    vector<double> logfact(1001, 0);
-    for (int i = 2; i <= 1000; ++i) {
-        logfact[i] = logfact[i - 1] + log2(i);
+    vector<vector<double>> pascal(1001);
+    pascal[0].push_back(1);
+    {
+        auto get = [&](int i, int j) {
+            if (i == -1 || j == -1) return 0.0;
+            if (pascal[i].size() <= j) return 0.0;
+            return pascal[i][j];
+        };
+        rep(i, 1001) {
+            if (i == 0) continue;
+            rep(j, i + 1) {
+                double a = get(i - 1, j - 1);
+                double b = get(i - 1, j);
+
+                double c = (a + b) / 2;
+                pascal[i].push_back(c);
+            }
+        }
     }
 
-    // https://atcoder.jp/contests/abc011/submissions/9638050
     double ans = 0;
-    for (ll right = 0; right <= n; right++) {
-        ll left = right - x;
-        ll vertical = n - right - left;
-        ll down = (vertical - y) / 2;
-        ll up = vertical - down;
-        if (left < 0 || left > n)continue;
-        if (right < 0 || right > n)continue;
-        if (up < 0 || up > n)continue;
-        if (down < 0 || down > n)continue;
-        if (up + down + left + right != n) continue;
-        if (up - down != y)continue;
-        if (right - left != x)continue;
+    rep(i, n + 1) {
+        int lr = i;
+        int ud = n - i;
 
-        double now = pow(2.0, (logfact[n] - logfact[up] - logfact[down] - logfact[left] - logfact[right] - 2.0 * n));
+        if (lr < x) continue;
+        if (ud < y) continue;
+        if ((lr - x) % 2 != 0) continue;
+        if ((ud - y) % 2 != 0) continue;
+
+        P lrc = get_count(lr, x);
+        P udc = get_count(ud, y);
+
+        double lrr = pascal[lr][lrc.first];
+        double udr = pascal[ud][udc.first];
+        double hvr = pascal[n][lr];
+
+        double now = lrr * udr * hvr;
         ans += now;
+
     }
+
     printf("%.20f\n", ans);
+
 }
