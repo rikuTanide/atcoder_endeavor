897dc8cff072638005f1e64974e8ae840f8aadfa
riku <riku@tanide.net>
2020-04-27 21:31:20 +0900

    【AC】[AtCoder Beginner Contest 013]D - 阿弥陀

--- abc013/abc013_4.cpp
+++ abc013/abc013_4.cpp
@@ -41,6 +41,37 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
+
+struct Doubling {
+    const int LOG;
+    vector<vector<int> > table;
+
+    Doubling(int sz, int64_t lim_t) : LOG(64 - __builtin_clzll(lim_t)) {
+        table.assign(LOG, vector<int>(sz, -1));
+    }
+
+    void set_next(int k, int x) {
+        table[0][k] = x;
+    }
+
+    void build() {
+        for (int k = 0; k + 1 < LOG; k++) {
+            for (int i = 0; i < table[k].size(); i++) {
+                if (table[k][i] == -1) table[k + 1][i] = -1;
+                else table[k + 1][i] = table[k][table[k][i]];
+            }
+        }
+    }
+
+    int query(int k, int64_t t) {
+        for (int i = LOG - 1; i >= 0; i--) {
+            if ((t >> i) & 1) k = table[i][k];
+        }
+        return k;
+    }
+};
+
+
 vector<int> amida_simulator(vector<int> &horizontal, int width) {
     vector<int> currents(width);
     vector<int> current_indexes(width);
@@ -59,110 +90,6 @@ vector<int> amida_simulator(vector<int> &horizontal, int width) {
     return current_indexes;
 }
 
-class UnionFind {
-public:
-    // 親の番号を格納する。親だった場合-size
-    vector<int> parents;
-
-    UnionFind(int n) {
-        parents = vector<int>(n, -1);
-    }
-
-    // aがどのグループに属しているか
-    int root(int a) {
-        if (parents[a] < 0) {
-            return a;
-        }
-        return parents[a] = root(parents[a]);
-    }
-
-    int size(int a) {
-        return -parents[root(a)];
-    }
-
-    // aとbをくっつける
-    bool connect(int a, int b) {
-        int ra = root(a);
-        int rb = root(b);
-        if (ra == rb) {
-            return false;
-        }
-        // 大きいほうにA
-        if (size(ra) < size(rb)) {
-            swap(ra, rb);
-        }
-        parents[ra] += parents[rb];
-        parents[rb] = ra;
-        return true;
-    }
-
-    bool is_union(int a, int b) {
-        int ra = root(a);
-        int rb = root(b);
-        return ra == rb;
-    }
-};
-
-
-struct Amida {
-    int n;
-    vector<int> horizontal;
-};
-
-vector<Amida> split_amida(int n, int m, vector<int> &horizontal) {
-    UnionFind uf(n);
-    for (int i : horizontal) {
-        uf.connect(i, i + 1);
-    }
-
-    map<int, vector<int>> vs;
-    for (int i : horizontal) {
-        int r = uf.root(i);
-        vs[r].push_back(i);
-    }
-
-    vector<Amida> res;
-    rep(i, n) {
-        if (uf.root(i) != i) continue;
-        Amida a = {uf.size(i), vs[i]};
-        res.push_back(a);
-    }
-    return res;
-}
-
-ll get_cycle(Amida &amida) {
-    int i = 1;
-    vector<int> table = amida_simulator(amida.horizontal, amida.n);
-
-    int now = table[0];
-    while (now != 0) {
-        now = table[now];
-        i++;
-    }
-
-    return i;
-}
-
-void solve(Amida &amida, ll d) {
-    vector<int> current(amida.n);
-    rep(i, amida.n) current[i] = i;
-    ll cycle = get_cycle(amida);
-    d %= cycle;
-
-    vector<int> table = amida_simulator(amida.horizontal, amida.n);
-
-    rep(i, d) {
-        vector<int> next(amida.n);
-        rep(j, amida.n) {
-            next[j] = current[table[j]];
-        }
-        current = next;
-    }
-
-    for (int i : current) cout << (i + 1) << endl;
-
-}
-
 int main() {
     int n, m;
     ll d;
@@ -171,11 +98,17 @@ int main() {
     rep(i, m) cin >> horizontal[i];
     rep(i, m) horizontal[i]--;
 
-    auto amidas = split_amida(n, m, horizontal);
+    vector<int> move_table = amida_simulator(horizontal, n);
 
-    for (Amida &a : amidas) {
-        solve(a, d);
+    Doubling doubling(n, d);
+    rep(i, n) doubling.set_next(i, move_table[i]);
+    doubling.build();
+
+
+    rep(i, n) {
+        cout << doubling.query(i, d) + 1 << endl;
     }
+
 
 
 }
