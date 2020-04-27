b1bfb0c9f419353d0dde2badf72edd77d697b6de
riku <riku@tanide.net>
2020-04-27 09:28:49 +0900

    【WA】[AtCoder Beginner Contest 010]D - 浮気予防

--- abc010/abc010_4.cpp
+++ abc010/abc010_4.cpp
@@ -38,7 +38,7 @@ const int mod = 1000000007;
 typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
 
-template< typename flow_t >
+template<typename flow_t>
 struct Dinic {
     const flow_t INF;
 
@@ -50,10 +50,10 @@ struct Dinic {
         int idx;
     };
 
-    vector< vector< edge > > graph;
-    vector< int > min_cost, iter;
+    vector<vector<edge> > graph;
+    vector<int> min_cost, iter;
 
-    Dinic(int V) : INF(numeric_limits< flow_t >::max()), graph(V) {}
+    Dinic(int V) : INF(numeric_limits<flow_t>::max()), graph(V) {}
 
     void add_edge(int from, int to, flow_t cap, int idx = -1) {
         graph[from].emplace_back((edge) {to, cap, (int) graph[to].size(), false, idx});
@@ -62,14 +62,14 @@ struct Dinic {
 
     bool bfs(int s, int t) {
         min_cost.assign(graph.size(), -1);
-        queue< int > que;
+        queue<int> que;
         min_cost[s] = 0;
         que.push(s);
-        while(!que.empty() && min_cost[t] == -1) {
+        while (!que.empty() && min_cost[t] == -1) {
             int p = que.front();
             que.pop();
-            for(auto &e : graph[p]) {
-                if(e.cap > 0 && min_cost[e.to] == -1) {
+            for (auto &e : graph[p]) {
+                if (e.cap > 0 && min_cost[e.to] == -1) {
                     min_cost[e.to] = min_cost[p] + 1;
                     que.push(e.to);
                 }
@@ -79,12 +79,12 @@ struct Dinic {
     }
 
     flow_t dfs(int idx, const int t, flow_t flow) {
-        if(idx == t) return flow;
-        for(int &i = iter[idx]; i < graph[idx].size(); i++) {
+        if (idx == t) return flow;
+        for (int &i = iter[idx]; i < graph[idx].size(); i++) {
             edge &e = graph[idx][i];
-            if(e.cap > 0 && min_cost[idx] < min_cost[e.to]) {
+            if (e.cap > 0 && min_cost[idx] < min_cost[e.to]) {
                 flow_t d = dfs(e.to, t, min(flow, e.cap));
-                if(d > 0) {
+                if (d > 0) {
                     e.cap -= d;
                     graph[e.to][e.rev].cap += d;
                     return d;
@@ -96,18 +96,18 @@ struct Dinic {
 
     flow_t max_flow(int s, int t) {
         flow_t flow = 0;
-        while(bfs(s, t)) {
+        while (bfs(s, t)) {
             iter.assign(graph.size(), 0);
             flow_t f = 0;
-            while((f = dfs(s, t, INF)) > 0) flow += f;
+            while ((f = dfs(s, t, INF)) > 0) flow += f;
         }
         return flow;
     }
 
     void output() {
-        for(int i = 0; i < graph.size(); i++) {
-            for(auto &e : graph[i]) {
-                if(e.isrev) continue;
+        for (int i = 0; i < graph.size(); i++) {
+            for (auto &e : graph[i]) {
+                if (e.isrev) continue;
                 auto &rev_e = graph[e.to][e.rev];
                 cout << i << "->" << e.to << " (flow: " << rev_e.cap << "/" << e.cap + rev_e.cap << ")" << endl;
             }
@@ -133,10 +133,10 @@ int main() {
     Dinic<ll> pd(n + 1);
 
     for (P p : edges) {
-        pd.add_edge(p.first, p.second, 1, 1);
+        pd.add_edge(p.first, p.second, 1);
     }
 
-    for (int i : targets) pd.add_edge(i, n, 1, 1);
+    for (int i : targets) pd.add_edge(i, n, 1);
     ll ans = pd.max_flow(0, n);
     cout << ans << endl;
 }
