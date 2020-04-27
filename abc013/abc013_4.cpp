826b8cd963df47e15df7e2501e5765b3571a2430
riku <riku@tanide.net>
2020-04-27 21:22:55 +0900

    【RE】[AtCoder Beginner Contest 013]D - 阿弥陀

--- abc013/abc013_4.cpp
+++ abc013/abc013_4.cpp
@@ -59,20 +59,123 @@ vector<int> amida_simulator(vector<int> &horizontal, int width) {
     return current_indexes;
 }
 
+class UnionFind {
+public:
+    // 親の番号を格納する。親だった場合-size
+    vector<int> parents;
+
+    UnionFind(int n) {
+        parents = vector<int>(n, -1);
+    }
+
+    // aがどのグループに属しているか
+    int root(int a) {
+        if (parents[a] < 0) {
+            return a;
+        }
+        return parents[a] = root(parents[a]);
+    }
+
+    int size(int a) {
+        return -parents[root(a)];
+    }
+
+    // aとbをくっつける
+    bool connect(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        if (ra == rb) {
+            return false;
+        }
+        // 大きいほうにA
+        if (size(ra) < size(rb)) {
+            swap(ra, rb);
+        }
+        parents[ra] += parents[rb];
+        parents[rb] = ra;
+        return true;
+    }
+
+    bool is_union(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        return ra == rb;
+    }
+};
+
+
+struct Amida {
+    int n;
+    vector<int> horizontal;
+};
+
+vector<Amida> split_amida(int n, int m, vector<int> &horizontal) {
+    UnionFind uf(n);
+    for (int i : horizontal) {
+        uf.connect(i, i + 1);
+    }
+
+    map<int, vector<int>> vs;
+    for (int i : horizontal) {
+        int r = uf.root(i);
+        vs[r].push_back(i);
+    }
+
+    vector<Amida> res;
+    rep(i, n) {
+        if (uf.root(i) != i) continue;
+        Amida a = {uf.size(i), vs[i]};
+        res.push_back(a);
+    }
+    return res;
+}
+
+ll get_cycle(Amida &amida) {
+    int i = 1;
+    vector<int> table = amida_simulator(amida.horizontal, amida.n);
+
+    int now = table[0];
+    while (now != 0) {
+        now = table[now];
+        i++;
+    }
+
+    return i;
+}
+
+void solve(Amida &amida, ll d) {
+    vector<int> current(amida.n);
+    rep(i, amida.n) current[i] = i;
+    ll cycle = get_cycle(amida);
+    d %= cycle;
+
+    vector<int> table = amida_simulator(amida.horizontal, amida.n);
+
+    rep(i, d) {
+        vector<int> next(amida.n);
+        rep(j, amida.n) {
+            next[j] = current[table[j]];
+        }
+        current = next;
+    }
+
+    for (int i : current) cout << (i + 1) << endl;
+
+}
+
 int main() {
     int n, m;
     ll d;
     cin >> n >> m >> d;
-
-    assert(d == 1);
-
     vector<int> horizontal(m);
     rep(i, m) cin >> horizontal[i];
     rep(i, m) horizontal[i]--;
 
-    auto res = amida_simulator(horizontal, n);
+    auto amidas = split_amida(n, m, horizontal);
 
-    for (int i : res) cout << i + 1 << endl;
+    for (Amida &a : amidas) {
+        solve(a, d);
+    }
 
 
 }
