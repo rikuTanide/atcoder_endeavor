0590027f09b5143897020f12fdbf449fc5d1df30
riku <riku@tanide.net>
2020-04-27 10:36:22 +0900

    【WA】[AtCoder Regular Contest 025]C - ウサギとカメ

---
+++ arc025/arc025_3.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+const int mod = 1000000007;
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+
+struct Edge {
+    ll to, cost;
+};
+
+
+class Dijkstra {
+    ll vector_count;
+
+    vector<ll> distances;
+    vector<vector<Edge>> edges;
+
+public:
+    Dijkstra(ll n) {
+        vector_count = n;
+        distances.resize(n, INF);
+        edges.resize(n);
+    }
+
+    void insertTowWay(ll from, ll to, ll cost) {
+        edges[from].push_back({to, cost});
+        edges[to].push_back({from, cost});
+    }
+
+    void insert(ll from, ll to, ll cost) {
+        edges[from].push_back({to, cost});
+    }
+
+    void dijkstra(ll k) {
+        distances = vector<ll>(vector_count, INF);
+        distances[k] = 0;  // 始点sへの最短距離は0
+
+        priority_queue<P, vector<P>, greater<P>> que;  // 距離が小さい順に取り出せるようgreater<P>を指定
+        que.push(P(0, k));
+        while (!que.empty()) {
+            P p = que.top();
+            que.pop();
+            int v = p.second;  // 更新した頂点の中で距離が最小の頂点v
+            if (distances[v] < p.first) {
+                continue;
+            }
+            for (auto e : edges[v]) {  // 頂点vから出る辺eを走査
+                if (distances[e.to] > distances[v] + e.cost) {
+                    distances[e.to] = distances[v] + e.cost;
+                    que.push(P(distances[e.to], e.to));
+                }
+            }
+        }
+    }
+
+    ll distance(ll to) {
+        return distances[to];
+    }
+
+};
+
+ll over(vector<ll> &v, ll x) {
+    auto begin = v.begin(), end = v.end();
+    return distance(upper_bound(begin, end, x), end);
+}
+
+int main() {
+    int n, m, r, t;
+    cin >> n >> m >> r >> t;
+
+    Dijkstra dijkstra(n);
+
+    rep(i, m) {
+        ll a, b, cost;
+        cin >> a >> b >> cost;
+        a--;
+        b--;
+        dijkstra.insertTowWay(a, b, cost);
+    }
+
+    ll ans = 0;
+    rep(i, n) {
+        dijkstra.dijkstra(i);
+
+        vector<ll> kame(n), usagi(n);
+        rep(j, n) kame[j] = dijkstra.distance(j) * r;
+        rep(j, n) usagi[j] = dijkstra.distance(j) * t;
+
+        sort(kame.begin(), kame.end());
+        sort(usagi.begin(), usagi.end());
+        ll now = 0;
+        for (ll k : kame) if(k != 0) now += over(usagi, k);
+        ans += now;
+    }
+    cout << ans << endl;
+}
