2b4bbd6a6a6e2ab75e3bbb5b9cea0c293188a6f1
riku <riku@tanide.net>
2020-04-29 00:43:52 +0900

    【AC】[AtCoder Beginner Contest 018]D - バレンタインデー

--- abc018/abc018_4.cpp
+++ abc018/abc018_4.cpp
@@ -1,34 +1,47 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-void recursive_comb(vector<int> &indexes, int s, int rest, std::function<void(vector<int> &)> &f) {
+void recursive_comb(vector<int> &indexes, int s, int rest, std::function<void(vector<int> &)> f) {
     if (rest == 0) {
         f(indexes);
     } else {
@@ -39,48 +52,51 @@ void recursive_comb(vector<int> &indexes, int s, int rest, std::function<void(ve
     }
 }
 
-void foreach_comb(int n, int k, std::function<void(vector<int> &)> f) {
-    vector<int> indexes(k);
+// nCkの組み合わせに対して処理を実行する
+void foreach_comb(int n, int k, function<void(vector<int> &)> f) {
+    vector<int> indexes(k, 0);
     recursive_comb(indexes, n - 1, k, f);
 }
 
-struct Barentain {
-    int boy;
-    int girl;
-    int happiness;
+struct Chocolate {
+    int from, to;
+    ll happy;
 };
 
-int main() {
+std::istream &operator>>(std::istream &in, Chocolate &o) {
+    cin >> o.from >> o.to >> o.happy;
+    o.from--;
+    o.to--;
+    return in;
+}
 
+int check(vector<int> &indexes, int q, int m, vector<vector<Chocolate>> &has) {
+    vector<int> mens(m);
+    for (int g: indexes) {
+        for (Chocolate &c : has[g]) {
+            mens[c.to] += c.happy;
+        }
+    }
+    sort(mens.rbegin(), mens.rend());
+    ll sum = accumulate(mens.begin(), mens.begin() + q, 0ll);
+    return sum;
+}
+
+int main() {
     int n, m, p, q, r;
+
     cin >> n >> m >> p >> q >> r;
 
-    vector<Barentain> barentains(r);
-    rep(i, r) {
-        int x, y, z;
-        cin >> x >> y >> z;
-        x--;
-        y--;
+    vector<Chocolate> chocoaltes(r);
+    rep(i, r) cin >> chocoaltes[i];
 
-        barentains[i].girl = x;
-        barentains[i].boy = y;
-        barentains[i].happiness = z;
-    }
+    vector<vector<Chocolate>> has(n);
+    for (Chocolate c : chocoaltes) has[c.from].push_back(c);
 
-    int ans = 0;
-
+    ll ans = 0;
     foreach_comb(n, p, [&](vector<int> &indexes) {
-        vector<int> boy_values(m);
-        for (Barentain &barentain: barentains) {
-            if (find(indexes.begin(), indexes.end(), barentain.girl) != indexes.end()) {
-                boy_values[barentain.boy] += barentain.happiness;
-            }
-        }
-        sort(boy_values.rbegin(), boy_values.rend());
-
-        int now = accumulate(boy_values.begin(), boy_values.begin() + q, 0);
+        ll now = check(indexes, q, m, has);
         cmax(ans, now);
     });
     cout << ans << endl;
 }
-
