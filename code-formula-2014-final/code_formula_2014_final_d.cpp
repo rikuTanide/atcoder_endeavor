2750583bca37e9fd38411e576da186b92f0514d7
riku <riku@tanide.net>
2020-04-28 15:45:35 +0900

    【AC】[Code Formula 2014 本選]D - 映画の連続視聴

--- code-formula-2014-final/code_formula_2014_final_d.cpp
+++ code-formula-2014-final/code_formula_2014_final_d.cpp
@@ -101,10 +101,10 @@ int main() {
         vector<Movie> ms(n);
         rep(i, n) cin >> ms[i].type >> ms[i].start >> ms[i].end;
         sort(ms.begin(), ms.end(), [](Movie &m1, Movie &m2) {
-            if (m1.start == m2.start) {
-                return m1.end < m2.end;
+            if (m1.end == m2.end) {
+                return m1.start > m2.start;
             }
-            return m1.start < m2.start;
+            return m1.end < m2.end;
         });
 
         for (Movie &m : ms) {
