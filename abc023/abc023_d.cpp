e1c47748d3632f0964e4060b0f841041cef1b0c2
riku <riku@tanide.net>
2020-04-29 17:07:01 +0900

    【AC】[AtCoder Beginner Contest 023]D - 射撃王

--- abc023/abc023_d.cpp
+++ abc023/abc023_d.cpp
@@ -1,85 +1,83 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 struct Balloon {
-    ll initial;
-    ll speed;
+    ll ini, speed;
 };
 
-ll check(Balloon balloon, ll score) {
-    if ((score - balloon.initial) < 0) return -1;
-    ll t = (score - balloon.initial) / balloon.speed;
-    return t;
+std::istream &operator>>(std::istream &in, Balloon &o) {
+    cin >> o.ini >> o.speed;
+    return in;
 }
 
-bool check_all(vector<Balloon> &balloons, ll score) {
-    vector<ll> times(balloons.size());
-    for (ll i = 0; i < balloons.size(); i++) {
-        Balloon balloon = balloons[i];
-        ll time = check(balloon, score);
-        if (time < 0) return false;
-        times[i] = time;
+bool check(vector<Balloon> &v, ll mid, int n) {
+    vector<ll> limits;
+    for (Balloon &b : v) {
+        ll t = mid - b.ini;
+        if (t < 0) return false;
+        ll limit = t / b.speed;
+        limits.push_back(limit);
     }
-    sort(times.begin(), times.end());
-    for (ll i = 0; i < balloons.size(); i++) {
-        if (times[i] < i) return false;
+    sort(limits.begin(), limits.end());
+    rep(i, n) {
+        if (limits[i] < i) return false;
     }
     return true;
 }
 
 int main() {
-    ll n;
+    int n;
     cin >> n;
 
-    vector<Balloon> balloons(n);
-    rep(i, n) {
-        cin >> balloons[i].initial;
-        cin >> balloons[i].speed;
-    }
+    vector<Balloon> v(n);
+    rep(i, n) cin >> v[i];
 
-    ll floor = 0;
-    ll ceil = INF;
 
+    ll floor = 0, ceil = INF;
     while (floor + 1 < ceil) {
         ll mid = (floor + ceil) / 2;
-        bool b = check_all(balloons, mid);
-
-        if (b) {
-            ceil = mid;
-        } else {
-            floor = mid;
-        }
+        bool b = check(v, mid, n);
+        if (b) ceil = mid;
+        else floor = mid;
     }
-
     cout << ceil << endl;
-
-
 }
