e2bb7e6e1e06adffc061d99c4a79bb798d42e21c
riku <riku@tanide.net>
2020-04-30 23:09:52 +0900

    【RE】[AtCoder Beginner Contest 030]D - へんてこ辞書

--- abc030/abc030_d.cpp
+++ abc030/abc030_d.cpp
@@ -1,82 +1,106 @@
 #include <bits/stdc++.h>
-#include <boost/multiprecision/cpp_int.hpp>
-#include <cmath>
 
 using namespace std;
-//typedef long long ll;
-typedef unsigned long long ll;
-namespace mp = boost::multiprecision;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+typedef pair<int, int> P;
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int cycle_first(int a, vector<ll> &dict) {
+    int i = 0;
+    set<int> used;
+    int now = a;
+    while (!contain(used, now)) {
+        used.insert(now);
+        now = dict[now];
+        i++;
+    }
+    return now;
+}
+
+int get_first(int target, int a, vector<ll> &dict) {
+    int i = 0;
+    int now = a;
+    while (now != target) {
+        now = dict[now];
+        i++;
+    }
+    return i;
+}
+
+int get_cycle_len(int target, vector<ll> &dict) {
+    int i = 0;
+    int now = dict[target];
+    while (now != target) {
+        now = dict[now];
+        i++;
+    }
+    return i + 1;
+}
+
+int count_ans(int k, int a, vector<ll> &dict) {
+    int ans = a;
+    for (int j = 0; j < k; j++) {
+        ans = dict[ans];
+    }
+    return ans + 1;
+}
 
 int main() {
-    ll n, a;
-    mp::cpp_int k;
+    int n, a;
+    ll k;
     cin >> n >> a >> k;
+
     a--;
+    vector<ll> dict(n);
+    rep(i, n) cin >> dict[i], dict[i]--;
+    int first_position = cycle_first(a, dict);
+    int first = get_first(first_position, a, dict);
+    int cycle_len = get_cycle_len(first_position, dict);
 
-    vector<ll> references(n);
-    vector<ll> position(n, 0);
-    vector<bool> visited(n, false);
-    rep(i, n) cin >> references[i];
-    rep(i, n) references[i]--;
-    ll now = a;
-    ll count = 0;
-
-    while (true) {
-        if (visited[now]) break;
-        position[count] = references[now];
-        visited[now] = true;
-        count++;
-        now = references[now];
+    if (first >= k) {
+        cout << count_ans(k, a, dict) << endl;
+        ret();
     }
 
-    count--;
+    k -= first;
+    k %= cycle_len;
 
-    ll loop_start = now;
-    ll loop_length = 0;
-    vector<ll> loop_position(n, 0);
-    while (true) {
-        loop_position[loop_length] = now;
-        loop_length++;
-        if (references[now] == loop_start) {
-            break;
-        }
-        now = references[now];
-    }
-
-    if (k <= count) {
-        ll ik = static_cast<ll>(k);
-        cout << position[ik - 1] + 1 << endl;
-    } else {
-        mp::cpp_int kk = k - count;
-        kk--;
-        kk %= loop_length;
-        ll ik = static_cast<ll>(kk);
-
-        cout << loop_position[ik] + 1 << endl;
-    }
+    cout << count_ans(k, first_position, dict) << endl;
 
 }
