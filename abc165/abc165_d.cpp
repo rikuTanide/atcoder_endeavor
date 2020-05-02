ce915bc8872a914b9973dfa0ac4ca87e4967810d
riku <riku@tanide.net>
2020-05-02 21:51:19 +0900

    【WA】[AtCoder Beginner Contest 165]D - Floor Function

---
+++ abc165/abc165_d.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+typedef pair<ll, ll> P;
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+
+int main() {
+    ll a, b, n;
+    cin >> a >> b >> n;
+    ll ma = max(0ll, n - a);
+    ll ans = 0;
+    for (ll x = n; x >= ma; x--) {
+        ll now = (a * x / b) - a * (x / b);
+        cmax(ans, now);
+    }
+    cout << ans << endl;
+
+
+}
+
