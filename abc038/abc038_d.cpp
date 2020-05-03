4ececa896de54ae0e80018d2f238bc135d12932a
riku <riku@tanide.net>
2020-05-03 12:56:56 +0900

    【TLE】[AtCoder Beginner Contest 038]D - プレゼント

--- abc038/abc038_d.cpp
+++ abc038/abc038_d.cpp
@@ -1,85 +1,80 @@
 #include <bits/stdc++.h>
-#include <cmath>
+
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX / 10;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+typedef pair<ll, ll> P;
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
-
-struct Bit {
-    int64_t size;
-    vector<int64_t> v;
-
-    Bit(int64_t n) : size(n + 1), v(size) {}
-
-    void update(int64_t a, int64_t w) {
-        for (int64_t x = a; x <= size; x += x & -x) v[x] = max(w, v[x]);
-    }
-
-    int query(int64_t a) {
-        int64_t res = 0;
-        for (int64_t x = a; x > 0; x -= x & -x) res = max(res, v[x]);
-        return res;
-    }
-};
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 struct Box {
-    int width, height;
+    int w, h;
 };
+
+std::istream &operator>>(std::istream &in, Box &o) {
+    cin >> o.w >> o.h;
+    return in;
+}
+
+bool in(Box &b1, Box &b2) {
+    if (b1.w > b2.w && b1.h > b2.h) return true;
+    return false;
+}
+
+int rec(int i, int n, vector<Box> &boxes, vector<int> &dp) {
+    if (dp[i] != -1) return dp[i];
+    int now = 0;
+    rep(j, n) {
+        if (!in(boxes[i], boxes[j])) continue;
+        cmax(now, rec(j, n, boxes, dp));
+    }
+    dp[i] = now + 1;
+    return now + 1;
+}
 
 int main() {
     int n;
     cin >> n;
-
-
     vector<Box> boxes(n);
-    rep(i, n) {
-        int w, h;
-        cin >> w >> h;
+    rep(i, n) cin >> boxes[i];
 
-        boxes[i].width = w;
-        boxes[i].height = h;
-    }
+    vector<int> dp(n, -1);
+    rep(i, n) dp[i] = rec(i, n, boxes, dp);
 
-    sort(boxes.begin(), boxes.end(), [](Box &b, Box &c) {
-        if (b.width == c.width) {
-            return b.height > c.height;
-        }
-        return b.width < c.width;
-    });
-
-    int max_h = accumulate(boxes.begin(), boxes.end(), 0, [](int m, Box &c) {
-        return max(m, c.height);
-    });
-
-    Bit bit(max_h);
-
-    for (Box &box : boxes) {
-        bit.update(box.height, bit.query(box.height - 1) + 1);
-    }
-
-    cout << bit.query(max_h) << endl;
-
+    cout << *max_element(dp.begin(), dp.end()) << endl;
 }
