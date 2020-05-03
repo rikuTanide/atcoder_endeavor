bf9dd3687089eb7f6a7bb4ce9ea3b5e973f11a75
riku <riku@tanide.net>
2020-05-03 21:06:27 +0900

    【AC】[AtCoder Beginner Contest 166]C - Peaks

---
+++ abc166/abc166_c.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+typedef pair<ll, ll> P;
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<ll> castels(n);
+    rep(i, n) cin >> castels[i];
+
+    vector<vector<ll>> edges(n);
+
+    rep(i, m) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+
+        edges[a].push_back(b);
+        edges[b].push_back(a);
+    }
+    int ans = 0;
+    rep(i, n) {
+        bool b = [&]() {
+            for (int j  : edges[i]) {
+                if (castels[i] <= castels[j]) return false;
+            }
+            return true;
+        }();
+        if (b) ans++;
+
+    }
+    cout << ans << endl;
+
+}
