b40c3f934fd77185aee18995b2a8b14378d9ca02
riku <riku@tanide.net>
2020-05-05 22:46:40 +0900

    【WA】[AtCoder Beginner Contest 048]D - An Ordinary Game

--- abc048/arc064_b.cpp
+++ abc048/arc064_b.cpp
@@ -1,42 +1,59 @@
 #include <bits/stdc++.h>
-#include <cmath>
+
 
 using namespace std;
-//typedef long long ll;
-typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-const int INF = INT_MAX;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+typedef pair<ll, ll> P;
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 
 int main() {
     string s;
     cin >> s;
 
-    int l = s.size() % 2 == 0;
-    int se = s.front() == s.back();
-    int ans = !((l) xor (se));
+    bool rt = s.front() == s.back();
+    bool gs = s.size() % 2 == 0;
 
-    cout << (ans ? "First" : "Second") << endl;
+    if (rt ^ gs) {
+        cout << "First" << endl;
+    } else {
+        cout << "Second" << endl;
+    }
 
 }
