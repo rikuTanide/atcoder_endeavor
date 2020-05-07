1414e1b696281f9fbe9b64815eded9b10416ba9c
riku <riku@tanide.net>
2020-05-07 20:37:16 +0900

    【WA】[AtCoder Beginner Contest 061]D - Score Attack

--- abc061/abc061_d.cpp
+++ abc061/abc061_d.cpp
@@ -1,94 +1,83 @@
 #include <bits/stdc++.h>
-#include <cmath>
-
-#include <assert.h>    // LON
-#include <math.h>    // sqrt()
 
 
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
-//ofstream outfile("log.txt");
-//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
-// std::cout << std::bitset<8>(9);
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+typedef pair<ll, ll> P;
 
-// https://qiita.com/wakimiko/items/69b86627bea0e8fe29d5
 struct Edge {
-    ll from; //出発点
-    ll to;   //到達点
-    ll cost; //移動コスト
+    ll to, cost;
 };
 
-ll bellmanFord(ll v, ll s, ll g, vector<Edge> &routes) {
+bool bellman_ford(int n, int s, vector<vector<Edge> > &graph, vector<ll> &dist) { // nは頂点数、sは開始頂点
 
-    vector<ll> costs(v, INF);
-    vector<bool> hasLoop(v, false);
-    costs[s] = 0;
-
-    for (ll i = 0; i < v - 1; i++) {
-        for (ll j = 0; j < (ll) routes.size(); j++) {
-            Edge e = routes[j];
-            if(costs[e.from] == INF) continue;
-            if (costs[e.to] > costs[e.from] + e.cost) {  //移動した後のコストが小さいと、頂点のコストを更新
-                costs[e.to] = costs[e.from] + e.cost;
+    dist[s] = 0; // 開始点の距離は0
+    for (int i = 0; i < n; i++) {
+        for (int v = 0; v < n; v++) {
+            for (int k = 0; k < graph[v].size(); k++) {
+                Edge e = graph[v][k];
+                if (dist[v] != INF && dist[e.to] > dist[v] + e.cost) {
+                    dist[e.to] = dist[v] + e.cost;
+                    if (i == n - 1) return true; // n回目にも更新があるなら負の閉路が存在
+                }
             }
         }
     }
-    for (ll j = 0; j < v; j++) {
-        for (ll i = 0; i < (ll) routes.size(); i++) {
-            ll f = routes[i].from;
-            ll t = routes[i].to;
-            ll c = routes[i].cost;
-            if (costs[f] == INF) continue;
-            if (costs[f] + c < costs[t]) hasLoop[t] = true;
-            if (hasLoop[f])hasLoop[t] = true;
-        }
-    }
-    if (hasLoop[g] == true) return MINF;
-    return costs[g];
+    return false;
 }
 
+
 int main() {
-    ll n, m;
+    int n, m;
     cin >> n >> m;
 
-    vector<Edge> routes(m);
+    vector<vector<Edge>> graph(n);
 
     rep(i, m) {
-        ll a, b, c;
+        int a, b;
+        ll c;
         cin >> a >> b >> c;
         a--;
         b--;
         c = -c;
-        Edge edge = {a, b, c};
-        routes[i] = edge;
+        graph[a].push_back({b, c});
     }
 
-    ll ans = bellmanFord(n, 0, n - 1, routes);
-
-    if (ans == MINF) {
+    vector<ll> dist(n, INF);
+    bool b = bellman_ford(n, 0, graph, dist);
+    if (b) {
         cout << "inf" << endl;
-        return 0;
+        ret();
     }
+    ll mi = dist[n - 1];
+    cout << -mi << endl;
 
-    cout << -ans << endl;
 }
-
