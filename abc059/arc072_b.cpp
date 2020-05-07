8e16bc0b1418a150a16748bce44a86486094fdc5
riku <riku@tanide.net>
2020-05-07 18:32:24 +0900

    【RE】[AtCoder Beginner Contest 059]D - Alice&Brown

--- abc059/arc072_b.cpp
+++ abc059/arc072_b.cpp
@@ -1,81 +1,57 @@
 #include <bits/stdc++.h>
-#include <cmath>
-
-#include <assert.h>    // LON
-#include <math.h>    // sqrt()
 
 
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
-//ofstream outfile("log.txt");
-//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
-// std::cout << std::bitset<8>(9);
-
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
-
-
-bool recursive(ll x, ll y, bool turn, ll depth) {
-
-    bool win = false;
-
-    for (ll k = 2; k <= x; k += 2) {
-        bool b = !recursive(x - k, y + (k / 2), !turn, depth + 1);
-        if (b) win = true;
-    }
-    for (ll k = 2; k <= y; k += 2) {
-        bool b = !recursive(x + (k / 2), y - k, !turn, depth + 1);
-        if (b) win = true;
-    }
-
-    return win;
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
 }
 
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+typedef pair<ll, ll> P;
+
+bool rec(ll x, ll y, bool b) {
+    for (int i = 1; 2 * i < x; i++) {
+        bool d = rec(x - (2 * i), y, !b);
+        if (b == d) return b;
+    }
+    for (int i = 1; 2 * i < y; i++) {
+        bool d = rec(x, y - (2 * i), !b);
+        if (b == d) return b;
+    }
+    return !b;
+}
 
 int main() {
-
     ll x, y;
     cin >> x >> y;
-
-//    for (ll x = 0; x < 10; x++) {
-//        for (ll y = 0; y < 10; y++) {
-//            bool b;
-//    if (abs(x - y) <= 1) {
-//        b = false;
-//    } else {
-//        b = true;
-//    }
-//
-//            cout << ' ' << b ;
-//
-//        }
-//        cout << endl;
-//    }
-
-//    bool b = recursive(x, y, true, 0);
-//    cout << (b ? "Alice" : "Brown") << endl;
-
-    if (abs(x - y) <= 1) {
-        cout << "Brown" << endl;
-    } else {
+    bool b = rec(x, y, true);
+    if (b) {
         cout << "Alice" << endl;
+    } else {
+        cout << "Brown" << endl;
     }
-
 }
-
