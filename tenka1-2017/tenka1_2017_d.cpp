fa97059ff0153f32df9988cfe12d8f5850a7ce5b
riku <riku@tanide.net>
2020-05-09 00:07:15 +0900

    【WA】[Tenka1 Programmer Contest]D - IntegerotS

---
+++ tenka1-2017/tenka1_2017_d.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+
+int main() {
+    ll n, k;
+    cin >> n >> k;
+
+    vector<ll> numbers(n), values(n);
+    rep(i, n) cin >> numbers[i] >> values[i];
+
+    vector<ll> candidate;
+    {
+        ll sum = 0;
+        rep(i, n) {
+            if ((k | numbers[i]) == k) sum += values[i];
+        }
+        candidate.push_back(sum);
+    }
+
+    rep(i, 35) {
+        bool d = (k >> i) & 1;
+        if (!d) continue;
+
+        ll sum = 0;
+
+        ll target = k >> (i + 1);
+        rep(j, n) {
+            ll l = numbers[j];
+            bool b = ((l >> i) & 1);
+            if (b) continue;
+            ll c = (l >> (i + 1)) & 1;
+            if ((target | c) != target) continue;
+            sum += values[j];
+        }
+        candidate.push_back(sum);
+    }
+
+    // 0
+
+    cout << *max_element(candidate.begin(), candidate.end()) << endl;
+}
+
+
