29362aedc7ef6eaf32c19b0aa5123402a810d51e
riku <riku@tanide.net>
2020-05-10 03:21:43 +0900

    【WA】[AtCoder Beginner Contest 093]D - Worst Case

--- abc093/arc094_b.cpp
+++ abc093/arc094_b.cpp
@@ -8,7 +8,7 @@ typedef pair<ll, ll> P;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-const ll INF = 10e10;
+const ll INF = 10e11;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
