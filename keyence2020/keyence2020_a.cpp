85236b8644cec8ee1cde3ae0aa0b5f0b90645d3a
riku <riku@tanide.net>
2020-05-13 00:57:30 +0900

    【AC】[キーエンス プログラミング コンテスト 2020]A - Painting

--- keyence2020/keyence2020_a.cpp
+++ keyence2020/keyence2020_a.cpp
@@ -1,7 +1,52 @@
-C:\Users\riku\CLionProjects\apg4b\cmake-build-debug\apg4b.exe
-2
-100
-200
-2
+#include <bits/stdc++.h>
 
-Process finished with exit code 0
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<double, double> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int h, w, n;
+    cin >> h >> w >> n;
+    if (h < w) swap(h, w);
+
+    int ans = (n + h - 1) / h;
+    cout << ans << endl;
+
+}
