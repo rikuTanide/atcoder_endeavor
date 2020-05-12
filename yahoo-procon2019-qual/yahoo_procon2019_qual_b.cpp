ad66d477ddb683d80c3f10ad793df0a94ded650b
riku <riku@tanide.net>
2020-05-12 23:15:37 +0900

    【AC】[「みんなのプロコン 2019」]B - Path

---
+++ yahoo-procon2019-qual/yahoo_procon2019_qual_b.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<double, double> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    vector<int> v(4);
+    rep(i, 3) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        v[a]++;
+        v[b]++;
+    }
+    sort(v.begin(), v.end());
+    vector<int> t = {1, 1, 2, 2,};
+    bool b = v == t;
+    string ans = b ? "YES" : "NO";
+    cout << ans << endl;
+}
