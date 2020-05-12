9f5c36f3f5023cd81be5547b5dabca5edbc4358d
riku <riku@tanide.net>
2020-05-12 12:18:32 +0900

    【AC】[AtCoder Regular Contest 007]B - 迷子のCDケース

---
+++ arc007/arc007_2.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<double, double> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> cd(n, 0);
+    rep(i, n)cd[i] = i + 1;
+    int now = 0;
+    rep(i, m) {
+        int k;
+        cin >> k;
+        if (now == k) continue;
+        int tmp = now;
+        auto it = find(cd.begin(), cd.end(), k);
+        (*it) = tmp;
+        now = k;
+    }
+
+    for (int c : cd) cout << c << endl;
+
+}
