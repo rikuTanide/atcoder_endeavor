ce0211181d7338d3fef81d86e1d4635025e4d9cb
riku <riku@tanide.net>
2020-05-12 22:25:56 +0900

    【AC】[第5回 ドワンゴからの挑戦状 予選]A - Thumbnail

---
+++ dwacon5th-prelims/dwacon5th_prelims_a.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<double, double> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<double> v(n);
+    rep(i, n) cin >> v[i];
+    double ave = accumulate(v.begin(), v.end(), 0.0) / n;
+    rep(i, n) v[i] = abs(v[i] - ave);
+    double target = *min_element(v.begin(), v.end());
+    int f = distance(v.begin(), find(v.begin(), v.end(), target));
+    cout << f << endl;
+
+}
