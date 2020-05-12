ae1d07f5ddd2f59c14cbf0a5fca9b2ffad4eed60
riku <riku@tanide.net>
2020-05-12 22:21:00 +0900

    【AC】[Tenka1 Programmer Beginner Contest]B - Exchange

---
+++ tenka1-2018-beginner/tenka1_2018_b.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<double, double> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    ll a, b;
+    int k;
+    cin >> a >> b >> k;
+
+    rep(i, k) {
+        if (i % 2 == 0) {
+            if (a % 2 == 1) a--;
+            a /= 2;
+            b += a;
+        } else {
+            if (b % 2 == 1) b--;
+            b /= 2;
+            a += b;
+        }
+    }
+
+    cout << a << ' ' << b << endl;
+}
