63c7ad55590b68f6c2cd49dfbd51ee9fb6a6e3cb
riku <riku@tanide.net>
2020-05-12 14:18:19 +0900

    【AC】[AtCoder Grand Contest 004]A - Divide a Cuboid

---
+++ agc004/agc004_a.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<double, double> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+
+int main() {
+    ll a, b, c;
+    cin >> a >> b >> c;
+
+    if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+        cout << 0 << endl;
+        ret();
+    }
+
+    vector<ll> abc = {a, b, c};
+    sort(abc.begin(), abc.end());
+    ll k = abc[0] * abc[1] * (abc[2] / 2);
+    ll l = a * b * c - k;
+
+    ll now = abs(l - k);
+    cout << now << endl;
+
+}
