f4ef93c243c27adfb2260e1b66ef54c1e74be99c
riku <riku@tanide.net>
2020-05-13 23:53:00 +0900

    【AC】[AtCoder Beginner Contest 104]D - We Love ABC

--- abc104/abc104_d.cpp
+++ abc104/abc104_d.cpp
@@ -1,27 +1,46 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-typedef long long ll;
-typedef pair<int, int> P;
-//typedef pair<ll, ll> P;
-const double INF = 1e10;
-//const ll INF = 10e10;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
 
-typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-const int mod = 1000000007;
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<double, double> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-typedef tuple<int, int, int> Q;
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+const int mod = 1000000007;
 
 struct mint {
     ll x; // typedef long long ll;
@@ -78,40 +97,39 @@ struct mint {
         mint res(*this);
         return res /= a;
     }
-};
 
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
 
 int main() {
     string s;
     cin >> s;
-    vector<vector<mint>> dp(s.size() + 1, vector<mint>(4, 0));
-    dp[0][0] = 1;
-    for (int i = 0; i < s.size(); i++) {
 
-        if (s[i] == 'A') {
-            dp[i + 1][1] += dp[i][0];
-        } else if (s[i] == 'B') {
-            dp[i + 1][2] += dp[i][1];
-        } else if (s[i] == 'C') {
-            dp[i + 1][3] += dp[i][2];
-        } else {
-            for (int j = 0; j < 3; j++) {
-                dp[i + 1][j + 1] += dp[i][j];
+    int n = s.size();
+    vector<vector<mint>> dp(n + 1, vector<mint>(4, -1));
+
+    for (int i = n; i >= 0; i--) {
+        for (int j = 3; j >= 0; j--) {
+            if (i == n) {
+                dp[i][j] = (j == 3 ? 1 : 0);
+            } else {
+                dp[i][j] = dp[i + 1][j] * (s[i] == '?' ? 3 : 1);
+                if (j < 3 && (s[i] == '?' || s[i] == "ABC"[j])) {
+                    dp[i][j] += dp[i + 1][j + 1];
+                }
             }
         }
-
-        if (s[i] == '?') {
-            for (int j = 0; j < 4; j++) {
-                dp[i + 1][j] += dp[i][j] * 3;
-            }
-        } else {
-            for (int j = 0; j < 4; j++) {
-                dp[i + 1][j] += dp[i][j];
-            }
-        }
-
     }
-
-    cout << dp[s.size()][3].x << endl;
-
+    cout << dp.front().front() << endl;
 }
