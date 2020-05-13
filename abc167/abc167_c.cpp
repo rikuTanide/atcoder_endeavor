57fc79d90c7a7edfd4c28483c1ad8a5a66142977
riku <riku@tanide.net>
2020-05-13 17:01:39 +0900

    【WA】[AtCoder Beginner Contest 167]C - Skill Up

--- abc167/abc167_c.cpp
+++ abc167/abc167_c.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 const double PI = 3.14159265358979323846;
 typedef long long ll;
-typedef pair<ll, ll> P;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-const ll INF = 10e14;
+//typedef pair<ll, ll> P;
+typedef pair<double, double> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
@@ -31,50 +32,47 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+
 struct Book {
-    int value;
-    vector<int> skills;
+    ll price;
+    vector<ll> points;
 };
 
+
 int main() {
-    int n, m, x;
+    ll n, m, x;
     cin >> n >> m >> x;
 
     vector<Book> books(n);
     rep(i, n) {
-
-        int value;
-        cin >> value;
-
-        books[i].value = value;
-        books[i].skills = vector<int>(m);
-
-        rep(j, m) {
-            cin >> books[i].skills[j];
-        }
-
+        cin >> books[i].price;
+        books[i].points = vector<ll>(m);
+        rep(j, m) cin >> books[i].points[j];
     }
+
     ll ans = INF;
     rep(i, 1 << n) {
-        vector<int> tmp(m);
-        ll sum = 0;
+        vector<ll> points(m);
+        ll price = 0;
         rep(j, n) {
-            if (!((i >> j) & 1))continue;
-            Book b = books[j];
-            sum += b.value;
-
-            rep(k, m) tmp[k] += b.skills[k];
+            bool b = (i >> j) & 1;
+            if (!b) continue;
+            rep(k, m) points[k] += books[j].points[k];
+            price += books[j].price;
         }
-        ll mi = *min_element(tmp.begin(), tmp.end());
-        if (mi < x) continue;
-        cmin(ans, sum);
-    }
-    if (ans == INF) {
-        cout << -1 << endl;
-        ret();
+
+        ll p = *min_element(points.begin(), points.end());
+        if (p < x) continue;
+        cmin(ans, price);
     }
     cout << ans << endl;
 }
-
-
-
