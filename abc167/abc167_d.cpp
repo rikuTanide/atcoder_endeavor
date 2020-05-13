b89ccaf0760b4d4384ff85abd57aa8f761604101
riku <riku@tanide.net>
2020-05-13 17:07:47 +0900

    【AC】[AtCoder Beginner Contest 167]D - Teleporter

--- abc167/abc167_d.cpp
+++ abc167/abc167_d.cpp
@@ -8,7 +8,7 @@ const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
-typedef pair<ll, ll> P;
+typedef pair<double, double> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
@@ -41,7 +41,6 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-
 struct Doubling {
     const int LOG;
     vector<vector<int> > table;
@@ -71,39 +70,17 @@ struct Doubling {
     }
 };
 
-
-vector<int> amida_simulator(vector<int> &horizontal, int width) {
-    vector<int> currents(width);
-    vector<int> current_indexes(width);
-    rep(i, width) currents[i] = i;
-    rep(i, width) current_indexes[i] = i;
-
-    for (int h : horizontal) {
-        int h1 = currents[h];
-        int h2 = currents[h + 1];
-
-        swap(current_indexes[h1], current_indexes[h2]);
-        currents[h] = h2;
-        currents[h + 1] = h1;
-
-    }
-    return current_indexes;
-}
-
 int main() {
     int n;
     ll k;
     cin >> n >> k;
+
+    vector<ll> v(n);
+    rep(i, n) cin >> v[i], v[i]--;
+
     Doubling doubling(n, k);
-    rep(i, n) {
-        int to;
-        cin >> to;
-        to--;
-        doubling.set_next(i, to);
-    }
+    rep(i, n) doubling.set_next(i, v[i]);
     doubling.build();
 
     cout << doubling.query(0, k) + 1 << endl;
-
-
 }
