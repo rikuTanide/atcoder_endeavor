bc719255447d8774d57be91b5cfe30909d01f0f1
riku <riku@tanide.net>
2020-05-13 16:28:30 +0900

    【AC】[AtCoder Grand Contest 029]A - Irreversible operation

---
+++ agc029/agc029_a.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<double, double> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    vector<int> v(n, 0);
+    v[0] = s[0] == 'W' ? 1 : 0;
+    rep(i, n) {
+        if (i == 0) continue;
+        v[i] = v[i - 1];
+        if (s[i] == 'W') v[i]++;
+    }
+    ll ans = 0;
+    rep(i, n) {
+        if (s[i] == 'B') continue;
+        int k = v[i];
+
+        int now = i - k + 1;
+        ans += now;
+    }
+    cout << ans << endl;
+
+    cout << endl;
+}
