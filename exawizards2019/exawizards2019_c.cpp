1a96c9359c910e5f8537ee3b1107d3672393c59f
riku <riku@tanide.net>
2020-05-15 22:23:58 +0900

    【TLE】[エクサウィザーズ 2019]C - Snuke the Wizard

---
+++ exawizards2019/exawizards2019_c.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<double, double> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+
+    string s;
+    cin >> s;
+
+    vector<int> g(n, 1);
+
+    rep(_, q) {
+        char c, d;
+        cin >> c >> d;
+        rep(i, n) {
+            if (s[i] != c) continue;
+            if (d == 'R') {
+                if (i + 1 < n) g[i + 1] += g[i];
+                g[i] = 0;
+            } else {
+                if (i - 1 >= 0)g[i - 1] += g[i];
+                g[i] = 0;
+            }
+
+        }
+    }
+
+    ll sum = accumulate(g.begin(), g.end(), 0ll);
+    cout << sum << endl;
+
+}
