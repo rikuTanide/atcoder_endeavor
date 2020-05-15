5f8edb9977fa313585cbdafdc217ec1c2027bce4
riku <riku@tanide.net>
2020-05-15 21:28:19 +0900

    【AC】[AtCoder Beginner Contest 116]D - Various Sushi

--- abc116/abc116_d.cpp
+++ abc116/abc116_d.cpp
@@ -1,139 +1,129 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-typedef long long ll;
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e10;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
-typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-const int mod = 1000000007;
 
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<double, double> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<string> &o) {
+    string a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+struct Sushi {
+    ll t, d;
+    bool first;
+};
+
+
+std::istream &operator>>(std::istream &in, Sushi &o) {
+    cin >> o.t >> o.d;
+    return in;
+}
 
 int main() {
-    ll n, k;
+    int n, k;
     cin >> n >> k;
+    vector<Sushi> sushis(n);
 
-    vector<P> all_neta(n);// <oishisa , shurui>
-
-    rep(i, n) {
-        ll t, d;
-        cin >> t >> d;
-        P p(d, t);
-        all_neta[i] = p;
-    }
-
-    ll neta_count = 0;
-    {
-
-        set<ll> neta_set;
-        for (auto a : all_neta) {
-            neta_set.insert(a.second);
-        }
-        neta_count = neta_set.size();
-
-        ll i = 0;
-        map<ll, ll> neta_id_map;
-        for (ll neta_id : neta_set) {
-            neta_id_map[neta_id] = i;
-            i++;
-        }
-        for (ll i = 0; i < n; i++) {
-            all_neta[i].second = neta_id_map[all_neta[i].second];
-        }
-
-    }
-
-    // 各ネタ最おいしソート
-    vector<P> kakuneta_sai_oishi_sort = vector<P>(neta_count, P(0, 0));
+    rep(i, n) cin >> sushis[i];
+    sort(sushis.rbegin(), sushis.rend(), [](Sushi &s1, Sushi &s2) {
+        return s1.d < s2.d;
+    });
 
     {
-
-        for (auto a : all_neta) {
-            ll neta = a.second;
-            ll oishisa = a.first;
-
-            if (kakuneta_sai_oishi_sort[neta].first < oishisa) {
-                kakuneta_sai_oishi_sort[neta] = P(oishisa, neta);
+        set<ll> t;
+        rep(i, n) {
+            if (t.find(sushis[i].t) == t.end()) {
+                sushis[i].first = true;
+                t.insert(sushis[i].t);
+            } else {
+                sushis[i].first = false;
             }
-
         }
     }
 
-    sort(all_neta.rbegin(), all_neta.rend());
-    sort(kakuneta_sai_oishi_sort.rbegin(), kakuneta_sai_oishi_sort.rend());
-
-    vector<ll> neta_saiyou_count = vector<ll>(neta_count, 0);
-
-    ll saiyo_su = 0;
-    ll oishisa_sum = 0;
-
-    for (ll i = 0; i < k; i++) {
-        P p = all_neta[i];
-        if (neta_saiyou_count[p.second] == 0) {
-            saiyo_su++;
+    vector<Sushi> v = [&] {
+        vector<Sushi> res;
+        rep(i, k) {
+            res.push_back(sushis[i]);
         }
-        oishisa_sum += p.first;
-        neta_saiyou_count[p.second]++;
-    }
+        sort(res.rbegin(), res.rend(), [&](Sushi &s1, Sushi &s2) {
+            ll a = s1.d;
+            ll b = s2.d;
+            if (s1.first) a += INF;
+            if (s2.first) b += INF;
+            return a < b;
 
-    ll ans = oishisa_sum + (saiyo_su * saiyo_su);
-
-    vector<PQ_ASK> netagoto_senkouochi_queue(neta_count, PQ_ASK());
-
-    for (ll i = 0; i < k; i++) {
-        P p = all_neta[i];
-        if (neta_saiyou_count[p.second] >= 2) {
-            netagoto_senkouochi_queue[p.second].push(p);
+        });
+        return res;
+    }(); // おいしくないやつが最後
+    vector<Sushi> u = [&] {
+        vector<Sushi> res;
+        rep(i, n) {
+            if (i < k) continue;
+            if (!sushis[i].first) continue;
+            res.push_back(sushis[i]);
         }
+        reverse(res.begin(), res.end());
+        return res;
+    }(); // おいしいやつが最後
+
+    ll prev_dsum = [&] {
+        ll sum = 0;
+        for (Sushi &s : v) sum += s.d;
+        return sum;
+    }();
+    ll t = [&] {
+        set<ll> types;
+        for (Sushi &s : v) types.insert(s.t);
+        return types.size();
+    }();
+    ll ma = t * t + prev_dsum;
+    while (!u.empty() && v.back().first == false) {
+        Sushi pv = v.back();
+        Sushi pu = u.back();
+        v.pop_back();
+        u.pop_back();
+
+        prev_dsum -= pv.d;
+        prev_dsum += pu.d;
+        t++;
+
+        ll now = t * t + prev_dsum;
+        cmax(ma, now);
     }
-
-    priority_queue<P> fukkatsu_list;
-    for (ll i = 0; i < neta_count; i++) {
-        P neta_daihyo = kakuneta_sai_oishi_sort[i];
-        if (neta_saiyou_count[neta_daihyo.second] == 0) {
-            fukkatsu_list.push(neta_daihyo);
-        }
-    }
-
-
-    PQ_ASK daihyo_senkouochi_queue;
-    for (ll i = 0; i < neta_count; i++) {
-        if (!netagoto_senkouochi_queue[i].empty()) {
-            P p = netagoto_senkouochi_queue[i].top();
-            daihyo_senkouochi_queue.push(p);
-            netagoto_senkouochi_queue[i].pop();
-        }
-    }
-
-
-    while (!daihyo_senkouochi_queue.empty() && !fukkatsu_list.empty()) {
-        P daihyo_ochi = daihyo_senkouochi_queue.top();
-        daihyo_senkouochi_queue.pop();
-        if (netagoto_senkouochi_queue[daihyo_ochi.second].size() > 1) {
-            P next_staging = netagoto_senkouochi_queue[daihyo_ochi.second].top();
-            daihyo_senkouochi_queue.push(next_staging);
-        }
-        P fukkatsu = fukkatsu_list.top();
-        fukkatsu_list.pop();
-
-        saiyo_su++;
-        oishisa_sum -= daihyo_ochi.first;
-        oishisa_sum += fukkatsu.first;
-
-        ll now = oishisa_sum + (saiyo_su * saiyo_su);
-        ans = max(now, ans);
-
-    }
-
-    cout << ans << endl;
-
+    cout << ma << endl;
 
 }
