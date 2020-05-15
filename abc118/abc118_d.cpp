d2f2cd07691bcee3ff7860e8f63b3f96ebb6041a
riku <riku@tanide.net>
2020-05-15 22:15:59 +0900

    【AC】[AtCoder Beginner Contest 118]D - Match Matching

--- abc118/abc118_d.cpp
+++ abc118/abc118_d.cpp
@@ -1,113 +1,108 @@
 #include <bits/stdc++.h>
-#include <cmath>
+#include <boost/multiprecision/cpp_int.hpp>
+
+namespace mp = boost::multiprecision;
 
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e10;
-const ll MINF = -10e10;
-//const int INF = INT_MAX;
-#define mins(x, y) x = min(x, y)
-#define maxs(x, y) x = max(x, y)
+typedef pair<double, double> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-const int mod = 1000000007;
-
-vector<int> seg = {
-        0,
-        2,
-        5,
-        5,
-        4,
-        5,
-        6,
-        3,
-        7,
-        6};
-int n, m;
-vector<int> options;
-
-
-int minSeg() {
-
-    int ans = 10;
-    for (int option : options) {
-        ans = min(ans, seg[option]);
-    }
-    return ans;
-
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
 }
 
-
-int maxLen() {
-
-
-    return n / minSeg();
-
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
 }
 
-
-int f(string s) {
-    int ans = 0;
-    for (int i = 0; i < s.size(); i++) {
-        ans += seg[s[i]];
-    }
-    return ans;
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
 }
 
-string fMax(string s, string t) {
-    if (s == "-1") {
-        return t;
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+struct Sushi {
+    ll t, d;
+    bool first;
+};
+
+
+std::istream &operator>>(std::istream &in, Sushi &o) {
+    cin >> o.t >> o.d;
+    return in;
+}
+
+int gp(int i) {
+    switch (i) {
+
+        case 1:
+            return 2;
+        case 2:
+            return 5;
+        case 3:
+            return 5;
+        case 4:
+            return 4;
+        case 5:
+            return 5;
+        case 6:
+            return 6;
+        case 7:
+            return 3;
+        case 8:
+            return 7;
+        case 9:
+            return 6;
     }
-    if (t == "-1") {
-        return s;
-    }
-    if (s.length() > t.length()) {
-        return s;
-    } else if (s.length() < t.length()) {
-        return t;
-    } else {
-        if (s.compare(t) > 0) {
-            return s;
-        } else {
-            return t;
-        }
-    }
+    __throw_runtime_error("konai");
 }
 
 int main() {
-
+    int n, m;
     cin >> n >> m;
-    rep(i, m) {
-        int a;
-        cin >> a;
-        options.push_back(a);
-    }
-    sort(options.rbegin(), options.rend());
 
-    vector<string> dp(n + 1, "-1");
-    for (int i = 0; i <= n; i++) {
-        for (int option : options) {
-            if (i < seg[option]) {
-                continue;
-            }
-            if (i == seg[option]) {
-                char c = '0' + option;
-                string s = "";
-                s += c;
-                dp[i] = fMax(s, dp[i]);
-                continue;
-            }
-            if (dp[i - seg[option]] == "-1") {
-                continue;
-            }
-            dp[i] = fMax(dp[i], dp[i - seg[option]] + (char) ('0' + option));
+    set<int> can;
+    rep(i, m) cin >> can;
 
+    vector<mp::cpp_int> dp(n + 100, -INF);
+    dp[0] = 0;
+
+    rep(i, n) {
+        mp::cpp_int now = dp[i];
+
+        for (int j : can) {
+            int p = gp(j);
+            int next_i = i + p;
+            mp::cpp_int next_c = now;
+            next_c *= 10;
+            next_c += j;
+            cmax(dp[next_i], next_c);
         }
+
     }
     cout << dp[n] << endl;
-}
 
+}
