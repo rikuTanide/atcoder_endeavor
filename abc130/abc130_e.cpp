79aa67c71e383f9fa4cbd99b195490717f9e790b
riku <riku@tanide.net>
2020-05-16 22:10:39 +0900

    【AC】[AtCoder Beginner Contest 130]E - Common Subsequence

--- abc130/abc130_e.cpp
+++ abc130/abc130_e.cpp
@@ -1,20 +1,53 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
-const ll INF = 1001001001;
+typedef pair<double, double> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 const int mod = 1000000007;
 
 struct mint {
-    ll x;
-
-    mint(ll x = 0) : x(x % mod) {}
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
 
     mint &operator+=(const mint a) {
         if ((x += a.x) >= mod) x -= mod;
@@ -45,30 +78,64 @@ struct mint {
         mint res(*this);
         return res *= a;
     }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
 };
 
 
-mint dp0[2005][2005];
-mint dp1[2005][2005];
-
 int main() {
+
     int n, m;
     cin >> n >> m;
+    vector<vector<mint>> dp(n + 1, vector<mint>(m + 1));
+    vector<vector<mint>> sum(n + 1, vector<mint>(m + 1, 0));
 
-    vector<int> s(n + 1), t(m + 1);
-    rep(i, n)scanf("%d", &s[i]);
-    rep(i, m)scanf("%d", &t[i]);
-    dp0[0][0] = 1;
+    vector<ll> s(n), t(m);
+    rep(i, n) cin >> s[i];
+    rep(i, m) cin >> t[i];
 
-    rep(i, n + 1)
-        rep(j, m + 1) {
-            dp0[i + 1][j] += dp0[i][j];
-            dp1[i][j] += dp0[i][j];
-            dp1[i][j + 1] += dp1[i][j];
-            if (s[i] == t[j]) {
-                dp0[i + 1][j + 1] += dp1[i][j];
+    rep(i, n + 1) sum[i][0] = 1;
+    rep(i, m + 1) sum[0][i] = 1;
+    mint ans = 1;
+    rep(x, n) rep(y, m) {
+            if (s[x] == t[y]) {
+                dp[x + 1][y + 1] = sum[x][y];
+                ans += dp[x + 1][y + 1];
             }
+            sum[x + 1][y + 1] = sum[x][y + 1] + sum[x + 1][y] - sum[x][y] + dp[x + 1][y + 1];
         }
-    mint ans = dp1[n][m];
-    cout << ans.x << endl;
+    cout << ans << endl;
 }
