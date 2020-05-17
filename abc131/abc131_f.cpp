8cb71af79ffc269ff3e589332edad8a25669e49e
riku <riku@tanide.net>
2020-05-17 14:51:35 +0900

    【AC】[AtCoder Beginner Contest 131]F - Must Be Rectangular!

--- abc131/abc131_f.cpp
+++ abc131/abc131_f.cpp
@@ -1,50 +1,119 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-typedef pair<int, int> P;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
-const ll INF = 1001001001;
+typedef pair<double, double> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-const int mod = 1000000007;
-
-const int V = 100005;
-
-vector<int> to[V * 2];
-bool visited[V * 2];
-vector<int> cnt;
-
-void dfs(int v) {
-    if (visited[v]) return;
-    visited[v] = true;
-    cnt[v / V]++;
-
-    for (int u : to[v]) {
-        dfs(u);
-    }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
 }
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+
+class UnionFind {
+public:
+    // 親の番号を格納する。親だった場合-size
+    vector<int> parents;
+
+    UnionFind(int n) {
+        parents = vector<int>(n, -1);
+    }
+
+    // aがどのグループに属しているか
+    int root(int a) {
+        if (parents[a] < 0) {
+            return a;
+        }
+        return parents[a] = root(parents[a]);
+    }
+
+    int size(int a) {
+        return -parents[root(a)];
+    }
+
+    // aとbをくっつける
+    bool connect(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        if (ra == rb) {
+            return false;
+        }
+        // 大きいほうにA
+        if (size(ra) < size(rb)) {
+            swap(ra, rb);
+        }
+        parents[ra] += parents[rb];
+        parents[rb] = ra;
+        return true;
+    }
+
+    bool is_union(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        return ra == rb;
+    }
+
+    set<int> get_parents() {
+        int n = parents.size();
+        set<int> r;
+        rep(i, n) r.insert(root(i));
+        return r;
+    }
+
+};
+
 int main() {
+
+    int ma = 10e4 + 1;
+
     int n;
     cin >> n;
-    rep(i, n) {
-        int x, y;
-        cin >> x >> y;
-        y += V;
-        to[x].push_back(y);
-        to[y].push_back(x);
-    }
-    ll ans = 0;
-    rep(i, V * 2) {
-        if (visited[i])continue;
-        cnt = vector<int>(2);
-        dfs(i);
-        ans += (ll) cnt[0] * cnt[1];
-    }
-    ans -= n;
-    cout << ans << endl;
+    UnionFind uf(2 * ma);
 
+    vector<P> points(n);
+    rep(i, n) cin >> points[i].first >> points[i].second;
+    for (P p : points) uf.connect(p.first, p.second + ma);
+
+    map<int, ll> x, y;
+    rep(i, 2 * ma) if (i < ma) x[uf.root(i)]++; else y[uf.root(i)]++;
+
+    auto s = uf.get_parents();
+    ll res = 0;
+    for (int i : s) res += x[i] * y[i];
+
+    cout << res - n << endl;
 }
