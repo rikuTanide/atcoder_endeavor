b44b7e9e0c30cff693ff38d6950857c6b008c5cb
riku <riku@tanide.net>
2020-05-17 21:24:49 +0900

    【WA】[AtCoder Beginner Contest 168]D - .. (Double Dots)

---
+++ abc168/abc168_d.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<double, double> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> edges(n);
+
+    rep(i, m) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        edges[a].push_back(b);
+        edges[b].push_back(a);
+    }
+
+    vector<int> prev(n, -1);
+
+    struct Q {
+        int to;
+        int prev;
+    };
+    queue<Q> q;
+    q.push(Q{0, -1});
+
+    while (!q.empty()) {
+        Q t = q.front();
+        q.pop();
+        for (int next : edges[t.to]) {
+            if (prev[next] != -1) continue;
+            prev[next] = t.to;
+            q.push(Q{next, t.prev});
+        }
+    }
+
+    rep(i, n) {
+        if (i == 0) continue;
+        cout << prev[i] + 1 << endl;
+    }
+
+}
