9ec583d859c487b2ae6d9a3202565520330a4941
riku <riku@tanide.net>
2020-05-18 17:43:23 +0900

    【AC】[AtCoder Beginner Contest 136]E - Max GCD

--- abc136/abc136_e.cpp
+++ abc136/abc136_e.cpp
@@ -1,53 +1,114 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-const ll INF = 1001001001;
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return sums.back();
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void calculate() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
+
+
+bool check(ll n, ll k, vector<ll> &numbers, ll target) {
+    vector<ll> v(n);
+    rep(i, n) v[i] = numbers[i] % target;
+    sort(v.begin(), v.end());
+    vector<ll> u(n);
+    rep(i, n) u[i] = target - v[i];
+
+    CumulativeSum csv(n), csu(n);
+    rep(i, n) csv.set(i, v[i]);
+    rep(i, n) csu.set(i, u[i]);
+    csv.calculate();
+    csu.calculate();
+
+    rep(i, n - 1) {
+        ll l = csv.getSectionSum(0, i);
+        ll r = csu.getSectionSum(i + 1, n - 1);
+        if (max(l, r) <= k) return true;
+    }
+    return false;
+}
 
 int main() {
-
-    int n, k;
+    ll n, k;
     cin >> n >> k;
-    vector<int> a(n);
+    vector<ll> numbers(n);
+    rep(i, n) cin >> numbers[i];
 
-    rep(i, n)cin >> a[i];
-    ll sum = 0;
-    rep(i, n) sum += a[i];
-
-    set<ll> candidates;
-    for (int i = 1; i * i <= sum; i++) {
-        if (sum % i != 0) continue;
-        candidates.insert(i);
-        candidates.insert(sum / i);
-    }
+    ll all = accumulate(numbers.begin(), numbers.end(), 0ll);
 
     ll ans = 1;
-    for (ll x : candidates) {
-        ll need;
-        {
-            vector<ll> r(n);
-            rep(i, n) r[i] = a[i] % x;
-            sort(r.begin(), r.end());
-            ll rsum = 0;
-            rep(i, n)rsum += x - r[i];
-            ll lsum = 0;
-            need = INF;
-            rep(i, n) {
-                lsum += r[i];
-                rsum -= x - r[i];
-                need = min(need, max(lsum, rsum));
-            }
-        }
-        if (need <= k) {
-            ans = max(ans, x);
-        }
+    for (ll i = 1; i * i < all; i++) {
+        if (all % i != 0) continue;
+        if (check(n, k, numbers, i)) cmax(ans, i);
+        if (check(n, k, numbers, all / i)) cmax(ans, all / i);
     }
-
     cout << ans << endl;
-
 }
