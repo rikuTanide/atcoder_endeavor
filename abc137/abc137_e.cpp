ec4ee3e4a07d3394a8f6124eb6bb850bd8eeccc5
riku <riku@tanide.net>
2020-05-18 18:01:12 +0900

    【WA】[AtCoder Beginner Contest 137]E - Coins Respawn

--- abc137/abc137_e.cpp
+++ abc137/abc137_e.cpp
@@ -1,87 +1,98 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) int(x.size())
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-const int INF = 1001001001;
-
-vector<int> to[2505];
-vector<int> rto[2505];
-bool reachableFrom1[2505];
-bool reachableToN[2505];
-bool ok[2005];
-
-
-void dfs(int v) {
-    if (reachableFrom1[v])return;
-    reachableFrom1[v] = true;
-    for (int u :to[v]) {
-        dfs(u);
-    }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
 }
 
-void rdfs(int v) {
-    if (reachableToN[v])return;
-    reachableToN[v] = true;
-    for (int u :rto[v]) {
-        rdfs(u);
-    }
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
 }
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+struct Edge {
+    ll to, cost;
+};
+
+
+// 特定のルートまでに閉路があるか調べたいならこう
+// https://atcoder.jp/contests/abc061/submissions/12926139
+bool bellman_ford(int n, int s, vector<vector<Edge> > &graph, vector<ll> &dist) { // nは頂点数、sは開始頂点
+
+    dist[s] = 0; // 開始点の距離は0
+    for (int i = 0; i < n; i++) {
+        for (int v = 0; v < n; v++) {
+            for (int k = 0; k < graph[v].size(); k++) {
+                Edge e = graph[v][k];
+                if (dist[v] != INF && dist[e.to] > dist[v] + e.cost) {
+                    dist[e.to] = dist[v] + e.cost;
+                    if (i == n - 1) return true; // n回目にも更新があるなら負の閉路が存在
+                }
+            }
+        }
+    }
+    return false;
+}
+
 
 int main() {
 
-    int n, m, p;
-
+    ll n, m, p;
     cin >> n >> m >> p;
 
-    vector<tuple<int, int, int>> edges;
+    vector<vector<Edge>> edges(n);
 
     rep(i, m) {
-        int a, b, c;
+        ll a, b, c;
         cin >> a >> b >> c;
         a--;
         b--;
-        c -= p;
-        c = -c;
-        edges.emplace_back(a, b, c);
-        to[a].push_back(b);
-        rto[b].push_back(a);
+        edges[a].push_back(Edge{b, -(c - p)});
     }
 
-    dfs(0);
-    rdfs(n - 1);
-    rep(i, n)ok[i] = reachableFrom1[i] & reachableToN[i];
+    vector<ll> dist(n, INF);
+    bellman_ford(n, 0, edges, dist);
+    ll ans = dist[n - 1];
 
-    {
-        vector<int> d(n, INF);
-        d[0] = 0;
-        bool upd = true;
-        int step = 0;
-        while (upd) {
-            upd = false;
-            rep(i, m) {
-                int a, b, c;
-                tie(a, b, c) = edges[i];
-                if (!ok[a])continue;
-                if (!ok[b])continue;
-                int newD = d[a] + c;
-                if (newD < d[b]) {
-                    upd = true;
-                    d[b] = newD;
-                }
-            }
-            step++;
-            if (step > n) {
-                puts("-1");
-                return 0;
-            }
-        }
-        int ans = -d[n - 1];
-        ans = max(ans, 0);
-        cout << ans << endl;
+    bellman_ford(n, 0, edges, dist);
 
+    if (dist[n - 1] != ans) {
+        cout << -1 << endl;
+    } else {
+        cout << max(0ll, -ans) << endl;
     }
+
 
 }
