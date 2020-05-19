65018ad44e78277d5a4d063d3688c29bcb6ee60b
riku <riku@tanide.net>
2020-05-19 22:02:34 +0900

    【AC】[AtCoder Beginner Contest 143]E - Travel by Car

--- abc143/abc143_e.cpp
+++ abc143/abc143_e.cpp
@@ -1,53 +1,131 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-int INF = INT_MAX / 2;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+
+class WarchallFloyd {
+
+    int n;
+    vector<vector<ll>> distances;
+
+public:
+    WarchallFloyd(int n) : n(n), distances(n, vector<ll>(n, INF)) {
+        rep(i, n) distances[i][i] = 0;
+    };
+
+    void warshall_floyd() {
+        for (int k = 0; k < n; k++) {       // 経由する頂点
+            for (int i = 0; i < n; i++) {    // 始点
+                for (int j = 0; j < n; j++) {  // 終点
+                    if (distances[i][j] == INF && (distances[i][k] == INF || distances[k][j] == INF)) {
+                        distances[i][j] = INF;
+                    } else {
+                        distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
+                    }
+                }
+            }
+        }
+    }
+
+    ll distance(int from, int to) {
+        return distances[from][to];
+    }
+
+    void add(int from, int to, ll cost) {
+        distances[from][to] = cost;
+        distances[to][from] = cost;
+    }
+
+
+    friend std::istream &operator>>(std::istream &in, WarchallFloyd &o) {
+        int from, to;
+        ll c;
+        cin >> from >> to >> c;
+        from--;
+        to--;
+
+        o.distances[from][to] = c;
+        o.distances[to][from] = c;
+        return in;
+    }
+};
+
 
 int main() {
+    int n, m;
+    ll l;
 
-    int n, m, l;
     cin >> n >> m >> l;
-    vector<vector<int>> dist(n, vector<int>(n, INF));
-    rep(i, n) dist[i][i] = 0;
-    rep(i, m) {
-        int a, b, c;
+
+    WarchallFloyd wf1(n);
+    rep(_, m) {
+        int a, b;
+        ll c;
         cin >> a >> b >> c;
         a--;
         b--;
-        dist[b][a] = dist[a][b] = c;
+
+        wf1.add(a, b, c);
     }
-
-    rep(k, n) rep(i, n) rep(j, n) {
-                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-            }
-    vector<vector<int>> dist2(n, vector<int>(n, INF));
-    rep(i, n) dist[i][i] = 0;
+    wf1.warshall_floyd();
 
 
-    rep(i, n) rep(j, n) {
-            if (dist[i][j] <= l) {
-                dist2[i][j] = 1;
-            }
-        }
-    rep(k, n) rep(i, n) rep(j, n) {
-                dist2[i][j] = min(dist2[i][j], dist2[i][k] + dist2[k][j]);
-            }
+    WarchallFloyd wf2(n);
+    rep(i, n) rep (j, n) if (i != j) if (wf1.distance(i, j) <= l) wf2.add(i, j, 1);
+    wf2.warshall_floyd();
 
     int q;
     cin >> q;
+    rep(_, q) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
 
-    rep(i, q) {
-        int s, t;
-        cin >> s >> t;
-        s--;
-        t--;
-        int dis = dist2[s][t] - 1;
-        if (dist2[s][t] == INF) dis = -1;
-        cout << dis << endl;
+        ll now = wf2.distance(a, b);
 
+        ll ans = now == INF ? -1 : now - 1;
+        cout << ans << endl;
     }
-
 
 }
