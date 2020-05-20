40401ead1f8a10b85877e56cbe1c3d2b10675269
riku <riku@tanide.net>
2020-05-20 18:37:32 +0900

    【TLE】[AtCoder Beginner Contest 147]E - Balanced Path

--- abc147/abc147_e.cpp
+++ abc147/abc147_e.cpp
@@ -1,83 +1,81 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<ll, int> P;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-bool contain(set<char> &s, int a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-
     int h, w;
     cin >> h >> w;
 
-    vector<vector<int>> as(h, vector<int>(w, 0));
-    vector<vector<int>> bs(h, vector<int>(w, 0));
+    vector<vector<int>> grid_a(h, vector<int>(w)), grid_b(h, vector<int>(w));
+    rep(y, h) rep(x, w) cin >> grid_a[y][x];
+    rep(y, h) rep(x, w) cin >> grid_b[y][x];
 
-    rep(y, h)rep(x, w) cin >> as[y][x];
-    rep(y, h)rep(x, w) cin >> bs[y][x];
+    vector<vector<set<int>>> dp(h, vector<set<int>>(w));
+    dp[0][0].insert(abs(grid_a[0][0] - grid_b[0][0]));
 
-    vector<vector<vector<bool>>> dp(h, vector<vector<bool>>(w, vector<bool>(12801, false)));
-    dp[0][0][abs(as[0][0] - bs[0][0])] = true;
-    for (int y = 0; y < h; y++) {
-        for (int x = 0; x < w; x++) {
-            if (x == 0 && y == 0) continue;
-            if (x != 0) {
-                for (int i = 0; i <= 12800; i++) {
-                    if (!dp[y][x - 1][i]) {
-                        continue;
-                    }
-                    int next_1 = i + abs(as[y][x] - bs[y][x]);
-                    int next_2 = abs(i - abs(as[y][x] - bs[y][x]));
-                    dp[y][x][next_1] = true;
-                    dp[y][x][next_2] = true;
-                }
+    auto get = [&](int y, int x) {
+        set<int> s;
+
+        if (y == 0 && x == 0) {
+            s.insert(0);
+            return s;
+        }
+        if (y > 0) s.insert(dp[y - 1][x].begin(), dp[y - 1][x].end());
+        if (x > 0) s.insert(dp[y][x - 1].begin(), dp[y][x - 1].end());
+
+        return s;
+    };
+
+    rep(y, h) rep(x, w) {
+            int sub = abs(grid_a[y][x] - grid_b[y][x]);
+
+            for (int next : get(y, x)) {
+                dp[y][x].insert(next + sub);
+                dp[y][x].insert(abs(next - sub));
             }
 
-            if (y != 0) {
-                for (int i = 0; i <= 12800; i++) {
-                    if (!dp[y - 1][x][i]) {
-                        continue;
-                    }
-                    int next_1 = i + abs(as[y][x] - bs[y][x]);
-                    int next_2 = abs(i - abs(as[y][x] - bs[y][x]));
-                    dp[y][x][next_1] = true;
-                    dp[y][x][next_2] = true;
-                }
-            }
-
-
         }
-    }
 
-    for (int i = 0; i < 12800; i++) {
-        if (dp[h - 1][w - 1][i]) {
-            cout << i << endl;
-            return 0;
-        }
-    }
-
+    int ans = *dp.back().back().begin();
+    cout << ans << endl;
 }
