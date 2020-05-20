3c31f54ced4db8394b232cd0035fda030d975f61
riku <riku@tanide.net>
2020-05-20 17:17:47 +0900

    【AC】[DISCO presents ディスカバリーチャンネル コードコンテスト2020 予選]D - Digit Sum Replace

---
+++ ddcc2020-qual/ddcc2020_qual_d.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    ll d_sum = 0;
+    ll d_cou = 0;
+
+    rep(_, n) {
+        ll d;
+        ll c;
+        cin >> d >> c;
+
+        d_cou += c;
+
+        d_sum += (d * c);
+
+    }
+
+    cout << (d_cou - 1) + ((d_sum - 1) / 9) << endl;
+
+}
