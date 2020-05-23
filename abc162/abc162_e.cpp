ab411b8c82ae6bdc2bb61464ade0adfabab4d2a5
riku <riku@tanide.net>
2020-05-23 18:19:14 +0900

    【AC】[AtCoder Beginner Contest 162]E - Sum of gcd of Tuples (Hard)

--- abc162/abc162_e.cpp
+++ abc162/abc162_e.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,15 +9,18 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -33,8 +38,8 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
 //const ll mod = 1e10;
+
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 const int mod = 1000000007;
@@ -109,75 +114,38 @@ struct mint {
 
 };
 
-
-ll gcd(ll x, ll y) {
-    if (x > y) swap(x, y);
-    ll m = 1;
-    while (m != 0) {
-        m = y % x;
-        y = x;
-        x = m;
-    }
-    return y;
-}
-
-
-vector<ll> get_primes(ll k) {
-
-    vector<bool> v(k + 1, true);
-    for (ll i = 2; i < k + 1; i++) {
-        if (!v[i]) continue;
-        for (ll j = 2; i * j < k + 1; j++) {
-            v[i * j] = false;
-        }
-    }
-
-    vector<ll> ans;
-    rep(i, k + 1) {
-        if (i <= 1) continue;
-        if (v[i]) ans.push_back(i);
-    }
-    return ans;
-}
-
 int main() {
-
-    ll n, k;
+    int n, k;
     cin >> n >> k;
-    
-    vector<ll> counts(k + 1, 0);
+
+    vector<mint> counts(k + 1, 0);
+
     rep(i, k + 1) {
         if (i <= 1) continue;
         counts[i] = k / i;
     }
 
-    vector<mint> dp(k + 1, 0);
-//    vector<ll> primes = get_primes(k);
-    // 答えが2になるのは2の倍数だけでできた場合から
-    // 4の倍数だけでできた場合などを引いた
+
+    vector<mint> dp(k + 1);
     for (int i = k; i >= 2; i--) {
-        mint now = mint(counts[i]).pow(n);
-        for (ll l = 2; l * i <= k; l++) {
-//            if (i * l > k) break;
-            now -= dp[i * l];
+        mint all = counts[i].pow(n);
+
+        mint sub = 0;
+        for (int j = 2; j * i <= k; j++) {
+            sub += dp[i * j];
         }
+        mint now = all - sub;
         dp[i] = now;
     }
 
-    mint sum = 0;
-    for (ll i = 2; i <= k; i++) {
-        sum += (dp[i] * i);
+    mint ans = 0;
+    for (int i = 2; i <= k; i++) {
+        ans += (dp[i] * i);
     }
 
-//    ll fsum = accumulate(counts.begin(), counts.end(), 0ll);
-    mint fsum = 0;
-    for (mint m : dp) {
-        fsum += m;
-    }
-    mint one = mint(k).pow(n);
-    one -= fsum;
-    sum += one;
-    cout << sum << endl;
+    mint one = mint(k).pow(n) - accumulate(dp.begin(), dp.end(), mint(0));
+
+    cout << ans + one << endl;
 
 }
 
