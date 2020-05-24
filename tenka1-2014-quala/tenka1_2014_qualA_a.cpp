8e154db71e2c4aa369f90ddea63b97ba6e3d7ca7
riku <riku@tanide.net>
2020-05-24 13:13:35 +0900

    【AC】[天下一プログラマーコンテスト2014予選A]A - 天下一序数

---
+++ tenka1-2014-quala/tenka1_2014_qualA_a.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+template<typename ... Args>
+std::string format(const std::string &fmt, Args ... args) {
+    size_t len = std::snprintf(nullptr, 0, fmt.c_str(), args ...);
+    std::vector<char> buf(len + 1);
+    std::snprintf(&buf[0], len + 1, fmt.c_str(), args ...);
+    return std::string(&buf[0], &buf[0] + len);
+}
+
+int main() {
+
+    vector<string> ss;
+
+    rep(i, 1000) {
+        int j = i + 1;
+        ss.push_back(format("%d", j));
+    }
+    sort(ss.begin(), ss.end());
+
+    for(string s : ss) cout << s << endl;
+}
