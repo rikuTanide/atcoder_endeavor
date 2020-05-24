6faea8ddf073de821f2a115c8bfd0e97869d1f38
riku <riku@tanide.net>
2020-05-24 14:00:44 +0900

    【AC】[Code Formula 2014 予選A]B - ボウリングゲーム

---
+++ code-formula-2014-quala/code_formula_2014_qualA_b.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    vector<char> pins(10, 'x');
+
+    rep(_, a) {
+        int p;
+        cin >> p;
+
+        pins[p] = '.';
+    }
+
+    rep(_, b) {
+        int p;
+        cin >> p;
+
+        pins[p] = 'o';
+    }
+
+    printf("%c %c %c %c\n", pins[7], pins[8], pins[9], pins[0]);
+    printf(" %c %c %c\n", pins[4], pins[5], pins[6]);
+    printf("  %c %c\n", pins[2], pins[3]);
+    printf("   %c\n", pins[1]);
+
+}
