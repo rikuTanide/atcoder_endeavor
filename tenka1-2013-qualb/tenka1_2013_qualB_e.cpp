47203ee1842dbe3cc46aa95e9ada672c6c93bf7f
riku <riku@tanide.net>
2020-05-25 14:40:39 +0900

    【AC】[天下一プログラマーコンテスト2013予選B]E - 天下一最短路コンテスト

---
+++ tenka1-2013-qualb/tenka1_2013_qualB_e.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+
+int main() {
+
+    vector<P> v;
+    rep(i, 49) {
+        v.emplace_back(0, i);
+    }
+    rep(i, 49) {
+        v.emplace_back(1, i);
+    }
+    v.emplace_back(0, 49);
+    v.emplace_back(1, 49);
+
+    cout << v.size() << endl;
+    for (P p : v) cout << p.first << ' ' << p.second << endl;
+}
