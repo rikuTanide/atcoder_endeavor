70a577b51f3b7c9f0a6dd4c00557daee4ddfed45
riku <riku@tanide.net>
2020-05-25 19:01:03 +0900

    【WA】[AtCoder Regular Contest 016]C - ソーシャルゲーム

--- arc016/arc016_3.cpp
+++ arc016/arc016_3.cpp
@@ -7,7 +7,7 @@ const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
 //typedef pair<ll, ll> P;
-typedef pair<ll, ll> P;
+typedef pair<double, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
@@ -41,7 +41,8 @@ typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 struct Lottery {
     int count;
     int cost;
-    vector<ll> rates;
+    int bits = 0;
+    vector<double> rates;
 
     Lottery(int n) : rates(n) {}
 
@@ -56,9 +57,63 @@ std::istream &operator>>(std::istream &in, Lottery &o) {
         cin >> idol;
         idol--;
         cin >> o.rates[idol];
+        o.rates[idol] /= 100;
+        o.bits |= (1 << idol);
+
     }
+
     return in;
 }
+
+struct Edge {
+    ll to;
+    double cost;
+};
+
+
+class Dijkstra {
+    ll vector_count;
+
+    vector<double> distances;
+    vector<vector<Edge>> edges;
+
+public:
+    Dijkstra(ll n) {
+        vector_count = n;
+        distances.resize(n, INF);
+        edges.resize(n);
+    }
+
+    void insert(ll from, ll to, double cost) {
+        edges[from].push_back({to, cost});
+    }
+
+    void dijkstra(ll k) {
+        distances[k] = 0;  // 始点sへの最短距離は0
+
+        priority_queue<P, vector<P>, greater<P>> que;  // 距離が小さい順に取り出せるようgreater<P>を指定
+        que.push(P(0, k));
+        while (!que.empty()) {
+            P p = que.top();
+            que.pop();
+            int v = p.second;  // 更新した頂点の中で距離が最小の頂点v
+            if (distances[v] < p.first) {
+                continue;
+            }
+            for (auto e : edges[v]) {  // 頂点vから出る辺eを走査
+                if (distances[e.to] > distances[v] + e.cost) {
+                    distances[e.to] = distances[v] + e.cost;
+                    que.push(P(distances[e.to], e.to));
+                }
+            }
+        }
+    }
+
+    double distance(ll to) {
+        return distances[to];
+    }
+
+};
 
 
 int main() {
@@ -69,18 +124,27 @@ int main() {
 
     rep(i, m)cin >> lotteries[i];
 
-    rep(i, m) assert(lotteries[i].count == 1);
+    int state_max = 1 << n;
 
-    vector<int> a(n, INT_MAX);
+    Dijkstra dijkstra(state_max);
 
-    for (Lottery &l : lotteries) {
-        rep(i, n) {
-            if (l.rates[i] == 100) cmin(a[i], l.cost);
+    vector<double> dp(n, 0);
+
+    rep(i, state_max) {
+        for (Lottery &lottery: lotteries) {
+            rep(j, n) {
+                if (lottery.rates[j] == 0) continue;
+
+                int next_bit = i | (1 << j);
+
+                double cost = (1.0 / lottery.rates[j]) * lottery.cost;
+                dijkstra.insert(i, next_bit, cost);
+
+            }
         }
     }
 
-    ll ans = accumulate(a.begin(), a.end(), 0ll);
-
-    cout << ans << endl;
-
+    dijkstra.dijkstra(0);
+    double ans = dijkstra.distance(state_max - 1);
+    printf("%.20f\n", ans);
 }
