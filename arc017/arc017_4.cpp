3dbfb34dc266c711b1d46dcb8f8e974f644fa504
riku <riku@tanide.net>
2020-05-25 23:51:11 +0900

    【TLE】[AtCoder Regular Contest 017]D - ARCたんクッキー

---
+++ arc017/arc017_4.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+ll gcd(ll x, ll y) {
+    if (x > y) swap(x, y);
+    ll m = 1;
+    while (m != 0) {
+        m = y % x;
+        y = x;
+        x = m;
+    }
+    return y;
+}
+
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    vector<ll> machines(n);
+    rep(i, n) cin >> machines[i];
+
+    int m;
+    cin >> m;
+    rep(_, m) {
+        int t, l, r;
+        cin >> t >> l >> r;
+
+        l--;
+        r--;
+
+        if (t == 0) {
+            ll g = machines[l];
+            for (int i = l; i <= r; i++) g = gcd(g, machines[i]);
+            cout << g << endl;
+        } else {
+            for (int i = l; i <= r; i++) machines[i] += t;
+        }
+
+    }
+
+
+}
