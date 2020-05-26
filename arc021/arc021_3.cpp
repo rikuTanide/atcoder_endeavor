1912c918015a793eb38705dec75bc26d2d8166ed
riku <riku@tanide.net>
2020-05-26 20:56:26 +0900

    【TLE】[AtCoder Regular Contest 021]C - 増築王高橋君

---
+++ arc021/arc021_3.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+int main() {
+    int k, n;
+    cin >> k >> n;
+
+    vector<P> v(n);
+    rep(i, n) cin >> v[i].first >> v[i].second;
+
+    PQ_ASK q;
+
+    for (P p : v) q.push(p);
+
+
+    ll sum = 0;
+    rep(_, k) {
+        P p = q.top();
+        q.pop();
+
+        sum += (p.first);
+        q.push(P(p.first + p.second, p.second));
+    }
+
+    cout << sum << endl;
+
+
+}
