823664326c2c180c611ff42d2ae1ce330498fd37
riku <riku@tanide.net>
2020-05-27 19:34:27 +0900

    【RE】[AtCoder Regular Contest 026]D - 道を直すお仕事

---
+++ arc026/arc026_4.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+
+class UnionFind {
+public:
+    // 親の番号を格納する。親だった場合-size
+    vector<int> parents;
+
+    UnionFind(int n) {
+        parents = vector<int>(n, -1);
+    }
+
+    // aがどのグループに属しているか
+    int root(int a) {
+        if (parents[a] < 0) {
+            return a;
+        }
+        return parents[a] = root(parents[a]);
+    }
+
+    int size(int a) {
+        return -parents[root(a)];
+    }
+
+    // aとbをくっつける
+    bool connect(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        if (ra == rb) {
+            return false;
+        }
+        // 大きいほうにA
+        if (size(ra) < size(rb)) {
+            swap(ra, rb);
+        }
+        parents[ra] += parents[rb];
+        parents[rb] = ra;
+        return true;
+    }
+
+    bool is_union(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        return ra == rb;
+    }
+};
+
+struct Road {
+    int from, to;
+    ll cost, time;
+};
+
+std::istream &operator>>(std::istream &in, Road &o) {
+    cin >> o.from >> o.to >> o.cost >> o.time;
+    return in;
+}
+
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<Road> roads(m);
+    rep(i, m) cin >> roads[i];
+
+    assert(m < 20);
+    double ans = INF;
+
+    rep(i, 1 << m) {
+
+        UnionFind uf(n);
+
+        ll costs = 0, times = 0;
+        rep(j, m) if ((i >> j) & 1) {
+                Road road = roads[j];
+                uf.connect(road.from, road.to);
+                costs += road.cost;
+                times += road.time;
+            }
+        if (uf.size(0) != n) continue;
+
+        double w = double(costs) / times;
+        cmin(ans, w);
+    }
+
+    printf("%.20f\n", ans);
+
+}
+
