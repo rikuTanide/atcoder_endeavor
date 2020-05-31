fcb65dbe1c788c6c217fbccf9a4d2254fdd7e478
riku <riku@tanide.net>
2020-05-31 11:39:09 +0900

    【RE】[Donutsプロコンチャレンジ2015]D - ドーナツの箱詰め

---
+++ donuts-2015/donuts_2015_4.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const long double EPS = 1e-9;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<char> &o) {
+    string a;
+    in >> a;
+    for (char c : a) o.insert(c);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    assert(k == 2);
+
+    vector<int> boxes(n);
+    rep(i, n) cin >> boxes[i];
+
+    int q;
+    cin >> q;
+    assert(q == 0);
+    sort(boxes.begin(), boxes.end());
+
+    int ans = INT_MAX;
+    rep(i, n - 1) {
+        int a = boxes[i] - boxes[0];
+        int b = boxes[n - 1] - boxes[i + 1];
+
+        int now = abs(a - b);
+        cmin(ans, now);
+    }
+    cout << ans << endl;
+
+}
