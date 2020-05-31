3c0263429a4a97cca631080c6b73010189e7333b
riku <riku@tanide.net>
2020-05-31 21:06:09 +0900

    【TLE】[AtCoder Beginner Contest 169]B - Multiplication 2

---
+++ abc169/abc169_b.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
+
+namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const long double EPS = 1e-9;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<char> &o) {
+    string a;
+    in >> a;
+    for (char c : a) o.insert(c);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<mp::cpp_int> v(n);
+    rep(i, n) cin >> v[i];
+
+    if (count(v.begin(), v.end(), 0) > 0) {
+        cout << 0 << endl;
+        ret();
+    }
+
+
+    mp::cpp_int ans = 1;
+
+    rep(i, n) {
+        ans *= v[i];
+    }
+
+    mp::cpp_int border = 1000000000000000000ll;
+
+    if (ans > border) {
+        cout << -1 << endl;
+        ret();
+    }
+    
+
+    cout << ans << endl;
+}
