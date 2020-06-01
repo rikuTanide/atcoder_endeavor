b9d9a4f17d98e89a7b0e1fcc8ce823f472124d8d
riku <riku@tanide.net>
2020-06-02 00:25:46 +0900

    【AC】[AtCoder Regular Contest 044]C - ビーム

--- arc044/arc044_c.cpp
+++ arc044/arc044_c.cpp
@@ -42,193 +42,47 @@ bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
-struct Edge {
-    ll to, cost;
-};
+const int MAXT = 100000;
 
-
-int to_id(int t, int h, int w, int y, int x) {
-    return (t * h * w) + (y * w) + x;
-};
-
-struct Direction {
-    int y, x;
-};
-
-
-vector<Direction> directions = {
-        {0,  1},
-        {1,  0},
-        {0,  -1},
-        {-1, 0},
-};
-
-struct Beam {
-    int t, d, x;
-
-
-};
-
-bool operator<(const Beam &b1, const Beam &b2) {
-    auto f = vector<int>{b1.t, b1.d, b1.x};
-    auto g = vector<int>{b2.t, b2.d, b2.x};
-    return f < g;
-}
-
-bool operator==(const Beam &b1, const Beam &b2) {
-    auto f = vector<int>{b1.t, b1.d, b1.x};
-    auto g = vector<int>{b2.t, b2.d, b2.x};
-    return f == g;
-}
-
-
-bool reachable(int h, int w, int y, int x) {
-    if (x == -1 || x == w || y == -1 || y == h) return false;
-    return true;
-};
-
-
-class Dijkstra {
-    ll vector_count;
-
-    vector<ll> distances;
-    vector<vector<Edge>> edges;
-
-public:
-    Dijkstra(ll n) {
-        vector_count = n;
-        distances.resize(n, INF);
-        edges.resize(n);
-    }
-
-    void insertTowWay(ll from, ll to, ll cost) {
-        edges[from].push_back({to, cost});
-        edges[to].push_back({from, cost});
-
-//        printf("insert tow %lld %lld\n", from, to);
-
-    }
-
-    void insert(ll from, ll to, ll cost) {
-        edges[from].push_back({to, cost});
-
-//        printf("insert %lld %lld\n", from, to);
-    }
-
-    void dijkstra(ll k) {
-        distances[k] = 0;  // 始点sへの最短距離は0
-
-        priority_queue<P, vector<P>, greater<P>> que;  // 距離が小さい順に取り出せるようgreater<P>を指定
-        que.push(P(0, k));
-        while (!que.empty()) {
-            P p = que.top();
-            que.pop();
-            int v = p.second;  // 更新した頂点の中で距離が最小の頂点v
-            if (distances[v] < p.first) {
-                continue;
-            }
-            for (auto e : edges[v]) {  // 頂点vから出る辺eを走査
-                if (distances[e.to] > distances[v] + e.cost) {
-                    distances[e.to] = distances[v] + e.cost;
-                    que.push(P(distances[e.to], e.to));
-                }
-            }
+ll calc(ll w, vector<vector<ll>> x) {
+    vector<ll> dp(MAXT, 0);
+    rep(t, MAXT) {
+        sort(x[t].begin(), x[t].end());
+        ll n = x[t].size();
+        for (ll i = 0; i < n; i++) {
+            ll nx = x[t][i] + 1;
+            if (nx < w) dp[nx] = min(dp[nx], dp[x[t][i]] + 1);
         }
+        for (ll i = n - 1; i >= 0; i--) {
+            ll nx = x[t][i] - 1;
+            if (nx >= 0) dp[nx] = min(dp[nx], dp[x[t][i]] + 1);
+        }
+        for (ll i = 0; i < n; i++) dp[x[t][i]] = INF;
     }
-
-    ll distance(ll to) {
-        return distances[to];
-    }
-
-};
-
+    ll ans = *min_element(dp.begin(), dp.begin() + w);
+    return ans;
+}
 
 int main() {
-    int w, h, q;
+    ll w, h, q;
     cin >> w >> h >> q;
 
-    set<Beam> beams;
-    rep(_, q) {
-        Beam b{0, 0, 0};
-        cin >> b.t >> b.d >> b.x;
-        b.x--;
-        beams.insert(b);
+
+    vector<vector<ll>> x(MAXT), y(MAXT);
+
+    rep(i, q) {
+        ll t, d, a;
+        cin >> t >> d >> a;
+        t--;
+        a--;
+
+        if (d == 0) x[t].push_back(a);
+        else y[t].push_back(a);
     }
 
+    ll ans = calc(w, x) + calc(h, y);
 
-    int tmax = [&] {
-        int ans = 0;
-        for (Beam b : beams) cmax(ans, b.t);
-        ans++;
-        return ans;
-    }();
-
-    Dijkstra dijkstra(w * h * tmax + 2);
-
-    // スタートから１個目
-
-    rep(y, h) {
-        rep(x, w) {
-            int start = w * h * tmax;
-            int id = to_id(0, h, w, y, x);
-            dijkstra.insert(start, id, 0);
-        }
-    }
-
-
-//    cout << endl;
-
-    // 同じ時間同士
-    rep(t, tmax) {
-        rep(y, h) {
-            rep(x, w) {
-                int from_id = to_id(t, h, w, y, x);
-                for (Direction d : directions) {
-                    int ny = y + d.y;
-                    int nx = x + d.x;
-
-                    if (!reachable(h, w, ny, nx)) continue;
-                    int next_id = to_id(t, h, w, ny, nx);
-                    dijkstra.insert(from_id, next_id, 1);
-                }
-            }
-        }
-    }
-//cout << endl;
-
-    // 時間を超える
-    rep(t, tmax - 1) {
-        rep(y, h) {
-            rep(x, w) {
-                int from_id = to_id(t, h, w, y, x);
-                if (!reachable(h, w, y, x)) continue;
-
-                if (beams.find(Beam{t + 1, 0, x}) != beams.end()) {
-                    continue;
-                }
-                if (beams.find(Beam{t + 1, 1, y}) != beams.end()) {
-                    continue;
-                }
-
-                int next_id = to_id(t + 1, h, w, y, x);
-                dijkstra.insert(from_id, next_id, 0);
-            }
-        }
-    }
-
-
-    // 最終面からゴール
-    rep(y, h) {
-        rep(x, w) {
-            int goal = w * h * tmax + 1;
-            int from_id = to_id(tmax - 1, h, w, y, x);
-            dijkstra.insert(from_id, goal, 0);
-        }
-    }
-
-    dijkstra.dijkstra(w * h * tmax);
-    ll k = dijkstra.distance(w * h * tmax + 1);
-    ll ans = k == INF ? -1 : k;
+    if (ans >= INF) ans = -1;
 
     cout << ans << endl;
 
