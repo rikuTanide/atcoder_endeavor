14cfb24efd57b165e8e1309deb7a5a864ac61c7a
riku <riku@tanide.net>
2020-06-01 13:38:13 +0900

    【AC】[AtCoder Regular Contest 036]D - 偶数メートル

---
+++ arc036/arc036_d.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const long double EPS = 1e-9;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<char> &o) {
+    string a;
+    in >> a;
+    for (char c : a) o.insert(c);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+
+class UnionFind {
+public:
+    // 親の番号を格納する。親だった場合-size
+    vector<int> parents;
+
+    UnionFind(int n) {
+        parents = vector<int>(n, -1);
+    }
+
+    // aがどのグループに属しているか
+    int root(int a) {
+        if (parents[a] < 0) {
+            return a;
+        }
+        return parents[a] = root(parents[a]);
+    }
+
+    int size(int a) {
+        return -parents[root(a)];
+    }
+
+    // aとbをくっつける
+    bool connect(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        if (ra == rb) {
+            return false;
+        }
+        // 大きいほうにA
+        if (size(ra) < size(rb)) {
+            swap(ra, rb);
+        }
+        parents[ra] += parents[rb];
+        parents[rb] = ra;
+        return true;
+    }
+
+    bool is_union(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        return ra == rb;
+    }
+};
+
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+
+    UnionFind uf(2 * n);
+
+    rep(_, q) {
+        int w, x, y, z;
+        cin >> w >> x >> y >> z;
+        x--;
+        y--;
+        if (w == 1) {
+            if (z % 2 == 0) {
+                uf.connect(2 * x, 2 * y);
+                uf.connect(2 * x + 1, 2 * y + 1);
+            } else {
+                uf.connect(2 * x + 1, 2 * y);
+                uf.connect(2 * x, 2 * y + 1);
+            }
+        } else {
+            bool b = uf.is_union(2 * x, 2 * y);
+            string ans = b ? "YES" : "NO";
+            cout << ans << endl;
+        }
+    }
+
+}
