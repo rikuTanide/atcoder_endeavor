d9b465a64b34f38814e5063b2a86158df3b79121
riku <riku@tanide.net>
2020-06-01 19:45:11 +0900

    【RE】[AtCoder Regular Contest 043]C - 転倒距離

--- arc043/arc043_c.cpp
+++ arc043/arc043_c.cpp
@@ -42,19 +42,68 @@ bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
-int bubble_sort(vector<ll> v) {
-    int n = v.size();
-    int ans = 0;
-    for (int i = 0; i < (n - 1); i++) {
-        for (int j = n - 1; j > i; j--) {
-            if (v[j] < v[j - 1]) {
-                swap(v[j], v[j - 1]);
-                ans++;
-            }
+template<class T, class F>
+struct SegmentTree {
+    F f;
+    T ti;
+    vector<T> dat;
+    int sz;
+
+    SegmentTree(const F &f, const T &ti) : f(f), ti(ti) {}
+
+    void build(const vector<T> &v) {
+        assert(v.size());
+        sz = 1;
+        while (sz < v.size())sz <<= 1;
+        dat.resize(sz << 1, ti);
+        for (int i = 0; i < v.size(); i++)dat[sz - 1 + i] = v[i];
+        for (int i = sz - 2; i >= 0; i--)dat[i] = f(dat[i * 2 + 1], dat[i * 2 + 2]);
+    }
+
+    inline void update(int k, T x) {
+        k += sz - 1;
+        dat[k] = x;
+        while (k) {
+            k = (k - 1) / 2;
+            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
         }
     }
 
-    return ans;
+    inline void add(int k, int x) {
+        k += sz - 1;
+        dat[k] = f(dat[k], x);
+        while (k) {
+            k = (k - 1) / 2;
+            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
+        }
+    }
+
+    inline T query(int a, int b) {
+        return query(a, b, 0, 0, sz);
+    }
+
+    T query(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l)return ti;
+        if (a <= l && r <= b)return dat[k];
+        return f(query(a, b, k * 2 + 1, l, (l + r) / 2), query(a, b, k * 2 + 2, (l + r) / 2, r));
+    }
+};
+
+
+vector<ll> bubble_sort(vector<ll> v) {
+    int n = v.size();
+
+    auto f = [](ll i, ll j) { return i + j; };
+    SegmentTree<ll, decltype(f)> segmentTree(f, 0);
+    vector<ll> u(n);
+    segmentTree.build(u);
+
+    vector<ll> inv(n);
+    for (int i = n - 1; i >= 0; i--) {
+        inv[v[i]] = segmentTree.query(0, v[i]);
+        segmentTree.add(v[i], 1);
+    }
+    return inv;
 }
 
 vector<ll> bubble_sort2(vector<ll> v, int tts) {
@@ -90,7 +139,9 @@ int main() {
     rep(i, n) b[i] = m[b[i]];
 
 
-    int tts = bubble_sort(b);
+    vector<ll> inv = bubble_sort(b);
+    ll tts = accumulate(inv.begin(), inv.end(), 0ll);
+
     if (tts % 2 == 1) {
         cout << -1 << endl;
         ret();
