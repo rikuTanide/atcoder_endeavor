58c21fb28e5ccbe324ba046384809d1fa59d07c9
riku <riku@tanide.net>
2020-06-02 22:46:57 +0900

    【RE】[AtCoder Beginner Contest 033]D - 三角形の分類

--- abc033/abc033_d.cpp
+++ abc033/abc033_d.cpp
@@ -1,156 +1,105 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
-typedef long long ll;
-//typedef unsigned long long ll;
 
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll>> PQ_ASK;
-const int mod = 1000000007;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-struct Point {
-    int x, y;
-};
+typedef long double ld;
+typedef pair<ld, ld> P;
 
-struct Angle {
-    int x, y;
-    double tan;
-};
+bool equal(ld a, ld b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+ld diff(P a, P b) {
+    ld x = a.first - b.first;
+    ld y = a.second - b.second;
+    ld xx = x * x;
+    ld yy = y * y;
+
+    return sqrtl(xx + yy);
+}
 
 int main() {
-    ll n;
+    int n;
     cin >> n;
-    vector<Point> points(n);
+    assert(n <= 100);
 
-    rep(i, n) {
-        cin >> points[i].x;
-        cin >> points[i].y;
+
+    vector<P> v(n);
+    for (P &p : v) cin >> p.first >> p.second;
+
+    int j = 0, k = 0, l = 0;
+
+    for (int x = 0; x < n; x++) {
+        for (int y = x + 1; y < n; y++) {
+            for (int z = y + 1; z < n; z++) {
+
+
+                P a = v[x];
+                P b = v[y];
+                P c = v[z];
+
+                ld e1 = diff(a, b);
+                ld e2 = diff(b, c);
+                ld e3 = diff(c, a);
+
+                vector<ld> tmp = {e1, e2, e3};
+                sort(tmp.begin(), tmp.end());
+                e1 = tmp[0];
+                e2 = tmp[1];
+                e3 = tmp[2];
+
+                ld aabb = e1 * e1 + e2 * e2;
+                ld cc = e3 * e3;
+
+                if (equal(aabb, cc)) {
+                    k++;
+                } else if (cc < aabb) {
+                    j++;
+                } else {
+                    l++;
+                }
+
+
+            }
+        }
     }
 
-    ll chokaku_count = 0;
-    ll donkaku_count = 0;
-
-    double pi = atan2(0.0, -1.0);
-
-    rep(i, n) {
-        // iを中心としたときの各頂点のatan2
-
-        int cx = points[i].x;
-        int cy = points[i].y;
-
-        vector<Angle> angles((n - 1) * 2);
-
-        rep(j, n) {
-            if (i == j) {
-                angles[j].tan = pi * 3;
-                continue;
-            }
-            int x = points[j].x;
-            int y = points[j].y;
-            x -= cx;
-            y -= cy;
-            double t = atan2((double) y, (double) x);
-
-            angles[j].x = x;
-            angles[j].y = y;
-            angles[j].tan = t;
-
-        }
-
-        sort(angles.begin(), angles.begin() + n, [](Angle &a, Angle &b) {
-            return a.tan < b.tan;
-        });
-
-        for (int j = 0; j < n - 1; j++) {
-            angles[j + (n - 1)].x = angles[j].x;
-            angles[j + (n - 1)].y = angles[j].y;
-            angles[j + (n - 1)].tan = angles[j].tan + (2 * pi);
-        }
-
-        auto getShogen = [&](int x, int y) {
-            if (0 <= x && 0 < y) {
-                return 1;
-            }
-            if (x < 0 && 0 <= y) {
-                return 2;
-            }
-            if (y < 0 && x <= 0) {
-                return 3;
-            }
-            if (0 < x && y <= 0) {
-                return 4;
-            }
-
-            __throw_runtime_error("nai");
-        };
-
-        auto isChokkaku = [&](int a, int b) {
-            int ax = angles[a].x;
-            int ay = angles[a].y;
-            int bx = angles[b].x;
-            int by = angles[b].y;
-
-            int as = getShogen(ax, ay);
-            int bs = getShogen(bx, by);
-            if (as == 1 && bs != 4) return false;
-            if (as == 2 && bs != 1) return false;
-            if (as == 3 && bs != 2) return false;
-            if (as == 4 && bs != 3) return false;
-
-            return (ax * bx + ay * by) == 0;
-        };
-
-        auto isEikaku = [&](int a, int b) {
-            return angles[a].tan - angles[b].tan < (pi / 2);
-        };
-
-        auto isDonkaku = [&](int a, int b) {
-            return angles[a].tan - angles[b].tan < pi;
-        };
-
-        int front_90 = 0;
-        int front_180 = 0;
-        for (int back = 0; back < n - 1; back++) {
-            while ((!isChokkaku(front_90, back)) && isEikaku(front_90, back)) {
-                front_90++;
-            }
-            front_180 = front_90;
-            while (isDonkaku(front_180, back)) {
-                front_180++;
-            }
-            donkaku_count += front_180 - front_90;
-            if (isChokkaku(front_90, back)) {
-                chokaku_count++;
-                donkaku_count--;
-            }else {
-                int a = 0;
-            }
-        }
-
-    }
-
-    ll eikaku = n * (n - 1) * (n - 2) / 6 - donkaku_count - chokaku_count;
-
-    cout << eikaku << ' ' << chokaku_count << ' ' << donkaku_count << endl;
+    printf("%d %d %d\n", j, k, l);
 
 }
