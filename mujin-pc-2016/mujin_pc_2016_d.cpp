17454e46f816e8bb84e642952d6a9976b11fe239
riku <riku@tanide.net>
2020-06-03 17:30:26 +0900

    【WA】[MUJIN プログラミングチャレンジ Programming Challenge]D - Parenthesis Sequence

--- mujin-pc-2016/mujin_pc_2016_d.cpp
+++ mujin-pc-2016/mujin_pc_2016_d.cpp
@@ -44,9 +44,10 @@ typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 class CumulativeSum {
     vector<ll> numbers;
-    vector<ll> sums;
 
 public:
+    vector<ll> sums;
+
     CumulativeSum(int n) {
         numbers.resize(n);
         sums.resize(n);
@@ -74,13 +75,63 @@ public:
 
 };
 
+template<class T, class F>
+struct SegmentTree {
+    F f;
+    T ti;
+    vector<T> dat;
+    int sz;
+
+    SegmentTree(const F &f, const T &ti) : f(f), ti(ti) {}
+
+    void build(const vector<T> &v) {
+        assert(v.size());
+        sz = 1;
+        while (sz < v.size())sz <<= 1;
+        dat.resize(sz << 1, ti);
+        for (int i = 0; i < v.size(); i++)dat[sz - 1 + i] = v[i];
+        for (int i = sz - 2; i >= 0; i--)dat[i] = f(dat[i * 2 + 1], dat[i * 2 + 2]);
+    }
+
+    inline void update(int k, T x) {
+        k += sz - 1;
+        dat[k] = x;
+        while (k) {
+            k = (k - 1) / 2;
+            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
+        }
+    }
+
+    inline void add(int k, int x) {
+        k += sz - 1;
+        dat[k] = f(dat[k], x);
+        while (k) {
+            k = (k - 1) / 2;
+            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
+        }
+    }
+
+    inline T query(int a, int b) {
+        return query(a, b, 0, 0, sz);
+    }
+
+    T query(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l)return ti;
+        if (a <= l && r <= b)return dat[k];
+        return f(query(a, b, k * 2 + 1, l, (l + r) / 2), query(a, b, k * 2 + 2, (l + r) / 2, r));
+    }
+};
+
 
 int solve(int n, int l, int r,
           CumulativeSum &qc_sum,
           CumulativeSum &oc_sum,
           CumulativeSum &cc_sum,
           CumulativeSum &o_depth_sum,
-          CumulativeSum &c_depth_sum) {
+          CumulativeSum &c_depth_sum,
+          SegmentTree<ll, function<ll(ll, ll)>> &ost,
+          SegmentTree<ll, function<ll(ll, ll)>> &cst
+) {
 
     int sz = r - l + 1;
 
@@ -109,7 +160,7 @@ int solve(int n, int l, int r,
         while (floor + 1 < ceil) {
             int mid = (floor + ceil) / 2;
             bool b = check(mid);
-            if(b) ceil = mid;
+            if (b) ceil = mid;
             else floor = mid;
         }
         return ceil;
@@ -143,6 +194,22 @@ int solve(int n, int l, int r,
         return ans;
     }();
 
+
+    int _l_min = [&] {
+        return ost.query(l, p + 1);
+    }();
+
+    int _r_min = [&] {
+        return cst.query(p + 1, r + 1);
+    }();
+
+    if (_l_min != l_min) {
+        cout << endl;
+    }
+    if (_r_min != r_min) {
+        cout << endl;
+    }
+
     if (l_min < l_high) return false;
     if ((r_min - c_depth_sum.getSectionSum(0, p)) + ph < 0) return false;
 
@@ -172,12 +239,30 @@ int main() {
     o_depth_sum.build();
     c_depth_sum.build();
 
+
+    auto mf = [](ll i, ll j) { return min(i, j); };
+    SegmentTree<ll, function<ll(ll, ll)>> ost(mf, INF), cst(mf, INF);
+    ost.build(o_depth_sum.sums);
+    cst.build(c_depth_sum.sums);
+
+
     rep(_, q) {
         int l, r;
         cin >> l >> r;
         l--;
         r--;
-        bool b = solve(n, l, r, qc_sum, oc_sum, cc_sum, o_depth_sum, c_depth_sum);
+        bool b = solve(
+                n,
+                l,
+                r,
+                qc_sum,
+                oc_sum,
+                cc_sum,
+                o_depth_sum,
+                c_depth_sum,
+                ost,
+                cst
+        );
         string ans = b ? "Yes" : "No";
         cout << ans << endl;
     }
