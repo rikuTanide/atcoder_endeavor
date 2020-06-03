fd522e2a9a133bfd2a54a1900a2abe7b8b7ebe0e
riku <riku@tanide.net>
2020-06-03 16:28:39 +0900

    【RE】[MUJIN プログラミングチャレンジ Programming Challenge]D - Parenthesis Sequence

--- mujin-pc-2016/mujin_pc_2016_d.cpp
+++ mujin-pc-2016/mujin_pc_2016_d.cpp
@@ -42,39 +42,84 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-int solve(string &s, int n, int l, int r) {
-    int sz = r - l + 1;
-    string t = s.substr(l, sz);
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
 
-    int qc = 0, oc = 0, cc = 0;
-    for (char c : t) {
-        if (c == '?') qc++;
-        else if (c == '(')oc++;
-        else cc++;
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
     }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return sums.back();
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void build() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
+
+
+int solve(string &s, int n, int l, int r,
+          CumulativeSum &qc_sum,
+          CumulativeSum &oc_sum,
+          CumulativeSum &cc_sum,
+          CumulativeSum &o_depth_sum,
+          CumulativeSum &c_depth_sum) {
+
+    int sz = r - l + 1;
+//    string t = s.substr(l, sz);
+
+    int
+            qc = qc_sum.getSectionSum(l, r),
+            oc = oc_sum.getSectionSum(l, r),
+            cc = cc_sum.getSectionSum(l, r);
 
 
     int ma = sz / 2;
     int ob = ma - oc;
     int cb = ma - ob;
 
-    for (char &c : t) {
-        if (c != '?') continue;
-        if (ob > 0) c = '(', ob--;
-        else c = ')', cb--;
-    }
+    // 切り替え地点を見つける
+    int p = [&] {
+        for (int i = l; i <= r; i++) {
+            int now = qc_sum.getSectionSum(l, i);
+            if (now == ob) return i;
+        }
+        return -1;
+    }();
+
+    if (p == -1) return false;
 
     int depth = 0;
-    for (char c : t) {
-        if (c == '(') depth++;
-        if (c == ')') {
-            depth--;
-            if (depth < 0) {
-                return false;
-            }
+    for (int i = l; i <= r; i++) {
+        char c = s[i];
+        if (i <= p) {
+            if (c == '?') depth++;
+            else if (c == '(') depth++;
+            else depth--;
+        } else {
+            if (c == '?') depth--;
+            else if (c == '(') depth++;
+            else depth--;
         }
+        if (depth < 0) return 0;
     }
-
     return depth == 0;
 }
 
@@ -85,12 +130,27 @@ int main() {
 
     assert(n <= 1000);
 
+    CumulativeSum qc_sum(n), oc_sum(n), cc_sum(n), o_depth_sum(n), c_depth_sum(n);
+
+    rep(i, n) if (s[i] == '?') qc_sum.set(i, 1);
+    rep(i, n) if (s[i] == '(') oc_sum.set(i, 1);
+    rep(i, n) if (s[i] == ')') cc_sum.set(i, 1);
+
+    rep(i, n) o_depth_sum.set(i, (s[i] == '?' || s[i] == '(') ? 1 : -1);
+    rep(i, n) c_depth_sum.set(i, (s[i] == '?' || s[i] == ')') ? 1 : -1);
+
+    qc_sum.build();
+    oc_sum.build();
+    cc_sum.build();
+    o_depth_sum.build();
+    c_depth_sum.build();
+
     rep(_, q) {
         int l, r;
         cin >> l >> r;
         l--;
         r--;
-        bool b = solve(s, n, l, r);
+        bool b = solve(s, n, l, r, qc_sum, oc_sum, cc_sum, o_depth_sum, c_depth_sum);
         string ans = b ? "Yes" : "No";
         cout << ans << endl;
     }
