b5d72e5c5aa4f6ac3951eb17ff432601329b3998
riku <riku@tanide.net>
2020-06-06 09:29:30 +0900

    【TLE】[AtCoder Regular Contest 068]E - Snuke Line

--- arc068/arc068_c.cpp
+++ arc068/arc068_c.cpp
@@ -1,26 +1,20 @@
 #include <bits/stdc++.h>
-//#include <boost/multiprecision/cpp_int.hpp>
-//namespace mp = boost::multiprecision;
-
-using namespace std;
 
 const double PI = 3.14159265358979323846;
+using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-double equal(double a, double b) {
-    return fabs(a - b) < DBL_EPSILON;
-}
-
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    int a;
+    ll a;
     in >> a;
     o.insert(a);
     return in;
@@ -35,29 +29,81 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+//const ll mod = 1e10;
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return sums.back();
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void calculate() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
 
 int main() {
+
     int n, m;
     cin >> n >> m;
 
     vector<P> v(n);
-    for (P &p:v)cin >> p.first >> p.second;
+    for (P &p : v) cin >> p.first >> p.second;
 
-    rep(i, m + 1) {
-        if (i == 0) continue;
-        set<int> s;
-        for (int j = 0; j <= m; j += i) {
-            rep(k, n) {
-                P p = v[k];
-                if (p.first <= j && j <= p.second) s.insert(k);
+    // 小さい順
+    // 含むを減らしていく
+    // 飛ぶを増やしていく
+
+
+    for (int k = 1; k <= m; k++) {
+
+        int a = 0, b = 0;
+        vector<int> imos(m + 2, 0);
+        for (P p : v) {
+            if (p.second - p.first + 1 >= k) {
+                a++;
+            } else {
+                imos[p.first]++;
+                imos[p.second + 1]--;
             }
         }
-        cout << s.size() << endl;
+        CumulativeSum cs(m + 2);
+        rep(i, m + 1) cs.set(i, imos[i]);
+        cs.calculate();
+
+        for (int j = 0; j <= m; j += k) {
+            b += cs.getSum(j);
+        }
+
+        cout << a + b << endl;
+
     }
+
 }
