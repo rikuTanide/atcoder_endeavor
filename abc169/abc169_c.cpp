8caaf6de1dfdfebb5ec903ad7d256cd86f6f82d1
riku <riku@tanide.net>
2020-06-08 11:14:00 +0900

    【AC】[AtCoder Beginner Contest 169]C - Multiplication 3

--- abc169/abc169_c.cpp
+++ abc169/abc169_c.cpp
@@ -1,29 +1,22 @@
 #include <bits/stdc++.h>
-//#include <boost/multiprecision/cpp_int.hpp>
-//namespace mp = boost::multiprecision;
-
-using namespace std;
 
 const double PI = 3.14159265358979323846;
+using namespace std;
 typedef long long ll;
-const long double EPS = 1e-9;
-
+const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-double equal(double a, double b) {
-    return fabs(a - b) < DBL_EPSILON;
-}
-
-std::istream &operator>>(std::istream &in, set<char> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
     in >> a;
-    for (char c : a) o.insert(c);
+    o.insert(a);
     return in;
 }
 
@@ -34,20 +27,30 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
 
+//const ll mod = 1e10;
 typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
-
 int main() {
-    long double a, b;
+    ll a;
+    string b;
     cin >> a >> b;
+    string d = "";
+    for (char c : b) {
+        if (c == '.') continue;
+        d.push_back(c);
+    }
 
-    unsigned long long ans = a * b;
-    cout << ans << endl;
+    ll k = atoi(d.c_str());
+
+    ll f = a * k;
+
+    cout << f / 100 << endl;
 
 }
