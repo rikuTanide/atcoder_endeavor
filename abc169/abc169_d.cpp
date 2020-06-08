118c2d4c168ab00655b339eb52c9594ea6f0d6f8
riku <riku@tanide.net>
2020-06-08 11:19:44 +0900

    【AC】[AtCoder Beginner Contest 169]D - Div Game

--- abc169/abc169_d.cpp
+++ abc169/abc169_d.cpp
@@ -1,29 +1,22 @@
 #include <bits/stdc++.h>
-//#include <boost/multiprecision/cpp_int.hpp>
-//namespace mp = boost::multiprecision;
-
-using namespace std;
 
 const double PI = 3.14159265358979323846;
+using namespace std;
 typedef long long ll;
-const long double EPS = 1e-9;
-
+const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-double equal(double a, double b) {
-    return fabs(a - b) < DBL_EPSILON;
-}
-
-std::istream &operator>>(std::istream &in, set<char> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
     in >> a;
-    for (char c : a) o.insert(c);
+    o.insert(a);
     return in;
 }
 
@@ -34,16 +27,15 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
 
+//const ll mod = 1e10;
 typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-
-
-const ll MAX = 1000000000000;
 
 map<ll, int> factorize(ll n) {
     map<ll, int> res;
@@ -63,34 +55,27 @@ map<ll, int> factorize(ll n) {
     return res;
 }
 
-ll rec(ll n) {
-    auto f = factorize(n);
-
-    int ans = 0;
-
-    for (auto &e : f) {
-
-        ll a = e.first;
-        ll b = e.second;
-
-        int r = 1;
-
-        while (r <= b) {
-            b -= r;
-            r++;
-            ans++;
-        }
-    }
-
-    return ans;
-
-}
 
 int main() {
-
     ll n;
     cin >> n;
 
-    cout << rec(n) << endl;
+    auto f = factorize(n);
+
+    int ans = 0;
+    for (auto &e : f) {
+
+        int k = e.second;
+        int j = 1;
+        while (k >= j) {
+            ans++;
+            k -= j;
+            j++;
+        }
+
+    }
+
+    cout << ans << endl;
+
 
 }
