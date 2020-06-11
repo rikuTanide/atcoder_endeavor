544fcb729309e1b752be17a094e28d16a1ebb0f2
riku <riku@tanide.net>
2020-06-11 17:46:55 +0900

    【TLE】[CODE FESTIVAL 2017 qual C]D - Yet Another Palindrome Partitioning

--- code-festival-2017-qualc/code_festival_2017_qualc_d.cpp
+++ code-festival-2017-qualc/code_festival_2017_qualc_d.cpp
@@ -1,26 +1,20 @@
 #include <bits/stdc++.h>
-//#include <boost/multiprecision/cpp_int.hpp>
-//namespace mp = boost::multiprecision;
-
-using namespace std;
 
 const double PI = 3.14159265358979323846;
+using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-double equal(double a, double b) {
-    return fabs(a - b) < DBL_EPSILON;
-}
-
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    int a;
+    ll a;
     in >> a;
     o.insert(a);
     return in;
@@ -33,42 +27,45 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+
 //const ll mod = 1e10;
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
-typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-
-int rev(int bit, int i) {
-    return bit ^ (1 << i);
+bool is_kaibun(string &s, int i, int j) {
+    set<char> st;
+    for (int k = i; k <= j; k++) {
+        if (contain(st, s[k])) st.erase(s[k]);
+        else st.insert(s[k]);
+    }
+    return st.size() <= 1;
 }
 
 int main() {
     string s;
     cin >> s;
-
     int n = s.size();
-
-    vector<int> dp(n + 1, INT_MAX / 10);
-    vector<int> mval(1ll << 26, INT_MAX / 10);
-
+    vector<int> dp(n + 1, -1);
     dp[0] = 0;
-    mval[0] = 0;
 
-    int par = 0;
-    for (int i = 1; i <= n; i++) {
-        par = rev(par, s[i - 1] - 'a');
-        cmin(dp[i], mval[par] + 1);
-        for (int j = 0; j < 26; j++) {
-            par = rev(par, j);
-            cmin(dp[i], mval[par] + 1);
-            par = rev(par, j);
+    auto set = [&](int i, int k) {
+        if (dp[i] == -1) dp[i] = k;
+        else
+            cmin(dp[i], k);
+    };
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            if (is_kaibun(s, i, j)) set(j + 1, dp[i] + 1);
         }
-        cmin(mval[par], dp[i]);
     }
+
     cout << dp[n] << endl;
+
 }
