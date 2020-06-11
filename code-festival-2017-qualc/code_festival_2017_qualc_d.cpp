ad0cce99c9faee2add36f258e7b54bdb349ab469
riku <riku@tanide.net>
2020-06-11 21:32:58 +0900

    【TLE】[CODE FESTIVAL 2017 qual C]D - Yet Another Palindrome Partitioning

--- code-festival-2017-qualc/code_festival_2017_qualc_d.cpp
+++ code-festival-2017-qualc/code_festival_2017_qualc_d.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -27,19 +33,17 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
 //const ll mod = 1e10;
-typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 bool is_kaibun(vector<int> &hs, int i, int j) {
-    int k = hs[i] ^hs[j + 1];
+    int k = hs[j] ^(i == 0 ? 0 : hs[i-1]);
     int f = __builtin_popcount(k);
     return f <= 1;
 }
@@ -49,21 +53,24 @@ int main() {
     cin >> s;
     int n = s.size();
 
-    vector<int> hs(n + 1, 0);
+    vector<int> hs(n, 0);
 
-    rep(i, n + 1) {
-        if (i == 0) continue;
-
-        char c = s[i - 1];
-        int j = c - 'a';
-
-        int next = hs[i - 1] ^(1 << j);
-        hs[i] = next;
+    rep(i, n) {
+        if (i == 0) {
+            char c = s[i];
+            int j = c - 'a';
+            int next = 1 << j;
+            hs[0] = next;
+        } else {
+            char c = s[i];
+            int j = c - 'a';
+            int next = hs[i - 1] ^(1 << j);
+            hs[i] = next;
+        }
     }
 
 
-    vector<int> dp(n + 1, -1);
-    dp[0] = 0;
+    vector<int> dp(n, -1);
 
     auto set = [&](int i, int k) {
         if (dp[i] == -1) dp[i] = k;
@@ -71,12 +78,14 @@ int main() {
             cmin(dp[i], k);
     };
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {
-            if (is_kaibun(hs, i, j)) set(j + 1, dp[i] + 1);
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i <= j; i++) {
+            if (is_kaibun(hs, i, j)) {
+                set(j, (i == 0 ? 0 : dp[i - 1]) + 1);
+            }
         }
     }
 
-    cout << dp[n] << endl;
+    cout << dp[n - 1] << endl;
 
 }
