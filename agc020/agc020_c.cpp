46e704e99d0c79f73bc002ded10294c1efab65e0
riku <riku@tanide.net>
2020-06-12 12:44:57 +0900

    【WA】[AtCoder Grand Contest 020]C - Median Sum

---
+++ agc020/agc020_c.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    rep(i, n) cin >> v[i];
+
+    vector<ll> cs(1 << n);
+
+    rep(i, 1 << n) {
+        ll sum = 0;
+        rep(j, n)if ((i >> j) & 1) sum += v[j];
+        cs[i] = sum;
+    }
+
+    sort(cs.begin(), cs.end());
+    cout << cs[cs.size() / 2] << endl;
+
+}
