7ef327897b281d90a3e2bea06223405d157dfef9
riku <riku@tanide.net>
2020-06-13 17:19:45 +0900

    【RE】[AtCoder Regular Contest 099]E - Independence

--- arc099/arc099_c.cpp
+++ arc099/arc099_c.cpp
@@ -38,44 +38,113 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
 
+class UnionFind {
+public:
+    // 親の番号を格納する。親だった場合-size
+    vector<int> parents;
+
+    UnionFind(int n) {
+        parents = vector<int>(n, -1);
+    }
+
+    // aがどのグループに属しているか
+    int root(int a) {
+        if (parents[a] < 0) {
+            return a;
+        }
+        return parents[a] = root(parents[a]);
+    }
+
+    int size(int a) {
+        return -parents[root(a)];
+    }
+
+    // aとbをくっつける
+    bool connect(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        if (ra == rb) {
+            return false;
+        }
+        // 大きいほうにA
+        if (size(ra) < size(rb)) {
+            swap(ra, rb);
+        }
+        parents[ra] += parents[rb];
+        parents[rb] = ra;
+        return true;
+    }
+
+};
+
+bool is_2(int i, vector<vector<bool>> &edges) {
+    int n = edges.size();
+    vector<int> costs(n, -1);
+    costs[i] = 0;
+
+    queue<int> q;
+    q.push(i);
+
+    while (!q.empty()) {
+        int from = q.front();
+        q.pop();
+        rep(to, n) {
+            if (from == to) continue;
+            else if (!edges[from][to]) continue;
+            else if (costs[to] != -1) {
+                int to_cost = costs[to];
+                int from_cost = costs[from];
+
+                if (to_cost == (from_cost ^ 1)) {
+                    continue;
+                } else {
+                    return false;
+                }
+            } else {
+                int from_cost = costs[from];
+                int to_cost = from_cost ^1;
+                costs[to] = to_cost;
+                q.push(to);
+            }
+        }
+    }
+    return true;
+}
+
 int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<bool>> edges(n, vector<bool>(n, false));
+    vector<vector<bool>> edges(n, vector<bool>(n, true));
     rep(i, m) {
         int a, b;
         cin >> a >> b;
         a--;
         b--;
 
-        edges[a][b] = true;
-        edges[b][a] = true;
+        edges[a][b] = false;
+        edges[b][a] = false;
     }
 
-    auto check = [&](vector<int> &as) -> bool {
-        int n = as.size();
-        rep(i, n) {
-            rep(j, n) {
-                if (i == j) continue;
-                if (!edges[as[i]][as[j]]) return false;
-            }
-        }
-        return true;
-    };
 
-    ll ans = INF;
-    rep(i, 1 << n) {
-        vector<int> as, bs;
-        rep(j, n) if ((i >> j) & 1) as.push_back(j); else bs.push_back(j);
-        if (check(as) && check(bs)) {
-            ll ac = as.size(), bc = bs.size();
-            ll now = ac * (ac - 1) / 2 + bc * (bc - 1) / 2;
-            cmin(ans, now);
+    UnionFind uf(n);
+
+    rep(i, n) {
+        rep(j, n) {
+            if (edges[i][j])uf.connect(i, j);
         }
     }
 
-    ans = ans == INF ? -1 : ans;
-    cout << ans << endl;
+
+    rep(i, n) {
+        if (uf.root(i) != i) continue;
+        bool b = is_2(i, edges);
+        if (!b) {
+            cout << -1 << endl;
+            ret();
+        }
+    }
+
+    assert(false);
 
 }
