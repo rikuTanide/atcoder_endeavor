b97ed7b9af174abdcf0e292d55a1ccafaca90917
riku <riku@tanide.net>
2020-06-13 15:23:28 +0900

    【AC】[AtCoder Grand Contest 025]C - Interval Game

---
+++ agc025/agc025_c.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+
+int main() {
+
+    int n;
+    cin >> n;
+
+
+    vector<ll> left(n + 1), right(n + 1);
+    rep(i, n) cin >> left[i] >> right[i];
+
+    sort(left.rbegin(), left.rend());
+    sort(right.begin(), right.end());
+
+    ll ans = 0;
+    rep(i, n + 1) {
+        if (left[i] > right[i]) {
+            ans += 2 * (left[i] - right[i]);
+        }
+    }
+    cout << ans << endl;
+}
