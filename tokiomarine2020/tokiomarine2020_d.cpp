3bbfeef9e7138903871a63c927fd3f09483801af
riku <riku@tanide.net>
2020-06-13 22:00:03 +0900

    【TLE】[東京海上日動　プログラミングコンテスト2020]D - Knapsack Queries on a tree

---
+++ tokiomarine2020/tokiomarine2020_d.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+struct Item {
+    ll v, w;
+};
+
+
+std::istream &operator>>(std::istream &in, Item &o) {
+    cin >> o.v >> o.w;
+    return in;
+}
+
+// 全列挙
+// 半全列挙
+// map
+
+vector<int> create_candidates(int v) {
+    vector<int> u;
+    u.push_back(v);
+    while (v > 0) {
+        v = (v + 1) / 2 - 1;
+        u.push_back(v);
+    }
+    return u;
+}
+
+int knapsack(vector<Item> &items, int l) {
+    int n = items.size();
+    int ans = 0;
+    rep(i, 1 << n) {
+        int values = 0, weight = 0;
+
+        rep(j, n) if ((i >> j) & 1) {
+                values += items[j].v;
+                weight += items[j].w;
+            }
+        if (weight > l) continue;
+        cmax(ans, values);
+    }
+    return ans;
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+    vector<Item> items(n);
+    rep(i, n) cin >> items[i];
+
+
+    int q;
+    cin >> q;
+
+    rep(_, q) {
+        int v, l;
+        cin >> v >> l;
+
+        vector<int> candidates = create_candidates(v - 1);
+
+        vector<Item> use_items(candidates.size());
+        rep(i, candidates.size()) use_items[i] = items[candidates[i]];
+        int ans = knapsack(use_items, l);
+        cout << ans << endl;
+    }
+}
