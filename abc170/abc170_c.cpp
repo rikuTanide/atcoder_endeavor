4e2dc04d3798ec00460d205ee73030c42fe46556
riku <riku@tanide.net>
2020-06-14 21:07:11 +0900

    【AC】[AtCoder Beginner Contest 170]C - Forbidden List

---
+++ abc170/abc170_c.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int x, n;
+    cin >> x >> n;
+    set<int> s;
+    rep(i, n) cin >> s;
+
+    rep(i, 1000) {
+        int j = x - i;
+        if (!contain(s, j)) {
+            cout << j << endl;
+            ret();
+        }
+        int k = x + i;
+        if (!contain(s, k)) {
+            cout << k << endl;
+            ret();
+        }
+    }
+
+}
