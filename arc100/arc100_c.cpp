f0aba05f97890d087dd4290a41d44726b5b22c88
riku <riku@tanide.net>
2020-06-14 12:22:26 +0900

    【AC】[AtCoder Regular Contest 100]E - Or Plus Max

---
+++ arc100/arc100_c.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+void chmax(P &a, P b) {
+    if (a.first < b.first) a.second = max(a.first, b.second), a.first = b.first;
+    else a.second = max(a.second, b.first);
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<ll> a(1 << n);
+    vector<P> dp(1 << n);
+
+    for (int bit = 0; bit < (1 << n); bit++) {
+        cin >> a[bit];
+        dp[bit] = {a[bit], -INF};
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int bit = 0; bit < (1 << n); bit++) {
+            if (bit & (1 << i)) chmax(dp[bit], dp[bit ^ (1 << i)]);
+        }
+    }
+    ll ans = -INF;
+    rep(bit, 1 << n) {
+        ll tmp = dp[bit].first + dp[bit].second;
+        cmax(ans, tmp);
+        if (bit) cout <<ans << endl;
+    }
+
+}
