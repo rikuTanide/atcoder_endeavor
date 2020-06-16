b97e4bc8c64fbf16dcd6c6caa04671e782b8f8dd
riku <riku@tanide.net>
2020-06-17 01:10:39 +0900

    【AC】[AtCoder Grand Contest 031]C - Differ by 1 Bit

--- agc031/agc031_c.cpp
+++ agc031/agc031_c.cpp
@@ -43,67 +43,72 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int bpc(int i) {
-    return __builtin_popcount(i);
-}
-
-vector<int> search_x(int a, int b) {
-    vector<int> ans;
-    rep(i, 32) {
-        if (a % 2 != b % 2) ans.push_back(i);
-        a /= 2;
-        b /= 2;
-    }
-    assert(!ans.empty());
-    return ans;
+    return __builtin_popcount(i) % 2;
 }
 
 bool bit(int n, int i) {
     return (n >> i) & 1;
 }
 
-vector<int> rec(vector<int> &v, int a, int b) {
+int search_x(int a, int b) {
+    rep(i, 32) {
+        if (bit(a, i) != bit(b, i)) return i;
+    }
+    __throw_runtime_error("konaidene");
+}
+
+
+vector<int> rec(vector<int> &v, set<int> &old_x, int a, int b) {
+    assert(bpc(a) != bpc(b));
     assert(find(v.begin(), v.end(), a) != v.end());
     assert(find(v.begin(), v.end(), b) != v.end());
 
     if (v.size() == 2) return {a, b};
 
-    vector<int> xs = search_x(a, b);
-    for (int x : xs) {
-        vector<int> u1, u2;
-        u1.push_back(a);
-        for (int i : v) {
-            if (i == a || i == b) continue;
-            if (bit(i, x) == bit(a, x)) u1.push_back(i); else u2.push_back(i);
-        }
-        u2.push_back(b);
+    int x = search_x(a, b);
+    old_x.insert(x);
 
-        assert(u1.size() == u2.size());
+    vector<int> u1, u2;
+    u1.push_back(a);
+    for (int i : v) {
+        if (i == a || i == b) continue;
+        if (bit(i, x) == bit(a, x)) u1.push_back(i); else u2.push_back(i);
+    }
+    u2.push_back(b);
 
-        P p = [&] {
-            for (int l1 : u1) {
-                if (l1 == a) continue;
-                for (int l2 : u2) {
-                    if (l2 == b) continue;
-                    int xx = 1 << x;
-                    if ((l1 | xx) == (l2 | xx)) {
-                        return P(l1, l2);
-                    }
+    {
+        for (ll l : u1) assert(bit(l, x) == bit(a, x));
+        for (ll l : u2) assert(bit(l, x) == bit(b, x));
+    }
+
+    assert(u1.size() == u2.size());
+
+    P p = [&] {
+        for (int l1 : u1) {
+            if (l1 == a) continue;
+            if (bpc(a) == bpc(l1))continue;
+
+            for (int l2 : u2) {
+                if (l2 == b) continue;
+                if (bpc(l2) == bpc(b))continue;
+                int xx = 1 << x;
+
+                assert(bit(l1, x) != bit(l2, x));
+
+                if ((l1 | xx) == (l2 | xx)) {
+                    return P(l1, l2);
                 }
             }
-            return P(-1, -1);
-        }();
-        if (p == P(-1, -1)) {
-            continue;
         }
+        __throw_runtime_error("konaide");
+    }();
 
-        auto t1 = rec(u1, a, p.first), t2 = rec(u2, p.second, b);
-        if (t1.empty() || t2.empty()) continue;
-        for (int k : t2) t1.push_back(k);
-        return t1;
-    }
-    return {};
+    auto t1 = rec(u1, old_x, a, p.first), t2 = rec(u2, old_x, p.second, b);
+    for (int k : t2) t1.push_back(k);
+    old_x.erase(x);
+    return t1;
+
 }
-
 
 int main() {
     int n, a, b;
@@ -120,7 +125,7 @@ int main() {
     rep(i, 1 << n) v[i] = i;
 
     set<int> old_x;
-    auto ans = rec(v, a, b);
+    auto ans = rec(v, old_x, a, b);
 
     for (int i : ans) cout << i << ' ';
     cout << endl;
