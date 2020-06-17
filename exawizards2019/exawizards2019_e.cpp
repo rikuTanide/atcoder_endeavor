1246875b19302790b70d0a2ba680a02930444976
riku <riku@tanide.net>
2020-06-17 16:13:28 +0900

    【RE】[エクサウィザーズ 2019]E - Black or White

---
+++ exawizards2019/exawizards2019_e.cpp
@@ -0,0 +1,149 @@
+#include <bits/stdc++.h>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+const int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
+
+int main() {
+
+    int b, w;
+    cin >> b >> w;
+
+    vector<vector<mint>> dp(b + 1, vector<mint>(w + 1));
+    dp[b][w] = 1;
+    int n = b + w;
+
+    vector<mint> ans(n, 0);
+
+    for (int bi = b; bi >= 0; bi--) {
+        for (int wi = w; wi >= 0; wi--) {
+
+            int i = n - bi - wi;
+
+            if (bi == 0 && wi == 0)continue;
+            if (bi == 0) {
+                dp[bi][wi - 1] += dp[bi][wi];
+            } else if (wi == 0) {
+                dp[bi - 1][wi] += dp[bi][wi];
+                ans[i] += dp[bi][wi];
+            } else {
+                dp[bi - 1][wi] += (dp[bi][wi] / 2);
+                dp[bi][wi - 1] += (dp[bi][wi] / 2);
+                ans[i] += (dp[bi][wi] / 2);
+            }
+
+
+        }
+    }
+
+
+    cout << endl;
+
+    for(mint m : ans) cout << m << endl;
+
+}
