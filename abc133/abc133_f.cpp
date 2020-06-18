7cb5f46b22834860c14958b54b9027c7d8be56d4
riku <riku@tanide.net>
2020-06-18 17:16:35 +0900

    【AC】[AtCoder Beginner Contest 133]F - Colorful Tree

--- abc133/abc133_f.cpp
+++ abc133/abc133_f.cpp
@@ -39,18 +39,9 @@ typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
 
 struct Edge {
-    int from, to, color;
+    int to, color;
     ll cost;
 };
-
-
-std::istream &operator>>(std::istream &in, Edge &o) {
-    cin >> o.from >> o.to >> o.color >> o.cost;
-    o.from--;
-    o.to--;
-    o.color--;
-    return in;
-}
 
 
 ll rec(vector<vector<Edge>> &g, int start, int now, int goal, int prev) {
@@ -65,32 +56,179 @@ ll rec(vector<vector<Edge>> &g, int start, int now, int goal, int prev) {
     return -1;
 }
 
+struct Query {
+    int color;
+    ll cost;
+    int from, to;
+
+    // <edge, cost>
+    map<int, ll> costs;
+    map<int, ll> counts;
+};
+
+std::istream &operator>>(std::istream &in, Query &o) {
+    cin >> o.color >> o.cost >> o.from >> o.to;
+    o.color--;
+    o.from--;
+    o.to--;
+    return in;
+}
+
+void dfs1(vector<ll> &plain_costs, int now, vector<vector<Edge>> &edges) {
+    for (Edge e : edges[now]) {
+        int next = e.to;
+        if (plain_costs[next] != -1) continue;
+        plain_costs[next] = plain_costs[now] + e.cost;
+        dfs1(plain_costs, next, edges);
+    }
+}
+
+template<typename G>
+struct DoublingLowestCommonAncestor {
+    const int LOG;
+    vector<int> dep;
+    const G &g;
+    vector<vector<int> > table;
+
+    DoublingLowestCommonAncestor(const G &g) : g(g), dep(g.size()), LOG(32 - __builtin_clz(g.size())) {
+        table.assign(LOG, vector<int>(g.size(), -1));
+    }
+
+    void dfs(int idx, int par, int d) {
+        table[0][idx] = par;
+        dep[idx] = d;
+        for (auto &to : g[idx]) {
+            if (to != par) dfs(to, idx, d + 1);
+        }
+    }
+
+    void build() {
+        dfs(0, -1, 0);
+        for (int k = 0; k + 1 < LOG; k++) {
+            for (int i = 0; i < table[k].size(); i++) {
+                if (table[k][i] == -1) table[k + 1][i] = -1;
+                else table[k + 1][i] = table[k][table[k][i]];
+            }
+        }
+    }
+
+    int query(int u, int v) {
+        if (dep[u] > dep[v]) swap(u, v);
+        for (int i = LOG - 1; i >= 0; i--) {
+            if (((dep[v] - dep[u]) >> i) & 1) v = table[i][v];
+        }
+        if (u == v) return u;
+        for (int i = LOG - 1; i >= 0; i--) {
+            if (table[i][u] != table[i][v]) {
+                u = table[i][u];
+                v = table[i][v];
+            }
+        }
+        return table[0][u];
+    }
+};
+
+void dfs2(vector<vector<Edge>> &edges,
+          int now,
+          int prev,
+          vector<ll> &colors_costs,
+          vector<int> &color_counts,
+          vector<vector<int>> &query_indexes,
+          vector<Query> &query) {
+
+    for (int qi : query_indexes[now]) {
+        Query &q = query[qi];
+        q.costs[now] = colors_costs[q.color];
+        q.counts[now] = color_counts[q.color];
+    }
+
+    for (Edge e : edges[now]) {
+        if (e.to == prev) continue;
+        colors_costs[e.color] += e.cost;
+        color_counts[e.color]++;
+        dfs2(edges, e.to, now, colors_costs, color_counts, query_indexes, query);
+        colors_costs[e.color] -= e.cost;
+        color_counts[e.color]--;
+
+    }
+
+}
+
+template<typename T>
+struct edge {
+    int src, to;
+    T cost;
+
+    edge(int to, T cost) : src(-1), to(to), cost(cost) {}
+
+    edge(int src, int to, T cost) : src(src), to(to), cost(cost) {}
+
+    edge &operator=(const int &x) {
+        to = x;
+        return *this;
+    }
+
+    operator int() const { return to; }
+};
+
+template<typename T>
+using Edges = vector<edge<T> >;
+template<typename T>
+using WeightedGraph = vector<Edges<T> >;
+using UnWeightedGraph = vector<vector<int> >;
+template<typename T>
+using Matrix = vector<vector<T> >;
+
 int main() {
     int n, q;
     cin >> n >> q;
 
-    vector<Edge> edges(n);
-    rep(i, n - 1) cin >> edges[i];
+    vector<vector<Edge>> edges(n);
 
-    rep(_, q) {
-        int x, y, u, v;
-        cin >> x >> y >> u >> v;
+    UnWeightedGraph g(n);
+    rep(i, n - 1) {
+        int from, to, color;
+        ll cost;
+        cin >> from >> to >> color >> cost;
+        from--;
+        to--;
+        color--;
+        edges[from].push_back({to, color, cost});
+        edges[to].push_back({from, color, cost});
 
-        x--;
-        u--;
-        v--;
-
-        vector<vector<Edge>> g(n);
-        for (Edge e : edges) {
-            ll cost = (e.color == x) ? y : e.cost;
-            g[e.from].push_back({e.from, e.to, e.color, cost});
-            g[e.to].push_back({e.to, e.from, e.color, cost});
-        }
-
-
-        ll now = rec(g, u, u, v, -1);
-        cout << now << endl;
-
+        g[from].push_back(to);
+        g[to].push_back(from);
     }
 
+    DoublingLowestCommonAncestor<UnWeightedGraph> lca(g);
+    lca.build();
+
+    vector<ll> plain_costs(n, -1);
+    plain_costs[0] = 0;
+    dfs1(plain_costs, 0, edges);
+
+    vector<Query> queries(q);
+    rep(i, q) cin >> queries[i];
+
+    vector<vector<int>> query_indexes(n);
+    rep(i, q) {
+        Query q = queries[i];
+        query_indexes[q.from].push_back(i);
+        query_indexes[q.to].push_back(i);
+        int l = lca.query(q.from, q.to);
+        query_indexes[l].push_back(i);
+    }
+
+    vector<ll> colors_costs(n, 0);
+    vector<int> colors_count(n, 0);
+    dfs2(edges, 0, -1, colors_costs, colors_count, query_indexes, queries);
+
+    for (Query &q: queries) {
+        int l = lca.query(q.from, q.to);
+        ll plain = plain_costs[q.from] + plain_costs[q.to] - plain_costs[l] * 2;
+        ll color_cost = q.costs[q.from] + q.costs[q.to] - q.costs[l] * 2;
+        ll count = q.counts[q.from] + q.counts[q.to] - q.counts[l] * 2;
+        ll add = count * q.cost;
+        cout << plain - color_cost + add << endl;
+    }
 }
