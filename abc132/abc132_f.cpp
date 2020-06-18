01cfc21798df03c1f260a2c82d0487aac9c763d7
riku <riku@tanide.net>
2020-06-18 11:08:30 +0900

    【TLE】[AtCoder Beginner Contest 132]F - Small Products

--- abc132/abc132_f.cpp
+++ abc132/abc132_f.cpp
@@ -1,88 +1,56 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
 typedef long long ll;
-typedef pair<int, int> P;
-const ll INF = 1001001001;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-const int mod = 1000000007;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
 
-struct mint {
-    ll x;
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
-    mint(ll x = 0) : x(x % mod) {}
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-    mint &operator+=(const mint a) {
-        if ((x += a.x) >= mod) x -= mod;
-        return *this;
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+int rec(ll n, int k, int depth, int prev) {
+    if (depth == k) return 1;
+    int ans = 0;
+    for (int i = 1; i * prev <= n; i++) {
+        ans += rec(n, k, depth + 1, i);
     }
-
-    mint &operator-=(const mint a) {
-        if ((x += mod - a.x) >= mod) x -= mod;
-        return *this;
-    }
-
-    mint &operator*=(const mint a) {
-        (x *= a.x) %= mod;
-        return *this;
-    }
-
-    mint operator+(const mint a) const {
-        mint res(*this);
-        return res += a;
-    }
-
-    mint operator-(const mint a) const {
-        mint res(*this);
-        return res -= a;
-    }
-
-    mint operator*(const mint a) const {
-        mint res(*this);
-        return res *= a;
-    }
-};
-
+    return ans;
+}
 
 int main() {
-    int n, k;
+    ll n;
+    int k;
     cin >> n >> k;
 
-    vector<P> s;
-    for (int i = 1; i * i <= n; i++) {
-        s.push_back(P(i, 0));
-        s.push_back(P(n / i, 0));
-    }
-    sort(s.begin(), s.end());
-    s.erase(unique(s.begin(), s.end()), s.end());
-
-    rep(i, s.size() - 1) {
-        int l = n / s[i + 1].first;
-        int r = n / s[i].first;
-        s[i].second = r - l;
-    }
-    s.back().second = 1;
-    int m = s.size();
-
-    map<int, int> mp;
-    rep(i, m) mp[s[i].first] = i;
-
-    vector<vector<mint>> dp(k + 1, vector<mint>(m));
-    dp[0][mp[n]] = 1;
-
-    rep(i, k) {
-        for (int j = m - 2; j >= 0; j--) dp[i][j] += dp[i][j + 1];
-        rep(j, m) {
-            int nj = mp[n / s[j].first];
-            dp[i + 1][j] = dp[i][nj] * s[j].second;
-        }
-    }
-
-    mint ans = 0;
-    rep(j, m)ans += dp[k][j];
-    cout << ans.x << endl;
+    cout << rec(n, k, 0, 1) << endl;
 
 }
