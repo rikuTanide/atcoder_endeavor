d0e8a28af56b7cf6b855825aaba4c385b94bddaa
riku <riku@tanide.net>
2020-06-18 17:51:37 +0900

    【RE】[AtCoder Grand Contest 036]C - GP 2

---
+++ agc036/agc036_c.cpp
@@ -0,0 +1,184 @@
+#include <bits/stdc++.h>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+
+struct Edge {
+    int to, color;
+    ll cost;
+};
+
+
+ll rec(vector<vector<Edge>> &g, int start, int now, int goal, int prev) {
+    if (goal == now) return 0;
+
+    for (Edge e : g[now]) {
+        if (e.to == prev) continue;
+        ll c = rec(g, start, e.to, goal, now);
+        if (c == -1) continue;
+        return c + e.cost;
+    }
+    return -1;
+}
+
+struct Query {
+    int color;
+    ll cost;
+    int from, to;
+
+    // <edge, cost>
+    map<int, ll> costs;
+    map<int, ll> counts;
+};
+
+std::istream &operator>>(std::istream &in, Query &o) {
+    cin >> o.color >> o.cost >> o.from >> o.to;
+    o.color--;
+    o.from--;
+    o.to--;
+    return in;
+}
+
+const int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
+
+struct combination {
+    vector<mint> fact, ifact;
+
+    combination(int n) : fact(n + 1), ifact(n + 1) {
+        assert(n < mod);
+        fact[0] = 1;
+        for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
+        ifact[n] = fact[n].inv();
+        for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
+    }
+
+    mint operator()(int n, int k) {
+        if (k < 0 || k > n) return 0;
+        return fact[n] * ifact[k] * ifact[n - k];
+    }
+} combination(1000000);
+
+
+mint sub(int n, int a, int b) {
+    cmin(b, n);
+    mint ans = 0;
+    for (int i = a % 2; i <= min({n, a, b,}); i += 2) {
+        ans += combination((a - i) / 2 + n - 1, n - 1) * combination(n, i);
+    }
+    return ans;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    mint ans = sub(n, 3 * m, m);
+    ans -= (sub(n, m, m) - sub(n - 1, m, m)) * n;
+    cout << ans << endl;
+
+}
