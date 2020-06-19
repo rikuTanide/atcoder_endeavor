1d88797152be6b0562392b889153466077c8c80a
riku <riku@tanide.net>
2020-06-19 17:32:18 +0900

    【WA】[AtCoder Beginner Contest 140]F - Many Slimes

--- abc140/abc140_f.cpp
+++ abc140/abc140_f.cpp
@@ -1,51 +1,68 @@
 #include <bits/stdc++.h>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
 typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-const int INF = 1001001001;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
 
 int main() {
-
     int n;
-
     cin >> n;
-    int n2 = 1 << n;
-
-    vector<int> a(n2);
-
-    rep(i, n2) {
-        cin >> a[i];
+    priority_queue<ll> slimes;
+    rep(i, 1 << n) {
+        ll s;
+        cin >> s;
+        slimes.push(s);
     }
 
-    sort(a.begin(), a.end());
+    vector<ll> used;
+    used.push_back(slimes.top());
+    slimes.pop();
 
-    vector<int> s;
-    s.push_back(a.back());
-    a.back() = -1;
-
-    rep(i, n) {
-        vector<int> p = s;
-        sort(p.begin(), p.end());
-        int ai = n2 - 1;
-        while (p.size() > 0 && ai >= 0) {
-            if (a[ai] == -1) {
-                ai--;
-                continue;
+    rep(_, n) {
+        vector<ll> new_slimes;
+        for (ll s : used) {
+            ll t = slimes.top();
+            if (t >= s) {
+                cout << "No" << endl;
+                ret();
             }
-            if (a[ai] < p.back()) {
-                s.push_back(a[ai]);
-                p.pop_back();
-                a[ai] = -1;
-            }
-            ai--;
+            new_slimes.push_back(t);
+            slimes.pop();
         }
-        if (p.size() > 0) {
-            cout << "No" << endl;
-            return 0;
-        }
+        used.insert(used.end(), new_slimes.begin(), new_slimes.end());
     }
-
     cout << "Yes" << endl;
 }
