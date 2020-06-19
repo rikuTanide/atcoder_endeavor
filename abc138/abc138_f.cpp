0e77a17f8331d48276cbd94f923a6f44d22a8d74
riku <riku@tanide.net>
2020-06-19 16:37:43 +0900

    【TLE】[AtCoder Beginner Contest 138]F - Coincidence

--- abc138/abc138_f.cpp
+++ abc138/abc138_f.cpp
@@ -1,13 +1,44 @@
 #include <bits/stdc++.h>
 
+const double PI = 3.14159265358979323846;
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) int(x.size())
-
 typedef long long ll;
-const int INF = 1001001001;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+//typedef pair<ll, ll> P;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-const int mod = 1000000007;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+
+const int mod = 1e9 + 7;
 
 struct mint {
     ll x; // typedef long long ll;
@@ -64,50 +95,30 @@ struct mint {
         mint res(*this);
         return res /= a;
     }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
 };
 
-mint dp[61][2][2][2];
 
 int main() {
     ll l, r;
     cin >> l >> r;
-
-    dp[60][0][0][0] = 1;
-
-    for (int i = 59; i >= 0; i--) {
-        int lb = l >> i & 1;
-        int rb = r >> i & 1;
-
-        rep(j, 2) rep(k, 2)rep(s, 2) {
-                    mint pre = dp[i + 1][j][k][s];
-                    rep(x, 2)
-                        rep(y, 2) {
-                            if (x && !y) continue;
-
-                            int nj = j, nk = k, ns = s;
-
-                            if (!s && x != y) continue;
-                            if (x && y) ns = 1;
-
-                            if (!j && !x && lb) continue;
-                            if (x && !lb) nj = 1;
-
-                            if (!k && y && !rb) continue;
-                            if (!y && rb) nk = 1;
-
-                            dp[i][nj][nk][ns] += pre;
-                        }
-
-                }
-    }
-
-    mint ans;
-    rep(j, 2) {
-        rep(k, 2) {
-            rep(s, 2) {
-                ans += dp[0][j][k][s];
-            }
+    mint ans = 0;
+    for (ll x = l; x <= r; x++) {
+        for (ll y = l; y <= r; y++) {
+            if (y % x == (y ^ x)) ans += 1;
         }
     }
-    cout << ans.x << endl;
+    cout << ans << endl;
 }
