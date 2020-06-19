754dfc0b76ef759302e2cd4adcd76a11ff2c12b7
riku <riku@tanide.net>
2020-06-19 16:50:29 +0900

    【AC】[AtCoder Beginner Contest 138]F - Coincidence

--- abc138/abc138_f.cpp
+++ abc138/abc138_f.cpp
@@ -110,15 +110,49 @@ struct mint {
 
 };
 
+mint dp[61][2][2][2];
 
 int main() {
+
     ll l, r;
     cin >> l >> r;
-    mint ans = 0;
-    for (ll x = l; x <= r; x++) {
-        for (ll y = l; y <= r; y++) {
-            if (y % x == (y ^ x)) ans += 1;
+
+
+    dp[60][0][0][0] = 1;
+    for (int i = 59; i >= 0; i--) {
+        int lb = l >> i & 1;
+        int rb = r >> i & 1;
+
+        rep(j, 2) rep(k, 2)rep(s, 2) {
+                    mint pre = dp[i + 1][j][k][s];
+                    rep(x, 2)
+                        rep(y, 2) {
+                            if (x && !y) continue;
+
+                            int nj = j, nk = k, ns = s;
+
+                            if (!s && x != y) continue;
+                            if (x && y) ns = 1;
+
+                            if (!j && !x && lb) continue;
+                            if (x && !lb) nj = 1;
+
+                            if (!k && y && !rb) continue;
+                            if (!y && rb) nk = 1;
+
+                            dp[i][nj][nk][ns] += pre;
+                        }
+
+                }
+    }
+
+    mint ans;
+    rep(j, 2) {
+        rep(k, 2) {
+            rep(s, 2) {
+                ans += dp[0][j][k][s];
+            }
         }
     }
-    cout << ans << endl;
+    cout << ans.x << endl;
 }
