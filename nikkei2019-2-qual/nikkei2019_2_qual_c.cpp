f0ebd8ae0ec0c4c932d813a80b66274375a72d8e
riku <riku@tanide.net>
2020-06-21 10:51:22 +0900

    【RE】[第二回全国統一プログラミング王決定戦予選]C - Swaps

---
+++ nikkei2019-2-qual/nikkei2019_2_qual_c.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+bool check(vector<ll> as, vector<ll> bs) {
+    int n = as.size();
+    sort(as.begin(), as.end());
+    sort(bs.begin(), bs.end());
+    rep(i, n) {
+        if (as[i] > bs[i]) return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> as(n), bs(n);
+    rep(i, n) cin >> as[i];
+    rep(i, n)cin >> bs[i];
+
+    if (!check(as, bs)) {
+        cout << "No" << endl;
+        ret();
+    }
+    assert(false);
+}
