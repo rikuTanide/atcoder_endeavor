591a11fa5dfd3415183bbb1faaf7331549aa6ef1
riku <riku@tanide.net>
2020-06-21 11:03:58 +0900

    【WA】[第二回全国統一プログラミング王決定戦予選]C - Swaps

--- nikkei2019-2-qual/nikkei2019_2_qual_c.cpp
+++ nikkei2019-2-qual/nikkei2019_2_qual_c.cpp
@@ -52,6 +52,18 @@ bool check(vector<ll> as, vector<ll> bs) {
     return true;
 }
 
+void sort_tuple(vector<ll> &as, vector<ll> &bs) {
+    int n = as.size();
+    vector<P> ps(n);
+    rep(i, n) ps[i].first = bs[i];
+    rep(i, n) ps[i].second = as[i];
+
+    sort(ps.begin(), ps.end());
+
+    rep(i, n) as[i] = ps[i].second;
+    rep(i, n) bs[i] = ps[i].first;
+}
+
 int main() {
     int n;
     cin >> n;
@@ -63,5 +75,35 @@ int main() {
         cout << "No" << endl;
         ret();
     }
-    assert(false);
+
+    sort_tuple(as, bs);
+
+    map<ll, int> index;
+    rep(i, n) index[as[i]] = i;
+
+    multiset<ll> wait;
+    for (ll l : as) wait.insert(l);
+
+    rep(i, n) {
+        if (as[i] <= bs[i]) {
+            cout << "Yes" << endl;
+            ret();
+        }
+
+        ll o = as[i];
+
+        ll sp = *wait.begin();
+        ll spi = index[sp];
+
+        swap(as[i], as[spi]);
+        swap(index[o], index[sp]);
+
+        assert(as[i] <= bs[i]);
+
+        wait.erase(wait.find(sp));
+
+    }
+
+    cout << "No" << endl;
+
 }
