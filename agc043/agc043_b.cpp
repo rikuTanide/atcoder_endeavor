bb99b151d5de044787c80e2ce933b7d878ea7969
riku <riku@tanide.net>
2020-06-22 23:20:18 +0900

    【AC】[AtCoder Grand Contest 043]B - 123 Triangle

--- agc043/agc043_b.cpp
+++ agc043/agc043_b.cpp
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -30,28 +35,16 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-#include <iostream>
-#include <vector>
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-using namespace std;
-
-
-void print_v(vector<int> &v) {
-    for (int i : v) cout << i;
-    cout << ' ';
-}
-
-
+// 一段減らす
 vector<int> sub(vector<int> &v) {
-    if(v.size() == 1) return v;
+    if (v.size() == 1) return v;
     vector<int> v2(v.size() - 1);
     rep(i, v.size() - 1) {
         v2[i] = abs(v[i] - v[i + 1]);
@@ -59,15 +52,22 @@ vector<int> sub(vector<int> &v) {
     return v2;
 }
 
-int check2(vector<int> v) {
+// 半分にする
+vector<int> tournament(vector<int> &v) {
+    vector<int> next;
+    for (int i = 0; i < v.size(); i += 2) {
+        int a = abs(v[i] - v[i + 1]);
+        next.push_back(a);
+    }
+    return next;
+}
+
+// 偶数なら半分にする
+// 奇数なら一段減らしてから半分にする
+int solv(vector<int> v) {
     while (v.size() != 1) {
         if (v.size() % 2 == 0) {
-            vector<int> next;
-            for (int i = 0; i < v.size(); i += 2) {
-                int a = abs(v[i] - v[i + 1]);
-                next.push_back(a);
-            }
-            v = next;
+            v = tournament(v);
         } else {
             v = sub(v);
         }
@@ -75,57 +75,19 @@ int check2(vector<int> v) {
     return v.front();
 }
 
-
-bool is_end(vector<int> &v) {
-    for (int i : v) if (i != 3) return false;
-    return true;
-}
-
-
-void digit(vector<int> &v) {
-    for (int i = 0; i < v.size(); i++) {
-        if (v[i] == 4) {
-            v[i] = 1;
-            v[i + 1]++;
-        }
-    }
-}
-
-int check(vector<int> v) {
-
-    while (v.size() != 1) {
-        vector<int> next;
-        for (int i = 1; i < v.size(); i++) {
-            int a = abs(v[i - 1] - v[i]);
-            next.push_back(a);
-        }
-        v = next;
-    }
-    return v.front();
-}
-
-
 int main() {
-
-
     int n;
     cin >> n;
-    vector<int> digits(n);
-    rep(i, n) {
-        char c;
-        cin >> c;
-        int a = c - '0';
-        digits[i] = a;
-    }
+    vector<char> u(n);
+    rep(i, n)cin >> u[i];
+    vector<int> v(n);
+    rep(i, n) v[i] = u[i] - '0';
 
-    vector<int> digits2 = sub(digits);
-    digits2 = sub(digits2);
-    digits2 = sub(digits2);
-    digits2 = sub(digits2);
-    cout << check2(digits2) << endl;
+    v = sub(v);
+    v = sub(v);
+    v = sub(v);
+    v = sub(v);
 
+    cout << solv(v) << endl;
 
 }
-
-
-
