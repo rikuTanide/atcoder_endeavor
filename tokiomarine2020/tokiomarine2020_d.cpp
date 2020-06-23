e84e6fe1b4f7fcec69d53b5caebcec5546162f13
riku <riku@tanide.net>
2020-06-23 15:28:51 +0900

    【RE】[東京海上日動　プログラミングコンテスト2020]D - Knapsack Queries on a tree

--- tokiomarine2020/tokiomarine2020_d.cpp
+++ tokiomarine2020/tokiomarine2020_d.cpp
@@ -125,7 +125,7 @@ void use_cache(ll u, ll l, unordered_map<ll, vector<P>> &cache, int fe, vector<I
         if (e1.first > l) continue;
         ll sub = l - e1.first;
 
-        while (it->first + e1.first > l) it--;
+//        while (it->first + e1.first > l) it--;
         assert(it->first + e1.first <= l);
         ll now = e1.second + it->second;
         cmax(ans, now);
