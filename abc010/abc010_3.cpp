0ad51a629dd68cf0c25e06eb879a2e665467a9bd
riku <riku@tanide.net>
2020-06-25 23:57:02 +0900

    【AC】[AtCoder Beginner Contest 010]C - 浮気調査

--- abc010/abc010_3.cpp
+++ abc010/abc010_3.cpp
@@ -1,59 +1,72 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
-//using namespace boost::multiprecision;
 using namespace std;
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<ll, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
+#define ret() return 0;
 
-//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 double distance(double x1, double y1, double x2, double y2) {
-    ll x = x1 - x2;
-    ll y = y1 - y2;
-    ll xx = x * x;
-    ll yy = y * y;
-    ll sum = xx + yy;
-    return sqrt(sum);
+    double x = x1 - x2;
+    double y = y1 - y2;
+    double xx = x * x;
+    double yy = y * y;
+    return sqrt(xx + yy);
 }
 
 int main() {
-    double txa, tya, txb, tyb, t, v;
-    cin >> txa >> tya >> txb >> tyb >> t >> v;
+    double tx1, ty1, tx2, ty2, t, v;
+    cin >> tx1 >> ty1 >> tx2 >> ty2 >> t >> v;
+
     int n;
     cin >> n;
-    double possible_distance = t * v;
-    rep(i, n) {
+
+    rep(_, n) {
         double x, y;
         cin >> x >> y;
 
-        double total_distance = distance(x, y, txa, tya) + distance(x, y, txb, tyb);
-        if (possible_distance >= total_distance - EPS) {
+        double a = distance(tx1, ty1, x, y);
+        double b = distance(x, y, tx2, ty2);
+
+        if ((a + b) - EPS <= t * v) {
             cout << "YES" << endl;
-            return 0;
+            ret();
         }
     }
     cout << "NO" << endl;
