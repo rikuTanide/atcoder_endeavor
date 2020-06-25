2b24abbdc28b234a3d77eb1aa64653eff659f045
riku <riku@tanide.net>
2020-06-25 23:39:14 +0900

    【AC】[DigitalArts プログラミングコンテスト2012]A - C-Filter

--- digitalarts2012/digitalarts_1.cpp
+++ digitalarts2012/digitalarts_1.cpp
@@ -1,23 +1,26 @@
-#define _GLIBCXX_DEBUG
-
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -32,87 +35,67 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-#include <iostream>
-#include <vector>
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-using namespace std;
-
-std::vector<std::string> split(std::string str, char del) {
-    int first = 0;
-    int last = str.find_first_of(del);
-    if (last == -1) return vector<string>{str};
-    std::vector<std::string> result;
-
-    while (first < str.size()) {
-        std::string subStr(str, first, last - first);
-
-        result.push_back(subStr);
-
-        first = last + 1;
-        last = str.find_first_of(del, first);
-
-        if (last == std::string::npos) {
-            last = str.size();
+vector<string> split(const string &s, char delim) {
+    vector<string> elems;
+    stringstream ss(s);
+    string item;
+    while (getline(ss, item, delim)) {
+        if (!item.empty()) {
+            elems.push_back(item);
         }
     }
-
-    return result;
+    return elems;
 }
 
-bool match(string &s, string &w) {
-    if (w.size() != s.size()) return false;
-
-    rep(i, w.size()) {
-        if (s[i] == w[i]) continue;
-        if (w[i] == '*') continue;
-        return false;
-    }
-    return true;
-}
-
-
-bool str_equal(string &s, vector<string> &words) {
-    for (string &w : words) {
-        bool b = match(s, w);
-        if (b) return true;
+bool is_match(string &str, vector<string> &masks) {
+    for (string &mask : masks) {
+        bool match = [&] {
+            if (mask.size() != str.size()) return false;
+            rep(i, mask.size()) {
+                if (mask[i] == '*' || mask[i] == str[i]) continue;
+                return false;
+            }
+            return true;
+        }();
+        if (match) return true;
     }
     return false;
 }
 
-string kome_filter(int i) {
-    string res = "";
-    rep(j, i) {
-        res += '*';
-    }
-    return res;
+string to_mask(string &s) {
+    string ans;
+    rep(i, s.size()) ans.push_back('*');
+    return ans;
 }
 
 int main() {
     string s;
     getline(cin, s);
 
-    vector<string> ss = split(s, ' ');
-
+    vector<string> strs = split(s, ' ');
 
     int n;
     cin >> n;
-    vector<string> words(n);
-    rep(i, n) cin >> words[i];
+    vector<string> masks(n);
+    rep(i, n) cin >> masks[i];
 
-    for (string &t : ss) {
-        if (str_equal(t, words)) {
-            cout << kome_filter(t.size()) << ' ';
+    rep (i, strs.size()) {
+        string &str = strs[i];
+        if (is_match(str, masks)) {
+            cout << to_mask(str);
         } else {
-            cout << t << ' ';
+            cout << str;
         }
+        if (i == strs.size() - 1) cout << endl;
+        else cout << ' ';
     }
-    cout << endl;
+
+
 }
