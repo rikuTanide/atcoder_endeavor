7a445fbac39614b6d12ddbc73f947e899d78166e
riku <riku@tanide.net>
2020-06-26 09:23:57 +0900

    【AC】[AtCoder Beginner Contest 032]B - 高橋君とパスワード

--- abc032/abc032_b.cpp
+++ abc032/abc032_b.cpp
@@ -1,49 +1,57 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
-//using namespace boost::multiprecision;
+const double PI = 3.14159265358979323846;
 using namespace std;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<ll, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
+#define ret() return 0;
 
-//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
+std::istream &operator>>(std::istream &in, set<ll> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+//const ll mod = 1e10;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+
 
 int main() {
+
     string s;
     int k;
     cin >> s >> k;
-    if (s.size() < k) {
-        cout << 0 << endl;
-        return 0;
-    }
-    set<string> candidate;
-    for (int i = 0; i <= s.size() - k; i++) {
-        string sub = s.substr(i, k);
-        candidate.insert(sub);
-    }
-    cout << candidate.size() << endl;
-}
 
+    int n = s.size();
+
+    set<string> v;
+
+    rep(i, n - k + 1) {
+        string t = s.substr(i, k);
+        v.insert(t);
+    }
+    cout << v.size() << endl;
+
+}
