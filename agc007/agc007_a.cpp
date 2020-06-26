b11ae1ecec16728add4985a8b288915d81d936a0
riku <riku@tanide.net>
2020-06-26 23:00:05 +0900

    【AC】[AtCoder Grand Contest 007]A - Shik and Stone

--- agc007/agc007_a.cpp
+++ agc007/agc007_a.cpp
@@ -1,75 +1,54 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
-//using namespace boost::multiprecision;
 using namespace std;
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-typedef pair<int, int> P;
-//typedef pair<ll, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
+#define ret() return 0;
 
-bool contain(set<string> &s, string a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+//const ll mod = 1e10;
 
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
     int h, w;
     cin >> h >> w;
+
     vector<vector<char>> board(h, vector<char>(w));
     rep(y, h) rep(x, w) cin >> board[y][x];
-
-    auto can = [&](int y, int x) {
-        if (y == h) return false;
-        if (x == w) return false;
-        if (board[y][x] == '.') return false;
-        return true;
-    };
-    auto set = [&](int y, int x) {
-        if (!can(y, x)) return;
-        board[y][x] = 'o';
-    };
-
-    board[0][0] = 'o';
-    rep(y, h)rep(x, w) {
-            if (board[y][x] == 'o') {
-                if (can(y + 1, x)) {
-                    set(y + 1, x);
-                } else if (can(y, x + 1)) {
-                    set(y, x + 1);
-                }
-            }
-        }
-
-
-    rep(y, h)rep(x, w) {
-            if (board[y][x] == '#') {
-                cout << "Impossible" << endl;
-                return 0;
-            }
-        }
-
-    cout << (board.back().back() == 'o' ? "Possible" : "Impossible") << endl;
+    int count = 0;
+    rep(y, h) rep(x, w) if (board[y][x] == '#') count++;
+    cout << (count == h + w - 1 ? "Possible" : "Impossible") << endl;
 }
-
-
