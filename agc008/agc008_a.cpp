42a8fa856b41596b281c5094a3a81527ad5ab6da
riku <riku@tanide.net>
2020-06-26 23:31:11 +0900

    【AC】[AtCoder Grand Contest 008]A - Simple Calculator

--- agc008/agc008_a.cpp
+++ agc008/agc008_a.cpp
@@ -1,64 +1,80 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
-//using namespace boost::multiprecision;
 using namespace std;
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-typedef pair<int, int> P;
-//typedef pair<ll, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
+#define ret() return 0;
 
-bool contain(set<string> &s, string a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+//const ll mod = 1e10;
 
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
     ll x, y;
     cin >> x >> y;
 
-    ll count = 0;
-    if (abs(x) > abs(y)) {
+    ll sum = 0;
+
+    if (abs(y) < abs(x)) {
+        // 絶対値を小さくしないといけないから
+        // x をマイナスにする
         if (x > 0) {
-            count++;
+            sum++;
             x = -x;
         }
-    } else {
+    } else if (abs(x) < abs(y)) {
+        // 絶対値を大きくしないといけないから
+        // xをプラスにする
         if (x < 0) {
-            count++;
+            sum++;
             x = -x;
         }
     }
+    ll add = abs(abs(x) - abs(y));
+    x += add;
+    sum += add;
 
-    ll diff = abs(abs(x) - abs(y));
-    count += diff;
-    x += diff;
+    assert(x == y || (-x) == y);
     if (x != y) {
-        count++;
+        sum++;
         x = -x;
     }
+
     assert(x == y);
-    cout << count << endl;
+
+    cout << sum << endl;
+
 }
-
-
