04849afb5c16778fc627c728e30dc1da8f49b120
riku <riku@tanide.net>
2020-06-26 23:52:39 +0900

    【WA】[AtCoder Regular Contest 080]C - 4-adjacent

--- arc080/arc080_a.cpp
+++ arc080/arc080_a.cpp
@@ -1,111 +1,69 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
-//using namespace boost::multiprecision;
 using namespace std;
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-typedef pair<int, int> P;
-//typedef pair<ll, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
+#define ret() return 0;
 
-bool contain(set<string> &s, string a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-vector<pair<ll, int >> factorize(ll n) {
-    vector<pair<ll, int >> res;
-
-    for (ll i = 2; i * i <= n; i++) {
-        if (n % i != 0) {
-            continue;
-        }
-        res.emplace_back(i, 0);
-        while (n % i == 0) {
-            n /= i;
-            res.back().second++;
-        }
-    }
-
-    if (n != 1) res.emplace_back(n, 1);
-    return res;
-
-}
-
-ll comb(ll l, ll r) {
-    vector<ll> factors(50);
-    for (ll j = l; j > (l - r); j--) {
-        auto fs = factorize(j);
-        for (auto f : fs) {
-            factors[f.first] += f.second;
-        }
-    }
-
-    for (ll j = 1; j <= r; j++) {
-        auto fs = factorize(j);
-        for (auto f : fs) {
-            factors[f.first] -= f.second;
-        }
-    }
-
-    ll s = 1;
-
-    for (int j = 1; j < 50; j++) {
-        for (int k = 0; k < factors[j]; k++) {
-            s *= j;
-        }
-    }
-
-    return s;
-
-}
-
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
     int n;
-
-
     cin >> n;
-    int odd = 0, b2 = 0, b4 = 0;
-    rep(i, n) {
-        ll a;
-        cin >> a;
-        if (a % 4 == 0) b4++;
-        else if (a % 2 == 0) b2++;
-        else odd++;
-    }
-    if (odd > 0 && b2 >= 1) {
-        odd++;
+    vector<ll> v(n);
+    rep(i, n) cin >> v[i];
+
+    int r2 = 0, r4 = 0, r1 = 0;
+    for (ll l : v) {
+        if (l % 4 == 0) r4++;
+        else if (l % 2 == 0) r2++;
+        else r1++;
     }
 
-    if (b4 == 0 && odd > 0) {
-        cout << "No" << endl;
-    } else if (odd - 1 > b4) {
-        cout << "No" << endl;
-    } else {
+    if (r4 >= (r2 + r1)) {
         cout << "Yes" << endl;
+        ret();
     }
 
+    if (r1 == 0) {
+        cout << "Yes" << endl;
+        ret();
+    }
 
+    cout << "No" << endl;
 }
-
