763ea3287709357a7ff628dbaba5ac97b1115ce6
riku <riku@tanide.net>
2020-06-26 22:43:46 +0900

    【AC】[AtCoder Beginner Contest 047]B - Snuke's Coloring 2 (ABC Edit)

--- abc047/abc047_b.cpp
+++ abc047/abc047_b.cpp
@@ -1,70 +1,84 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
-//using namespace boost::multiprecision;
 using namespace std;
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-typedef pair<int, int> P;
-//typedef pair<ll, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
+#define ret() return 0;
 
-//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+//const ll mod = 1e10;
 
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int check(int t, vector<int> &v) {
+    int ans = 0;
+    for (int i : v) ans += (i - t) * (i - t);
+    return ans;
+}
 
 int main() {
+    int w, h, n;
+    cin >> w >> h >> n;
 
-    int sx = 0, ex, sy = 0, ey;
-    cin >> ex >> ey;
+    int l = 0, r = w, u = h, d = 0;
 
-    int n;
-    cin >> n;
+    rep(_, n) {
+        int x, y, a;
+        cin >> x >> y >> a;
 
-    rep(i, n) {
-        int x, y, z;
-        cin >> x >> y >> z;
-        if (z == 1) {
-            cmax(sx, x);
-        } else if (z == 2) {
-            cmin(ex, x);
-        } else if (z == 3) {
-            cmax(sy, y);
-        } else if (z == 4) {
-            cmin(ey, y);
+
+        if (a == 1) {
+            // 左
+            cmax(l, x);
+        } else if (a == 2) {
+            // 右
+            cmin(r, x);
+        } else if (a == 3) {
+            // 下
+            cmax(d, y);
         } else {
-            __throw_runtime_error("nai");
+            cmin(u, y);
         }
     }
 
-    int w = ex - sx;
-    int h = ey - sy;
-    if (w <= 0 || h <= 0) {
+    if (r <= l || u <= d) {
         cout << 0 << endl;
-        return 0;
+    } else {
+        int width = r - l;
+        int height = u - d;
+        cout << width * height << endl;
     }
 
-    cout << w * h << endl;
-
 }
-
