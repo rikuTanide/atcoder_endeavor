3dbca157acc1b3ba5307d3f86704629dc5b7e1b5
riku <riku@tanide.net>
2020-06-26 23:09:12 +0900

    【AC】[CODE FESTIVAL 2016 Final]B - Exactly N points

--- cf16-final/codefestival_2016_final_b.cpp
+++ cf16-final/codefestival_2016_final_b.cpp
@@ -1,57 +1,64 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
-//using namespace boost::multiprecision;
 using namespace std;
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-typedef pair<int, int> P;
-//typedef pair<ll, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
+#define ret() return 0;
 
-bool contain(set<string> &s, string a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+//const ll mod = 1e10;
 
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-    int n;
+    ll n;
     cin >> n;
 
-    int sum = 0;
-    int ma = 0;
-    for (int i = 1; i <= n; i++) {
+    set<ll> s;
+    ll sum = 0;
+
+    for (ll i = 1; i <= n; i++) {
         sum += i;
-        ma = i;
-
-        if (sum > n) break;
+        s.insert(i);
+        if (sum >= n) break;
     }
-    int diff = sum - n;
-
-    for (int i = 1; i <= ma; i++) {
-        if (i != diff)
-            cout << i << endl;
+    if (sum > n) {
+        ll diff = sum - n;
+        s.erase(diff);
     }
+
+    for(ll l : s) cout << l << endl;
 
 }
-
-
