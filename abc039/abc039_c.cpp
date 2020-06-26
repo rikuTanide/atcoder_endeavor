7a735c9e635498c1ddcdbb8b297943f5d01dce92
riku <riku@tanide.net>
2020-06-26 22:28:11 +0900

    【AC】[AtCoder Beginner Contest 039]C - ピアニスト高橋君

--- abc039/abc039_c.cpp
+++ abc039/abc039_c.cpp
@@ -1,81 +1,84 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
-//using namespace boost::multiprecision;
 using namespace std;
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<ll, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
+#define ret() return 0;
 
-//bool contain(set<P> &s, P a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+//const ll mod = 1e10;
 
-bool check(int i, string org, string s) {
-    for (int j = 0; j < s.size(); j++) {
-        if (org[j + i] != s[j]) return false;
-    }
-    return true;
-}
-
-string toAns(int i) {
-    switch (i) {
-        case 0:
-            return "Do";
-        case 2:
-            return "Re";
-        case 4:
-            return "Mi";
-        case 5:
-            return "Fa";
-        case 7:
-            return "So";
-        case 9:
-            return "La";
-        case 11:
-            return "Si";
-    }
-    __throw_runtime_error("nai");
-}
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-
-    string org = "WBWBWWBWBWBW";
-    org += org;
-    org += org;
-    org += org;
-
+    string _board = "WBWBWWBWBWBW";
+    string board;
+    rep(i, 10) board += _board;
 
     string s;
     cin >> s;
 
-    for (int i = 0; i < org.size(); i++) {
-        bool b = check(i, org, s);
-        if (!b) continue;
-        cout << toAns(i) << endl;
-        return 0;
+    int l = s.size();
+
+    int i = [&] {
+        rep(i, 20) {
+            if (board.substr(i, l) == s) {
+                return i;
+            }
+        }
+        __throw_runtime_error("konaide");
+    }();
+
+
+    if (i == 0) {
+        cout << "Do" << endl;
+    } else if (i == 2) {
+        cout << "Re" << endl;
+    } else if (i == 4) {
+        cout << "Mi" << endl;
+    } else if (i == 5) {
+        cout << "Fa" << endl;
+    } else if (i == 7) {
+        cout << "So" << endl;
+    } else if (i == 9) {
+        cout << "La" << endl;
+    } else if (i == 11) {
+        cout << "Si" << endl;
+    } else {
+        __throw_runtime_error("konaide");
     }
-    __throw_runtime_error("nai1");
+
 
 }
-
