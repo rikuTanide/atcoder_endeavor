99ff0e71bf09b81f44a76d7ee7ee30a61fc4697f
riku <riku@tanide.net>
2020-06-26 23:40:58 +0900

    【AC】[AtCoder Regular Contest 069]C - Scc Puzzle

--- arc069/arc069_a.cpp
+++ arc069/arc069_a.cpp
@@ -1,54 +1,61 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef long long ll;
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef pair<int, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e15;
-const ll MINF = -10e10;
-//const int INF = INT_MAX / 100;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("~/Downloads/02.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-const int mod = 1000000007;
-const double PI = 3.14159265358979323846;
-
-typedef pair<ll, ll> P;
-
-void print_line(vector<vector<int>> &ans) {
-    for (int i = 0; i < ans.size(); i++) {
-        for (int y = 0; y < ans[i].size(); y++) {
-            cout << ans[i][y];
-        }
-        cout << endl;
-    }
-}
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-    ll n, m;
-    cin >> n >> m;
+    ll s, c;
+    cin >> s >> c;
 
-    // どっちが足を引っ張ってるか
-    if (n * 2 > m) {
-        // cが足りない
-        cout << m / 2 << endl;
-        return 0;
+    if (s * 2 >= c) {
+        cout << c / 2 << endl;
+    } else {
+        ll ans = s;
+        c -= (2 * s);
+        s -= s;
+        assert(c >= 0);
+        ll add = c / 4;
+        ans += add;
+        cout << ans << endl;
     }
-
-    ll z = (m - (2 * n)) / 4;
-    cout << n + z << endl;
 
 }
