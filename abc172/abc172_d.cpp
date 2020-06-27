34ecc8a5325bc86b04af6d52c7647518f2621e9f
riku <riku@tanide.net>
2020-06-27 21:15:00 +0900

    【TLE】[AtCoder Beginner Contest 172]D - Sum of Divisors

---
+++ abc172/abc172_d.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+map<ll, int> factorize(ll n) {
+    map<ll, int> res;
+
+    for (ll i = 2; i * i <= n; i++) {
+        if (n % i != 0) {
+            continue;
+        }
+        res[i] = 0;
+        while (n % i == 0) {
+            n /= i;
+            res[i]++;
+        }
+    }
+
+    if (n != 1) res[n] = 1;
+    return res;
+}
+
+ll f(ll k) {
+    auto fs = factorize(k);
+    ll sum = 1;
+    for (auto e : fs) {
+        ll now = e.second + 1;
+        sum *= now;
+    }
+    return sum;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+
+    ll sum = 0;
+    for (int k = 1; k <= n; k++) {
+        sum += (f(k) * k);
+    }
+    cout << sum << endl;
+}
