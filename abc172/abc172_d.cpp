e334d06b6404d918e3269a3488694728962577a5
riku <riku@tanide.net>
2020-06-27 22:51:29 +0900

    【AC】[AtCoder Beginner Contest 172]D - Sum of Divisors

--- abc172/abc172_d.cpp
+++ abc172/abc172_d.cpp
@@ -41,87 +41,18 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 //const ll mod = 1e10;
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
-/*
-map<ll, int> factorize(ll n) {
-    map<ll, int> res;
 
-    for (ll i = 2; i * i <= n; i++) {
-        if (n % i != 0) {
-            continue;
-        }
-        res[i] = 0;
-        while (n % i == 0) {
-            n /= i;
-            res[i]++;
-        }
-    }
-
-    if (n != 1) res[n] = 1;
-    return res;
-}
-
-ll f(ll k) {
-
-
-    auto fs = factorize(k);
-    ll sum = 1;
-    for (auto e : fs) {
-        ll now = e.second + 1;
-        sum *= now;
-    }
-    return sum;
-}
-*/
-
-//エラトステネスの篩
-void Eratosthenes(vector<bool> &arr, int n) {
-    for (int i = 2; i < sqrt(n); i++) {
-        if (arr[i]) {
-            for (int j = 0; i * (j + 2) < n; j++) {
-                arr[i * (j + 2)] = false;
-            }
-        }
-    }
-}
-
-map<ll, ll> f(ll k, vector<map<ll, ll>> &memo) {
-    for (ll i = 2; i * i <= k; i++) {
-        if (k % i == 0) {
-            ll t = k / i;
-            map<ll, ll> m = memo[t];
-            m[i]++;
-            return m;
-        }
-    }
-    __throw_runtime_error("konaide");
-}
 
 int main() {
     ll n;
     cin >> n;
 
-    vector<bool> fac_memo(n + 1, true);
-    Eratosthenes(fac_memo, n + 1);
-
-    vector<map<ll, ll>> memo(n + 1);
-
-    for (ll k = 1; k <= n; k++) {
-        if (fac_memo[k]) memo[k][k] = 1;
-        else memo[k] = f(k, memo);
-    }
 
     ll ans = 0;
-
-    for (ll k = 1; k <= n; k++) {
-        ll sum = 1;
-        for (auto e : memo[k]) {
-            ll p = (e.second + 1);
-            sum *= p;
-        }
-        sum *= k;
-        ans += sum;
+    for (ll i = 1; i <= n; i++) {
+        ll c = n / i;
+        ll s = c * (2 * i + (c - 1) * i) / 2;
+        ans += s;
     }
-
-    cout << ans - 1 << endl;
-
+    cout << ans << endl;
 }
