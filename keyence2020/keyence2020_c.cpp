d605964c64cd73c36592f61e9becfe3c99c51bd6
riku <riku@tanide.net>
2020-06-28 23:07:21 +0900

    【AC】[キーエンス プログラミング コンテスト 2020]C - Subarray Sum

--- keyence2020/keyence2020_c.cpp
+++ keyence2020/keyence2020_c.cpp
@@ -1,60 +1,63 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-//typedef pair<ll, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-//const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
 #define ret() return 0;
 
-bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
     ll n, k, s;
     cin >> n >> k >> s;
 
-    ll s1 = s + 1;
-    if (s == 1000000000ll) {
-        s1 = s - 1;
+    vector<ll> ans(n);
+    rep(i, k) ans[i] = s;
+
+    ll emp = 1e9;
+    if (s == 1e9) {
+        emp = 1;
     }
 
-    if (k == 0) {
-        vector<ll> ans(n, s1);
-        for (ll a : ans) cout << a << ' ';
-        ret();
-    }
+    for (int i = k; i < n; i++) ans[i] = emp;
 
-    vector<ll> ans(n, s1);
-    for (int i = 0; i < k; i++) {
-        ans[i] = s;
-    }
-    for (ll a : ans) cout << a << ' ';
+
+    for (ll l : ans) cout << l << ' ';
+    cout << endl;
+
 }
-
-
