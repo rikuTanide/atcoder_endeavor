de8eeea9e72289c6373f118d1948abc8b7d0b500
riku <riku@tanide.net>
2020-06-28 15:43:12 +0900

    【AC】[AtCoder Beginner Contest 122]C - GeT AC

--- abc122/abc122_c.cpp
+++ abc122/abc122_c.cpp
@@ -1,38 +1,46 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
-//using namespace boost::multiprecision;
 using namespace std;
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-typedef pair<int, int> P;
-//typedef pair<ll, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
 #define ret() return 0;
 
-bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 class CumulativeSum {
     vector<ll> numbers;
@@ -50,7 +58,7 @@ public:
 
     ll getSum(int i) {
         if (i == -1) return 0;
-        if (i == sums.size()) return 0;
+        if (i == sums.size()) return sums.back();
         return sums[i];
     }
 
@@ -58,7 +66,7 @@ public:
         return getSum(end) - getSum(start - 1);
     }
 
-    void calculate() {
+    void build() {
         for (int i = 0; i < numbers.size(); i++) {
             sums[i] = getSum(i - 1) + numbers[i];
         }
@@ -74,39 +82,23 @@ int main() {
     string s;
     cin >> s;
 
-    CumulativeSum rsum(n), lsum(n);
-
-    for (int i = 0; i < n - 1; i++) {
-        if (s[i] == 'A' && s[i + 1] == 'C') {
-            lsum.set(i, 1);
-        }
+    CumulativeSum cs(n);
+    rep(i, n - 1) {
+        if (s[i] == 'A' && s[i + 1] == 'C') cs.set(i, 1);
     }
+    cs.build();
 
-    for (int i = 1; i < n; i++) {
-        if (s[i] == 'C' && s[i - 1] == 'A') {
-            rsum.set(i, 1);
-        }
-    }
-
-    rsum.calculate();
-    lsum.calculate();
-
-    rep(i, q) {
+    rep(_, q) {
         int l, r;
         cin >> l >> r;
         l--;
         r--;
-        //         return getSum(end) - getSum(start - 1);
 
-
-        ll rs = rsum.getSum(r);
-        ll ls = lsum.getSum(l - 1);
-
-        ll now = rs - ls;
-
+        int now = cs.getSectionSum(l, r);
+        if (r < n - 1 && s[r] == 'A' && s[r + 1] == 'C') {
+            now--;
+        }
         cout << now << endl;
-
     }
-
 
 }
