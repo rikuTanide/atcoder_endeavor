2fa53268030a6ac4d5d1fa6b7ce34276a4e7c532
riku <riku@tanide.net>
2020-06-28 11:23:58 +0900

    【AC】[AtCoder Regular Contest 091]C - Flip,Flip, and Flip......

--- arc091/arc091_a.cpp
+++ arc091/arc091_a.cpp
@@ -47,6 +47,11 @@ int main() {
     cin >> n >> m;
     if (!(n < m)) swap(n, m);
 
+    if (n == 1 && m == 1) {
+        cout << 1 << endl;
+        ret();
+    }
+
     if (n == 1) {
         cout << m - 2 << endl;
         ret();
