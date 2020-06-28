ebcbea97f5e7b73d940f01ff35fa0568f36196a4
riku <riku@tanide.net>
2020-06-28 16:47:32 +0900

    【WA】[AtCoder Grand Contest 035]A - XOR Circle

--- agc035/agc035_a.cpp
+++ agc035/agc035_a.cpp
@@ -1,87 +1,74 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
-//using namespace boost::multiprecision;
 using namespace std;
-//typedef long long ll;
-//typedef unsigned long long ll;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-//typedef pair<ll, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-//const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
 #define ret() return 0;
 
-bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-    typedef unsigned long long ll;
     int n;
     cin >> n;
 
-    vector<ll> caps(n);
-    rep(i, n)cin >> caps[i];
+    vector<ll> v(n);
+    rep(i, n) cin >> v[i];
 
     map<ll, int> m;
-    for (ll c: caps) m[c]++;
-
+    for (ll l : v) m[l]++;
 
     if (m.find(0) != m.end() && m[0] == n) {
         cout << "Yes" << endl;
         ret();
     }
 
-    if (n % 3 != 0) {
-        cout << "No" << endl;
+    vector<ll> keys;
+    for (auto e : m)keys.push_back(e.first);
+
+    if (keys.size() == 2 && keys[0] == 0 && m[0] == n / 3 * 2) {
+        cout << "Yes" << endl;
         ret();
     }
 
-    vector<ll> numbers, counts;
-    for (auto e : m) {
-        numbers.push_back(e.first);
-        counts.push_back(e.second);
-    }
-
-    if (numbers.size() == 2) {
-        if (numbers[0] == 0 && counts[1] == 2 * n / 3) {
-            cout << "Yes" << endl;
-        } else {
-            cout << "No" << endl;
-        }
+    if (keys.size() == 3 && m[keys[0]] == n / 3 && m[keys[0]] == n / 3 && m[keys[0]] == n / 3 &&
+        (keys[0] ^ keys[1] ^ keys[2]) == 0) {
+        cout << "Yes" << endl;
         ret();
     }
-
-    if (numbers.size() == 3) {
-        if (((numbers[0] ^ numbers[1] ^ numbers[2]) == 0) && counts[0] == n / 3 && counts[1] == n / 3 &&
-            counts[2] == n / 3) {
-            cout << "Yes" << endl;
-        } else {
-            cout << "No" << endl;
-        }
-        ret();
-    }
-
     cout << "No" << endl;
-
 }
-
