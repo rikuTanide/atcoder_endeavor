dac6562ef29ba17450ef033dc709fc2abc9111a8
riku <riku@tanide.net>
2020-06-28 22:58:36 +0900

    【WA】[AtCoder Grand Contest 040]A - ><

--- agc040/agc040_a.cpp
+++ agc040/agc040_a.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,8 +9,7 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
-typedef pair<double, double> P;
+typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
@@ -18,8 +19,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -44,22 +45,19 @@ typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 int main() {
     string s;
     cin >> s;
-    int n = s.size() + 1;
-
-    vector<int> vr(n, 0), vl(n, 0);
-
-    for (int i = n - 2; i >= 0; i--) {
-        char c = s[i];
-        if (c == '>') vr[i] = vr[i + 1] + 1;
+    int n = s.size();
+    vector<int> up(n + 1), down(n + 1);
+    rep(i, n) {
+        if (s[i] == '<') up[i + 1] = up[i] + 1;
     }
-    for (int i = 0; i < n - 1; i++) {
-        char c = s[i];
-        if (c == '<') vl[i + 1] = vl[i] + 1;
+    for (int i = n - 1; i >= 0; i--) {
+        if (s[i] == '>') down[i] = down[i + 1] + 1;
     }
 
-    vector<ll> v(n);
-    rep(i, n) v[i] = max(vr[i], vl[i]);
-    ll sum = accumulate(v.begin(), v.end(), 0ll);
-    cout << sum << endl;
+    int ans = 0;
+    rep(i, n + 1) {
+        ans += max(up[i], down[i]);
+    }
 
+    cout << ans << endl;
 }
