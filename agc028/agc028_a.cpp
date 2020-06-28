7b2d28574f73c92d850be89755a3b9d70f42ccb8
riku <riku@tanide.net>
2020-06-28 12:55:43 +0900

    【WA】[AtCoder Grand Contest 028]A - Two Abbreviations

--- agc028/agc028_a.cpp
+++ agc028/agc028_a.cpp
@@ -1,38 +1,46 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
-//using namespace boost::multiprecision;
 using namespace std;
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-typedef pair<int, int> P;
-//typedef pair<ll, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
 #define ret() return 0;
 
-bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 ll gcd(ll x, ll y) {
     if (x > y) swap(x, y);
@@ -51,23 +59,23 @@ ll lcm(ll x, ll y) {
 
 int main() {
     int n, m;
+    cin >> n >> m;
+
     string s, t;
-    cin >> n >> m >> s >> t;
+    cin >> s >> t;
 
     ll l = lcm(n, m);
-    map<int, char> x;
 
-    for (int i = 0; i < n; i++) {
-        x[l / n * i] = s[i];
-    }
-    for (int i = 0; i < m; i++) {
-        x[l / m * i] = t[i];
-    }
-    for (int i = 0; i < n; i++) {
-        if (x[l / n * i] != s[i]) {
+    map<ll, char> x;
+
+    rep(i, n) x[i * l / n] = s[i];
+
+    rep(i, m) x[i * l / m] = t[i];
+
+    rep(i, n) if (x[i * l / n] != s[i]) {
             cout << -1 << endl;
             ret();
         }
-    }
+
     cout << l << endl;
 }
