4fdf9103b04ffc79d4d60f26fffac6fe3b02d3f8
riku <riku@tanide.net>
2020-06-29 23:46:43 +0900

    【AC】[AtCoder Beginner Contest 040]C - 柱柱柱柱柱

--- abc040/abc040_c.cpp
+++ abc040/abc040_c.cpp
@@ -1,58 +1,64 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-//typedef pair<ll, int> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-const ll INF = 1e15;
-//const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
 #define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-//const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
     int n;
     cin >> n;
-    vector<ll> polls(n);
-    rep(i, n) cin >> polls[i];
+    vector<ll> pillars(n);
+    rep(i, n) cin >> pillars[i];
 
     vector<ll> dp(n, INF);
     dp[0] = 0;
-
-    auto set = [&](ll from, ll to) {
-        if (to >= n) return;
-        cmin(dp[to], dp[from] + abs(polls[from] - polls[to]));
-    };
-
-    for (ll i = 0; i < n; i++) {
-        set(i, i + 1);
-        set(i, i + 2);
+    rep(i, n) {
+        if (i + 1 < n) {
+            ll c = abs(pillars[i] - pillars[i + 1]);
+            cmin(dp[i + 1], dp[i] + c);
+        }
+        if (i + 2 < n) {
+            ll c = abs(pillars[i] - pillars[i + 2]);
+            cmin(dp[i + 2], dp[i] + c);
+        }
     }
 
     cout << dp.back() << endl;
