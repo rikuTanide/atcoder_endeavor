817026ce69d1e54128c3e5e4b40fb4c6b8e1d23b
riku <riku@tanide.net>
2020-06-29 21:55:58 +0900

    【AC】[CODE FESTIVAL 2014 決勝]E - 常ならずグラフ

--- code-festival-2014-final/code_festival_final_e.cpp
+++ code-festival-2014-final/code_festival_final_e.cpp
@@ -1,23 +1,26 @@
-#define _GLIBCXX_DEBUG
-
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
 const double PI = 3.14159265358979323846;
 typedef long long ll;
 const double EPS = 1e-9;
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -35,37 +38,47 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
 //const ll mod = 1e10;
-typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
     int n;
     cin >> n;
-    vector<ll> numbers(n);
-    rep(i, n) cin >> numbers[i];
-    vector<ll> numbers2;
+    vector<int> rates(n);
+    rep(i, n) cin >> rates[i];
 
-    auto is_uu = [&](int i) {
-        return numbers2[i - 1] < numbers2[i] && numbers2[i] < numbers2[i + 1];
-    };
-
-    auto is_dd = [&](int i) {
-        return numbers2[i - 1] > numbers2[i] && numbers2[i] > numbers2[i + 1];
-    };
-
-    for (ll l : numbers)
-        if (numbers2.empty()) numbers2.push_back(l);
-        else if (numbers2.back() != l) numbers2.push_back(l);
-
-    int ans = numbers2.size();
-    rep(i, numbers2.size()) {
-        if (i == 0) continue;
-        else if (i == numbers2.size() - 1) continue;
-        else if (is_uu(i) || is_dd(i)) ans--;
-        else continue;
+    vector<int> graph;
+    rep(i, n) {
+        if (i == 0) {
+            graph.push_back(rates[i]);
+        } else if (graph.size() == 1) {
+            if (graph.back() != rates[i]) graph.push_back(rates[i]);
+        } else {
+            int back = graph.size() - 1;
+            int back2 = back - 1;
+            if (graph[back2] < graph[back]) {
+                if (graph[back] < rates[i]) {
+                    graph.back() = rates[i];
+                } else if (graph[back] == graph[back2]) {
+                    continue;
+                } else if (graph[back] > rates[i]) {
+                    graph.push_back(rates[i]);
+                }
+            } else {
+                if (graph[back] < rates[i]) {
+                    graph.push_back(rates[i]);
+                } else if (graph[back] == graph[back2]) {
+                    continue;
+                } else if (graph[back] > rates[i]) {
+                    graph.back() = rates[i];
+                }
+            }
+        }
     }
-    if (ans <= 2) ans = 0;
-    cout << ans << endl;
+
+    int size = graph.size();
+
+    cout << (size <= 2 ? 0 : size) << endl;
+
 }
