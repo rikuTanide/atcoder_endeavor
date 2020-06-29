9c936d7bcb2009f6b0732ae52758f9c7c9491a78
riku <riku@tanide.net>
2020-06-29 21:27:32 +0900

    【AC】[AtCoder Regular Contest 010]B - 超大型連休

--- arc010/arc010_2.cpp
+++ arc010/arc010_2.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
-typedef pair<int, int> P;
-const ll INF = 1e15;
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -29,75 +35,69 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-template<typename ... Args>
-std::string format(const std::string &fmt, Args ... args) {
-    size_t len = std::snprintf(nullptr, 0, fmt.c_str(), args ...);
-    std::vector<char> buf(len + 1);
-    std::snprintf(&buf[0], len + 1, fmt.c_str(), args ...);
-    return std::string(&buf[0], &buf[0] + len);
-}
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-    vector<int> md = {
-            0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-    };
-
-    map<string, int> days;
-
-    int k = 0;
-    for (int i = 1; i <= 12; i++) {
-        for (int d = 1; d <= md[i]; d++) {
-            string s = format("%d/%d", i, d);
-            days[s] = k;
-            k++;
-        }
-    }
-
-    vector<int> year(366, 0);
-    for (int i = 0; i < 366; i++) {
-        if (i % 7 == 0) year[i]++;
-        if (i % 7 == 6) year[i]++;
-    }
-
     int n;
     cin >> n;
+    set<P> holidays;
     rep(i, n) {
-        string day;
-        cin >> day;
+        int m, d;
+        scanf("%d/%d", &m, &d);
 
-        auto it = days.find(day);
-        assert(it != days.end());
-        int d = (*it).second;
-        year[d]++;
-
+        holidays.insert({m, d});
     }
-    
-    for (int i = 0; i < 365; i++) {
-        if (year[i] >= 2) {
-            year[i + 1] += (year[i] - 1);
-            year[i] = 1;
+
+    vector<int> month_days = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    vector<P> days;
+    for (int i = 1; i <= 12; i++) {
+        for (int j = 1; j <= month_days[i - 1]; j++) {
+            days.push_back(P(i, j));
         }
     }
 
-    vector<int> dp(366, 0);
+    vector<bool> is_holiday(366, false);
     rep(i, 366) {
-        if (year[i] > 0) dp[i] = 1;
+        is_holiday[i] = i % 7 == 0 || i % 7 == 6;
     }
 
-    for (int i = 1; i <= 365; i++) {
-        if (year[i] >= 1) {
-            dp[i] += dp[i - 1];
+    int q = 0;
+    rep(i, 366) {
+        P day = days[i];
+        if (holidays.find(day) != holidays.end()) {
+            // 祝日
+            if (is_holiday[i]) {
+                q++;
+            } else {
+                is_holiday[i] = true;
+            }
+        } else {
+            if (is_holiday[i]) {
+
+            } else if (q > 0) {
+                q--;
+                is_holiday[i] = true;
+            } else {
+
+            }
         }
     }
 
-    cout << *max_element(dp.begin(), dp.end()) << endl;
+    vector<int> dp(366, -1);
+    dp[0] = 1;
+    rep(i, 366) {
+        if (i == 0) continue;
+        if (is_holiday[i]) dp[i] = dp[i - 1] + 1;
+        else dp[i] = 0;
+    }
+
+    int ans = *max_element(dp.begin(), dp.end());
+    cout << ans << endl;
 
 }
