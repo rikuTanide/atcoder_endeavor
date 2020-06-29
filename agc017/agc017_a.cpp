f60d3027173749b2fb48f98ba16820b381f34584
riku <riku@tanide.net>
2020-06-30 00:12:07 +0900

    【AC】[AtCoder Grand Contest 017]A - Biscuits

--- agc017/agc017_a.cpp
+++ agc017/agc017_a.cpp
@@ -1,59 +1,67 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
-//using namespace boost::multiprecision;
 using namespace std;
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-typedef pair<int, int> P;
-//typedef pair<ll, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
+#define ret() return 0;
 
-bool contain(set<string> &s, string a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-vector<pair<ll, int >> factorize(ll n) {
-    vector<pair<ll, int >> res;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+map<ll, int> factorize(ll n) {
+    map<ll, int> res;
 
     for (ll i = 2; i * i <= n; i++) {
         if (n % i != 0) {
             continue;
         }
-        res.emplace_back(i, 0);
+        res[i] = 0;
         while (n % i == 0) {
             n /= i;
-            res.back().second++;
+            res[i]++;
         }
     }
 
-    if (n != 1) res.emplace_back(n, 1);
+    if (n != 1) res[n] = 1;
     return res;
-
 }
 
 ll comb(ll l, ll r) {
-    vector<ll> factors(50);
+    map<ll, ll> factors;
     for (ll j = l; j > (l - r); j--) {
         auto fs = factorize(j);
         for (auto f : fs) {
@@ -70,38 +78,38 @@ ll comb(ll l, ll r) {
 
     ll s = 1;
 
-    for (int j = 1; j < 50; j++) {
-        for (int k = 0; k < factors[j]; k++) {
-            s *= j;
+    for (auto e : factors) {
+
+        for (int k = 0; k < e.second; k++) {
+            s *= e.first;
         }
     }
 
     return s;
-
 }
-
-
 int main() {
     int n, p;
-
     cin >> n >> p;
-    int odd = 0, even = 0;
-    rep(i, n) {
-        int a;
-        cin >> a;
-        if (a % 2 == 1) odd++;
-        else even++;
+    vector<int> as(n);
+    rep(i, n) cin >> as[i];
+
+    int zero = 0, one = 0;
+    for (int a : as) if (a % 2 == 0) zero++; else one++;
+
+    if (p == 0) {
+        ll pz = pow(2, zero);
+        ll ans = 0;
+        rep(i, one + 1) {
+            if (i % 2 == 0)ans += comb(one, i) * pz;
+        }
+        cout << ans << endl;
+    } else {
+        ll pz = pow(2, zero);
+        ll ans = 0;
+        rep(i, one + 1) {
+            if (i % 2 == 1)ans += comb(one, i) * pz;
+        }
+        cout << ans << endl;
     }
 
-    ll even2 = 1ll << even,
-            odd2 = 0;
-
-    for (int i = 0; i <= odd; i++) {
-        if (i % 2 != p) continue;
-        odd2 += comb(odd, i);
-    }
-
-
-    cout << even2 * odd2 << endl;
 }
-
