b92f5d1d0673e13eac4bea5ba5065a20f38b094f
riku <riku@tanide.net>
2020-06-29 22:03:52 +0900

    【AC】[AtCoder Beginner Contest 015]C - 高橋くんのバグ探し

--- abc015/abc015_3.cpp
+++ abc015/abc015_3.cpp
@@ -1,58 +1,68 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-//typedef pair<ll, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-//const ll INF = 1e15;
-//const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
 #define ret() return 0;
 
-bool contain(set<char> &s, char a) { return s.find(a) != s.end(); }
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-//const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-bool check(int n, int k, vector<vector<int>> &points, int depth, int sum) {
-    if (depth == n) {
-        return sum == 0;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+bool rec(vector<vector<int>> &questions, int n, int k, int i, int sum) {
+    if (i == n) return sum == 0;
+
+    rep(j, k) {
+        bool b = rec(questions, n, k, i + 1, sum ^ questions[i][j]);
+        if (b) return true;
     }
-    for (int i = 0; i < k; i++) {
-        bool b = check(n, k, points, depth + 1, sum ^ points[depth][i]);
-        if (b) {
-            return true;
-        }
-    }
+
     return false;
 }
 
 int main() {
     int n, k;
     cin >> n >> k;
-    vector<vector<int>> points(n, vector<int>(k, 0));
-    rep(i, n) rep(j, k) cin >> points[i][j];
-    bool b = check(n, k, points, 0, 0);
+
+    vector<vector<int>> questions(n, vector<int>(k));
+    rep(i, n) rep(j, k) cin >> questions[i][j];
+
+    bool b = rec(questions, n, k, 0, 0);
+
     cout << (b ? "Found" : "Nothing") << endl;
+
+
 }
