8769e3059cbe4651cadf637bfcffcf27af705560
riku <riku@tanide.net>
2020-06-29 23:35:51 +0900

    【AC】[AtCoder Regular Contest 053]B - 回文分割

--- arc053/arc053_b.cpp
+++ arc053/arc053_b.cpp
@@ -1,70 +1,69 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
-//typedef unsigned long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define sz(x) ll(x.size())
-//typedef pair<int, int> P;
-//typedef pair<ll, int> P;
 typedef pair<ll, ll> P;
-//const double INF = 1e10;
-//const ll INF = LONG_LONG_MAX / 100;
-//const ll INF = (1ll << 31) - 1;
-const ll INF = 1e15;
-//const ll MINF = LONG_LONG_MIN;
-//const int INF = INT_MAX / 10;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
-//typedef pair<int, int> P;
-//typedef pair<double, double> P;
 #define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-//const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-
     string s;
     cin >> s;
-    vector<int> counter(26, 0);
-    for (char c : s) counter[c - 'a']++;
+    vector<int> alphabets(26);
+    int n = s.size();
+    rep(i, n) alphabets[s[i] - 'a']++;
 
-    PQ_ASK q;
-    rep(i, 26) {
-        int j = counter[i];
-        if (j % 2 == 1) {
-            q.push(1);
-            counter[i]--;
-        }
+    int cs = 0;
+    for (int a : alphabets) cs += a % 2;
+    int ps = 0;
+    for (int a : alphabets) ps += a / 2;
+
+    if (cs == 0) {
+        cout << n << endl;
+        ret();
     }
 
-    if (q.empty()) q.push(0);
 
-    for (int i : counter) assert(i % 2 == 0);
+    int pb = ps / cs;
 
-    for (int i : counter) {
-        int l = i / 2;
-        for (int j = 0; j < l; j++) {
-            int k = q.top();
-            q.pop();
-            k += 2;
-            q.push(k);
-        }
-    }
+    int ans = pb * 2 + 1;
+    cout << ans << endl;
 
-    cout << q.top() << endl;
 
 }
