f318ef24b83350a34109c479e8ec55af96bf084f
riku <riku@tanide.net>
2020-06-30 21:01:46 +0900

    【AC】[Tenka1 Programmer Contest]C - 4/N

--- tenka1-2017/tenka1_2017_c.cpp
+++ tenka1-2017/tenka1_2017_c.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-const ll INF = 1e15;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -29,43 +35,28 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
-int check(ll n, ll x, ll y) {
-    ll bunshi = n * x * y;
-    ll bunbo = x * y * 4 - n * (y + x);
-    if (bunbo == 0) return -1;
-    if (bunshi % bunbo != 0) return -1;
-    ll ans = bunshi / bunbo;
-    if (ans <= 0) return -1;
-    return ans;
-}
-
-void test(double n, double x, double y, double now) {
-    cout << (4 / n) << endl;
-    cout << (1 / x) + (1 / y) + (1 / now) << endl;
-}
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
+    int N;
+    cin >> N;
 
-    ll n;
-    cin >> n;
-
-    for (ll x = 1; x <= 3500; x++) {
-        for (ll y = 1; y <= 3500; y++) {
-            ll now = check(n, x, y);
-            if (now == -1) continue;
-            printf("%d %d %d\n", x, y, now);
-//            test(n, x, y, now);
-            ret();
+    for (ll h = 1; h <= 3500; h++) {
+        for (ll w = 1; w <= 3500; w++) {
+            ll bunshi = N * h * w;
+            ll bunbo = 4ll * h * w - N * w - N * h;
+            if (bunbo <= 0) continue;
+            if (bunshi % bunbo == 0) {
+                ll n = bunshi / bunbo;
+                cout << h << ' ' << n << ' ' << w;
+                ret();
+            }
         }
     }
-
 
 }
