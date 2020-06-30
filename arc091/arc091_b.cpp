014ed1f18b962c4bca33102ab04319fad4e01e6b
riku <riku@tanide.net>
2020-06-30 22:16:59 +0900

    【AC】[AtCoder Regular Contest 091]D - Remainder Reminder

--- arc091/arc091_b.cpp
+++ arc091/arc091_b.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-const ll INF = 1e15;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -29,29 +35,29 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-    ll n, k;
+    int n, k;
     cin >> n >> k;
 
     ll ans = 0;
-    for (ll b = 1; b <= n; b++) {
-        ll p = n / b;
-        ll r = n % b;
-        ll ac = p * max(0ll, b - k);
-        ac += max(0ll, r - k + 1);
-        if (0 % b >= k) ac--;
-        ans += ac;
 
+    for (int b = 1; b <= n; b++) {
+        if (b <= k) continue;
 
+        ll ac1 = n / b * (b - k);
+        ll nokori = n - (n / b * b);
+        ll keshi = k == 0 ? 0 : k - 1;
+        ll ac2 = max(nokori - keshi, 0ll);
+
+        ll now = ac1 + ac2;
+        ans += now;
     }
-
     cout << ans << endl;
 }
