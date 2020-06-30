0c64f88d14d2775bfc109f17fec13938f7bbfb40
riku <riku@tanide.net>
2020-06-30 23:45:51 +0900

    【AC】[AtCoder Regular Contest 102]C - Triangular Relationship

--- arc102/arc102_a.cpp
+++ arc102/arc102_a.cpp
@@ -1,59 +1,65 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef long long ll;
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef pair<int, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e15;
-const ll MINF = -10e10;
-//const int INF = INT_MAX / 100;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("~/Downloads/02.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-const int mod = 1000000007;
-const double PI = 3.14159265358979323846;
-
-typedef pair<ll, ll> P;
-
-void print_line(vector<vector<int>> &ans) {
-    for (int i = 0; i < ans.size(); i++) {
-        for (int y = 0; y < ans[i].size(); y++) {
-            cout << ans[i][y];
-        }
-        cout << endl;
-    }
-}
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-
-    ll n, k;
+    int n, k;
     cin >> n >> k;
 
-    vector<ll> mods(k, 0);
-    for (ll i = 1; i <= n; i++) {
-        mods[i % k]++;
+    vector<ll> ps(k);
+    for (int i = 1; i <= n; i++) {
+        ps[i % k]++;
     }
 
-    ll ans = 0;
-    for (ll i = 0; i < k; i++) {
-        if ((i + i) % k == 0) {
-            ans += mods[i] * mods[i] * mods[i];
-        }
+    ll z = ps[0] * ps[0] * ps[0];
+
+    if (k % 2 == 1) {
+        cout << z << endl;
+        ret();
     }
 
-    cout << ans << endl;
+    ll h = ps[k / 2] * ps[k / 2] * ps[k / 2];
+
+    cout << z + h << endl;
 
 }
