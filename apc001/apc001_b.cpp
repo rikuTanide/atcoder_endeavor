fea36c3ff6a62a26432d6421e8303796af02ae47
riku <riku@tanide.net>
2020-06-30 21:49:26 +0900

    【WA】[AtCoder Petrozavodsk Contest 001]B - Two Arrays

--- apc001/apc001_b.cpp
+++ apc001/apc001_b.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-const ll INF = 1e15;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -29,46 +35,38 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
 
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-
     int n;
     cin >> n;
     vector<ll> as(n), bs(n);
     rep(i, n) cin >> as[i];
     rep(i, n) cin >> bs[i];
 
-    ll a_all = accumulate(as.begin(), as.end(), 0ll),
-            b_all = accumulate(bs.begin(), bs.end(), 0ll);
-    if (a_all > b_all) {
+    ll asum = accumulate(as.begin(), as.end(), 0ll);
+    ll bsum = accumulate(bs.begin(), bs.end(), 0ll);
+
+    ll x = bsum - asum;
+
+    if (x < 0) {
         cout << "No" << endl;
         ret();
     }
 
-    ll turn = b_all - a_all;
 
-    ll a_count = 0;
+    ll ab = 0;
+    rep(i, n) if (as[i] > bs[i]) ab += as[i] - bs[i];
 
-    for (int i = 0; i < n; i++) {
-        if (as[i] == bs[i]) continue;
-        if (as[i] < bs[i]) {
-            a_count += (bs[i] - as[i] + 1) / 2;
-        }
-    }
-
-    if (a_count <= turn) {
+    if (ab <= x) {
         cout << "Yes" << endl;
     } else {
         cout << "No" << endl;
     }
-
 
 }
