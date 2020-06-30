07044eb065599702ebfeb7d1cc52e9e2800b6cbf
riku <riku@tanide.net>
2020-06-30 22:29:08 +0900

    【TLE】[AtCoder Grand Contest 023]A - Zero-Sum Ranges

--- agc023/agc023_a.cpp
+++ agc023/agc023_a.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-const ll INF = 1e15;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -29,13 +35,12 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<long long, vector<long long>, greater<long long> > PQ_ASK;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 class CumulativeSum {
     vector<ll> numbers;
@@ -53,7 +58,7 @@ public:
 
     ll getSum(int i) {
         if (i == -1) return 0;
-        if (i == sums.size()) return 0;
+        if (i == sums.size()) return sums.back();
         return sums[i];
     }
 
@@ -61,7 +66,7 @@ public:
         return getSum(end) - getSum(start - 1);
     }
 
-    void calculate() {
+    void build() {
         for (int i = 0; i < numbers.size(); i++) {
             sums[i] = getSum(i - 1) + numbers[i];
         }
@@ -69,23 +74,30 @@ public:
 
 };
 
+
 int main() {
     int n;
     cin >> n;
+    vector<ll> as(n);
+    rep(i, n) cin >> as[i];
+
     CumulativeSum cs(n);
-    rep(i, n) {
-        int a;
-        cin >> a;
-        cs.set(i, a);
-    }
-    cs.calculate();
-    map<ll, ll> m;
-    m[0] = 1;
-    rep(i, n) m[cs.getSum(i)]++;
+    rep(i, n) cs.set(i, as[i]);
+    cs.build();
+
+    multiset<ll> st;
+    rep(i, n) st.insert(cs.getSum(i));
 
     ll ans = 0;
-    for (auto e : m) {
-        ans += (e.second * (e.second - 1)) / 2;
+    rep(i, n) {
+        ll target = as[i];
+        ll now = st.count(cs.getSum(i) - target);
+        ans += now;
+
+        st.erase(st.find(cs.getSum(i)));
+
     }
     cout << ans << endl;
+
 }
+
