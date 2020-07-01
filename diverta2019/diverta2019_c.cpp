c82437360953f5aaa67f25d2287885f6607bacd0
riku <riku@tanide.net>
2020-07-01 18:58:27 +0900

    【RE】[diverta 2019 Programming Contest]C - AB Substrings

--- diverta2019/diverta2019_c.cpp
+++ diverta2019/diverta2019_c.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-const ll INF = 1e15;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -29,51 +35,39 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-struct Reach {
-    int x, y, time;
-};
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
     int n;
     cin >> n;
+    vector<string> v(n);
+    rep(i, n) cin >> v[i];
 
-    int xa = 0, bx = 0, ba = 0;
-    int ans = 0;
-    rep(i, n) {
-        string s;
-        cin >> s;
-        rep(j, s.size() - 1) {
-            if (s[j] == 'A' && s[j + 1] == 'B') ans++;
+    int ab = 0, ba = 0, bx = 0, xa = 0;
+
+    for (string s: v) {
+        rep(i, s.size() - 1) {
+            if (s.substr(i, 2) == "AB") ab++;
         }
+    }
+
+    for (string s: v) {
         if (s.front() == 'B' && s.back() == 'A') ba++;
         else if (s.front() == 'B') bx++;
         else if (s.back() == 'A') xa++;
     }
 
+
     if (ba == 0) {
         int add = min(bx, xa);
-        cout << ans + add << endl;
+        cout << add + ab << endl;
     } else {
-        int add = ba - 1;
-        if (xa > 0) {
-            add++;
-            xa--;
-        }
-        if (bx > 0) {
-            add++;
-            bx--;
-        }
-        add += min(bx, xa);
-        cout << ans + add << endl;
+        __throw_runtime_error("konai");
     }
-
 
 }
