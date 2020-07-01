ca6d2508ffbfe8de77161e0bb31e29c0ea6da27e
riku <riku@tanide.net>
2020-07-01 13:52:33 +0900

    【AC】[AtCoder Beginner Contest 112]D - Partition

--- abc112/abc112_d.cpp
+++ abc112/abc112_d.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-const ll INF = 1e15;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -29,30 +35,27 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
     ll n, m;
     cin >> n >> m;
-    ll ans = 1;
-    for (ll i = 1; i * i <= m; i++) {
-        if (m % i != 0) continue;
-        ll j = m / i;
-        if (j >= n) {
-            cmax(ans, i);
 
-        }
-        if (i >= n) {
-            cmax(ans, j);
-        }
+    set<ll> st;
+    for (int i = 1; i * i <= m; i++) {
+        if (m % i != 0)continue;
+        st.insert(i);
+        st.insert(m / i);
+    }
+
+    ll ans = 0;
+    for (ll i : st) {
+        if (m / n >= i) cmax(ans, i);
     }
     cout << ans << endl;
-
 }
