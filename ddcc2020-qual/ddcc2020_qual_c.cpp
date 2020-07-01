088109d1af81e5fa8e0d4088d04aec81e506e4ab
riku <riku@tanide.net>
2020-07-01 23:43:12 +0900

    【AC】[DISCO presents ディスカバリーチャンネル コードコンテスト2020 予選]C - Strawberry Cakes

--- ddcc2020-qual/ddcc2020_qual_c.cpp
+++ ddcc2020-qual/ddcc2020_qual_c.cpp
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
-typedef pair<double, double> P;
-const ll INF = 1e15;
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -30,13 +35,12 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 
 class UnionFind {
@@ -83,9 +87,6 @@ public:
     }
 };
 
-int to_id(int h, int w, int x, int y) {
-    return w * y + x;
-}
 
 class Conv {
     ll cursor = 0;
@@ -135,68 +136,60 @@ public:
 int main() {
     int h, w, k;
     cin >> h >> w >> k;
+    auto to_id = [&](int y, int x) {
+        return y * w + x;
+    };
+
+    vector<vector<char>> cake(h, vector<char>(w));
+    rep(y, h) rep(x, w) cin >> cake[y][x];
+
+    vector<bool> has_row(h);
+    rep(y, h) has_row[y] = count(cake[y].begin(), cake[y].end(), '#') > 0;
 
     UnionFind uf(w * h);
 
-    vector<vector<bool>> cake(h, vector<bool>(w, false));
     rep(y, h) rep(x, w) {
-            char c;
-            cin >> c;
-            cake[y][x] = c == '#';
+            if (has_row[y])continue;
+            if (y == 0)continue;
+            uf.connect(to_id(y, x), to_id(y - 1, x));
         }
 
-    vector<bool> has_strawberry(h, false);
-    rep(y, h) {
-        rep(x, w) has_strawberry[y] = (has_strawberry[y] || cake[y][x]);
+    int first_has_row = distance(has_row.begin(), find(has_row.begin(), has_row.end(), true));
+    rep(x, w) {
+        uf.connect(to_id(first_has_row, x), to_id(0, x));
     }
 
-    for (int y = 0; y < h; y++) {
-        if (!has_strawberry[y]) continue;
+    rep(y, h) rep(x, w) {
+            if (!has_row[y])continue;
+            if (x == w - 1)continue;
+            if (cake[y][x] == '#') continue;
+            uf.connect(to_id(y, x), to_id(y, x + 1));
+        }
+
+    rep(y, h) {
+        if (!has_row[y])continue;
+        int last_s = 0;
         rep(x, w) {
-            if (x == 0) continue;
-            else if (cake[y][x - 1]) continue;
-            else uf.connect(to_id(h, w, x - 1, y), to_id(h, w, x, y));
+            if (cake[y][x] == '#') last_s = x;
         }
-
-        int last_s = -1;
-        rep(x, w) if (cake[y][x]) last_s = x;
-        assert(last_s != -1);
-        if (last_s != w - 1) uf.connect(to_id(h, w, last_s + 1, y), to_id(h, w, last_s, y));
+        uf.connect(to_id(y, last_s), to_id(y, w - 1));
     }
 
-    if (!has_strawberry[0]) {
-        int ay = 0;
-        for (int y = 1; y < h; y++) {
-            if (has_strawberry[y]) {
-                ay = y;
-                break;
-            }
-        }
 
-        assert(ay != 0);
-        rep(x, w) uf.connect(to_id(h, w, x, ay), to_id(h, w, x, 0));
-        
-    }
-    rep(y, h) {
-        if (y == 0) continue;
-        if (has_strawberry[y]) continue;
-        rep(x, w) uf.connect(to_id(h, w, x, y - 1), to_id(h, w, x, y));
-    }
     Conv conv;
-    rep(y, h) {
-        rep(x, w) {
-            int k = uf.root(to_id(h, w, x, y));
-            conv.cache(k);
-        }
-    }
+    rep(y, h)rep(x, w) conv.cache(uf.root(to_id(y, x)));
     conv.build();
 
+    vector<vector<int>> ans(h, vector<int>(w));
+    rep(y, h)rep(x, w) ans[y][x] = conv.convert(uf.root(to_id(y, x))) + 1;
+
     rep(y, h) {
         rep(x, w) {
-            int k = uf.root(to_id(h, w, x, y));
-            k = conv.convert(k);
-            cout << k + 1 << ' ';
+            cout << ans[y][x] << ' ';
         }
         cout << endl;
     }
+
+    cout << endl;
+
 }
