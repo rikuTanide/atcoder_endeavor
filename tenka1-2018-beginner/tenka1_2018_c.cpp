c372ea06194b96abb3a5a512dc507037fe326ff8
riku <riku@tanide.net>
2020-07-01 16:15:42 +0900

    【WA】[Tenka1 Programmer Beginner Contest]C - Align

--- tenka1-2018-beginner/tenka1_2018_c.cpp
+++ tenka1-2018-beginner/tenka1_2018_c.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-const ll INF = 1e15;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -29,69 +35,60 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-
     int n;
     cin >> n;
-    vector<ll> numbers(n);
-    rep(i, n) cin >> numbers[i];
-    sort(numbers.begin(), numbers.end());
+    vector<ll> v(n);
+    rep(i, n) cin >> v[i];
+    sort(v.begin(), v.end());
 
     deque<ll> q;
-    for (ll l : numbers) q.push_back(l);
+    for (ll l : v) q.push_back(l);
 
     list<ll> l;
-    l.push_back(q.front());
-    q.pop_front();
 
+    int i = 0;
     while (!q.empty()) {
 
-        ll f = q.front();
-        ll b = q.back();
-
-        ll ff = abs(f - l.front());
-        ll fb = abs(f - l.back());
-        ll bf = abs(b - l.front());
-        ll bb = abs(b - l.back());
-
-        if (ff >= fb && ff >= bf && ff >= bb) {
-            l.push_front(f);
-            q.pop_front();
-            continue;
+        if (q.size() == 1) {
+            if (abs(l.back() - q.front()) > abs(l.front() - q.front())) {
+                l.push_back(q.front());
+            } else {
+                l.push_front(q.front());
+            }
+            break;
         }
-        if (fb >= ff && fb >= bf && fb >= bb) {
+
+        if (i % 2 == 0) {
+            ll f = q.front();
+            q.pop_front();
             l.push_back(f);
-            q.pop_front();
-            continue;
-        }
-        if (bf >= ff && bf >= fb && bf >= bb) {
-            l.push_front(b);
+
+            ll b = q.back();
             q.pop_back();
-            continue;
-        }
-        if (bb >= ff && bb >= fb && bb >= bf) {
             l.push_back(b);
+        } else {
+            ll f = q.front();
+            q.pop_front();
+            l.push_back(f);
+
+            ll b = q.back();
             q.pop_back();
-            continue;
+            l.push_front(b);
         }
-        __throw_runtime_error("konai");
+        i++;
     }
 
-    ll prev = l.front(), ans = 0;
-    for (ll i : l) {
-        ans += abs(i - prev);
-        prev = i;
-    }
-
-
+    vector<ll> a;
+    for (ll j : l) a.push_back(j);
+    ll ans = 0;
+    rep(i, n - 1) ans += abs(a[i] - a[i + 1]);
     cout << ans << endl;
 }
