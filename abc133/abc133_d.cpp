0db00ebef5a8fb82932ea55aa4ab5d4d93cec2a1
riku <riku@tanide.net>
2020-07-01 21:46:09 +0900

    【AC】[AtCoder Beginner Contest 133]D - Rain Flows into Dams

--- abc133/abc133_d.cpp
+++ abc133/abc133_d.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-const ll INF = 1e15;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -29,34 +35,33 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
     int n;
     cin >> n;
-    vector<ll> water(n);
-    rep(i, n) cin >> water[i];
+    vector<ll> dam(n);
+    rep(i, n) cin >> dam[i];
 
-    ll s = 0;
-    for (int i = 0; i < n; i++) {
-        if (i % 2 == 0) s += water[i];
-        else s -= water[i];
+
+    ll first = 0;
+    rep(i, n) {
+        if (i % 2 == 0) first += dam[i];
+        else first -= dam[i];
     }
+    cout << first << endl;
 
-    vector<ll> mountains(n);
-    mountains[0] = s;
-    for (int i = 1; i < n; i++) {
-        ll a = mountains[i - 1];
-        ll w = water[i - 1];
-        mountains[i] = 2 * w - a;
+    rep(i, n) {
+        if (i == 0) continue;
+        first -= dam[i - 1];
+        first = -first;
+        first += dam[i - 1];
+        cout << first << endl;
     }
-
-    for (ll a : mountains) cout << a << endl;
 
 }
