7503e014f701395285d8ef8521f67226058cd23b
riku <riku@tanide.net>
2020-07-03 00:25:17 +0900

    【WA】[キーエンス プログラミング コンテスト 2020]B - Robot Arms

--- keyence2020/keyence2020_b.cpp
+++ keyence2020/keyence2020_b.cpp
@@ -110,6 +110,54 @@ struct Range {
     int l, r;
 };
 
+template<class T, class F>
+struct SegmentTree {
+    F f;
+    T ti;
+    vector<T> dat;
+    int sz;
+
+    SegmentTree(const F &f, const T &ti) : f(f), ti(ti) {}
+
+    void build(const vector<T> &v) {
+        assert(v.size());
+        sz = 1;
+        while (sz < v.size())sz <<= 1;
+        dat.resize(sz << 1, ti);
+        for (int i = 0; i < v.size(); i++)dat[sz - 1 + i] = v[i];
+        for (int i = sz - 2; i >= 0; i--)dat[i] = f(dat[i * 2 + 1], dat[i * 2 + 2]);
+    }
+
+    inline void update(int k, T x) {
+        k += sz - 1;
+        dat[k] = x;
+        while (k) {
+            k = (k - 1) / 2;
+            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
+        }
+    }
+
+    inline void add(int k, int x) {
+        k += sz - 1;
+        dat[k] = f(dat[k], x);
+        while (k) {
+            k = (k - 1) / 2;
+            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
+        }
+    }
+
+    inline T query(int a, int b) {
+        return query(a, b, 0, 0, sz);
+    }
+
+    T query(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l)return ti;
+        if (a <= l && r <= b)return dat[k];
+        return f(query(a, b, k * 2 + 1, l, (l + r) / 2), query(a, b, k * 2 + 2, (l + r) / 2, r));
+    }
+};
+
+
 int main() {
     int n;
     cin >> n;
@@ -129,18 +177,20 @@ int main() {
         return r1.r < r2.r;
     });
 
-    vector<int> dp(conv.next(), 0);
+    auto f = [](ll i, ll j) { return max(i, j); };
+    SegmentTree<ll, decltype(f)> segmentTree(f, 0);
+    segmentTree.build(vector<ll>(conv.next(), 0));
 
     auto get = [&](int i) -> int {
         if (i == -1) return 0;
-        return dp[i];
+        return segmentTree.query(0, i + 1);
     };
 
     rep(i, n) {
         Range range = ranges[i];
-        dp[range.r] = max({get(range.l - 1) + 1, get(range.r - 1), dp[range.r]});
+        segmentTree.update(range.r, max(get(range.l - 1) + 1, get(range.r)));
     }
 
-    cout << dp.back() << endl;
+    cout << segmentTree.query(0, conv.next()) << endl;
 
 }
