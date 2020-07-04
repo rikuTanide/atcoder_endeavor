28421c27b40745bcc24acbea6d671d04bc844ef2
riku <riku@tanide.net>
2020-07-04 23:20:32 +0900

    【AC】[AtCoder Beginner Contest 020]C - 壁抜け

--- abc020/abc020_c.cpp
+++ abc020/abc020_c.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,15 +9,18 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -36,137 +41,123 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 //const ll mod = 1e10;
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
-
-struct Point {
-    int y, x;
+// https://qiita.com/ta-ka/items/a023a11efe17ab097433
+struct Edge {
+    ll to, cost;
 };
 
-Point get_start(vector<vector<char>> &grid, int h, int w) {
-    rep(y, h) rep (x, w) if (grid[y][x] == 'S') return Point{y, x};
-    __throw_runtime_error("konai");
-}
 
+class Dijkstra {
+    ll vector_count;
 
-Point get_goal(vector<vector<char>> &grid, int h, int w) {
-    rep(y, h) rep (x, w) if (grid[y][x] == 'G') return Point{y, x};
-    __throw_runtime_error("konai");
-}
-
-
-class WarchallFloyd {
-
-    int n;
-    vector<vector<ll>> distances;
+    vector<ll> distances;
+    vector<vector<Edge>> edges;
 
 public:
-    WarchallFloyd(int n) : n(n), distances(n, vector<ll>(n, INF)) {
-        rep(i, n) distances[i][i] = 0;
-    };
+    Dijkstra(ll n) {
+        vector_count = n;
+        distances.resize(n, INF);
+        edges.resize(n);
+    }
 
-    void warshall_floyd() {
-        for (int k = 0; k < n; k++) {       // 経由する頂点
-            for (int i = 0; i < n; i++) {    // 始点
-                for (int j = 0; j < n; j++) {  // 終点
-                    if (distances[i][j] == INF && (distances[i][k] == INF || distances[k][j] == INF)) {
-                        distances[i][j] = INF;
-                    } else {
-                        distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
-                    }
+    void insertTowWay(ll from, ll to, ll cost) {
+        edges[from].push_back({to, cost});
+        edges[to].push_back({from, cost});
+    }
+
+    void insert(ll from, ll to, ll cost) {
+        edges[from].push_back({to, cost});
+    }
+
+    void dijkstra(ll k) {
+        distances[k] = 0;  // 始点sへの最短距離は0
+
+        priority_queue<P, vector<P>, greater<P>> que;  // 距離が小さい順に取り出せるようgreater<P>を指定
+        que.push(P(0, k));
+        while (!que.empty()) {
+            P p = que.top();
+            que.pop();
+            int v = p.second;  // 更新した頂点の中で距離が最小の頂点v
+            if (distances[v] < p.first) {
+                continue;
+            }
+            for (auto e : edges[v]) {  // 頂点vから出る辺eを走査
+                if (distances[e.to] > distances[v] + e.cost) {
+                    distances[e.to] = distances[v] + e.cost;
+                    que.push(P(distances[e.to], e.to));
                 }
             }
         }
     }
 
-    ll distance(int from, int to) {
-        return distances[from][to];
+    ll distance(ll to) {
+        return distances[to];
     }
-
-    void add(int from, int to, ll cost) {
-        distances[from][to] = cost;
-//        distances[to][from] = cost;
-    }
-
-    friend std::istream &operator>>(std::istream &in, WarchallFloyd &o) {
-        int from, to;
-        ll c;
-        cin >> from >> to >> c;
-        from--;
-        to--;
-
-        o.distances[from][to] = c;
-        o.distances[to][from] = c;
-        return in;
-    }
-
 
 };
 
 
-bool check(vector<vector<char>> &grid, ll mid, int h, int w, Point start, Point goal, ll t) {
-    struct Direction {
-        int x, y;
+bool check(int h, int w, vector<vector<char>> &grid, P s, P g, ll target, ll mid) {
+
+    auto to_id = [&](int y, int x) {
+        return y * w + x;
     };
 
+    struct Direction {
+        int y, x;
+    };
+    auto reachable = [&](int y, int x) {
+        if (x == -1 || x == w || y == -1 || y == h) return false;
+        return true;
+    };
     vector<Direction> directions = {
             {0,  1},
             {1,  0},
             {0,  -1},
             {-1, 0},
     };
-
-
-    auto reachable = [&](int x, int y) {
-        if (x == -1 || x == w || y == -1 || y == h) return false;
-        return true;
-    };
-
-    auto to_id = [&](int y, int x) {
-        return y * w + x;
-    };
-
-    WarchallFloyd wf(h * w);
-
-    for (int y = 0; y < h; y++) {
-        for (int x = 0; x < w; x++) {
-            for (Direction d : directions) {
-                int nx = x + d.x;
+    Dijkstra dijkstra(h * w);
+    rep(y, h) rep(x, w) for (Direction d : directions) {
                 int ny = y + d.y;
-                if (!reachable(nx, ny))continue;
-                char next = grid[ny][nx];
-                if (next == '#') {
-                    wf.add(to_id(y, x), to_id(ny, nx), mid);
-                } else {
-                    wf.add(to_id(y, x), to_id(ny, nx), 1);
-                }
+                int nx = x + d.x;
+
+                int from = to_id(y, x);
+                int to = to_id(ny, nx);
+
+                if (!reachable(ny, nx)) continue;
+                if (grid[ny][nx] == '#') dijkstra.insert(from, to, mid);
+                else dijkstra.insert(from, to, 1);
             }
-        }
-    }
+    dijkstra.dijkstra(to_id(s.first, s.second));
+    ll now = dijkstra.distance(to_id(g.first, g.second));
+    return now <= target;
 
-    wf.warshall_floyd();
-
-    return wf.distance(to_id(start.y, start.x), to_id(goal.y, goal.x)) <= t;
 }
 
 int main() {
     int h, w;
     ll t;
     cin >> h >> w >> t;
-    vector<vector<char>> grid(h, vector<char>(w));
-    rep(y, h) rep (x, w) cin >> grid[y][x];
 
-    Point start = get_start(grid, h, w);
-    Point goal = get_goal(grid, h, w);
+    vector<vector<char>> grid(h, vector<char>(w));
+    rep(y, h) rep(x, w) cin >> grid[y][x];
+
+    P s = [&] {
+        rep(y, h) rep(x, w) if (grid[y][x] == 'S') return P(y, x);
+        __throw_runtime_error("konaide");
+    }();
+    P g = [&] {
+        rep(y, h) rep(x, w) if (grid[y][x] == 'G') return P(y, x);
+        __throw_runtime_error("konaide");
+    }();
 
     ll floor = 0, ceil = INF;
     while (floor + 1 < ceil) {
         ll mid = (floor + ceil) / 2;
-        bool b = check(grid, mid, h, w, start, goal, t);
-        if (b) {
-            floor = mid;
-        } else {
-            ceil = mid;
-        }
+        bool ok = check(h, w, grid, s, g, t, mid);
+        if (ok) floor = mid;
+        else ceil = mid;
     }
+
     cout << floor << endl;
 }
-
