b3f7bc230046c6be80793c9311dff6919ffaea01
riku <riku@tanide.net>
2020-07-07 22:04:42 +0900

    【AC】[AtCoder Beginner Contest 034]C - 経路

--- abc034/abc034_c.cpp
+++ abc034/abc034_c.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,7 +9,6 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -19,7 +20,7 @@ double equal(double a, double b) {
 }
 
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -40,7 +41,6 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 //const ll mod = 1e10;
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
-
 const int mod = 1000000007;
 
 struct mint {
@@ -130,13 +130,10 @@ struct combination {
     }
 } combination(1000000);
 
-
 int main() {
-    int w, h;
-    cin >> w >> h;
 
-    w--;
-    h--;
-    cout << combination((h + 1) + w - 1, w) << endl;
+    int n, k;
+    cin >> n >> k;
+    cout << combination(n - 1 + k - 1 + 1 - 1, n - 1) << endl;
 
 }
