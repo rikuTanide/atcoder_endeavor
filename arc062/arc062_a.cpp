046f075e709b7a5d76030f33a03df58d84875922
riku <riku@tanide.net>
2020-07-08 22:22:05 +0900

    【AC】[AtCoder Regular Contest 062]C - AtCoDeer and Election Report

--- arc062/arc062_a.cpp
+++ arc062/arc062_a.cpp
@@ -1,51 +1,72 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define sz(x) ll(x.size())
-//typedef long long ll;
+
+const double PI = 3.14159265358979323846;
 typedef long long ll;
-//typedef pair<int, int> P;
-//typedef pair<ll, ll> P;
-//const double INF = 1e10;
-const ll INF = 10e15;
-const ll MINF = -10e10;
-//const int INF = INT_MAX / 100;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
 
-//ifstream myfile("~/Downloads/02.txt");
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
 
-//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-const int mod = 1000000007;
-const double PI = 3.14159265358979323846;
-
-typedef pair<char, char> P;
-
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
     int n;
     cin >> n;
-    ll tn = 1, an = 1;
-    rep(i, n) {
-        ll t, a;
-        cin >> t >> a;
 
-        ll tr = (tn + t - 1) / t;
-        ll ar = (an + a - 1) / a;
+    vector<P> v(n);
+    for (P &p: v) cin >> p.first >> p.second;
+
+    ll t = 1, a = 1;
+
+    auto calc = [](ll x, ll y) -> ll {
+        if (x <= y) return 1;
+        return (x + y - 1) / y;
+    };
+
+    for (P p : v) {
+        ll tr = calc(t, p.first);
+        ll ar = calc(a, p.second);
+
         ll r = max(tr, ar);
-
-        tn = t * r;
-        an = a * r;
+        t = p.first * r;
+        a = p.second * r;
 
     }
-    cout << tn + an << endl;
+
+
+    cout << t + a << endl;
 
 }
-
