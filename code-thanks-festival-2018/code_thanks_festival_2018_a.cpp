11091b10899f7c308d1f232c3721199c79c295ce
riku <riku@tanide.net>
2020-07-09 21:03:32 +0900

    【AC】[CODE THANKS FESTIVAL 2018]A - Two Problems

---
+++ code-thanks-festival-2018/code_thanks_festival_2018_a.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    ll t, a, b, c, d;
+    cin >> t >> a >> b >> c >> d;
+
+    ll ans = 0;
+
+    if (a <= t) {
+        cmax(ans, b);
+    }
+    if (c <= t) {
+        cmax(ans, d);
+    }
+    if (a + c <= t) {
+        cmax(ans, b + d);
+    }
+    cout << ans << endl;
+
+}
