5484d51d8320987fb63acc17b022fb5aa6073591
riku <riku@tanide.net>
2020-07-10 11:15:57 +0900

    【AC】[CODE THANKS FESTIVAL 2017(Parallel)]B - Concatenated Palindrome

---
+++ code-thanks-festival-2017-open/code_thanks_festival_2017_b.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+const double PI = 3.14159265358979323846;
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+std::istream &operator>>(std::istream &in, set<ll> &o) {
+    ll a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+
+//const ll mod = 1e10;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+
+bool check(string s) {
+    string t = s;
+    reverse(t.begin(), t.end());
+    return s == t;
+}
+
+int main() {
+
+    string s;
+    cin >> s;
+
+    rep(i, s.size() + 1) {
+        string t = s.substr(0, i);
+        reverse(t.begin(), t.end());
+        string u = s + t;
+        if (check(u)) {
+            cout << i << endl;
+            ret();
+        }
+    }
+
+}
