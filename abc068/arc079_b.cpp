ce33879e0eebbb3e2989953d6f215530bf0a730b
riku <riku@tanide.net>
2020-07-10 23:31:59 +0900

    【WA】[AtCoder Beginner Contest 068]D - Decrease (Contestant ver.)

--- abc068/arc079_b.cpp
+++ abc068/arc079_b.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-const ll INF = 10e15;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -29,32 +35,46 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-const int column = 50;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
 
 int main() {
-    vector<ll> build(column, column - 1);
+    int MAX = 50;
+
     ll k;
     cin >> k;
 
-    ll l = k / column;
-    rep(i, column) build[i] += l;
-    ll m = k % column;
-    rep(i, m) build[i] += column - m + 1;
-    for (ll i = m; i < column; i++) {
-        build[i] -= m;
+
+    if (k == 0) {
+        printf("4\n3 3 3 3\n");
+        ret();
+    }
+    if (k == 1) {
+        printf("3\n1 0 3\n");
+        ret();
     }
 
-    cout << column << endl;
-    for (ll n : build) cout << n << ' ';
+    if (k <= MAX) {
+        MAX = 2;
+    }
+
+    cout << MAX << endl;
+    ll f = k % MAX;
+    ll g = k / MAX + (k - 1);
+
+    ll p = g + MAX - (f - 1);
+
+    rep(i, MAX) {
+        if (i < f) cout << p << ' ';
+        else cout << g - f << ' ';
+    }
+
     cout << endl;
 
-}
 
+}
