672be987a6e1f93b5179d149ca662852c8a6a6bf
riku <riku@tanide.net>
2020-07-11 15:22:21 +0900

    【WA】[AtCoder Beginner Contest 074]D - Restoring Road Network

--- abc074/arc083_b.cpp
+++ abc074/arc083_b.cpp
@@ -1,19 +1,18 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 const double PI = 3.14159265358979323846;
 using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-const ll INF = 10e15;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-std::istream &operator>>(std::istream &in, set<int> &o) {
+std::istream &operator>>(std::istream &in, set<ll> &o) {
     ll a;
     in >> a;
     o.insert(a);
@@ -33,9 +32,58 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
+
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
+
+class UnionFind {
+public:
+    // 親の番号を格納する。親だった場合-size
+    vector<int> parents;
+
+    UnionFind(int n) {
+        parents = vector<int>(n, -1);
+    }
+
+    // aがどのグループに属しているか
+    int root(int a) {
+        if (parents[a] < 0) {
+            return a;
+        }
+        return parents[a] = root(parents[a]);
+    }
+
+    int size(int a) {
+        return -parents[root(a)];
+    }
+
+    // aとbをくっつける
+    bool connect(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        if (ra == rb) {
+            return false;
+        }
+        // 大きいほうにA
+        if (size(ra) < size(rb)) {
+            swap(ra, rb);
+        }
+        parents[ra] += parents[rb];
+        parents[rb] = ra;
+        return true;
+    }
+
+    bool is_union(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        return ra == rb;
+    }
+};
+
+struct Road {
+    int from, to;
+    ll cost;
+};
 
 class WarchallFloyd {
 
@@ -62,13 +110,12 @@ public:
         }
     }
 
-    void set(int from, int to, ll c) {
-        distances[from][to] = c;
-        distances[to][from] = c;
-    }
-
     ll distance(int from, int to) {
         return distances[from][to];
+    }
+
+    void add(int from, int to, ll cost) {
+        distances[from][to] = cost;
     }
 
 
@@ -78,60 +125,67 @@ public:
         cin >> from >> to >> c;
         from--;
         to--;
-        o.set(from, to, c);
+
+        o.distances[from][to] = c;
+        o.distances[to][from] = c;
         return in;
     }
+
+
 };
 
-struct Road {
-    int from, to;
-    ll cost;
-};
 
 int main() {
     int n;
     cin >> n;
 
-    vector<vector<ll>> distances(n, vector<ll>(n));
-    rep(y, n) rep(x, n) cin >> distances[y][x];
+    vector<vector<ll>> costs(n, vector<ll>(n));
+    rep(y, n)rep(x, n) cin >> costs[y][x];
 
-    WarchallFloyd wf(n);
-    rep(i, n) rep(j, n) wf.set(i, j, distances[i][j]);
-    wf.warshall_floyd();
+    vector<Road> v;
+    rep(y, n) rep(x, n) {
+            if (y <= x) continue;
+            v.push_back({y, x, costs[y][x]});
+        }
 
-    if (distances != wf.distances) {
+    sort(v.begin(), v.end(), [](Road r1, Road r2) {
+        return r1.cost < r2.cost;
+    });
+
+    vector<vector<ll>> tmp(n, vector<ll>(n, INF));
+    rep(i, n) tmp[i][i] = 0;
+
+    ll ans = 0;
+    for (Road r : v) {
+        bool b = [&] {
+            rep(i, n) {
+                if (tmp[r.from][i] + tmp[i][r.to] <= r.cost) return false;
+            }
+            return true;
+        }();
+        if (!b) continue;
+        ans += r.cost;
+        tmp[r.from][r.to] = r.cost;
+        tmp[r.to][r.from] = r.cost;
+    }
+
+    for (int k = 0; k < n; k++) {       // 経由する頂点
+        for (int i = 0; i < n; i++) {    // 始点
+            for (int j = 0; j < n; j++) {  // 終点
+                if (tmp[i][j] == INF && (tmp[i][k] == INF || tmp[k][j] == INF)) {
+                    tmp[i][j] = INF;
+                } else {
+                    tmp[i][j] = min(tmp[i][j], tmp[i][k] + tmp[k][j]);
+                }
+            }
+        }
+    }
+
+    if (costs != tmp) {
         cout << -1 << endl;
         ret();
     }
 
-    auto is_shortest = [&](int from, int to, int via) {
-        ll direct = distances[from][to];
-        ll v = distances[from][via] + distances[via][to];
-        return direct < v;
-    };
-
-    auto is_all_shortest = [&](int from, int to) {
-        rep(k, n) {
-            if (k == from || k == to) continue;
-            // iからjに行く最短距離よりiからjを経由したほうが近ければfalse
-            bool b = is_shortest(from, to, k);
-            if (!b) return false;
-        }
-        return true;
-    };
-
-    vector<Road> roads;
-    rep(i, n)rep(j, n)if (i < j) roads.push_back({i, j, distances[i][j]});
-    sort(roads.begin(), roads.end(), [](Road &r, Road &s) { return r.cost < s.cost; });
-    ll ans = 0;
-    for (Road &r : roads) {
-        bool b = is_all_shortest(r.from, r.to);
-        if (!b) {
-            continue;
-        }
-        ans += r.cost;
-    }
-
     cout << ans << endl;
-}
 
+}
