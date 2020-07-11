19dc85c5217bb3db43849d7ef2dce83a0517ebb2
riku <riku@tanide.net>
2020-07-11 21:08:44 +0900

    【AC】[エイシング プログラミング コンテスト 2020]C - XYZ Triplets

---
+++ aising2020/aising2020_c.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+ll calc(ll n) {
+
+    auto check = [](ll x, ll y, ll z) -> ll {
+        return x * x + y * y + z * z + x * y + y * z + z * x;
+    };
+    ll ans = 0;
+    for (ll x = 1; check(x, 1, 1) <= n; x++) {
+        for (ll y = 1; check(x, y, 1) <= n; y++) {
+            for (ll z = 1; check(x, y, z) <= n; z++) {
+                if (check(x, y, z) == n) ans++;
+            }
+        }
+    }
+    return ans;
+}
+
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cout << calc(i) << endl;
+    }
+
+}
