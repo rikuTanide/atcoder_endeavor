78704fad2843086defe438b58de0acab7e6fc56b
riku <riku@tanide.net>
2020-07-12 18:10:19 +0900

    【AC】[SoundHound Inc. Programming Contest 2018 -Masters Tournament-]C - Ordinary Beauty

--- soundhound2018-summer-qual/soundhound2018_summer_qual_c.cpp
+++ soundhound2018-summer-qual/soundhound2018_summer_qual_c.cpp
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -30,33 +35,25 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-#include <iostream>
-#include <vector>
-
-using namespace std;
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
     double n, m, d;
     cin >> n >> m >> d;
 
-    if (d == 0) {
-        double ans = 1 / n * (m - 1);
-        printf("%.20f\n", ans);
-    } else {
-        double ans = 2.0 * (n - d) / n / n * (m - 1);
-        printf("%.20f\n", ans);
-    }
+    double ans = [&]() -> double {
 
+        if (d == 0) {
+            return 1.0 / n * (m - 1);
+        } else {
+            return 2.0 * (n - d) / n / n * (m - 1);
+        }
+    }();
 
+    cout << setprecision(20) << ans << endl;
 }
-
-
-
