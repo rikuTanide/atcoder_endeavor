794ce43a443845a85e5e531be6d2ee8b0cc4b45c
riku <riku@tanide.net>
2020-07-12 18:48:57 +0900

    【AC】[AtCoder Beginner Contest 113]D - Number of Amidakuji

--- abc113/abc113_d.cpp
+++ abc113/abc113_d.cpp
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -30,19 +35,12 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 const int mod = 1000000007;
 
 struct mint {
@@ -115,82 +113,81 @@ struct mint {
 
 };
 
-bool is_adjoin(int n, int i) {
-    for (int j = 0; j < n - 2; j++) {
-        bool a = (i >> j) & 1;
-        bool b = (i >> (j + 1)) & 1;
-        if (a && b) return true;
-    }
-    return false;
-}
+bool check(int w, int i) {
+    rep(j, w - 2) {
+        bool f = (i >> j) & 1;
+        bool g = (i >> (j + 1)) & 1;
 
-vector<int> get_enable_bars(int n) {
-    vector<int> ans;
-    for (int i = 0; i < (1 << (n - 1)); i++) {
-        if (is_adjoin(n, i)) {
-            continue;
+        if (f && g) return false;
+    }
+    return true;
+};
+
+vector<mint> get_h_dp(int w) {
+
+    vector<mint> ans(w - 1, 0);
+
+
+    rep(i, 1 << (w - 1)) {
+
+        bool ok = check(w, i);
+        if (!ok) continue;
+
+        rep(j, w - 1) {
+            if (!((i >> j) & 1)) continue;
+            ans[j] = ans[j] + 1;
         }
-        ans.push_back(i);
     }
+
     return ans;
 }
 
+vector<mint> get_h_dp_e(int w, vector<mint> hdp) {
+    mint all = 0;
 
-int get_to_left(int x, vector<int> &enable_bars) {
-    int ans = 0;
-    for (int i : enable_bars) {
-        bool b = (i >> (x - 1)) & 1;
-        if (b) ans++;
+    rep(i, 1 << (w - 1)) {
+        bool ok = check(w, i);
+        if (!ok) continue;
+        all = all + 1;
     }
-    return ans;
-}
 
-int get_to_down(int x, vector<int> &enable_bars) {
-    int ans = 0;
-    for (int i : enable_bars) {
-        bool b = (i >> (x - 1)) & 1;
-        bool c = (i >> x) & 1;
-        assert(!(b && c));
-        bool d = b || c;
 
-        if (!d) ans++;
+    vector<mint> ans(w, all);
+    rep(i, w) {
+        if (i < w - 1) {
+            ans[i] -= hdp[i];
+        }
+        if (0 < i) {
+            ans[i] -= hdp[i - 1];
+        }
     }
-    return ans;
-}
 
-int get_to_right(int x, vector<int> &enable_bars) {
-    int ans = 0;
-    for (int i : enable_bars) {
-        bool b = (i >> x) & 1;
-        if (b) ans++;
-    }
     return ans;
+
 }
 
 int main() {
-    ll h, w, k;
+    int h, w, k;
     cin >> h >> w >> k;
-    vector<int> enable_bars = get_enable_bars(w);
-    vector<vector<mint>> dp(h + 1, vector<mint>(w, 0));
+
+    vector<mint> hdp = get_h_dp(w);
+    vector<mint> hdp_e = get_h_dp_e(w, hdp);
+
+    vector<vector<mint>> dp(h + 1, vector<mint>(w));
     dp[0][0] = 1;
-    rep(y, h) {
-        rep(x, w) {
-            if (x != 0) {
-                ll to_left = get_to_left(x, enable_bars);
-                dp[y + 1][x - 1] += dp[y][x] * to_left;
+
+    rep(i, h) {
+        rep(j, w) {
+            if (j < w - 1) {
+                dp[i + 1][j + 1] += (dp[i][j] * hdp[j]);
             }
-            ll to_down = get_to_down(x, enable_bars);
-            dp[y + 1][x] += dp[y][x] * to_down;
-            if (x != w - 1) {
-                ll to_right = get_to_right(x, enable_bars);
-                dp[y + 1][x + 1] += dp[y][x] * to_right;
+            dp[i + 1][j] += (dp[i][j] * hdp_e[j]);
+            if (0 < j) {
+                dp[i + 1][j - 1] += (dp[i][j] * hdp[j - 1]);
             }
         }
     }
 
-    cout << dp.back()[k - 1] << endl;
+    cout << dp[h][k - 1] << endl;
 
 }
-
-
-
