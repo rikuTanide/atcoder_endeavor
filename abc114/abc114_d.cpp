20696401f27790c2dd7d6004cc19e24505623084
riku <riku@tanide.net>
2020-07-12 20:11:36 +0900

    【AC】[AtCoder Beginner Contest 114]D - 756

--- abc114/abc114_d.cpp
+++ abc114/abc114_d.cpp
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -30,23 +35,15 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-#include <iostream>
-#include <vector>
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-using namespace std;
-
-const int mod = 1000000007;
-
-map<ll, ll> factorize(ll n) {
-    map<ll, ll> res;
+map<ll, int> factorize(ll n) {
+    map<ll, int> res;
 
     for (ll i = 2; i * i <= n; i++) {
         if (n % i != 0) {
@@ -59,55 +56,82 @@ map<ll, ll> factorize(ll n) {
         }
     }
 
-
     if (n != 1) res[n] = 1;
     return res;
-
 }
 
-
 int main() {
-    ll n;
+    int n;
     cin >> n;
-    map<ll, ll> factors;
-    for (ll i = 1; i <= n; i++) {
-        map<ll, ll> f = factorize(i);
+
+    map<ll, int> fs;
+
+    for (int i = 2; i <= n; i++) {
+        auto f = factorize(i);
         for (auto e : f) {
-            factors[e.first] += e.second;
+            fs[e.first] += e.second;
         }
     }
 
+    vector<int> counts;
+    for (auto e : fs) counts.push_back(e.second);
 
-    ll ans = 0;
-    for (auto e : factors) {
-        for (auto f : factors) {
-            for (auto g : factors) {
-                if (e.first == f.first) continue;
-                if (f.first == g.first) continue;
-                if (g.first == e.first) continue;
-                if (f.first >= g.first) continue;
+    int m = counts.size();
 
-                if (e.second >= 2 && f.second >= 4 && g.second >= 4) ans++;
+    int ans = 0;
 
+    for (int i = 0; i < m; i++) {
+        for (int j = i + 1; j < m; j++) {
+            for (int k = j + 1; k < m; k++) {
+                if (counts[i] >= 2 && counts[j] >= 4 && counts[k] >= 4)ans++;
             }
         }
     }
 
-    for (auto e : factors) {
-        for (auto f : factors) {
-            if (e.first == f.first) continue;
-            if (e.second >= 2 && f.second >= 24) ans++;
+    for (int i = 0; i < m; i++) {
+        for (int j = i + 1; j < m; j++) {
+            for (int k = j + 1; k < m; k++) {
+                if (counts[i] >= 4 && counts[j] >= 2 && counts[k] >= 4)ans++;
+            }
         }
     }
 
-    for (auto e : factors) {
-        for (auto f : factors) {
-            if (e.first == f.first) continue;
-            if (e.second >= (5 - 1) && f.second >= (15 - 1)) ans++;
+    for (int i = 0; i < m; i++) {
+        for (int j = i + 1; j < m; j++) {
+            for (int k = j + 1; k < m; k++) {
+                if (counts[i] >= 4 && counts[j] >= 4 && counts[k] >= 2) ans++;
+            }
         }
     }
 
-    for (auto e : factors) if (e.second >= 74) ans++;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = i + 1; j < m; j++) {
+            if (counts[i] >= 14 && counts[j] >= 4)ans++;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = i + 1; j < m; j++) {
+            if (counts[i] >= 4 && counts[j] >= 14) ans++;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = i + 1; j < m; j++) {
+            if (counts[i] >= 2 && counts[j] >= 24) ans++;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = i + 1; j < m; j++) {
+            if (counts[i] >= 24 && counts[j] >= 2)ans++;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        if (counts[i] >= 74) ans++;
+    }
 
     cout << ans << endl;
 }
