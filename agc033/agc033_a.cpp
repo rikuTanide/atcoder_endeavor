9b2b6e6958fc60b45fb27b67ca4949cbcbe28209
riku <riku@tanide.net>
2020-07-13 22:21:43 +0900

    【AC】[AtCoder Grand Contest 033]A - Darker and Darker

--- agc033/agc033_a.cpp
+++ agc033/agc033_a.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-//using namespace boost::multiprecision;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
-const ll INF = 1e15;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -29,51 +35,78 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
-struct Reach {
-    int x, y, time;
-};
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
     int h, w;
     cin >> h >> w;
 
     vector<vector<char>> grid(h, vector<char>(w));
-    rep(y, h) rep(x, w) cin >> grid[y][x];
+    rep(y, h)rep(x, w) cin >> grid[y][x];
 
-    vector<vector<int>> distances(h, vector<int>(w, INT_MAX));
-    rep(y, h) rep(x, w) if (grid[y][x] == '#') distances[y][x] = 0;
+    vector<vector<int>> distances(h, vector<int>(w, INT_MAX / 10));
 
-    queue<Reach> q;
-    rep(y, h) rep(x, w) if (grid[y][x] == '#') q.push({x, y, 0});
-
-    auto set = [&](int x, int y, int time) {
-        if (x == -1)return;;
-        if (x == w)return;;
-        if (y == -1)return;;
-        if (y == h)return;;
-        if (distances[y][x] <= time) return;
-        distances[y][x] = time;
-        q.push({x, y, time});
+    rep(y, h) rep(x, w) if (grid[y][x] == '#')distances[y][x] = 0;
+    struct Point {
+        ll y, x, cost;
     };
 
+    queue<Point> q;
+    rep(y, h) rep(x, w) if (grid[y][x] == '#') q.push({y, x, 0});
+
+
+    auto to_id = [&](int y, int x) {
+        return y * w + x;
+    };
+
+    struct Direction {
+        int y, x;
+    };
+
+
+    vector<Direction> directions = {
+            {0,  1},
+            {1,  0},
+            {0,  -1},
+            {-1, 0},
+    };
+
+
+    auto reachable = [&](int y, int x, int distance) {
+        if (x == -1 || x == w || y == -1 || y == h) return false;
+        if (grid[y][x] == '#') {
+            return false;
+        }
+        if (distances[y][x] <= distance) {
+            return false;
+        }
+        return true;
+    };
+
+
     while (!q.empty()) {
-        auto f = q.front();
+        Point p = q.front();
         q.pop();
-        set(f.x - 1, f.y, f.time + 1);
-        set(f.x + 1, f.y, f.time + 1);
-        set(f.x, f.y - 1, f.time + 1);
-        set(f.x, f.y + 1, f.time + 1);
+        for (Direction d : directions) {
+            int nx = p.x + d.x;
+            int ny = p.y + d.y;
+            int nd = p.cost + 1;
+            if (reachable(ny, nx, nd)) {
+                distances[ny][nx] = nd;
+                q.push({ny, nx, nd});
+            }
+        }
     }
+
     int ans = 0;
     rep(y, h) rep(x, w) cmax(ans, distances[y][x]);
     cout << ans << endl;
 
 }
+
+
