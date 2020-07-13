df8bbb910ef13b42976317610a45c34d797dbd85
riku <riku@tanide.net>
2020-07-13 23:30:40 +0900

    【WA】[AtCoder Grand Contest 033]B - LRUD Game

--- agc033/agc033_b.cpp
+++ agc033/agc033_b.cpp
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
-#include <cmath>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -33,25 +38,9 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-
 //const ll mod = 1e10;
+
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-ll gcd(ll x, ll y) {
-    if (x > y) swap(x, y);
-    ll m = 1;
-    while (m != 0) {
-        m = y % x;
-        y = x;
-        x = m;
-    }
-    return y;
-}
 
 int main() {
     int h, w, n, sy, sx;
@@ -63,45 +52,44 @@ int main() {
     string s, t;
     cin >> s >> t;
 
-    reverse(s.begin(), s.end());
-    reverse(t.begin(), t.end());
+    int l = 0, r = w - 1, u = h - 1, d = 0;
 
-    int l = 0, r = w - 1, u = 0, d = h - 1;
 
-    rep(i, n) {
-        if (i == 0) {
-            char c = s[0];
-            if (c == 'L') l++;
-            else if (c == 'R') r--;
-            else if (c == 'U') u++;
-            else if (c == 'D') d--;
-        } else {
-            char tc = t[i], sc = s[i];
-            if (tc == 'L') {
-                r = min(w - 1, r + 1);
-            } else if (tc == 'R') {
-                l = max(0, l - 1);
-            } else if (tc == 'U') {
-                d = min(h - 1, d + 1);
-            } else if (tc == 'D') {
-                u = max(0, u - 1);
-            }
-            if (sc == 'L') {
-                l++;
-            } else if (sc == 'R') {
-                r--;
-            } else if (sc == 'U') {
-                u++;
-            } else if (sc == 'D') {
-                d--;
-            }
-            if (l > r || u > d) {
-                cout << "NO" << endl;
-                ret();
+    for (int i = n - 1; i >= 0; i--) {
+        {
+            char c = t[i];
+            if (c == 'U') {
+                d = max(d - 1, 0);
+            } else if (c == 'D') {
+                u = min(u + 1, h - 1);
+            } else if (c == 'R') {
+                l = min(l - 1, 0);
+            } else {
+                r = max(r + 1, w - 1);
             }
         }
+
+        {
+            char c = s[i];
+            if (c == 'U') {
+                u--;
+            } else if (c == 'D') {
+                d++;
+            } else if (c == 'R') {
+                r--;
+            } else {
+                l++;
+            }
+        }
+
+        if (r < l || u < d) {
+            cout << "NO" << endl;
+            ret();
+        }
+
     }
-    if (l <= sx && sx <= r && u <= sy && sy <= d) {
+
+    if (l <= sx && sx <= r && d <= sy && sy <= u) {
         cout << "YES" << endl;
     } else {
         cout << "NO" << endl;
