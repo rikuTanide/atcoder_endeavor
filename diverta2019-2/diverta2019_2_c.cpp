20f4da47bbae87497f6afd9027d5eec53362b846
riku <riku@tanide.net>
2020-07-16 10:03:10 +0900

    【AC】[diverta 2019 Programming Contest 2]C - Successive Subtraction

--- diverta2019-2/diverta2019_2_c.cpp
+++ diverta2019-2/diverta2019_2_c.cpp
@@ -1,20 +1,18 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 const double PI = 3.14159265358979323846;
 using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-std::istream &operator>>(std::istream &in, set<int> &o) {
+std::istream &operator>>(std::istream &in, set<ll> &o) {
     ll a;
     in >> a;
     o.insert(a);
@@ -30,60 +28,65 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
 
 //const ll mod = 1e10;
-typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
 
 int main() {
     int n;
     cin >> n;
-    vector<ll> numbers(n);
-    rep(i, n) cin >> numbers[i];
+    vector<ll> v(n);
+    rep(i, n) cin >> v[i];
 
-    sort(numbers.begin(), numbers.end());
+    vector<ll> plus, minus;
+    for (ll l : v)
+        if (l < 0) minus.push_back(l);
+        else plus.push_back(l);
 
-    vector<P> build;
+    sort(plus.rbegin(), plus.rend());
+    sort(minus.begin(), minus.end());
 
-    queue<ll> plus, minus;
-    rep(i, n) {
-        if (i == 0) {
-            minus.push(numbers[i]);
-        } else if (i == n - 1) {
-            plus.push(numbers[i]);
-        } else {
-            if (numbers[i] < 0) minus.push(numbers[i]);
-            else plus.push(numbers[i]);
-        }
-    }
+    if (plus.empty()) plus.push_back(minus.back()), minus.pop_back();
+    if (minus.empty()) minus.push_back(plus.back()), plus.pop_back();
+
+    vector<P> ans;
+
 
     while (plus.size() > 1) {
-        ll pf = plus.front(), mf = minus.front();
-        plus.pop();
-        minus.pop();
-        minus.push(mf - pf);
-        build.emplace_back(mf, pf);
-    }
-    ll ans;
-    while (!minus.empty()) {
-        ll pf = plus.front(), mf = minus.front();
-        plus.pop();
-        minus.pop();
-        ans = pf - mf;
-        plus.push(ans);
-        build.emplace_back(pf, mf);
-    }
-    cout << ans << endl;
+        ll m = minus.back();
+        minus.pop_back();
+        ll p = plus.back();
+        plus.pop_back();
 
-    for (P p : build) {
-        printf("%lld %lld\n", p.first, p.second);
+        ll next = m - p;
+        assert(next <= 0);
+        minus.push_back(next);
+
+        ans.push_back({m, p});
     }
+
+    while (minus.size() > 1) {
+        ll m = minus.back();
+        minus.pop_back();
+        ll p = plus.back();
+        plus.pop_back();
+
+        ll next = p - m;
+        assert(next >= 0);
+        plus.push_back(next);
+
+        ans.push_back({p, m});
+    }
+
+    ans.push_back({plus.back(), minus.back()});
+
+    cout << ans.back().first - ans.back().second << endl;
+
+    for (P p : ans) printf("%lld %lld\n", p.first, p.second);
+
 }
