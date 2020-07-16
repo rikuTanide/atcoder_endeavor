f8294dafa70e7b942602252d2852a11e7d927cce
riku <riku@tanide.net>
2020-07-16 17:54:57 +0900

    【AC】[AtCoder Beginner Contest 138]E - Strings of Impurity

--- abc138/abc138_e.cpp
+++ abc138/abc138_e.cpp
@@ -1,20 +1,18 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 const double PI = 3.14159265358979323846;
 using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-std::istream &operator>>(std::istream &in, set<int> &o) {
+std::istream &operator>>(std::istream &in, set<ll> &o) {
     ll a;
     in >> a;
     o.insert(a);
@@ -30,48 +28,41 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
 
 //const ll mod = 1e10;
-typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
-#include <iostream>
-#include <vector>
-
-using namespace std;
 
 int main() {
     string s, t;
     cin >> s >> t;
+    string ss = s + s;
 
-    s = s + s;
-
-    vector<vector<ll >> point(26);
-    rep(i, s.size()) {
-        char c = s[i];
-        ll j = c - 'a';
-        point[j].push_back(i);
+    vector<vector<int>> table(26);
+    rep(i, ss.size()) {
+        char c = ss[i];
+        int j = c - 'a';
+        table[j].push_back(i);
     }
 
-    ll position = -1;
-    rep(i, t.size()) {
-        char c = t[i];
-        ll j = c - 'a';
-        ll ps = position == -1 ? -1 : position % (s.size() / 2);
-        auto it = upper_bound(point[j].begin(), point[j].end(), ps);
-        if (it == point[j].end()) {
+    for (char c : t) {
+        if (table[c - 'a'].empty()) {
             cout << -1 << endl;
             ret();
         }
-        ll k = *it;
-        assert(ps < k);
-        ll m = k - ps;
-        position += m;
     }
-    cout << position + 1 << endl;
+
+    ll prev = 0;
+    for (char c : t) {
+        int ci = c - 'a';
+        int next = *lower_bound(table[ci].begin(), table[ci].end(), prev % s.size());
+        ll add = next - (prev % s.size()) + 1;
+        prev += add;
+    }
+    cout << prev << endl;
+
 }
-
-
-
