7710f6ac0a02889f6a2fdcb5fcbb1c850a1799d3
riku <riku@tanide.net>
2020-07-16 17:11:03 +0900

    【AC】[AtCoder Beginner Contest 137]D - Summer Vacation

--- abc137/abc137_d.cpp
+++ abc137/abc137_d.cpp
@@ -1,20 +1,18 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 const double PI = 3.14159265358979323846;
 using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-std::istream &operator>>(std::istream &in, set<int> &o) {
+std::istream &operator>>(std::istream &in, set<ll> &o) {
     ll a;
     in >> a;
     o.insert(a);
@@ -30,45 +28,36 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
 
 //const ll mod = 1e10;
-typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
-#include <iostream>
-#include <vector>
-
-using namespace std;
 
 int main() {
-
     int n, m;
     cin >> n >> m;
-    map<int, vector<int>> v;
 
-    rep(i, n) {
-        int a, b;
+    priority_queue<ll> q;
+    map<ll, vector<int>> works;
+
+    rep(_, n) {
+        ll a, b;
         cin >> a >> b;
-        v[a].push_back(b);
+        a--;
+        works[m - 1 - a].push_back(b);
     }
-    ll sum = 0;
-    priority_queue<int> q;
-    rep(i, m + 1) {
-        if (v.find(i) != v.end()) {
-            for (int b : v[i]) {
-                q.push(b);
-            }
-        }
-        if (!q.empty()) {
-            int t = q.top();
-            sum += t;
-            q.pop();
-        }
+
+    ll ans = 0;
+    for (int i = m - 1; i >= 0; i--) {
+        for (ll l : works[i]) q.push(l);
+        if (q.empty()) continue;
+        ans += q.top();
+        q.pop();
     }
-    cout << sum << endl;
+    cout << ans << endl;
+
 }
-
-
-
