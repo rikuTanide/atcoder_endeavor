9ecaf5c4452a97136f7bfa9829922d1359ea87f8
riku <riku@tanide.net>
2020-07-18 15:13:38 +0900

    【WA】[AtCoder Beginner Contest 144]E - Gluttony

--- abc144/abc144_e.cpp
+++ abc144/abc144_e.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 const double PI = 3.14159265358979323846;
 using namespace std;
@@ -7,14 +6,13 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-std::istream &operator>>(std::istream &in, set<int> &o) {
+std::istream &operator>>(std::istream &in, set<ll> &o) {
     ll a;
     in >> a;
     o.insert(a);
@@ -34,54 +32,50 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
+
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-ll check(int n, ll mid, ll k, vector<ll> &skills, vector<ll> &foods) {
-    ll sum = 0;
-    rep(i, n) {
-        ll ns = mid / foods[i];
-        if (ns < skills[i]) {
-            sum += (skills[i] - ns);
-        }
-    }
-    return k >= sum;
-}
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
 int main() {
-    int n;
-    ll k;
+    int n, k;
     cin >> n >> k;
 
-    vector<ll> skills(n), foods(n);
-    rep(i, n) cin >> skills[i];
+    vector<ll> costs(n), foods(n);
+    rep(i, n) cin >> costs[i];
     rep(i, n) cin >> foods[i];
 
-    sort(skills.begin(), skills.end());
+    sort(costs.begin(), costs.end());
     sort(foods.rbegin(), foods.rend());
 
-    bool b = check(n, 0, k, skills, foods);
-    if (b) {
+    auto check = [&](ll x) -> bool {
+        ll ans = 0;
+        rep(i, n) {
+            ll c = costs[i];
+            ll f = foods[i];
+            if (c * f <= x) continue;
+            ll border = x / f;
+            assert(border < c);
+            ll t = c - border;
+            ans += t;
+        }
+        return ans <= k;
+    };
+
+
+    ll floor = 0, ceil = INF;
+
+    if (check(floor)) {
         cout << 0 << endl;
         ret();
     }
 
-    ll floor = 0, ceil = INF;
-
     while (floor + 1 < ceil) {
         ll mid = (floor + ceil) / 2;
-        bool b = check(n, mid, k, skills, foods);
-        if (b) {
-            ceil = mid;
-        } else {
-            floor = mid;
-        }
+        bool ok = check(mid);
+        if (ok) ceil = mid;
+        else floor = mid;
     }
+
     cout << ceil << endl;
+
 }
