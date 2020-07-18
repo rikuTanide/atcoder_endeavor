d78ca3c4a0cba279edb03f0eb15283b86faf30af
riku <riku@tanide.net>
2020-07-18 15:57:39 +0900

    【AC】[AtCoder Beginner Contest 146]F - Sugoroku

--- abc146/abc146_f.cpp
+++ abc146/abc146_f.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 const double PI = 3.14159265358979323846;
 using namespace std;
@@ -7,14 +6,13 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-std::istream &operator>>(std::istream &in, set<int> &o) {
+std::istream &operator>>(std::istream &in, set<ll> &o) {
     ll a;
     in >> a;
     o.insert(a);
@@ -34,87 +32,37 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
+
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
-#include <iostream>
-#include <vector>
 
-using namespace std;
-
-struct Food {
-    ll time, good;
-};
-
-struct SegTreeMin {
-    ll N, dat[4 * 100000];
-
-    SegTreeMin() {}
-
-    SegTreeMin(ll n) {
-        N = 1;
-        while (N < n) N *= 2;
-        for (ll i = 0; i < 2 * N - 1; i++)
-            dat[i] = INF;
-    }
-
-    // update k th element
-    void update(ll k, ll a) {
-        k += N - 1; // leaf
-        dat[k] = a;
-        while (k > 0) {
-            k = (k - 1) / 2;
-            dat[k] = min(dat[k * 2 + 1], dat[k * 2 + 2]);
-        }
-    }
-
-    // min [a, b)
-    ll query(ll a, ll b) { return query(a, b, 0, 0, N); }
-
-    ll query(ll a, ll b, ll k, ll l, ll r) {
-        if (r <= a or b <= l) return INF;
-        if (a <= l and r <= b) return dat[k];
-        ll m = (l + r) / 2;
-        return min(query(a, b, k * 2 + 1, l, m), query(a, b, k * 2 + 2, m, r));
-    }
-};
 
 int main() {
     int n, m;
     string s;
-
     cin >> n >> m >> s;
-    SegTreeMin st(n + 1);
-    rep(i, n + 2) st.update(i, INF);
-    st.update(n, 0);
+    vector<int> dp(n + 1, 0);
 
-    for (int i = n - 1; i >= 0; i--) {
-        if (s[i] == '1') {
-            continue;
-        }
-        ll start = i + 1;
-        ll end = min(i + m + 1, n + 1);
-        ll mi = st.query(start, end);
-        if (mi == INF) {
+    for (int i = n; i >= 0; i--) {
+        if (s[i] == '0') dp[i] = i;
+        else dp[i] = dp[i + 1];
+    }
+    int now = n;
+
+    vector<int> ans;
+    while (now > 0) {
+        int next = dp[max(now - m, 0)];
+        if (next == now) {
             cout << -1 << endl;
             ret();
         }
-        st.update(i, mi + 1);
+        ans.push_back(now - next);
+        now = next;
     }
-    ll ma = st.query(0, 1);
-    vector<ll> shortness(n + 1);
-    rep(i, n + 1) shortness[i] = st.query(i, i + 1);
 
-    vector<ll> points(ma + 1);
-    for (ll i = n; i >= 0; i--) {
-        ll p = shortness[i];
-        if (p == INF) continue;
-        points[p] = i;
-    }
-    reverse(points.begin(), points.end());
-    rep(i, ma) {
-        cout << points[i + 1] - points[i] << ' ';
-    }
-    cout << endl;
+    reverse(ans.begin(), ans.end());
+
+    for (int a : ans) cout << a << ' ';
+
 }
