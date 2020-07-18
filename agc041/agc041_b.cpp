bd2ec7fbcc0feb0d70370ce687012615be377cb4
riku <riku@tanide.net>
2020-07-18 18:06:05 +0900

    【AC】[AtCoder Grand Contest 041]B - Voting Judges

--- agc041/agc041_b.cpp
+++ agc041/agc041_b.cpp
@@ -1,20 +1,18 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 const double PI = 3.14159265358979323846;
 using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
-//#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-std::istream &operator>>(std::istream &in, set<int> &o) {
+std::istream &operator>>(std::istream &in, set<ll> &o) {
     ll a;
     in >> a;
     o.insert(a);
@@ -30,79 +28,74 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
 
 //const ll mod = 1e10;
-typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-bool can_choose(ll n, ll m, ll v, ll p, vector<ll> &problems, ll border) {
-    // borderを当選させたい。可能か？
-    if (border < p) return true;
-    ll target = problems[p - 1];
-    if (problems[border] + m < target) return false;
-
-    ll budget = v * m;
-    // border問目に全員が投票
-    // P-1問目までは全員が投票
-    // p問目からborder問目まではborder問目と一致するように投票
-    // border+1問目移行は全部上げる
-    // 票が余ったらfalse
-
-    auto allocate = [&](ll ma) {
-        ll k = min({budget, m, ma});
-        budget -= k;
-        assert(budget >= 0);
-        return k;
-    };
-
-    ll border_point = problems[border] + allocate(INF);
-    rep(i, p - 1) allocate(INF);
-
-    for (ll i = border + 1; i < n; i++) allocate(INF);
-
-    auto allocate_for_border_point = [&](ll i) {
-        ll from = problems[i];
-        assert(border_point >= from);
-        ll diff = border_point - from;
-        allocate(diff);
-    };
-
-    for (ll i = p - 1; i < border; i++) allocate_for_border_point(i);
-
-    return budget == 0;
-}
 
 
 int main() {
-    ll n, m, v, p;
+    int n, v, p;
+    ll m;
     cin >> n >> m >> v >> p;
-
 
     vector<ll> problems(n);
     rep(i, n) cin >> problems[i];
+
     sort(problems.rbegin(), problems.rend());
-    ll floor = 0, ceil = n - 1;
-    if (can_choose(n, m, v, p, problems, ceil)) {
+
+    auto ct = [&](ll x) -> int {
+        return count_if(problems.begin(), problems.end(), [&](ll k) { return k >= x; });
+    };
+
+    auto check = [&](ll x) -> bool {
+
+        ll border = problems[p - 1];
+
+        if (x >= border) return true;
+        if (x + m < border) return false;
+
+        int vi = v;
+        rep(i, n) {
+            if (i < p - 1) vi--;
+            else if (problems[i] <= x) vi--;
+        }
+
+        if (vi <= 0) return true;
+
+        ll avc = vi * m;
+
+        rep(i, n) {
+            if (i < p - 1) continue;
+            if (problems[i] <= x) continue;
+            avc -= ((x + m) - problems[i]);
+        }
+
+        return avc <= 0;
+
+    };
+
+    ll floor = 0, ceil = INF;
+
+    check(5);
+
+    if (check(floor)) {
         cout << n << endl;
         ret();
     }
+
     while (floor + 1 < ceil) {
         ll mid = (floor + ceil) / 2;
-        bool b = can_choose(n, m, v, p, problems, mid);
-        if (b) {
-            floor = mid;
-        } else {
-            ceil = mid;
-        }
+        ll ok = check(mid);
+        if (ok) ceil = mid;
+        else floor = mid;
     }
-    assert(can_choose(n, m, v, p, problems, floor));
-    assert(!can_choose(n, m, v, p, problems, ceil));
-    cout << floor + 1 << endl;
+
+
+    cout << ct(ceil) << endl;
+
 }
