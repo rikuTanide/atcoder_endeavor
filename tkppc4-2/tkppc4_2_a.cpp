13a1a5b34d4a081975194df585801e7eed04391d
riku <riku@tanide.net>
2020-07-23 17:02:18 +0900

    【WA】[技術室奥プログラミングコンテスト#4 Day2]A - Jumping!!

---
+++ tkppc4-2/tkppc4_2_a.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    ll x, y;
+    cin >> x >> y;
+
+    if (y == 0 && x == 0) {
+        cout << 0 << endl;
+        ret();
+    }
+
+    if (y < 0) {
+        cout << -1 << endl;
+        ret();
+    }
+
+    if (y % 2 != 0) {
+        cout << -1 << endl;
+        ret();
+    }
+
+    ll k = y / 2;
+    if (abs(x) > k) {
+        cout << -1 << endl;
+        ret();
+    }
+
+    if ((abs(x) - k) % 2 == 1) {
+        cout << -1 << endl;
+        ret();
+    }
+
+    cout << k << endl;
+
+}
+
+
