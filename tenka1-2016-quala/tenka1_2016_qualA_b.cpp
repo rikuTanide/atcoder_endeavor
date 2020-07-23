906f6c7eb95b37e9c8cd68010c95c501b5c3b929
riku <riku@tanide.net>
2020-07-23 23:42:43 +0900

    【AC】[天下一プログラマーコンテスト2016予選A]B - PackDrop

---
+++ tenka1-2016-quala/tenka1_2016_qualA_b.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+struct Triangle {
+    int parent;
+    vector<int> children;
+};
+
+class Tree {
+    vector<vector<int>> edges;
+public:
+    Tree(int n) : edges(n) {}
+
+    void edge(int i, int j) {
+        edges[i].push_back(j);
+        edges[j].push_back(i);
+    }
+
+    vector<Triangle> root(int r) {
+        vector<Triangle> leafs;
+
+        queue<int> vs;
+        vs.push(r);
+        vector<bool> check(edges.size(), false);
+        check[r] = true;
+        while (!vs.empty()) {
+            int k = vs.front();
+            Triangle triangle;
+            triangle.parent = k;
+
+            for (int i : edges[k]) {
+                if (check[i]) continue;
+                check[i] = true;
+                triangle.children.push_back(i);
+                vs.push(i);
+            }
+            vs.pop();
+            leafs.push_back(triangle);
+        }
+        reverse(leafs.begin(), leafs.end());
+        return leafs;
+    }
+};
+
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    Tree tree(n);
+    rep(i, n - 1) {
+        int p;
+        cin >> p;
+        tree.edge(i + 1, p);
+
+    }
+
+    vector<Triangle> leafs = tree.root(0);
+
+    vector<int> dp(n, 0);
+    rep(i, m) {
+        int b, c;
+        cin >> b >> c;
+        dp[b] = c;
+    }
+
+    for (Triangle leaf: leafs) {
+        if (leaf.children.empty()) continue;
+        if (leaf.parent == 0) continue;
+        int mi = INT_MAX;
+        for (int c : leaf.children) {
+            mi = min(mi, dp[c]);
+        }
+
+        dp[leaf.parent] += mi;
+        for (int c : leaf.children) {
+            dp[c] -= mi;
+        }
+    }
+
+    int ans = accumulate(dp.begin(), dp.end(), 0);
+    cout << ans << endl;
+
+}
