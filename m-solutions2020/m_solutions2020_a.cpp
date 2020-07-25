b62c8c74b95564addf2cda327bc86d6188d2c108
riku <riku@tanide.net>
2020-07-25 22:34:00 +0900

    【TLE】[M-SOLUTIONS プロコンオープン 2020]A - Kyu in AtCoder

--- m-solutions2020/m_solutions2020_a.cpp
+++ m-solutions2020/m_solutions2020_a.cpp
@@ -1,3 +1,7 @@
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
 #include <bits/stdc++.h>
 //#include <boost/multiprecision/cpp_int.hpp>
 //namespace mp = boost::multiprecision;
@@ -42,9 +46,72 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
+struct Village {
+    ll x, y, p;
+};
+
+std::istream &operator>>(std::istream &in, Village &o) {
+    in >> o.x >> o.y >> o.p;
+    return in;
+}
+
+ll check(int n, vector<Village> &villages, vector<char> &ptn) {
+    vector<ll> v = {0}, h = {0};
+//    rep(i, n) {
+//        Village &village = villages[i];
+//        if (ptn[i] == 'e') continue;
+//        else if (ptn[i] == 'v') v.push_back(village.x);
+//        else if (ptn[i] == 'h') h.push_back(village.y);
+//    }
+    ll ans = 0;
+    rep(i, n) {
+//    for (Village &village : villages) {
+        if (ptn[i] != 'e') continue;
+        Village &village = villages[i];
+        ll now = INF;
+//        for (ll x : v) {
+//            cmin(now, abs(x - village.x));
+//        }
+//        for (ll y : h) {
+//            cmin(now, abs(y - village.y));
+//        }
+        ans += (now * village.p);
+    }
+    return ans;
+
+}
+
+void rec(int n, int i, vector<Village> &villages, vector<char> &ptn, vector<ll> &anss) {
+    if (i == n) {
+        ll ans = check(n, villages, ptn);
+        int t = n - count(ptn.begin(), ptn.end(), 'e');
+        cmin(anss[t], ans);
+        return;
+    }
+
+    ptn[i] = 'e';
+    rec(n, i + 1, villages, ptn, anss);
+    ptn[i] = 'h';
+    rec(n, i + 1, villages, ptn, anss);
+    ptn[i] = 'v';
+    rec(n, i + 1, villages, ptn, anss);
+}
+
 int main() {
-    int x;
-    cin >> x;
-    int f = (x - 400) / 200;
-    cout << 8 - f << endl;
+
+    cout << int(pow(3, 15)) << endl;
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<Village> villages(n);
+    rep(i, n) cin >> villages[i];
+    vector<char> ptn(n, ' ');
+    vector<ll> ans(n + 1, INF);
+    rec(n, 0, villages, ptn, ans);
+
+    for (ll l : ans) cout << l << '\n';
+
+
 }
