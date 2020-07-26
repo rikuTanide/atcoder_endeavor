b746266fecfaac31ef1e293cebbb889ef84070b3
riku <riku@tanide.net>
2020-07-26 14:20:39 +0900

    【WA】[CODE FESTIVAL 2018 Final]C - Telephone Charge

---
+++ code-festival-2018-final/code_festival_2018_final_c.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int n;
+    cin >> n;
+    map<ll, ll> mp;
+    rep(_, n) {
+        ll a, b;
+        cin >> a >> b;
+        mp[a] = b;
+    }
+
+    int m;
+    cin >> m;
+    rep(_, m) {
+        ll t;
+        cin >> t;
+        auto it = mp.upper_bound(t);
+        if (it == mp.begin()) {
+            cout << (*it).second << endl;
+            ret();
+        }
+        it--;
+        auto e = *it;
+        ll s = e.second + (t - e.first);
+
+        it++;
+        if (it != mp.end()) {
+            ll k = (*it).second;
+            cmin(s, k);
+        }
+        cout << s << endl;
+    }
+}
