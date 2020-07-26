c1266252c95ef90f1469066ce6960b342a576cca
riku <riku@tanide.net>
2020-07-26 17:42:58 +0900

    【AC】[いろはちゃんコンテスト Day1]E - 放課後

---
+++ iroha2019-day1/iroha2019_day1_e.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    ll n, a;
+    int b;
+    cin >> n >> a >> b;
+
+    if (b == 0) {
+        cout << n - n / a << endl;
+        ret();
+    }
+
+    vector<ll> ds(b);
+    rep(i, b) cin >> ds[i];
+
+    sort(ds.begin(), ds.end());
+
+    ll add = 0;
+    rep(i, b - 1) {
+        ll dis = ds[i + 1] - ds[i] - 1;
+        ll p = dis / a;
+        add += p;
+    }
+
+    add += ((ds.front() - 1) / a);
+    add += ((n - ds.back()) / a);
+
+    ll ans = n - add - b;
+    cout << ans << endl;
+
+
+}
