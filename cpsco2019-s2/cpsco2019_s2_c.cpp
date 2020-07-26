c0867854081b028f31c4cce5afa9afd753a1f41d
riku <riku@tanide.net>
2020-07-26 22:13:38 +0900

    【AC】[CPSCO2019 Session2]C - Delicious Burgers

---
+++ cpsco2019-s2/cpsco2019_s2_c.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int n, k;
+    string s;
+    cin >> n >> k >> s;
+
+    vector<int> stk(n);
+    stk[0] = 1;
+    for (int i = 1; i < n; i++) {
+        if (s[i] == '(')stk[i] = stk[i - 1] + 1;
+        else stk[i] = stk[i - 1] - 1;
+    }
+
+    sort(stk.rbegin(), stk.rend());
+    ll ans = accumulate(stk.begin(), stk.begin() + k, 0ll);
+
+    cout << ans << endl;
+
+}
