d6d44e83eec758a2e6748e9df9a6237dd0b2bc98
riku <riku@tanide.net>
2020-08-02 22:16:40 +0900

    【AC】[AtCoder Beginner Contest 174]E - Logs

---
+++ abc174/abc174_e.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<int, vector<int>, greater<int> > PQ_ASK;
+
+int main() {
+    int n;
+    ll k;
+    cin >> n >> k;
+    vector<ll> logs(n);
+    rep(i, n) cin >> logs[i];
+
+    auto check = [&](ll mid) -> bool {
+        ll t = 0;
+
+        for (ll l : logs) {
+            ll now = (l + mid - 1) / mid;
+            now--;
+            t += now;
+        }
+
+        return t <= k;
+    };
+
+    ll floor = 1, ceil = INF;
+    if (check(1)) {
+        cout << 1 << endl;
+        ret();
+    }
+
+    while (floor + 1 < ceil) {
+        ll mid = (floor + ceil) / 2;
+        bool b = check(mid);
+        if (b) ceil = mid;
+        else floor = mid;
+    }
+
+    // floorより大きくできるか
+    auto can_maxi = [&](ll floor) -> bool {
+        assert(check(ceil));
+        assert(!check(floor));
+        int u = 0;
+        for (ll l : logs) {
+            if (l % ceil == 0) u++;
+        }
+
+        ll t = 0;
+        for (ll l : logs) {
+            ll now = (l + ceil - 1) / ceil;
+            now--;
+            t += now;
+        }
+
+        assert(t <= k);
+
+        bool ans = t + u <= k;
+        return ans;
+    };
+
+    cout << ceil << endl;
+
+//    if (can_maxi(floor)) {
+//        cout << ceil << endl;
+//    } else {
+//        cout << floor << endl;
+//
+//    }
+
+
+}
