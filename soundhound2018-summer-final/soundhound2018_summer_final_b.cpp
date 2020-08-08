d550f5ea61457bb9035be219fd95aaedd30d0b2f
riku <riku@tanide.net>
2020-08-08 16:34:07 +0900

    【AC】[ SoundHound Programming Contest 2018 Masters Tournament 本戦]B - Neutralize

--- soundhound2018-summer-final/soundhound2018_summer_final_b.cpp
+++ soundhound2018-summer-final/soundhound2018_summer_final_b.cpp
@@ -42,6 +42,53 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
+template<class T, class F>
+struct SegmentTree {
+    F f;
+    T ti;
+    vector<T> dat;
+    int sz;
+
+    SegmentTree(const F &f, const T &ti) : f(f), ti(ti) {}
+
+    void build(const vector<T> &v) {
+        assert(v.size());
+        sz = 1;
+        while (sz < v.size())sz <<= 1;
+        dat.resize(sz << 1, ti);
+        for (int i = 0; i < v.size(); i++)dat[sz - 1 + i] = v[i];
+        for (int i = sz - 2; i >= 0; i--)dat[i] = f(dat[i * 2 + 1], dat[i * 2 + 2]);
+    }
+
+    inline void update(int k, T x) {
+        k += sz - 1;
+        dat[k] = x;
+        while (k) {
+            k = (k - 1) / 2;
+            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
+        }
+    }
+
+    inline void add(int k, int x) {
+        k += sz - 1;
+        dat[k] = f(dat[k], x);
+        while (k) {
+            k = (k - 1) / 2;
+            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
+        }
+    }
+
+    inline T query(int a, int b) {
+        return query(a, b, 0, 0, sz);
+    }
+
+    T query(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l)return ti;
+        if (a <= l && r <= b)return dat[k];
+        return f(query(a, b, k * 2 + 1, l, (l + r) / 2), query(a, b, k * 2 + 2, (l + r) / 2, r));
+    }
+};
+
 int main() {
     int n, k;
     cin >> n >> k;
@@ -56,11 +103,25 @@ int main() {
         return dp[i];
     };
 
+
+    auto f = [](ll i, ll j) { return max(i, j); };
+    SegmentTree<ll, decltype(f)> segmentTree(f, -INF);
+    vector<ll> imos(n + 1, -INF);
+    imos[0] = 0;
+    segmentTree.build(imos);
+
     rep(i, n) {
-        for (int j = 0; j <= i - (k - 1); j++) {
-            cmax(dp[i], get(j - 1));
+
+        if (i >= (k - 1)) {
+            ll p = segmentTree.query(0, i - k + 2);
+            cmax(dp[i], p);
         }
+
+//        for (int j = 0; j <= i - (k - 1); j++) {
+//            cmax(dp[i], get(j - 1));
+//        }
         cmax(dp[i], get(i - 1) + v[i]);
+        segmentTree.update(i + 1, dp[i]);
     }
 
     cout << dp.back() << endl;
