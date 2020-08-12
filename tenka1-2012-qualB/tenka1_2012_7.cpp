a12ec67803bf6ef76e2a528d010a7dabcc85673a
riku <riku@tanide.net>
2020-08-13 00:08:20 +0900

    【TLE】[天下一プログラマーコンテスト2012 予選B]C - 席が足りない

--- tenka1-2012-qualB/tenka1_2012_7.cpp
+++ tenka1-2012-qualB/tenka1_2012_7.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,7 +9,6 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -19,7 +20,7 @@ double equal(double a, double b) {
 }
 
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -41,74 +42,74 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-int get_minute() {
-    char a, b, c, d, e;
-    cin >> a >> b >> c >> d >> e;
-    assert(c == ':');
-    int hour = (a - '0') * 10 + (b - '0');
-    int minute = (d - '0') * 10 + (e - '0');
-
-    int minutes = hour * 60 + minute;
-    return minutes;
+int get_time() {
+    int a, b;
+    scanf("%d:%d", &a, &b);
+    return a * 100 + b;
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> starts(n), ends(n);
 
-    rep(i, n) {
-        starts[i] = get_minute();
-        ends[i] = get_minute();
+    vector<int> starts(n), ends(n);
+    rep(i, n) starts[i] = get_time(), ends[i] = get_time();
+
+    vector<int> dp(1 << n, n);
+    for (int i = 1; i < (1 << n); i = (i << 1)) {
+        dp[i] = 1;
     }
 
-    auto can_one = [&](int i) {
-        vector<int> s, e;
-        rep(j, n) {
-            if (!((i >> j) & 1))continue;
-            s.push_back(starts[j]);
-            e.push_back(ends[j]);
-        }
 
-        for (int j = 0; j < s.size(); j++) {
-            for (int k = j + 1; k < s.size(); k++) {
-                int s1 = s[j], e1 = e[j], s2 = s[k], e2 = e[k];
-                if (s1 > s2) {
-                    swap(s1, s2);
-                    swap(e1, e2);
-                }
-                assert(s1 <= s2);
+    auto can_one = [&](int i) -> bool {
+        vector<int> v;
+        rep(j, n) if ((i >> j) & 1) v.push_back(j);
+
+        for (int x = 0; x < v.size(); x++) {
+            for (int y = x + 1; y < v.size(); y++) {
+                int s1 = starts[v[x]], e1 = ends[v[x]];
+                int s2 = starts[v[y]], e2 = ends[v[y]];
+
+                if (s2 < s1) swap(s1, s2), swap(e1, e2);
+
                 if (s2 < e1) return false;
-                if (s1 + (24 * 60) < e2) return false;
+
+                if (e2 >= 2400) {
+                    e2 -= 2400;
+                    if (s1 < e2) return false;
+                }
+
             }
         }
         return true;
     };
 
-    vector<int> dp(1 << n, n);
-    dp[0] = 0;
-    for (int i = 1; i < (1 << n); i = i << 1) {
-        dp[i] = 1;
-    }
 
-    auto min_chair = [&](int i) {
+    auto get_min = [&](int i) -> int {
+        vector<int> v;
+        rep(j, n) if ((i >> j) & 1) v.push_back(j);
+        int ans = INT_MAX;
+        rep(j, 1 << v.size()) {
+            vector<int> a, b;
+            rep(l, v.size()) {
+                if ((j >> l) & 1) a.push_back(v[l]);
+                else b.push_back(v[l]);
+            }
 
-        vector<int> u;
-        rep(j, n) if ((i >> j) & 1) u.push_back(j);
-        int ans = n;
-        for (int s = (i - 1) & i; s > 0; s = (s - 1) & i) {
-            int t = i ^s;
-            int now = dp[s] + dp[t];
-            cmin(ans, now);
+            int x = 0, y = 0;
+            for (int k : a) x += (1 << k);
+            for (int k : b) y += (1 << k);
+
+            assert(x + y == i);
+            cmin(ans, dp[x] + dp[y]);
         }
         return ans;
     };
 
-
     rep(i, 1 << n) {
-        if (__builtin_popcount(i) <= 1) continue;
+        if (__builtin_popcount(i) == 1) continue;
         if (can_one(i)) dp[i] = 1;
-        else dp[i] = min_chair(i);
+        else dp[i] = get_min(i);
     }
 
     cout << dp[(1 << n) - 1] << endl;
