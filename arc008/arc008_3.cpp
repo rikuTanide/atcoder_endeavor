07c79e05028a2338feac096ccab775438e9bc400
riku <riku@tanide.net>
2020-08-13 00:30:21 +0900

    【WA】[AtCoder Regular Contest 008]C - THE☆たこ焼き祭り2012

--- arc008/arc008_3.cpp
+++ arc008/arc008_3.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,7 +9,6 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<double, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -19,7 +20,7 @@ double equal(double a, double b) {
 }
 
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -41,15 +42,17 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-struct Participant {
-    double x, y, throw_limit, catch_limit;
+struct User {
+    double x, y, t, r;
 };
 
-std::istream &operator>>(std::istream &in, Participant &o) {
-    cin >> o.x >> o.y >> o.throw_limit >> o.catch_limit;
+
+std::istream &operator>>(std::istream &in, User &o) {
+    in >> o.x >> o.y >> o.t >> o.r;
     return in;
 }
 
+// https://qiita.com/ta-ka/items/a023a11efe17ab097433
 struct Edge {
     ll to;
     double cost;
@@ -57,12 +60,14 @@ struct Edge {
 
 
 class Dijkstra {
+    ll vector_count;
 
     vector<double> distances;
     vector<vector<Edge>> edges;
 
 public:
     Dijkstra(ll n) {
+        vector_count = n;
         distances.resize(n, INF);
         edges.resize(n);
     }
@@ -103,43 +108,41 @@ public:
 
 };
 
-
 int main() {
     int n;
     cin >> n;
-    vector<Participant> participants(n);
-    rep(i, n) cin >> participants[i];
+
+    vector<User> users(n);
+    rep(i, n) cin >> users[i];
 
     Dijkstra dijkstra(n);
 
-    rep(i, n) rep(j, n) {
+    rep(i, n) {
+        rep(j, n) {
             if (i == j) continue;
-            Participant p1 = participants[i], p2 = participants[j];
-            double speed = min(p1.throw_limit, p2.catch_limit);
+            User u = users[i], t = users[j];
+            double speed = min(u.t, t.r);
 
-            double x = p1.x - p2.x;
-            double y = p1.y - p2.y;
+            double x = u.x - t.x;
+            double y = u.y - t.y;
             double xx = x * x;
             double yy = y * y;
 
-            double distance = sqrt(xx + yy);
-            double cost = distance / speed;
-            dijkstra.insert(i, j, cost);
+            double dis = sqrt(xx + yy);
+            double time = dis / speed;
+            dijkstra.insert(i, j, time);
+
         }
+    }
+
     dijkstra.dijkstra(0);
 
-    vector<double> time(n);
-    rep(i, n) {
-        if (i == 0) continue;
-        time[i] = dijkstra.distance(i);
-    }
-    sort(time.rbegin(), time.rend());
-    rep(i, n - 1) {
-        time[i] += (1.0 * i);
-    }
+    vector<double> v(n);
+    rep(i, n) v[i] = dijkstra.distance(i);
+    sort(v.rbegin(), v.rend());
+    rep(i, n) v[i] += i;
 
-    double ans = *max_element(time.begin(), time.end());
-    printf("%.20f\n", ans);
-
+    double ans = *max_element(v.begin(), v.end());
+    cout << setprecision(20) << ans << endl;
 
 }
