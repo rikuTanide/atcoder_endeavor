f0a7f42ea3b05770acffac953dcefc7599d785d8
riku <riku@tanide.net>
2020-08-12 20:15:47 +0900

    【WA】[AtCoder Grand Contest 047]A - Integer Product

--- agc047/agc047_a.cpp
+++ agc047/agc047_a.cpp
@@ -42,125 +42,81 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-bool nine(string &s) {
-    int i = distance(find(s.begin(), s.end(), '.'), s.end());
-    return i == 10;
-}
-
-vector<ll> up(vector<string> &v) {
-    vector<ll> res;
-    for (string s : v) {
-
-        if (find(s.begin(), s.end(), '.') == s.end()) {
-            s += ".0";
-        }
-
-        while (!nine(s)) {
-            s += "0";
-        }
-
-        ll a = 0;
-        for (char c : s) {
-            if (c == '.') continue;
-            ll i = c - '0';
-            a *= 10;
-            a += i;
-        }
-        res.push_back(a);
-    }
-    return res;
-}
-
-vector<ll> sp(vector<ll> v, ll s) {
-    vector<ll> res;
-    for (ll l : v) {
-        int ans = 0;
-        while (l % s == 0) {
-            ans++;
-            l /= s;
-        }
-        res.push_back(ans);
-    }
-    return res;
-}
-
-// 2と5の両方が足して18になる組み合わせ
-//
-class MatrixSum {
-    vector<vector<ll>> sum;
-public:
-    MatrixSum(ll x, ll y) {
-        sum = vector<vector<ll>>(x, vector<ll>(y));
-    }
-
-    void add(ll x, ll y) {
-        sum[x][y]++;
-    }
-
-    ll get(ll x, ll y) {
-        if (x == -1 || y == -1) {
-            return 0;
-        }
-        if (x == sum.size() || y == sum[x].size()) {
-            return 0;
-        }
-        return sum[x][y];
-    }
-
-    void setUp() {
-        for (ll x = 0; x < sum.size(); x++) {
-            for (ll y = 0; y < sum[x].size(); y++) {
-                sum[x][y] += get(x - 1, y) + get(x, y - 1) - get(x - 1, y - 1);
-            }
-        }
-    }
-
-    ll getSum(ll xs, ll ys, ll xe, ll ye) {
-        return get(xe, ye) - get(xs - 1, ye) - get(xe, ys - 1) + get(xs - 1, ys - 1);
-    }
-
-};
-
 int main() {
     int n;
     cin >> n;
-    vector<string> v(n);
-    rep(i, n) cin >> v[i];
 
-    vector<ll> v2 = up(v);
+    auto get_ll = [&]() {
+        vector<ll> v(n);
 
-    vector<ll> tows = sp(v2, 2);
-    vector<ll> fives = sp(v2, 5);
+        rep(i, n) {
+            string s;
+            cin >> s;
 
-    MatrixSum ms(200, 200);
+            if (find(s.begin(), s.end(), '.') == s.end()) s += ".0";
+
+            while (distance(find(s.begin(), s.end(), '.'), s.end()) <= 9) {
+                s += '0';
+            }
+
+            ll a = 0;
+            for (char c : s) {
+                if (c == '.') continue;
+                ll k = c - '0';
+                a *= 10;
+                a += k;
+            }
+
+            v[i] = a;
+        }
+        return v;
+    };
+
+    vector<ll> v = get_ll();
+
+    auto f = [&](int k) -> vector<int> {
+        vector<int> res(n);
+        rep(i, n) {
+            ll t = v[i];
+            int l = 0;
+            while (t % k == 0) {
+                l++;
+                t /= k;
+            }
+            res[i] = l;
+        }
+        return res;
+    };
+
+    vector<int> tows = f(2);
+    vector<int> fives = f(5);
+
+    vector<P> pairs;
     rep(i, n) {
-        ms.add(tows[i], fives[i]);
+        pairs.push_back(P(tows[i], fives[i]));
     }
-    ms.setUp();
-
-
+    map<P, int> counts;
+    for (P p : pairs) counts[p]++;
     ll ans = 0;
-    rep(i, n) {
 
-        int now = ms.getSum(max<int>(18 - tows[i], 0), max<int>(18 - fives[i], 0), 150, 150);
-        ans += now;
-    }
+    auto check = [&](P a, P b) -> bool {
+        return a.first + b.first >= 18 && a.second + b.second >= 18;
+    };
 
-
-//    rep(i, n) {
-//        rep(j, n) {
-//            if (tows[i] + tows[j] >= 18 && fives[i] + fives[j] >= 18) {
-//                ans++;
-//            }
-//        }
-//    }
-
-    rep(i, n) {
-        if (tows[i] + tows[i] >= 18 && fives[i] + fives[i] >= 18) {
-            ans--;
+    for (auto e1 : counts) {
+        for (auto e2 : counts) {
+            if (check(e1.first, e2.first))ans += e1.second * e2.second;
         }
     }
+
+    auto check_one = [&](P a) -> bool {
+        return a.first >= 9 && a.second >= 9;
+    };
+    for (P p : pairs) {
+        if (check_one(p)) ans--;
+    }
     ans /= 2;
+
 
     cout << ans << endl;
 
