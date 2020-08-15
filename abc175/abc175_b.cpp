db021cfd678c99ad78fa86f50ccf1384ff970dce
riku <riku@tanide.net>
2020-08-15 21:05:29 +0900

    【AC】[AtCoder Beginner Contest 175]B - Making Triangle

---
+++ abc175/abc175_b.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> v(n);
+    rep(i, n) cin >> v[i];
+    sort(v.begin(), v.end());
+
+    ll ans = 0;
+    for (int x = 0; x < n; x++) {
+        for (int y = x + 1; y < n; y++) {
+            for (int z = y + 1; z < n; z++) {
+                ll a = v[x];
+                ll b = v[y];
+                ll c = v[z];
+
+                if (a + b > c && a != b && b != c) {
+                    ans++;
+                }
+
+            }
+        }
+    }
+    cout << ans << endl;
+}
