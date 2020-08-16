14c9bba891f791b64209032044befa4ff2ebe539
riku <riku@tanide.net>
2020-08-16 19:20:31 +0900

    【AC】[AtCoder Regular Contest 024]C - だれじゃ

--- arc024/arc024_3.cpp
+++ arc024/arc024_3.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,7 +9,6 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -19,7 +20,7 @@ double equal(double a, double b) {
 }
 
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -41,79 +42,40 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-vector<vector<int>> get_array(string &s, int k) {
-    vector<int> tmp(26);
-    rep(i, k) {
-        tmp[s[i] - 'a']++;
-    }
-    vector<vector<int>> res;
-    res.push_back(tmp);
-    rep(i, s.size() - k) {
-        tmp[s[i] - 'a']--;
-        tmp[s[i + k] - 'a']++;
-        res.push_back(tmp);
-    }
-    return res;
-}
-
 int main() {
     int n, k;
     string s;
     cin >> n >> k >> s;
 
-    auto res = get_array(s, k);
-    multiset<vector<int>> sres;
-    for (auto r : res) sres.insert(r);
+    vector<int> tmp(26, 0);
 
-    auto del = [&](int i) {
-        if (i >= res.size()) return;
-        auto &r = res[i];
-//        cout << "del ";
-//        for(int j : r) cout << j << ' ';
-//        cout << endl;
-        auto it = sres.lower_bound(r);
-        assert(it != sres.end());
-        sres.erase(it);
-    };
+    vector<vector<int>> cache(n - k + 1);
 
-//    cout << "res" << endl;
-//    for (auto r :res) {
-//        rep(i, 26) cout << r[i] << ' ';
-//        cout << endl;
-//    }
+    rep(i, k) {
+        char c = s[i];
+        tmp[c - 'a']++;
+    }
 
-//    cout << "sres" << endl;
+    cache[0] = tmp;
 
-//    for (auto r :sres) {
-//        rep(i, 26) cout << r[i] << ' ';
-//        cout << endl;
-//    }
+    for (int i = 1; i <= n - k; i++) {
+        vector<int> next = tmp;
+        char out = s[i - 1];
+        char in = s[i + k - 1];
+        next[out - 'a']--;
+        next[in - 'a']++;
+        cache[i] = next;
+        tmp = next;
+    }
 
-    rep(i, k) del(i);
+    set<vector<int>> used;
 
-    rep(i, res.size()) {
-        auto &r = res[i];
-//        cout << "check ";
-//        for(int j : r) cout << j << ' ';
-//        cout << endl;
-
-        auto it = sres.lower_bound(r);
-        if (it == sres.end()) {
-            del(i + k);
-        } else if (vector<int>(*it) != r) {
-            del(i + k);
-        } else {
+    for (int i = k; i <= n - k; i++) {
+        used.insert(cache[i - k]);
+        if (used.find(cache[i]) != used.end()) {
             cout << "YES" << endl;
             ret();
-//            del(i + k);
-
         }
     }
-//
-//    for (auto r :sres) {
-//        rep(i, 26) cout << r[i] << ' ';
-//        cout << endl;
-//    }
     cout << "NO" << endl;
 }
-
