4ca61111d9f88d3163656c170d531cab616705a8
riku <riku@tanide.net>
2020-08-16 15:58:44 +0900

    【AC】[AtCoder Beginner Contest 006]D - トランプ挿入ソート

--- abc006/abc006_4.cpp
+++ abc006/abc006_4.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,7 +9,6 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -19,7 +20,7 @@ double equal(double a, double b) {
 }
 
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -41,37 +42,25 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-int search(vector<int> &dp, int x) {
-    auto begin = dp.begin();
-    auto end = dp.end();
-
-    auto er = equal_range(begin, end, x);
-    int eq = distance(er.first, er.second);
-    int sm = distance(begin, lower_bound(begin, end, x));
-    return eq + sm;
-}
-
 int main() {
     int n;
     cin >> n;
-
-    vector<int> cards(n);
-    rep(i, n) cin >> cards[i];
-
+    vector<int> trumps(n);
+    rep(i, n) cin >> trumps[i];
     vector<int> dp(n + 1, INT_MAX);
     dp[0] = INT_MIN;
 
     rep(i, n) {
-        int k = cards[i];
-        int index = search(dp, k);
-        cmin(dp[index], k);
-    }
-    int now = 0;
-    rep(i, n + 1) {
-        int k = dp[i];
-        if (k == INT_MIN || k == INT_MAX) continue;
-        now = i;
-    }
-    cout << n - now << endl;
-}
+        int card = trumps[i];
 
+        auto it = upper_bound(dp.begin(), dp.end(), card);
+        if (*it > card) *it = card;
+
+    }
+
+    int ma = 0;
+    rep(i, n + 1) if (dp[i] != INT_MAX) ma = i;
+
+    cout << n - ma << endl;
+
+}
