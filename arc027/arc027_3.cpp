c692e1ea310a8450602c3113308cce2c6efd1a0f
riku <riku@tanide.net>
2020-08-18 20:16:45 +0900

    【RE】[AtCoder Regular Contest 027]C - 最高のトッピングにしような

--- arc027/arc027_3.cpp
+++ arc027/arc027_3.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -29,59 +35,54 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 struct Topping {
-    ll ticket, happiness;
+    int cost;
+    ll happiness;
 };
 
 std::istream &operator>>(std::istream &in, Topping &o) {
-    cin >> o.ticket >> o.happiness;
+    in >> o.cost >> o.happiness;
     return in;
 }
 
-
 int main() {
-    int x, y, n;
-    cin >> x >> y >> n;
+    int x, y;
+    cin >> x >> y;
+
+    int n;
+    cin >> n;
 
     vector<Topping> toppings(n);
     rep(i, n) cin >> toppings[i];
+    const int MAX = 300 * 600 + 10;
+    vector<vector<vector<ll>>> dp(n + 1, vector<vector<ll>>(n + 1, vector<ll>(MAX + 400, 0)));
 
-    vector<vector<ll>> prev(n + 1, vector<ll>(x + y + 1, -1));
-    prev[0][0] = 0;
+    auto get = [&](int i, int j, int k) -> ll {
+        return dp[i][j][k];
+    };
 
+    auto set = [&](int i, int j, int k, ll value) {
+        cmax(dp[i][j][k], value);
+    };
 
     rep(i, n) {
-
-        vector<vector<ll>> next(n + 1, vector<ll>(x + y + 1, -1));
-        next[0][0] = 0;
-
-        auto set = [&](int j, int ticket, ll happiness) {
-            if (ticket > x + y) return;
-            cmax(next[j][ticket], happiness);
-        };
-
-        Topping t = toppings[i];
         rep(j, n) {
-            rep(k, x + y + 1) {
-                ll now = prev[j][k];
-                if (now == -1)continue;
-                set(j + 1, k + t.ticket, now + t.happiness);
-                set(j, k, now);
+            rep(k, MAX) {
+                Topping t = toppings[i];
+                ll prev_happy = get(i, j, k);
+                set(i + 1, j + 1, k + t.cost, prev_happy + t.happiness);
+                set(i + 1, j, k, prev_happy);
             }
         }
-        prev = next;
     }
-
     ll ans = 0;
-    rep(j, min(x, n) + 1) rep(k, x + y + 1) cmax(ans, prev[j][k]);
+    rep(j, min(n + 1, x + 1)) rep(k, x + y + 1) cmax(ans, dp[n][j][k]);
     cout << ans << endl;
-
 }
