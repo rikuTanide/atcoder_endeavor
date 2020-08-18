79e6aa9a3b6ecbba4f8040bb605578fbe8a74a82
riku <riku@tanide.net>
2020-08-18 22:51:18 +0900

    【AC】[AtCoder Beginner Contest 013]C - 節制

--- abc013/abc013_3.cpp
+++ abc013/abc013_3.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -29,41 +35,39 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-const int mod = 1000000007;
 //const ll mod = 1e10;
-typedef priority_queue<string, vector<string>, greater<string> > PQ_ASK;
 
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-
-    ll nichi, shoki;
-
-    ll f_yen, f_manzoku, s_yen, s_manzoku, heri;
-    cin >> nichi >> shoki >> f_yen >> f_manzoku >> s_yen >> s_manzoku >> heri;
+    int n;
+    ll h;
+    ll a, b, c, d, e;
+    cin >> n >> h >> a >> b >> c >> d >> e;
 
     ll ans = INF;
+    for (int nukuhi = 0; nukuhi <= n; nukuhi++) {
+        ll heri = h - e * nukuhi;
+        int taberuhi = n - nukuhi;
+        if (b * taberuhi + heri <= 0) continue;
 
-    rep(i, nichi + 1) {
-        ll f_kai = i;
-        ll bunshi = -(shoki + f_kai * f_manzoku - nichi * heri + f_kai * heri);
-        if (bunshi < 0) {
-            ll cost = f_kai * f_yen;
-            cmin(ans, cost);
+        if (d * taberuhi + heri > 0) {
+            ll now = c * taberuhi;
+            cmin(ans, now);
             continue;
         }
-        ll bunbo = s_manzoku + heri;
-        ll s_kai = bunshi / bunbo + 1;
 
-        if (s_kai + f_kai > nichi) continue;
+        ll t = -(heri + d * taberuhi);
+        assert(t >= 0);
+        ll s = b - d;
 
-        ll cost = f_kai * f_yen + s_kai * s_yen;
-        cmin(ans, cost);
+        ll hutu = (t + s) / s;
+        ll siso = taberuhi - hutu;
+        ll now = hutu * a + siso * c;
+        cmin(ans, now);
     }
-    assert(ans != INF);
     cout << ans << endl;
-
 }
