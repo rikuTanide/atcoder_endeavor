940e1ce82f7bcae34d8db0af2ae31e227f2251d4
riku <riku@tanide.net>
2020-08-19 21:48:28 +0900

    【AC】[AtCoder Beginner Contest 017]C - ハイスコア

--- abc017/abc017_3.cpp
+++ abc017/abc017_3.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,7 +9,6 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -18,8 +19,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -41,28 +42,37 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-struct Heritage {
-    int left, right;
-    ll point;
+struct Iseki {
+    int left, right, point;
 };
 
 int main() {
-
     int n, m;
     cin >> n >> m;
+    vector<Iseki> isekis(n);
+    for (Iseki &iseki : isekis) {
+        cin >> iseki.left >> iseki.right >> iseki.point;
+        iseki.left--;
+        iseki.right--;
+    }
 
-    vector<Heritage> v(n);
-    rep(i, n) cin >> v[i].left >> v[i].right >> v[i].point;
-    rep(i, n) v[i].left--, v[i].right--;
+    vector<ll> imos(m + 1);
+    for (Iseki iseki: isekis) {
+        imos[iseki.left] += iseki.point;
+        imos[iseki.right + 1] -= iseki.point;
+    }
 
-    vector<int> imos(m + 1, 0);
-    for (Heritage h : v) imos[h.left] += h.point;
-    for (Heritage h : v) imos[h.right + 1] -= h.point;
-    rep(i, m) imos[i + 1] += imos[i];
+    rep(i, m - 1) {
+        imos[i + 1] += imos[i];
+    }
 
-    ll sum = accumulate(v.begin(), v.end(), 0ll, [](ll l, Heritage h) { return l + h.point; });
-
-    ll mi = *min_element(imos.begin(), imos.end() - 1);
-    cout << sum - mi << endl;
-
+    ll sum = accumulate(isekis.begin(), isekis.end(), 0ll, [](ll sum, Iseki i1) {
+        return sum + i1.point;
+    });
+    ll ans = 0;
+    rep(i, m) {
+        ll now = sum - imos[i];
+        cmax(ans, now);
+    }
+    cout << ans << endl;
 }
