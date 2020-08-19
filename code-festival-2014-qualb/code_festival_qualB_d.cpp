ed0eaf5ab862995bbf757ae2933079ab33beb143
riku <riku@tanide.net>
2020-08-19 17:25:30 +0900

    【AC】[CODE FESTIVAL 2014 予選B]D - 登山家

--- code-festival-2014-qualb/code_festival_qualB_d.cpp
+++ code-festival-2014-qualb/code_festival_qualB_d.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,7 +9,6 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -18,8 +19,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -40,54 +41,54 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 //const ll mod = 1e10;
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
-
 struct Mountain {
+    int i;
     ll height;
-    int index;
 };
+
+vector<int> solve(vector<Mountain> mountains, int n) {
+    sort(mountains.rbegin(), mountains.rend(), [](Mountain m1, Mountain m2) {
+        //　大きい順、左にある順
+        // こうすると、自分より右にある奴はすべてまだ出てきてない
+        return P(m1.height, -m1.i) < P(m2.height, -m2.i);
+    });
+
+    vector<int> ans(n);
+    set<int> used;
+
+    rep(i, n) {
+        Mountain mountain = mountains[i];
+        int r = [&]() -> int {
+            auto it = used.upper_bound(mountain.i);
+            if (it == used.end()) {
+                int b = n - mountain.i - 1;
+                return b;
+            }
+            int b = (*it) - mountain.i - 1;
+            return b;
+        }();
+        ans[mountain.i] = r;
+        used.insert(mountain.i);
+    }
+
+    return ans;
+
+}
 
 int main() {
     int n;
     cin >> n;
 
     vector<Mountain> mountains(n);
+    rep(i, n)mountains[i].i = i;
     rep(i, n) cin >> mountains[i].height;
-    rep(i, n) mountains[i].index = i;
 
-    sort(mountains.rbegin(), mountains.rend(), [](Mountain &m1, Mountain &m2) { return m1.height < m2.height; });
-    vector<vector<ll>> v;
-    v.push_back(vector<ll>{mountains[0].index});
-    rep(i, n) {
-        if (i == 0) continue;
-        if (mountains[i - 1].height == mountains[i].height) v.back().push_back(mountains[i].index);
-        else v.push_back(vector<ll>{mountains[i].index});
-    }
-
-    set<int> used_indexes;
+    vector<int> right = solve(mountains, n);
+    rep(i, n) mountains[i].i = n - mountains[i].i - 1;
+    vector<int> left = solve(mountains, n);
+    reverse(left.begin(), left.end());
     vector<int> ans(n);
-    for (auto &u : v) {
-        for (int i : u) {
-            if (used_indexes.empty()) ans[i] = n - 1;
-            else {
-                int end = [&] {
-                    auto end_it = used_indexes.upper_bound(i);
-                    if (end_it == used_indexes.end()) return n;
-                    return *end_it;
-                }();
-                int start = [&] {
-                    auto begin_it = used_indexes.upper_bound(i);
-                    if (begin_it == used_indexes.begin()) return -1;
-                    begin_it--;
-                    return *begin_it;
-                }();
-
-                int now = end - start - 2;
-                ans[i] = now;
-            }
-        }
-        for (int i : u)used_indexes.insert(i);
-    }
-
+    rep(i, n) ans[i] = right[i] + left[i];
     for (int i : ans) cout << i << endl;
 
 }
