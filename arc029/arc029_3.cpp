74429dd6a9d916b3ce46b5aeed7fba352f322b89
riku <riku@tanide.net>
2020-08-19 14:57:51 +0900

    【AC】[AtCoder Regular Contest 029]C - 高橋君と国家

--- arc029/arc029_3.cpp
+++ arc029/arc029_3.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,7 +9,6 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -18,8 +19,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -40,6 +41,31 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 //const ll mod = 1e10;
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+template<typename T>
+struct edge {
+    int src, to;
+    T cost;
+
+    edge(int to, T cost) : src(-1), to(to), cost(cost) {}
+
+    edge(int src, int to, T cost) : src(src), to(to), cost(cost) {}
+
+    edge &operator=(const int &x) {
+        to = x;
+        return *this;
+    }
+
+    operator int() const { return to; }
+};
+
+template<typename T>
+using Edges = vector<edge<T> >;
+template<typename T>
+using WeightedGraph = vector<Edges<T> >;
+using UnWeightedGraph = vector<vector<int> >;
+template<typename T>
+using Matrix = vector<vector<T> >;
 
 struct UnionFind {
     vector<int> data;
@@ -68,32 +94,6 @@ struct UnionFind {
 };
 
 template<typename T>
-struct edge {
-    int src, to;
-    T cost;
-
-    edge(int to, T cost) : src(-1), to(to), cost(cost) {}
-
-    edge(int src, int to, T cost) : src(src), to(to), cost(cost) {}
-
-    edge &operator=(const int &x) {
-        to = x;
-        return *this;
-    }
-
-    operator int() const { return to; }
-};
-
-template<typename T>
-using Edges = vector<edge<T> >;
-template<typename T>
-using WeightedGraph = vector<Edges<T> >;
-using UnWeightedGraph = vector<vector<int> >;
-template<typename T>
-using Matrix = vector<vector<T> >;
-
-
-template<typename T>
 T kruskal(Edges<T> &edges, int V) {
     sort(begin(edges), end(edges), [](const edge<T> &a, const edge<T> &b) {
         return (a.cost < b.cost);
@@ -109,32 +109,30 @@ T kruskal(Edges<T> &edges, int V) {
 int main() {
     int n, m;
     cin >> n >> m;
-    
+
+    vector<ll> coins(n);
+    rep(i, n) cin >> coins[i];
+
     Edges<ll> edges;
 
+    rep(_, m) {
+        int a, b;
+        ll r;
+        cin >> a >> b >> r;
+        a--;
+        b--;
+
+        edges.push_back(edge<ll>(a, b, r));
+        edges.push_back(edge<ll>(a, b, r));
+
+    }
+
     rep(i, n) {
-        ll cost;
-        cin >> cost;
-        edge<ll> e1(n, i, cost);
-        edge<ll> e2(i, n, cost);
-        edges.push_back(e1);
-        edges.push_back(e2);
+        edges.push_back(edge<ll>(i, n, coins[i]));
+        edges.push_back(edge<ll>(n, i, coins[i]));
     }
 
-    rep(i, m) {
-        int from, to;
-        ll cost;
-        cin >> from >> to >> cost;
-        from--;
-        to--;
-        edge<ll> e1(from, to, cost);
-        edge<ll> e2(to, from, cost);
-        edges.push_back(e1);
-        edges.push_back(e2);
-    }
-
-
-    ll ans = kruskal(edges, n + 1);
+    ll ans = kruskal(edges, n +1);
     cout << ans << endl;
 
 }
