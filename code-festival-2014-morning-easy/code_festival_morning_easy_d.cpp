1fc8ba1d19cc3d47769731088c72fedfbfe210dd
riku <riku@tanide.net>
2020-08-19 18:41:56 +0900

    【WA】[CODE FESTIVAL 2014 Easy]D - 枕決め

--- code-festival-2014-morning-easy/code_festival_morning_easy_d.cpp
+++ code-festival-2014-morning-easy/code_festival_morning_easy_d.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,7 +9,6 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -18,8 +19,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -44,30 +45,33 @@ typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 int main() {
     int n, m;
     cin >> n >> m;
-    int MAX = 100001;
-    vector<vector<int>> visitors(MAX);
 
-    rep(i, n) {
-        int x, y;
-        cin >> x >> y;
-        visitors[x].push_back(y);
-    }
+    vector<P> members(n);
+    for (P &p : members) cin >> p.first >> p.second;
+    vector<int> makura(n);
+    for (int &ma : makura) cin >> ma;
 
-    vector<int> makuras(MAX, 0);
-    rep(i, m) {
-        int a;
-        cin >> a;
-        makuras[a]++;
-    }
+    sort(makura.begin(), makura.end());
+    sort(members.rbegin(), members.rend());
 
     PQ_ASK q;
 
     int ans = 0;
-    rep(i, MAX) {
-        for (int h : visitors[i]) q.push(h);
-        while (!q.empty() && q.top() < i) q.pop();
-        rep(j, makuras[i]) if (!q.empty())q.pop(), ans++;
-    }
-    cout << ans << endl;
+    for (int ma : makura) {
 
+        while (!members.empty() && members.back().first <= ma) {
+            q.push(members.back().second);
+            members.pop_back();
+        }
+
+        while (!q.empty() && q.top() < ma) q.pop();
+
+        if (!q.empty()) {
+            ans++;
+            q.pop();
+        }
+
+    }
+
+    cout << ans << endl;
 }
