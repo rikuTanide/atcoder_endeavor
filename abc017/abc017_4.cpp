7095b30b13a499740668648249e98f0daf575f03
riku <riku@tanide.net>
2020-08-20 23:33:34 +0900

    【WA】[AtCoder Beginner Contest 017]D - サプリメント

--- abc017/abc017_4.cpp
+++ abc017/abc017_4.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,7 +9,6 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -18,8 +19,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -41,78 +42,55 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-void tow_pointer(int n,
-                 function<bool(int left)> check_begin,
-                 function<void(int left)> begin,
-                 function<bool(int right)> check_right,
-                 function<void(int left, int right)> push_right,
-                 function<void(int left)> pop_left,
-                 function<void(int left, int right)> dead_end
-) {
+template<class T, class F>
+struct SegmentTree {
+    F f;
+    T ti;
+    vector<T> dat;
+    int sz;
 
-    auto go = [&](P range) {
-        int right = -1;
-        for (int left = range.first; left <= range.second; left++) {
-            if (right < left) {
-                right = left;
-                begin(left);
-                dead_end(left, right);
-            }
-            while (right + 1 <= range.second && check_right(right + 1)) {
-                right++;
-                push_right(left, right);
-            }
-            dead_end(left, right);
-            pop_left(left);
+    SegmentTree(const F &f, const T &ti) : f(f), ti(ti) {}
+
+    void build(const vector<T> &v) {
+        assert(v.size());
+        sz = 1;
+        while (sz < v.size())sz <<= 1;
+        dat.resize(sz << 1, ti);
+        for (int i = 0; i < v.size(); i++)dat[sz - 1 + i] = v[i];
+        for (int i = sz - 2; i >= 0; i--)dat[i] = f(dat[i * 2 + 1], dat[i * 2 + 2]);
+    }
+
+    inline void update(int k, T x) {
+        k += sz - 1;
+        dat[k] = x;
+        while (k) {
+            k = (k - 1) / 2;
+            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
         }
-    };
+    }
 
-
-    auto get_ranges = [&] {
-        vector<bool> enable_items(n);
-        rep(i, n) enable_items[i] = check_begin(i);
-
-        auto start = [&](int i) {
-            if (i == 0) {
-                return enable_items[i] == true;
-            }
-            if (!enable_items[i - 1] && enable_items[i]) {
-                return true;
-            }
-            return false;
-        };
-
-        auto end = [&](int i) {
-
-            if (i == n - 1) {
-                return enable_items[i] == true;
-            }
-
-            if (enable_items[i] && !enable_items[i + 1]) {
-                return true;
-            }
-
-            return false;
-        };
-
-        vector<P> ans;
-        P p;
-        rep(i, n) {
-            if (start(i)) {
-                p = P(i, -1);
-            }
-            if (end(i)) {
-                p.second = i;
-                ans.push_back(p);
-            }
+    inline void add(int k, int x) {
+        k += sz - 1;
+        dat[k] = f(dat[k], x);
+        while (k) {
+            k = (k - 1) / 2;
+            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
         }
-        return ans;
-    };
+    }
 
+    inline T query(int a, int b) {
+        if (a == -1) {
+            return query(0, b) + 1;
+        }
+        return query(a, b, 0, 0, sz);
+    }
 
-    vector<P> ranges = get_ranges();
-    for (P range: ranges) go(range);
-}
+    T query(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l)return ti;
+        if (a <= l && r <= b)return dat[k];
+        return f(query(a, b, k * 2 + 1, l, (l + r) / 2), query(a, b, k * 2 + 2, (l + r) / 2, r));
+    }
+};
 
 const int mod = 1000000007;
 
@@ -186,125 +164,41 @@ struct mint {
 
 };
 
-
-template<class T, class F>
-struct SegmentTree {
-    F f;
-    T ti;
-    vector<T> dat;
-    int sz;
-
-    SegmentTree(const F &f, const T &ti) : f(f), ti(ti) {}
-
-    void build(const vector<T> &v) {
-        assert(v.size());
-        sz = 1;
-        while (sz < v.size())sz <<= 1;
-        dat.resize(sz << 1, ti);
-        for (int i = 0; i < v.size(); i++)dat[sz - 1 + i] = v[i];
-        for (int i = sz - 2; i >= 0; i--)dat[i] = f(dat[i * 2 + 1], dat[i * 2 + 2]);
-    }
-
-    inline void update(int k, T x) {
-        k += sz - 1;
-        dat[k] = x;
-        while (k) {
-            k = (k - 1) / 2;
-            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
-        }
-    }
-
-    inline void add(int k, int x) {
-        k += sz - 1;
-        dat[k] = f(dat[k], x);
-        while (k) {
-            k = (k - 1) / 2;
-            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
-        }
-    }
-
-    inline T query(int a, int b) {
-        return query(a, b, 0, 0, sz);
-    }
-
-    T query(int a, int b, int k, int l, int r) {
-        if (r <= a || b <= l)return ti;
-        if (a <= l && r <= b)return dat[k];
-        return f(query(a, b, k * 2 + 1, l, (l + r) / 2), query(a, b, k * 2 + 2, (l + r) / 2, r));
-    }
-};
-
-
 int main() {
-    int n;
-    ll k;
-    cin >> n >> k;
+    int n, m;
+    cin >> n >> m;
 
-    vector<ll> supplements(n);
-    rep(i, n) cin >> supplements[i];
+    vector<int> v(n);
+    rep(i, n) cin >> v[i];
+
+    map<int, int> prev;
+
+    vector<int> back(n);
+
+    rep(i, n) {
+        int s = v[i];
+        if (prev.find(s) == prev.end()) {
+            back[i] = -1;
+        } else {
+            back[i] = prev[s];
+        }
+        prev[s] = i;
+    }
+
 
     auto f = [](mint i, mint j) { return i + j; };
-    SegmentTree<mint, decltype(f)> segmentTree(f, mint(0));
-    vector<mint> dp(n + 1,  mint(0));
-    dp[0] = 1;
-    segmentTree.build(dp);
-
-    auto add = [&](int left, int right) {
-        mint q = segmentTree.query(left, right + 1);
-        segmentTree.update(right + 1, q);
-    };
-
-    // この要素は単体で条件を満たすか？
-    auto check_begin = [&](int left) {
-        return true;
-    };
-
-    set<int> current;
-    // leftを左として尺を初期化しろ
-    auto begin = [&](int left) {
-        current = set<int>();
-        current.insert(supplements[left]);
-        add(left, left);
-    };
-
-    // 右に1個伸びれるか確認しろ
-    // 次の「1個右の要素は」rightだ
-    auto check_right = [&](int right) {
-        int s = supplements[right];
-        return current.find(s) == current.end();
-    };
-
-    // 右に1このびろ
-    // 次の「1個右の要素は」rightだ
-    auto push_right = [&](int left, int right) {
-        int s = supplements[right];
-        current.insert(s);
-        add(left, right);
-    };
-
-    // 左を1個捨てろ
-    // 捨てる左の要素はleftだ
-    auto pop_left = [&](int left) {
-        int s = supplements[left];
-        assert(current.find(s) != current.end());
-        current.erase(s);
-    };
-
-    // 尺が伸びれるところまで伸びた
-    auto dead_end = [&](int left, int right) {
-//        cout << left << ' ' << right << endl;
-
-    };
+    SegmentTree<mint, decltype(f)> segmentTree(f, 0);
+    segmentTree.build(vector<mint>(n));
 
 
-    tow_pointer(
-            n,
-            check_begin,
-            begin,
-            check_right,
-            push_right,
-            pop_left,
-            dead_end
-    );
-    cout << segmentTree.query(n, n + 1) << endl;
+    rep(i, n) {
+        int bk = back[i];
+        mint now = segmentTree.query(bk, i);
+        segmentTree.update(i, now);
+    }
+
+    mint ans = segmentTree.query(n - 1, n);
+
+    cout << ans << endl;
+
 }
