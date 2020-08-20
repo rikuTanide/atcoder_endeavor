3642a2489f6502b9ade2ba90629c69629467e5be
riku <riku@tanide.net>
2020-08-21 00:49:05 +0900

    【AC】[AtCoder Beginner Contest 017]D - サプリメント

--- abc017/abc017_4.cpp
+++ abc017/abc017_4.cpp
@@ -164,43 +164,160 @@ struct mint {
 
 };
 
+void tow_pointer(int n,
+                 function<bool(int left)> check_begin,
+                 function<void(int left)> begin,
+                 function<bool(int right)> check_right,
+                 function<void(int right)> push_right,
+                 function<void(int left)> pop_left,
+                 function<void(int left, int right)> dead_end
+) {
+
+    auto go = [&](P range) {
+        int right = -1;
+        for (int left = range.first; left <= range.second; left++) {
+            if (right < left) {
+                right = left;
+                begin(left);
+            }
+            while (right + 1 <= range.second && check_right(right + 1)) {
+                right++;
+                push_right(right);
+            }
+            dead_end(left, right);
+            pop_left(left);
+        }
+    };
+
+
+    auto get_ranges = [&] {
+        vector<bool> enable_items(n);
+        rep(i, n) enable_items[i] = check_begin(i);
+
+        auto start = [&](int i) {
+            if (i == 0) {
+                return enable_items[i] == true;
+            }
+            if (!enable_items[i - 1] && enable_items[i]) {
+                return true;
+            }
+            return false;
+        };
+
+        auto end = [&](int i) {
+
+            if (i == n - 1) {
+                return enable_items[i] == true;
+            }
+
+            if (enable_items[i] && !enable_items[i + 1]) {
+                return true;
+            }
+
+            return false;
+        };
+
+        vector<P> ans;
+        P p;
+        rep(i, n) {
+            if (start(i)) {
+                p = P(i, -1);
+            }
+            if (end(i)) {
+                p.second = i;
+                ans.push_back(p);
+            }
+        }
+        return ans;
+    };
+
+
+    vector<P> ranges = get_ranges();
+    for (P range: ranges) go(range);
+}
+
+vector<int> create_back(int n, vector<int> v) {
+    reverse(v.begin(), v.end());
+    v.push_back(v.back());
+
+    n++;
+
+    set<int> used;
+    // この要素は単体で条件を満たすか？
+    auto check_begin = [&](int left) {
+        return !contain(used, v[left]);
+    };
+
+    // leftを左として尺を初期化しろ
+    auto begin = [&](int left) {
+        used.clear();
+        used.insert(v[left]);
+    };
+
+    // 右に1個伸びれるか確認しろ
+    // 次の「1個右の要素は」rightだ
+    auto check_right = [&](int right) {
+        return !contain(used, v[right]);
+    };
+
+    // 右に1このびろ
+    // 次の「1個右の要素は」rightだ
+    auto push_right = [&](int right) {
+        used.insert(v[right]);
+    };
+
+    // 左を1個捨てろ
+    // 捨てる左の要素はleftだ
+    auto pop_left = [&](int left) {
+        used.erase(v[left]);
+    };
+    vector<int> back(n, INT_MAX);
+
+
+    // 尺が伸びれるところまで伸びた
+    auto dead_end = [&](int left, int right) {
+        back[left] = right + 1;
+    };
+
+    tow_pointer(
+            n,
+            check_begin,
+            begin,
+            check_right,
+            push_right,
+            pop_left,
+            dead_end
+    );
+
+    return back;
+}
+
 int main() {
     int n, m;
     cin >> n >> m;
 
     vector<int> v(n);
     rep(i, n) cin >> v[i];
-
-    map<int, int> prev;
-
-    vector<int> back(n);
-
-    rep(i, n) {
-        int s = v[i];
-        if (prev.find(s) == prev.end()) {
-            back[i] = -1;
-        } else {
-            back[i] = prev[s];
-        }
-        prev[s] = i;
-    }
-
+    vector<int> back = create_back(n, v);
 
     auto f = [](mint i, mint j) { return i + j; };
     SegmentTree<mint, decltype(f)> segmentTree(f, 0);
     segmentTree.build(vector<mint>(n));
 
-
-    rep(i, n) {
-        int bk = back[i];
-        mint now = segmentTree.query(bk, i);
-        segmentTree.update(i, now);
-        if (bk != -1) {
-            segmentTree.update(bk, 0);
+    auto get = [&](int l, int r) -> mint {
+        if (r == n) {
+            return segmentTree.query(l, r) + 1;
         }
+        return segmentTree.query(l, r + 1);
+    };
+
+    for (int i = n - 1; i >= 0; i--) {
+        int r = back[i];
+        mint now = get(i, r);
+        segmentTree.update(i, now);
     }
 
-    mint ans = segmentTree.query(n - 1, n);
+    mint ans = segmentTree.query(0, 1);
 
     cout << ans << endl;
 
