9e339bbe1d0b2e602c17de81764553c0ca772450
riku <riku@tanide.net>
2020-08-22 21:06:02 +0900

    【AC】[AtCoder Beginner Contest 176]C - Step

---
+++ abc176/abc176_c.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> v(n);
+    for (ll &l:v) cin >> l;
+
+    vector<ll> r = {v[0]};
+    ll ans = 0;
+    for (int i = 1; i < n; i++) {
+        ll next = v[i];
+        if (r.back() <= next) {
+            r.push_back(next);
+            continue;
+        }
+
+        assert(next < r.back());
+        ll sub = r.back() - next;
+        ans += sub;
+        r.push_back(r.back());
+    }
+
+    cout << ans << endl;
+
+}
