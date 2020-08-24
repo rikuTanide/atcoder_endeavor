7afbb138c2c22b885f3a881164d872921d032dac
riku <riku@tanide.net>
2020-08-25 04:45:36 +0900

    【AC】[AtCoder Beginner Contest 031]D - 語呂合わせ

--- abc031/abc031_d.cpp
+++ abc031/abc031_d.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
-
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -8,6 +9,7 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
@@ -17,8 +19,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -31,8 +33,6 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-typedef pair<ll, ll> P;
-
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ofstream outfile("log.txt");
@@ -42,64 +42,135 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-struct Word {
-    string num, target;
+
+struct Pair {
+    string number;
+    string word;
 };
 
-std::istream &operator>>(std::istream &in, Word &o) {
-    cin >> o.num >> o.target;
-    return in;
-}
 
-bool rec(map<char, string> &dict, vector<Word> &words, int wi, int ci, int ni) {
-    if (wi == words.size()) return true;
+struct Index {
+    int pair_id, number_index, word_index;
+};
 
-    Word w = words[wi];
+struct Pairs {
+    vector<Pair> pairs;
 
+    Pairs(int n) : pairs(n) {}
 
-    if (ni == w.num.size()) {
-        bool b = ci == w.target.size();
-        if (!b) return false;
-        return rec(dict, words, wi + 1, 0, 0);
+    char getChar(Index index) {
+        return pairs[index.pair_id].number[index.number_index];
     }
 
-    char num = w.num[ni];
-    if (dict.find(num) != dict.end()) {
-        string match = dict[num];
-        if (w.target.substr(ci, match.size()) == dict[num]) {
-            return rec(dict, words, wi, ci + match.size(), ni + 1);
-        } else {
-            return false;
+    bool check(Index index, string term) {
+        string cut = subStr(index, term.size());
+        if (cut == "") return false;
+        return cut == term;
+    }
+
+    string subStr(Index index, int term_len) {
+        string &word = pairs[index.pair_id].word;
+        int word_len = word.size();
+        if (word_len < index.word_index + term_len) return "";
+        string cut = word.substr(index.word_index, term_len);
+        return cut;
+    }
+
+    Index next(Index index, int l) {
+        Index nx = {
+                index.pair_id,
+                index.number_index + 1,
+                index.word_index + l,
+        };
+        string word = pairs[index.pair_id].word;
+        string number = pairs[index.pair_id].number;
+
+        assert(nx.word_index <= word.size());
+        assert(nx.number_index <= number.size());
+
+        if (nx.word_index == word.size() || nx.number_index == number.size()) {
+            if (nx.word_index == word.size() && nx.number_index == number.size()) {
+                return Index{
+                        index.pair_id + 1,
+                        0,
+                        0,
+                };
+            }
+            return Index{
+                    -1,
+                    0,
+                    0,
+            };
         }
+
+        return nx;
     }
 
-    rep(i, 3) {
-        if (ci + i >= w.target.size()) {
-            continue;
-        }
-        string next = w.target.substr(ci, i + 1);
-        dict[num] = next;
-        bool b = rec(dict, words, wi, ci + next.size(), ni + 1);
-        if (b) return true;
-        dict.erase(num);
+
+};
+
+struct Rule {
+    map<char, string> rules;
+
+    bool hasRule(char c) {
+        return rules.find(c) != rules.end();
     }
+
+    string getRule(char c) {
+        assert(rules.find(c) != rules.end());
+        return rules[c];
+    }
+
+    void add(char c, string term) {
+        assert (rules.find(c) == rules.end());
+        rules[c] = term;
+    }
+
+    void remove(char c, string term) {
+        assert(rules[c] == term);
+        rules.erase(rules.find(c));
+    }
+
+};
+
+bool recursive(Pairs &pairs, Rule &rule, Index index) {
+    if (index.pair_id == pairs.pairs.size()) return true;
+    if (index.pair_id == -1) return false;
+
+    char c = pairs.getChar(index);
+    if (rule.hasRule(c)) {
+        if (!pairs.check(index, rule.getRule(c))) return false;
+        Index nextIndex = pairs.next(index, rule.getRule(c).size());
+        return recursive(pairs, rule, nextIndex);
+    }
+
+    for (int l = 1; l <= 3; l++) {
+        string next = pairs.subStr(index, l);
+        if (next == "") continue;
+        rule.add(c, next);
+        Index nextIndex = pairs.next(index, l);
+        bool ok = recursive(pairs, rule, nextIndex);
+        if (ok) return true;
+        rule.remove(c, next);
+    }
+
     return false;
+
 }
 
 int main() {
     int k, n;
     cin >> k >> n;
 
-    vector<Word> words(n);
-    rep(i, n) cin >> words[i];
+    Pairs pairs(n);
+    for (Pair &pair: pairs.pairs) cin >> pair.number >> pair.word;
+    Rule rule;
+    bool ok = recursive(pairs, rule, {0, 0, 0});
+    assert(ok);
 
-    map<char, string> dict;
-
-    bool b = rec(dict, words, 0, 0, 0);
-    assert(b);
-
-    for (char c = '1'; c <= '0' + k; c++) {
-        cout << dict[c] << endl;
+    for (int i = 1; i <= k; i++) {
+        char c = '0' + i;
+        cout << rule.getRule(c) << endl;
     }
 
 }
