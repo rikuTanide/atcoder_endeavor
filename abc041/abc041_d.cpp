b3d41015cc11d7e11e118a7615bc3ca93504d58c
riku <riku@tanide.net>
2020-08-26 01:07:57 +0900

    【TLE】[AtCoder Beginner Contest 041]D - 徒競走

--- abc041/abc041_d.cpp
+++ abc041/abc041_d.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
-
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -8,6 +9,7 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
@@ -17,8 +19,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -31,8 +33,6 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-typedef pair<ll, ll> P;
-
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ofstream outfile("log.txt");
@@ -42,31 +42,118 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-// https://atcoder.jp/contests/abc041/submissions/me
+const int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
+
+int bit_rev(int n, int st, int i) {
+    assert((st >> i) & 1);
+    return st - (1 << i);
+}
+
+mint recursive(int n, vector<vector<int>> &g, int st) {
+    if (__builtin_popcount(st) == 1) return 1;
+    mint ans = 0;
+
+    vector<bool> is_terminate(n, true);
+    rep(i, n) {
+        if (!((st >> i) & 1)) continue;
+        for (int to : g[i]) {
+            if (!((st >> to) & 1)) continue;
+            is_terminate[i] = false;
+        }
+    }
+
+    rep(i, n) {
+        if ((st >> i) & 1) {
+            if (is_terminate[i]) {
+                int next_st = bit_rev(n, st, i);
+                mint now = recursive(n, g, next_st);
+                ans += now;
+            }
+        }
+    }
+    return ans;
+}
 
 int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<bool>> edges(n, vector<bool>(n, false));
+    vector<P> v(m);
+    for (P &p:v) cin >> p.first >> p.second, p.first--, p.second--;
+    vector<vector<int>> g(n);
+    for (P p : v) g[p.first].push_back(p.second);
 
 
-    rep(i, m) {
-        int x, y;
-        cin >> x >> y;
-        x--;
-        y--;
-        edges[y][x] = true;
-    }
-
-    vector<ll> dp(1 << n);
-    dp[0] = 1;
-    rep(i, 1 << n) {
-        rep(j, n) if (!(i & (1 << j))) {
-                bool ok = true;
-                rep(k, n) if (i & (1 << k)) if (edges[k][j]) ok = false;
-                if (ok) dp[i + (1 << j)] += dp[i];
-            }
-    }
-    cout << dp[(1 << n) - 1] << endl;
+    mint ans = recursive(n, g, (1 << n) - 1);
+    cout << ans << endl;
 }
