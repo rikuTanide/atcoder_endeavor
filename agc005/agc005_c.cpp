a4c0d2c0cbad600b2bb5f6e72494ff05ea3b1b70
riku <riku@tanide.net>
2020-08-26 19:47:00 +0900

    【AC】[AtCoder Grand Contest 005]C - Tree Restoring

--- agc005/agc005_c.cpp
+++ agc005/agc005_c.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
-
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -8,6 +9,7 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
@@ -17,8 +19,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -31,8 +33,6 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-typedef pair<ll, ll> P;
-
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ofstream outfile("log.txt");
@@ -42,34 +42,42 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
+const string IMPOSSIBLE = "Impossible";
+const string POSSIBLE = "Possible";
+
 int main() {
     int n;
     cin >> n;
+    vector<int> v(n);
+    rep(i, n) cin >> v[i];
 
-    vector<int> numbers(n);
-    rep(i, n) cin >> numbers[i];
-
-    int ma = *max_element(numbers.begin(), numbers.end());
-
-    multiset<int> ps;
-    for (int i : numbers) ps.insert(i);
-
-    rep(i, ma + 1) {
-        int j = max(i, ma - i);
-        auto it = ps.find(j);
-        if (it == ps.end()) {
-            cout << "Impossible" << endl;
-            ret();
-        }
-        ps.erase(it);
-    }
-
-    for (int i : ps) {
-        int b = ma % 2 == 0 ? ma / 2 + 1 : (ma + 1) / 2 + 1;
-        if (i >= b) continue;
-        cout << "Impossible" << endl;
+    int ma = *max_element(v.begin(), v.end());
+    int ma_count = count(v.begin(), v.end(), ma);
+    if (ma_count < 2) {
+        cout << IMPOSSIBLE << endl;
         ret();
     }
-    cout << "Possible" << endl;
 
+    vector<int> t(ma + 1, 0);
+    for (int i : v) t[i]++;
+
+    set<int> enable;
+    for (int i = 0; i <= ma; i++) {
+        int l = max(i, ma - i);
+        t[l]--;
+        enable.insert(l + 1);
+    }
+
+    rep(i, ma + 1) {
+        if (t[i] < 0) {
+            cout << IMPOSSIBLE << endl;
+            ret();
+        }
+        if (t[i] == 0) continue;
+        if (contain(enable, i)) continue;
+        cout << IMPOSSIBLE << endl;
+        ret();
+    }
+
+    cout << POSSIBLE << endl;
 }
