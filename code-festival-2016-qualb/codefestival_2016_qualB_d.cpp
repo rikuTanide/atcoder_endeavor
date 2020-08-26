0e2baf2c5c6e5d92a6a50ca817acf8062b077ba5
riku <riku@tanide.net>
2020-08-26 21:36:01 +0900

    【AC】[CODE FESTIVAL 2016 qual B]D - Greedy customers

--- code-festival-2016-qualb/codefestival_2016_qualB_d.cpp
+++ code-festival-2016-qualb/codefestival_2016_qualB_d.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
-
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -8,6 +9,7 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
@@ -17,8 +19,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -31,8 +33,6 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-typedef pair<ll, ll> P;
-
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ofstream outfile("log.txt");
@@ -42,32 +42,26 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-
 int main() {
     int n;
     cin >> n;
-    vector<ll> numbers(n);
-    rep(i, n) cin >> numbers[i];
+    vector<ll> v(n);
+    rep(i, n) cin >> v[i];
 
-    vector<ll> ps(n);
-    {
-        int t = 2;
-        ps[0] = 1;
-        for (int i = 1; i < n; i++) {
-            ps[i] = t;
-            if (numbers[i] == t) {
-                t++;
-            }
+    int mi = 0;
+    ll ans = 0;
+    rep(i, n) {
+        ll t = v[i];
+        if (i == 0) {
+            mi = 1;
+            ans += t - 1;
+        } else if (t <= mi) continue;
+        else if (t == mi + 1) {
+            mi = mi + 1;
+        } else {
+            ans += ((t - 1) / (mi + 1));
         }
     }
-
-    vector<ll> ans_list(n);
-    rep(i, n) {
-        if (numbers[i] % ps[i] == 0) ans_list[i] = numbers[i] / ps[i] - 1;
-        else ans_list[i] = numbers[i] / ps[i];
-    }
-
-    ll ans = accumulate(ans_list.begin(), ans_list.end(), 0ll);
     cout << ans << endl;
 
 }
