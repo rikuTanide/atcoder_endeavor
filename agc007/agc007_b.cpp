b8bf4811741f28d678ca738dc6a8a79d0b05857e
riku <riku@tanide.net>
2020-08-28 20:12:32 +0900

    【AC】[AtCoder Grand Contest 007]B - Construct Sequences

--- agc007/agc007_b.cpp
+++ agc007/agc007_b.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
-
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -8,6 +9,7 @@ typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
@@ -17,8 +19,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -31,8 +33,6 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-typedef pair<ll, ll> P;
-
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 //ofstream outfile("log.txt");
@@ -42,31 +42,23 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-
 int main() {
     int n;
     cin >> n;
-    vector<ll> numbers(n);
-    rep(i, n) cin >> numbers[i];
-    vector<ll> indexes(n);
-    rep(i, n) indexes[numbers[i] - 1] = i;
-    vector<ll> as(n), bs(n);
-    rep(i, n) {
-        ll r = indexes[i];
-        ll a = 30000 * (i + 1);
-        ll b = 30000 * (n - (i + 1)) + r + 1;
-        as[i] = a;
-        bs[i] = b;
-    }
+    vector<ll> v(n);
+    rep(i, n) cin >> v[i], v[i]--;
 
-    rep (i, n) {
-        cout << as[i];
-        if (i != n - 1) cout << ' ';
+    vector<ll> a(n), b(n);
+    for (ll t = 0; t < n; t++) a[t] = (t + 1) * n;
+    for (ll t = 0; t < n; t++) b[t] = n * n - t * n;
+
+    rep(i, n) {
+        a[v[i]] += i;
     }
+    for (ll l : a) cout << l << ' ';
     cout << endl;
-    rep (i, n) {
-        cout << bs[i];
-        if (i != n - 1) cout << ' ';
-    }
+    for (ll l : b) cout << l << ' ';
+    cout << endl;
+
     cout << endl;
 }
