3a3bd166b2cb7c9985dc2cb678d21bf03fbbfb40
riku <riku@tanide.net>
2020-08-29 17:29:47 +0900

    【WA】[AtCoder Grand Contest 010]B - Boxes

--- agc010/agc010_b.cpp
+++ agc010/agc010_b.cpp
@@ -1,24 +1,19 @@
 #include <bits/stdc++.h>
 
-
-using namespace std;
-
 const double PI = 3.14159265358979323846;
+using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<double, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-double equal(double a, double b) {
-    return fabs(a - b) < DBL_EPSILON;
-}
-
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<ll> &o) {
+    ll a;
     in >> a;
     o.insert(a);
     return in;
@@ -31,58 +26,42 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-typedef pair<ll, double> P;
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-//const ll mod = 1e10;
-typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
+//const ll mod = 1e10;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
 int main() {
-
-//    ifstream infile("C:\\Users\\riku\\Downloads\\in1.txt");
-
-    ll n;
+    int n;
     cin >> n;
-    vector<ll> numbers(n);
-    rep(i, n) cin >> numbers[i];
-    ll sum = accumulate(numbers.begin(), numbers.end(), 0ll);
-    vector<ll> cs(n, 0), rt(n, 0);
-    ll one = n * (n + 1) / 2;
-    if (sum % one != 0) {
+    vector<ll> v(n);
+    rep(i, n)cin >> v[i];
+
+    ll par = n * (n + 1) / 2;
+    ll all = accumulate(v.begin(), v.end(), 0ll);
+    if (all % par != 0) {
         cout << "NO" << endl;
         ret();
     }
-    ll count = sum / one;
-    rep(i, n) {
-        ll prev = (i + n - 1) % n;
-        ll s = numbers[prev] - numbers[i];
-        if ((s + count) % n != 0) {
-            cout << "NO" << endl;
-            ret();
-        }
-        if ((s + count) / n < 0) {
-            cout << "NO" << endl;
-            ret();
-        }
 
-        cs[i] = (s + count) / n;
+    ll turn = all / par;
+
+
+    vector<ll> diff(n);
+    rep(i, n) diff[i] = v[(i + 1) % n] - v[i] - turn;
+
+    for (ll g : diff) {
+        if (g <= 0 && (-g) % n == 0) continue;
+        cout << "NO" << endl;
+        ret();
     }
 
-    rep(i, n) rt[0] += cs[i] * ((n - i) % n + 1);
-    rep(i, n) if (i > 0) rt[i] = rt[i - 1] - cs[i] * n + count;
-
-    rep(i, n) {
-        if (rt[i] != numbers[i]) {
-            cout << "NO" << endl;
-            ret();
-        }
-    }
     cout << "YES" << endl;
+
 }
-
-
