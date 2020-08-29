84ace8d4e6bae1de0ec4fa08c2859d6b453749bd
riku <riku@tanide.net>
2020-08-29 16:39:08 +0900

    【AC】[AtCoder Grand Contest 009]B - Tournament

--- agc009/agc009_b.cpp
+++ agc009/agc009_b.cpp
@@ -1,24 +1,19 @@
 #include <bits/stdc++.h>
 
-
-using namespace std;
-
 const double PI = 3.14159265358979323846;
+using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<double, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-double equal(double a, double b) {
-    return fabs(a - b) < DBL_EPSILON;
-}
-
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<ll> &o) {
+    ll a;
     in >> a;
     o.insert(a);
     return in;
@@ -31,37 +26,47 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-typedef pair<ll, double> P;
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+
 //const ll mod = 1e10;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
-typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-// https://tokumini.hatenablog.com/entry/2018/07/23/182500
-
-ll dfs(int to, vector<vector<int>> &loser) {
-    if (loser[to].empty()) return 0;
-    vector<int> nums;
-    for (int l : loser[to]) nums.push_back(dfs(l, loser));
-    sort(nums.rbegin(), nums.rend());
-    rep(i, nums.size()) nums[i] += (i + 1);
-    return *max_element(nums.begin(), nums.end());
-
+int rec(int i, vector<vector<int>> &i_win) {
+    if (i_win[i].empty()) return 0;
+    vector<int> d;
+    for (int to : i_win[i]) {
+        int now = rec(to, i_win);
+        d.push_back(now);
+    }
+    sort(d.begin(), d.end());
+    rep(j, d.size()) {
+        d[j] = d[j] + d.size() - j;
+    }
+    int ans = *max_element(d.begin(), d.end());
+    return ans;
 }
 
 int main() {
+
     int n;
     cin >> n;
-    vector<int> a(n);
-    rep(i, n) if (i != 0) cin >> a[i], a[i]--;
-    vector<vector<int>> loser(n);
-    rep(i, n) if (i != 0) loser[a[i]].push_back(i);
 
-    cout << dfs(0, loser) << endl;
+    vector<vector<int>> i_win(n);
 
+    rep(i, n - 1) {
+        int a;
+        cin >> a;
+        a--;
+        i_win[a].push_back(i + 1);
+    }
+
+    int ans = rec(0, i_win);
+    cout << ans << endl;
 }
