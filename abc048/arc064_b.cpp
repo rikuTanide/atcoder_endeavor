bbd3996ce73f3290765d30629689498e50fb5db6
riku <riku@tanide.net>
2020-08-29 14:14:09 +0900

    【AC】[AtCoder Beginner Contest 048]D - An Ordinary Game

--- abc048/arc064_b.cpp
+++ abc048/arc064_b.cpp
@@ -1,24 +1,19 @@
 #include <bits/stdc++.h>
 
-
-using namespace std;
-
 const double PI = 3.14159265358979323846;
+using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-double equal(double a, double b) {
-    return fabs(a - b) < DBL_EPSILON;
-}
-
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<ll> &o) {
+    ll a;
     in >> a;
     o.insert(a);
     return in;
@@ -31,29 +26,21 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-typedef pair<ll, ll> P;
-
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+
 //const ll mod = 1e10;
-
-typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
-
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
 int main() {
     string s;
     cin >> s;
-
-    bool rt = s.front() == s.back();
-    bool gs = s.size() % 2 == 0;
-
-    if (rt ^ gs) {
-        cout << "Second" << endl;
-    } else {
-        cout << "First" << endl;
-    }
-
+    bool od = s.size() % 2 == 0;
+    bool mt = s.front() == s.back();
+    bool ans = od ^mt;
+    cout << (ans ? "Second" : "First") << endl;
 }
