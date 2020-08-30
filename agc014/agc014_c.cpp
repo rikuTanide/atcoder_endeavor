e55aabbcf7d90babd3a6802f21d2835e9c9cc7f9
riku <riku@tanide.net>
2020-08-30 22:07:36 +0900

    【AC】[AtCoder Grand Contest 014]C - Closed Rooms

--- agc014/agc014_c.cpp
+++ agc014/agc014_c.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
-
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,7 +8,7 @@ const double PI = 3.14159265358979323846;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
@@ -17,8 +18,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -31,53 +32,42 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-typedef pair<ll, ll> P;
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-P get_start(vector<vector<char>> &board, int h, int w) {
-    rep(y, h) rep(x, w) {
-            if (board[y][x] == 'S') return P(y, x);
-        }
-    __throw_runtime_error("");
-}
-
-ll check(ll y, ll x, ll h, ll w, ll k) {
-    ll l = (x + k - 1) / k;
-    ll u = (y + k - 1) / k;
-    ll r = (w - x - 1 + k - 1) / k;
-    ll d = (h - y - 1 + k - 1) / k;
-
-    return min({l, u, r, d});
-
-}
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-    ll h, w, k;
+    int h, w, k;
     cin >> h >> w >> k;
-
 
     vector<vector<char>> board(h, vector<char>(w, ' '));
     rep(y, h) rep(x, w) cin >> board[y][x];
     vector<vector<int>> distances(h, vector<int>(w, INT_MAX / 10));
 
-    P s = get_start(board, h, w);
+    P start = [&] {
+        rep(y, h) {
+            rep(x, w) {
+                if (board[y][x] == 'S') return P(y, x);
+            }
+        }
+        __throw_runtime_error("konaide");
+    }();
 
-    distances[s.first][s.second] = 0;
-
+    distances[start.first][start.second] = 0;
     struct Point {
         ll y, x, cost;
     };
 
     queue<Point> q;
-    q.push({s.first, s.second, 0});
+    q.push({start.first, start.second, 0});
 
     auto to_id = [&](int y, int x) {
         return y * w + x;
     };
 
     struct Direction {
-        int x, y;
+        int y, x;
     };
-
     vector<Direction> directions = {
             {0,  1},
             {1,  0},
@@ -97,7 +87,6 @@ int main() {
         return true;
     };
 
-
     while (!q.empty()) {
         Point p = q.front();
         q.pop();
@@ -112,12 +101,26 @@ int main() {
         }
     }
 
-    ll ans = INF;
-    rep(y, h) rep(x, w) {
+    auto calc = [&](int y, int x) -> int {
+        auto p = [&](int a) -> int {
+            return (a + k - 1) / k;
+        };
+        int l = p(x);
+        int r = p(w - x - 1);
+        int u = p(y);
+        int d = p(h - y - 1);
+
+
+        return min({l, r, u, d});
+    };
+
+    int ans = INT_MAX;
+    rep(y, h) {
+        rep(x, w) {
             if (distances[y][x] > k) continue;
-            ll now = check(y, x, h, w, k) + 1;
+            int now = calc(y, x);
             cmin(ans, now);
         }
-    cout << ans << endl;
-
+    }
+    cout << ans + 1 << endl;
 }
