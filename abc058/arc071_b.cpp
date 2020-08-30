4ab0a52d95a1fe8c4c4075dc627533d1364d434f
riku <riku@tanide.net>
2020-08-30 17:33:02 +0900

    【AC】[AtCoder Beginner Contest 058]D - ###

--- abc058/arc071_b.cpp
+++ abc058/arc071_b.cpp
@@ -1,50 +1,13 @@
 #include <bits/stdc++.h>
-
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
-
-const double PI = 3.14159265358979323846;
-typedef long long ll;
-const double EPS = 1e-9;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
-const ll INF = 10e17;
-#define cmin(x, y) x = min(x, y)
-#define cmax(x, y) x = max(x, y)
-#define ret() return 0;
-
-double equal(double a, double b) {
-    return fabs(a - b) < DBL_EPSILON;
-}
-
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
-    in >> a;
-    o.insert(a);
-    return in;
-}
-
-std::istream &operator>>(std::istream &in, queue<int> &o) {
-    ll a;
-    in >> a;
-    o.push(a);
-    return in;
-}
-
-typedef pair<ll, ll> P;
-
-bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
-
-//ofstream outfile("log.txt");
-//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
-// std::cout << std::bitset<8>(9);
-//const ll mod = 1e10;
-typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 const int mod = 1000000007;
 
 struct mint {
-    ll x; // typedef long long ll;
-    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+    long long x; // typedef long long ll;
+    mint(long long x = 0) : x((x % mod + mod) % mod) {}
 
     mint &operator+=(const mint a) {
         if ((x += a.x) >= mod) x -= mod;
@@ -76,7 +39,7 @@ struct mint {
         return res *= a;
     }
 
-    mint pow(ll t) const {
+    mint pow(long long t) const {
         if (!t) return 1;
         mint a = pow(t >> 1);
         a *= a;
@@ -99,7 +62,7 @@ struct mint {
     }
 
     friend std::istream &operator>>(std::istream &in, mint &o) {
-        ll a;
+        long long a;
         in >> a;
         o = a;
         return in;
@@ -112,29 +75,102 @@ struct mint {
 
 };
 
-mint calc(int n, vector<ll> &xs) {
-    mint xa = 0;
-    for (int k = 1; k <= n; k++) {
-        ll l = xs[k - 1] * (k - 1);
-        ll r = xs[k - 1] * (n - k);
+const double PI = 3.14159265358979323846;
+typedef mint ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
 
-        mint now = l - r;
-        xa += now;
-    }
-    return xa;
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
 }
 
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+//ofstream outfile("log.txt");
+//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
+// std::cout << std::bitset<8>(9);
+//const ll mod = 1e10;
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return sums.back();
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void build() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
+
 int main() {
-    int n, m;
-    cin >> n >> m;
+    int h, w;
+    cin >> h >> w;
+    vector<ll> horizontal(h), vertical(w);
 
-    vector<ll> xs(n), ys(m);
-    rep(i, n) cin >> xs[i];
-    rep(i, m) cin >> ys[i];
+    rep(y, h) cin >> horizontal[y];
+    rep(x, w) cin >> vertical[x];
 
-    mint x = calc(n, xs);
-    mint y = calc(m, ys);
-    mint ans = x * y;
+    CumulativeSum hori_sum(h), ver_sum(w);
+    rep(y, h) hori_sum.set(y, horizontal[y]);
+    rep(x, w) ver_sum.set(x, vertical[x]);
+    hori_sum.build();
+    ver_sum.build();
+
+    ll s = 0, t = 0;
+
+    for (int y = h - 1; y > 0; y--) {
+        ll a = horizontal[y] * y;
+        ll b = hori_sum.getSum(y - 1);
+        ll n = a - b;
+        s += n;
+    }
+
+    for (int x = w - 1; x > 0; x--) {
+        ll a = vertical[x] * x;
+        ll b = ver_sum.getSum(x - 1);
+        ll n = a - b;
+        t += n;
+    }
+
+    ll ans = s * t;
     cout << ans << endl;
 
 }
