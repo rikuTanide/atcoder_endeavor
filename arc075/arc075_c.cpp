d430d13b1ccc895f78f5fa74d4e78523f6d438ed
riku <riku@tanide.net>
2020-09-01 02:11:00 +0900

    【TLE】[AtCoder Regular Contest 075]E - Meaningful Mean

--- arc075/arc075_c.cpp
+++ arc075/arc075_c.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
-
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -7,7 +8,7 @@ const double PI = 3.14159265358979323846;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
@@ -17,8 +18,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -31,7 +32,9 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-typedef pair<ll, ll> P;
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 class CumulativeSum {
     vector<ll> numbers;
@@ -57,7 +60,7 @@ public:
         return getSum(end) - getSum(start - 1);
     }
 
-    void calculate() {
+    void build() {
         for (int i = 0; i < numbers.size(); i++) {
             sums[i] = getSum(i - 1) + numbers[i];
         }
@@ -65,136 +68,26 @@ public:
 
 };
 
-
-class Conv {
-    ll cursor = 0;
-    map<ll, ll> to_short; // <original, small >
-    map<ll, ll> to_long; // <small, original>
-    std::set<ll> tmp;
-
-
-public:
-    void set(ll original) {
-        if (to_short.find(original) != to_short.end()) {
-            return;
-        }
-        to_long[cursor] = original;
-        to_short[original] = cursor;
-        cursor++;
-    }
-
-    ll revert(ll after) {
-        assert(to_long.find(after) != to_long.end());
-        return to_long[after];
-    }
-
-    ll convert(ll original) {
-        assert(to_short.find(original) != to_short.end());
-        return to_short[original];
-    }
-
-    ll next() {
-        return cursor;
-    }
-
-    // 前計算省略のため
-    void cache(ll t) {
-        tmp.insert(t);
-    }
-
-    void build() {
-        for (ll t : tmp) {
-            set(t);
-        }
-    }
-
-};
-
-template<class T, class F>
-struct SegmentTree {
-    F f;
-    T ti;
-    vector<T> dat;
-    int sz;
-
-    SegmentTree(const F &f, const T &ti) : f(f), ti(ti) {}
-
-    void build(const vector<T> &v) {
-        assert(v.size());
-        sz = 1;
-        while (sz < v.size())sz <<= 1;
-        dat.resize(sz << 1, ti);
-        for (int i = 0; i < v.size(); i++)dat[sz - 1 + i] = v[i];
-        for (int i = sz - 2; i >= 0; i--)dat[i] = f(dat[i * 2 + 1], dat[i * 2 + 2]);
-    }
-
-    inline void update(int k, T x) {
-        k += sz - 1;
-        dat[k] = x;
-        while (k) {
-            k = (k - 1) / 2;
-            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
-        }
-    }
-
-    inline void add(int k, int x) {
-        k += sz - 1;
-        dat[k] = f(dat[k], x);
-        while (k) {
-            k = (k - 1) / 2;
-            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
-        }
-    }
-
-    inline T query(int a, int b) {
-        return query(a, b, 0, 0, sz);
-    }
-
-    T query(int a, int b, int k, int l, int r) {
-        if (r <= a || b <= l)return ti;
-        if (a <= l && r <= b)return dat[k];
-        return f(query(a, b, k * 2 + 1, l, (l + r) / 2), query(a, b, k * 2 + 2, (l + r) / 2, r));
-    }
-};
-
-
 int main() {
-    ll n, k;
+    int n;
+    ll k;
     cin >> n >> k;
 
-    vector<ll> numbers(n);
-    rep(i, n) cin >> numbers[i], numbers[i] -= k;
+    vector<ll> v(n);
+    rep(i, n) cin >> v[i], v[i] -= k;
 
     CumulativeSum cs(n);
-    rep(i, n) cs.set(i, numbers[i]);
-    cs.calculate();
-
-    Conv conv;
-    conv.cache(0);
-    rep(i, n) conv.cache(cs.getSum(i));
-    conv.build();
-
-    vector<ll> imos(n + 5, 0);
-
-    auto f = [](ll i, ll j) { return i + j; };
-    SegmentTree<ll, decltype(f)> segmentTree(f, 0);
-    segmentTree.build(imos);
+    rep(i, n) cs.set(i, v[i]);
+    cs.build();
 
     ll ans = 0;
-
-    vector<ll> bs(n);
-    rep(i, n) bs[i] = conv.convert(cs.getSum(i));
-    bs.insert(bs.begin(), conv.convert(0));
-
-    rep(i, n + 1) {
-        ll cv = bs[i];
-        ll t = segmentTree.query(0, cv + 1);
-        ans += t;
-
-
-        ll from = segmentTree.query(cv, cv + 1);
-        segmentTree.update(cv, from + 1);
+    rep(i, n) {
+        ll a = v[i];
+        if (a >= 0) ans++;
+        for (int j = i + 1; j < n; j++) {
+            ll b = cs.getSectionSum(i + 1, j);
+            if (a + b >= 0) ans++;
+        }
     }
     cout << ans << endl;
-
 }
