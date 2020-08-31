26dc2087795c97e24720be749da330d8a5a49b11
riku <riku@tanide.net>
2020-09-01 02:39:02 +0900

    【AC】[AtCoder Regular Contest 075]E - Meaningful Mean

--- arc075/arc075_c.cpp
+++ arc075/arc075_c.cpp
@@ -68,6 +68,98 @@ public:
 
 };
 
+class Conv {
+    ll cursor = 0;
+    map<ll, ll> to_short; // <original, small >
+    map<ll, ll> to_long; // <small, original>
+    std::set<ll> tmp;
+
+
+public:
+    void set(ll original) {
+        if (to_short.find(original) != to_short.end()) {
+            return;
+        }
+        to_long[cursor] = original;
+        to_short[original] = cursor;
+        cursor++;
+    }
+
+    ll revert(ll after) {
+        assert(to_long.find(after) != to_long.end());
+        return to_long[after];
+    }
+
+    ll convert(ll original) {
+        assert(to_short.find(original) != to_short.end());
+        return to_short[original];
+    }
+
+    ll next() {
+        return cursor;
+    }
+
+    // 前計算省略のため
+    void cache(ll t) {
+        tmp.insert(t);
+    }
+
+    void build() {
+        for (ll t : tmp) {
+            set(t);
+        }
+    }
+
+};
+
+
+template<class T, class F>
+struct SegmentTree {
+    F f;
+    T ti;
+    vector<T> dat;
+    int sz;
+
+    SegmentTree(const F &f, const T &ti) : f(f), ti(ti) {}
+
+    void build(const vector<T> &v) {
+        assert(v.size());
+        sz = 1;
+        while (sz < v.size())sz <<= 1;
+        dat.resize(sz << 1, ti);
+        for (int i = 0; i < v.size(); i++)dat[sz - 1 + i] = v[i];
+        for (int i = sz - 2; i >= 0; i--)dat[i] = f(dat[i * 2 + 1], dat[i * 2 + 2]);
+    }
+
+    inline void update(int k, T x) {
+        k += sz - 1;
+        dat[k] = x;
+        while (k) {
+            k = (k - 1) / 2;
+            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
+        }
+    }
+
+    inline void add(int k, int x) {
+        k += sz - 1;
+        dat[k] = f(dat[k], x);
+        while (k) {
+            k = (k - 1) / 2;
+            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
+        }
+    }
+
+    inline T query(int a, int b) {
+        return query(a, b, 0, 0, sz);
+    }
+
+    T query(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l)return ti;
+        if (a <= l && r <= b)return dat[k];
+        return f(query(a, b, k * 2 + 1, l, (l + r) / 2), query(a, b, k * 2 + 2, (l + r) / 2, r));
+    }
+};
+
 int main() {
     int n;
     ll k;
@@ -80,14 +172,36 @@ int main() {
     rep(i, n) cs.set(i, v[i]);
     cs.build();
 
+    Conv conv;
+    conv.cache(0);
+    rep(i, n) conv.cache(cs.getSum(i));
+    conv.build();
+
+    vector<ll> imos(conv.next(), 0);
+    rep(i, n) imos[conv.convert(cs.getSum(i))]++;
+
+    auto f = [](ll i, ll j) { return i + j; };
+    SegmentTree<ll, decltype(f)> segmentTree(f, 0);
+    segmentTree.build(imos);
+
+
     ll ans = 0;
     rep(i, n) {
-        ll a = v[i];
-        if (a >= 0) ans++;
-        for (int j = i + 1; j < n; j++) {
-            ll b = cs.getSectionSum(i + 1, j);
-            if (a + b >= 0) ans++;
+        ll prev = cs.getSum(i - 1);
+        ll t = conv.convert(prev);
+        {
+            ll now = segmentTree.query(t, conv.next());
+            ans += now;
         }
+        {
+            ll now = cs.getSum(i);
+            ll s = conv.convert(now);
+            ll from = segmentTree.query(s, s + 1);
+            segmentTree.update(s, from - 1);
+        }
+
     }
     cout << ans << endl;
+
+
 }
