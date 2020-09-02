724c815b9c2927ee84ab9386437599c399a6ce8d
riku <riku@tanide.net>
2020-09-02 14:40:58 +0900

    【RE】[AtCoder Beginner Contest 086]D - Checker

--- abc086/arc089_b.cpp
+++ abc086/arc089_b.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
 const double PI = 3.14159265358979323846;
 typedef long long ll;
-typedef pair<ll, ll> P;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
@@ -17,8 +18,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -31,15 +32,10 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
-struct Believe {
-    ll x, y;
-    char c;
-};
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-std::istream &operator>>(std::istream &in, Believe &o) {
-    cin >> o.x >> o.y >> o.c;
-    return in;
-}
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
 
 class MatrixSum {
     vector<vector<ll>> sum;
@@ -71,109 +67,77 @@ public:
     }
 
     ll getSum(ll xs, ll ys, ll xe, ll ye) {
+        assert(xs < xe);
+        assert(ys < ye);
         return get(xe, ye) - get(xs - 1, ye) - get(xe, ys - 1) + get(xs - 1, ys - 1);
     }
 
 };
 
-struct Rect {
-    ll y1, x1, y2, x2;
+struct Hope {
+    int x, y;
+    char c;
+
+    Hope norm(int k) {
+        int nx = x % (2 * k);
+        int ny = y % (2 * k);
+
+        if (nx >= k && ny >= k) {
+            return Hope{nx % k, ny % k, c,};
+        }
+        if (ny >= k) {
+            return Hope{nx + k, ny % k, c,};
+        }
+        return Hope{nx, ny, c,};
+    }
+
+    Hope up_left(int k) {
+        return Hope{x - k, y + k, c};
+    }
+
+    Hope right(int k) {
+        return Hope{x + 2 * k, y, c};
+    }
+
+    Hope up_right(int k) {
+        return Hope{x + k, y + k, c};
+    }
+
 };
-
-vector<Rect> get_segments(Rect rect, ll k) {
-    if (rect.y2 < k && rect.x2 < k) return {rect};
-    if (rect.y2 >= k) {
-        ll w = rect.x2 - rect.x1 + 1;
-        ll h = rect.y2 - rect.y1 + 1;
-        Rect old = Rect{
-                rect.y1, rect.x1, k - 1, rect.x1 + w - 1,
-        };
-        ll oh = old.y2 - old.y1 + 1;
-        ll nh = h - oh;
-        Rect add = Rect{
-                0, old.x2 + 1, nh - 1, old.x2 + 1 + w - 1,
-        };
-
-        vector<Rect> v;
-        for (Rect r : get_segments(old, k)) v.push_back(r);
-        for (Rect r : get_segments(add, k)) v.push_back(r);
-        return v;
-    }
-    if (rect.x1 >= k * 2) {
-        Rect next = Rect{
-                rect.y1, rect.x1 - (k * 2), rect.y2, rect.x2 - (k * 2),
-        };
-        return {next};
-    }
-    if (rect.x2 >= k * 2) {
-        ll w = rect.x2 - rect.x1 + 1;
-        ll h = rect.y2 - rect.y1 + 1;
-        Rect old = Rect{
-                rect.y1, rect.x1, rect.y2, 2 * k - 1,
-        };
-        ll ow = old.x2 - old.x1 + 1;
-        ll nw = w - ow;
-        Rect add = Rect{
-                rect.y1, 0, rect.y2, nw - 1,
-        };
-        return {old, add};
-    }
-    return {rect};
-}
 
 
 int main() {
-    ll n, k;
+    int n, k;
     cin >> n >> k;
+    vector<Hope> hopes(n);
+    for (Hope &hope: hopes)cin >> hope.x >> hope.y >> hope.c;
 
-    vector<Believe> believes(n);
-    rep(i, n) cin >> believes[i];
-    rep(i, n) believes[i].x %= (k * 2);
-    rep(i, n) believes[i].y %= (k * 2);
-    rep(i, n) {
-        Believe &b = believes[i];
-        if (b.x < k && b.y < k) continue;
-        else if (b.x >= k && b.y >= k) b.x -= k, b.y -= k;
-        else if (b.y >= k) b.x += k, b.y -= k;
+    rep(i, n) hopes[i] = hopes[i].norm(k);
+
+    {
+        vector<Hope> dummy;
+        for (Hope hope: hopes) dummy.push_back(hope);
+//        for (Hope hope: hopes) dummy.push_back(hope.up_left(k));
+        for (Hope hope: hopes) dummy.push_back(hope.right(k));
+        for (Hope hope: hopes) dummy.push_back(hope.up_right(k));
+        hopes = dummy;
     }
 
-    MatrixSum
-            blacks(k * 2, k),
-            whites(k * 2, k);
-
-    rep(i, n) {
-        Believe b = believes[i];
-        if (b.c == 'W') whites.add(b.x, b.y);
-        else blacks.add(b.x, b.y);
+    MatrixSum blacks(4 * k, 2 * k), whites(4 * k, 2 * k);
+    for (Hope hope: hopes) {
+        if (hope.c == 'B') blacks.add(hope.x, hope.y);
+        if (hope.c == 'W') whites.add(hope.x, hope.y);
     }
     blacks.setUp();
     whites.setUp();
-
-    ll b_all = 0;
-    for (Believe b : believes) if (b.c == 'B') b_all++;
-
-    auto get_count = [&](MatrixSum &ms, vector<Rect> &rect) {
-        ll sum = 0;
-        for (Rect &r : rect) {
-            sum += ms.getSum(r.x1, r.y1, r.x2, r.y2);
-        }
-        return sum;
-    };
-
-
-    ll ans = 0;
-    for (ll y = 0; y < k; y++) {
-        for (ll x = 0; x < k * 2; x++) {
-            vector<Rect> segments = get_segments(Rect{y, x, y + k - 1, x + k - 1}, k);
-
-            ll w = get_count(whites, segments);
-            ll b = get_count(blacks, segments);
-
-            ll now = w + (b_all - b);
+    int ans = 0;
+    rep(x, 2 * k) {
+        rep(y, k) {
+            int black = blacks.getSum(x, y, x + k - 1, y + k - 1);
+            int white = whites.getSum(x + k, y, x + k + k - 1, y + k - 1);
+            int now = black + white;
             cmax(ans, now);
-
         }
     }
     cout << ans << endl;
 }
-
