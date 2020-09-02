a945843e685ba279c4dd246c035dbfedf71dc983
riku <riku@tanide.net>
2020-09-02 19:41:19 +0900

    【WA】[AtCoder Beginner Contest 093]D - Worst Case

--- abc093/arc094_b.cpp
+++ abc093/arc094_b.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
 const double PI = 3.14159265358979323846;
-typedef unsigned long long ll;
-typedef pair<ll, ll> P;
+typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
-const ll INF = 10e10;
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
@@ -17,8 +18,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -31,6 +32,9 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 ll solve(ll a, ll b) {
     if (a == b) return (a - 1) * 2;
     ll floor = 1, ceil = INF;
@@ -56,6 +60,3 @@ int main() {
     }
 
 }
-
-
-
