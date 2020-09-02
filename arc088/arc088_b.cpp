fe5808b893d62f150fdaae38e66399bd56588773
riku <riku@tanide.net>
2020-09-02 13:29:22 +0900

    【WA】[AtCoder Regular Contest 088]D - Wide Flip

--- arc088/arc088_b.cpp
+++ arc088/arc088_b.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
 
 using namespace std;
 
@@ -6,7 +8,7 @@ const double PI = 3.14159265358979323846;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
@@ -16,8 +18,8 @@ double equal(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -30,24 +32,22 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
 int main() {
     string s;
     cin >> s;
     int n = s.size();
 
-    if (*min_element(s.begin(), s.end()) == *max_element(s.begin(), s.end())) {
-        cout << n << endl;
-        ret();
-    }
-
-
     int ans = INT_MAX;
     rep(i, n - 1) {
         if (s[i] == s[i + 1]) continue;
-        int k = max(i + 1, n - i - 1);
-        cmin(ans, k);
+        int l = i + 1;
+        int r = n - i - 1;
+        int ma = max(l, r);
+        cmin(ans, ma);
     }
     cout << ans << endl;
-
 }
-
