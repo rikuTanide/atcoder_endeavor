6b8a0242826426e21cd1f1936cefdee68168919a
riku <riku@tanide.net>
2020-09-03 14:16:07 +0900

    【AC】[AtCoder Beginner Contest 110]D - Factorization

--- abc110/abc110_d.cpp
+++ abc110/abc110_d.cpp
@@ -1,25 +1,19 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 const double PI = 3.14159265358979323846;
+using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-typedef pair<ll, ll> P;
-//typedef pair<double, double> P;
+//typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-double equal(double a, double b) {
-    return fabs(a - b) < DBL_EPSILON;
-}
-
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<ll> &o) {
+    ll a;
     in >> a;
     o.insert(a);
     return in;
@@ -32,16 +26,16 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
+
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+
 //const ll mod = 1e10;
-
-typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
-
-
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 const int mod = 1000000007;
 
 struct mint {
@@ -131,7 +125,6 @@ struct combination {
     }
 } combination(1000000);
 
-
 map<ll, int> factorize(ll n) {
     map<ll, int> res;
 
@@ -154,11 +147,13 @@ int main() {
     ll n, m;
     cin >> n >> m;
 
-    auto fact = factorize(m);
+    auto f = factorize(m);
+
     mint ans = 1;
-    for (auto f : fact) {
-        mint now = combination(n + f.second - 1, f.second);
+    for (auto e : f) {
+        mint now = combination(n + e.second - 1, e.second);
         ans *= now;
     }
     cout << ans << endl;
+
 }
