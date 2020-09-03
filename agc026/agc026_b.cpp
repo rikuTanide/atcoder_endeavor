f5a75b08ef115089d1d4d967228fbd902c8e9341
riku <riku@tanide.net>
2020-09-03 09:56:34 +0900

    【AC】[AtCoder Grand Contest 026]B - rng_10s

--- agc026/agc026_b.cpp
+++ agc026/agc026_b.cpp
@@ -1,25 +1,19 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 const double PI = 3.14159265358979323846;
+using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
-typedef pair<double, double> P;
+typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-double equal(double a, double b) {
-    return fabs(a - b) < DBL_EPSILON;
-}
-
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<ll> &o) {
+    ll a;
     in >> a;
     o.insert(a);
     return in;
@@ -32,48 +26,40 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
+
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
+
 //const ll mod = 1e10;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
-typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-ll gcd(ll x, ll y) {
-    if (x > y) swap(x, y);
-    ll m = 1;
-    while (m != 0) {
-        m = y % x;
-        y = x;
-        x = m;
-    }
-    return y;
-}
 
 int main() {
     int t;
     cin >> t;
+    rep(_, t) {
 
-    rep(i, t) {
-        bool b = [&] {
+        bool ok = []() -> bool {
             ll a, b, c, d;
             cin >> a >> b >> c >> d;
-
             if (b > a) return false;
             if (b > d) return false;
             if (c >= b) return true;
 
-            assert(b <= a);
-            assert(b <= d);
-            assert(c < b);
+            ll g = __gcd(b, d);
+            ll ma = b - g + (a % g);
+            if (ma > c) return false;
+            return true;
 
-            ll g = gcd(b, d);
-            ll ma = (b - a % g + g - 1) / g * g + a % g - g;
-            return ma <= c;
+
         }();
-        cout << (b ? "Yes" : "No") << endl;
+        cout << (ok ? "Yes" : "No") << endl;
     }
+
 
 }
