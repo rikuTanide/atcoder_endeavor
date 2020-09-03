7a3dc1a300e625033b2a8f94c287d391e250c1c2
riku <riku@tanide.net>
2020-09-03 15:06:10 +0900

    【WA】[AtCoder Regular Contest 103]E - Tr/ee

--- arc103/arc103_c.cpp
+++ arc103/arc103_c.cpp
@@ -1,25 +1,19 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 const double PI = 3.14159265358979323846;
+using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 //#define rep(i, n) for (ll i = 0; i < (n); ++i)
-typedef pair<ll, ll> P;
-//typedef pair<double, double> P;
+//typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-double equal(double a, double b) {
-    return fabs(a - b) < DBL_EPSILON;
-}
-
-std::istream &operator>>(std::istream &in, set<string> &o) {
-    string a;
+std::istream &operator>>(std::istream &in, set<ll> &o) {
+    ll a;
     in >> a;
     o.insert(a);
     return in;
@@ -32,14 +26,16 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
     return in;
 }
 
+
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
+//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
 //ofstream outfile("log.txt");
 //outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
 // std::cout << std::bitset<8>(9);
-//const ll mod = 1e10;
 
-typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+//const ll mod = 1e10;
+//typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
 
 int main() {
     string s;
@@ -53,18 +49,15 @@ int main() {
         cout << -1 << endl;
         ret();
     }
-    for (int i = 1; i <= n - 1; i++) {
-        if (s[i - 1] != s[n - 1 - i]) {
+
+    rep(i, n - 1) {
+        if (s[i] != s[n - i - 1]) {
             cout << -1 << endl;
             ret();
         }
     }
 
-    s[n - 1] = '1';
-    int pre = n;
-    for (int i = n - 1; i >= 1; i--) {
-        cout << i << ' ' << pre << ' ' << endl;
-        if (s[i - 1] == '1') pre = i;
-    }
-
+    __throw_runtime_error("konai");
+    
+    
 }
