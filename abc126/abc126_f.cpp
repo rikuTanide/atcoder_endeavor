519d048b8ffdf4c2fefa32cdfbd60aab9c11924f
riku <riku@tanide.net>
2020-09-05 00:27:09 +0900

    【RE】[AtCoder Beginner Contest 126]F - XOR Matching

--- abc126/abc126_f.cpp
+++ abc126/abc126_f.cpp
@@ -36,18 +36,53 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
+void sample() {
+
+    rep(m, 4) {
+        vector<ll> v;
+        rep(i, 1 << m) v.push_back(i);
+        rep(i, 1 << m) v.push_back(i);
+        int n = v.size();
+        vector<int> arr(n);
+        rep(i, n) arr[i] = i;
+
+        do {
+            set<ll> xx;
+            rep(i, 1 << m) {
+                vector<int> k;
+                rep(j, n) if (v[arr[j]] == i) k.push_back(j);
+
+                ll x = 0;
+                for (int j = k.front(); j <= k.back(); j++) {
+                    x ^= v[arr[j]];
+                }
+                xx.insert(x);
+            }
+            if (xx.size() == 1 && (*xx.begin()) != 0) {
+
+                cout << *xx.begin() << ':';
+                rep(i, n) cout << v[arr[i]] << ' ';
+                cout << endl;
+
+            }
+        } while (std::next_permutation(arr.begin(), arr.end()));
+
+    }
+
+}
+
 int main() {
+//    sample();
+//    ret();
     int n;
     ll k;
     cin >> n >> k;
 
-    if (k != 0) {
+    if (k >= (1 << n)) {
         cout << -1 << endl;
         ret();
     }
 
-    rep(i, 1 << n) cout << i << ' ' << i << ' ';
-    cout << endl;
-
-
+    __throw_runtime_error("mada");
+    
 }
