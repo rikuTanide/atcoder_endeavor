7cc90e8af3202e8d49dc43df07857cc66386d396
riku <riku@tanide.net>
2020-09-05 19:56:18 +0900

    【TLE】[AtCoder Beginner Contest 128]E - Roadwork

--- abc128/abc128_e.cpp
+++ abc128/abc128_e.cpp
@@ -8,9 +8,7 @@ const double PI = 3.14159265358979323846;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
-typedef pair<double, double> P;
+typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
@@ -36,60 +34,116 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ofstream outfile("log.txt");
-//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
-// std::cout << std::bitset<8>(9);
-//const ll mod = 1e10;
-
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-struct Event {
-    ll time, type, value;
+// queueで順番に入れていく方法と
+// 事前に減産する方法がありそう。
+
+// まず、スタート地点をマイナスで表す
+//
+
+class Conv {
+    ll cursor = 0;
+    map<ll, ll> to_short; // <original, small >
+    map<ll, ll> to_long; // <small, original>
+    std::set<ll> tmp;
+
+
+public:
+    void set(ll original) {
+        if (to_short.find(original) != to_short.end()) {
+            return;
+        }
+        to_long[cursor] = original;
+        to_short[original] = cursor;
+        cursor++;
+    }
+
+    ll revert(ll after) {
+        assert(to_long.find(after) != to_long.end());
+        return to_long[after];
+    }
+
+    ll convert(ll original) {
+        assert(to_short.find(original) != to_short.end());
+        return to_short[original];
+    }
+
+    ll next() {
+        return cursor;
+    }
+
+    // 前計算省略のため
+    void cache(ll t) {
+        tmp.insert(t);
+    }
+
+    void build() {
+        for (ll t : tmp) {
+            set(t);
+        }
+    }
+
+};
+
+
+struct Construction {
+    int id;
+    ll start, end, x;
+
+    void norm() {
+        ll diff = end - start;
+        end = x - start;
+        start = end - diff;
+    }
 };
 
 int main() {
-    int n, q;
-    cin >> n >> q;
+    int n, query_count;
+    cin >> n >> query_count;
 
-    vector<Event> events;
-    rep(i, n) {
-        int s, t, x;
-        cin >> s >> t >> x;
-        Event e1 = {s - x, -1, x};
-        Event e2 = {t - x, 0, x};
-        events.push_back(e1);
-        events.push_back(e2);
+    vector<Construction> constructions(n);
+    for (Construction &construction: constructions) cin >> construction.start >> construction.end >> construction.x;
+    sort(constructions.begin(), constructions.end(), [](Construction c1, Construction c2) { return c1.x < c2.x; });
+    rep(i, n) constructions[i].id = i;
+
+    vector<ll> queries(query_count);
+    rep(i, query_count) cin >> queries[i];
+
+    rep(i, n) constructions[i].norm();
+
+    Conv conv;
+    for (Construction construction : constructions) conv.cache(construction.x);
+    for (Construction construction : constructions) conv.cache(construction.start);
+    for (Construction construction : constructions) conv.cache(construction.end);
+    for (ll l : queries) conv.cache(-l);
+    conv.build();
+
+    for (Construction &c : constructions) c.start = conv.convert(c.start);
+    for (Construction &c : constructions) c.end = conv.convert(c.end);
+    for (Construction &c : constructions) c.x = conv.convert(c.x);
+    for (ll &q : queries) q = conv.convert(-q);
+
+    vector<vector<int>> starts(conv.next()), ends(conv.next());
+    for (Construction c : constructions) starts[c.start].push_back(c.id);
+    for (Construction c : constructions) ends[c.end].push_back(c.id);
+
+    vector<int> constructors(conv.next());
+    set<int> current_constructors;
+    rep(i, conv.next()) {
+        if (current_constructors.empty()) constructors[i] = -1;
+        else constructors[i] = (*current_constructors.begin());
+        for (int c : starts[i]) current_constructors.insert(c);
+        for (int c : ends[i]) current_constructors.erase(current_constructors.find(c));
     }
 
-    rep(i, q) {
-        int d;
-        cin >> d;
-
-        Event e = {d, 1, i};
-        events.push_back(e);
+    for (ll q : queries) {
+        int c = constructors[q];
+        if (c == -1) {
+            cout << -1 << endl;
+        } else {
+            cout << conv.revert(constructions[c].x) << endl;
+        }
     }
-
-    sort(events.begin(), events.end(), [](Event &e1, Event &e2) {
-        vector<ll>
-                a = {e1.time, e1.type, e1.value},
-                b = {e2.time, e2.type, e2.value};
-
-        return a < b;
-    });
-
-    vector<ll> res(q);
-
-    multiset<ll> se;
-
-    for (auto &p : events) {
-        ll x = p.time;
-        ll t = p.type;
-        ll value = p.value;
-        if (t == -1) se.insert(value);
-        else if (t == 0) se.erase(se.lower_bound(value));
-        else res[value] = (se.empty() ? -1 : *(se.begin()));
-    }
-
-    for (auto v : res) cout << v << endl;
 
 }
