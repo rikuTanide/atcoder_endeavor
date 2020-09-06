a3bd74cee06d17c35f8eecd66c9974da547d3c8f
riku <riku@tanide.net>
2020-09-07 06:53:09 +0900

    【WA】[第一回日本最強プログラマー学生選手権-予選-]C - Cell Inversion

--- jsc2019-qual/jsc2019_qual_c.cpp
+++ jsc2019-qual/jsc2019_qual_c.cpp
@@ -8,7 +8,6 @@ const double PI = 3.14159265358979323846;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -35,12 +34,39 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ofstream outfile("log.txt");
-//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
-// std::cout << std::bitset<8>(9);
-//const ll mod = 1e10;
-
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return sums.back();
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void build() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
 
 const int mod = 1000000007;
 
@@ -114,43 +140,59 @@ struct mint {
 
 };
 
+
 int main() {
     int n;
     string s;
     cin >> n >> s;
 
-    int depth = 0;
-    mint ans = 1;
-    rep(i, 2 * n) {
-        char c = s[i];
-
-        if (c == 'W') {
-            if (depth == 0) {
-                cout << 0 << endl;
-                ret();
-            }
-            if (depth % 2 == 1) {
-                depth++;
-            } else {
-                ans *= depth;
-                depth--;
-            }
-        } else {
-            if (depth % 2 == 0) {
-                depth++;
-            } else {
-                ans *= depth;
-                depth--;
-            }
-        }
-    }
-    if(depth != 0) {
+    if (s.front() != 'B' || s.back() != 'B') {
         cout << 0 << endl;
         ret();
     }
-    rep(i, n + 1) {
+
+    auto turn = [&](char c) {
+        if (c == 'L') return 'R';
+        return 'L';
+    };
+
+    vector<char> m(2 * n);
+    m[0] = 'L';
+    rep(i, n * 2) {
         if (i == 0) continue;
-        ans *= i;
+        if (s[i] == s[i - 1]) m[i] = turn(m[i - 1]);
+        else m[i] = m[i - 1];
     }
+
+    if (count(m.begin(), m.end(), 'L') != n) {
+        cout << 0 << endl;
+        ret();
+    }
+
+    CumulativeSum cs_r(2 * n), cs_l(2 * n);
+    rep(i, 2 * n) cs_r.set(i, m[i] == 'R' ? 1 : 0);
+    rep(i, 2 * n) cs_l.set(i, m[i] == 'L' ? 1 : 0);
+    cs_r.build();
+    cs_l.build();
+
+    vector<mint> dp(2 * n);
+    rep(i, 2 * n) {
+        if (m[i] != 'L') continue;
+        ll l = cs_r.getSectionSum(i + 1, n * 2);
+        ll r = cs_l.getSectionSum(i + 1, n * 2);
+        assert(r <= l);
+
+        ll now = r - l;
+
+        dp[i] = now;
+    }
+
+    mint ans = 1;
+    rep(i, 2 * n) {
+        if (m[i] == 'L') ans *= dp[i];
+    }
+
+    for (int i = 1; i <= n; i++) ans *= i;
     cout << ans << endl;
+
 }
