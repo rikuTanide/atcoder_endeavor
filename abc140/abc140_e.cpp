9b34c782e976e96171582a92774529d565d2da1d
riku <riku@tanide.net>
2020-09-08 00:10:16 +0900

    【AC】[AtCoder Beginner Contest 140]E - Second Sum

--- abc140/abc140_e.cpp
+++ abc140/abc140_e.cpp
@@ -8,7 +8,6 @@ const double PI = 3.14159265358979323846;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -35,67 +34,81 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ofstream outfile("log.txt");
-//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
-// std::cout << std::bitset<8>(9);
-//const ll mod = 1e10;
-
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
+ll solve(int n, vector<ll> &v) {
+    map<ll, ll> index;
+    rep(i, n) index[v[i]] = i;
+
+    ll ans = 0;
+
+    set<int> used;
+
+    for (int i = n; i > 0; i--) {
+        int own_index = index[i];
+        used.insert(own_index);
+        bool has_left_lager = [&]() -> bool {
+            auto it = used.lower_bound(own_index);
+            if (it == used.begin()) return false;
+            return true;
+        }();
+        int left_index = [&]() -> int {
+            if (!has_left_lager) return -1;
+            auto it = used.lower_bound(own_index);
+            it--;
+            return *it;
+        }();
+        bool has_right_lager = [&]() -> bool {
+            auto it = used.lower_bound(own_index);
+            it++;
+            if (it == used.end()) return false;
+            return true;
+        }();
+        if (!has_right_lager) continue;
+        int right_lager_index = [&]() -> int {
+            auto it = used.lower_bound(own_index);
+            it++;
+            return *it;
+        }();
+        bool has_right_right_lager = [&]() -> bool {
+            auto it = used.lower_bound(right_lager_index);
+            it++;
+            if (it == used.end()) return false;
+            return true;
+        }();
+        int right_right_index = [&]() -> int {
+            if (!has_right_right_lager) return n;
+            auto it = used.lower_bound(right_lager_index);
+            it++;
+            return *it;
+        }();
+
+        ll left_range = [&]() -> int {
+            return own_index - left_index;
+        }();
+        ll right_range = [&]() -> int {
+            return right_right_index - right_lager_index;
+        }();
+
+        ll now = left_range * right_range * i;
+
+        ans += now;
+    }
+
+    return ans;
+}
 
 int main() {
     int n;
     cin >> n;
+    vector<ll> v(n);
+    rep(i, n) cin >> v[i];
 
-    vector<ll> numbers(n);
-    rep(i, n) cin >> numbers[i];
+    ll a = solve(n, v);
+    reverse(v.begin(), v.end());
+    ll b = solve(n, v);
 
-    map<ll, ll> m;
-    rep(i, n) m[numbers[i]] = i;
 
-    set<int> used;
-    ll ans = 0;
-    for (int i = n; i >= 1; i--) {
-        int j = m[i];
-        int r1 = [&] {
-            auto it = used.lower_bound(j);
-            if (it == used.end()) return n;
-            return *it;
-        }();
-
-        int r2 = [&] {
-            auto it = used.lower_bound(j);
-            if (it == used.end()) return n;
-            it++;
-            if (it == used.end()) return n;
-            return *it;
-        }();
-
-        int l1 = [&] {
-            auto it = used.lower_bound(j);
-            if (it == used.begin()) return -1;
-            it--;
-            return *it;
-        }();
-
-        int l2 = [&] {
-            auto it = used.lower_bound(j);
-            if (it == used.begin()) return -1;
-            it--;
-            if (it == used.begin()) return -1;
-            it--;
-            return *it;
-        }();
-
-        ll k1 = (l1 - l2) * (r1 - j);
-        ll k2 = (r2 - r1) * (j - l1);
-
-        ll now = i * k1 + i * k2;
-
-        ans += now;
-
-        used.insert(j);
-
-    }
+    ll ans = a + b;
     cout << ans << endl;
 }
