0dbc9174e509c74c5145dbc6cded61097a950a38
riku <riku@tanide.net>
2020-09-08 11:48:00 +0900

    【AC】[AtCoder Grand Contest 038]B - Sorting a Segment

--- agc038/agc038_b.cpp
+++ agc038/agc038_b.cpp
@@ -37,9 +37,9 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-// 愚直解を書く
-// 自分の左が自分より小さければノーカンを検証
-// 移動のたびに出し入れ
+    // 愚直解を書く
+    // 自分の左が自分より小さければノーカンを検証
+    // 移動のたびに出し入れ
 
     int n, k;
     cin >> n >> k;
@@ -47,11 +47,52 @@ int main() {
     rep(i, n) cin >> v[i];
     set<vector<int>> s;
 
-    rep(i, n - k + 1) {
+
+    auto is_first = [&]() -> bool {
         vector<int> u = v;
-        sort(u.begin() + i, u.begin() + i + k);
-        s.insert(u);
+        sort(u.begin(), u.begin() + k);
+        return u != v;
+    };
+
+    int ans = 1;
+//    if (is_first()) ans++;
+
+    set<int> used;
+    rep(i, k) used.insert(v[i]);
+
+    rep(i, n - k + 1) {
+        if (i == 0) continue;
+
+        int next_in = v[i + k - 1];
+        int next_out = v[i - 1];
+
+        bool is_left_min = [&]() -> bool {
+            auto it = used.begin();
+            return *it == next_out;
+        }();
+        bool is_right_max = [&]() -> bool {
+            auto it = used.end();
+            it--;
+            return *it < next_in;
+        }();
+
+        if ((!is_left_min) || (!is_right_max))ans++;
+
+        used.insert(next_in);
+        used.erase(used.find(next_out));
     }
 
-    cout << s.size() << endl;
+    vector<int> dp(n, 1);
+    rep(i, n) {
+        if (i == 0) continue;
+        if (v[i - 1] < v[i])dp[i] = dp[i - 1] + 1;
+    }
+
+    int uc = count(dp.begin(), dp.end(), k);
+    if (uc >= 2) {
+        ans -= (uc - 1);
+    }
+
+
+    cout << ans << endl;
 }
