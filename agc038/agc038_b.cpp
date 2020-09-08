e8d65e1ee3a23ff2144aac09e38842a84841d6a1
riku <riku@tanide.net>
2020-09-08 10:56:24 +0900

    【TLE】[AtCoder Grand Contest 038]B - Sorting a Segment

--- agc038/agc038_b.cpp
+++ agc038/agc038_b.cpp
@@ -8,7 +8,6 @@ const double PI = 3.14159265358979323846;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -35,107 +34,24 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ofstream outfile("log.txt");
-//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
-// std::cout << std::bitset<8>(9);
-//const ll mod = 1e10;
-
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-template<class T, class F>
-struct SegmentTree {
-    F f;
-    T ti;
-    vector<T> dat;
-    int sz;
-
-    SegmentTree(const F &f, const T &ti) : f(f), ti(ti) {}
-
-    void build(const vector<T> &v) {
-        assert(v.size());
-        sz = 1;
-        while (sz < v.size())sz <<= 1;
-        dat.resize(sz << 1, ti);
-        for (int i = 0; i < v.size(); i++)dat[sz - 1 + i] = v[i];
-        for (int i = sz - 2; i >= 0; i--)dat[i] = f(dat[i * 2 + 1], dat[i * 2 + 2]);
-    }
-
-    inline void update(int k, T x) {
-        k += sz - 1;
-        dat[k] = x;
-        while (k) {
-            k = (k - 1) / 2;
-            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
-        }
-    }
-
-    inline void add(int k, int x) {
-        k += sz - 1;
-        dat[k] = f(dat[k], x);
-        while (k) {
-            k = (k - 1) / 2;
-            dat[k] = f(dat[k * 2 + 1], dat[k * 2 + 2]);
-        }
-    }
-
-    inline T query(int a, int b) {
-        return query(a, b, 0, 0, sz);
-    }
-
-    T query(int a, int b, int k, int l, int r) {
-        if (r <= a || b <= l)return ti;
-        if (a <= l && r <= b)return dat[k];
-        return f(query(a, b, k * 2 + 1, l, (l + r) / 2), query(a, b, k * 2 + 2, (l + r) / 2, r));
-    }
-};
-
-
 int main() {
+// 愚直解を書く
+// 自分の左が自分より小さければノーカンを検証
+// 移動のたびに出し入れ
+
     int n, k;
     cin >> n >> k;
     vector<int> v(n);
     rep(i, n) cin >> v[i];
+    set<vector<int>> s;
 
-    auto fmi = [](int i, int j) { return min(i, j); };
-    auto fma = [](int i, int j) { return max(i, j); };
-    SegmentTree<int, decltype(fmi)> minTree(fmi, INT_MAX);
-    SegmentTree<int, decltype(fma)> maxTree(fma, 0);
-    minTree.build(v);
-    maxTree.build(v);
-
-    vector<int> dp(n);
-    dp[0] = 1;
-    rep(i, n) {
-        if (i == 0) continue;
-        if (v[i - 1] < v[i]) dp[i] = dp[i - 1] + 1;
-        else dp[i] = 1;
-    }
-    int ans = 0;
-    if (dp[k - 1] != k) ans++;
-
-    rep(i, n - k) {
-
-        int j = i + k;
-        int l = i + 1;
-        int r = j - 1;
-
-        if (dp[j] >= k) continue;
-
-        int mi = minTree.query(l, r + 1);
-        int ma = maxTree.query(l, r + 1);
-
-        int out = v[i];
-        int in = v[j];
-
-        if (out < mi && ma < in) continue;
-        else ans++;
-
+    rep(i, n - k + 1) {
+        vector<int> u = v;
+        sort(u.begin() + i, u.begin() + i + k);
+        s.insert(u);
     }
 
-
-    int ma = *max_element(dp.begin(), dp.end());
-    if (ma >= k) ans++;
-
-    cout << ans << endl;
-
+    cout << s.size() << endl;
 }
