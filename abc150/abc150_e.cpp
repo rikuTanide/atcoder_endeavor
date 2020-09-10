ee4e7db9f01b912b177c9be7a8f0d8ddb7e2dc36
riku <riku@tanide.net>
2020-09-11 03:32:09 +0900

    【WA】[AtCoder Beginner Contest 150]E - Change a Little Bit

--- abc150/abc150_e.cpp
+++ abc150/abc150_e.cpp
@@ -8,7 +8,6 @@ const double PI = 3.14159265358979323846;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -35,13 +34,7 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ofstream outfile("log.txt");
-//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
-// std::cout << std::bitset<8>(9);
-//const ll mod = 1e10;
-
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
-
 const int mod = 1000000007;
 
 struct mint {
@@ -114,32 +107,25 @@ struct mint {
 
 };
 
+ll calc(vector<int> &v, int i) {
+    rep(j, 10) {
+        if ((i >> j) & 1) {
+            return __builtin_popcount(i) * v[j] + calc(v, i - (1 << j));
+        }
+    }
+    return 0;
+}
 
 int main() {
     int n;
     cin >> n;
-    vector<ll> points(n);
-    rep(i, n) cin >> points[i];
-
-    sort(points.rbegin(), points.rend());
-
-    if (n == 1) {
-        cout << mint(points[0]) * 2 << endl;
-        ret();
+    vector<int> v(n);
+    rep(i, n) cin >> v[i];
+    sort(v.begin(), v.end());
+    mint ans = 0;
+    rep(i, 1 << n) {
+        ans += calc(v, i);
     }
-
-    mint res = 0;
-    mint n2 = mint(2).pow(n - 2);
-    mint n1 = mint(2).pow(n - 1);
-
-    rep(i, n) {
-        mint fac = n2 * i + n1;
-        res += fac * points[i];
-    }
-
-    mint ans = res * mint(2).pow(n);
-
+    ans = ans * mint(2).pow(n);
     cout << ans << endl;
-
-
 }
