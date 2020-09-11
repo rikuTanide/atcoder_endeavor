780c3b9449d4e252e5e016f3b2a96d2a777a6d92
riku <riku@tanide.net>
2020-09-11 21:33:42 +0900

    【WA】[キーエンス プログラミング コンテスト 2020]D - Swap and Flip

--- keyence2020/keyence2020_d.cpp
+++ keyence2020/keyence2020_d.cpp
@@ -8,7 +8,6 @@ const double PI = 3.14159265358979323846;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -35,52 +34,78 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ofstream outfile("log.txt");
-//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
-// std::cout << std::bitset<8>(9);
-//const ll mod = 1e10;
-
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+bool is_enable(int n, vector<int> e, vector<int> o) {
+
+    if (!(e.size() == (n + 1) / 2)) return false;
+    if (!(o.size() == n / 2)) return false;
+
+    sort(e.begin(), e.end());
+    sort(o.begin(), o.end());
+    vector<int> tmp;
+    rep(i, e.size()) {
+        tmp.push_back(e[i]);
+        if (o.size() > i) tmp.push_back(o[i]);
+    }
+
+    rep(i, tmp.size() - 1) {
+        if (!(tmp[i] <= tmp[i + 1])) return false;
+    }
+    return true;
+
+}
+
+int calc(vector<int> tmp) {
+    int n = tmp.size();
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (!(tmp[i] <= tmp[j])) ans++;
+        }
+    }
+    return ans;
+}
+
 
 int main() {
     int n;
     cin >> n;
-    vector<vector<ll>> cards(2, vector<ll>(n));
-    rep(i, 2) rep(j, n) cin >> cards[i][j];
+    vector<int> a(n), b(n);
+    rep(i, n) cin >> a[i];
+    rep(i, n) cin >> b[i];
 
-    vector<vector<ll>> dp(1 << n, vector<ll>(n, INF * 2));
-    rep(i, n) dp[1 << i][i] = i;
-    rep(mask, (1 << n) - 1) {
-        rep(i, n) {
-            bool b = mask & (1 << i);
-            if (!b) continue;
-            bool b2 = dp[mask][i] < INF;
-            if (!b2) continue;
+    int ans = INT_MAX;
+    rep(i, 1 << n) {
 
-            int pc = __builtin_popcount(mask);
-            int cur = cards[abs(i - pc - 1) % 2][i];
-            rep(x, n) {
-                bool b3 = (mask & (1 << x)) == 0;
-                if (b3) {
-                    int nex = cards[abs(x - pc) % 2][x];
-                    int num = __builtin_popcount((mask ^ (0xfffff)) & ((1 << x) - 1));
-                    if (nex >= cur) {
-                        dp[mask | (1 << x)][x] = min(dp[mask | (1 << x)][x], dp[mask][i] + num);
-                    }
+        vector<int> e, o, tmp;
+        rep(j, n) {
+
+            if (j % 2 == 0) {
+                if ((i >> j) & 1) {
+                    o.push_back(b[j]);
+                    tmp.push_back(b[j]);
+                } else {
+                    e.push_back(a[j]);
+                    tmp.push_back(a[j]);
+                }
+            } else {
+                if ((i >> j) & 1) {
+                    e.push_back(b[j]);
+                    tmp.push_back(b[j]);
+
+                } else {
+                    o.push_back(a[j]);
+                    tmp.push_back(a[j]);
                 }
             }
-
         }
+
+        bool ok = is_enable(n, e, o);
+        if (!ok) continue;
+        int cost = calc(tmp);
+        cmin(ans, cost);
     }
-
-    ll mi = INF;
-    rep(i, n) cmin(mi, dp[(1 << n) - 1][i]);
-
-    if (mi >= INF) {
-        cout << -1 << endl;
-    } else {
-        cout << mi << endl;
-    }
-
-
+    ans = ans == INT_MAX ? -1 : ans;
+    cout << ans << endl;
 }
