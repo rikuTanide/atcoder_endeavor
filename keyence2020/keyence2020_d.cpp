e1b668323899bcc8598bdf40413faef3af0d645f
riku <riku@tanide.net>
2020-09-12 10:00:31 +0900

    【WA】[キーエンス プログラミング コンテスト 2020]D - Swap and Flip

--- keyence2020/keyence2020_d.cpp
+++ keyence2020/keyence2020_d.cpp
@@ -49,6 +49,7 @@ bool is_enable(int n, vector<int> e, vector<int> o) {
         if (o.size() > i) tmp.push_back(o[i]);
     }
 
+
     rep(i, tmp.size() - 1) {
         if (!(tmp[i] <= tmp[i + 1])) return false;
     }
@@ -56,50 +57,53 @@ bool is_enable(int n, vector<int> e, vector<int> o) {
 
 }
 
-int calc(vector<int> e, vector<int> o) {
-    vector<P> ep, op;
-    rep(i, e.size()) ep.push_back({e[i], -1});
-    rep(i, o.size()) op.push_back({o[i], -1});
+int calc(vector<P> v) {
+    int n = v.size();
 
     vector<int> tmp;
-    for (int i : e) tmp.push_back(i);
-    for (int i : o) tmp.push_back(i);
+    for (P p : v) tmp.push_back(p.first);
     sort(tmp.begin(), tmp.end());
 
-    rep(i, tmp.size()) {
-        if (i % 2 == 0) {
-            rep(j, ep.size()) {
-                if (ep[j].first == tmp[i] && ep[j].second == -1) {
-                    ep[j].second = i;
-                    break;
-                }
-            }
-        } else {
-            rep(j, op.size()) {
-                if (op[j].first == tmp[i] && op[j].second == -1) {
-                    op[j].second = i;
-                    break;
-                }
+    vector<int> rank(n, -1);
+    rep(i, n) {
+        rep(j, n) {
+            if (
+                    v[j].first == tmp[i] &&
+                    i % 2 == v[j].second &&
+                    rank[j] == -1
+                    ) {
+                rank[j] = i;
+                break;
             }
         }
     }
-    vector<int> rank;
-    rep(i, ep.size()) {
-        rank.push_back(ep[i].second);
-        if (op.size() > i) rank.push_back(op[i].second);
-    }
-    int n = rank.size();
+
+
     int ans = 0;
     for (int i = 1; i < n; i++) {
         for (int j = 0; j < i; j++) {
             if (rank[j] > rank[i]) ans++;
         }
     }
+
+
+    if (ans == 46) {
+        for (int i = 1; i < n; i++) {
+            int now = 0;
+            for (int j = 0; j < i; j++) {
+                if (rank[j] > rank[i]) now++;
+            }
+            cout << now << ' ';
+        }
+        cout << endl;
+    }
+
     return ans;
 }
 
 
 int main() {
+//    cout << bitset<15>(20259) << endl;
     int n;
     cin >> n;
     vector<int> a(n), b(n);
@@ -110,28 +114,32 @@ int main() {
     rep(i, 1 << n) {
 
         vector<int> e, o;
-        vector<vector<int>> tmp;
+        vector<P> tmp;
 
         rep(j, n) {
 
             if (j % 2 == 0) {
                 if ((i >> j) & 1) {
                     o.push_back(b[j]);
+                    tmp.push_back({b[j], 1});
                 } else {
                     e.push_back(a[j]);
+                    tmp.push_back({a[j], 0});
                 }
             } else {
                 if ((i >> j) & 1) {
                     e.push_back(b[j]);
+                    tmp.push_back({b[j], 0});
                 } else {
                     o.push_back(a[j]);
+                    tmp.push_back({a[j], 1});
                 }
             }
         }
 
         bool ok = is_enable(n, e, o);
         if (!ok) continue;
-        int cost = calc(e, o);
+        int cost = calc(tmp);
         cmin(ans, cost);
     }
     ans = ans == INT_MAX ? -1 : ans;
