2488156d391f8b8522166fb97795d17704497557
riku <riku@tanide.net>
2020-09-12 15:07:35 +0900

    【AC】[AtCoder Beginner Contest 152]F - Tree and Constraints

--- abc152/abc152_f.cpp
+++ abc152/abc152_f.cpp
@@ -8,7 +8,6 @@ const double PI = 3.14159265358979323846;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -35,78 +34,87 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ofstream outfile("log.txt");
-//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
-// std::cout << std::bitset<8>(9);
-//const ll mod = 1e10;
-
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-struct Edge {
-    int to, id;
-};
+bool dfs(vector<vector<int>> &g, int now, int goal, int prev, vector<int> &route) {
+    route.push_back(now);
+    if (now == goal) return true;
 
-bool dfs(int now, int to, int prev, vector<int> &routes, vector<vector<Edge>> &edges) {
-    if (now == to) {
-        return true;
+    for (int to : g[now]) {
+        if (to == prev) continue;
+        bool ok = dfs(g, to, goal, now, route);
+        if (ok) return true;
     }
-    for (Edge &next : edges[now]) {
-        if (next.to == prev) continue;
-        bool b = dfs(next.to, to, now, routes, edges);
-        if (!b) continue;
-        routes.push_back(next.id);
-        return true;
-    }
+
+    route.pop_back();
     return false;
+}
+
+P make_edge(int a, int b) {
+    if (a < b) return P(a, b);
+    return P(b, a);
+}
+
+ll calc(int m, vector<vector<P>> &use_edges_p, int i) {
+//    vector<P> use_edges;
+    set<P> use_edges;
+    rep(j, m) {
+        bool use = (i >> j) & 1;
+        if (!use) continue;
+
+        for (P p : use_edges_p[j]) {
+            use_edges.insert(p);
+        }
+    }
+//    sort(use_edges.begin(), use_edges.end());
+//    use_edges.erase(unique(use_edges.begin(), use_edges.end()), use_edges.end());
+    return use_edges.size();
 }
 
 int main() {
     int n;
     cin >> n;
 
-    vector<vector<Edge>> edges(n);
+    vector<P> v(n - 1);
+    for (P &p : v) cin >> p.first >> p.second, p.first--, p.second--;
 
-    rep(i, n - 1) {
-        int a, b;
-        cin >> a >> b;
-        a--;
-        b--;
+    for (P p : v) if (p.first > p.second) swap(p.first, p.second);
 
-        edges[a].push_back(Edge{b, i});
-        edges[b].push_back(Edge{a, i});
-    }
+    vector<vector<int>> g(n);
+    for (P p : v) g[p.first].push_back(p.second);
+    for (P p : v) g[p.second].push_back(p.first);
 
     int m;
     cin >> m;
-    vector<ll> rs(m);
+    vector<P> u(m);
+    for (P &p : u) cin >> p.first >> p.second, p.first--, p.second--;
 
+
+    vector<vector<int>> use_edges(m);
+    rep(i, m) dfs(g, u[i].first, u[i].second, -1, use_edges[i]);
+
+    vector<vector<P>> use_edges_p(m);
     rep(i, m) {
-        int u, v;
-        cin >> u >> v;
-        u--;
-        v--;
-
-        vector<int> routes;
-        dfs(u, v, -1, routes, edges);
-
-        ll route_bit = 0;
-        for (int j : routes) route_bit |= (1ll << j);
-        rs[i] = route_bit;
+        rep(j, use_edges[i].size() - 1) {
+            use_edges_p[i].push_back(make_edge(use_edges[i][j], use_edges[i][j + 1]));
+        }
     }
 
     ll ans = 0;
     rep(i, 1 << m) {
-        ll k = 0;
-        rep (j, m) if ((i >> j) & 1) {
-                k |= rs[j];
-            }
-        int white = __builtin_popcountll(k);
-        ll now = 1ll << (n - 1 - white);
-        if (__builtin_popcount(i) % 2 == 0) {
-            ans += now;
-        } else {
+        int method = __builtin_popcount(i) % 2;
+        if (method == 0) {
+            // またはを引く
+            ll k = calc(m, use_edges_p, i);
+            ll now = ((1ll << k) - 1) * (1ll << (n - 1 - k));
             ans -= now;
+        } else {
+            // またはを足す
+            ll k = calc(m, use_edges_p, i);
+            ll now = ((1ll << k) - 1) * (1ll << (n - 1 - k));
+            ans += now;
         }
     }
     cout << ans << endl;
+
 }
