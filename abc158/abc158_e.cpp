601e0dcf115e98760fa5726bebdc549409affae4
riku <riku@tanide.net>
2020-09-13 18:50:12 +0900

    【AC】[AtCoder Beginner Contest 158]E - Divisible Substring

--- abc158/abc158_e.cpp
+++ abc158/abc158_e.cpp
@@ -1,6 +1,3 @@
-#pragma GCC target("avx")
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
 
 #include <bits/stdc++.h>
 //#include <boost/multiprecision/cpp_int.hpp>
@@ -40,18 +37,12 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-int main() {
-    ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+ll simple(int n, int p, string s) {
 
-    int n, p;
-    string s;
-    cin >> n >> p >> s;
-
-    vector<int> dp(p, 0);
+    vector<ll> dp(p, 0);
     ll ans = 0;
     rep(i, n) {
-        vector<int> next(p, 0);
+        vector<ll> next(p, 0);
         int x = s[i] - '0';
         rep(j, p) {
             next[(j * 10 + x) % p] += dp[j];
@@ -60,6 +51,149 @@ int main() {
         dp = next;
         ans += dp[0];
     }
-    cout << ans << endl;
+    return ans;
+}
 
+int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
+
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return sums.back();
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void build() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
+
+int main() {
+
+    int n, p;
+    string s;
+    cin >> n >> p >> s;
+
+
+    if (p == 2 || p == 5) {
+        ll ans = simple(n, p, s);
+        cout << ans << endl;
+        ret();
+    }
+    mod = p;
+    reverse(s.begin(), s.end());
+
+    CumulativeSum cs(n);
+
+    rep(i, n) {
+        int x = s[i] - '0';
+        mint f = mint(x) * mint(10).pow(i);
+        cs.set(i, f.x);
+    }
+    cs.build();
+
+    vector<ll> md(p);
+    md[0] = 1;
+
+    rep(i, n) {
+        md[cs.getSum(i) % p]++;
+    }
+
+    ll ans = 0;
+    rep(i, p) {
+        ll k = md[i];
+        ll now = k * (k - 1) / 2;
+        ans += now;
+    }
+    cout << ans << endl;
 }
