aa3067e4c256bbb4cdee088436e1422abbc47da9
riku <riku@tanide.net>
2020-09-13 22:39:52 +0900

    【TLE】[AtCoder Beginner Contest 178]E - Dist Max

--- abc178/abc178_e.cpp
+++ abc178/abc178_e.cpp
@@ -148,6 +148,7 @@ int main() {
     cin >> n;
     vector<P> v(n);
     for (P &p:v) cin >> p.first >> p.second;
+    for(P&p:v) swap(p.first, p.second);
     cout << solve(v) << endl;
 
 //    int n = 10;
