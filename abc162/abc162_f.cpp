c73f3e4df263efffbfea0ebc6053e0368d71fa57
riku <riku@tanide.net>
2020-09-24 01:34:27 +0900

    【CE】[AtCoder Beginner Contest 162]F - Select Half

--- abc162/abc162_f.cpp
+++ abc162/abc162_f.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 //#include <boost/multiprecision/cpp_int.hpp>
 //namespace mp = boost::multiprecision;
-//#include "atcoder/all"
+#include "atcoder/all"
 
 using namespace std;
 
 const double PI = 3.14159265358979323846;
-//typedef long long ll;
-typedef __int128 ll;
+typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
@@ -27,41 +26,13 @@ std::istream &operator>>(std::istream &in, set<int> &o) {
     return in;
 }
 
-std::istream &operator>>(std::istream &in, ll &o) {
-    long long a;
-    in >> a;
-    o = a;
-    return in;
-}
-
 std::istream &operator>>(std::istream &in, queue<int> &o) {
     ll a;
     in >> a;
     o.push(a);
     return in;
 }
-std::ostream &operator<<(std::ostream &dest, __int128_t value) {
-    std::ostream::sentry s(dest);
-    if (s) {
-        __uint128_t tmp = value < 0 ? -value : value;
-        char buffer[128];
-        char *d = std::end(buffer);
-        do {
-            --d;
-            *d = "0123456789"[tmp % 10];
-            tmp /= 10;
-        } while (tmp != 0);
-        if (value < 0) {
-            --d;
-            *d = '-';
-        }
-        int len = std::end(buffer) - d;
-        if (dest.rdbuf()->sputn(d, len) != len) {
-            dest.setstate(std::ios_base::badbit);
-        }
-    }
-    return dest;
-}
+
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
@@ -72,20 +43,53 @@ int main() {
     vector<ll> v(n);
     for (ll &l:v) cin >> l;
 
-    if (n % 2 == 0) {
-        ll a = 0, b = 0;
-        rep(i, n) {
-            if (i % 2 == 0) {
-                a += v[i];
+    vector<vector<map<int, ll>>> dp(n + 1, vector<map<int, ll>>(2));// dp[ここまで見て][一個前を使った][選んだ数]=合計
+    dp[0][0][0] = 0;
+
+    rep(i, n) {
+
+        int ma = min(i / 2 + 5, n);
+        int mi = max(i / 2 - 5, 0);
+
+        auto set = [&](bool b, int j, ll value) {
+            if (dp[i + 1][b].find(j) == dp[i + 1][b].end()) {
+                dp[i + 1][b][j] = value;
             } else {
-                b += v[i];
+                cmax(dp[i + 1][b][j], value);
+            }
+        };
+
+
+        {
+            // 選ばない → 選ぶ
+            for (auto e : dp[i][false]) {
+                set(true, e.first + 1, e.second + v[i]);
+//                dp[i + 1][true][e.first + 1] = e.second + v[i];
             }
         }
-        ll ans = max(a, b);
-        cout << ans << endl;
-        ret();
+
+        {
+            // 選ばない → 選ばない
+            for (auto e : dp[i][false]) {
+                set(false, e.first, e.second);
+//                dp[i + 1][false][e.first] = e.second;
+            }
+        }
+
+        {
+            // 選ぶ → 選ばない
+            for (auto e : dp[i][true]) {
+                set(false, e.first, e.second);
+//                cmax(dp[i + 1][false][e.first], e.second);
+            }
+        }
+
     }
 
-    __throw_runtime_error("konaide");
+    ll ans = -INF;
+    rep(b, 2) {
+        cmax(ans, dp[n][b][n / 2]);
+    }
+    cout << ans << endl;
 
 }
