7df7c7e50335cd92b020097d298ae7748c32f35d
riku <riku@tanide.net>
2020-09-24 00:03:32 +0900

    【RE】[AtCoder Beginner Contest 162]F - Select Half

--- abc162/abc162_f.cpp
+++ abc162/abc162_f.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 //#include <boost/multiprecision/cpp_int.hpp>
 //namespace mp = boost::multiprecision;
+//#include "atcoder/all"
 
 using namespace std;
 
@@ -8,7 +9,6 @@ const double PI = 3.14159265358979323846;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -35,51 +35,27 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ofstream outfile("log.txt");
-//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
-// std::cout << std::bitset<8>(9);
-//const ll mod = 1e10;
-
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-const int mod = 1000000007;
-
 int main() {
-
     int n;
     cin >> n;
+    vector<ll> v(n);
+    for (ll &l:v) cin >> l;
 
-
-    vector<ll> as(n);
-    rep(i, n) cin >> as[i];
-
-    int skip = (n % 2) + 1;
-
-
-    vector<vector<ll>> dp(n + 1, vector<ll>(skip + 1, -INF));
-
-    auto set = [&](int i, int j, ll value) {
-        if (i > n) return;
-        if (j > skip) return;
-        cmax(dp[i][j], value);
-    };
-
-    dp[0][0] = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= skip; j++) {
-            // 飛ばす
-            set(i + 1, j + 1, dp[i][j]);
-
-            // 飛ばさない
-            // 〇なら足す
-            // ×なら足さない
-
-            bool b = (i + j) % 2 == 0;
-            ll now = b ? as[i] : 0;
-            set(i + 1, j, dp[i][j] + now);
-
+    if (n % 2 == 0) {
+        ll a = 0, b = 0;
+        rep(i, n) {
+            if (i % 2 == 0) {
+                a += v[i];
+            } else {
+                b += v[i];
+            }
         }
+        cout << max(a, b) << endl;
+        ret();
     }
 
-    cout << dp.back().back() << endl;
+    __throw_runtime_error("konaide");
+
 }
