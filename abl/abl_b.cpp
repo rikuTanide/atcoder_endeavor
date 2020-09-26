fc54a7c6d652635e94de5c7aa0e6126d5980e99d
riku <riku@tanide.net>
2020-09-26 21:03:59 +0900

    【AC】[ACL Beginner Contest]B - Integer Preference

---
+++ abl/abl_b.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+bool solve() {
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    if (c <= a && a <= d) {
+        return true;
+    }
+
+    if (c <= b && b <= d) {
+        return true;
+    }
+
+    if (a <= c && c <= b) {
+        return true;
+    }
+
+    if (a <= d && d <= b) {
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    cout << (solve() ? "Yes" : "No") << endl;
+}
