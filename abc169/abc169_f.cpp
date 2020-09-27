a586dfc0fbead97c05d1654f2927f3389c5004ad
riku <riku@tanide.net>
2020-09-27 23:39:13 +0900

    【AC】[AtCoder Beginner Contest 169]F - Knapsack for All Subsets

--- abc169/abc169_f.cpp
+++ abc169/abc169_f.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
 
 const double PI = 3.14159265358979323846;
-using namespace std;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
-//typedef pair<ll, ll> P;
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
 std::istream &operator>>(std::istream &in, set<int> &o) {
-    ll a;
+    int a;
     in >> a;
     o.insert(a);
     return in;
@@ -29,15 +35,7 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ifstream myfile("C:\\Users\\riku\\Downloads\\0_00.txt");
-//ofstream outfile("log.txt");
-//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
-// std::cout << std::bitset<8>(9);
-
-//const ll mod = 1e10;
-typedef priority_queue<P, vector<P>, greater<P> > PQ_ASK;
-
-
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 const int mod = 998244353;
 
 struct mint {
@@ -111,29 +109,26 @@ struct mint {
 };
 
 int main() {
-    int n;
-    ll s;
+    int n, s;
     cin >> n >> s;
-
-    vector<ll> v(n);
+    vector<int> v(n);
     rep(i, n) cin >> v[i];
 
     vector<vector<mint>> dp(n + 1, vector<mint>(s + 1, 0));
     dp[0][0] = 1;
 
-    auto add = [&](int i, int j, mint v) {
-        if (i > n) return;
+    auto add = [&](int i, int j, mint value) {
         if (j > s) return;
-        dp[i][j] += v;
+        dp[i][j] += value;
     };
 
     rep(i, n) {
         rep(j, s + 1) {
-            add(i + 1, j, dp[i][j] * 2);
-            add(i + 1, j + v[i], dp[i][j]);
+            mint from = dp[i][j];
+            add(i + 1, j + v[i], from);
+            add(i + 1, j, from);
+            add(i + 1, j, from);
         }
     }
-
     cout << dp[n][s] << endl;
-
 }
