37cbc8bdffe6558c2633849ffc28d60e644f63d1
riku <riku@tanide.net>
2020-09-27 14:26:59 +0900

    【RE】[AtCoder Beginner Contest 166]F - Three Variables Game

--- abc166/abc166_f.cpp
+++ abc166/abc166_f.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 //#include <boost/multiprecision/cpp_int.hpp>
 //namespace mp = boost::multiprecision;
+//#include "atcoder/all"
 
 using namespace std;
 
@@ -8,7 +9,6 @@ const double PI = 3.14159265358979323846;
 typedef long long ll;
 const double EPS = 1e-9;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//#define rep(i, n) for (ll i = 0; i < (n); ++i)
 typedef pair<ll, ll> P;
 const ll INF = 10e17;
 #define cmin(x, y) x = min(x, y)
@@ -35,114 +35,58 @@ std::istream &operator>>(std::istream &in, queue<int> &o) {
 
 bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
-//ofstream outfile("log.txt");
-//outfile << setw(6) << setfill('0') << prefecture << setw(6) << setfill('0') << rank << endl;
-// std::cout << std::bitset<8>(9);
-//const ll mod = 1e10;
-
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
 int main() {
-    int n;
-    ll a, b, c;
+    int n, a, b, c;
     cin >> n >> a >> b >> c;
+    vector<string> ss(n);
+    rep(i, n) cin >> ss[i];
 
-    vector<string> method(n);
-    rep(i, n) cin >> method[i];
-
+    if (a + b + c == 2) {
+        __throw_runtime_error("konaide");
+    }
     vector<char> ans;
-
-    rep(i, n) {
-        string s = method[i];
+    for (string s : ss) {
         if (s == "AB") {
-            if (a + b == 0) {
-                cout << "No" << endl;
-                ret();
-            }
+
             if (a > b) {
-                a--;
-                b++;
                 ans.push_back('B');
-            } else if (a == b && i < n - 1) {
-                string t = method[i + 1];
-                if (t == "AB") {
-                    a--;
-                    b++;
-                    ans.push_back('B');
-                } else if (t == "BC") {
-                    a--;
-                    b++;
-                    ans.push_back('B');
-                } else {
-                    a++;
-                    b--;
-                    ans.push_back('A');
-                }
+                b++;
+                a--;
             } else {
+                ans.push_back('A');
                 a++;
                 b--;
-                ans.push_back('A');
             }
+
         } else if (s == "BC") {
-            if (b + c == 0) {
-                cout << "No" << endl;
-                ret();
-            }
             if (b > c) {
-                b--;
-                c++;
                 ans.push_back('C');
-            } else if (b == c && i < n - 1) {
-                string t = method[i + 1];
-                if (t == "AB") {
-                    c--;
-                    b++;
-                    ans.push_back('B');
-                } else if (t == "BC") {
-                    c--;
-                    b++;
-                    ans.push_back('B');
-                } else {
-                    c++;
-                    b--;
-                    ans.push_back('C');
-                }
+                c++;
+                b--;
             } else {
+                ans.push_back('B');
                 b++;
                 c--;
-                ans.push_back('B');
             }
         } else {
-            if (a + c == 0) {
-                cout << "No" << endl;
-                ret();
-            }
             if (a > c) {
-                a--;
-                c++;
                 ans.push_back('C');
-            } else if (a == c && i < n - 1) {
-                string t = method[i + 1];
-                if (t == "AB") {
-                    c--;
-                    a++;
-                    ans.push_back('A');
-                } else if (t == "BC") {
-                    c++;
-                    a--;
-                    ans.push_back('C');
-                } else {
-                    c++;
-                    a--;
-                    ans.push_back('C');
-                }
+                c++;
+                a--;
             } else {
+                ans.push_back('A');
                 a++;
                 c--;
-                ans.push_back('A');
             }
         }
+        if (a < 0 || b < 0 || c < 0) {
+            cout << "No" << endl;
+            ret();
+        }
     }
+
     cout << "Yes" << endl;
     for (char c : ans) cout << c << endl;
 
