b6c30898ac401f2de3c466dce7644f203f9ba52f
riku <riku@tanide.net>
2020-10-02 03:52:23 +0900

    【RE】[ACL Contest 1]C - Moving Pieces

--- acl1/acl1_c.cpp
+++ acl1/acl1_c.cpp
@@ -37,135 +37,21 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-typedef int Weight;
-
-struct Edge {
-    int src, dst;
-    Weight weight;
-
-    Edge(int src, int dst, Weight weight) :
-            src(src), dst(dst), weight(weight) {}
+struct Goal {
+    int y, x, point;
 };
 
-bool operator<(const Edge &e, const Edge &f) {
-    return e.weight != f.weight ? e.weight > f.weight : // !!INVERSE!!
-           e.src != f.src ? e.src < f.src : e.dst < f.dst;
-}
-
-#define _GLIBCXX_DEBUG
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-template<typename flow_t, typename cost_t>
-struct PrimalDual {
-    const cost_t INF;
-
-    struct edge {
-        int to;
-        flow_t cap;
-        cost_t cost;
-        int rev;
-        bool isrev;
-    };
-    vector<vector<edge> > graph;
-    vector<cost_t> potential, min_cost;
-    vector<int> prevv, preve;
-
-    PrimalDual(int V) : graph(V), INF(numeric_limits<cost_t>::max()) {}
-
-    void add_edge(int from, int to, flow_t cap, cost_t cost) {
-//        cout << from << ' ' << to << endl;
-        graph[from].emplace_back((edge) {to, cap, cost, (int) graph[to].size(), false});
-        graph[to].emplace_back((edge) {from, 0, -cost, (int) graph[from].size() - 1, true});
-    }
-
-    cost_t min_cost_flow(int s, int t, flow_t f) {
-        int V = (int) graph.size();
-        cost_t ret = 0;
-        using Pi = pair<cost_t, int>;
-        priority_queue<Pi, vector<Pi>, greater<Pi> > que;
-        potential.assign(V, 0);
-        preve.assign(V, -1);
-        prevv.assign(V, -1);
-
-        while (f > 0) {
-            min_cost.assign(V, INF);
-            que.emplace(0, s);
-            min_cost[s] = 0;
-            while (!que.empty()) {
-                Pi p = que.top();
-                que.pop();
-                if (min_cost[p.second] < p.first) continue;
-                for (int i = 0; i < graph[p.second].size(); i++) {
-                    edge &e = graph[p.second][i];
-                    cost_t nextCost = min_cost[p.second] + e.cost + potential[p.second] - potential[e.to];
-                    if (e.cap > 0 && min_cost[e.to] > nextCost) {
-                        min_cost[e.to] = nextCost;
-                        prevv[e.to] = p.second, preve[e.to] = i;
-                        que.emplace(min_cost[e.to], e.to);
-                    }
-                }
-            }
-            if (min_cost[t] == INF) return -1;
-            for (int v = 0; v < V; v++) potential[v] += min_cost[v];
-            flow_t addflow = f;
-            for (int v = t; v != s; v = prevv[v]) {
-                addflow = min(addflow, graph[prevv[v]][preve[v]].cap);
-            }
-            f -= addflow;
-            ret += addflow * potential[t];
-            for (int v = t; v != s; v = prevv[v]) {
-                edge &e = graph[prevv[v]][preve[v]];
-                e.cap -= addflow;
-                graph[v][e.rev].cap += addflow;
-            }
-        }
-        return ret;
-    }
-
-    void output() {
-        for (int i = 0; i < graph.size(); i++) {
-            for (auto &e : graph[i]) {
-                if (e.isrev) continue;
-                auto &rev_e = graph[e.to][e.rev];
-                cout << i << "->" << e.to << " (flow: " << rev_e.cap << "/" << rev_e.cap + e.cap << ")" << endl;
-            }
-        }
-    }
-};
-
-P get_rd(int h, int w, vector<vector<char>> &board) {
-    for (int y = h - 1; y >= 0; y--) {
-        for (int x = w - 1; x >= 0; x--) {
-            if (board[y][x] == 'o') return {y, x};
-        }
-    }
-    return {-1, -1};
-}
-
-struct X {
-    int from;
-    int to;
-    int cost;
-};
-
-vector<X> calc_dp(int h, int w, vector<vector<char>> &board, P rd) {
+vector<Goal> calc_distance(int start_y, int start_x, int h, int w, vector<vector<char>> &board) {
     vector<vector<int>> distances(h, vector<int>(w, INT_MAX / 10));
 
-    distances[rd.first][rd.second] = 0;
+    distances[start_y][start_x] = 0;
     struct Point {
         ll y, x, cost;
     };
 
     queue<Point> q;
-    q.push({rd.first, rd.second, 0});
+    q.push({start_y, start_x, 0});
 
-    auto to_id = [&](int y, int x) {
-        return y * w + x;
-    };
 
     struct Direction {
         int y, x;
@@ -203,58 +89,76 @@ vector<X> calc_dp(int h, int w, vector<vector<char>> &board, P rd) {
         }
     }
 
-    vector<X> ans;
-    for (int y = h - 1; y >= 0; y--) {
-        for (int x = w - 1; x >= 0; x--) {
-            if (distances[y][x] >= INT_MAX / 100) continue;
-            X xi = {
-                    to_id(rd.first, rd.second),
-                    to_id(y, x) + (h * w),
-                    distances[y][x],
-            };
-            ans.push_back(xi);
+    vector<Goal> ans;
+    rep(y, h) {
+        rep(x, h) {
+            if (distances[y][x] >= INT_MAX / 10) continue;
+            ans.push_back({y, x, distances[y][x]});
         }
     }
     return ans;
+
+
 }
 
 int main() {
     int h, w;
     cin >> h >> w;
+
     vector<vector<char>> board(h, vector<char>(w));
-    rep(y, h) rep(x, w) cin >> board[y][x];
+    rep(y, h)rep(x, w) cin >> board[y][x];
 
-    int start = h * w * 2;
-    int goal = start + 1;
-//    atcoder::mcf_graph<ll, ll> pd(h * w * 2 + 2);
-    PrimalDual<ll, ll> pd(h * w * 2 + 2);
+    auto to_id = [&](int y, int x) {
+        return y * w + x;
+    };
 
-    rep(i, h * w) {
-        pd.add_edge(start, i, 1, 0);
-    }
-    rep(i, h * w) {
-        pd.add_edge(h * w + i, goal, 1, 0);
-    }
+    atcoder::mcf_graph<ll, ll> flow(h * w * 2 + 2);
+    const ll MAX = 100000;
+    auto connect = [&](int from, int to, int point) {
+//        printf("%d %d %d\n", from, to, point);
+        flow.add_edge(from, to, 1, point);
+    };
 
-    int count = 0;
     rep(y, h) {
         rep(x, w) {
             if (board[y][x] != 'o') continue;
-            count++;
-            vector<X> distances = calc_dp(h, w, board, {y, x});
+            vector<Goal> goals = calc_distance(y, x, h, w, board);
 
-            for (X x : distances) pd.add_edge(x.from, x.to, 1, -x.cost);
-
-//
-//        rep(y, h) {
-//            rep(x, w) cout << board[y][x];
-//            cout << endl;
-//        }
-//        cout << endl;
+            for (Goal g : goals) {
+                connect(to_id(y, x), to_id(g.y, g.x) + h * w, MAX - g.point);
+            }
 
         }
     }
-//    P ans = pd.flow(start, goal, 1);
-    ll ans = -pd.min_cost_flow(start, goal, count);
-    cout << ans << endl;
+    int start = h * w * 2;
+    int goal = h * w * 2 + 1;
+    rep(y, h) {
+        rep(x, w) {
+            if (board[y][x] == 'o') {
+                connect(start, to_id(y, x), 0);
+            }
+        }
+    }
+    rep(y, h) {
+        rep(x, w) {
+            if (board[y][x] != 'x') {
+                connect(to_id(y, x) + h * w, goal, 0);
+            }
+        }
+    }
+
+    int ball_count = 0;
+    rep(y, h) {
+        rep(x, w) {
+            if (board[y][x] == 'o') {
+                ball_count++;
+            }
+        }
+    }
+
+    auto ans = flow.flow(start, goal, ball_count);
+//    cout << ans.second << endl;
+    cout << -ans.second + (MAX * ans.first) << endl;
+
+
 }
