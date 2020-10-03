6ee252133e4eae87b4fe3efdb4caae4917e0794b
riku <riku@tanide.net>
2020-10-03 22:53:42 +0900

    【WA】[AtCoder Regular Contest 104]C - Fair Elevator

--- arc104/arc104_c.cpp
+++ arc104/arc104_c.cpp
@@ -69,39 +69,11 @@ public:
 
 };
 
-int main() {
-    int n;
-    cin >> n;
-    vector<P> v(n);
-    for (P &p:v)cin >> p.first >> p.second, p.first--, p.second--;
+bool rec(vector<P> &v, int now, int n, map<int, int> &in_out, map<int, int> &out_in) {
 
-    for (P p: v) {
-        if (p.first == -2) continue;
-        if (p.second == -2)continue;
-        if (p.first >= p.second) {
-            cout << "No" << endl;
-            ret();
-        }
+    if (now == n * 2) {
+        return true;
     }
-
-    {
-        map<int, int> counter;
-        for (P p : v) counter[p.first]++;
-        for (P p : v) counter[p.second]++;
-
-        for (auto e : counter) {
-            if (e.first == -2) continue;
-            if (e.second >= 2) {
-                cout << "No" << endl;
-                ret();
-            }
-        }
-    }
-
-
-    map<int, int> in_out, out_in;
-    for (P p : v)if (p.first != -2) in_out[p.first] = p.second;
-    for (P p : v)if (p.second != -2) out_in[p.second] = p.first;
 
     auto in_out_is = [&](int i, int j) -> bool {
         if (in_out.find(i) == in_out.end())return true;
@@ -114,57 +86,154 @@ int main() {
         return out_in[i] == j || out_in[i] == -2;
     };
 
+    auto in_out_has = [&](int a) -> bool {
+        if (in_out.find(a) != in_out.end())return true;
+        return false;
+    };
+
+    auto out_in_has = [&](int a) -> bool {
+        if (out_in.find(a) != out_in.end())return true;
+        return false;
+    };
 
     auto check = [&](int now, int i) -> bool {
         for (int left = now; left < now + i; left++) {
             int right = left + i;
+            if (in_out_has(right)) return false;
+            if (out_in_has(left)) return false;
+
             if (!in_out_is(left, right)) return false;
             if (!out_in_is(right, left)) return false;
         }
         return true;
     };
 
-    int now = 0;
-    while (now < 2 * n) {
-        int i = [&]() -> int {
-            for (int i = 1; now + i * 2 <= 2 * n; i++) {
-                if (check(now, i)) return i;
-            }
-            return -1;
-        }();
-
-        if (i == -1) {
-            cout << "No" << endl;
-            ret();
+    for (int i = 1; now + i * 2 <= 2 * n; i++) {
+        if (check(now, i)) {
+            bool b = rec(v, now + i * 2, n, in_out, out_in);
+            if (b) return true;
         }
-//        cout << i << endl;
-        now += i * 2;
     }
-
-//    cout << "Yes" << endl;
-    throw_with_nested("hoge");
+    return false;
 }
 
-//int main() {
-//    int n = 4;
-//
-//    vector<int> arr1(n);
-//    vector<int> arr2(n);
-//    rep(i, n)arr1[i] = i;
-//    rep(i, n)arr2[i] = i;
-//
-//    do {
-//        do {
-//            vector<P> v(n);
-//            rep(i, n)v[i].first = arr1[i];
-//            rep(i, n)v[i].second = arr2[i];
-//
-//            check(v);
-//
-//            rep(i, 1 << (2 * n)) {
-//
-//            }
-//
-//        } while (std::next_permutation(arr2.begin(), arr2.end()));
-//    } while (std::next_permutation(arr1.begin(), arr1.end()));
-//}
+bool solve(int n, vector<P> &v) {
+//    int n;
+//    cin >> n;
+//    vector<P> v(n);
+//    for (P &p:v)cin >> p.first >> p.second, p.first--, p.second--;
+
+    vector<P> k = {{0,  1},
+                   {-2, 4},
+                   {-2, 5}};
+    if (v == k) {
+        cout << endl;
+    }
+
+
+    for (P p: v) {
+        if (p.first == -2) continue;
+        if (p.second == -2)continue;
+        if (p.first >= p.second) {
+            return false;
+        }
+    }
+
+    {
+        map<int, int> counter;
+        for (P p : v) counter[p.first]++;
+        for (P p : v) counter[p.second]++;
+
+        for (auto e : counter) {
+            if (e.first == -2) continue;
+            if (e.second >= 2) {
+                return false;
+            }
+        }
+    }
+
+
+    map<int, int> in_out, out_in;
+    for (P p : v)if (p.first != -2) in_out[p.first] = p.second;
+    for (P p : v)if (p.second != -2) out_in[p.second] = p.first;
+
+
+    return rec(v, 0, 2 * n, in_out, out_in);
+
+}
+
+bool check(vector<P> v) {
+
+    for (P p : v) if (p.first >= p.second)return false;
+
+    for (int i = 0; i < v.size(); i++) {
+        for (int j = i + 1; j < v.size(); j++) {
+            int xa = v[i].first;
+            int xb = v[i].second;
+            int ya = v[j].first;
+            int yb = v[j].second;
+
+            if (
+                    (xa <= ya && ya <= xb)
+                    || (xa <= yb && yb <= xb)
+                    || (ya <= xa && xa <= yb)
+                    || (ya <= xb && xb <= yb)
+                    ) {
+                if (xa - xb != ya - yb) return false;
+            }
+
+        }
+    }
+    return true;
+
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<P> v(n);
+    for (P &p:v)cin >> p.first >> p.second, p.first--, p.second--;
+    bool b = solve(n, v);
+    cout << (b ? "Yes" : "No") << endl;
+}
+
+int main3() {
+    int n = 3;
+
+    vector<int> arr(2 * n);
+    rep(i, 2 * n)arr[i] = i;
+
+    do {
+        vector<P> v(n);
+        rep(i, 2 * n) if (i % 2 == 0) v[i / 2].first = arr[i];
+        rep(i, 2 * n) if (i % 2 == 1) v[i / 2].second = arr[i];
+
+        bool b1 = check(v);
+
+        rep(i, 1 << (2 * n)) {
+            vector<P> u = v;
+            rep(j, 2 * n) {
+                if ((i >> j) & 1) {
+                    if (j >= n) u[j - n].second = -2;
+                    else u[j].first = -2;
+                }
+            }
+            bool b2 = solve(n, u);
+            if (b1 != b2) {
+
+                if (b1 && !(b2)) {
+                    cout << "tf" << endl;
+                } else {
+                    cout << "ft" << endl;
+                }
+
+                for (P p : u) {
+                    printf("%lld %lld\n", p.first, p.second);
+                }
+                cout << endl;
+            }
+        }
+
+
+    } while (std::next_permutation(arr.begin(), arr.end()));
+}
