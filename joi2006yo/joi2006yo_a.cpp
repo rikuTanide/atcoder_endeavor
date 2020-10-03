a70ebc31a32ea8ff78cf4424d35f43ceaf39c147
riku <riku@tanide.net>
2020-10-04 04:03:32 +0900

    【AC】[第５回日本情報オリンピック 予選（オンライン）]A - JOI 2006 予選 問題1

---
+++ joi2006yo/joi2006yo_a.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a = 0, b = 0;
+    rep(_, n) {
+        int x, y;
+        cin >> x >> y;
+
+        if (x == y) {
+            a += x;
+            b += y;
+        } else if (x > y) {
+            a += x;
+            a += y;
+        } else {
+            b += x;
+            b += y;
+        }
+
+
+    }
+    printf("%d %d\n", a, b);
+
+}
