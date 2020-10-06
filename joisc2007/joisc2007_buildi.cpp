669a24b31ece465824c7e7cbc2bff4dc8fc59121
riku <riku@tanide.net>
2020-10-07 01:57:31 +0900

    【AC】[2007年 日本情報オリンピック春合宿OJ]building - ビルの飾り付け (Building)

---
+++ joisc2007/joisc2007_buildi.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> buildings(n);
+    rep(i, n) cin >> buildings[i];
+
+    vector<ll> dp(10000 + 10, INF);
+    dp[0] = -INF;
+
+    for (ll l : buildings) {
+        auto it = lower_bound(dp.begin(), dp.end(), l);
+        cmin(*it, l);
+    }
+
+    auto it = lower_bound(dp.begin(), dp.end(), INF);
+    int d = distance(dp.begin(), it);
+    int ans = d - 1;
+    cout << ans << endl;
+}
