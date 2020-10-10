0b438f01b19a63ff9d11ce91e558385396994d91
riku <riku@tanide.net>
2020-10-11 03:28:57 +0900

    【AC】[第７回日本情報オリンピック 本選（オンライン）]E - ペンキの色

--- joi2008ho/joi2008ho_e.cpp
+++ joi2008ho/joi2008ho_e.cpp
@@ -87,10 +87,10 @@ struct Tape {
 };
 
 class MatrixSum {
-    vector<vector<unsigned char>> sum;
+    vector<vector<unsigned short>> sum;
 public:
     MatrixSum(int x, int y) {
-        sum = vector<vector<unsigned char>>(x, vector<unsigned char>(y));
+        sum = vector<vector<unsigned short>>(x, vector<unsigned short>(y));
     }
 
     void add(int x, int y, int value) {
