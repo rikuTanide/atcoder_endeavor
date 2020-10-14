21e58341febe0889691ab68bdd9817193097dd24
riku <riku@tanide.net>
2020-10-14 20:20:15 +0900

    【WA】[2008年 日本情報オリンピック春合宿OJ]origami - 折り紙 (Origami)

--- joisc2008/joisc2008_origami.cpp
+++ joisc2008/joisc2008_origami.cpp
@@ -72,23 +72,62 @@ public:
 
 };
 
-int main() {
-    int n;
-    int w, h;
-    cin >> n >> w >> h;
+struct Point {
+    int x1, y1, x2, y2;
 
+};
+
+class Conv {
+    ll cursor = 0;
+    map<ll, ll> to_short; // <original, small >
+    map<ll, ll> to_long; // <small, original>
+    std::set<ll> tmp;
+
+
+public:
+    void set(ll original) {
+        if (to_short.find(original) != to_short.end()) {
+            return;
+        }
+        to_long[cursor] = original;
+        to_short[original] = cursor;
+        cursor++;
+    }
+
+    ll revert(ll after) {
+        assert(to_long.find(after) != to_long.end());
+        return to_long[after];
+    }
+
+    ll convert(ll original) {
+        assert(to_short.find(original) != to_short.end());
+        return to_short[original];
+    }
+
+    ll next() {
+        return cursor;
+    }
+
+    // 前計算省略のため
+    void cache(ll t) {
+        tmp.insert(t);
+    }
+
+    void build() {
+        for (ll t : tmp) {
+            set(t);
+        }
+    }
+
+};
+
+P solve(ll w, ll h, Conv &conv_x, Conv &conv_y, vector<Point> &points) {
     MatrixSum ms(w + 1, h + 1);
-    rep(_, n) {
-        int x1, y1, x2, y2;
-        cin >> x1 >> y1 >> x2 >> y2;
-        x1--;
-        y1--;
-        x2--;
-        y2--;
-        ms.add(x1, y1, 1);
-        ms.add(x1, y2 + 1, -1);
-        ms.add(x2 + 1, y1, -1);
-        ms.add(x2 + 1, y2 + 1, 1);
+    for (Point &p : points) {
+        ms.add(p.x1, p.y1, 1);
+        ms.add(p.x1, p.y2 + 1, -1);
+        ms.add(p.x2 + 1, p.y1, -1);
+        ms.add(p.x2 + 1, p.y2 + 1, 1);
     }
     ms.setUp();
 
@@ -99,13 +138,46 @@ int main() {
         }
     }
 
-    cout << a << endl;
-
-    int b = 0;
+    ll b = 0;
     rep(x, w) {
         rep(y, h) {
-            if (ms.get(x, y) == a) b++;
+            if (ms.get(x, y) == a) {
+                ll wi = conv_x.revert(x + 1) - conv_x.revert(x);
+                ll hi = conv_y.revert(y + 1) - conv_x.revert(y);
+                ll now = wi * hi;
+                b += now;
+            }
         }
     }
-    cout << b << endl;
+    return P(a, b);
+}
+
+int main() {
+    int n;
+    int w, h;
+    cin >> n >> w >> h;
+
+    vector<Point> points(n);
+    for (Point &p: points)cin >> p.x1 >> p.y1 >> p.x2 >> p.y2;
+    for (Point &p: points) p.x1--;
+    for (Point &p: points) p.y1--;
+    for (Point &p: points) p.x2--;
+    for (Point &p: points) p.y2--;
+
+    Conv conv_x, conv_y;
+    for (Point p : points) for (int i = -1; i <= 1; i++) conv_x.cache(p.x1 + i);
+    for (Point p : points) for (int i = -1; i <= 1; i++) conv_x.cache(p.x2 + i);
+    for (Point p : points) for (int i = -1; i <= 1; i++) conv_y.cache(p.y1 + i);
+    for (Point p : points) for (int i = -1; i <= 1; i++) conv_y.cache(p.y2 + i);
+    conv_x.build();
+    conv_y.build();
+
+    for (Point &p : points) p.x1 = conv_x.convert(p.x1);
+    for (Point &p : points) p.x2 = conv_x.convert(p.x2);
+    for (Point &p : points) p.y1 = conv_y.convert(p.y1);
+    for (Point &p : points) p.y2 = conv_y.convert(p.y2);
+
+    P ans = solve(conv_x.next(), conv_y.next(), conv_x, conv_y, points);
+
+    printf("%d\n%d\n", ans.first, ans.second);
 }
