4351da8fc57b4e5ed74242b7a7d3f11b4f8e9159
riku <riku@tanide.net>
2020-10-18 02:54:02 +0900

    【AC】[第９回日本情報オリンピック 本選（オンライン）]B - お菓子の分割

--- joi2010ho/joi2010ho_b.cpp
+++ joi2010ho/joi2010ho_b.cpp
@@ -49,14 +49,14 @@ int main() {
     vector<int> v(n + 1);
     rep(i, n) if (i != 0) cin >> v[i];
 
-    vector<vector<int>> prev(n / 2 + 1, vector<int>(2, INT_MAX / 1000));
-    vector<vector<int>> next(n / 2 + 1, vector<int>(2, INT_MAX / 1000));
+    vector<vector<int>> prev(n / 2 + 2, vector<int>(2, INT_MAX / 1000));
+    vector<vector<int>> next(n / 2 + 2, vector<int>(2, INT_MAX / 1000));
 
     prev[0][0] = 0;
 
     for (int i = 1; i <= n; i++) {
-        rep(j, n / 2 + 1)rep(k, 2) next[j][k] = INT_MAX / 1000;
-        rep(j, n / 2) {
+        rep(j, n / 2 + 2)rep(k, 2) next[j][k] = INT_MAX / 1000;
+        rep(j, n / 2 + 1) {
             {
                 // Aのターン
 
