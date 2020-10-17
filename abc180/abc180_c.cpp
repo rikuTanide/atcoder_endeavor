668206b1274046a7ec04e8004e74c370e0dd236b
riku <riku@tanide.net>
2020-10-17 20:09:23 +0900

    【AC】[AtCoder Beginner Contest 180]C - Cream puff

---
+++ abc180/abc180_c.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+void solve() {
+    ll n;
+    cin >> n;
+
+    set<ll> ans;
+    for (ll i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ans.insert(i);
+            ans.insert(n / i);
+        }
+    }
+
+    for (ll l : ans) cout << l << endl;
+
+}
+
+int main() {
+    solve();
+}
