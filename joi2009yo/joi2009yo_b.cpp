34fbca4592f5f224ad2ff18c3c794bc5d508961c
riku <riku@tanide.net>
2020-10-17 15:11:39 +0900

    【AC】[第８回日本情報オリンピック 予選（オンライン）]B - コンテスト

---
+++ joi2009yo/joi2009yo_b.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+//#include "atcoder/all"
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+
+int main() {
+    vector<int> w(10), k(10);
+    rep(i, 10) cin >> w[i];
+    rep(i, 10) cin >> k[i];
+
+    sort(w.rbegin(), w.rend());
+    sort(k.rbegin(), k.rend());
+
+    int ws = accumulate(w.begin(), w.begin() + 3, 0);
+    int ks = accumulate(k.begin(), k.begin() + 3, 0);
+
+    printf("%d %d\n", ws, ks);
+
+}
