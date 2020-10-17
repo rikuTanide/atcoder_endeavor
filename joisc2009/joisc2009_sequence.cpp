aaeb352cba7a2c2073d5792f0b4f98093a70caae
riku <riku@tanide.net>
2020-10-17 23:09:30 +0900

    【RE】[2009年 日本情報オリンピック春合宿OJ]sequence - 数列 (Sequence)

---
+++ joisc2009/joisc2009_sequence.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return sums.back();
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        if (start > end) {
+            return getSectionSum(end + 1, start);
+        }
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void build() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<ll> points(n - 1);
+    rep(i, n - 1) cin >> points[i];
+
+    CumulativeSum cs(n - 1);
+    rep(i, n - 1)cs.set(i, points[i]);
+    cs.build();
+
+    int now = 0;
+    ll ans = 0;
+    rep(_, m) {
+        int a;
+        cin >> a;
+        int next = now + a;
+        ll move = cs.getSectionSum(now, next);
+        ans += move;
+        ans %= 100000;
+        now = next;
+    }
+    cout << ans << endl;
+}
