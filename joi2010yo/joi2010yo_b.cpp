b8c0e40535f39e4bd745fae8498fb4395e65274e
riku <riku@tanide.net>
2020-10-18 15:16:04 +0900

    【AC】[第９回日本情報オリンピック 予選（オンライン）]B - すごろく

---
+++ joi2010yo/joi2010yo_b.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> cells(n);
+    rep(i, n) cin >> cells[i];
+
+    vector<int> cy(m);
+    rep(i, m) cin >> cy[i];
+
+    int now = 0;
+    int count = 0;
+    while (true) {
+        int me = cy[count];
+        count++;
+        now = now + me;
+        if (now >= n) break;
+        int move = cells[now];
+        now = now + move;
+        if (now >= n - 1) break;
+    }
+
+    cout << count << endl;
+
+}
