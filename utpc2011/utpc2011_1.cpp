16bd17f9d4d78341aa88dfbc805baa0617cdff45
riku <riku@tanide.net>
2020-10-23 17:36:01 +0900

    【AC】[UTPC 2011]A - プログラミングコンテスト

---
+++ utpc2011/utpc2011_1.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> v(n, vector<int>(m));
+    rep(y, n) {
+        rep(x, m) {
+            cin >> v[y][x];
+        }
+    }
+
+    int ans = 0;
+    rep(y, n) cmax(ans, accumulate(v[y].begin(), v[y].end(), 0));
+
+    cout << ans << endl;
+
+}
