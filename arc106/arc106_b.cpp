e7981789d633ca7dc60b748c5168b04e6e4491dc
riku <riku@tanide.net>
2020-10-24 22:02:24 +0900

    【RE】[AtCoder Regular Contest 106]B - Values

--- arc106/arc106_b.cpp
+++ arc106/arc106_b.cpp
@@ -37,95 +37,19 @@ bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
 
-
-class UnionFind {
-public:
-    // 親の番号を格納する。親だった場合-size
-    vector<int> parents;
-    int n;
-
-    UnionFind(int n) : n(n) {
-        parents = vector<int>(n, -1);
-    }
-
-    // aがどのグループに属しているか
-    int root(int a) {
-        if (parents[a] < 0) {
-            return a;
-        }
-        return parents[a] = root(parents[a]);
-    }
-
-    int size(int a) {
-        return -parents[root(a)];
-    }
-
-    // aとbをくっつける
-    bool connect(int a, int b) {
-        int ra = root(a);
-        int rb = root(b);
-        if (ra == rb) {
-            return false;
-        }
-        // 大きいほうにA
-        if (size(ra) < size(rb)) {
-            swap(ra, rb);
-        }
-        parents[ra] += parents[rb];
-        parents[rb] = ra;
-        return true;
-    }
-
-    bool is_union(int a, int b) {
-        int ra = root(a);
-        int rb = root(b);
-        return ra == rb;
-    }
-
-    int parent_count() {
-        int ans = 0;
-        rep(i, n) {
-            if (root(i) == i) ans++;
-        }
-        return ans;
-    }
-
-};
-
-
-bool solve() {
+int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<ll> as(n), bs(n);
-    rep(i, n) cin >> as[i];
-    rep(i, n) cin >> bs[i];
-
-    vector<P> edges(m);
-    for (P &p:edges)cin >> p.first >> p.second, p.first--, p.second--;
-
-    // 合計が合わなければダメ
-    ll asum = accumulate(as.begin(), as.end(), 0ll);
-    ll bsum = accumulate(bs.begin(), bs.end(), 0ll);
-    if (asum != bsum) return false;
-
-
-    UnionFind uf(n);
-    for (P p : edges) uf.connect(p.first, p.second);
-
-    map<int, ll> ma, mb;
-    rep(i, n) {
-        ma[uf.root(i)] += as[i];
-        mb[uf.root(i)] += bs[i];
+    if (n == 1 && m == 0) {
+        cout << "1 2" << endl;
+        ret();
     }
 
-    return ma == mb;
+    if (n - abs(m) < 2) {
+        cout << -1 << endl;
+        ret();
+    }
 
-    // 隣り合う辺の偶奇が会わなければダメ
-
-}
-
-int main() {
-    cout << (solve() ? "Yes" : "No") << endl;
-
+    throw_with_nested("konaide");
 }
