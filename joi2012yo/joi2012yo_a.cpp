e00b21ba86444e54b88d988336515d766c352a92
riku <riku@tanide.net>
2020-10-25 16:18:19 +0900

    【AC】[第１１回日本情報オリンピック 予選（オンライン）]A - ランチ (Lunch)

---
+++ joi2012yo/joi2012yo_a.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    vector<int> pasuta(3);
+    rep(i, 3) cin >> pasuta[i];
+    vector<int> juice(2);
+    rep(i, 2)cin >> juice[i];
+
+    int p = *min_element(pasuta.begin(), pasuta.end());
+    int j = *min_element(juice.begin(), juice.end());
+
+    int ans = p + j - 50;
+    cout << ans << endl;
+
+}
