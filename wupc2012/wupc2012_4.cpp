de86315cd0dbd3108e7c04cd5e714a39864faf3a
riku <riku@tanide.net>
2020-10-27 23:19:05 +0900

    【AC】[WUPC 2012]D - 三角パズル

---
+++ wupc2012/wupc2012_4.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> prev(1);
+    cin >> prev[0];
+
+    for (int i = 2; i <= n; i++) {
+
+        vector<int> v(i);
+        rep(j, i) cin >> v[j];
+
+
+        vector<int> next(i);
+
+        rep(j, i - 1) {
+            cmax(next[j], prev[j] + v[j]);
+            cmax(next[j + 1], prev[j] + v[j + 1]);
+        }
+
+        prev = next;
+    }
+
+    cout << *max_element(prev.begin(), prev.end()) << endl;
+
+}
