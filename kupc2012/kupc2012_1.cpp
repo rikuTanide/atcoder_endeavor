82bfcaadadabac8e814310eee142a1e457f1dcdb
riku <riku@tanide.net>
2020-10-28 18:53:25 +0900

    【AC】[京都大学プログラミングコンテスト2012]A - アルデンテ

---
+++ kupc2012/kupc2012_1.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int n, t, e;
+    cin >> n >> t >> e;
+
+    rep(i, n) {
+        int a;
+        cin >> a;
+
+        int l = t / a * a;
+        ll r = (t + a - 1) / a * a;
+
+        if (t - e <= l && l <= t + e || t - e <= r && r <= t + e) {
+            cout << (i + 1) << endl;
+            ret();
+        }
+
+    }
+    cout << -1 << endl;
+}
