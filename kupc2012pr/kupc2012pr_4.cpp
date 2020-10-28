1f90425544b1ee8a160970f0708d3c2280b83de5
riku <riku@tanide.net>
2020-10-28 18:34:21 +0900

    【WA】[京都大学プログラミングコンテスト2012 practice]D - A mul B Problem

---
+++ kupc2012pr/kupc2012pr_4.cpp
@@ -0,0 +1,169 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+template<class T>
+struct Matrix {
+    vector<vector<T> > A;
+
+    Matrix() {}
+
+    Matrix(size_t n, size_t m) : A(n, vector<T>(m, 0)) {}
+
+    Matrix(size_t n) : A(n, vector<T>(n, 0)) {};
+
+    size_t height() const {
+        return (A.size());
+    }
+
+    size_t width() const {
+        return (A[0].size());
+    }
+
+    inline const vector<T> &operator[](int k) const {
+        return (A.at(k));
+    }
+
+    inline vector<T> &operator[](int k) {
+        return (A.at(k));
+    }
+
+    static Matrix I(size_t n) {
+        Matrix mat(n);
+        for (int i = 0; i < n; i++) mat[i][i] = 1;
+        return (mat);
+    }
+
+    Matrix &operator+=(const Matrix &B) {
+        size_t n = height(), m = width();
+        assert(n == B.height() && m == B.width());
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                (*this)[i][j] += B[i][j];
+        return (*this);
+    }
+
+    Matrix &operator-=(const Matrix &B) {
+        size_t n = height(), m = width();
+        assert(n == B.height() && m == B.width());
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                (*this)[i][j] -= B[i][j];
+        return (*this);
+    }
+
+    Matrix &operator*=(const Matrix &B) {
+        size_t n = height(), m = B.width(), p = width();
+        assert(p == B.height());
+        vector<vector<T> > C(n, vector<T>(m, 0));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                for (int k = 0; k < p; k++)
+                    C[i][j] = (C[i][j] + (*this)[i][k] * B[k][j]);
+        A.swap(C);
+        return (*this);
+    }
+
+    Matrix &operator^=(long long k) {
+        Matrix B = Matrix::I(height());
+        while (k > 0) {
+            if (k & 1) B *= *this;
+            *this *= *this;
+            k >>= 1LL;
+        }
+        A.swap(B.A);
+        return (*this);
+    }
+
+    Matrix operator+(const Matrix &B) const {
+        return (Matrix(*this) += B);
+    }
+
+    Matrix operator-(const Matrix &B) const {
+        return (Matrix(*this) -= B);
+    }
+
+    Matrix operator*(const Matrix &B) const {
+        return (Matrix(*this) *= B);
+    }
+
+    Matrix operator^(const long long k) const {
+        return (Matrix(*this) ^= k);
+    }
+
+    friend ostream &operator<<(ostream &os, Matrix &p) {
+        size_t n = p.height(), m = p.width();
+        for (int i = 0; i < n; i++) {
+            os << "[";
+            for (int j = 0; j < m; j++) {
+                os << p[i][j] << (j + 1 == m ? "]\n" : ",");
+            }
+        }
+        return (os);
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    Matrix<ll> a(n), b(n), c(n);
+
+    rep(y, n) {
+        rep(x, n) {
+            cin >> a[y][x];
+        }
+    }
+
+    rep(y, n) {
+        rep(x, n) {
+            cin >> b[y][x];
+        }
+    }
+
+    rep(y, n) {
+        rep(x, n) {
+            cin >> c[y][x];
+        }
+    }
+
+    Matrix<ll> ab = a * b;
+
+    cout << (ab.A == c.A ? "Yes" : "No") << endl;
+
+}
