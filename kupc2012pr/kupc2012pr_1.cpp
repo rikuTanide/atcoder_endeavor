353054211e6174c382d3fd011dffdeb38083c8cb
riku <riku@tanide.net>
2020-10-28 17:37:36 +0900

    【TLE】[京都大学プログラミングコンテスト2012 practice]A - Wikipedia

---
+++ kupc2012pr/kupc2012pr_1.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+ll ackermann(ll m, ll n) {
+
+    if (m == 0) return n + 1;
+    if (n == 0) return ackermann(m - 1, 1);
+
+    return ackermann(m - 1, ackermann(m, n - 1));
+
+}
+
+int main() {
+    ll m, n;
+    cin >> m >> n;
+    cout << ackermann(m, n) << endl;
+}
