ca1e97228fce918481df7ebd867b0d4be2d930a1
riku <riku@tanide.net>
2020-10-30 16:16:48 +0900

    【AC】[東京大学プログラミングコンテスト2012]A - 2012年12月02日

---
+++ utpc2012/utpc2012_01.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, set<T> &o) {
+    T a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, queue<T> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+template<class T>
+bool contain(set<T> &s, T a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    vector<char> y(4);
+    vector<char> md(4);
+
+    scanf("%c%c%c%c/%c%c/%c%c",
+          &y[0],
+          &y[1],
+          &y[2],
+          &y[3],
+          &md[0],
+          &md[1],
+          &md[2],
+          &md[3]
+    );
+
+    sort(y.begin(), y.end());
+    sort(md.begin(), md.end());
+
+    bool ok = y == md;
+
+    cout << (ok ? "yes" : "no") << endl;
+}
