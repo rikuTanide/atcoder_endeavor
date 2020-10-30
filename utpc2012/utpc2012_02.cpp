055b660cc2e3845b3e626d6122f317d6b9ba1079
riku <riku@tanide.net>
2020-10-30 16:41:32 +0900

    【AC】[東京大学プログラミングコンテスト2012]B - 残像に口紅を

---
+++ utpc2012/utpc2012_02.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, set<T> &o) {
+    T a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, queue<T> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+template<class T>
+bool contain(set<T> &s, T a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    string s;
+    cin >> s;
+
+    reverse(s.begin(), s.end());
+
+    set<char> used;
+
+    vector<char> ans;
+
+    for (char c : s) {
+        if (!contain(used, c)) {
+            ans.push_back(c);
+            used.insert(c);
+        }
+    }
+
+    for (char c = 'A'; c <= 'H'; c++) {
+        if (!contain(used, c)) ans.push_back(c);
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    for (char c : ans)cout << c;
+    cout << endl;
+
+}
