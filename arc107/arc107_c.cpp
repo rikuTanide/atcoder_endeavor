453fe3132f212a285a52c9bed354e8d34ebb508d
riku <riku@tanide.net>
2020-10-31 21:51:48 +0900

    【AC】[AtCoder Regular Contest 107]C - Shuffle Permutation

---
+++ arc107/arc107_c.cpp
@@ -0,0 +1,215 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, set<T> &o) {
+    T a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, queue<T> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+template<class T>
+bool contain(set<T> &s, T a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+const int mod = 998244353;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
+
+class UnionFind {
+public:
+    // 親の番号を格納する。親だった場合-size
+    vector<int> parents;
+
+    UnionFind(int n) {
+        parents = vector<int>(n, -1);
+    }
+
+    // aがどのグループに属しているか
+    int root(int a) {
+        if (parents[a] < 0) {
+            return a;
+        }
+        return parents[a] = root(parents[a]);
+    }
+
+    int size(int a) {
+        return -parents[root(a)];
+    }
+
+    // aとbをくっつける
+    bool connect(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        if (ra == rb) {
+            return false;
+        }
+        // 大きいほうにA
+        if (size(ra) < size(rb)) {
+            swap(ra, rb);
+        }
+        parents[ra] += parents[rb];
+        parents[rb] = ra;
+        return true;
+    }
+
+    bool is_union(int a, int b) {
+        int ra = root(a);
+        int rb = root(b);
+        return ra == rb;
+    }
+};
+
+bool check(int n, vector<vector<int>> &sum, int i, int j, int k) {
+    rep(y, n) {
+        if (!(sum[y][i] + sum[y][j] <= k)) return false;
+    }
+    return true;
+}
+
+vector<int> grouping(int n, int k, vector<vector<int>> &sum) {
+    UnionFind uf(n);
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (check(n, sum, i, j, k)) {
+                uf.connect(i, j);
+            }
+        }
+    }
+    vector<int> ans;
+    rep(i, n) {
+        if (uf.root(i) == i) ans.push_back(uf.size(i));
+    }
+    return ans;
+}
+
+struct combination {
+    vector<mint> fact, ifact;
+
+    combination(int n) : fact(n + 1), ifact(n + 1) {
+        assert(n < mod);
+        fact[0] = 1;
+        for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
+        ifact[n] = fact[n].inv();
+        for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
+    }
+
+    mint operator()(int n, int k) {
+        if (k < 0 || k > n) return 0;
+        return fact[n] * ifact[k] * ifact[n - k];
+    }
+} combination(10000000);
+
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<vector<int>> board(n, vector<int>(n));
+    rep(y, n) rep(x, n) cin >> board[y][x];
+    vector<vector<int>> board2(n, vector<int>(n));
+    rep(y, n) rep(x, n) board2[x][y] = board[y][x];
+
+
+    auto v_groups = grouping(n, k, board), h_groups = grouping(n, k, board2);
+    mint ans = 1;
+    for (int s : v_groups) ans *= combination.fact[s];
+    for (int s : h_groups) ans *= combination.fact[s];
+    cout << ans << endl;
+}
