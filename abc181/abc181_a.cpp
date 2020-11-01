701152a38d4744470bbd881438d10af7e06650e8
riku <riku@tanide.net>
2020-11-01 21:00:39 +0900

    【AC】[AtCoder Beginner Contest 181]A - Heavy Rotation

---
+++ abc181/abc181_a.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, set<T> &o) {
+    T a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, queue<T> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+template<class T>
+bool contain(set<T> &s, T a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+string solve() {
+    int n;
+    cin >> n;
+    return (n % 2 == 0 ? "White" : "Black");
+}
+
+int main() {
+    cout << solve() << endl;
+}
