93f8d7c74ba72f60d43613e7714bd5aab9299269
riku <riku@tanide.net>
2020-11-04 03:20:42 +0900

    【RE】[2007年 日本情報オリンピック春合宿OJ]lines - 直線 (Lines)

--- joisc2007/joisc2007_lines.cpp
+++ joisc2007/joisc2007_lines.cpp
@@ -25,6 +25,7 @@ bool equal(double a, double b) {
     return fabs(a - b) < EPS;
 }
 
+
 template<class T>
 std::istream &operator>>(std::istream &in, set<T> &o) {
     T a;
@@ -45,9 +46,85 @@ template<class T>
 bool contain(set<T> &s, T a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+const int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
+
+
+bool equal(mint a, mint b) {
+    return a.x == b.x;
+}
 
 struct Point {
-    double x, y;
+    mint x, y;
 
     friend std::istream &operator>>(std::istream &in, Point &p) {
         in >> p.x >> p.y;
@@ -60,9 +137,9 @@ struct Point {
 
     friend bool operator<(Point p1, Point p2) {
         if (!equal(p1.x, p2.x)) {
-            return p1.x < p2.x;
+            return p1.x.x < p2.x.x;
         }
-        return p1.y < p2.y;
+        return p1.y.x < p2.y.x;
     }
 
     friend std::ostream &operator<<(std::ostream &out, Point &p) {
@@ -79,29 +156,29 @@ struct Line {
         return in;
     }
 
-    double slope() const {
+    mint slope() const {
         assert(!is_x());
         assert(!is_y());
-        double y = g.y - s.y;
-        double x = g.x - s.x;
+        mint y = g.y - s.y;
+        mint x = g.x - s.x;
         return y / x;
     }
 
-    double slope_e() const {
+    mint slope_e() const {
         if (is_y()) return 0;
         if (is_x()) return INF;
-        double y = g.y - s.y;
-        double x = g.x - s.x;
+        mint y = g.y - s.y;
+        mint x = g.x - s.x;
         return y / x;
     }
 
-    Point assign_x(double x) const {
-        double y = slope() * x + intercept();
+    Point assign_x(ll x) const {
+        mint y = slope() * x + intercept();
         return Point{x, y};
     }
 
-    Point assign_y(double y) const {
-        double x = (y - intercept()) / slope();
+    Point assign_y(ll y) const {
+        mint x = (mint(y) - intercept()) / slope();
         return Point{x, y};
     }
 
@@ -113,26 +190,26 @@ struct Line {
         return equal(s.y, g.y);
     }
 
-    double intercept() const {
+    mint intercept() const {
 
         assert(!is_x());
         assert(!is_y());
 
 
-        double y1 = s.y;
-        double y2 = g.y;
-        double x1 = s.x;
-        double x2 = g.x;
+        mint y1 = s.y;
+        mint y2 = g.y;
+        mint x1 = s.x;
+        mint x2 = g.x;
 
         return y1 - ((y2 - y1) / (x2 - x1)) * x1;
     }
 
     friend bool operator==(Line l1, Line l2) {
         if (l1.is_x() && l2.is_x()) {
-            return l1.s.x == l2.s.x;
+            return equal(l1.s.x, l2.s.x);
         }
         if (l1.is_y() && l2.is_y()) {
-            return l1.s.y == l2.s.y;
+            return equal(l1.s.y, l2.s.y);
         }
         if (!l1.is_x() && !l1.is_y() && !l2.is_x() && !l2.is_y()) {
             return equal(l1.slope(), l2.slope()) && equal(l1.intercept(), l2.intercept());
@@ -141,7 +218,7 @@ struct Line {
     }
 
     friend bool operator<(Line l1, Line l2) {
-        return l1.slope_e() < l2.slope_e();
+        return l1.slope_e().x < l2.slope_e().x;
     }
 
 };
@@ -159,26 +236,26 @@ bool is_intersection(const Line &l1, const Line &l2) {
 }
 
 Point calc_intersection_point(const Line &l1, const Line &l2) {
-    if (l2.is_x()) return calc_intersection_point(l2, l1);
-    if (l1.is_x()) {
-        if (l2.is_y()) return Point{l1.s.x, l2.s.y};
-        return l2.assign_x(l1.s.x);
-    }
-    if (l2.is_y()) return calc_intersection_point(l2, l1);
-    if (l1.is_y()) return l2.assign_y(l1.s.y);
+//    if (l2.is_x()) return calc_intersection_point(l2, l1);
+//    if (l1.is_x()) {
+//        if (l2.is_y()) return Point{l1.s.x, l2.s.y};
+//        return l2.assign_x(l1.s.x);
+//    }
+//    if (l2.is_y()) return calc_intersection_point(l2, l1);
+//    if (l1.is_y()) return l2.assign_y(l1.s.y);
 
     // https://qiita.com/uyuutosa/items/8de1f7602cb14c29606f
 
-    double a = l1.slope();
-    double b = l1.intercept();
+    mint a = l1.slope();
+    mint b = l1.intercept();
 
-    double c = l2.slope();
-    double d = l2.intercept();
+    mint c = l2.slope();
+    mint d = l2.intercept();
 
-    assert((a - c) != double(0));
+//    assert((a - c) != mint(0));
 
-    double x = (d - b) / (a - c);
-    double y = (a * d - b * c) / (a - c);
+    mint x = (d - b) / (a - c);
+    mint y = (a * d - b * c) / (a - c);
 
     return Point{x, y};
 
@@ -203,8 +280,8 @@ bool is_parallel(const Line &l1, const Line &l2) {
     if (!l1.is_x() && l2.is_x()) return false;
     if (!l1.is_y() && l2.is_y()) return false;
 
-    double s1 = l1.slope();
-    double s2 = l2.slope();
+    mint s1 = l1.slope();
+    mint s2 = l2.slope();
 
     return equal(s1, s2);
 }
@@ -226,8 +303,8 @@ bool is_duplicate(const Line &l1, const Line &l2) {
         return equal(l1.s.y, l2.s.y);
     }
 
-    double i1 = l1.intercept();
-    double i2 = l2.intercept();
+    mint i1 = l1.intercept();
+    mint i2 = l2.intercept();
 
     return equal(i1, i2);
 
@@ -247,9 +324,9 @@ int main() {
     cin >> n;
     vector<Line> lines(n);
     rep(i, n) cin >> lines[i];
-    sort(lines.begin(), lines.end());
-    lines.erase(unique(lines.begin(), lines.end()), lines.end());
-    n = lines.size();
+//    sort(lines.begin(), lines.end());
+//    lines.erase(unique(lines.begin(), lines.end()), lines.end());
+//    n = lines.size();
 
 //    sort(lines.begin(), lines.end(), [](Line l1, Line l2) {
 //        return l1.slope_e() < l2.slope_e();
@@ -278,7 +355,7 @@ int main() {
 //    cout << n << endl;
 //    cout << endl;
 
-    set<Point> unique_intersections;
+//    set<Point> unique_intersections;
     vector<int> intersection_count(n);
 //    ll ans = 0;
     rep(i, n) {
@@ -287,17 +364,20 @@ int main() {
         rep(j, i) {
 //            if (i == j) continue;
 //            ans += lines[i].s.x.num + lines[j].g.y.deno;
-            if (!is_intersection(lines[i], lines[j])) continue;
+            if (!is_intersection(lines[i], lines[j])) {
+                continue;
+            }
             Point p = calc_intersection_point(lines[i], lines[j]);
 //            intersection_count[i]++;
             st.insert(p);
-            unique_intersections.insert(p);
+//            unique_intersections.insert(p);
 //            intersections[i].push_back(p);
 
 //            cout << ' ' << p;
 
         }
         intersection_count[i] = st.size();
+//        cout << st.size() << endl;
 //        cout << endl;
     }
 
