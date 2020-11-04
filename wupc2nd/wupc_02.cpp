5918eba864b1f8740e4b2c1ae3d22e86d4ee347d
riku <riku@tanide.net>
2020-11-04 19:05:47 +0900

    【AC】[第2回早稲田大学プログラミングコンテスト ]B - 雨上がり

---
+++ wupc2nd/wupc_02.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+bool equal(double a, double b) {
+//    return fabs(a - b) < DBL_EPSILON;
+    return fabs(a - b) < EPS;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, set<T> &o) {
+    T a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, queue<T> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+template<class T>
+bool contain(set<T> &s, T a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    vector<int> seq(n, 0);
+    rep(i, n) {
+        if (s[i] == '.') continue;
+        else seq[i] = seq[i - 1] + 1;
+    }
+    vector<int> sizes;
+    rep(i, n - 1) {
+        if (s[i] == 'X' && s[i + 1] == '.')sizes.push_back(seq[i]);
+    }
+
+    int ans = 0;
+    for (int i : sizes) ans += (i / 3);
+    cout << ans << endl;
+
+}
