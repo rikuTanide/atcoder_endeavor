cf0ac6ffb156ce34b042cd481a210e7cfb514801
riku <riku@tanide.net>
2020-11-06 20:06:06 +0900

    【WA】[第2回早稲田大学プログラミングコンテスト ]E - 独立記念日

--- wupc2nd/wupc_05.cpp
+++ wupc2nd/wupc_05.cpp
@@ -85,22 +85,26 @@ public:
     }
 };
 
-int main() {
-    int n, m, k;
-    cin >> n >> m >> k;
+struct Road {
+    int from, to;
+    ll cost;
+
+    friend bool operator==(const Road &r1, const Road &r2) {
+        return r1.tov() == r2.tov();
+    }
+
+    vector<ll> tov() const {
+        return {from, to, cost};
+    }
+
+};
+
+ll solve_non_loop(int n, int m, int k, vector<Road> &roads) {
 
     UnionFind uf(n);
-
     vector<ll> costs(m);
-    rep(i, m) {
-        int a, b;
-        ll c;
-        cin >> a >> b >> c;
-        a--;
-        b--;
-        uf.connect(a, b);
-        costs[i] = c;
-    }
+    rep(i, m) costs[i] = roads[i].cost;
+    for (Road &r : roads) uf.connect(r.from, r.to);
     int f = 0;
     rep(i, n) if (i == uf.root(i)) f++;
 
@@ -111,5 +115,107 @@ int main() {
     rep(i, ct) {
         ans += costs[i];
     }
+    return ans;
+}
+
+bool
+rec(int n, int now, int prev,
+    vector<vector<int>> &edges, vector<int> &route, vector<bool> &used, vector<char> &memo) {
+    if (memo[now] != '-') return memo[now] == 'o';
+    route.push_back(now);
+    if (used[now]) return true;
+    used[now] = true;
+
+    for (int next : edges[now]) {
+        if (next == prev) continue;
+        bool found = rec(n, next, now, edges, route, used, memo);
+        if (found) {
+            memo[now] = 'o';
+            return true;
+        }
+    }
+    memo[now] = 'x';
+    used[now] = false;
+    route.pop_back();
+    return false;
+}
+
+vector<int> find_loop(int n, int m, vector<Road> &roads) {
+    vector<vector<int>> edges(n);
+    for (Road r : roads) edges[r.from].push_back(r.to);
+    for (Road r : roads) edges[r.to].push_back(r.from);
+
+    vector<char> memo(n, '-');
+    rep(i, n) {
+        vector<int> route;
+        vector<bool> used(n, false);
+        bool found = rec(n, i, -1, edges, route, used, memo);
+        if (found) {
+
+            int start = route.back();
+            reverse(route.begin(), route.end());
+            while (route.back() != start) {
+                route.pop_back();
+            }
+
+            return route;
+        }
+    }
+    return vector<int>(0);
+}
+
+Road find_min_loop_edge(vector<Road> &roads, vector<int> &loop) {
+    map<P, ll> costs;
+    for (Road &r: roads) costs[P(r.from, r.to)] = r.cost;
+
+    ll ma = 0;
+    rep(i, loop.size() - 1) {
+        int from = loop[i];
+        int to = loop[i + 1];
+        if (from > to)swap(from, to);
+        cmax(ma, costs[P(from, to)]);
+    }
+
+    rep(i, loop.size() - 1) {
+        int from = loop[i];
+        int to = loop[i + 1];
+        if (from > to)swap(from, to);
+        ll c = costs[P(from, to)];
+        if (c == ma) return Road{from, to, c};
+    }
+
+    assert(false);
+}
+
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    vector<Road> roads(m);
+    rep(i, m) {
+        int a, b;
+        ll c;
+        cin >> a >> b >> c;
+        a--;
+        b--;
+        roads[i].from = min(a, b);
+        roads[i].to = max(a, b);
+        roads[i].cost = c;
+    }
+
+    auto l = find_loop(n, m, roads);
+
+    if (l.empty()) {
+        ll ans = solve_non_loop(n, m, k, roads);
+        cout << ans << endl;
+        ret();
+    }
+
+//    for (int i  : l) cout << i + 1 << endl;
+
+    Road min_loop_edge = find_min_loop_edge(roads, l);
+    roads.erase(find(roads.begin(), roads.end(), min_loop_edge));
+//
+    ll ans = min_loop_edge.cost + solve_non_loop(n, m - 1, k, roads);
     cout << ans << endl;
 }
