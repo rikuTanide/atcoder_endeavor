92192a504fff1d7fe3e415686df1be113a0ff88d
riku <riku@tanide.net>
2020-11-07 15:21:31 +0900

    【WA】[第１２回日本情報オリンピック 予選（オンライン）]A - 宿題 (Homework)

---
+++ joi2013yo/joi2013yo_a.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+//#include "atcoder/all"
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
+}
+
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+std::istream &operator>>(std::istream &in, queue<int> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+const int mod = 1000000007;
+
+int main() {
+    ll l, a, b, c, d;
+    cin >> l >> a >> b >> c >> d;
+
+    ll p = (a + c - 1) / c;
+    ll q = (b + d - 1) / b;
+
+    ll r = max(p, q);
+    cout << l - r << endl;
+
+}
