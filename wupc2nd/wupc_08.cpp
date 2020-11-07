3d1106641d77babbb24713b8c03a75163f7e38dc
riku <riku@tanide.net>
2020-11-07 15:00:41 +0900

    【WA】[第2回早稲田大学プログラミングコンテスト ]H - ダイヤグラム

--- wupc2nd/wupc_08.cpp
+++ wupc2nd/wupc_08.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
+//#include "atcoder/all"
 //#include <boost/multiprecision/cpp_int.hpp>
 //namespace mp = boost::multiprecision;
-//#include "atcoder/all"
 
 using namespace std;
 
@@ -15,52 +15,125 @@ const ll INF = 10e17;
 #define cmax(x, y) x = max(x, y)
 #define ret() return 0;
 
-bool equal(double a, double b) {
-//    return fabs(a - b) < DBL_EPSILON;
-    return fabs(a - b) < EPS;
+double equal(double a, double b) {
+    return fabs(a - b) < DBL_EPSILON;
 }
 
-template<class T>
-std::istream &operator>>(std::istream &in, set<T> &o) {
-    T a;
+std::istream &operator>>(std::istream &in, set<int> &o) {
+    int a;
     in >> a;
     o.insert(a);
     return in;
 }
 
-template<class T>
-std::istream &operator>>(std::istream &in, queue<T> &o) {
+std::istream &operator>>(std::istream &in, queue<int> &o) {
     ll a;
     in >> a;
     o.push(a);
     return in;
 }
 
-template<class T>
-bool contain(set<T> &s, T a) { return s.find(a) != s.end(); }
+bool contain(set<int> &s, int a) { return s.find(a) != s.end(); }
 
 typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+const int mod = 1000000007;
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x = 0) : x((x % mod + mod) % mod) {}
+
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res += a;
+    }
+
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res -= a;
+    }
+
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod - 2);
+    }
+
+    mint &operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res /= a;
+    }
+
+    friend std::istream &operator>>(std::istream &in, mint &o) {
+        ll a;
+        in >> a;
+        o = a;
+        return in;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const mint &o) {
+        out << o.x;
+        return out;
+    }
+
+};
 
 int main() {
     int n, m;
     cin >> n >> m;
-    vector<P> trains(m);
-    for (P &p:trains)cin >> p.first >> p.second, p.first--, p.second--;
 
-    assert(n <= 8 && m <= 8);
+    vector<P> v(m);
+    for (P &p:v)cin >> p.first >> p.second;
 
-    int ans = 0;
-    rep(i, 1 << m) {
-        vector<int> ts(n);
-        rep(j, m) {
-            if (!((i >> j) & 1)) continue;
-            P p = trains[j];
-            for (int k = p.first; k <= p.second; k++) ts[k]++;
+    sort(v.begin(), v.end());
+
+    vector<vector<mint>> dp(n + 1, vector<mint>(n + 1, 0));
+    dp[0][0] = 1;
+
+    for (P p : v) {
+        vector<vector<mint>> next = dp;
+
+        for (int i = p.first; i <= p.second; i++) {
+            for (int j = i; j <= p.second; j++) {
+                next[j][p.second] += dp[i][j];
+            }
         }
-
-        int count = 0;
-        rep(j, n) if (ts[j] >= 2) count++;
-        if (count == n) ans++;
+        next[p.second][p.second] += dp[p.first - 1][p.second];
+        next[p.first - 1][p.second] += dp[p.first - 1][p.first - 1];
+        dp = next;
     }
-    cout << ans << endl;
+
+    cout << dp.back().back() << endl;
+
 }
