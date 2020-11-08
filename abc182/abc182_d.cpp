b380fe2c5c006dea05a4c71a35b1d4c8397438e7
riku <riku@tanide.net>
2020-11-08 21:32:58 +0900

    【AC】[AtCoder Beginner Contest 182]D - Wandering

---
+++ abc182/abc182_d.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+bool equal(double a, double b) {
+//    return fabs(a - b) < DBL_EPSILON;
+    return fabs(a - b) < EPS;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, set<T> &o) {
+    T a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, queue<T> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+template<class T>
+bool contain(set<T> &s, T a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+class CumulativeSum {
+    vector<ll> numbers;
+    vector<ll> sums;
+
+public:
+    CumulativeSum(int n) {
+        numbers.resize(n);
+        sums.resize(n);
+    }
+
+    void set(int i, ll value) {
+        numbers[i] = value;
+    }
+
+    ll getSum(int i) {
+        if (i == -1) return 0;
+        if (i == sums.size()) return sums.back();
+        return sums[i];
+    }
+
+    ll getSectionSum(int start, int end) {
+        return getSum(end) - getSum(start - 1);
+    }
+
+    void build() {
+        for (int i = 0; i < numbers.size(); i++) {
+            sums[i] = getSum(i - 1) + numbers[i];
+        }
+    }
+
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> v(n);
+    rep(i, n) cin >> v[i];
+
+    vector<ll> u;
+    rep(i, n) {
+        ll back = u.empty() ? 0 : u.back();
+
+        u.push_back(back + v[i]);
+    }
+
+    CumulativeSum cs(n);
+    rep(i, n) cs.set(i, v[i]);
+    cs.build();
+
+    vector<ll> t(n);
+    ll ma = -INF;
+    rep(i, n) {
+        ll now = cs.getSum(i);
+        cmax(ma, now);
+        t[i] = ma;
+    }
+
+    ll current = 0;
+    ll ans = 0;
+    rep(i, n) {
+        ll next = current + t[i];
+        cmax(ans, next);
+        current += u[i];
+    }
+
+    cout << ans << endl;
+
+}
