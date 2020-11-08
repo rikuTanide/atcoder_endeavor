bc5fe67f6b1be10d5652de8737f5f8ac95805c46
riku <riku@tanide.net>
2020-11-08 16:17:42 +0900

    【WA】[京都大学プログラミングコンテスト2013]A - 旧総合研究７号館

---
+++ kupc2013/kupc2013_a.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+bool equal(double a, double b) {
+//    return fabs(a - b) < DBL_EPSILON;
+    return fabs(a - b) < EPS;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, set<T> &o) {
+    T a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, queue<T> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+template<class T>
+bool contain(set<T> &s, T a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+struct Name {
+    int year;
+    string name;
+};
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    vector<Name> names(n);
+    for (Name &name: names)cin >> name.year >> name.name;
+    names.insert(names.begin(), {1, "kogakubu10gokan"});
+
+    rep(i, q) {
+        if (names[i + 1].year > q) {
+            cout << names[i].name << endl;
+            ret();
+        }
+    }
+
+
+}
