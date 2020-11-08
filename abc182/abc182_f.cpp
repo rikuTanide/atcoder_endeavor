6dced7352c4a724f9697e8d7e7740bbf47a64e9a
riku <riku@tanide.net>
2020-11-08 22:35:17 +0900

    【WA】[AtCoder Beginner Contest 182]F - Valid payments

---
+++ abc182/abc182_f.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//namespace mp = boost::multiprecision;
+//#include "atcoder/all"
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+const double EPS = 1e-9;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+typedef pair<ll, ll> P;
+const ll INF = 10e17;
+#define cmin(x, y) x = min(x, y)
+#define cmax(x, y) x = max(x, y)
+#define ret() return 0;
+
+bool equal(double a, double b) {
+//    return fabs(a - b) < DBL_EPSILON;
+    return fabs(a - b) < EPS;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, set<T> &o) {
+    T a;
+    in >> a;
+    o.insert(a);
+    return in;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &in, queue<T> &o) {
+    ll a;
+    in >> a;
+    o.push(a);
+    return in;
+}
+
+template<class T>
+bool contain(set<T> &s, T a) { return s.find(a) != s.end(); }
+
+typedef priority_queue<ll, vector<ll>, greater<ll> > PQ_ASK;
+
+void rec(vector<ll> v, ll x, set<ll> &used) {
+    if (v.empty()) return;
+    ll b = v.back();
+    ll t = (x + b - 1) / b;
+    used.insert(t);
+
+    ll u = x - (x / b * b);
+    v.pop_back();
+    rec(v, u, used);
+
+}
+
+int main() {
+    int n;
+    ll x;
+    cin >> n >> x;
+    vector<ll> v(n);
+    rep(i, n) cin >> v[i];
+
+    set<ll> used;
+    rec(v, x, used);
+    cout << used.size() << endl;
+}
